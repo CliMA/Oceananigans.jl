6c66a5c4c67e469f002cc82c01a632f0dbd48cf2
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2018-11-09 09:26:41 -0500

    Ignoring Seapickle package for faster development
    Sorry Greg!

--- examples/deep_convection.jl
+++ examples/deep_convection.jl
@@ -1,8 +1,34 @@
 using Statistics: mean
 
-using Seapickle
+import Makie
 
-include("../src/constants.jl")
+# using Seapickle
+# include("../src/Seapickle.jl")
+
+include("../src/operators.jl")
+include("../src/equation_of_state.jl")
+include("../src/pressure_solve.jl")
+
+# ### Physical constants.
+Ω = 7.2921150e-5  # Rotation rate of the Earth [rad/s].
+f = 1e-4  # Nominal value for the Coriolis frequency [rad/s].
+g = 9.80665  # Standard acceleration due to gravity [m/s²].
+
+# ### Numerical method parameters.
+χ = 0.1  # Adams-Bashforth (AB2) parameter.
+
+# ### Defining model parameters.
+const NumType = Float64  # Number data type.
+
+Nˣ, Nʸ, Nᶻ = 100, 100, 50  # Number of grid points in (x,y,z).
+Lˣ, Lʸ, Lᶻ = 2000, 2000, 1000  # Domain size [m].
+
+Δx, Δy, Δz = Lˣ/Nˣ, Lʸ/Nʸ, Lᶻ/Nᶻ  # Grid spacing [m].
+Aˣ, Aʸ, Aᶻ = Δy*Δz, Δx*Δz, Δx*Δy  # Cell face areas [m²].
+V = Δx*Δy*Δz  # Volume of a cell [m³].
+
+Nᵗ = 10  # Number of time steps to run for.
+Δt = 20  # Time step [s].
 
 # Initializing prognostic and diagnostic variable fields.
 uⁿ = Array{NumType, 3}(undef, Nˣ, Nʸ, Nᶻ)  # Velocity in x-direction [m/s].
@@ -19,6 +45,8 @@ Tˢ = 20  # Surface temperature [°C].
 Q₀ = 800  # Cooling disk flux? [W?].
 Q₁ = 10  # Noise added to cooling? [W?].
 Nˢ = 10 * (f*Rᶜ/Lᶻ)  # Stratification or Brunt–Väisälä frequency [s⁻¹].
+
+const αᵥ = 2.07e-4  # Volumetric coefficient of thermal expansion for water [K⁻¹].
 Tᶻ = Nˢ^2 / (g*αᵥ)  # Vertical temperature gradient [K/m].
 
 # Coordinates used to generate surface heat flux.
@@ -58,7 +86,7 @@ Fˢ = Array{NumType, 3}(undef, Nˣ, Nʸ, Nᶻ)
 uⁿ .= 0; vⁿ .= 0; wⁿ .= 0;
 
 # Tⁿ = repeat(reshape(T_ref, 1, 1, 50), Nˣ, Nʸ, 1)
-
+const ρ₀ = 1.027e3  # Reference density [kg/m³]
 pHY_profile = [-ρ₀*g*h for h in z₀]
 pʰʸ = repeat(reshape(pHY_profile, 1, 1, 50), Nˣ, Nʸ, 1)
 pⁿ = copy(pʰʸ)
@@ -143,3 +171,5 @@ for n in 1:Nᵗ
   Sⁿ = Sⁿ .+ Gˢⁿ⁺ʰ./Δt
   Tⁿ = Tⁿ .+ Gᵀⁿ⁺ʰ./Δt
 end
+
+Makie.volume(Tⁿ, algorithm = :mip)
