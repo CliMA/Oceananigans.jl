015d397c8dcc7893cea52a89c4ac16df14ed6258
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2018-11-12 16:49:55 -0500

    Trying different FFT plans.

--- src/pressure_solve.jl
+++ src/pressure_solve.jl
@@ -14,22 +14,36 @@ V = Δx*Δy*Δz  # Volume of a cell [m³].
 kˣ² = (2 / Δx^2) .* (cos.(π*(1:Nˣ) / Nˣ) .- 1)
 kʸ² = (2 / Δy^2) .* (cos.(π*(1:Nʸ) / Nʸ) .- 1)
 kᶻ² = (2 / Δz^2) .* (cos.(π*(1:Nᶻ) / Nᶻ) .- 1)
+# kˣ² = (2 / Δx^2) .* (sin.(π*(1:Nˣ) / Nˣ) .- 1)
+# kʸ² = (2 / Δy^2) .* (sin.(π*(1:Nʸ) / Nʸ) .- 1)
+# kᶻ² = (2 / Δz^2) .* (sin.(π*(1:Nᶻ) / Nᶻ) .- 1)
+# kˣ² = - (2 .* sin.(π*(1:Nˣ) / Nˣ) / Δx).^2
+# kʸ² = - (2 .* sin.(π*(1:Nʸ) / Nʸ) / Δy).^2
+# kᶻ² = - (2 .* sin.(π*(1:Nᶻ) / Nᶻ) / Δz).^2
 
 kˣ² = repeat(reshape(kˣ², Nˣ, 1, 1), 1, Nʸ, Nᶻ)
 kʸ² = repeat(reshape(kʸ², 1, Nʸ, 1), Nˣ, 1, Nᶻ)
 kᶻ² = repeat(reshape(kᶻ², 1, 1, Nᶻ), Nˣ, Nʸ, 1)
 
-prefactor = 1 ./ (kˣ² .+ kʸ² .+ kᶻ²)
+prefactor = - 1 ./ (kˣ² .+ kʸ² .+ kᶻ²)
 prefactor[1, 1, 1] = 0  # Solvability condition: DC component is zero.
 
 # Solve an elliptic Poisson equation ∇²ϕ = ℱ for the pressure field ϕ where
 # ℱ = ∇ ⋅ G and G = (Gᵘ,Gᵛ,Gʷ) using the Fourier-spectral method.
 function solve_for_pressure(Gᵘ, Gᵛ, Gʷ)
-  RHS = δˣ(Gᵘ) + δʸ(Gᵛ) + δᶻ(Gʷ)
+  RHS = div(Gᵘ, Gᵛ, Gʷ)
 
-  RHS_hat, fft_t, fft_bytes, fft_gc = @timed FFTW.r2r(FFTW.fft(RHS, [1, 2]), FFTW.REDFT10, 3)
+  # RHS_hat, fft_t, fft_bytes, fft_gc = @timed FFTW.fft(RHS)
+  RHS_hat, fft_t, fft_bytes, fft_gc = @timed FFTW.fft(FFTW.r2r(RHS, FFTW.REDFT10, 3), [1, 2])
+  # RHS_hat, fft_t, fft_bytes, fft_gc = @timed FFTW.r2r(FFTW.fft(RHS, [1, 2]), FFTW.REDFT10, 3)
+  # RHS_hat, fft_t, fft_bytes, fft_gc = @timed FFTW.r2r(RHS, FFTW.REDFT10)
+
   φ_hat, hat_t, hat_bytes, hat_gc = @timed prefactor .* RHS_hat
+
+  # φ, ifft_t, ifft_bytes, ifft_gc = @timed FFTW.ifft(φ_hat)
   φ, ifft_t, ifft_bytes, ifft_gc = @timed FFTW.r2r(FFTW.ifft(φ_hat, [1, 2]), FFTW.REDFT01, 3)
+  # φ, ifft_t, ifft_bytes, ifft_gc = @timed FFTW.fft(FFTW.r2r(φ_hat, FFTW.REDFT01, 3), [1, 2])
+  # φ, ifft_t, ifft_bytes, ifft_gc = @timed FFTW.r2r(φ_hat, FFTW.REDFT01)
 
   @info begin
     string("Fourier-spectral profiling:\n",
@@ -37,6 +51,6 @@ function solve_for_pressure(Gᵘ, Gᵛ, Gʷ)
            @sprintf("FFTc: (%.0f ms, %.2f MiB, %.1f%% GC)\n", hat_t*1000, hat_bytes/1024^2, hat_gc*100),
            @sprintf("iFFT: (%.0f ms, %.2f MiB, %.1f%% GC)\n", ifft_t*1000, ifft_bytes/1024^2, ifft_gc*100))
   end
-
+  @info "φ[50, 50, 1] = $(φ[50, 50, 1])"
   return real.(φ)
 end
