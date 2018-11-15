b8d1c088271e2060f796744bb4b0d0e6b5bbe571
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2018-11-14 22:05:22 -0500

    Much nicer way of imposing initial conditions!

--- examples/deep_convection_fast.jl
+++ examples/deep_convection_fast.jl
@@ -1,1 +1,59 @@
-import OceanDispatch
+using OceanDispatch
+
+function generate_cooling_disk!(c::PlanetaryConstants, g::Grid, Fũ::ForcingFields)
+    # Parameters for generating initial surface heat flux.
+    Rc = 600  # Radius of cooling disk [m].
+    Ts = 20  # Surface temperature [°C].
+    Q₀ = 800  # Cooling disk heat flux [W/m²].
+    Q₁ = 10  # Noise added to cooling disk heat flux [W/m²].
+    Ns = 0 * (c.f * Rc/g.Lz)  # Stratification or Brunt–Väisälä frequency [s⁻¹].
+
+    αᵥ = 2.07e-4  # Volumetric coefficient of thermal expansion for water [K⁻¹].
+    cᵥ = 4181.3   # Isobaric mass heat capacity [J / kg·K].
+
+    Tz = Ns^2 / (c.g * αᵥ)  # Vertical temperature gradient [K/m].
+
+    # Center horizontal coordinates so that (x,y) = (0,0) corresponds to the center
+    # of the domain (and the cooling disk).
+    x₀ = g.xCA[:, :, 1] .- mean(g.xCA[:, :, 1])
+    y₀ = g.yCA[:, :, 1] .- mean(g.yCA[:, :, 1])
+
+    # Calculate vertical temperature profile and convert to Kelvin.
+    T_ref = 273.15 .+ Ts .+ Tz .* (g.zCR .- mean(Tz * g.zCR))
+
+    # Set surface heat flux to zero outside of cooling disk of radius Rᶜ.
+    @. r₀² = x₀*x₀ + y₀*y₀
+
+    # Generate surface heat flux field with small random fluctuations.
+    Q = Q₀ .+ Q₁ * (0.5 .+ rand(g.Nx, g.Ny))
+    Q[findall(r₀² .> Rc^2)] .= 0  # Set Q=0 outside cooling disk.
+
+    # Convert surface heat flux into 3D forcing term for use when calculating
+    # source terms at each time step. Also convert surface heat flux [W/m²]
+    # into a temperature tendency forcing [K/s].
+    @. Fũ.FT[:, :, 1] = (Q / cᵥ) * (g.Az / M)
+end
+
+function impose_initial_conditions!(c::PlanetaryConstants, g::Grid, fs::Fields)
+    ρ₀ = 1.027e3  # Reference density [kg/m³]
+
+    fs.u .= 0; fs.v .= 0; fs.w .= 0;
+    fs.S .= 35;  # TODO: Should set to EOS.S₀
+    fs.T .= 273.15 + 20;  # 20°C.
+
+    pHY_profile = [-ρ₀ * c.g * h for h in g.zCR]
+    fs.pHY .= repeat(reshape(pHY_profile, 1, 1, g.Nz), g.Nx, g.Ny, 1)
+    fs.p .= fs.pHY  # Initial pressure is just the hydrostatic pressure.
+
+    ρⁿ .= ρ.(Tⁿ, Sⁿ, pⁿ)
+end
+
+c = EarthConstants()
+
+g =  RegularCartesianGrid((100, 100, 50), (2000, 2000, 1000), Float64)
+fs = Fields(g)
+Gũ = SourceTermFields(g)
+Fũ = ForcingFields(g)
+
+generate_cooling_disk!(c, g, Fũ)
+impose_initial_conditions!(g, fs)
