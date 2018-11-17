950d135e53c23d8a9f6c6770c795ccade1053114
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2018-11-17 14:49:55 -0500

    Spectral convergence!
    Turns out I was using the wavenumbers from my weird derivation for a 2nd-order accurate spectral method when if I just inserted the DFT into Poisson's equation you'd get out the proper wavenumbers.

--- src/spectral_solvers.jl
+++ src/spectral_solvers.jl
@@ -1,13 +1,16 @@
 import FFTW
 
 # Solve a 1D Poisson equation ∇²ϕ = d²ϕ/dx² = f(x) with periodic boundary
-# conditions and grid spacing Δx using a second-order Fourier-spectral method.
-# Right now the solver expects an odd number of points in f where the last point
-# is the periodic point and equal to the first
-function solve_poisson_1d_pbc(f, Δx)
-    n = 0:N            # Wavenumber indices.
-    k² = @. (4 / Δx^2) * sin(π*n / N)^2  # Wavenumber squared.
+# conditions and domain length L using the Fourier-spectral method.
+function solve_poisson_1d_pbc(f, L)
     N = length(f)  # Number of grid points (excluding the periodic end point).
+    n = 0:N        # Wavenumber indices.
+
+    # Second-order convergence.
+    # k² = @. (4 / Δx^2) * sin(π*n / N)^2  # Wavenumber squared.
+
+    # Spectral convergence.
+    k² = @. ((2*π / L) * n)^2
 
     # Forward transform the real-valued source term.
     fh = FFTW.rfft(f)
