5419e4085079866ae0a98e14d874a85e44f17488
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2018-11-17 17:22:27 -0500

    Working 2D Fourier-spectral Poisson solver!

--- src/spectral_solvers.jl
+++ src/spectral_solvers.jl
@@ -1,7 +1,11 @@
 import FFTW
 
 # Solve a 1D Poisson equation ∇²ϕ = d²ϕ/dx² = f(x) with periodic boundary
-# conditions and domain length L using the Fourier-spectral method.
+# conditions and domain length L using the Fourier-spectral method. Solutions to
+# Poisson's equation with periodic boundary conditions are unique up to a
+# constant so you may need to appropriately normalize the solution if you care
+# about the numerical value of the solution itself and not just derivatives of
+# the solution.
 function solve_poisson_1d_pbc(f, L)
     N = length(f)  # Number of grid points (excluding the periodic end point).
     n = 0:N        # Wavenumber indices.
@@ -26,4 +30,40 @@ function solve_poisson_1d_pbc(f, L)
     # Take the inverse transform of the . We need to specify that the input f
     # had a length of N+1 as
     ϕ = FFTW.irfft(ϕh, N)
+end
+
+# Solve a @D Poisson equation ∇²ϕ = f(x,y) with periodic boundary conditions and
+# domain lengths Lx, Ly using the Fourier-spectral method. Solutions to
+# Poisson's equation with periodic boundary conditions are unique up to a
+# constant so you may need to appropriately normalize the solution if you care
+# about the numerical value of the solution itself and not just derivatives of
+# the solution.
+function solve_poisson_2d_pbc(f, Lx, Ly)
+    Nx, Ny = size(f)  # Number of grid points (excluding the periodic end point).
+
+    # Forward transform the real-valued source term.
+    fh = FFTW.rfft(f)
+
+    # Wavenumbers. Can't figure out why this didn't work...
+    # l, m = 0:Nx, 0:Ny  # Wavenumber indices.
+    # kx² = @. ((2*π / Lx) * l)^2
+    # ky² = @. ((2*π / Ly) * m)^2
+    # ϕh = - fh ./ k²[1:Int(Nx/2 + 1), :]; ϕh[1, 1] = 0; ϕh[1, end] = 0; ϕh
+
+    # Wavenumbers that work.
+    l1 = 0:Int(Nx/2)
+    l2 = Int(-Nx/2 + 1):-1
+    m1 = 0:Int(Ny/2)
+    m2 = Int(-Ny/2 + 1):-1
+    kx = reshape((2π/Lx) * cat(l1, l2, dims=1), (Nx, 1))
+    ky = reshape((2π/Ly) * cat(m1, m2, dims=1), (1, Ny))
+    k² = @. kx^2 + ky^2
+
+    ϕh = - fh ./ k²[1:Int(Nx/2 + 1), :]
+
+    # Setting the DC/zero Fourier component to zero.
+    ϕh[1, 1] = 0
+
+    # Take the inverse transform of the solution's Fourier coefficients.
+    ϕ = FFTW.irfft(ϕh, Nx)
 end
