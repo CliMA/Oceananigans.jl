fabaf2b293690b3e2411786d5de5fabaeb6d53a5
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2018-12-08 11:01:03 -0500

    Clearer field documentation and fewer functions.

--- src/field.jl
+++ src/field.jl
@@ -1,13 +1,35 @@
 import Base: size, getindex, similar, *, +, -
 
+
+struct CellField{T,G<:Grid{T}} <: Field{G}
+    data::AbstractArray{T}
+    grid::G
+end
+
+struct FaceFieldX{T,G<:Grid{T}} <: FaceField{G}
+    data::AbstractArray{T}
+    grid::G
+end
+
+struct FaceFieldY{T,G<:Grid{T}} <: FaceField{G}
+    data::AbstractArray{T}
+    grid::G
+end
+
+struct FaceFieldZ{T,G<:Grid{T}} <: FaceField{G}
+    data::AbstractArray{T}
+    grid::G
+end
+
 """
     CellField(grid::Grid)
 
 Construct a `CellField` whose values are defined at the center of a cell.
 """
-struct CellField{T,G<:Grid{T}} <: Field{G}
-    data::AbstractArray{T}
-    grid::G
+function CellField(grid::Grid{T}) where T <: AbstractFloat
+    sz = size(grid)
+    data = zeros(T, sz)
+    CellField(data, grid)
 end
 
 """
@@ -15,9 +37,10 @@ end
 
 A `Field` whose values are defined on the x-face of a cell.
 """
-struct FaceFieldX{T,G<:Grid{T}} <: FaceField{G}
-    data::AbstractArray{T}
-    grid::G
+function FaceFieldX(grid::Grid{T}) where T <: AbstractFloat
+    sz = size(grid)
+    data = zeros(T, sz)
+    FaceFieldX(data, grid)
 end
 
 """
@@ -25,9 +48,10 @@ end
 
 A `Field` whose values are defined on the y-face of a cell.
 """
-struct FaceFieldY{T,G<:Grid{T}} <: FaceField{G}
-    data::AbstractArray{T}
-    grid::G
+function FaceFieldY(grid::Grid{T}) where T <: AbstractFloat
+    sz = size(grid)
+    data = zeros(T, sz)
+    FaceFieldY(data, grid)
 end
 
 """
@@ -35,51 +59,26 @@ end
 
 A `Field` whose values are defined on the z-face of a cell.
 """
-struct FaceFieldZ{T,G<:Grid{T}} <: FaceField{G}
-    data::AbstractArray{T}
-    grid::G
-end
-
-function CellField(grid::Grid{T}) where T
-    sz = size(grid)
-    data = zeros(T, sz)
-    CellField(data, grid)
-end
-
-function FaceFieldX(grid::Grid{T}) where T
-    sz = size(grid)
-    data = zeros(T, sz)
-    FaceFieldX(data, grid)
-end
-
-function FaceFieldY(grid::Grid{T}) where T
-    sz = size(grid)
-    data = zeros(T, sz)
-    FaceFieldY(data, grid)
-end
-
-function FaceFieldZ(grid::Grid{T}) where T
+function FaceFieldZ(grid::Grid{T}) where T <: AbstractFloat
     sz = size(grid)
     data = zeros(T, sz)
     FaceFieldZ(data, grid)
 end
 
-similar(f::CellField) = CellField(f.grid)
-size(f::CellField) = size(f.grid)
+size(f::Field) = size(f.grid)
+show(io::IO, f::Field) = show(io, f.data)
 
+# TODO: This will not work if T=Float32 and v::Irrational.
 set!(u::Field, v) = @. u.data = v
 set!(u::Field, v::Field) = @. u.data = v.data
 
-# TODO: Revise this using just xC, yC, zC.
-# set!(u::Field{G}, f::Function) where {G<:RegularCartesianGrid} = @. u.data = f(u.grid.xCA, u.grid.yCA, u.grid.zCA)
-
+similar(f::CellField) = CellField(f.grid)
 similar(f::FaceFieldX{T,G}) where {T,G} = FaceFieldX(f.grid)
 similar(f::FaceFieldY{T,G}) where {T,G} = FaceFieldY(f.grid)
 similar(f::FaceFieldZ{T,G}) where {T,G} = FaceFieldZ(f.grid)
-size(f::FaceFieldX{T,G}) where {T,G} = size(f.grid)
-size(f::FaceFieldY{T,G}) where {T,G} = size(f.grid)
-size(f::FaceFieldZ{T,G}) where {T,G} = size(f.grid)
 
+# TODO: Revise this using just xC, yC, zC.
+# set!(u::Field{G}, f::Function) where {G<:RegularCartesianGrid} = @. u.data = f(u.grid.xCA, u.grid.yCA, u.grid.zCA)
 getindex(f::Field, inds...) = getindex(f.data, inds...)
 
 # Define +, -, and * on fields as element-wise calculations on their data. This
