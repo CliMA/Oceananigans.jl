384a42393b91dce70f9a387fd289e01ea14b9ed0
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2018-12-20 11:43:52 -0500

    Split temporary fieldset into operator and stepper sets.

--- src/fieldset.jl
+++ src/fieldset.jl
@@ -32,7 +32,22 @@ struct ForcingFields <: FieldSet
     FS::CellField
 end
 
-struct TemporaryFields <: FieldSet
+struct OperatorTemporaryFields <: FieldSet
+    fC1::CellField
+    fC2::CellField
+    fC3::CellField
+    fC4::CellField
+    fFX::FaceFieldX
+    fFX2::FaceFieldX
+    fFY::FaceFieldY
+    fFY2::FaceFieldX
+    fFZ::FaceFieldZ
+    fFZ2::FaceFieldX
+    fCC1::CellField
+    fCC2::CellField
+end
+
+struct StepperTemporaryFields <: FieldSet
     fC1::CellField
     fC2::CellField
     fC3::CellField
@@ -86,7 +101,7 @@ function ForcingFields(g)
     ForcingFields(Fu, Fv, Fw, FT, FS)
 end
 
-function TemporaryFields(g)
+function OperatorTemporaryFields(g)
     fC1 = CellField(g)
     fC2 = CellField(g)
     fC3 = CellField(g)
@@ -99,5 +114,21 @@ function TemporaryFields(g)
     fFZ2 = FaceFieldX(g)
     fCC1 = CellField(g, Complex{eltype(g)})
     fCC2 = CellField(g, Complex{eltype(g)})
-    TemporaryFields(fC1, fC2, fC3, fC4, fFX, fFX2, fFY, fFY2, fFZ, fFZ2, fCC1, fCC2)
+    OperatorTemporaryFields(fC1, fC2, fC3, fC4, fFX, fFX2, fFY, fFY2, fFZ, fFZ2, fCC1, fCC2)
+end
+
+function StepperTemporaryFields(g)
+    fC1 = CellField(g)
+    fC2 = CellField(g)
+    fC3 = CellField(g)
+    fC4 = CellField(g)
+    fFX = FaceFieldX(g)
+    fFX2 = FaceFieldX(g)
+    fFY = FaceFieldY(g)
+    fFY2 = FaceFieldX(g)
+    fFZ = FaceFieldZ(g)
+    fFZ2 = FaceFieldX(g)
+    fCC1 = CellField(g, Complex{eltype(g)})
+    fCC2 = CellField(g, Complex{eltype(g)})
+    StepperTemporaryFields(fC1, fC2, fC3, fC4, fFX, fFX2, fFY, fFY2, fFZ, fFZ2, fCC1, fCC2)
 end
