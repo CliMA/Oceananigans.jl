9a71fcffc1e920889430c89d09b2bf4d8f47a54f
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2019-01-10 11:27:06 -0500

    Wittled down simple 2D example to just several lines.

--- examples/isotropic_diffusion_2d.jl
+++ examples/isotropic_diffusion_2d.jl
@@ -10,8 +10,10 @@ using Interact, Plots
 
 using Oceananigans
 
-function make_movies(R::SavedFields)
-    print("Creating tracer movie... ($(Int(Nt/ΔR)) frames)\n")
+function make_movies(problem::Problem, R::SavedFields, Nt, Δt)
+    g = problem.g
+
+    print("Creating tracer movie... ($(Int(Nt/R.ΔR)) frames)\n")
 
     Plots.gr()
 
@@ -23,12 +25,12 @@ function make_movies(R::SavedFields)
     # end
     # mp4(animU, "uvel_$(round(Int, time())).mp4", fps = 30)
 
-    animT = @animate for tidx in 1:Int(Nt/ΔR)
-        print("\rframe = $tidx / $(Int(Nt/ΔR))   ")
+    animT = @animate for tidx in 1:Int(Nt/R.ΔR)
+        print("\rframe = $tidx / $(Int(Nt/R.ΔR))   ")
         Plots.heatmap(g.xC ./ 1000, g.zC ./ 1000, rotl90(R.T[tidx, :, 1, :]) .- 283, color=:balance,
                       clims=(-0.1, 0),
                       # clims=(-maximum(R.T[tidx, :, 1, :] .- 283), maximum(R.T[tidx, :, 1, :] .- 283)),
-                      title="T change @ t=$(tidx*ΔR*Δt)")
+                      title="T change @ t=$(tidx*R.ΔR*Δt)")
     end
     mp4(animT, "tracer_T_$(round(Int, time())).mp4", fps = 30)
 
@@ -43,15 +45,16 @@ function make_movies(R::SavedFields)
 end
 
 function isotropic_diffusion_2d()
-    N = (100, 1, 50)
-    L = (2000, 1, 1000)
-    problem = Problem(N, L)
+    Nx, Ny, Nz = 100, 1, 50
+    Lx, Ly, Lz = 2000, 1, 1000
+    Nt, Δt = 5000, 10
 
+    problem = Problem((Nx, Ny, Nz), (Lx, Ly, Lz))
     @. problem.F.FT.data[Int(problem.g.Nx/10):Int(9problem.g.Nx/10), 1, 1] = -0.5e-5 + 1e-6*rand()
 
     R  = SavedFields(problem.g, 5000, 10)
 
-    time_stepping!(problem; Nt=5000, Δt=10, R=R)
+    time_stepping!(problem; Nt=Nt, Δt=Δt, R=R)
 
-    make_movies(R)
+    make_movies(problem, R, Nt, Δt)
 end
