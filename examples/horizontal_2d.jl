f9f0e0cb06dd9406e582e351673b38f7c87d9fa7
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2019-01-29 13:29:04 -0500

    Fixed make_movies for horizontal slices.

--- examples/horizontal_2d.jl
+++ examples/horizontal_2d.jl
@@ -12,10 +12,8 @@ function make_movies(problem::Problem, R::SavedFields, Nt, Δt)
 
     animT = @animate for tidx in 1:Int(Nt/R.ΔR)
         print("\rframe = $tidx / $(Int(Nt/R.ΔR))   ")
-        Plots.heatmap(g.xC, g.yC, rotl90(R.T[tidx, :, :, 1]) .- 283, color=:balance,
-                      clims=(0, 0.01),
-                      # clims=(-maximum(R.T[tidx, :, 1, :] .- 283), maximum(R.T[tidx, :, 1, :] .- 283)),
-                      title="T change @ t=$(tidx*R.ΔR*Δt)")
+        Plots.heatmap(g.xC, g.yC, R.T[tidx, :, :, 1] .- 283, color=:balance,
+                      clims=(0, 0.01), title="T change @ t=$(tidx*R.ΔR*Δt)")
     end
     mp4(animT, "tracer_T_$(round(Int, time())).mp4", fps = 30)
 end
@@ -26,7 +24,7 @@ function horizontal_2d()
     Nt, Δt, ΔR = 2500, 20, 10
 
     problem = Problem((Nx, Ny, Nz), (Lx, Ly, Lz));
-    @. problem.tr.T.data[10:30, 10:30, 1] = 283.01;
+    @. problem.tr.T.data[70:90, 10:30, 1] = 283.01;
     @. problem.U.u.data = 0.1;
     @. problem.U.v.data = 0.1;
 
