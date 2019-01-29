77d1df00e83a004bbc8e068ba9974f35115d1b07
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2019-01-28 20:00:08 -0500

    Benchmarking local/element-wise operator, it's even slower.

--- benchmark/gpu.jl
+++ benchmark/gpu.jl
@@ -68,16 +68,59 @@ Time(%)      Time     Calls       Avg       Min       Max  Name
   0.01%     758ns         2     379ns     314ns     444ns  cuDeviceGet
   0.01%     625ns         1     625ns     625ns     625ns  cuCtxGetDevice
 '''
+
+@inline incmod1(a, n) = a == n ? 1 : a+1
+δx(f, Nx, i, j, k) = f[incmod1(i, Nx), j, k] - f[i, j, k]
+
+function time_stepping(f, δxf)
+    Nx, Ny, Nz = size(f)
+    for k in 1:Nz, j in 1:Ny, i in 1:Nx
+        δxf[i, j, k] = δx(f, Nx, i, j, k)
+    end
+end
+
 '''
-julia> using CuArrays
-julia> xc = rand(100, 100, 100); yc = rand(100, 100, 100);
-julia> xg = cu(rand(100, 100, 100)); yg = cu(rand(100, 100, 100));
+julia> using BenchmarkTools
+julia> Nx, Ny, Nz = 128, 128, 128;
+julia> xc = rand(Nx, Ny, Nz); yc = zeros(Nx, Ny, Nz);
+julia> @benchmark time_stepping(xc, yc)
+BenchmarkTools.Trial:
+  memory estimate:  0 bytes
+  allocs estimate:  0
+  --------------
+  minimum time:     3.377 ms (0.00% GC)
+  median time:      3.387 ms (0.00% GC)
+  mean time:        3.449 ms (0.00% GC)
+  maximum time:     3.865 ms (0.00% GC)
+  --------------
+  samples:          1449
+  evals/sample:     1
+'''
 
-julia> @btime δx!(xc, yc, 100, 100, 100)
-  979.906 μs (0 allocations: 0 bytes)
+'''
+~ nvprof --profile-from-start off /home/alir/julia/bin/julia
+julia> using CUDAnative, CUDAdrv, CuArrays
+julia> Nx, Ny, Nz = 128, 128, 128;
+julia> xg = cu(rand(Float32, Nx, Ny, Nz)); yg = cu(rand(Float32, Nx, Ny, Nz));
+julia> CUDAdrv.@profile @cuda threads=128 time_stepping(xg, yg)
+julia> CUDAdrv.@profile @cuda threads=128 time_stepping(xg, yg)
 
-julia> @time δx!(xg, yg, 100, 100, 100)
-   19.749605 seconds (15.00 M allocations: 671.387 MiB, 0.24% gc time)
+==55354== Profiling application: /home/alir/julia/bin/julia
+==55354== Profiling result:
+Time(%)      Time     Calls       Avg       Min       Max  Name
+100.00%  2.86728s         2  1.43364s  1.42919s  1.43808s  ptxcall_time_stepping_2
+
+==55354== API calls:
+Time(%)      Time     Calls       Avg       Min       Max  Name
+ 99.82%  98.188ms         1  98.188ms  98.188ms  98.188ms  cuModuleLoadDataEx
+  0.14%  138.82us         2  69.411us  51.497us  87.326us  cuLaunchKernel
+  0.02%  14.819us         1  14.819us  14.819us  14.819us  cuProfilerStart
+  0.01%  11.707us         6  1.9510us     173ns  9.1930us  cuDeviceGetAttribute
+  0.01%  7.3640us         6  1.2270us     200ns  2.9550us  cuCtxGetCurrent
+  0.00%  4.5960us         1  4.5960us  4.5960us  4.5960us  cuDeviceGetCount
+  0.00%     977ns         2     488ns     486ns     491ns  cuCtxGetDevice
+  0.00%     930ns         1     930ns     930ns     930ns  cuModuleGetFunction
+  0.00%     807ns         2     403ns     288ns     519ns  cuDeviceGet
 '''
 
 @inline incmod1(i, n) = 1 + (i % n)
