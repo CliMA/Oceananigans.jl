ea5e5aab45a719fb090b2ba7665aabd8893a84d9
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2019-02-08 21:55:11 -0500

    Awesome! Deep convection still works!

--- examples/deep_convection_2d.jl
+++ examples/deep_convection_2d.jl
@@ -1,20 +1,12 @@
 using Plots
 using Oceananigans
 
-function make_movies(problem::Problem, R::SavedFields, Nt, Δt)
-    g = problem.g
+function make_movies(model::Model, R::SavedFields, Nt, Δt)
+    g = model.grid
 
     print("Creating tracer movie... ($(Int(Nt/R.ΔR)) frames)\n")
 
     Plots.gr()
-
-    # animU = @animate for tidx in 1:Int(Nt/ΔR)
-    #     print("\rframe = $tidx / $(Int(Nt/ΔR))   ")
-    #     Plots.heatmap(g.xC ./ 1000, g.zC ./ 1000, rotl90(R.u[tidx, :, 1, :]), color=:balance,
-    #                   clims=(-0.01, 0.01),
-    #                   title="u-velocity @ t=$(tidx*ΔR*Δt)")
-    # end
-    # mp4(animU, "uvel_$(round(Int, time())).mp4", fps = 30)
 
     animT = @animate for tidx in 1:Int(Nt/R.ΔR)
         print("\rframe = $tidx / $(Int(Nt/R.ΔR))   ")
@@ -23,15 +15,6 @@ function make_movies(problem::Problem, R::SavedFields, Nt, Δt)
                       title="T change @ t=$(tidx*R.ΔR*Δt)")
     end
     mp4(animT, "deep_convection_2d_$(round(Int, time())).mp4", fps = 30)
-
-    # animρ = @animate for tidx in 1:Int(Nt/ΔR)
-    #     print("\rframe = $tidx / $(Int(Nt/ΔR))   ")
-    #     Plots.heatmap(g.xC ./ 1000, g.zC ./ 1000, rotl90(R.ρ[tidx, :, 1, :]) .- eos.ρ₀, color=:balance,
-    #                   clims=(-0.001, 0.001),
-    #                   # clims=(-maximum(R.ρ[tidx, :, 1, :] .- eos.ρ₀), maximum(R.ρ[tidx, :, 1, :] .- eos.ρ₀)),
-    #                   title="delta rho @ t=$(tidx*ΔR*Δt)")
-    # end
-    # mp4(animρ, "tracer_δρ_$(round(Int, time())).mp4", fps = 30)
 end
 
 function deep_convection_2d()
@@ -40,12 +23,12 @@ function deep_convection_2d()
     Nt, Δt = 2500, 20
     ΔR = 10
 
-    problem = Problem((Nx, Ny, Nz), (Lx, Ly, Lz))
-    @. problem.F.FT.data[Int(problem.g.Nx/10):Int(9problem.g.Nx/10), 1, 1] = -0.5e-5 + 1e-6*rand()
+    model = Model((Nx, Ny, Nz), (Lx, Ly, Lz))
+    @. model.forcings.FT.data[Int(Nx/10):Int(9Nx/10), 1, 1] = -0.5e-5 + 1e-6*rand()
 
-    R  = SavedFields(problem.g, Nt, ΔR)
+    R  = SavedFields(model.grid, Nt, ΔR)
 
-    time_stepping!(problem; Nt=Nt, Δt=Δt, R=R)
+    time_step!(model; Nt=Nt, Δt=Δt, R=R)
+    make_movies(model, R, Nt, Δt)
 
-    make_movies(problem, R, Nt, Δt)
 end
