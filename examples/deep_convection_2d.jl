57c01908b4a974b986b53b460798526f06963e28
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2019-02-09 12:33:46 -0500

    Making movies with FieldWriter works!

--- examples/deep_convection_2d.jl
+++ examples/deep_convection_2d.jl
@@ -1,20 +1,24 @@
 using Plots
 using Oceananigans
 
-function make_movies(model::Model, R::SavedFields, Nt, Δt)
-    g = model.grid
+function make_temperature_movie(model::Model, fw::FieldWriter)
+    n_frames = Int(model.clock.time_step / fw.output_frequency)
 
-    print("Creating tracer movie... ($(Int(Nt/R.ΔR)) frames)\n")
+    xC, zC = model.grid.xC, model.grid.zC
+    Δt = 20
+
+    print("Creating temperature movie... ($n_frames frames)\n")
 
     Plots.gr()
-
-    animT = @animate for tidx in 1:Int(Nt/R.ΔR)
-        print("\rframe = $tidx / $(Int(Nt/R.ΔR))   ")
-        Plots.heatmap(g.xC ./ 1000, g.zC ./ 1000, rotl90(R.T[tidx, :, 1, :]) .- 283, color=:balance,
+    movie = @animate for tidx in 0:n_frames
+        print("\rframe = $tidx / $n_frames   ")
+        temperature_xz = read_output(model, fw, "T", tidx*fw.output_frequency*Δt)
+        Plots.heatmap(xC, zC, rotl90(temperature_xz[:, 1, :]) .- 283, color=:balance,
                       clims=(-0.02, 0),
-                      title="T change @ t=$(tidx*R.ΔR*Δt)")
+                      title="T change @ t=$(tidx*fw.output_frequency*Δt)")
     end
-    mp4(animT, "deep_convection_2d_$(round(Int, time())).mp4", fps = 30)
+
+    mp4(movie, "deep_convection_2d_$(round(Int, time())).mp4", fps = 30)
 end
 
 function deep_convection_2d()
@@ -26,10 +30,16 @@ function deep_convection_2d()
     model = Model((Nx, Ny, Nz), (Lx, Ly, Lz))
     @. model.forcings.FT.data[Int(Nx/10):Int(9Nx/10), 1, 1] = -0.5e-5 + 1e-6*rand()
 
-    push!(model.output_writers, Checkpointer(".", "deep_convection_2d", 1000))
+    checkpointer = Checkpointer(".", "deep_convection_2d", 1000)
+    field_writer = FieldWriter(".", "deep_convection_2d", 100,
+                               [model.velocities.u, model.tracers.T],
+                               ["u", "T"])
+
+    push!(model.output_writers, checkpointer)
+    push!(model.output_writers, field_writer)
 
     R  = SavedFields(model.grid, Nt, ΔR)
 
     time_step!(model; Nt=Nt, Δt=Δt, R=R)
-    make_movies(model, R, Nt, Δt)
+    make_temperature_movie(model, field_writer)
 end
