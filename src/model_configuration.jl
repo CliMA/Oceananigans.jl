e1be1f0e3f067cdb4236eb5e7b962e0c772ce4d1
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2019-02-11 18:41:17 -0500

    Wrong units on eddy viscosity. Thanks @jm-c

--- src/model_configuration.jl
+++ src/model_configuration.jl
@@ -1,8 +1,8 @@
 struct ModelConfiguration
     κh # Horizontal Laplacian heat diffusion [m²/s]. diffKhT in MITgcm.
     κv # Vertical Laplacian heat diffusion [m²/s]. diffKzT in MITgcm.
-    𝜈h # Horizontal eddy viscosity [Pa·s]. viscAh in MITgcm.
-    𝜈v # Vertical eddy viscosity [Pa·s]. viscAz in MITgcm.
+    𝜈h # Horizontal eddy viscosity [m²/s]. viscAh in MITgcm.
+    𝜈v # Vertical eddy viscosity [m²/s]. viscAz in MITgcm.
 end
 
 function _ModelConfiguration(κh, κv, 𝜈h, 𝜈v)
