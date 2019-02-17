50dab9709efd140b17651ac632658ecc24538fa4
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-02-17 16:48:10 -0500

    Easier to debug.

--- examples/deep_convection_3d_gpu.jl
+++ examples/deep_convection_3d_gpu.jl
@@ -5,26 +5,30 @@ using Oceananigans
 function deep_convection_3d_gpu()
     Nx, Ny, Nz = 128, 128, 64
     Lx, Ly, Lz = 2000, 2000, 1000
-    Nt, Δt = 50, 20
+    Nt, Δt = 20, 20
 
     model = Model((Nx, Ny, Nz), (Lx, Ly, Lz), :gpu, Float32)
 
-    T_initial = zeros(Nx, Ny, Nz)
+    T_initial = 293.15 .* ones(Nx, Ny, Nz)
     forcing = zeros(Nx, Ny, Nz)
     i1, i2, j1, j2 = Int(round(Nx/10)), Int(round(9Nx/10)), Int(round(Ny/10)), Int(round(9Ny/10))
     
-    @. T_initial[i1:i2, j1:j2, 1] += 0.01*rand()
+    @. T_initial[i1:i2, j1:j2, 1] += 0.01
     @. forcing[i1:i2, j1:j2, 1] = -0.25e-5
     
     model.tracers.T.data .= cu(T_initial)
     model.forcings.FT.data .= cu(forcing)
 
-    field_writer = FieldWriter(".", "deep_convection_2d", 50,
-                               [model.velocities.u, model.tracers.T],
-                               ["u", "T"])
-
+    field_writer = FieldWriter(".", "deep_convection_3d", 10, [model.tracers.T], ["T"])
     push!(model.output_writers, field_writer)
-
+    
+    u, v, w, T, S, ρ = model.velocities.u, model.velocities.v, model.velocities.w, model.tracers.T, model.tracers.S, model.tracers.ρ
+    pNHS = model.pressures.pNHS
+    Gu, Gv, Gw, GT, GS = model.G.Gu, model.G.Gv, model.G.Gw, model.G.GT, model.G.GS
+    field_summaries = FieldSummary(1, [u, v, w, T, S, ρ, pNHS, Gu, Gv, Gw, GT, GS],
+                                   ["u", "v", "w", "T", "S", "ρ", "pNHS", "Gu", "Gv", "Gw", "GT", "GS"])
+    # push!(model.diagnostics, field_summaries)
+    
     time_step_kernel!(model, Nt, Δt)
 
     # time_step!(model; Nt=Nt, Δt=Δt)
