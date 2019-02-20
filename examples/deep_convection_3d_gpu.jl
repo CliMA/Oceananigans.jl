fa9684a684dba72a98956f8430c3b97690f0484a
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-02-20 00:41:48 -0500

    Using circular cooling disk.

--- examples/deep_convection_3d_gpu.jl
+++ examples/deep_convection_3d_gpu.jl
@@ -1,56 +1,73 @@
 # using Plots
-using GPUifyLoops, CUDAnative, CuArrays
+using Statistics
+using CuArrays, CUDAnative, GPUifyLoops
 using Oceananigans
 
 function deep_convection_3d_gpu()
     Nx, Ny, Nz = 128, 128, 64
     Lx, Ly, Lz = 2000, 2000, 1000
-    Nt, Δt = 20, 20
+    Nt, Δt = 1000, 20
 
     model = Model((Nx, Ny, Nz), (Lx, Ly, Lz), :gpu, Float32)
+    impose_initial_conditions!(model)
 
-    T_initial = 293.15 .* ones(Nx, Ny, Nz)
-    forcing = zeros(Nx, Ny, Nz)
-    i1, i2, j1, j2 = Int(round(Nx/10)), Int(round(9Nx/10)), Int(round(Ny/10)), Int(round(9Ny/10))
-    
-    @. T_initial[i1:i2, j1:j2, 1] += 0.01
-    @. forcing[i1:i2, j1:j2, 1] = -0.25e-5
-    
-    model.tracers.T.data .= cu(T_initial)
-    model.forcings.FT.data .= cu(forcing)
-
-    field_writer = FieldWriter(".", "deep_convection_3d", 10, [model.tracers.T], ["T"])
+    field_writer = FieldWriter(".", "deep_convection_3d_gpu", 10, [model.tracers.T], ["T"])
     push!(model.output_writers, field_writer)
     
-    u, v, w, T, S, ρ = model.velocities.u, model.velocities.v, model.velocities.w, model.tracers.T, model.tracers.S, model.tracers.ρ
-    pNHS = model.pressures.pNHS
-    Gu, Gv, Gw, GT, GS = model.G.Gu, model.G.Gv, model.G.Gw, model.G.GT, model.G.GS
-    field_summaries = FieldSummary(1, [u, v, w, T, S, ρ, pNHS, Gu, Gv, Gw, GT, GS],
-                                   ["u", "v", "w", "T", "S", "ρ", "pNHS", "Gu", "Gv", "Gw", "GT", "GS"])
-    # push!(model.diagnostics, field_summaries)
-    
     time_step_kernel!(model, Nt, Δt)
-
-    # time_step!(model; Nt=Nt, Δt=Δt)
-    # make_temperature_movie(model, field_writer)
 end
 
-# function make_temperature_movie(model::Model, fw::FieldWriter)
-#     n_frames = Int(model.clock.time_step / fw.output_frequency)
+function impose_cooling_disk!(model::Model)
+    g = model.grid
+    c = model.constants
 
-#     xC, zC = model.grid.xC, model.grid.zC
-#     Δt = 20
+    # Parameters for generating initial surface heat flux.
+    Rc = 600  # Radius of cooling disk [m].
+    Ts = 20  # Surface temperature [°C].
+    Q₀ = -800  # Cooling disk heat flux [W/m²].
+    Q₁ = 10  # Noise added to cooling disk heat flux [W/m²].
+    Ns = 5 * (c.f * Rc/g.Lz)  # Stratification or Brunt–Väisälä frequency [s⁻¹].
 
-#     print("Creating temperature movie... ($n_frames frames)\n")
+    αᵥ = 2.07e-4  # Volumetric coefficient of thermal expansion for water [K⁻¹].
+    cᵥ = 4181.3   # Isobaric mass heat capacity [J / kg·K].
 
-#     Plots.gr()
-#     movie = @animate for tidx in 0:n_frames
-#         print("\rframe = $tidx / $n_frames   ")
-#         temperature_xz = read_output(model, fw, "T", tidx*fw.output_frequency*Δt)
-#         Plots.heatmap(xC, zC, rotl90(temperature_xz[:, 1, :]) .- 283, color=:balance,
-#                       clims=(-0.02, 0),
-#                       title="T change @ t=$(tidx*fw.output_frequency*Δt)")
-#     end
+    Tz = Ns^2 / (c.g * αᵥ)  # Vertical temperature gradient [K/m].
 
-#     mp4(movie, "deep_convection_2d_$(round(Int, time())).mp4", fps = 30)
-# end
+    # Center horizontal coordinates so that (x,y) = (0,0) corresponds to the center
+    # of the domain (and the cooling disk).
+    x₀ = g.xC .- mean(g.xC)
+    y₀ = g.yC .- mean(g.yC)
+
+    # Calculate vertical temperature profile and convert to Kelvin.
+    T_ref = 273.15 .+ Ts .+ Tz .* (g.zC .- mean(Tz * g.zC))
+
+    # Impose reference temperature profile.
+    model.tracers.T.data .= cu(repeat(reshape(T_ref, 1, 1, g.Nz), g.Nx, g.Ny, 1))
+
+    # Set surface heat flux to zero outside of cooling disk of radius Rᶜ.
+    r₀² = @. x₀*x₀ + y₀'*y₀'
+
+    # Generate surface heat flux field with small random fluctuations.
+    Q = Q₀ .+ Q₁ * (0.5 .+ rand(g.Nx, g.Ny))
+    Q[findall(r₀² .> Rc^2)] .= 0  # Set Q=0 outside cooling disk.
+
+    # Convert surface heat flux into 3D forcing term for use when calculating
+    # source terms at each time step. Also convert surface heat flux [W/m²]
+    # into a temperature tendency forcing [K/s].
+    T_forcing = zeros(g.Nx, g.Ny, g.Nz)
+    T_forcing[:, :, 1] = (Q / cᵥ) * (g.Az / (model.eos.ρ₀ * g.V))
+    model.forcings.FT.data .= cu(T_forcing)
+    nothing
+end
+
+function impose_initial_conditions!(model::Model)
+    g = model.grid
+
+    impose_cooling_disk!(model)
+
+    @. model.tracers.S.data = model.eos.S₀
+
+    pHY_profile = [-model.eos.ρ₀ * model.constants.g * h for h in g.zC]
+    model.pressures.pHY.data .= cu(repeat(reshape(pHY_profile, 1, 1, g.Nz), g.Nx, g.Ny, 1))
+    nothing
+end
