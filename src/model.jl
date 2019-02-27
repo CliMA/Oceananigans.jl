a86e2d87339048b265ac2e50a9a53f38c60b97d4
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2019-02-27 15:17:30 -0500

    Implements a new keyword-based model constructor

--- src/model.jl
+++ src/model.jl
@@ -20,54 +20,94 @@ mutable struct Model
     diagnostics::Array{Diagnostic,1}
 end
 
-function Model(N, L, arch=:cpu, float_type=Float64)
-    metadata = _ModelMetadata(arch, float_type)
-    configuration = _ModelConfiguration(4e-2, 4e-2, 4e-2, 4e-2)
-    boundary_conditions = _BoundaryConditions(:periodic, :periodic, :rigid_lid, :free_slip)
+"""
+    Model(; kwargs...)
 
-    constants = Earth()
-    eos = LinearEquationOfState()
+Construct an `Oceananigans.jl` model. The keyword arguments are:
 
-    grid = RegularCartesianGrid(metadata, N, L)
+          N (tuple) : model resolution in (x, y, z)
+          L (tuple) : model domain in (x, y, z)
+         dt (float) : time step
+ start_time (float) : start time for the simulation
+  time_step (float) : ?
+         arch (sym) : architecture (:cpu or :gpu)
+  float_type (type) : floating point type for model data (typically Float32 or Float64)
+          constants : planetary constants (?)
+                eos : equation of state to infer density from temperature and salinity
+            forcing : forcing functions for (u, v, w, T, S)
+boundary_conditions : boundary conditions
+     output_writers : output writer
+        diagonstics : diagnostics
+"""
+function Model(;
+    # Model resolution and domain size
+             N = (32, 1, 32),
+             L = (1.0, 1.0, 1.0),
+    # Molecular parameters
+             ν = 1.05e-6,
+            νh = ν,
+            νv = ν,
+             κ = 1.43e-7,
+            κh = κ,
+            κv = κ,
+    # Time stepping
+            dt = 1e-16,
+    start_time = 0,
+     time_step = 0, # ?
+    # Model architecture and floating point precision
+          arch = :cpu,
+    float_type = Float64,
+     constants = Earth(),
+    # Equation of State
+           eos = LinearEquationOfState(),
+    # Forcing and boundary conditions for (u, v, w, T, S)
+       forcing = Forcing(nothing, nothing, nothing, nothing, nothing),
+    boundary_conditions = _BoundaryConditions(:periodic, :periodic, :rigid_lid, :free_slip),
+    # Output and diagonstics
+    output_writers = OutputWriter[],
+       diagnostics = Diagnostic[]
+)
 
-    velocities  = VelocityFields(metadata, grid)
-    tracers = TracerFields(metadata, grid)
-    pressures = PressureFields(metadata, grid)
-    G  = SourceTerms(metadata, grid)
-    Gp = SourceTerms(metadata, grid)
-    forcings = ForcingFields(metadata, grid)
-    stepper_tmp = StepperTemporaryFields(metadata, grid)
+    # Initialize model basics.
+         metadata = _ModelMetadata(arch, float_type)
+    configuration = _ModelConfiguration(νh, νv, κh, κv)
+             grid = RegularCartesianGrid(metadata, N, L)
+            clock = Clock(start_time, time_step, dt)
+
+    # Initialize fields, including source terms and temporary variables.
+      velocities = VelocityFields(metadata, grid)
+         tracers = TracerFields(metadata, grid)
+       pressures = PressureFields(metadata, grid)
+               G = SourceTerms(metadata, grid)
+              Gp = SourceTerms(metadata, grid)
+        forcings = ForcingFields(metadata, grid)
+     stepper_tmp = StepperTemporaryFields(metadata, grid)
     operator_tmp = OperatorTemporaryFields(metadata, grid)
 
-    forcing = Forcing(nothing, nothing, nothing, nothing, nothing)
-
-    time, time_step, Δt = 0, 0, 0
-    clock = Clock(time, time_step, Δt)
-
-    output_writers = OutputWriter[]
-    diagnostics = Diagnostic[]
-
+    # Initialize Poisson solver.
     if metadata.arch == :cpu
         stepper_tmp.fCC1.data .= rand(metadata.float_type, grid.Nx, grid.Ny, grid.Nz)
-        ssp = SpectralSolverParameters(grid, stepper_tmp.fCC1, FFTW.PATIENT; verbose=true)
+        ssp = SpectralSolverParameters(grid, stepper_tmp.fCC1, FFTW.MEASURE; verbose=true)
     elseif metadata.arch == :gpu
         stepper_tmp.fCC1.data .= CuArray{Complex{Float64}}(rand(metadata.float_type, grid.Nx, grid.Ny, grid.Nz))
         ssp = SpectralSolverParametersGPU(grid, stepper_tmp.fCC1)
     end
 
-    # Setting some initial configuration.
-    velocities.u.data  .= 0
-    velocities.v.data  .= 0
-    velocities.w.data  .= 0
+    # Default initial condition
+    velocities.u.data .= 0
+    velocities.v.data .= 0
+    velocities.w.data .= 0
     tracers.S.data .= 35
     tracers.T.data .= 283
 
+    # ?
     pHY_profile = [-eos.ρ₀*constants.g*h for h in grid.zC]
 
+    # ?
     if metadata.arch == :cpu
         pressures.pHY.data .= repeat(reshape(pHY_profile, 1, 1, grid.Nz), grid.Nx, grid.Ny, 1)
     elseif metadata.arch == :gpu
-        pressures.pHY.data .= cu(repeat(reshape(pHY_profile, 1, 1, grid.Nz), grid.Nx, grid.Ny, 1))
+        pressures.pHY.data .= CuArray(repeat(reshape(pHY_profile, 1, 1, grid.Nz), grid.Nx, grid.Ny, 1))
     end
 
     # Calculate initial density based on tracer values.
@@ -77,3 +117,6 @@ function Model(N, L, arch=:cpu, float_type=Float64)
           velocities, tracers, pressures, G, Gp, forcings, forcing,
           stepper_tmp, operator_tmp, ssp, clock, output_writers, diagnostics)
 end
+
+"Legacy constructor for `Model`."
+Model(N, L; arch=:cpu, float_type=Float64) = Model(N=N, L=L; arch=arch, float_type=float_type)
