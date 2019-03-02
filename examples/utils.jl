6692eb577c57aabfae0336a466b2f9e8c60c284b
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2019-03-02 18:57:43 -0500

    Improved example: Barotropic tracer advection.

--- examples/utils.jl
+++ examples/utils.jl
@@ -19,6 +19,24 @@ function make_vertical_slice_movie(model::Model, nc_writer::NetCDFOutputWriter, 
     mp4(animation, nc_writer.filename_prefix * "$(round(Int, time())).mp4", fps=30)
 end
 
+function make_horizontal_slice_movie(model::Model, nc_writer::NetCDFOutputWriter, var_name, Nt, Δt, var_offset=0)
+    freq = nc_writer.output_frequency
+    N_frames = Int(Nt/freq)
+
+    print("Producing movie... ($N_frames frames)\n")
+    Plots.gr(dpi=150)
+
+    animation = @animate for n in 0:N_frames
+        print("\rframe = $n / $N_frames   ")
+        var = read_output(nc_writer, var_name, freq*n)
+        Plots.heatmap(model.grid.xC, model.grid.yC, var[:, :, 1] .- var_offset,
+                      color=:balance, clims=(-0.01, 0.01),
+                      title="t=$(freq*n*Δt) s ($(round(freq*n*Δt/86400; digits=2)) days)")
+    end
+
+    mp4(animation, nc_writer.filename_prefix * "$(round(Int, time())).mp4", fps=30)
+end
+
 function make_vertical_profile_movie(model::Model, nc_writer::NetCDFOutputWriter, var_name, Nt, Δt, var_offset=0)
     freq = nc_writer.output_frequency
     N_frames = Int(Nt/freq)
@@ -32,6 +50,6 @@ function make_vertical_profile_movie(model::Model, nc_writer::NetCDFOutputWriter
         Plots.plot(var[1, 1, :] .- var_offset, model.grid.zC,
                    title="t=$(freq*n*Δt) s ($(round(freq*n*Δt/86400; digits=2)) days)")
     end
-    
+
     mp4(animation, nc_writer.filename_prefix * "$(round(Int, time())).mp4", fps=30)
 end
