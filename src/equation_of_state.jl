8b34f51c43409702074337185ed70aabaf8b5b56
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2019-03-11 06:44:49 -0400

    Removing global operators that we shouldn't use

--- src/equation_of_state.jl
+++ src/equation_of_state.jl
@@ -31,27 +31,3 @@ function LinearEquationOfState()
     αᵥ = 2.07e-4
     LinearEquationOfState(ρ₀, βT, βS, βp, T₀, S₀, p₀, cᵥ, αᵥ)
 end
-
-function ρ!(s::LinearEquationOfState, g::Grid, tr::TracerFields)
-    for k in 1:g.Nz, j in 1:g.Ny, i in 1:g.Nx
-        @inbounds tr.ρ.data[i, j, k] =  s.ρ₀ * (1 - s.βT * (tr.T.data[i, j, k] - s.T₀))
-    end
-    nothing
-end
-
-function δρ!(s::LinearEquationOfState, g::Grid, δρ::CellField, T::CellField)
-    for k in 1:g.Nz, j in 1:g.Ny, i in 1:g.Nx
-        @inbounds δρ.data[i, j, k] =  - s.ρ₀ * s.βT * (T.data[i, j, k] - s.T₀)
-    end
-    nothing
-end
-
-function ∫δρgdz!(g::Grid, c::PlanetaryConstants, δρ::CellField, δρz::FaceFieldZ, pHY′::CellField)
-    gΔz = c.g * g.Δz
-    for j in 1:g.Ny, i in 1:g.Nx
-      pHY′.data[i, j, 1] = δρ.data[i, j, 1] * gΔz / 2
-    end
-    for k in 2:g.Nz, j in 1:g.Ny, i in 1:g.Nx
-      pHY′.data[i, j, k] = pHY′.data[i, j, k-1] + (δρz.data[i, j, k] * gΔz)
-    end
-end
