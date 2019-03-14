ea0c6207fb7cd78ee723012f96edf69ca2715ccc
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-03-14 16:34:16 +0000

    Hacky util to plot vertical temperature profile.

--- examples/utils.jl
+++ examples/utils.jl
@@ -1,6 +1,8 @@
 using Plots
 using Oceananigans
 
+using Statistics
+
 """
     make_vertical_slice_movie(model::Model, nc_writer::NetCDFOutputWriter,
                               var_name, Nt, Δt, var_offset=0, slice_idx=1)
@@ -86,4 +88,36 @@ function make_vertical_profile_movie(model::Model, nc_writer::NetCDFOutputWriter
     end
 
     mp4(animation, nc_writer.filename_prefix * "$(round(Int, time())).mp4", fps=30)
+end
+
+using NetCDF
+
+function make_avg_temperature_profile_movie()
+    Nt, dt = 86400, 0.5
+    freq = 3600
+    N_frames = Int(Nt/freq)
+    filename_prefix = "convection"
+    var_offset = 273.15
+    
+    Nz, Lz = 128, 100
+    dz = Lz/Nz
+    zC = -dz/2:-dz:-Lz 
+
+    print("Producing movie... ($N_frames frames)\n")
+    Plots.gr(dpi=150)
+
+    animation = @animate for n in 0:N_frames
+        print("\rframe = $n / $N_frames   ")
+
+        filepath = filename_prefix * lpad(freq*n, 9, "0") * ".nc"
+        field_data = ncread(filepath, "T")
+        ncclose(filepath)
+        
+        T_profile = mean(field_data; dims=[1,2])
+        
+        Plots.plot(reshape(T_profile, Nz) .- var_offset, zC,
+                   title="t=$(freq*n*dt) s ($(round(freq*n*dt/86400; digits=2)) days)")
+    end
+
+    mp4(animation, filename_prefix * "$(round(Int, time())).mp4", fps=30)
 end
