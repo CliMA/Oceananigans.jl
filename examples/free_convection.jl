d02bfeb48eadca5399a757756ac734f6d2d93f94
SandreOuza <andrenogeirasouza@gmail.com>
2019-03-15 22:18:07 +0000

    adds details to free_convection example

--- examples/free_convection.jl
+++ examples/free_convection.jl
@@ -1,23 +1,32 @@
 using Oceananigans
 using CuArrays
 
+#physical constants
+p0 = 1027
+cp = 4181.3
+
+#set simulation parameters
 Nx, Ny, Nz = 128, 128, 128
 Lx, Ly, Lz = 100, 100, 100
-Nt, Δt = 10000, 1
-ν, κ = 1e-2, 1e-2
-
-model = Model(N=(Nx, Ny, Nz), L=(Lx, Ly, Lz), ν=ν, κ=κ,arch=:GPU,float_type=Float32)
-model.boundary_conditions = BoundaryConditions(:periodic, :periodic, :rigid_lid, :free_slip)
-
-#@inline fT(u,v,w,T,S,Nx,Ny,Nz,dx,dy,dz,i,j,k ) = ifelse(k ==1, -9e-6,0)
-#model.forcing = Forcing(nothing,nothing,nothing, fT, nothing)
-
-N² = 2e-5
+Nt, Δt = 10000, 6
+ν, κ = 1e-4, 1e-4
 Tz = 0.01
-T_prof = 273.15 .+ 20 .+ Tz .* model.grid.zC
+bottom_gradient = Tz
+top_flux = 75 / (p0 * cp) #for flux bc this should be positive for cooling
 
+#create the model
+model = Model(N=(Nx, Ny, Nz), L=(Lx, Ly, Lz), ν=ν, κ=κ, arch=:GPU, float_type=Float32)
+
+#set boundary conditions
+model.boundary_conditions.T.z.left = BoundaryCondition(Flux,top_flux)
+model.boundary_conditions.T.z.right  = BoundaryCondition(Gradient, bottom_gradient)
+#the default boundary conditions for velocity is free-slip
+
+#set initial condition
+T_prof = 273.15 .+ 20 .+ Tz .* model.grid.zC
+#make initial condition into 3D array
 T_3d = repeat(reshape(T_prof, 1, 1, Nz), Nx, Ny, 1)
-@. T_3d[:, :, 1:round(Int, Nz/2)] += 0.01*rand()
+@. T_3d[:, :, 1:round(Int, Nz/2)] += 0.01*randn() #add noise to the top half of the domain for convection
 
 model.tracers.T.data .= CuArray(T_3d)
 
