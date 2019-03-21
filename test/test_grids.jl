52b924a22d324f7681dc3e5fb8199b180bb23931
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2019-03-21 15:04:14 -0400

    Proper grid tests for Float32.
    Former-commit-id: 6da23ca3dc87eabdf2d6a9cf46ce2fc01ccbe971

--- test/test_grids.jl
+++ test/test_grids.jl
@@ -1,18 +1,19 @@
 function test_grid_size(mm::ModelMetadata)
     g = RegularCartesianGrid(mm, (4, 6, 8), (2π, 4π, 9π))
 
-    (g.Nx == 4  && g.Ny == 6  && g.Nz == 8 &&
-     g.Lx == 2π && g.Ly == 4π && g.Lz == 9π)
+    # Checking ≈ as the grid could be storing Float32 values.
+    (g.Nx ≈ 4  && g.Ny ≈ 6  && g.Nz ≈ 8 &&
+     g.Lx ≈ 2π && g.Ly ≈ 4π && g.Lz ≈ 9π)
 end
 
 function test_cell_volume(mm::ModelMetadata)
     Nx, Ny, Nz = 19, 13, 7
     Δx, Δy, Δz = 0.1, 0.2, 0.3
-    V = Δx*Δy*Δz
     Lx, Ly, Lz = Nx*Δx, Ny*Δy, Nz*Δz
     g = RegularCartesianGrid(mm, (Nx, Ny, Nz), (Lx, Ly, Lz))
 
-    V == g.V
+    # Checking ≈ as the grid could be storing Float32 values.
+    g.V ≈ Δx*Δy*Δz
 end
 
 function test_faces_start_at_zero(mm::ModelMetadata)
