9e107bba9824c1b98477bed6d558fa0d71560c4a
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2019-03-26 05:04:29 -0400

    Renaming tests with Boolean sounding names.
    Also getting rid of redundant test_ prefixes for all test functions.

--- test/test_grids.jl
+++ test/test_grids.jl
@@ -1,4 +1,4 @@
-function test_grid_size(ft::DataType)
+function correct_grid_size(ft::DataType)
     g = RegularCartesianGrid(ft, (4, 6, 8), (2π, 4π, 9π))
 
     # Checking ≈ as the grid could be storing Float32 values.
@@ -6,7 +6,7 @@ function test_grid_size(ft::DataType)
      g.Lx ≈ 2π && g.Ly ≈ 4π && g.Lz ≈ 9π)
 end
 
-function test_cell_volume(ft::DataType)
+function correct_cell_volume(ft::DataType)
     Nx, Ny, Nz = 19, 13, 7
     Δx, Δy, Δz = 0.1, 0.2, 0.3
     Lx, Ly, Lz = Nx*Δx, Ny*Δy, Nz*Δz
@@ -16,12 +16,12 @@ function test_cell_volume(ft::DataType)
     g.V ≈ Δx*Δy*Δz
 end
 
-function test_faces_start_at_zero(ft::DataType)
+function faces_start_at_zero(ft::DataType)
     g = RegularCartesianGrid(ft, (10, 10, 10), (2π, 2π, 2π))
     g.xF[1] == 0 && g.yF[1] == 0 && g.zF[1] == 0
 end
 
-function test_end_faces_match_grid_length(ft::DataType)
+function end_faces_match_grid_length(ft::DataType)
     g = RegularCartesianGrid(ft, (12, 13, 14), (π, π^2, π^3))
     (g.xF[end] - g.xF[1] ≈ π   &&
      g.yF[end] - g.yF[1] ≈ π^2 &&
