177b388b7e9d9bbd70c5889a3a919ecb250d5672
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-04-15 17:49:22 -0400

    Account for halo regions in Model constructor.

--- src/models.jl
+++ src/models.jl
@@ -63,26 +63,32 @@ function Model(;
 
     arch == GPU() && !HAVE_CUDA && throw(ArgumentError("Cannot create a GPU model. No CUDA-enabled GPU was detected!"))
 
+    Nx, Ny, Nz = N
+
     # Initialize model basics.
     configuration = ModelConfiguration(νh, νv, κh, κv)
              grid = RegularCartesianGrid(float_type, N, L)
             clock = Clock{float_type}(start_time, iteration)
 
-    # Initialize fields, including source terms and temporary variables.
+    # Initialize fields including source terms
       velocities = VelocityFields(arch, grid)
          tracers = TracerFields(arch, grid)
        pressures = PressureFields(arch, grid)
                G = SourceTerms(arch, grid)
               Gp = SourceTerms(arch, grid)
 
-     poisson_solver = PoissonSolver(arch, grid)
+    # Initialize Poisson solver.
+    poisson_solver = PoissonSolver(arch, grid)
 
     # Default initial condition
     velocities.u.data .= 0
     velocities.v.data .= 0
     velocities.w.data .= 0
-    tracers.S.data .= eos.S₀
-    tracers.T.data .= eos.T₀
+    tracers.T.data .= 0
+    tracers.S.data .= 0
+
+    @views tracers.T.data[1:Nx, 1:Ny, 1:Nz] .= eos.T₀
+    @views tracers.S.data[1:Nx, 1:Ny, 1:Nz] .= eos.S₀
 
     Model{typeof(arch)}(configuration, boundary_conditions, constants, eos, grid,
                         velocities, tracers, pressures, G, Gp, forcing,
