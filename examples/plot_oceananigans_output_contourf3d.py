61fd91f4f6851e982cd5ad1c2f2a0fe2f7e4ddc6
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2019-04-20 10:11:19 -0400

    Diagnostic plots for T, TKE, and buoyancy flux. (#185)

--- examples/plot_oceananigans_output_contourf3d.py
+++ examples/plot_oceananigans_output_contourf3d.py
@@ -4,16 +4,19 @@ matplotlib.use("Agg")
 
 import os
 import numpy as np
-from numpy import ones, meshgrid, linspace
+from numpy import ones, meshgrid, linspace, square, mean
 import xarray as xr
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import axes3d
 
 plt.switch_backend("Agg")
 
 from joblib import Parallel, delayed
 
 def plot_contourf3d_from_netcdf(nc_filepath, png_filepath, var, dt, vmin, vmax, n_contours, cmap="inferno", var_offset=0, time=0):
+    # For some reason I need to do the import here so it shows up on all joblib workers.
+    from mpl_toolkits.mplot3d import Axes3D
+    
     # Enforcing style in here so that it's applied to all workers launched by joblib.
     plt.style.use("dark_background")
 
@@ -39,8 +42,9 @@ def plot_contourf3d_from_netcdf(nc_filepath, png_filepath, var, dt, vmin, vmax, 
     data = nc_output[var]
 
     fig = plt.figure(figsize=(16, 9))
-    # ax = fig.gca(projection="3d")
-    ax = Axes3D(fig)  # This is the proper call. See: https://stackoverflow.com/questions/3810865/matplotlib-unknown-projection-3d-error
+
+    ax = plt.subplot2grid((3, 4), (0, 0), rowspan=3, colspan=3, projection="3d")
+    plt.subplots_adjust(left=0.05, bottom=0.05, right=0.95, top=0.95, hspace=0.25)
 
     data_z_slice = data.sel(yC=slice(0, Lx//2), zC=zC[0]).values - var_offset
     data_x_slice = data.sel(xC=xC[-1], yC=slice(0, Lx//2)).values - var_offset
@@ -81,6 +85,40 @@ def plot_contourf3d_from_netcdf(nc_filepath, png_filepath, var, dt, vmin, vmax, 
     ax.set_yticks(linspace(0, Ly, num=5))
     ax.set_zticks(linspace(0, -Lz, num=5))
 
+    T_profile = nc_output["T"].mean(dim=["xC", "yC"])
+
+    ax2 = plt.subplot2grid((3, 4), (0, 3))
+    ax2.plot(T_profile.sel(zC=slice(0, -25)) - 273.15, zC.sel(zC=slice(0, -25)))
+    ax2.set_title(r"$\overline{T}(z)$ [°C]")
+    ax2.set_ylabel("z (m)")
+    ax2.set_xlim(19.75, 20)
+    ax2.set_ylim(-25, 0)
+
+    u, v, w = nc_output["u"], nc_output["v"], nc_output["w"]
+    HTKE_profile = 0.5 * mean(square(u.values) + square(v.values), axis=(1, 2))
+    VTKE_profile = 0.5 * square(w).mean(dim=["xC", "yC"])
+
+    ax3 = plt.subplot2grid((3, 4), (1, 3))
+    ax3.plot(HTKE_profile[0:Nz//4], zC.sel(zC=slice(0, -25)).values, color="tab:orange", label=r"$(u^2 + v^2)/2$")
+    ax3.plot(10 * VTKE_profile.sel(zF=slice(0, -25)), zC.sel(zC=slice(0, -25)), color="tab:green", label=r"$10 \times w^2/2$")
+    ax3.set_title("Turbulent kinetic energy [m$^2$/s$^2$]")
+    ax3.set_ylabel("z (m)")
+    ax3.set_xlim(-0.001, 0.02)
+    ax3.set_ylim(-25, 0)
+    ax3.legend(loc="lower right")
+
+    alpha = 2.07e-4
+    g = 9.80665
+    T = nc_output["T"]
+    buoyancy_flux_profile = mean(alpha * g * w.values * T.values, axis=(1, 2))
+
+    ax4 = plt.subplot2grid((3, 4), (2, 3))
+    ax4.plot(buoyancy_flux_profile[0:Nz//4], zC.sel(zC=slice(0, -25)).values, color="tab:red")
+    ax4.set_title(r"Buoyancy flux $\alpha g \overline{w' T'}$")
+    ax4.set_ylabel("z (m)")
+    ax4.set_xlim(-5e-8, 5e-8)
+    ax4.set_ylim(-25, 0)
+
     # plt.show()
 
     plt.savefig(png_filepath, dpi=300, format="png", transparent=False)
@@ -102,8 +140,8 @@ if __name__ == "__main__":
     # Plot many frames from many NetCDF files in parallel.
     freq = 20  # Output frequency in iterations.
     dt = 0.25
-    Parallel(n_jobs=-1)(
+    Parallel(n_jobs=32)(
         delayed(plot_contourf3d_from_netcdf)(nc_filepath=input_filepath(n), png_filepath=output_filepath(n), var="T",
                                             dt=dt, vmin=19, vmax=20.05, n_contours=400, cmap="prism", var_offset=273.15, time=n*dt*freq)
-        for n in [0, 10, 100, 400, 1000] # np.arange(0, 1000)
+        for n in np.arange(0, 2000)
     )
