e9cc5eb8960fb9f380da8908d92c35a7f7df6c92
Gregory Wagner <wagner.greg@gmail.com>
2019-04-25 08:50:11 -0700

    Adds an example that dmonstrates how to work with the field abstraction system

---
+++ examples/working_with_fields.jl
@@ -0,0 +1,18 @@
+using Pkg; Pkg.activate("..")
+
+using Oceananigans, PyPlot
+
+import PyPlot: Plot
+
+zplot(c::CellField, args...; kwargs...) = plot(znodes(c), data(c), args...; kwargs...)
+
+grid = RegularCartesianGrid((1, 1, 128), (1, 1, 1))
+T = CellField(grid)
+
+H = grid.Lz
+
+softstep(z, d) = 0.5*(tanh(z/d) + 1)
+T0(x, y, z) = 20 - 0.01 * z + 0.001 * softstep(z+H/2, H/10) * rand()
+model.tracers.T = T0
+
+plot(model.tracers.T)
