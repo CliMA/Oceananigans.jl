23b0cd15e90061d1f0113627569ae34d53976510
Gregory Wagner <wagner.greg@gmail.com>
2019-05-24 13:36:32 -0400

    uncomments main tests!

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,7 +14,6 @@ float_types = (Float32, Float64)
 @testset "Oceananigans" begin
     println("Testing Oceananigans...")
 
-    #=
     @testset "Grid" begin
         println("  Testing grids...")
         include("test_grids.jl")
@@ -332,7 +331,6 @@ float_types = (Float32, Float64)
             @test test_diffusion_cosine(fld)
         end
     end
-    =#
 
     @testset "Turbulence closures tests" begin
         println("  Testing turbulence closures...")
