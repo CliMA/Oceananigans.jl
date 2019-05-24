25240a564a6cf9fbc4b74f662c4094a54521680f
Gregory Wagner <wagner.greg@gmail.com>
2019-05-24 13:36:08 -0400

    runs closure tests for different float types, imports TurbulenceClosures into runtests

--- test/test_turbulence_closures.jl
+++ test/test_turbulence_closures.jl
@@ -1,25 +1,11 @@
-function test_closure_instantiation(; closures=(
-        :ConstantSmagorinsky,
-        :ConstantIsotropicDiffusivity,
-        :DirectionalDiffusivity)
-        )
-
-    results = Bool[]
-
-    for closurename in closures
-        closure64 = getproperty(TurbulenceClosures, closurename)()
-        closure32 = getproperty(TurbulenceClosures, closurename)(Float32)
-
-        push!(results, eltype(closure64) == Float64)
-        push!(results, eltype(closure32) == Float32)
-    end
-
-    return all(results)
+function test_closure_instantiation(T, closurename)
+    closure = getproperty(TurbulenceClosures, closurename)(T)
+    return eltype(closure) == T
 end
 
-function test_function_interpolation()
-    grid = RegularCartesianGrid((3, 3, 3), (3, 3, 3))
-    ϕ = rand(3, 3, 3)
+function test_function_interpolation(T=Float64)
+    grid = RegularCartesianGrid(T, (3, 3, 3), (3, 3, 3))
+    ϕ = rand(T, 3, 3, 3)
     ϕ² = ϕ.^2
 
     ▶x_ϕ_c = (ϕ²[2, 2, 2] + ϕ²[1, 2, 2]) / 2
@@ -45,9 +31,9 @@ function test_function_interpolation()
         )
 end
 
-function test_function_differentiation()
-    grid = RegularCartesianGrid((3, 3, 3), (3, 3, 3))
-    ϕ = rand(3, 3, 3)
+function test_function_differentiation(T=Float64)
+    grid = RegularCartesianGrid(T, (3, 3, 3), (3, 3, 3))
+    ϕ = rand(T, 3, 3, 3)
     ϕ² = ϕ.^2
 
     ∂x_ϕ_c = ϕ²[2, 2, 2] - ϕ²[1, 2, 2]
@@ -74,38 +60,38 @@ function test_function_differentiation()
         )
 end
 
-function test_constant_isotropic_diffusivity_basic(; ν=0.3, κ=0.7)
-    closure = TurbulenceClosures.ConstantIsotropicDiffusivity(κ=κ, ν=ν)
+function test_constant_isotropic_diffusivity_basic(T=Float64; ν=T(0.3), κ=T(0.7))
+    closure = TurbulenceClosures.ConstantIsotropicDiffusivity(T, κ=κ, ν=ν)
     return (                    closure.ν == ν &&
                                 closure.κ == κ &&
         TurbulenceClosures.κ_ccc(nothing, nothing, nothing, nothing, closure) == κ
     )
 end
 
-function test_constant_isotropic_diffusivity_fluxdiv(; ν=0.3, κ=0.7)
-    closure = TurbulenceClosures.ConstantIsotropicDiffusivity(κ=κ, ν=ν)
-    grid = RegularCartesianGrid((3, 1, 1), (3, 1, 1))
-    u = zeros(3, 1, 1); v = zeros(3, 1, 1); w = zeros(3, 1, 1)
-    T = zeros(3, 1, 1); S = zeros(3, 1, 1)
+function test_constant_isotropic_diffusivity_fluxdiv(TF=Float64; ν=TF(0.3), κ=TF(0.7))
+    closure = TurbulenceClosures.ConstantIsotropicDiffusivity(TF, κ=κ, ν=ν)
+    grid = RegularCartesianGrid(TF, (3, 1, 1), (3, 1, 1))
+    u = zeros(TF, 3, 1, 1); v = zeros(TF, 3, 1, 1); w = zeros(TF, 3, 1, 1)
+    T = zeros(TF, 3, 1, 1); S = zeros(TF, 3, 1, 1)
 
     u[:, 1, 1] .= [0, -1, 0]
     v[:, 1, 1] .= [0, -2, 0]
     w[:, 1, 1] .= [0, -3, 0]
     T[:, 1, 1] .= [0, -1, 0]
 
-    return (TurbulenceClosures.∇_κ_∇ϕ(2, 1, 1, grid, T, closure, u, v, w, T, S) == 2κ &&
-            TurbulenceClosures.∂ⱼ_2ν_Σ₁ⱼ(2, 1, 1, grid, closure, u, v, w, T, S) == 2ν &&
-            TurbulenceClosures.∂ⱼ_2ν_Σ₂ⱼ(2, 1, 1, grid, closure, u, v, w, T, S) == 4ν &&
-            TurbulenceClosures.∂ⱼ_2ν_Σ₃ⱼ(2, 1, 1, grid, closure, u, v, w, T, S) == 6ν
+    return (∇_κ_∇ϕ(2, 1, 1, grid, T, closure, u, v, w, T, S) == 2κ &&
+            ∂ⱼ_2ν_Σ₁ⱼ(2, 1, 1, grid, closure, u, v, w, T, S) == 2ν &&
+            ∂ⱼ_2ν_Σ₂ⱼ(2, 1, 1, grid, closure, u, v, w, T, S) == 4ν &&
+            ∂ⱼ_2ν_Σ₃ⱼ(2, 1, 1, grid, closure, u, v, w, T, S) == 6ν
             )
 end
 
-function test_directional_diffusivity_fluxdiv(; νh=0.3, κh=0.7, νv=0.1, κv=0.5)
-    closure = TurbulenceClosures.DirectionalDiffusivity(κh=κh, νh=νh, κv=κv, νv=νv)
-    grid = RegularCartesianGrid((3, 1, 3), (3, 1, 3))
+function test_anisotropic_diffusivity_fluxdiv(TF=Float64; νh=TF(0.3), κh=TF(0.7), νv=TF(0.1), κv=TF(0.5))
+    closure = TurbulenceClosures.ConstantAnisotropicDiffusivity(TF, κh=κh, νh=νh, κv=κv, νv=νv)
+    grid = RegularCartesianGrid(TF, (3, 1, 3), (3, 1, 3))
 
-    u = zeros(3, 1, 3); v = zeros(3, 1, 3); w = zeros(3, 1, 3)
-    T = zeros(3, 1, 3); S = zeros(3, 1, 3)
+    u = zeros(TF, 3, 1, 3); v = zeros(TF, 3, 1, 3); w = zeros(TF, 3, 1, 3)
+    T = zeros(TF, 3, 1, 3); S = zeros(TF, 3, 1, 3)
 
     u[:, 1, 1] .= [0,  1, 0]
     u[:, 1, 2] .= [0, -1, 0]
@@ -122,23 +108,23 @@ function test_directional_diffusivity_fluxdiv(; νh=0.3, κh=0.7, νv=0.1, κv=0
     T[:, 1, 2] .= [0, -4, 0]
     T[:, 1, 3] .= [0,  1, 0]
 
-    return (TurbulenceClosures.∇_κ_∇ϕ(2, 1, 2, grid, T, closure, u, v, w, T, S) == 8κh + 10κv &&
-            TurbulenceClosures.∂ⱼ_2ν_Σ₁ⱼ(2, 1, 2, grid, closure, u, v, w, T, S) == 2νh + 4νv &&
-            TurbulenceClosures.∂ⱼ_2ν_Σ₂ⱼ(2, 1, 2, grid, closure, u, v, w, T, S) == 4νh + 6νv &&
-            TurbulenceClosures.∂ⱼ_2ν_Σ₃ⱼ(2, 1, 2, grid, closure, u, v, w, T, S) == 6νh + 8νv
+    return (∇_κ_∇ϕ(2, 1, 2, grid, T, closure, u, v, w, T, S) == 8κh + 10κv &&
+            ∂ⱼ_2ν_Σ₁ⱼ(2, 1, 2, grid, closure, u, v, w, T, S) == 2νh + 4νv &&
+            ∂ⱼ_2ν_Σ₂ⱼ(2, 1, 2, grid, closure, u, v, w, T, S) == 4νh + 6νv &&
+            ∂ⱼ_2ν_Σ₃ⱼ(2, 1, 2, grid, closure, u, v, w, T, S) == 6νh + 8νv
             )
 end
 
-function test_smag_divflux_finiteness()
-    closure = TurbulenceClosures.ConstantSmagorinsky()
-    grid = RegularCartesianGrid((3, 3, 3), (3, 3, 3))
-    u, v, w = rand(size(grid)...), rand(size(grid)...), rand(size(grid)...)
-    T, S = rand(size(grid)...), rand(size(grid)...)
+function test_smag_divflux_finiteness(TF=Float64)
+    closure = TurbulenceClosures.ConstantSmagorinsky(TF)
+    grid = RegularCartesianGrid(TF, (3, 3, 3), (3, 3, 3))
+    u, v, w = rand(TF, size(grid)...), rand(TF, size(grid)...), rand(TF, size(grid)...)
+    T, S = rand(TF, size(grid)...), rand(TF, size(grid)...)
 
     return (
-        isfinite(TurbulenceClosures.∇_κ_∇ϕ(2, 1, 2, grid, T, closure, u, v, w, T, S)) &&
-        isfinite(TurbulenceClosures.∂ⱼ_2ν_Σ₁ⱼ(2, 1, 2, grid, closure, u, v, w, T, S)) &&
-        isfinite(TurbulenceClosures.∂ⱼ_2ν_Σ₂ⱼ(2, 1, 2, grid, closure, u, v, w, T, S)) &&
-        isfinite(TurbulenceClosures.∂ⱼ_2ν_Σ₃ⱼ(2, 1, 2, grid, closure, u, v, w, T, S))
+        isfinite(∇_κ_∇ϕ(2, 1, 2, grid, T, closure, u, v, w, T, S)) &&
+        isfinite(∂ⱼ_2ν_Σ₁ⱼ(2, 1, 2, grid, closure, u, v, w, T, S)) &&
+        isfinite(∂ⱼ_2ν_Σ₂ⱼ(2, 1, 2, grid, closure, u, v, w, T, S)) &&
+        isfinite(∂ⱼ_2ν_Σ₃ⱼ(2, 1, 2, grid, closure, u, v, w, T, S))
         )
 end
