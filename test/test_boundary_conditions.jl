8919acb94f5c4fa842f6fa38054178c646adaf28
Gregory Wagner <wagner.greg@gmail.com>
2019-05-24 22:38:23 -0400

    fixes bug in bc test

--- test/test_boundary_conditions.jl
+++ test/test_boundary_conditions.jl
@@ -32,12 +32,13 @@ function test_z_boundary_condition_array(arch, TF, field_name, Nx, Ny, Nz)
 
     time_step!(model, 1, 1e-16)
 
-    getbc(bcs.z.top, 1, 2) == bcarray[1, 2]
+    bcs.z.top[1, 2] == bcarray[1, 2]
 end
 
 function test_flux_budget(arch, TF, field_name)
-    model = Model(N=(1, 1, 16), L=(1, 1, 0.7), ν=1, κ=1,
-        arch=arch, float_type=TF, eos=LinearEquationOfState(βS=0, βT=0)
+    κ, Lz = 1, 0.7
+    model = Model(N=(1, 1, 16), L=(1, 1, Lz), ν=κ, κ=κ,
+        arch=arch, float_type=TF, eos=LinearEquationOfState(βS=0, βT=0))
 
     if field_name ∈ (:u, :v, :w)
         field = getfield(model.velocities, field_name)
@@ -62,5 +63,5 @@ function test_flux_budget(arch, TF, field_name)
 
     # budget is Lz * ∂<ϕ>/∂t = -Δflux = -top_flux/Lz (left) + bottom_flux/Lz (right);
     # therefore <ϕ> = bottom_flux * t / Lz
-    isapprox(mean(field.data) - mean_init, bottom_flux*model.clock.time/Lz)
+    isapprox(mean(field.data) - mean_init, bottom_flux * model.clock.time / Lz)
 end
