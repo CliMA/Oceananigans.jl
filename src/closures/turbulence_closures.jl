4047d59b41a3e5cedd95e88ab4a903080305b7ba
Gregory Wagner <wagner.greg@gmail.com>
2019-05-27 14:50:33 -0400

    adds tensor diffusivity superabstraction for arbitrary boundary conditions
    Former-commit-id: 016dc05667ff1aa9b321ebe13a60adc101d80d69

--- src/closures/turbulence_closures.jl
+++ src/closures/turbulence_closures.jl
@@ -11,9 +11,8 @@ export
   ∂ⱼ_2ν_Σ₂ⱼ,
   ∂ⱼ_2ν_Σ₃ⱼ,
 
-  ν_ccc, ν_ffc, ν_fcf, ν_cff,
-  
-  κ_ccc,
+  ν₁₁, ν₂₂, ν₃₃,
+  κ₁₁, κ₂₂, κ₃₃,
 
   ∂x_caa, ∂x_faa, ∂x²_caa, ∂x²_faa,
   ∂y_aca, ∂y_afa, ∂y²_aca, ∂y²_afa,
@@ -22,7 +21,7 @@ export
   ▶x_caa, ▶x_faa,
   ▶y_aca, ▶y_afa,
   ▶z_aac, ▶z_aaf,
-  
+
   ▶xy_cca, ▶xz_cac, ▶yz_acc,
   ▶xy_cfa, ▶xz_caf, ▶yz_acf,
   ▶xy_fca, ▶xz_fac, ▶yz_afc,
@@ -35,6 +34,27 @@ using Oceananigans.Operators: incmod1, decmod1
 abstract type TurbulenceClosure{T} end
 abstract type IsotropicDiffusivity{T} <: TurbulenceClosure{T} end
 abstract type TensorDiffusivity{T} <: TurbulenceClosure{T} end
+
+# Tensors... !
+κ₁₁_ccc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = κ_ccc(i, j, k, grid, closure, args...)
+κ₂₂_ccc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = κ_ccc(i, j, k, grid, closure, args...)
+κ₃₃_ccc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = κ_ccc(i, j, k, grid, closure, args...)
+
+ν₁₁_ccc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_ccc(i, j, k, grid, closure, args...)
+ν₂₂_ccc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_ccc(i, j, k, grid, closure, args...)
+ν₃₃_ccc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_ccc(i, j, k, grid, closure, args...)
+
+ν₁₁_ffc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_ffc(i, j, k, grid, closure, args...)
+ν₂₂_ffc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_ffc(i, j, k, grid, closure, args...)
+ν₃₃_ffc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_ffc(i, j, k, grid, closure, args...)
+
+ν₁₁_fcf(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_fcf(i, j, k, grid, closure, args...)
+ν₂₂_fcf(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_fcf(i, j, k, grid, closure, args...)
+ν₃₃_fcf(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_fcf(i, j, k, grid, closure, args...)
+
+ν₁₁_cff(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_cff(i, j, k, grid, closure, args...)
+ν₂₂_cff(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_cff(i, j, k, grid, closure, args...)
+ν₃₃_cff(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_cff(i, j, k, grid, closure, args...)
 
 geo_mean_Δ(grid::RegularCartesianGrid) = (grid.Δx * grid.Δy * grid.Δz)^(1/3)
 
@@ -51,5 +71,14 @@ include("closure_operators.jl")
 include("velocity_gradients.jl")
 include("constant_diffusivity_closures.jl")
 include("constant_smagorinsky.jl")
+
+# Packaged operators
+ν₁₁ = (ccc=ν₁₁_ccc, ffc=ν₁₁_ffc, fcf=ν₁₁_fcf, cff=ν₁₁_cff)
+ν₂₂ = (ccc=ν₂₂_ccc, ffc=ν₂₂_ffc, fcf=ν₂₂_fcf, cff=ν₂₂_cff)
+ν₃₃ = (ccc=ν₃₃_ccc, ffc=ν₃₃_ffc, fcf=ν₃₃_fcf, cff=ν₃₃_cff)
+
+κ₁₁ = (ccc=κ₁₁_ccc, )
+κ₂₂ = (ccc=κ₂₂_ccc, )
+κ₃₃ = (ccc=κ₃₃_ccc, )
 
 end # module
