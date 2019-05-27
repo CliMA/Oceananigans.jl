36c4e3ee34cc160ad2e01be8e46e08fe70136b48
Gregory Wagner <wagner.greg@gmail.com>
2019-05-27 09:13:59 -0400

    refactors model to use TurbulenceClosures instead of model configuration, cleans up code and adds comments
    Former-commit-id: 4d679d3847e8d0d31a119e82bbca388722cf37ff

--- src/models.jl
+++ src/models.jl
@@ -1,58 +1,50 @@
-mutable struct Model{A<:Architecture}
-    configuration::ModelConfiguration
-    boundary_conditions::ModelBoundaryConditions
-    constants::PlanetaryConstants
-    eos::EquationOfState
-    grid::Grid
-    velocities::VelocityFields
-    tracers::TracerFields
-    pressures::PressureFields
-    G::SourceTerms
-    Gp::SourceTerms
-    forcing  # ::Forcing  # No type so we can set it to nothing while checkpointing.
-    stepper_tmp::StepperTemporaryFields
-    poisson_solver  # ::PoissonSolver or ::PoissonSolverGPU
-    clock::Clock
-    output_writers::Array{OutputWriter,1}
-    diagnostics::Array{Diagnostic,1}
+using .TurbulenceClosures
+
+mutable struct Model{A<:Architecture, G, TC, T}
+              arch :: A                  # Computer `Architecture` on which `Model` is run.
+              grid :: G                  # Grid of physical points on which `Model` is solved.
+             clock :: Clock{T}           # Tracks iteration number and simulation time of `Model`.
+               eos :: EquationOfState    # Defines relationship between temperature,  salinity, and 
+                                         # buoyancy in the Boussinesq vertical momentum equation.
+         constants :: PlanetaryConstants # A set of physical constants, inc. gravitational acceleration.
+        velocities :: VelocityFields     # A container for velocity fields `u`, `v`, and `w`.
+           tracers :: TracerFields       # A container for tracer fields.
+         pressures :: PressureFields     # A container for hydrostatic and nonhydrostatic pressure.
+           forcing                       # A container for forcing functions defined by the user
+           closure :: TC                 # Diffusive 'turbulence closure' for all model fields
+    boundary_conditions :: ModelBoundaryConditions # A container for 3d boundary conditions on all model fields.
+                 G :: SourceTerms        # A container for the right-hand-side of the PDE that governs `Model`
+                Gp :: SourceTerms        # The rhs at a previous time-step used for Adams-Bashforth time integration
+    poisson_solver                       # ::PoissonSolver or ::PoissonSolverGPU
+       stepper_tmp :: StepperTemporaryFields # Temporary fields used for the Poisson solver.
+    output_writers :: Array{OutputWriter, 1} # Array of objects that write data to disk.
+       diagnostics :: Array{Diagnostic, 1}   # Array of objects that calc diagnostics on-line during a simulation.
 end
+
 
 """
     Model(; kwargs...)
 
-Construct an `Oceananigans.jl` model. The keyword arguments are:
-
-          N (tuple) : model resolution in (x, y, z)
-          L (tuple) : model domain in (x, y, z)
-         dt (float) : time step
- start_time (float) : start time for the simulation
-  iteration (float) : ?
-         arch (sym) : architecture (:cpu or :gpu)
-  float_type (type) : floating point type for model data (typically Float32 or Float64)
-          constants : planetary constants (?)
-                eos : equation of state to infer density from temperature and salinity
-            forcing : forcing functions for (u, v, w, T, S)
-boundary_conditions : boundary conditions
-     output_writers : output writer
-        diagonstics : diagnostics
-
-        ... and more.
+Construct an `Oceananigans.jl` model.
 """
 function Model(;
     # Model resolution and domain size
              N,
              L,
-    # Molecular parameters
-             ν = 1.05e-6, νh = ν, νv = ν,
-             κ = 1.43e-7, κh = κ, κv = κ,
-    # Time stepping
-    start_time = 0,
-     iteration = 0,
     # Model architecture and floating point precision
           arch = CPU(),
     float_type = Float64,
+          grid = RegularCartesianGrid(float_type, N, L),
+    # Isotropic transport coefficients (exposed to `Model` constructor for convenience)
+             ν = 1.05e-6,
+             κ = 1.43e-7,
+       closure = ConstantIsotropicDiffusivity(ν=ν, κ=κ),
+    # Time stepping
+    start_time = 0,
+     iteration = 0,
+         clock = Clock{float_type}(start_time, iteration),
+    # Fluid and physical parameters
      constants = Earth(),
-    # Equation of State
            eos = LinearEquationOfState(),
     # Forcing and boundary conditions for (u, v, w, T, S)
        forcing = Forcing(nothing, nothing, nothing, nothing, nothing),
@@ -64,34 +56,40 @@ function Model(;
 
     arch == GPU() && !HAVE_CUDA && throw(ArgumentError("Cannot create a GPU model. No CUDA-enabled GPU was detected!"))
 
-    # Initialize model basics.
-    configuration = ModelConfiguration(νh, νv, κh, κv)
-             grid = RegularCartesianGrid(float_type, N, L)
-            clock = Clock{float_type}(start_time, iteration)
-
-    # Initialize fields, including source terms and temporary variables.
-      velocities = VelocityFields(arch, grid)
-         tracers = TracerFields(arch, grid)
-       pressures = PressureFields(arch, grid)
-               G = SourceTerms(arch, grid)
-              Gp = SourceTerms(arch, grid)
-     stepper_tmp = StepperTemporaryFields(arch, grid)
-
+    # Model components not initialized in function signature:
+         velocities = VelocityFields(arch, grid)
+            tracers = TracerFields(arch, grid)
+          pressures = PressureFields(arch, grid)
+                  G = SourceTerms(arch, grid)
+                 Gp = SourceTerms(arch, grid)
+        stepper_tmp = StepperTemporaryFields(arch, grid)
      poisson_solver = init_poisson_solver(arch, grid, stepper_tmp.fCC1)
 
-    # Default initial condition
-    velocities.u.data .= 0
-    velocities.v.data .= 0
-    velocities.w.data .= 0
-    tracers.S.data .= eos.S₀
-    tracers.T.data .= eos.T₀
+    # Set the default initial condition
+    initialize_with_defaults!(eos, tracers, velocities, G, Gp)
 
-    Model{typeof(arch)}(configuration, boundary_conditions, constants, eos, grid,
-                        velocities, tracers, pressures, G, Gp, forcing,
-                        stepper_tmp, poisson_solver, clock, output_writers, diagnostics)
+    Model(arch, grid, clock, eos, constants,
+          velocities, tracers, pressures, forcing, closure, boundary_conditions,
+          G, Gp, poisson_solver, stepper_tmp, output_writers, diagnostics)
 end
 
 arch(model::Model{A}) where A <: Architecture = A
 float_type(m::Model) = eltype(model.grid)
-
 add_bcs!(model::Model; kwargs...) = add_bcs(model.boundary_conditions; kwargs...)
+
+function initialize_with_defaults!(eos, tracers, sets...)
+
+    # Default tracer initial condition is deteremined by eos.
+    tracers.S.data    .= eos.S₀
+    tracers.T.data    .= eos.T₀
+
+    # Set all further fields to 0
+    for set in sets
+        for fldname in propertynames(set)
+            fld = getproperty(set, fldname)
+            fld.data .= 0 # promotes to eltype of fld.data
+        end
+    end
+    
+    return nothing
+end
