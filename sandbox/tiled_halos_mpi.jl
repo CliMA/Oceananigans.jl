0defdc9653881990ead849f7fa693d26cbb9e0d0
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-06-05 18:51:57 -0400

    Sending and reciving halo data.
    Former-commit-id: 541c389f3d5fd88ba0f2752c71e0ae5ef9cb1636

--- sandbox/tiled_halos_mpi.jl
+++ sandbox/tiled_halos_mpi.jl
@@ -5,12 +5,28 @@ using Oceananigans
 @inline index2rank(I, J, Mx, My) = J*My + I
 @inline rank2index(r, Mx, My) = div(r, Mx), mod(r, My)
 
+@inline north_halo(tile) = @views @inbounds tile.data[1-tile.grid.Hx:0, :, :]
+@inline south_halo(tile) = @views @inbounds tile.data[tile.grid.Nx+1:tile.grid.Nx+tile.grid.Hx, :, :]
+@inline  west_halo(tile) = @views @inbounds tile.data[:, 1-tile.grid.Hy:0, :]
+@inline  east_halo(tile) = @views @inbounds tile.data[:, tile.grid.Ny+1:tile.grid.Ny+tile.grid.Hy, :]
+
+@inline north_data(tile) = @views @inbounds tile.data[1:tile.grid.Hx, :, :]
+@inline south_data(tile) = @views @inbounds tile.data[tile.grid.Nx-tile.grid.Hx+1:tile.grid.Nx, :, :]
+@inline  west_data(tile) = @views @inbounds tile.data[:, 1:tile.grid.Hy, :]
+@inline  east_data(tile) = @views @inbounds tile.data[:, tile.grid.Ny-tile.grid.Hy+1:tile.grid.Ny, :]
+
+@inline distribute_tag(rank) = 100 + rank
+@inline  send_west_tag(rank) = 200 + rank
+@inline  send_east_tag(rank) = 300 + rank
+@inline send_north_tag(rank) = 400 + rank
+@inline send_south_tag(rank) = 500 + rank
+
 function fill_halo_regions_mpi!(FT, arch, Nx, Ny, Nz, Mx, My)
     Lx, Ly, Lz = 10, 10, 10
 
     Nx′, Ny′, Nz′ = Int(Nx/Mx), Int(Ny/My), Nz
     Lx′, Ly′, Lz′ = Lx/Mx, Ly/My, Lz
-    
+
     comm = MPI.COMM_WORLD
 
     MPI.Barrier(comm)
@@ -39,10 +55,8 @@ function fill_halo_regions_mpi!(FT, arch, Nx, Ny, Nz, Mx, My)
             j1, j2 = J′*Ny′+1, (J′+1)*Ny′
             send_mesg = rands[i1:i2, j1:j2, :]
 
-            tag = 100 + r
-            println("[rank $rank] Sending R[$i1:$i2, $j1:$j2, :] to rank $r with tag $tag...")
-
-            sreq = MPI.Isend(send_mesg, r, tag, comm)
+            println("[rank $rank] Sending R[$i1:$i2, $j1:$j2, :] to rank $r...")
+            sreq = MPI.Isend(send_mesg, r, distribute_tag(r), comm)
             push!(send_reqs, sreq)
         end
 
@@ -52,15 +66,26 @@ function fill_halo_regions_mpi!(FT, arch, Nx, Ny, Nz, Mx, My)
     tile_grid = RegularCartesianGrid((Nx′, Ny′, Nz′), (Lx′, Ly′, Lz′))
     tile = CellField(FT, arch, tile_grid)
 
+    println("[rank $rank] Receiving message from rank 0...")
     recv_mesg = zeros(FT, Nx′, Ny′, Nz′)
-
-    tag = 100 + rank
-    println("[rank $rank] Receiving message from rank 0 with tag $tag...")
-    rreq = MPI.Irecv!(recv_mesg, 0, tag, comm)
-
-    data(tile) .= recv_mesg
+    rreq = MPI.Irecv!(recv_mesg, 0, distribute_tag(rank), comm)
 
     stats = MPI.Waitall!([rreq])
+    data(tile) .= recv_mesg
+
+    println("[rank $rank] Sending halo data...")
+    se_req = MPI.Isend(west_data(tile),  east_rank,  send_east_tag(rank),  comm)
+    sw_req = MPI.Isend(east_data(tile),  west_rank,  send_west_tag(rank),  comm)
+    sn_req = MPI.Isend(south_data(tile), north_rank, send_north_tag(rank), comm)
+    ss_req = MPI.Isend(north_data(tile), south_rank, send_south_tag(rank), comm)
+
+    MPI.Barrier(comm)
+
+    println("[rank $rank] Receiving halo data...")
+    re_req = MPI.Irecv!(west_halo(tile),  east_rank,  send_west_tag(east_rank),  comm)
+    rw_req = MPI.Irecv!(east_halo(tile),  west_rank,  send_east_tag(west_rank),  comm)
+    rn_req = MPI.Irecv!(south_halo(tile), north_rank, send_south_tag(north_rank), comm)
+    rs_req = MPI.Irecv!(north_halo(tile), south_rank, send_north_tag(south_rank), comm)
 
     MPI.Barrier(comm)
 end
