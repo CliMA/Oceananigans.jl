ff0f875a943407022a3c5b3480d0db4e31ca88cb
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-06-05 18:30:00 -0400

    Fix typos and stuf
    Former-commit-id: 7da2fbaf7f2e4b0a6fae4c890e1a3b91a3c97718

--- sandbox/tiled_halos_mpi.jl
+++ sandbox/tiled_halos_mpi.jl
@@ -3,8 +3,14 @@ import MPI
 using Oceananigans
 
 @inline index2rank(I, J, Mx, My) = J*My + I
+@inline rank2index(r, Mx, My) = div(r, Mx), mod(r, My)
 
 function fill_halo_regions_mpi!(FT, arch, Nx, Ny, Nz, Mx, My)
+    Lx, Ly, Lz = 10, 10, 10
+
+    Nx′, Ny′, Nz′ = Int(Nx/Mx), Int(Ny/My), Nz
+    Lx′, Ly′, Lz′ = Lx/Mx, Ly/My, Lz
+    
     comm = MPI.COMM_WORLD
 
     MPI.Barrier(comm)
@@ -12,22 +18,26 @@ function fill_halo_regions_mpi!(FT, arch, Nx, Ny, Nz, Mx, My)
     rank = MPI.Comm_rank(comm)
     size = MPI.Comm_size(comm)
 
+    I, J = rank2index(rank, Mx, My)
     I⁻, I⁺ = mod(I-1, Mx), mod(I+1, Mx)
     J⁻, J⁺ = mod(J-1, My), mod(J+1, My)
+    Nx′, Ny′, Nz′ = Int(Nx/Mx), Int(Ny/My), Nz
+    Lx′, Ly′, Lz′ = Lx/Mx, Ly/My, Lz
 
     north_rank = index2rank(I,  J⁻, Mx, My)
     south_rank = index2rank(I,  J⁺, Mx, My)
     east_rank  = index2rank(I⁺, J,  Mx, My)
     west_rank  = index2rank(I⁻, J,  Mx, My)
 
-    send_reqs = []
+    send_reqs = MPI.Request[]
     if rank == 0
         rands = rand(Nx, Ny, Nz)
 
-        for r in 0:Mx*My
-            i1, i2 = I*Nx′+1, (I+1)*Nx′
-            j1, j2 = J*Ny′+1, (J+1)*Ny′
-            send_mesg = R[i1:i2, j1:j2, :]
+        for r in 0:Mx*My-1
+            I′, J′ = rank2index(r, Mx, My)
+            i1, i2 = I′*Nx′+1, (I′+1)*Nx′
+            j1, j2 = J′*Ny′+1, (J′+1)*Ny′
+            send_mesg = rands[i1:i2, j1:j2, :]
 
             tag = 100 + r
             println("[rank $rank] Sending R[$i1:$i2, $j1:$j2, :] to rank $r with tag $tag...")
@@ -39,15 +49,13 @@ function fill_halo_regions_mpi!(FT, arch, Nx, Ny, Nz, Mx, My)
         MPI.Waitall!(send_reqs)
     end
 
-    Nx′, Ny′, Nz′ = Int(Nx/Mx), Int(Ny/My), Nz
-    Lx′, Ly′, Lz′ = Lx/Mx, Ly/My, Lz
     tile_grid = RegularCartesianGrid((Nx′, Ny′, Nz′), (Lx′, Ly′, Lz′))
     tile = CellField(FT, arch, tile_grid)
 
     recv_mesg = zeros(FT, Nx′, Ny′, Nz′)
 
-    tag = 100 + r
-    println("[rank $rank] Receiving message from rank $src with tag $tag...")
+    tag = 100 + rank
+    println("[rank $rank] Receiving message from rank 0 with tag $tag...")
     rreq = MPI.Irecv!(recv_mesg, 0, tag, comm)
 
     data(tile) .= recv_mesg
