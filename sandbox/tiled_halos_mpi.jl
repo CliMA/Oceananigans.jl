95b2edff92f0564c4091763688459b24b690eb5e
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-06-06 15:18:06 -0400

    Halo comm working on 4 ranks
    Former-commit-id: bd5cf58b730ff42d5cd87a5283ba196a1bc6da48

--- sandbox/tiled_halos_mpi.jl
+++ sandbox/tiled_halos_mpi.jl
@@ -3,17 +3,17 @@ import MPI
 using Oceananigans
 
 @inline index2rank(I, J, Mx, My) = J*My + I
-@inline rank2index(r, Mx, My) = div(r, Mx), mod(r, My)
+@inline rank2index(r, Mx, My) = mod(r, Mx), div(r, My)
 
-@inline north_halo(tile) = @views @inbounds tile.data[1-tile.grid.Hx:0, :, :]
-@inline south_halo(tile) = @views @inbounds tile.data[tile.grid.Nx+1:tile.grid.Nx+tile.grid.Hx, :, :]
-@inline  west_halo(tile) = @views @inbounds tile.data[:, 1-tile.grid.Hy:0, :]
-@inline  east_halo(tile) = @views @inbounds tile.data[:, tile.grid.Ny+1:tile.grid.Ny+tile.grid.Hy, :]
+@inline north_halo(tile) = @views @inbounds tile.data.parent[1:tile.grid.Hx, :, :]
+@inline south_halo(tile) = @views @inbounds tile.data.parent[tile.grid.Nx+tile.grid.Hx+1:tile.grid.Nx+2tile.grid.Hx, :, :]
+@inline  west_halo(tile) = @views @inbounds tile.data.parent[:, 1:tile.grid.Hy, :]
+@inline  east_halo(tile) = @views @inbounds tile.data.parent[:, tile.grid.Ny+tile.grid.Hy+1:tile.grid.Ny+2tile.grid.Hy, :]
 
-@inline north_data(tile) = @views @inbounds tile.data[1:tile.grid.Hx, :, :]
-@inline south_data(tile) = @views @inbounds tile.data[tile.grid.Nx-tile.grid.Hx+1:tile.grid.Nx, :, :]
-@inline  west_data(tile) = @views @inbounds tile.data[:, 1:tile.grid.Hy, :]
-@inline  east_data(tile) = @views @inbounds tile.data[:, tile.grid.Ny-tile.grid.Hy+1:tile.grid.Ny, :]
+@inline north_data(tile) = @views @inbounds tile.data.parent[1+tile.grid.Hx:2tile.grid.Hx,   :, :]
+@inline south_data(tile) = @views @inbounds tile.data.parent[tile.grid.Nx+1:tile.grid.Nx+tile.grid.Hx, :, :]
+@inline  west_data(tile) = @views @inbounds tile.data.parent[:, 1+tile.grid.Hy:2tile.grid.Hy,   :]
+@inline  east_data(tile) = @views @inbounds tile.data.parent[:, tile.grid.Ny+1:tile.grid.Ny+tile.grid.Hy, :]
 
 @inline distribute_tag(rank) = 100 + rank
 @inline  send_west_tag(rank) = 200 + rank
@@ -32,7 +32,7 @@ function fill_halo_regions_mpi!(FT, arch, Nx, Ny, Nz, Mx, My)
     MPI.Barrier(comm)
 
     rank = MPI.Comm_rank(comm)
-    size = MPI.Comm_size(comm)
+    # size = MPI.Comm_size(comm)
 
     I, J = rank2index(rank, Mx, My)
     I⁻, I⁺ = mod(I-1, Mx), mod(I+1, Mx)
@@ -70,22 +70,32 @@ function fill_halo_regions_mpi!(FT, arch, Nx, Ny, Nz, Mx, My)
     recv_mesg = zeros(FT, Nx′, Ny′, Nz′)
     rreq = MPI.Irecv!(recv_mesg, 0, distribute_tag(rank), comm)
 
-    stats = MPI.Waitall!([rreq])
+    stats = MPI.Wait!(rreq)
     data(tile) .= recv_mesg
 
     println("[rank $rank] Sending halo data...")
 
-    west_data_buf = copy(west_data(tile))
-    east_data_buf = copy(east_data(tile))
-   north_data_buf = copy(north_data(tile))
-   south_data_buf = copy(south_data(tile))
+     west_data_buf = zeros(size(west_data(tile)))
+     east_data_buf = zeros(size(east_data(tile)))
+    north_data_buf = zeros(size(north_data(tile)))
+    south_data_buf = zeros(size(south_data(tile)))
 
-    se_req = MPI.Isend(west_data_buf,  east_rank,  send_east_tag(rank),  comm)
-    sw_req = MPI.Isend(east_data_buf,  west_rank,  send_west_tag(rank),  comm)
-    sn_req = MPI.Isend(south_data_buf, north_rank, send_north_tag(rank), comm)
-    ss_req = MPI.Isend(north_data_buf, south_rank, send_south_tag(rank), comm)
+     west_data_buf .= copy(west_data(tile))
+     east_data_buf .= copy(east_data(tile))
+    north_data_buf .= copy(north_data(tile))
+    south_data_buf .= copy(south_data(tile))
 
-    MPI.Barrier(comm)
+    se_req = MPI.Isend(east_data_buf,  east_rank,  send_east_tag(rank),  comm)
+    sw_req = MPI.Isend(west_data_buf,  west_rank,  send_west_tag(rank),  comm)
+    sn_req = MPI.Isend(north_data_buf, north_rank, send_north_tag(rank), comm)
+    ss_req = MPI.Isend(south_data_buf, south_rank, send_south_tag(rank), comm)
+
+    @debug "[rank $rank] sending #$(send_east_tag(rank)) to rank $east_rank"
+    @debug "[rank $rank] sending #$(send_west_tag(rank)) to rank $west_rank"
+    @debug "[rank $rank] sending #$(send_north_tag(rank)) to rank $north_rank"
+    @debug "[rank $rank] sending #$(send_south_tag(rank)) to rank $south_rank"
+
+    MPI.Waitall!([se_req, sw_req, sn_req, ss_req])
 
      west_halo_buf = zeros(size(west_halo(tile)))
      east_halo_buf = zeros(size(east_halo(tile)))
@@ -93,17 +103,22 @@ function fill_halo_regions_mpi!(FT, arch, Nx, Ny, Nz, Mx, My)
     south_halo_buf = zeros(size(south_halo(tile)))
 
     println("[rank $rank] Receiving halo data...")
-    re_req = MPI.Irecv!(west_halo_buf,  east_rank,  send_west_tag(east_rank),  comm)
-    rw_req = MPI.Irecv!(east_halo_buf,  west_rank,  send_east_tag(west_rank),  comm)
-    rn_req = MPI.Irecv!(south_halo_buf, north_rank, send_south_tag(north_rank), comm)
-    rs_req = MPI.Irecv!(north_halo_buf, south_rank, send_north_tag(south_rank), comm)
+    re_req = MPI.Irecv!(west_halo_buf,  west_rank,  send_east_tag(west_rank),  comm)
+    rw_req = MPI.Irecv!(east_halo_buf,  east_rank,  send_west_tag(east_rank),  comm)
+    rn_req = MPI.Irecv!(south_halo_buf, south_rank, send_north_tag(south_rank), comm)
+    rs_req = MPI.Irecv!(north_halo_buf, north_rank, send_south_tag(north_rank), comm)
 
-    MPI.Barrier(comm)
+    @debug "[rank $rank] waiting for #$(send_east_tag(west_rank)) from rank $west_rank..."
+    @debug "[rank $rank] waiting for #$(send_west_tag(east_rank)) from rank $east_rank..."
+    @debug "[rank $rank] waiting for #$(send_north_tag(south_rank)) from rank $south_rank..."
+    @debug "[rank $rank] waiting for #$(send_south_tag(north_rank)) from rank $north_rank..."
 
-    east_halo(tile) .=  west_halo_buf
-    west_halo(tile) .=  east_halo_buf
-   north_halo(tile) .= south_halo_buf
-   south_halo(tile) .= north_halo_buf
+    MPI.Waitall!([re_req, rw_req, rn_req, rs_req])
+
+    east_halo(tile) .=  east_halo_buf
+    west_halo(tile) .=  west_halo_buf
+   north_halo(tile) .= north_halo_buf
+   south_halo(tile) .= south_halo_buf
 end
 
 MPI.Init()
