5710009049d85cfd41df9f1c7bf046dbe6f832c5
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-06-23 09:40:00 -0400

    Revise area and volume operators.

--- src/operators/areas_and_volumes.jl
+++ src/operators/areas_and_volumes.jl
@@ -1,18 +1,33 @@
+# Grid spacing between cells and faces in the x-direction.
 @inline Δx(i, j, k, grid::RegularCartesianGrid) = grid.Δx
 @inline Δx(i, j, k, grid::VerticallyStretchedCartesianGrid) = grid.Δx
 @inline Δx(i, j, k, grid::Grid) = @inbounds grid.Δx[i, j, k]
 
+# Grid spacing between cells and faces in the y-direction.
 @inline Δy(i, j, k, grid::RegularCartesianGrid) = grid.Δy
 @inline Δy(i, j, k, grid::VerticallyStretchedCartesianGrid) = grid.Δy
 @inline Δy(i, j, k, grid::Grid) = @inbounds grid.Δy[i, j, k]
 
-@inline Δz(i, j, k, grid::RegularCartesianGrid) = grid.Δz
-@inline Δz(i, j, k, grid::VerticallyStretchedCartesianGrid) = @inbounds grid.Δz[k]
-@inline Δz(i, j, k, grid::Grid) = @inbounds grid.Δz[i, j, k]
+# Grid spacing between cell centers in the z-direction.
+@inline ΔzC(i, j, k, grid::RegularCartesianGrid) = grid.Δz
+@inline ΔzC(i, j, k, grid::VerticallyStretchedCartesianGrid) = @inbounds grid.ΔzC[k]
+@inline ΔzC(i, j, k, grid::Grid) = @inbounds grid.ΔzC[i, j, k]
 
-@inline Ax(i, j, k, grid::Grid) = Δy(i, j, k, grid) * Δz(i, j, k, grid)
-@inline Ay(i, j, k, grid::Grid) = Δx(i, j, k, grid) * Δz(i, j, k, grid)
+# Grid spacing between cell faces in the z-direction.
+@inline ΔzF(i, j, k, grid::RegularCartesianGrid) = grid.Δz
+@inline ΔzF(i, j, k, grid::VerticallyStretchedCartesianGrid) = @inbounds grid.ΔzF[k]
+@inline ΔzF(i, j, k, grid::Grid) = @inbounds grid.ΔzF[i, j, k]
+
+# Area of cell faces surrounding cell center and cell face (i, j, k) in the x-direction.
+@inline AxC(i, j, k, grid::Grid) = Δy(i, j, k, grid) * ΔzC(i, j, k, grid)
+@inline AxF(i, j, k, grid::Grid) = Δy(i, j, k, grid) * ΔzF(i, j, k, grid)
+
+# Area of cell faces surrounding cell center and cell face (i, j, k) in the y-direction.
+@inline AyC(i, j, k, grid::Grid) = Δx(i, j, k, grid) * ΔzC(i, j, k, grid)
+@inline AyF(i, j, k, grid::Grid) = Δx(i, j, k, grid) * ΔzF(i, j, k, grid)
+
+# Area of cell faces surrounding cell center and cell face (i, j, k) in the z-direction.
 @inline Az(i, j, k, grid::Grid) = Δx(i, j, k, grid) * Δy(i, j, k, grid)
 
+# Volume of a cell.
 @inline V(i, j, k, grid::Grid) = Δx(i, j, k, grid) * Δy(i, j, k, grid) * Δz(i, j, k, grid)
-@inline V⁻¹(i, j, k, grid::Grid) = 1 / V(i, j, k, grid)
