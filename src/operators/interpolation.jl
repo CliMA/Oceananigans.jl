7a2b9df6547448f72258e4d909ca6a1c0eefdd82
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-06-23 12:24:12 -0400

    More physical momentum advection operators.
    Former-commit-id: cc45290e7050de926da8eea9841cd26c5840fb54

--- src/operators/interpolation.jl
+++ src/operators/interpolation.jl
@@ -1,21 +1,3 @@
-# Operators that interpolate areas.
-@inline ϊAx_caa(i, j, k, grid::Grid) where T = T(0.5) * (Ax(i, j, k, grid) + Ax(i+1, j, k, grid))
-@inline ϊAy_aca(i, j, k, grid::Grid) where T = T(0.5) * (Ay(i, j, k, grid) + Ay(i, j+1, k, grid))
-@inline ϊAz_aac(i, j, k, grid::Grid) where T = T(0.5) * (Az(i, j, k, grid) + Az(i, j, k+1, grid))
-
-@inline ϊAx_faa(i, j, k, grid::Grid) where T = T(0.5) * (Ax(i-1, j, k, grid) + Ax(i, j, k, grid))
-@inline ϊAy_afa(i, j, k, grid::Grid) where T = T(0.5) * (Ay(i, j-1, k, grid) + Ay(i, j, k, grid))
-@inline ϊAz_aaf(i, j, k, grid::Grid) where T = T(0.5) * (Az(i, j, k-1, grid) + Az(i, j, k, grid))
-
-# Operators that interpolate volumes.
-@inline ϊx_V(i, j, k, grid::Grid{T}) where T = T(0.5) * (V(i+1, j, k, grid) + V(i, j, k, grid))
-@inline ϊy_V(i, j, k, grid::Grid{T}) where T = T(0.5) * (V(i, j+1, k, grid) + V(i, j, k, grid))
-@inline ϊz_V(i, j, k, grid::Grid{T}) where T = T(0.5) * (V(i, j, k+1, grid) + V(i, j, k, grid))
-
-@inline ϊx_V⁻¹(i, j, k, grid::Grid{T}) = 1 / ϊx_V(i, j, k, grid)
-@inline ϊy_V⁻¹(i, j, k, grid::Grid{T}) = 1 / ϊy_V(i, j, k, grid)
-@inline ϊz_V⁻¹(i, j, k, grid::Grid{T}) = 1 / ϊz_V(i, j, k, grid)
-
 # Interpolation operators.
 @inline ϊx_caa(i, j, k, grid::Grid{T}, f::AbstractArray) where T = @inbounds T(0.5) * (f[i+1, j, k] + f[i,    j, k])
 @inline ϊx_faa(i, j, k, grid::Grid{T}, f::AbstractArray) where T = @inbounds T(0.5) * (f[i,   j, k] + f[i-1,  j, k])
@@ -40,37 +22,37 @@ end
 end
 
 # Interpolation operators of the form ϊ(A*f) where A is an area and f is an array.
-@inline ϊxAx_caa(i, j, k, grid::Grid{T}, f::AbstractArray) where T = @inbounds T(0.5) * (Ax(i+1, j, k, grid) * f[i+1, j, k] + Ax(i,   j, k, grid) * f[i,    j, k])
-@inline ϊxAx_faa(i, j, k, grid::Grid{T}, f::AbstractArray) where T = @inbounds T(0.5) * (Ax(i,   j, k, grid) * f[i,   j, k] + Ax(i-1, j, k, grid) * f[i-1,  j, k])
-@inline ϊxAy_faa(i, j, k, grid::Grid{T}, f::AbstractArray) where T = @inbounds T(0.5) * (Ay(i,   j, k, grid) * f[i,   j, k] + Ay(i-1, j, k, grid) * f[i-1,  j, k])
+@inline ϊxAxF_caa(i, j, k, grid::Grid{T}, f::AbstractArray) where T = @inbounds T(0.5) * (AxF(i+1, j, k, grid) * f[i+1, j, k] + AxF(i,   j, k, grid) * f[i,    j, k])
+@inline ϊxAxF_faa(i, j, k, grid::Grid{T}, f::AbstractArray) where T = @inbounds T(0.5) * (AxF(i,   j, k, grid) * f[i,   j, k] + AxF(i-1, j, k, grid) * f[i-1,  j, k])
+@inline ϊxAyF_faa(i, j, k, grid::Grid{T}, f::AbstractArray) where T = @inbounds T(0.5) * (AyF(i,   j, k, grid) * f[i,   j, k] + AyF(i-1, j, k, grid) * f[i-1,  j, k])
 @inline ϊxAz_faa(i, j, k, grid::Grid{T}, f::AbstractArray) where T = @inbounds T(0.5) * (Az(i,   j, k, grid) * f[i,   j, k] + Az(i-1, j, k, grid) * f[i-1,  j, k])
 
-@inline ϊyAy_aca(i, j, k, grid::Grid{T}, f::AbstractArray) where T = @inbounds T(0.5) * (Ay(i, j+1, k, grid) * f[i, j+1, k] + Ay(i,   j, k, grid) * f[i,    j, k])
-@inline ϊyAx_afa(i, j, k, grid::Grid{T}, f::AbstractArray) where T = @inbounds T(0.5) * (Ax(i,   j, k, grid) * f[i,   j, k] + Ax(i, j-1, k, grid) * f[i,  j-1, k])
-@inline ϊyAy_afa(i, j, k, grid::Grid{T}, f::AbstractArray) where T = @inbounds T(0.5) * (Ay(i,   j, k, grid) * f[i,   j, k] + Ay(i, j-1, k, grid) * f[i,  j-1, k])
+@inline ϊyAyF_aca(i, j, k, grid::Grid{T}, f::AbstractArray) where T = @inbounds T(0.5) * (AyF(i, j+1, k, grid) * f[i, j+1, k] + AyF(i,   j, k, grid) * f[i,    j, k])
+@inline ϊyAxF_afa(i, j, k, grid::Grid{T}, f::AbstractArray) where T = @inbounds T(0.5) * (AxF(i,   j, k, grid) * f[i,   j, k] + AxF(i, j-1, k, grid) * f[i,  j-1, k])
+@inline ϊyAyF_afa(i, j, k, grid::Grid{T}, f::AbstractArray) where T = @inbounds T(0.5) * (AyF(i,   j, k, grid) * f[i,   j, k] + AyF(i, j-1, k, grid) * f[i,  j-1, k])
 @inline ϊyAz_afa(i, j, k, grid::Grid{T}, f::AbstractArray) where T = @inbounds T(0.5) * (Az(i,   j, k, grid) * f[i,   j, k] + Az(i, j-1, k, grid) * f[i,  j-1, k])
 
-@inline function ϊzAz_aac(i, j, k, grid::Grid{T}, f::AbstractArray) where T
+@inline function ϊzAzF_aac(i, j, k, grid::Grid{T}, f::AbstractArray) where T
     if k == grid.Nz
-        @inbounds return T(0.5) * Az(i, j, k, grid) * f[i, j, k]
+        @inbounds return T(0.5) * AzF(i, j, k, grid) * f[i, j, k]
     else
-        @inbounds return T(0.5) * (Az(i, j, k+1, grid) * f[i, j, k+1] + Az(i, j, k, grid) * f[i, j, k])
+        @inbounds return T(0.5) * (AzF(i, j, k+1, grid) * f[i, j, k+1] + AzF(i, j, k, grid) * f[i, j, k])
     end
 end
 
-@inline function ϊzAx_aaf(i, j, k, grid::Grid{T}, f::AbstractArray) where T
+@inline function ϊzAxF_aaf(i, j, k, grid::Grid{T}, f::AbstractArray) where T
     if k == 1
-        @inbounds return Ax(i, j, k, grid) * f[i, j, k]
+        @inbounds return AxF(i, j, k, grid) * f[i, j, k]
     else
-        @inbounds return T(0.5) * (Ax(i, j, k, grid) * f[i, j, k] + Ax(i, j, k-1, grid) * f[i, j, k-1])
+        @inbounds return T(0.5) * (AxF(i, j, k, grid) * f[i, j, k] + AxF(i, j, k-1, grid) * f[i, j, k-1])
     end
 end
 
-@inline function ϊzAy_aaf(i, j, k, grid::Grid{T}, f::AbstractArray) where T
+@inline function ϊzAyF_aaf(i, j, k, grid::Grid{T}, f::AbstractArray) where T
     if k == 1
-        @inbounds return Ay(i, j, k, grid) * f[i, j, k]
+        @inbounds return AyF(i, j, k, grid) * f[i, j, k]
     else
-        @inbounds return T(0.5) * (Ay(i, j, k, grid) * f[i, j, k] + Ay(i, j, k-1, grid) * f[i, j, k-1])
+        @inbounds return T(0.5) * (AyF(i, j, k, grid) * f[i, j, k] + AyF(i, j, k-1, grid) * f[i, j, k-1])
     end
 end
 
