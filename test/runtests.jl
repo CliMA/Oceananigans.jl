3f7b03d996f90e186b10491434bc33605c442da9
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-06-23 19:48:39 -0400

    Some low-effort tests.
    Former-commit-id: 0d43f0805d344e57cff10c71204232eff07e6758

--- test/runtests.jl
+++ test/runtests.jl
@@ -275,11 +275,24 @@ float_types = (Float32, Float64)
     @testset "Model" begin
         println("  Testing model...")
 
-        for arch in archs, FT in float_types
-            model = Model(N=(4, 5, 6), L=(1, 2, 3), arch=arch, float_type=FT)
+        @testset "Doubly periodic model" begin
+            println("    Testing doubly periodic model...")
+            for arch in archs, FT in float_types
+                model = Model(N=(4, 5, 6), L=(1, 2, 3), arch=arch, float_type=FT)
 
-            # Just testing that a Model was constructed with no errors/crashes.
-            @test true
+                # Just testing that a Model was constructed with no errors/crashes.
+                @test true
+            end
+        end
+
+        @testset "Reentrant channel model" begin
+            println("    Testing reentrant channel model...")
+            for arch in archs, FT in float_types
+                model = ChannelModel(N=(6, 5, 4), L=(3, 2, 1), arch=arch, float_type=FT)
+
+                # Just testing that a ChannelModel was constructed with no errors/crashes.
+                @test true
+            end
         end
     end
 
