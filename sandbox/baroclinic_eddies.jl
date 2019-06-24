976a574abac6b8f28180179f36413371ad24db9d
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-06-23 23:49:52 -0400

    Update eddying channel script.
    Former-commit-id: e91c0e413aabceca95bb3d07ddf63c839e1b26e1

--- sandbox/baroclinic_eddies.jl
+++ sandbox/baroclinic_eddies.jl
@@ -1,24 +1,49 @@
-using Oceananigans, PyPlot
+using Oceananigans, PyPlot, Statistics
 
 include("../examples/utils.jl")
 
 function update_plot!(ax, model)
-    xC, yC, zC = model.grid.xC, model.grid.yC, model.grid.zC
+    xC, yC, zC = model.grid.xC / 1000, model.grid.yC / 1000, model.grid.zC
     T = model.tracers.T
 
     sca(ax[1, 1])
-    PyPlot.pcolormesh(xC, yC, data(T)[:, :, 1])
+    PyPlot.contourf(xC, yC, data(T)[:, :, 1]', vmin=7.5, vmax=15, cmap=:inferno, levels=40)
+    PyPlot.xlim(0, 160)
+    PyPlot.ylim(0, 500)
+    PyPlot.xlabel("x (km)")
+    PyPlot.ylabel("y (km)")
+    # PyPlot.colorbar()
 
-    sca(axs[2, 1])
-    PyPlot.pcolormesh(xC, zC, data(T)[:, 10, :])
+    t = Int(round(model.clock.time / 3600))
+    PyPlot.title("surface (t = $t hours)")
+
+    sca(ax[2, 1])
+    PyPlot.contourf(yC, zC, data(T)[Int(Nx/2), :, :]', vmin=7.5, vmax=15, cmap=:inferno, levels=20)
+    PyPlot.xlim(0, 500)
+    PyPlot.ylim(-1000, 0)
+    PyPlot.xlabel("x (km)")
+    PyPlot.ylabel("z (m)")
+    PyPlot.title("x = 80 km")
+    # PyPlot.colorbar()
+
+    sca(ax[3, 1])
+    PyPlot.contourf(xC, zC, data(T)[:, Int(Ny/2), :]', vmin=7.5, vmax=15, cmap=:inferno, levels=20)
+    PyPlot.xlim(0, 160)
+    PyPlot.ylim(-1000, 0)
+    PyPlot.xlabel("x (km)")
+    PyPlot.ylabel("z (m)")
+    PyPlot.title("y = 250 km")
+    # PyPlot.colorbar()
+
+    PyPlot.draw()
 end
 
 Lx, Ly, Lz = 160e3, 500e3, 1e3  # 160×500×1 km
-Δh, Δz = 10e3, 50  # Horizontal and vertical grid spacing [m].
+Δh, Δz = 5e3, 50  # Horizontal and vertical grid spacing [m].
 Nx, Ny, Nz = Int(Lx/Δh), Int(Ly/Δh), Int(Lz/Δz)
 
 α = Δz/Δh # Grid cell aspect ratio.
-νh, κh = 20, 20
+νh, κh = 20.0, 20.0
 νv, κv = α*νh, α*κh
 
 @show Nx, Ny, Nz
@@ -27,15 +52,23 @@ Nx, Ny, Nz = Int(Lx/Δh), Int(Ly/Δh), Int(Lz/Δz)
 model = ChannelModel(N=(Nx, Ny, Nz), L=(Lx, Ly, Lz),
                        νh=νh, νv=νv, κh=κh, κv=κv)
 
-Ty = 1e-5  # Meridional temperature gradient [K/m].
+nan_checker = NaNChecker(10, [model.velocities.w, model.tracers.T], ["w", "T"])
+push!(model.diagnostics, nan_checker)
+
+Ty = 1e-4  # Meridional temperature gradient [K/m].
 Tz = 5e-3  # Vertical temperature gradient [K/m].
-T₀(x, y, z) = 15 + Ty*y * Tz*z  # Initial temperature field [°C].
+T₀(x, y, z) = 10 + Ty*min(max(0, y-225e3), 50e3) + Tz*z + 0.0001*rand()  # Initial temperature field [°C].
 
 set_ic!(model, T=T₀)
 
-fig, ax = subplots(ncols=2, nrows=1, figsize=(16, 9))
+fig, ax = subplots(ncols=3, nrows=1, figsize=(21, 7))
+update_plot!(ax, model)
 
-for n in 1:100
+for n in 1:1000
+    i, t = model.clock.iteration, model.clock.time
+    Tavg = mean(data(model.tracers.T))
+    @info "i = $i, t = $(Int(round(t/3600))) hours, ⟨T⟩=$Tavg °C"
+
     update_plot!(ax, model)
-    time_step!(model; Nt=100, Δt=60)
+    time_step!(model; Nt=100, Δt=5*60)
 end
