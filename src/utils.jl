37481bbb8a14c6e2a5fda90d09468354759d216e
Gregory Wagner <wagner.greg@gmail.com>
2019-08-03 16:44:56 -0400

    may not need to adapt structure for SubArrays anymore
    Former-commit-id: 6935186bd4f81424308868dfbb81d9de3940f3b0

--- src/utils.jl
+++ src/utils.jl
@@ -1,12 +1,10 @@
-using Printf
-
 # Adapt an offset CuArray to work nicely with CUDA kernels.
 Adapt.adapt_structure(to, x::OffsetArray) = OffsetArray(adapt(to, parent(x)), x.offsets)
 
 # Need to adapt SubArray indices as well.
 # See: https://github.com/JuliaGPU/Adapt.jl/issues/16
-Adapt.adapt_structure(to, A::SubArray{<:Any,<:Any,AT}) where {AT} =
-    SubArray(adapt(to, parent(A)), adapt.(Ref(to), parentindices(A)))
+#Adapt.adapt_structure(to, A::SubArray{<:Any,<:Any,AT}) where {AT} =
+#    SubArray(adapt(to, parent(A)), adapt.(Ref(to), parentindices(A)))
 
 # Source: https://github.com/JuliaCI/BenchmarkTools.jl/blob/master/src/trials.jl
 function prettytime(t)
