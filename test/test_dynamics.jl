5a2aa5f5e10b28a97fda215708f58e83ef47491a
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-08-06 19:07:09 -0400

    Make Pearson vortex a real test.
    Former-commit-id: a2cabed3e91f9f3e81467bdf11607c5ced1e9b8f

--- test/test_dynamics.jl
+++ test/test_dynamics.jl
@@ -1,3 +1,5 @@
+using Printf
+
 xnodes(ϕ) = repeat(reshape(ϕ.grid.xC, ϕ.grid.Nx, 1, 1), 1, ϕ.grid.Ny, ϕ.grid.Nz)
 ynodes(ϕ) = repeat(reshape(ϕ.grid.yC, 1, ϕ.grid.Ny, 1), ϕ.grid.Nx, 1, ϕ.grid.Nz)
 znodes(ϕ) = repeat(reshape(ϕ.grid.zC, 1, 1, ϕ.grid.Nz), ϕ.grid.Nx, ϕ.grid.Ny, 1)
@@ -207,6 +209,71 @@ function passive_tracer_advection_test(; N=128, κ=1e-12, Nt=100)
     return T_relative_error(model, T) < 1e-4
 end
 
+"""
+Pearson vortex test
+See p. 310 of "Nodal Discontinuous Galerkin Methods: Algorithms, Analysis, and Application" by Hesthaven & Warburton.
+"""
+function pearson_vortex_test(arch; FT=Float64, N=64, Nt=10)
+    Nx, Ny, Nz = N, N, 2
+    Lx, Ly, Lz = 1, 1, 1
+    ν = 1
+
+    # Choose a very small time step ~O(1/Δx²) as we are diffusion-limited in this test.
+    Δt = 1 / (10*π*Nx^2)
+
+    # Pearson vortex analytic solution.
+    @inline u(x, y, z, t) = -sin(2π*y) * exp(-4π^2 * ν * t)
+    @inline v(x, y, z, t) =  sin(2π*x) * exp(-4π^2 * ν * t)
+
+    # We end up with time-dependent Dirchlet boundary conditions at the top and bottom for u and v.
+    @inline u_top(i, j, grid, t, args...) = u(grid.xF[i], grid.yC[j], 0, t)
+    @inline v_top(i, j, grid, t, args...) = v(grid.xC[i], grid.yF[j], 0, t)
+    @inline u_bot(i, j, grid, t, args...) = u(grid.xF[i], grid.yC[j], model.grid.Lz, t)
+    @inline v_bot(i, j, grid, t, args...) = v(grid.xC[i], grid.yF[j], model.grid.Lz, t)
+
+    ubcs = HorizontallyPeriodicBCs(   top = BoundaryCondition(Value, u_top),
+                                   bottom = BoundaryCondition(Value, u_bot))
+
+    vbcs = HorizontallyPeriodicBCs(   top = BoundaryCondition(Value, v_top),
+                                   bottom = BoundaryCondition(Value, v_bot))
+
+    model = Model(N = (Nx, Ny, Nz), L = (Lx, Ly, Lz), arch = arch,
+                  constants = PlanetaryConstants(f=0, g=0),  # Turn off rotation and gravity.
+                    closure = ConstantIsotropicDiffusivity(FT; ν = 1, κ = 0),  # Turn off diffusivity.
+                        eos = LinearEquationOfState(βT=0, βS=0),  # Turn off buoyancy.
+                        bcs = BoundaryConditions(u=ubcs, v=vbcs))
+
+    u₀(x, y, z) = u(x, y, z, 0)
+    v₀(x, y, z) = v(x, y, z, 0)
+    T₀(x, y, z) = 0
+    S₀(x, y, z) = 0
+
+    set_ic!(model; u=u₀, v=v₀, T=T₀, S=S₀)
+
+    time_step!(model, Nt, Δt)
+
+    xC, yC, zC = reshape(model.grid.xC, (Nx, 1, 1)), reshape(model.grid.yC, (1, Ny, 1)), reshape(model.grid.zC, (1, 1, Nz))
+    xF, yF = reshape(model.grid.xF[1:end-1], (Nx, 1, 1)), reshape(model.grid.yF[1:end-1], (1, Ny, 1))
+
+    t = model.clock.time
+    i = model.clock.iteration
+
+    # Calculate relative error between model and analytic solutions for u and v.
+    u_rel_err = abs.((data(model.velocities.u) .- u.(xF, yC, zC, t)) ./ u.(xF, yC, zC, t))
+    u_rel_err_avg = mean(u_rel_err)
+    u_rel_err_max = maximum(u_rel_err)
+
+    v_rel_err = abs.((data(model.velocities.v) .- v.(xC, yF, zC, t)) ./ v.(xC, yF, zC, t))
+    v_rel_err_avg = mean(v_rel_err)
+    v_rel_err_max = maximum(v_rel_err)
+
+    @info "Pearson vortex test ($arch, $FT) with Nx=Ny=$N @ Nt=$Nt: " *
+          @sprintf("Δu: (avg=%6.3g, max=%6.3g), Δv: (avg=%6.3g, max=%6.3g)\n",
+                   u_rel_err_avg, u_rel_err_max, v_rel_err_avg, v_rel_err_max)
+
+    u_rel_err_max < 5e-6 && v_rel_err_max < 5e-6
+end
+
 @testset "Dynamics" begin
     println("Testing dynamics...")
 
@@ -239,5 +306,10 @@ end
     @testset "Internal wave" begin
         println("  Testing internal wave...")
         @test internal_wave_test()
+    end
+
+    @testset "Pearson vortex" begin
+        println("  Testing Pearson vortex...")
+        @test pearson_vortex_test(CPU())
     end
 end
