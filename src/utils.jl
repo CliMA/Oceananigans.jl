59fb16f817c63e1524c4a63ba06d7a20ae8d00d3
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-08-06 19:48:48 -0400

    Move dynamic launch configuration to utils.
    Former-commit-id: 7d8148bff19cf153c36672c12915be30800da5a7

--- src/utils.jl
+++ src/utils.jl
@@ -100,3 +100,26 @@ function update_Δt!(wizard, model)
 
     return nothing
 end
+
+# Dynamic launch configuration
+function launch_config(grid, dims)
+    return function (kernel)
+        fun = kernel.fun
+        config = launch_configuration(fun)
+
+        # adapt the suggested config from 1D to the requested grid dimensions
+        if dims == 3
+            threads = floor(Int, cbrt(config.threads))
+            blocks = ceil.(Int, [grid.Nx, grid.Ny, grid.Nz] ./ threads)
+            threads = [threads, threads, threads]
+        elseif dims == 2
+            threads = floor(Int, sqrt(config.threads))
+            blocks = ceil.(Int, [grid.Nx, grid.Ny] ./ threads)
+            threads = [threads, threads]
+        else
+            error("unsupported launch configuration")
+        end
+
+        return (threads=Tuple(threads), blocks=Tuple(blocks))
+    end
+end
