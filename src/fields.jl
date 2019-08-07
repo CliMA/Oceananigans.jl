62ce8cb4f9539d31f477178bed1669222abe4362
Gregory Wagner <wagner.greg@gmail.com>
2019-08-07 17:54:33 -0400

    more powerful set! functions
    Former-commit-id: 8ea9d10bbd822bf04108108ec9b851f933c24b8f

--- src/fields.jl
+++ src/fields.jl
@@ -1,11 +1,9 @@
- import GPUifyLoops: @launch, @loop
-
 """
     CellField{A<:AbstractArray, G<:Grid} <: Field
 
 A cell-centered field defined on a grid `G` whose values are stored in an `A`.
 """
-struct CellField{A<:AbstractArray, G<:Grid} <: Field
+struct CellField{A<:AbstractArray, G<:Grid} <: Field{A, G}
     data::A
     grid::G
 end
@@ -15,7 +13,7 @@ end
 
 An x-face-centered field defined on a grid `G` whose values are stored in an `A`.
 """
-struct FaceFieldX{A<:AbstractArray, G<:Grid} <: FaceField
+struct FaceFieldX{A<:AbstractArray, G<:Grid} <: FaceField{A, G}
     data::A
     grid::G
 end
@@ -25,7 +23,7 @@ end
 
 A y-face-centered field defined on a grid `G` whose values are stored in an `A`.
 """
-struct FaceFieldY{A<:AbstractArray, G<:Grid} <: FaceField
+struct FaceFieldY{A<:AbstractArray, G<:Grid} <: FaceField{A, G}
     data::A
     grid::G
 end
@@ -35,17 +33,7 @@ end
 
 A z-face-centered field defined on a grid `G` whose values are stored in an `A`.
 """
-struct FaceFieldZ{A<:AbstractArray, G<:Grid} <: FaceField
-    data::A
-    grid::G
-end
-
-"""
-    EdgeField{A<:AbstractArray, G<:Grid} <: Field
-
-An edge-centered field defined on a grid `G` whose values are stored in an `A`.
-"""
-struct EdgeField{A<:AbstractArray, G<:Grid} <: Field
+struct FaceFieldZ{A<:AbstractArray, G<:Grid} <: FaceField{A, G}
     data::A
     grid::G
 end
@@ -84,19 +72,15 @@ Return a `FaceFieldZ` with element type `T` on `arch` and `grid`.
 """
 FaceFieldZ(T, arch, grid) = FaceFieldZ(zeros(T, arch, grid), grid)
 
-"""
-    EdgeField([T=eltype(grid)], arch, grid)
-
-Return an `EdgeField` with element type `T` on `arch` and `grid`.
-`T` defaults to the element type of `grid`.
-"""
- EdgeField(T, arch, grid) =  EdgeField(zeros(T, arch, grid), grid)
-
  CellField(arch, grid) =  CellField(zeros(arch, grid), grid)
 FaceFieldX(arch, grid) = FaceFieldX(zeros(arch, grid), grid)
 FaceFieldY(arch, grid) = FaceFieldY(zeros(arch, grid), grid)
 FaceFieldZ(arch, grid) = FaceFieldZ(zeros(arch, grid), grid)
- EdgeField(arch, grid) =  EdgeField(zeros(arch, grid), grid)
+
+fieldtype(::CellField) = CellField
+fieldtype(::FaceFieldX) = FaceFieldX
+fieldtype(::FaceFieldY) = FaceFieldY
+fieldtype(::FaceFieldZ) = FaceFieldZ
 
 @inline size(f::Field) = size(f.grid)
 @inline length(f::Field) = length(f.data)
@@ -121,15 +105,12 @@ FaceFieldZ(arch, grid) = FaceFieldZ(zeros(arch, grid), grid)
 show(io::IO, f::Field) = show(io, f.data)
 iterate(f::Field, state=1) = iterate(f.data, state)
 
-set!(u::Field, v) = u.data.parent .= convert(eltype(u.grid), v)
-set!(u::Field, v::Field) = @. u.data = v.data
-
 # Define +, -, and * on fields as element-wise calculations on their data. This
 # is only true for fields of the same type, e.g. when adding a FaceFieldY to
 # another FaceFieldY, otherwise some interpolation or averaging must be done so
 # that the two fields are defined at the same point, so the operation which
 # will not be commutative anymore.
-for ft in (:CellField, :FaceFieldX, :FaceFieldY, :FaceFieldZ, :EdgeField)
+for ft in (:CellField, :FaceFieldX, :FaceFieldY, :FaceFieldZ)
     for op in (:+, :-, :*)
         @eval begin
             # +, -, * a Field by a Number on the left.
@@ -164,37 +145,125 @@ nodes(ϕ) = (xnodes(ϕ), ynodes(ϕ), znodes(ϕ))
 
 zerofunk(args...) = 0
 
-set_ic!(model; vals..) = set!(mode; vals...)  # legacy wrapper
+set!(u::Field, v::Number) = @. u.data = v
+set!(u::Field{A}, v::Field{A}) where A = @. u.data.parent = v.data.parent
 
-function set!(model; ics...)
-    for (fld, ic) in ics
-        if fld ∈ (:u, :v, :w)
-            ϕ = getproperty(model.velocities, fld)
+"Set the CPU field `u` to the array `v`."
+function set!(u::Field{A}, v::Array) where {
+    A <: OffsetArray{T, D, <:Array} where {T, D}}
+    for k in 1:u.grid.Nz, j in 1:u.grid.Ny, i in 1:u.grid.Nx
+        u[i, j, k] = v[i, j, k]
+    end
+    return nothing
+end
+
+"Set the GPU field `u` to the array `v`."
+@hascuda function set!(u::Field{A}, v::Array) where {
+    A <: OffsetArray{T, D, <:CuArray} where {T, D}}
+
+    FieldType = fieldtype(u)
+    v_field = FieldType(CPU(), u.grid)
+    set!(v_field, v)
+    set!(u, v_field)
+    return nothing
+end
+
+"Set the GPU field `u` to the CuArray `v`."
+@hascuda function set!(u::Field{A}, v::CuArray) where {
+    A <: OffsetArray{T, D, <:CuArray} where {T, D}}
+    @launch device(GPU()) config=launch_config(u.grid, 3) _set_gpu!(u.data, v, u.grid)
+end
+
+function _set_gpu!(u, v, grid)
+    @loop for k in (1:grid.Nz; (blockIdx().z - 1) * blockDim().z + threadIdx().z)
+        @loop for j in (1:grid.Ny; (blockIdx().y - 1) * blockDim().y + threadIdx().y)
+            @loop for i in (1:grid.Nx; (blockIdx().x - 1) * blockDim().x + threadIdx().x)
+	            u[i, j, k] = v[i, j, k]
+	        end
+	    end
+    end
+    return nothing
+end
+
+"Set the GPU field `u` data to the CPU field data of `v`."
+@hascuda function set!(u::Field{Au}, v::Field{Av}) where {
+    Au<:OffsetArray{T1, D, <:CuArray}, Av<:OffsetArray{T2, D, <:Array} where {
+    T1, T2, D}}
+
+    copyto!(u.data.parent, v.data.parent)
+    return nothing
+end
+
+"Set the CPU field `u` data to the GPU field data of `v`."
+@hascuda function set!(u::Field{Au}, v::Field{Av}) where {
+    Au<:OffsetArray{T1, D, <:Array}, Av<:OffsetArray{T2, D, <:CuArray} where {
+    T1, T2, D}}
+
+    u.data.parent .= Array(v.data.parent)
+    return nothing
+end
+
+"Set the CPU field `u` data to the function `f(x, y, z)`."
+set!(u::Field, f::Function) = data(u) .= f.(nodes(u)...)
+
+"Set the GPU field `u` data to the function `f(x, y, z)`."
+@hascuda function set!(u::Field{A1}, f::Function) where {
+    A1 <: OffsetArray{T, D, <:CuArray} where {T, D}}
+                                                                 
+    u_cpu = CellField(CPU(), u.grid)
+    set!(u_cpu, f)
+    set!(u, u_cpu)
+    return nothing
+end
+
+"""
+    set!(model; kwargs...)
+
+Set velocity and tracer fields of `model`. The keyword arguments 
+`kwargs...` take the form `name=data`, where `name` refers to one of the
+fields of `model.velocities` or `model.tracers`, and the `data` may be an array, 
+a function with arguments `(x, y, z)`, or any data type for which a 
+`set!(ϕ::Field, data)` function exists.
+
+Example
+=======
+
+model = Model(grid=RegularCartesianGrid(N=(32, 32, 32), L=(1, 1, 1))
+
+# Set u to a parabolic function of z, v to random numbers damped
+# at top and bottom, and T to some silly array of half zeros, 
+# half random numbers.
+
+u₀(x, y, z) = z/model.grid.Lz * (1 + z/model.grid.Lz)
+v₀(x, y, z) = 1e-3 * rand() * u₀(x, y, z)
+
+T₀ = rand(size(model.grid)...)
+T₀[T₀ .< 0.5] .= 0
+
+set!(model, u=u₀, v=v₀, T=T₀)
+"""
+function set!(model::AbstractModel; kwargs...)
+    for (fldname, value) in kwargs
+        if fldname ∈ propertynames(model.velocities)
+            ϕ = getproperty(model.velocities, fldname)
+        elseif fldname ∈ propertynames(model.tracers)
+            ϕ = getproperty(model.tracers, fldname)
         else
-            ϕ = getproperty(model.tracers, fld)
+            error("Name $fldname not found in model.velocities or model.tracers.")
         end
-
-        arch, grid = model.arch, model.grid
-        @launch device(arch) config=launch_config(grid, 3) set!(grid, ϕ.data, ic, nodes(ϕ)...)
+        @show data(ϕ) value
+        set!(ϕ, value)
+        @show data(ϕ)
     end
+    return nothing
 end
 
-function set!(grid, ϕ, ic::AbstractArray, args...)
-    @loop for k in (1:grid.Nz; (blockIdx().z - 1) * blockDim().z + threadIdx().z)
-        @loop for j in (1:grid.Ny; (blockIdx().y - 1) * blockDim().y + threadIdx().y)
-            @loop for i in (1:grid.Nx; (blockIdx().x - 1) * blockDim().x + threadIdx().x)
-                ϕ[i, j, k] = ic[i, j, k]
-            end
-        end
+function set!(Φ::NamedTuple; kwargs...)
+    for (fldname, value) in kwargs
+        ϕ = getproperty(Φ, fldname)
+        set!(ϕ, value)
     end
+    return nothing
 end
 
-function set!(grid, ϕ, ic::Function, xN, yN, zN)
-    @loop for k in (1:grid.Nz; (blockIdx().z - 1) * blockDim().z + threadIdx().z)
-        @loop for j in (1:grid.Ny; (blockIdx().y - 1) * blockDim().y + threadIdx().y)
-            @loop for i in (1:grid.Nx; (blockIdx().x - 1) * blockDim().x + threadIdx().x)
-                ϕ[i, j, k] = ic(xN[i], yN[j], zN[k])
-            end
-        end
-    end
-end
+set_ic!(model; kwargs...) = set!(model; kwargs...)  # legacy wrapper
