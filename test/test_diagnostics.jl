5a49f9fcf554d82e1a45c176e3a306f94e2da1e0
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-08-10 18:00:29 -0400

    Test for horizontally averaged vertical profile
    Former-commit-id: 9918274562398b2eba887dee2f3fa4c7fe086a47

--- test/test_diagnostics.jl
+++ test/test_diagnostics.jl
@@ -1,3 +1,17 @@
+function horizontally_averaged_vertical_profile_is_correct(arch, FT)
+    model = Model(N = (16, 16, 16), L = (100, 100, 100), arch=arch, float_type=FT)
+    
+    # Set a linear stably stratified temperature profile.
+    T₀(x, y, z) = 20 + 0.01*z
+    set!(model; T=T₀)
+    
+    T̅ = HorizontallyAveragedVerticalProfile(model, model.tracers.T; frequency=1)
+    push!(model.diagnostics, T̅)
+
+    time_step!(model, 1, 1)
+    all(@. T̅.profile[:] ≈ 20 + 0.01 * model.grid.zC)
+end
+
 function nan_checker_aborts_simulation(arch, FT)
     model = Model(N = (16, 16, 2), L = (1, 1, 1), arch=arch, float_type=FT)
 
@@ -13,10 +27,17 @@ end
 @testset "Diagnostics" begin
     println("Testing diagnostics...")
 
+    @testset "Horizontally averaged vertical profiles" begin
+        println("  Testing horizontally averaged vertical profiles...")
+        for arch in archs, FT in float_types
+            @test horizontally_averaged_vertical_profile_is_correct(arch, FT)
+        end
+    end
+
     @testset "NaN Checker" begin
         println("  Testing NaN Checker...")
-        for arch in archs, ft in float_types
-            @test_throws ErrorException nan_checker_aborts_simulation(arch, ft)
+        for arch in archs, FT in float_types
+            @test_throws ErrorException nan_checker_aborts_simulation(arch, FT)
         end
     end
 end
