fba057564dd5136089250f7b9c1af9aef59ec26b
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-08-10 17:09:59 -0400

    Parallel cumulative sum CUDA kernel for horizontal averages.

--- src/diagnostics.jl
+++ src/diagnostics.jl
@@ -1,6 +1,8 @@
 using Statistics: mean
 using Printf
 
+@hascuda using CUDAdrv, CUDAnative
+
 time_to_run(clock, diag) = (clock.iteration % diag.frequency) == 0
 
 ####
@@ -15,6 +17,55 @@ function velocity_div!(grid::RegularCartesianGrid, u, v, w, div)
             end
         end
     end
+end
+
+# Work-inefficient inclusive 3D scan that reduces along the xy dimensions.
+# Modified from: https://github.com/JuliaGPU/CUDAnative.jl/blob/master/examples/scan.jl
+@hascuda function gpu_accumulate_xy!(Rxy::CuDeviceArray{T}, Rx::CuDeviceArray{T}, data::CuDeviceArray{T}, op::Function) where {T}
+    lvl, lvls = blockIdx().y,  gridDim().y
+    col, cols = blockIdx().x,  gridDim().x
+    row, rows = threadIdx().x, blockDim().x
+
+    if lvl <= lvls && col <= cols && row <= rows
+        shmem = @cuDynamicSharedMem(T, 2*rows)
+        shmem[row] = data[row, col, lvl]
+        sync_threads()
+
+        # parallel reduction
+        pin, pout, offset = 1, 0, 1
+        while offset < rows
+            pout = 1 - pout
+            pin = 1 - pin
+
+            if row > offset
+                shmem[pout * rows + row] = op(shmem[pin * rows + row], shmem[pin * rows + row - offset])
+            else
+                 shmem[pout * rows + row] = shmem[pin * rows + row]
+            end
+
+            sync_threads()
+            offset *= UInt32(2)
+        end
+
+        shmem[pin * rows + row] = shmem[pout * rows + row]
+        sync_threads()
+
+        # write results
+        if row == rows
+            Rx[1, col, lvl] = shmem[row]
+        end
+        sync_threads()
+
+        if col == cols
+            sum = 0
+            for j in 1:cols
+                sum += Rx[1, j, lvl]
+            end
+            Rxy[1, 1, lvl] = sum
+        end
+    end
+
+    return
 end
 
 ####
