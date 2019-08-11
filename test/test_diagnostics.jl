6ba5703ce4a1d13b38c8f680a9d6b159b3826314
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-08-10 22:33:58 -0400

    Add test for product profiles

--- test/test_diagnostics.jl
+++ test/test_diagnostics.jl
@@ -1,17 +1,30 @@
-
-
-function horizontally_averaged_vertical_profile_is_correct(arch, FT)
+function vertical_profile_is_correct(arch, FT)
     model = Model(N = (16, 16, 16), L = (100, 100, 100), arch=arch, float_type=FT)
     
     # Set a linear stably stratified temperature profile.
     T₀(x, y, z) = 20 + 0.01*z
     set!(model; T=T₀)
     
-    T̅ = HorizontallyAveragedVerticalProfile(model, model.tracers.T; interval=0.5second)
+    T̅ = VerticalProfile(model, model.tracers.T; interval=0.5second)
     push!(model.diagnostics, T̅)
 
     time_step!(model, 1, 1)
     all(@. T̅.profile[:] ≈ 20 + 0.01 * model.grid.zC)
+end
+
+function product_profile_is_correct(arch, FT)
+    model = Model(N = (16, 16, 16), L = (100, 100, 100), arch=arch, float_type=FT)
+
+    # Set a linear stably stratified temperature profile and a sinusoidal u(z) profile.
+    u₀(x, y, z) = sin(z)
+    T₀(x, y, z) = 20 + 0.01*z
+    set!(model; u=u₀, T=T₀)
+
+    uT = ProductProfile(model, model.velocities.u, model.tracers.T; interval=0.5second)
+    run_diagnostic(model, uT)
+
+    correct_profile = @. sin.(model.grid.zC) * (20 + 0.01 * model.grid.zC)
+    Array(uT.profile[:]) ≈ correct_profile
 end
 
 function nan_checker_aborts_simulation(arch, FT)
