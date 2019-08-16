a72591cccad5ae021e4a62e592ae6ee4b5651dab
Gregory Wagner <wagner.greg@gmail.com>
2019-08-16 12:25:33 -0400

    cleans up unused functions and unnecessary exports, exports verstappen closure

--- src/turbulence_closures/TurbulenceClosures.jl
+++ src/turbulence_closures/TurbulenceClosures.jl
@@ -4,9 +4,16 @@ export
   MolecularDiffusivity,
   IsotropicDiffusivity,
   ConstantIsotropicDiffusivity,
+
   ConstantAnisotropicDiffusivity,
-  AnisotropicMinimumDissipation,
+
   ConstantSmagorinsky,
+  DeardorffSmagorinsky,
+  BlasiusSmagorinsky,
+
+  AnisotropicMinimumDissipation,
+  RozemaAnisotropicMinimumDissipation,
+  VerstappenAnisotropicMinimumDissipation,
 
   TurbulentDiffusivities,
   calc_diffusivities!,
@@ -16,23 +23,7 @@ export
   ∇_κ_∇S,
   ∂ⱼ_2ν_Σ₁ⱼ,
   ∂ⱼ_2ν_Σ₂ⱼ,
-  ∂ⱼ_2ν_Σ₃ⱼ,
-
-  ν₁₁, ν₂₂, ν₃₃,
-  κ₁₁, κ₂₂, κ₃₃,
-
-  ∂x_caa, ∂x_faa, ∂x²_caa, ∂x²_faa,
-  ∂y_aca, ∂y_afa, ∂y²_aca, ∂y²_afa,
-  ∂z_aac, ∂z_aaf, ∂z²_aac, ∂z²_aaf,
-
-  ▶x_caa, ▶x_faa,
-  ▶y_aca, ▶y_afa,
-  ▶z_aac, ▶z_aaf,
-
-  ▶xy_cca, ▶xz_cac, ▶yz_acc,
-  ▶xy_cfa, ▶xz_caf, ▶yz_acf,
-  ▶xy_fca, ▶xz_fac, ▶yz_afc,
-  ▶xy_ffa, ▶xz_faf, ▶yz_aff
+  ∂ⱼ_2ν_Σ₃ⱼ
 
 using
   Oceananigans,
@@ -45,31 +36,8 @@ abstract type TurbulenceClosure{T} end
 abstract type IsotropicDiffusivity{T} <: TurbulenceClosure{T} end
 abstract type TensorDiffusivity{T} <: TurbulenceClosure{T} end
 
-# Tensors... !
-κ₁₁_ccc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = κ_ccc(i, j, k, grid, closure, args...)
-κ₂₂_ccc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = κ_ccc(i, j, k, grid, closure, args...)
-κ₃₃_ccc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = κ_ccc(i, j, k, grid, closure, args...)
-
-ν₁₁_ccc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_ccc(i, j, k, grid, closure, args...)
-ν₂₂_ccc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_ccc(i, j, k, grid, closure, args...)
-ν₃₃_ccc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_ccc(i, j, k, grid, closure, args...)
-
-ν₁₁_ffc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_ffc(i, j, k, grid, closure, args...)
-ν₂₂_ffc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_ffc(i, j, k, grid, closure, args...)
-ν₃₃_ffc(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_ffc(i, j, k, grid, closure, args...)
-
-ν₁₁_fcf(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_fcf(i, j, k, grid, closure, args...)
-ν₂₂_fcf(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_fcf(i, j, k, grid, closure, args...)
-ν₃₃_fcf(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_fcf(i, j, k, grid, closure, args...)
-
-ν₁₁_cff(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_cff(i, j, k, grid, closure, args...)
-ν₂₂_cff(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_cff(i, j, k, grid, closure, args...)
-ν₃₃_cff(i, j, k, grid, closure::IsotropicDiffusivity, args...) = ν_cff(i, j, k, grid, closure, args...)
-
 @inline ∇_κ_∇T(args...) = ∇_κ_∇c(args...)
 @inline ∇_κ_∇S(args...) = ∇_κ_∇c(args...)
-
-@inline geo_mean_Δ(grid::RegularCartesianGrid{T}) where T = (grid.Δx * grid.Δy * grid.Δz)^T(1/3)
 
 function typed_keyword_constructor(T, Closure; kwargs...)
     closure = Closure(; kwargs...)
@@ -81,19 +49,11 @@ end
 Base.eltype(::TurbulenceClosure{T}) where T = T
 
 include("closure_operators.jl")
-include("velocity_gradients.jl")
+include("velocity_tracer_gradients.jl")
 include("constant_diffusivity_closures.jl")
 include("smagorinsky.jl")
-include("anisotropic_minimum_dissipation.jl")
-
-# Packaged operators
-ν₁₁ = (ccc=ν₁₁_ccc, ffc=ν₁₁_ffc, fcf=ν₁₁_fcf, cff=ν₁₁_cff)
-ν₂₂ = (ccc=ν₂₂_ccc, ffc=ν₂₂_ffc, fcf=ν₂₂_fcf, cff=ν₂₂_cff)
-ν₃₃ = (ccc=ν₃₃_ccc, ffc=ν₃₃_ffc, fcf=ν₃₃_fcf, cff=ν₃₃_cff)
-
-κ₁₁ = (ccc=κ₁₁_ccc, )
-κ₂₂ = (ccc=κ₂₂_ccc, )
-κ₃₃ = (ccc=κ₃₃_ccc, )
+include("rozema_anisotropic_minimum_dissipation.jl")
+include("verstappen_anisotropic_minimum_dissipation.jl")
 
 basetype(::ConstantSmagorinsky) = ConstantSmagorinsky
 basetype(::ConstantIsotropicDiffusivity) = ConstantIsotropicDiffusivity
