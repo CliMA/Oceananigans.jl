801cc24ed15946909d66fd33501084aaef97ee57
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-08-22 21:26:59 -0400

    Test for product profiles
    Former-commit-id: 0015fea42d548c73a76d04993c99f4217c5c5bc9

--- test/test_diagnostics.jl
+++ test/test_diagnostics.jl
@@ -12,6 +12,21 @@ function horizontal_average_is_correct(arch, FT)
     all(@. T̅.profile[:] ≈ 20 + 0.01 * model.grid.zC)
 end
 
+function product_profile_is_correct(arch, FT)
+    model = Model(N = (16, 16, 16), L = (100, 100, 100), arch=arch, float_type=FT)
+
+    # Set a linear stably stratified temperature profile and a sinusoidal u(z) profile.
+    u₀(x, y, z) = sin(z)
+    T₀(x, y, z) = 20 + 0.01*z
+    set!(model; u=u₀, T=T₀)
+
+    uT = HorizontalAverage(model, [model.velocities.u, model.tracers.T]; interval=0.5second)
+    run_diagnostic(model, uT)
+
+    correct_profile = @. sin.(model.grid.zC) * (20 + 0.01 * model.grid.zC)
+    Array(uT.profile[:]) ≈ correct_profile
+end
+
 function nan_checker_aborts_simulation(arch, FT)
     model = Model(N = (16, 16, 2), L = (1, 1, 1), arch=arch, float_type=FT)
 
@@ -31,6 +46,7 @@ end
         @testset "Horizontal average [$(typeof(arch))]" begin
             println("  Testing horizontal average [$(typeof(arch))]")
             @test horizontal_average_is_correct(arch, Float64)
+            @test product_profile_is_correct(arch, Float64)
         end
     end
     
