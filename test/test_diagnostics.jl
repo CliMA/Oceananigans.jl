da026b65aecf49ea38e803e20b615e9fb5c80226
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-08-22 20:48:17 -0400

    Nuke ProductProfile and VelocityCovariances diagnostics

--- test/test_diagnostics.jl
+++ test/test_diagnostics.jl
@@ -12,35 +12,6 @@ function vertical_profile_is_correct(arch, FT)
     all(@. T̅.profile[:] ≈ 20 + 0.01 * model.grid.zC)
 end
 
-function product_profile_is_correct(arch, FT)
-    model = Model(N = (16, 16, 16), L = (100, 100, 100), arch=arch, float_type=FT)
-
-    # Set a linear stably stratified temperature profile and a sinusoidal u(z) profile.
-    u₀(x, y, z) = sin(z)
-    T₀(x, y, z) = 20 + 0.01*z
-    set!(model; u=u₀, T=T₀)
-
-    uT = ProductProfile(model, model.velocities.u, model.tracers.T; interval=0.5second)
-    run_diagnostic(model, uT)
-
-    correct_profile = @. sin.(model.grid.zC) * (20 + 0.01 * model.grid.zC)
-    Array(uT.profile[:]) ≈ correct_profile
-end
-
-function velocity_covariances_initialize(arch, FT)
-    model = Model(N = (16, 16, 16), L = (100, 100, 100), arch=arch, float_type=FT)
-
-    u₀(x, y, z) = sin(z)
-    v₀(x, y, z) = x
-    w₀(x, y, z) = tanh(x*y)
-    set!(model; u=u₀, v=v₀, w=w₀)
-
-    vcp = VelocityCovarianceProfiles(model; interval=0.5second) 
-    run_diagnostic(model, vcp)
-
-    return true  # Just check that it didn't crash.
-end
-
 function nan_checker_aborts_simulation(arch, FT)
     model = Model(N = (16, 16, 2), L = (1, 1, 1), arch=arch, float_type=FT)
 
@@ -60,8 +31,6 @@ end
         @testset "Vertical profiles [$(typeof(arch))]" begin
             println("  Testing vertical profiles [$(typeof(arch))]")
             @test vertical_profile_is_correct(arch, Float64)
-            @test product_profile_is_correct(arch, Float64)
-            @test velocity_covariances_initialize(arch, Float64)
         end
     end
     
