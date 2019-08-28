4e15c946cb4e840dfedabe6d1ef18916e7cf2153
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-08-27 21:13:43 -0400

    Switch from gpu_accumulate_xy to zeroing halo regions to calculate means
    Former-commit-id: a5c05f45d46d3a3e8ef867b10514f3e86e0d9573

--- src/diagnostics.jl
+++ src/diagnostics.jl
@@ -37,61 +37,6 @@ function velocity_div!(grid::RegularCartesianGrid, u, v, w, div)
     end
 end
 
-@inline _prod(data, ::Nothing, i, j, k) = @inbounds data[i, j, k]
-@inline _prod(data1,    data2, i, j, k) = @inbounds data1[i, j, k] * data2[i, j, k]
-
-# Work-inefficient inclusive scan that reduces along the xy dimensions.
-# Useful for computing mean(; dim=[1, 2]) lol.
-# Modified from: https://github.com/JuliaGPU/CUDAnative.jl/blob/master/examples/scan.jl
-@hascuda function gpu_accumulate_xy!(Rxy::CuDeviceArray{T}, Rx, data1, data2, op::Function) where T
-    lvl, lvls = blockIdx().y,  gridDim().y
-    col, cols = blockIdx().x,  gridDim().x
-    row, rows = threadIdx().x, blockDim().x
-
-    if lvl <= lvls && col <= cols && row <= rows
-        shmem = @cuDynamicSharedMem(T, 2*rows)
-        shmem[row] = _prod(data1, data2, row, col, lvl)
-        sync_threads()
-
-        # parallel reduction
-        pin, pout, offset = 1, 0, 1
-        while offset < rows
-            pout = 1 - pout
-            pin = 1 - pin
-
-            if row > offset
-                shmem[pout * rows + row] = op(shmem[pin * rows + row], shmem[pin * rows + row - offset])
-            else
-                 shmem[pout * rows + row] = shmem[pin * rows + row]
-            end
-
-            sync_threads()
-            offset *= UInt32(2)
-        end
-
-        shmem[pin * rows + row] = shmem[pout * rows + row]
-        sync_threads()
-
-        # write results
-        if row == rows
-            Rx[1, col, lvl] = shmem[rows]
-        end
-        sync_threads()
-
-        if col == cols && row == rows
-            sum = 0
-            @cuprintf(" ")  # Have absolutely no idea why this is "needed" but horizontal average is wrong without it...
-            for j in 1:cols
-                sum += Rx[1, j, lvl]
-            end
-
-            Rxy[1, 1, lvl] = real(sum)
-        end
-    end
-
-    return
-end
-
 ####
 #### Horizontally averaged vertical profiles
 ####
@@ -105,33 +50,28 @@ mutable struct HorizontalAverage{P, F, I, T} <: Diagnostic
 end
 
 tupleit(t::Tuple) = t
-tupleit(nt) = Tuple(nt)
+tupleit(nt) = tuple(nt)
 
 function HorizontalAverage(model, fields; frequency=nothing, interval=nothing)
     fields = tupleit(fields)
-    length(fields) > 2 && @error "Cannot take horizontal average of more than 2 fields."
+    fields = Tuple([field.data.parent for field in fields])
+    @show typeof(fields)
     validate_interval(frequency, interval)
-    profile = zeros(model.arch, model.grid, 1, 1, model.grid.Nz)
+    profile = zeros(model.arch, model.grid, 1, 1, model.grid.Tz)
     HorizontalAverage(profile, fields, frequency, interval, 0.0)
 end
 
-function run_diagnostic(model::Model, P::HorizontalAverage{<:Array})
+function run_diagnostic(model::Model, P::HorizontalAverage)
+    zero_halo_regions!(P.fields, model.grid)
     if length(P.fields) == 1
-        P.profile .= mean(data(P.fields[1]), dims=[1, 2])
+        sum!(P.profile, P.fields[1])
     else
-        P.profile .= mean(data(P.fields[1]) .* data(P.fields[2]), dims=[1, 2])
+        tmp = model.pressures.pNHS.data.parent
+        @. tmp = *(P.fields...)
+        sum!(P.profile, tmp)
     end
-end
 
-@hascuda function run_diagnostic(model::Model, P::HorizontalAverage{<:CuArray})
-    grid = P.fields[1].grid
-    Nx, Ny, Nz = grid.Nx, grid.Ny, grid.Nz
-    sz = 2Nx * sizeof(eltype(P.profile))
-    if length(P.fields) == 1
-        @cuda threads=Nx blocks=(Ny, Nz) shmem=sz gpu_accumulate_xy!(P.profile, model.poisson_solver.storage, P.fields[1].data, nothing, +)
-    else
-        @cuda threads=Nx blocks=(Ny, Nz) shmem=sz gpu_accumulate_xy!(P.profile, model.poisson_solver.storage, P.fields[1].data, P.fields[2].data, +)
-    end
+    Nx, Ny = model.grid.Nx, model.grid.Ny
     P.profile /= (Nx*Ny)  # Normalize to get the mean from the sum.
 end
 
