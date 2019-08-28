fbe6a9a8eef62a58acdafc76e666c410c50c30ee
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-08-28 14:26:56 -0400

    Clean up benchmark scripts

--- benchmark/benchmark_static_ocean.jl
+++ benchmark/benchmark_static_ocean.jl
@@ -1,4 +1,4 @@
-using TimerOutputs, Printf
+using Printf, TimerOutputs
 using Oceananigans
 
 include("benchmark_utils.jl")
@@ -8,30 +8,28 @@ const timer = TimerOutput()
 Ni = 2   # Number of iterations before benchmarking starts.
 Nt = 10  # Number of iterations to use for benchmarking time stepping.
 
-# Model resolutions to benchmarks. Focusing on 3D models for GPU.
-Ns = [(32, 32, 32), (64, 64, 64), (128, 128, 128), (256, 256, 256)]
+# Model resolutions to benchmarks. Focusing on 3D models for GPU benchmarking.
+            Ns = [(32, 32, 32), (64, 64, 64), (128, 128, 128), (256, 256, 256)]
+   float_types = [Float32, Float64]  # Float types to benchmark.
+         archs = [CPU()]             # Architectures to benchmark on.
+@hascuda archs = [CPU(), GPU()]      # Benchmark GPU on systems with CUDA-enabled GPUs.
 
-float_types = [Float32, Float64]  # Float types to benchmark.
-archs = [CPU()]  # Architectures to benchmark on.
-
-# Benchmark GPU on systems with CUDA-enabled GPUs.
-@hascuda archs = [CPU(), GPU()]
-
+# Run benchmarks.
 for arch in archs, float_type in float_types, N in Ns
     Nx, Ny, Nz = N
-    Lx, Ly, Lz = 100, 100, 100
+    Lx, Ly, Lz = 1, 1, 1
 
     model = Model(N=(Nx, Ny, Nz), L=(Lx, Ly, Lz), arch=arch, float_type=float_type)
-    time_step!(model, Ni, 1)  # First 1~2 iterations are usually slower.
+    time_step!(model, Ni, 1)
 
-    bname =  benchmark_name(N, arch, float_type)
+    bname =  benchmark_name(N, "", arch, float_type)
     @printf("Running static ocean benchmark: %s...\n", bname)
     for i in 1:Nt
         @timeit timer bname time_step!(model, 1, 1)
     end
 end
 
-print_timer(timer, title="Oceananigans.jl static ocean benchmarks")
+print_timer(timer, title="Static ocean benchmarks")
 
 println("\n\nCPU Float64 -> Float32 speedup:")
 for N in Ns
