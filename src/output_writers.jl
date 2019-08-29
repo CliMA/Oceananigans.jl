a67d11c3e3c19950caa514f2303d325319c71894
Gregory Wagner <wagner.greg@gmail.com>
2019-08-29 07:29:05 -0400

    bugfix in output_writer for JLD2OutputWriter
    Former-commit-id: 4cabf5096d2b0b0c653dcae4864a9dabc84c0dde

--- src/output_writers.jl
+++ src/output_writers.jl
@@ -27,7 +27,8 @@ saveproperty!(file, location, p::AbstractRange) = file[location] = collect(p)
 saveproperty!(file, location, p::AbstractArray) = file[location] = Array(p)
 saveproperty!(file, location, p::Field)         = file[location] = Array(p.data.parent)
 saveproperty!(file, location, p::Function) = @warn "Cannot save Function property into $location"
-saveproperty!(file, location, p) = [saveproperty!(file, location * "/$subp", getproperty(p, subp)) for subp in propertynames(p)]
+saveproperty!(file, location, p) = [saveproperty!(file, location * "/$subp", getproperty(p, subp)) 
+                                        for subp in propertynames(p)]
 
 # Special saveproperty! so boundary conditions are easily readable outside julia.    
 function saveproperty!(file, location, cbcs::CoordinateBoundaryConditions)
@@ -78,7 +79,7 @@ end
 ####  JLD2 output writer
 ####
 
-mutable struct JLD2OutputWriter{F, I, O, IF, IN} <: OutputWriter
+mutable struct JLD2OutputWriter{F, I, O, IF, IN, KW} <: OutputWriter
         filepath :: String
          outputs :: O
         interval :: I
@@ -91,6 +92,7 @@ mutable struct JLD2OutputWriter{F, I, O, IF, IN} <: OutputWriter
            async :: Bool
            force :: Bool
          verbose :: Bool
+         jld2_kw :: KW
 end
 
 noinit(args...) = nothing
@@ -105,29 +107,31 @@ a single JLD2 file, where `label` is a symbol that labels the output and `func` 
 function of the form `func(model)` that returns the data to be saved.
 
 # Keyword arguments
-- `frequency::Int`:    Save output every `n` model iterations.
-- `interval::Int`:     Save output every `t` units of model clock time.
-- `dir::String`:       Directory to save output to. Default: "." (current working
+- `frequency::Int`   : Save output every `n` model iterations.
+- `interval::Int`    : Save output every `t` units of model clock time.
+- `dir::String`      : Directory to save output to. Default: "." (current working
                        directory).
-- `prefix::String`:    Descriptive filename prefixed to all output files. Default: "".
-- `init::Function`:    A function of the form `init(file, model)` that runs when a JLD2
+- `prefix::String`   : Descriptive filename prefixed to all output files. Default: "".
+- `init::Function`   : A function of the form `init(file, model)` that runs when a JLD2
                        output file is initialized. Default: `noinit(args...) = nothing`.
-- `including::Array`:  List of model properties to save with every file. By default, the
+- `including::Array` : List of model properties to save with every file. By default, the
                        grid, equation of state, planetary constants, and the turbulence
                        closure parameters are saved.
-- `part::Int`:         The starting part number used if `max_filesize` is finite.
+- `part::Int`        : The starting part number used if `max_filesize` is finite.
                        Default: 1.
 - `max_filesize::Int`: The writer will stop writing to the output file once the file size
                        exceeds `max_filesize`, and write to a new one with a consistent
                        naming scheme ending in `part1`, `part2`, etc. Defaults to `Inf`.
-- `force::Bool`:       Remove existing files if their filenames conflict. Default: `false`.
-- `async::Bool`:       Write output asynchronously. Default: `false`.
-- `verbose::Bool`:     Log what the output writer is doing with statistics on compute/write
+- `force::Bool`      : Remove existing files if their filenames conflict. Default: `false`.
+- `async::Bool`      : Write output asynchronously. Default: `false`.
+- `verbose::Bool`    : Log what the output writer is doing with statistics on compute/write
                        times and file sizes. Default: `false`.
+- `jld2_kw::Dict`    : Dict of kwargs to be passed to `jldopen` when data is written.
 """
 function JLD2OutputWriter(model, outputs; interval=nothing, frequency=nothing, dir=".", prefix="",
                           init=noinit, including=[:grid, :eos, :constants, :closure],
-                          part=1, max_filesize=Inf, force=false, async=false, verbose=false)
+                          part=1, max_filesize=Inf, force=false, async=false, verbose=false,
+                          jld2_kw=Dict{Symbol, Any}())
 
     validate_interval(interval, frequency)
 
@@ -135,19 +139,20 @@ function JLD2OutputWriter(model, outputs; interval=nothing, frequency=nothing, d
     filepath = joinpath(dir, prefix * ".jld2")
     force && isfile(filepath) && rm(filepath, force=true)
 
-    jldopen(filepath, "a+") do file
+    jldopen(filepath, "a+"; jld2_kw...) do file
         init(file, model)
         saveproperties!(file, model, including)
     end
 
     return JLD2OutputWriter(filepath, outputs, interval, frequency, init, including,
-                            0.0, part, max_filesize, async, force, verbose)
+                            0.0, part, max_filesize, async, force, verbose, jld2_kw)
 end
 
 function write_output(model, fw::JLD2OutputWriter)
     verbose = fw.verbose
     verbose && @info @sprintf("Calculating JLD2 output %s...", keys(fw.outputs))
-    tc = Base.@elapsed data = Dict((name, f(model)) for (name, f) in fw.outputs)
+    tc = Base.@elapsed data = Dict((name, f(model)) for (name, f) 
+                                    in zip(keys(fw.outputs), values(fw.outputs)))
     verbose && @info "Calculation time: $(prettytime(tc))"
 
     iter = model.clock.iteration
@@ -160,21 +165,44 @@ function write_output(model, fw::JLD2OutputWriter)
     t0, sz = time_ns(), filesize(path)
 
     if fw.async
-        @async remotecall(jld2output!, 2, path, iter, time, data)
+        @async remotecall(jld2output!, 2, path, iter, time, data, fw.jld2_kw)
     else
-        jld2output!(path, iter, time, data)
+        jld2output!(path, iter, time, data, fw.jld2_kw)
     end
 
     t1, newsz = time_ns(), filesize(path)
-    verbose && @info "Writing done: time=$(prettytime((t1-t0)/1e9)), size=$(pretty_filesize(newsz)), Δsize=$(pretty_filesize(newsz-sz))"
+
+    verbose && @info @sprintf("Writing done: time=%s, size=%s, Δsize=%s",
+                              prettytime((t1-t0)/1e9), pretty_filesize(newsz), pretty_filesize(newsz-sz))
 
     return
 end
 
+"""
+    jld2output!(path, iter, time, data, kwargs)
+
+Write the (name, value) pairs in `data`, including the simulation 
+`time`, to the JLD2 file at `path` in the `timeseries` group, 
+stamping them with `iter` and using `kwargs` when opening
+the JLD2 file.
+"""
+function jld2output!(path, iter, time, data, kwargs)
+    jldopen(path, "r+"; kwargs...) do file
+        file["timeseries/t/$iter"] = time
+        for (name, datum) in data
+            file["timeseries/$name/$iter"] = datum
+        end
+    end
+    return
+end
+
+
 function start_next_file(model::Model, fw::JLD2OutputWriter)
     verbose = fw.verbose
     sz = filesize(fw.filepath)
-    verbose && @info "Filesize $(pretty_filesize(sz)) has exceeded maximum file size $(pretty_filesize(fw.max_filesize))."
+    verbose && @info begin
+        "Filesize $(pretty_filesize(sz)) has exceeded maximum file size $(pretty_filesize(fw.max_filesize))."
+    end
 
     if fw.part == 1
         part1_path = replace(fw.filepath, r".jld2$" => "_part1.jld2")
@@ -188,20 +216,10 @@ function start_next_file(model::Model, fw::JLD2OutputWriter)
     fw.force && isfile(fw.filepath) && rm(fw.filepath, force=true)
     verbose && @info "Now writing to: $(fw.filepath)"
 
-    jldopen(fw.filepath, "a+") do file
+    jldopen(fw.filepath, "a+"; fw.jld2_kw...) do file
         fw.init(file, model)
         saveproperties!(file, model, fw.including)
     end
-end
-
-function jld2output!(path, iter, time, data)
-    jldopen(path, "r+") do file
-        file["timeseries/t/$iter"] = time
-        for (name, datum) in data
-            file["timeseries/$name/$iter"] = datum
-        end
-    end
-    return
 end
 
 ####
@@ -382,10 +400,10 @@ mutable struct Checkpointer{I, T, P, A} <: OutputWriter
            verbose :: Bool
 end
 
-function Checkpointer(model; frequency=nothing, interval=nothing, dir=".", prefix="checkpoint", force=false, verbose=false,
-                      properties = [:arch, :boundary_conditions, :grid, :clock, :eos, :constants, :closure,
-                                    :velocities, :tracers, :timestepper])
-
+function Checkpointer(model; frequency=nothing, interval=nothing, dir=".", prefix="checkpoint", force=false, 
+                      verbose=false, properties = [:arch, :boundary_conditions, :grid, :clock, :eos, :constants, 
+                                                   :closure, :velocities, :tracers, :timestepper])
+                      
     validate_interval(frequency, interval)
 
     # Grid needs to be checkpointed for restoring to work.
