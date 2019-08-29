db53b073d5d4aae98bb4b8436710e45e865383cb
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-08-29 17:47:56 -0400

    Add test to make sure forcing functions don't crash on GPU.
    Former-commit-id: eb8c46fa2d532e427b193092b3af7e61a213c216

--- test/test_forcings.jl
+++ test/test_forcings.jl
@@ -1,16 +1,46 @@
+add_one(args...) = 1.0
+
+function test_forcing(fld)
+    kwarg = Dict(Symbol(:F, fld) => add_one)
+    forcing = Forcing(; kwarg...)
+    f = getfield(forcing, fld)
+    f() == 1.0
+end
+
+function time_step_with_forcing_function(arch)
+    @inline Fu(grid, U, Φ, i, j, k) = @inbounds ifelse(k == grid.Nz, -u[i, j, k] / 60, 0)
+    @inline Fv(grid, U, Φ, i, j, k) = @inbounds ifelse(k == grid.Nz, -v[i, j, k] / 60, 0)
+    @inline Fw(grid, U, Φ, i, j, k) = @inbounds ifelse(k == grid.Nz, -w[i, j, k] / 60, 0)
+
+    forcing = Forcing(Fu=Fu, Fv=Fv, Fw=Fw)
+
+    model = Model(N=(8, 8, 8), L=(1, 1, 1), arch=arch, forcing=forcing)
+    time_step!(model, 1, 1)
+    return true
+end
+
+function time_step_with_forcing_function_const(arch)
+    const τ = 60
+    @inline Fu(grid, U, Φ, i, j, k) = @inbounds ifelse(k == grid.Nz, -u[i, j, k] / τ, 0)
+    @inline Fv(grid, U, Φ, i, j, k) = @inbounds ifelse(k == grid.Nz, -v[i, j, k] / τ, 0)
+    @inline Fw(grid, U, Φ, i, j, k) = @inbounds ifelse(k == grid.Nz, -w[i, j, k] / τ, 0)
+
+    forcing = Forcing(Fu=Fu, Fv=Fv, Fw=Fw)
+
+    model = Model(N=(8, 8, 8), L=(1, 1, 1), arch=arch, forcing=forcing)
+    time_step!(model, 1, 1)
+    return true
+end
+
 @testset "Forcings" begin
     println("Testing forcings...")
 
-    add_one(args...) = 1.0
-
-    function test_forcing(fld)
-        kwarg = Dict(Symbol(:F, fld)=>add_one)
-        forcing = Forcing(; kwarg...)
-        f = getfield(forcing, fld)
-        f() == 1.0
-    end
-
     for fld in (:u, :v, :w, :T, :S)
         @test test_forcing(fld)
+    end
+
+    for arch in archs
+        @test time_step_with_forcing_function(arch)
+        @test time_step_with_forcing_function_const(arch)
     end
 end
