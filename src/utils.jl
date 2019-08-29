3ebf773ee372aae63ab9ccd539f8c0ad684e949f
Gregory Wagner <wagner.greg@gmail.com>
2019-08-29 15:19:54 -0400

    move tupling utilities to utils.jl
    Former-commit-id: 956fdba5bdb4856db584304320a701d3b6327a3c

--- src/utils.jl
+++ src/utils.jl
@@ -119,6 +119,16 @@ function update_Δt!(wizard, model)
     return nothing
 end
 
+#####
+##### Some utilities for tupling
+#####
+
+tupleit(t::Tuple) = t
+tupleit(a::AbstractArray) = Tuple(a)
+tupleit(nt) = tuple(nt)
+
+parenttuple(obj) = Tuple(f.data.parent for f in obj)
+
 @inline datatuple(obj::Nothing) = nothing
 @inline datatuple(obj::AbstractArray) = obj
 @inline datatuple(obj::Field) = obj.data
