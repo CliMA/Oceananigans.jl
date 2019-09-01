5a7b8c92505c04261c5b1811e3e4a98a6df8e536
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-09-01 10:08:43 -0400

    Add forcing function test with sin and exp.
    Former-commit-id: e113346312915daf8722c1c0a4ae10170f119ede

--- test/test_forcings.jl
+++ test/test_forcings.jl
@@ -7,7 +7,7 @@ function test_forcing(fld)
     f() == 1.0
 end
 
-function time_step_with_forcing_function(arch)
+function time_step_with_forcing_functions(arch)
     @inline Fu(grid, U, Φ, i, j, k) = @inbounds ifelse(k == grid.Nz, -U.u[i, j, k] / 60, 0)
     @inline Fv(grid, U, Φ, i, j, k) = @inbounds ifelse(k == grid.Nz, -U.v[i, j, k] / 60, 0)
     @inline Fw(grid, U, Φ, i, j, k) = @inbounds ifelse(k == grid.Nz, -U.w[i, j, k] / 60, 0)
@@ -20,7 +20,7 @@ function time_step_with_forcing_function(arch)
 end
 
 const τ = 60
-function time_step_with_forcing_function_const(arch)
+function time_step_with_forcing_functions_const(arch)
     @inline Fu(grid, U, Φ, i, j, k) = @inbounds ifelse(k == grid.Nz, -U.u[i, j, k] / τ, 0)
     @inline Fv(grid, U, Φ, i, j, k) = @inbounds ifelse(k == grid.Nz, -U.v[i, j, k] / τ, 0)
     @inline Fw(grid, U, Φ, i, j, k) = @inbounds ifelse(k == grid.Nz, -U.w[i, j, k] / τ, 0)
@@ -32,15 +32,36 @@ function time_step_with_forcing_function_const(arch)
     return true
 end
 
+
+const α = 1
+const β = 2
+function time_step_with_forcing_functions_sin_exp(arch)
+    @inline Fu(grid, U, Φ, i, j, k) = @inbounds sin(α * grid.xC[i])
+    @inline FT(grid, U, Φ, i, j, k) = @inbounds exp(-β * Φ.T[i, j, k])
+
+    forcing = Forcing(Fu=Fu, FT=FT)
+
+    model = Model(N=(8, 8, 8), L=(1, 1, 1), arch=arch, forcing=forcing)
+    time_step!(model, 1, 1)
+    return true
+end
+
 @testset "Forcings" begin
     println("Testing forcings...")
 
-    for fld in (:u, :v, :w, :T, :S)
-        @test test_forcing(fld)
+    @testset "Forcing function initialization" begin
+        println("Testing forcing function initialization...")
+        for fld in (:u, :v, :w, :T, :S)
+            @test test_forcing(fld)
+        end
     end
 
     for arch in archs
-        @test time_step_with_forcing_function(arch)
-        @test time_step_with_forcing_function_const(arch)
+        @testset "Forcing function time stepping [$(typeof(arch))]" begin
+            println("Testing forcing function time stepping [$(typeof(arch))]...")
+            @test time_step_with_forcing_functions(arch)
+            @test time_step_with_forcing_functions_const(arch)
+            @test time_step_with_forcing_functions_sin_exp(arch)
+        end
     end
 end
