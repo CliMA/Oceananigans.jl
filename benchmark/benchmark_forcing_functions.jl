ed11a3d5443e4a27b1b975cf02081fb6002fa2cf
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-09-11 08:41:23 -0400

    Benchmark consts vs. parameters forcing functions
    Former-commit-id: bd0ce5143ad4e954226dd39397009e5710f691ea

--- benchmark/benchmark_forcing_functions.jl
+++ benchmark/benchmark_forcing_functions.jl
@@ -1,8 +1,13 @@
 using TimerOutputs, Printf
-
 using Oceananigans
 
+include("benchmark_utils.jl")
+
 const timer = TimerOutput()
+
+####
+#### Benchmark parameters
+####
 
 Ni = 2   # Number of iterations before benchmarking starts.
 Nt = 10  # Number of iterations to use for benchmarking time stepping.
@@ -13,26 +18,11 @@ Nt = 10  # Number of iterations to use for benchmarking time stepping.
          archs = [CPU()]    # Architectures to benchmark on.
 @hascuda archs = [GPU()]    # Benchmark GPU on systems with CUDA-enabled GPUs.
 
-arch_name(::String) = ""
-arch_name(::CPU) = "CPU"
-arch_name(::GPU) = "GPU"
+####
+#### Forcing function definitions
+####
 
-function benchmark_name(N, id, arch, FT; npad=2)
-    Nx, Ny, Nz = N
-
-    bn = ""
-    bn *= lpad(Nx, npad, " ") * "×" * lpad(Ny, npad, " ") * "×" * lpad(Nz, npad, " ")
-    bn *= " $id"
-
-    arch = arch_name(arch)
-    bn *= " ($arch, $FT)"
-
-    return bn
-end
-
-benchmark_name(N, id) = benchmark_name(N, id, "", "", "")
-
-@inline function Fu(grid, U, Φ, i, j, k, params)
+@inline function Fu_params(i, j, k, grid, time, U, C, params)
     if k == 1
         return @inbounds -2*params.K/grid.Δz^2 * (U.u[i, j, 1] - 0)
     elseif k == grid.Nz
@@ -42,20 +32,48 @@ benchmark_name(N, id) = benchmark_name(N, id, "", "", "")
     end
 end
 
-@inline FT(grid, U, Φ, i, j, k, params) = @inbounds ifelse(k == 1, -params.λ * (Φ.T[i, j, 1] - 0), 0)
-forcing = Forcing(Fu=Fu, FT=FT)
+const K = 0.1
+@inline function Fu_consts(i, j, k, grid, time, U, C, params)
+    if k == 1
+        return @inbounds -2*K/grid.Δz^2 * (U.u[i, j, 1] - 0)
+    elseif k == grid.Nz
+        return @inbounds -2*K/grid.Δz^2 * (U.u[i, j, grid.Nz] - 0)
+    else
+        return 0
+    end
+end
+
+@inline FT_params(i, j, k, grid, time, U, C, params) = @inbounds ifelse(k == 1, -params.λ * (C.T[i, j, 1] - 0), 0)
+
+const λ = 1e-4
+@inline FT_consts(i, j, k, grid, time, U, C, params) = @inbounds ifelse(k == 1, -λ * (C.T[i, j, 1] - 0), 0)
+
+####
+#### Run benchmarks
+####
 
 for arch in archs, float_type in float_types, N in Ns
     Nx, Ny, Nz = N
     Lx, Ly, Lz = 1, 1, 1
     
-    forced_model = Model(N=(Nx, Ny, Nz), L=(Lx, Ly, Lz), arch=arch, float_type=float_type, forcing=forcing, parameters=(K=0.1, λ=1e-4))
-    time_step!(forced_model, Ni, 1)  # First 1-2 iterations usually slower.
+    forced_model_params = Model(N=(Nx, Ny, Nz), L=(Lx, Ly, Lz), arch=arch, float_type=float_type,
+                                forcing=Forcing(Fu=Fu_params, FT=FT_params), parameters=(K=0.1, λ=1e-4))
+    time_step!(forced_model_params, Ni, 1)  # First 1-2 iterations usually slower.
 
-    bn =  benchmark_name(N, "with forcing", arch, float_type)
+    bn =  benchmark_name(N, "with forcing (params)", arch, float_type)
     @printf("Running benchmark: %s...\n", bn)
     for i in 1:Nt
-        @timeit timer bn time_step!(forced_model, 1, 1)
+        @timeit timer bn time_step!(forced_model_params, 1, 1)
+    end
+    
+    forced_model_consts = Model(N=(Nx, Ny, Nz), L=(Lx, Ly, Lz), arch=arch, float_type=float_type,
+                                forcing=Forcing(Fu=Fu_consts, FT=FT_consts))
+    time_step!(forced_model_consts, Ni, 1)  # First 1-2 iterations usually slower.
+
+    bn =  benchmark_name(N, "with forcing (consts)", arch, float_type)
+    @printf("Running benchmark: %s...\n", bn)
+    for i in 1:Nt
+        @timeit timer bn time_step!(forced_model_consts, 1, 1)
     end
 
     unforced_model = Model(N=(Nx, Ny, Nz), L=(Lx, Ly, Lz), arch=arch, float_type=float_type)
