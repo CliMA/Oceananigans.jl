f06cdc4b0b4bb09cc680f7bc20369e235007361d
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-09-12 15:22:28 +0000

    Need N=128^3 to match published results

--- test/verification/stratified_couette_flow/stratified_couette_flow.jl
+++ test/verification/stratified_couette_flow/stratified_couette_flow.jl
@@ -270,7 +270,7 @@ function simulate_stratified_couette_flow(; Nxy, Nz, h=1, Uw=1, Re=4250, Pr=0.7,
     end
 end
 
-simulate_stratified_couette_flow(Nxy=128, Nz=64, Ri=0)
-simulate_stratified_couette_flow(Nxy=128, Nz=64, Ri=0.01)
-simulate_stratified_couette_flow(Nxy=128, Nz=64, Ri=0.04)
+simulate_stratified_couette_flow(Nxy=128, Nz=128, Ri=0)
+simulate_stratified_couette_flow(Nxy=128, Nz=128, Ri=0.01)
+simulate_stratified_couette_flow(Nxy=128, Nz=128, Ri=0.04)
 
