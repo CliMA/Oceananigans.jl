6451d5355c9031bc902b89325c847a3a9afcf943
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2019-09-14 14:25:32 -0400

    Minor cosmetic change to runtests.jl
    Former-commit-id: df354d8be28589091b6755482b1cbb153db40de1

--- src/utils.jl
+++ src/utils.jl
@@ -108,38 +108,6 @@ cell_advection_timescale(model) =
                              model.velocities.w.data.parent,
                              model.grid)
 
-# Timescale for diffusion across one cell
-min_Δxyz(grid) = min(grid.Δx, grid.Δy, grid.Δz)
-min_Δxy(grid) = min(grid.Δx, grid.Δy)
-min_Δz(grid) = grid.Δz
-
-"Returns the time-scale for diffusion on a regular grid across a single grid cell."
-function cell_diffusion_timescale(model::Model{TS, <:ConstantIsotropicDiffusivity}) where TS
-    Δ = min_Δxyz(model.grid)
-    return min(Δ^2 / model.closure.ν, Δ^2 / model.closure.κ)
-end
-
-function cell_diffusion_timescale(model::Model{TS, <:ConstantAnisotropicDiffusivity}) where TS
-    Δh = min_Δxy(model.grid)
-    Δz = min_Δz(model.grid)
-    return min(Δz^2 / model.closure.νv, Δh^2 / model.closure.νh,
-               Δz^2 / model.closure.κv, Δh^2 / model.closure.κh)
-end
-
-function cell_diffusion_timescale(model::Model{TS, <:AbstractSmagorinsky}) where TS
-    Δ = min_Δxyz(model.grid)
-    max_νκ = maximum(model.diffusivities.νₑ.data.parent) * max(1, 1/model.closure.Pr)
-    return min(Δ^2 / max_νκ, Δ^2 / model.closure.κ)
-end
-
-function cell_diffusion_timescale(model::Model{TS, <:AnisotropicMinimumDissipation}) where TS
-    Δ = min_Δxyz(model.grid)
-    max_ν = maximum(model.diffusivities.νₑ.data.parent)
-    max_κ = max(Tuple(maximum(κₑ.data.parent) for κₑ in model.diffusivities.κₑ)...)
-    return min(Δ^2 / max_ν, Δ^2 / max_κ)
-end
-
-
 ####
 #### Adaptive time stepping
 ####
