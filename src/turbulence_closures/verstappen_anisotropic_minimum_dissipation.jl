1b3e3591c60d2f55ecdb351818493b50e8580871
Gregory Wagner <wagner.greg@gmail.com>
2019-09-19 03:24:21 -0400

    Complete transition to using buoyancy function versus buoyancy_perturbation
    Former-commit-id: 4a68b3248248dce0927dca3b353b16e24b485390

--- src/turbulence_closures/verstappen_anisotropic_minimum_dissipation.jl
+++ src/turbulence_closures/verstappen_anisotropic_minimum_dissipation.jl
@@ -54,7 +54,7 @@ function TurbulentDiffusivities(arch::AbstractArchitecture, grid::AbstractGrid, 
     return (νₑ=νₑ, κₑ=(T=κTₑ, S=κSₑ))
 end
 
-@inline function ν_ccc(i, j, k, grid::AbstractGrid{FT}, closure::VAMD, c, buoyancy, U, Φ) where FT
+@inline function ν_ccc(i, j, k, grid::AbstractGrid{FT}, closure::VAMD, c, buoyancy_params, U, Φ) where FT
     ijk = (i, j, k, grid)
     q = norm_tr_∇u_ccc(ijk..., U.u, U.v, U.w)
 
@@ -62,7 +62,7 @@ end
         νˢᵍˢ = zero(FT)
     else
         r = norm_uᵢₐ_uⱼₐ_Σᵢⱼ_ccc(ijk..., closure, U.u, U.v, U.w)
-        ζ = norm_wᵢ_bᵢ_ccc(ijk..., closure, buoyancy, U.w, Φ) / Δᶠz_ccc(ijk...)
+        ζ = norm_wᵢ_bᵢ_ccc(ijk..., closure, buoyancy_params, U.w, Φ) / Δᶠz_ccc(ijk...)
         δ² = 3 / (1 / Δᶠx_ccc(ijk...)^2 + 1 / Δᶠy_ccc(ijk...)^2 + 1 / Δᶠz_ccc(ijk...)^2)
         νˢᵍˢ = - closure.C * δ² * (r - closure.Cb * ζ) / q
     end
@@ -70,7 +70,7 @@ end
     return max(zero(FT), νˢᵍˢ) + closure.ν
 end
 
-@inline function κ_ccc(i, j, k, grid::AbstractGrid{FT}, closure::VAMD, c, buoyancy, U, Φ) where FT
+@inline function κ_ccc(i, j, k, grid::AbstractGrid{FT}, closure::VAMD, c, buoyancy_params, U, Φ) where FT
     ijk = (i, j, k, grid)
     σ =  norm_θᵢ²_ccc(i, j, k, grid, c) # Tracer variance
 
@@ -157,17 +157,17 @@ end
     )
 end
 
-@inline function norm_wᵢ_bᵢ_ccc(i, j, k, grid, closure, buoyancy, w, C)
+@inline function norm_wᵢ_bᵢ_ccc(i, j, k, grid, closure, buoyancy_params, w, C)
     ijk = (i, j, k, grid)
 
     wx_bx = (▶xz_cac(ijk..., norm_∂x_w, w)
-             * Δᶠx_ccc(ijk...) * ▶x_caa(ijk..., ∂x_faa, buoyancy_perturbation, buoyancy, C))
+             * Δᶠx_ccc(ijk...) * ▶x_caa(ijk..., ∂x_faa, buoyancy, buoyancy_params, C))
 
     wy_by = (▶yz_acc(ijk..., norm_∂y_w, w)
-             * Δᶠy_ccc(ijk...) * ▶y_aca(ijk..., ∂y_afa, buoyancy_perturbation, buoyancy, C))
+             * Δᶠy_ccc(ijk...) * ▶y_aca(ijk..., ∂y_afa, buoyancy, buoyancy_params, C))
 
     wz_bz = (norm_∂z_w(ijk..., w)
-             * Δᶠz_ccc(ijk...) * ▶z_aac(ijk..., ∂z_aaf, buoyancy_perturbation, buoyancy, C))
+             * Δᶠz_ccc(ijk...) * ▶z_aac(ijk..., ∂z_aaf, buoyancy, buoyancy_params, C))
 
     return wx_bx + wy_by + wz_bz
 end
@@ -226,13 +226,13 @@ Return the diffusive flux divergence `∇ ⋅ (κ ∇ S)` for the turbulence
     + ∂z_aac(i, j, k, grid, κ_∂z_c, S, diffusivities.κₑ.S, closure)
 )
 
-function calc_diffusivities!(K, grid, closure::VAMD, buoyancy, U, Φ)
+function calc_diffusivities!(K, grid, closure::VAMD, buoyancy_params, U, Φ)
     @loop for k in (1:grid.Nz; (blockIdx().z - 1) * blockDim().z + threadIdx().z)
         @loop for j in (1:grid.Ny; (blockIdx().y - 1) * blockDim().y + threadIdx().y)
             @loop for i in (1:grid.Nx; (blockIdx().x - 1) * blockDim().x + threadIdx().x)
-                @inbounds K.νₑ[i, j, k]   = ν_ccc(i, j, k, grid, closure, nothing, buoyancy, U, Φ)
-                @inbounds K.κₑ.T[i, j, k] = κ_ccc(i, j, k, grid, closure, Φ.T,     buoyancy, U, Φ)
-                @inbounds K.κₑ.S[i, j, k] = κ_ccc(i, j, k, grid, closure, Φ.S,     buoyancy, U, Φ)
+                @inbounds K.νₑ[i, j, k]   = ν_ccc(i, j, k, grid, closure, nothing, buoyancy_params, U, Φ)
+                @inbounds K.κₑ.T[i, j, k] = κ_ccc(i, j, k, grid, closure, Φ.T,     buoyancy_params, U, Φ)
+                @inbounds K.κₑ.S[i, j, k] = κ_ccc(i, j, k, grid, closure, Φ.S,     buoyancy_params, U, Φ)
             end
         end
     end
