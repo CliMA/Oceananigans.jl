44979c2bc9df8f01ff75ea618129d11ab1f6cbb3
Gregory Wagner <wagner.greg@gmail.com>
2019-09-19 14:27:34 +0100

    Nukes VerticalRotationAxis and replaces with FPlane
    Former-commit-id: 4b222a91072d00c5f8b73624ca9c2cfbcb0bb841

--- src/coriolis.jl
+++ src/coriolis.jl
@@ -7,16 +7,16 @@
 @inline z_f_cross_U(i, j, k, grid::AbstractGrid{T}, ::Nothing, U) where T = zero(T)
 
 """
-    VerticalRotationAxis{T} <: AbstractRotation
+    FPlane{T} <: AbstractRotation
 
 A parameter object for constant rotation around a vertical axis.
 """
-struct VerticalRotationAxis{T} <: AbstractRotation
+struct FPlane{T} <: AbstractRotation
     f :: T
 end
 
 """
-    VerticalRotationAxis([T=Float64;] f)
+    FPlane([T=Float64;] f)
 
 Returns a parameter object for constant rotation at the angular frequency
 `2f`, and therefore with background vorticity `f`, around a vertical axis.
@@ -24,11 +24,9 @@ Returns a parameter object for constant rotation at the angular frequency
 Also called `FPlane`, after the "f-plane" approximation for the local effect of 
 Earth's rotation in a planar coordinate system tangent to the Earth's surface.
 """
-function VerticalRotationAxis(T::DataType=Float64; f) 
-    return VerticalRotationAxis{T}(f)
+function FPlane(T::DataType=Float64; f) 
+    return FPlane{T}(f)
 end
-
-const FPlane = VerticalRotationAxis
 
 @inline fv(i, j, k, grid::RegularCartesianGrid{T}, f, v) where T = 
     T(0.5) * f * (avgy_f2c(grid, v, i-1,  j, k) + avgy_f2c(grid, v, i, j, k))
@@ -36,7 +34,7 @@ const FPlane = VerticalRotationAxis
 @inline fu(i, j, k, grid::RegularCartesianGrid{T}, f, u) where T = 
     T(0.5) * f * (avgx_f2c(grid, u, i,  j-1, k) + avgx_f2c(grid, u, i, j, k))
 
-@inline x_f_cross_U(i, j, k, grid, rotation::VerticalRotationAxis, U) = -fv(i, j, k, grid, rotation.f, U.v)
-@inline y_f_cross_U(i, j, k, grid, rotation::VerticalRotationAxis, U) =  fu(i, j, k, grid, rotation.f, U.u)
-@inline z_f_cross_U(i, j, k, grid::AbstractGrid{T}, ::VerticalRotationAxis, U) where T = zero(T)
+@inline x_f_cross_U(i, j, k, grid, rotation::FPlane, U) = -fv(i, j, k, grid, rotation.f, U.v)
+@inline y_f_cross_U(i, j, k, grid, rotation::FPlane, U) =  fu(i, j, k, grid, rotation.f, U.u)
+@inline z_f_cross_U(i, j, k, grid::AbstractGrid{T}, ::FPlane, U) where T = zero(T)
 
