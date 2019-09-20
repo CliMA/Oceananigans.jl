0cbe4b1adbefc55b66aaf61d1ad356911ca0a054
Gregory Wagner <wagner.greg@gmail.com>
2019-09-20 08:10:19 +0100

    Nukes grav function, updates Roquet docstring, and uses polynomial_coeffs instead of just coeffs
    Former-commit-id: 7c50eb438e886b3acef783f02e4128c11caba63b

--- src/buoyancy.jl
+++ src/buoyancy.jl
@@ -55,9 +55,6 @@ function SeawaterBuoyancy(T=Float64;
     return SeawaterBuoyancy{T, typeof(equation_of_state)}(gravitational_acceleration, equation_of_state)
 end
 
-""" Convenience function for extracting the gravitational acceleration from a buoyancy struct. """
-@inline grav(b::SeawaterBuoyancy) = b.gravitational_acceleration
-
 """ 
     buoyancy_frequency_squared(i, j, k, grid, b::SeawaterBuoyancy, C)
 
@@ -73,8 +70,9 @@ temperature or conservative temperature, where applicable, and ``\$ S \$`` is th
 salinity or absolute salinity, where applicable.
 """
 @inline buoyancy_frequency_squared(i, j, k, grid, b::SeawaterBuoyancy, C) = 
-    grav(b) * (    thermal_expansion(i, j, k, grid, b.equation_of_state, C) * ∂z_aaf(i, j, k, grid, C.T)
-                - haline_contraction(i, j, k, grid, b.equation_of_state, C) * ∂z_aaf(i, j, k, grid, C.S))
+    b.gravitational_acceleration * (    
+           thermal_expansion(i, j, k, grid, b.equation_of_state, C) * ∂z_aaf(i, j, k, grid, C.T)
+        - haline_contraction(i, j, k, grid, b.equation_of_state, C) * ∂z_aaf(i, j, k, grid, C.S) )
 
 #####
 ##### Linear equation of state
@@ -109,8 +107,8 @@ LinearEquationOfState(T=Float64; α=1.67e-4, β=7.80e-4) =
 const LinearSeawaterBuoyancy = SeawaterBuoyancy{FT, <:LinearEquationOfState} where FT
 
 @inline buoyancy_perturbation(i, j, k, grid, b::LinearSeawaterBuoyancy, C) = 
-    return @inbounds grav(b) * (   b.equation_of_state.α * C.T[i, j, k]
-                                 - b.equation_of_state.β * C.S[i, j, k] )
+    return @inbounds b.gravitational_acceleration * (   b.equation_of_state.α * C.T[i, j, k]
+                                                      - b.equation_of_state.β * C.S[i, j, k] )
 
 @inline  thermal_expansion(i, j, k, grid, eos::LinearEquationOfState, C) = eos.α
 @inline haline_contraction(i, j, k, grid, eos::LinearEquationOfState, C) = eos.β
@@ -120,14 +118,14 @@ const LinearSeawaterBuoyancy = SeawaterBuoyancy{FT, <:LinearEquationOfState} whe
 #####
 
 @inline buoyancy_perturbation(i, j, k, grid, b::AbstractBuoyancy{<:AbstractNonlinearEquationOfState}, C) = 
-    - grav(b) * ρ′(i, j, k, grid, b.equation_of_state, C) / b.ρ₀
+    - b.gravitational_acceleration * ρ′(i, j, k, grid, b.equation_of_state, C) / b.ρ₀
 
 #####
 ##### Roquet et al 2015 idealized nonlinear equations of state
 #####
 
 # Reference: Table 3 in Roquet et el, "Defining a Simplified yet 'Realistic' Equation of State for Seawater", (JPO, 2015)
-roquet_coeffs = Dict(
+optimized_roquet_coeffs = Dict(
                   :linear => (R₀₁₀ = -1.775e-1, R₁₀₀ = 7.718e-1, R₀₂₀ = 0,         R₀₁₁ = 0,          R₂₀₀ = 0,         R₁₀₁ = 0,         R₁₁₀ = 0),
                :cabbeling => (R₀₁₀ = -0.844e-1, R₁₀₀ = 7.718e-1, R₀₂₀ = -4.561e-3, R₀₁₁ = 0,          R₂₀₀ = 0,         R₁₀₁ = 0,         R₁₁₀ = 0),
 :cabbeling_thermobaricity => (R₀₁₀ = -0.651e-1, R₁₀₀ = 7.718e-1, R₀₂₀ = -5.027e-3, R₀₁₁ = -2.5681e-5, R₂₀₀ = 0,         R₁₀₁ = 0,         R₁₁₀ = 0),
@@ -143,20 +141,32 @@ Roquet et al., "Defining a Simplified yet 'Realistic' Equation of State for Seaw
 Journal of Physical Oceanography (2015).
 """
 struct RoquetIdealizedNonlinearEquationOfState{F, C, T} <: AbstractNonlinearEquationOfState
-        ρ₀ :: T
-    coeffs :: C
+                   ρ₀ :: T
+    polynomial_coeffs :: C
 end
 
 type_convert_roquet_coeffs(T, coeffs) = NamedTuple{propertynames(coeffs)}(Tuple(T(R) for R in coeffs))
 
 """
-    RoquetIdealizedNonlinearEquationOfState([T=Float64,] flavor, ρ₀=1025, coeffs=roquet_coeffs[flavor])
+    RoquetIdealizedNonlinearEquationOfState([T=Float64,] flavor, ρ₀=1024.6, 
+                                            polynomial_coeffs=optimized_roquet_coeffs[flavor])
 
-Returns parameters for an idealized nonlinear equation of state with reference density 
-`ρ₀`. The `flavor` of the nonlinear equation of state is a symbol corresponding to one of 
-the 5 equations of state in equations (12)--(15) of Roquet et al., "Defining a Simplified 
-yet 'Realistic' Equation of State for Seawater", Journal of Physical Oceanography (2015). 
-The equations of state define the density anomaly `ρ′`, and have the polynomial form
+Returns parameters for the idealized polynomial nonlinear equation of state with 
+reference density `ρ₀` and `polynomial_coeffs` proposed by Roquet et al., "Defining a 
+Simplified yet 'Realistic' Equation of State for Seawater", Journal of Physical 
+Oceanography (2015). The default reference density is `ρ₀ = 1024.6 kg m⁻³`, the average 
+surface density of seawater in the world ocean.
+
+The `flavor` of the nonlinear equation of state is a symbol that selects a set of optimized 
+polynomial coefficients defined in Table 2 of Roquet et al., "Defining a Simplified yet 
+'Realistic' Equation of State for Seawater", Journal of Physical Oceanography (2015), and 
+further discussed in the text surrounding equations (12)--(15). The optimization minimizes 
+errors in estimated horizontal density gradient estiamted from climatological temperature 
+and salinity distributions between the 5 simplified forms chosen by Roquet et. al
+and the full-fledged [TEOS-10](http://www.teos-10.org) equation of state.
+
+The equations of state define the density anomaly `ρ′`, and have 
+the polynomial form
 
     `ρ′(T, S, D) = Σᵢⱼₐ Rᵢⱼₐ Tⁱ Sʲ Dᵃ`,
 
@@ -180,10 +190,28 @@ Flavors of idealized nonlinear equations of state
     - `:second_order`: includes quadratic salinity, halibaricity, and thermohaline term,
                        `ρ′ = R₁₀₀ * T + R₀₁₀ * S + R₀₂₀ * T^2 + R₀₁₁ * T * D`
                              + R₂₀₀ * S^2 + R₁₀₁ * S * D + R₁₁₀ * S * T`
+
+Example
+=======
+
+julia> using Oceananigans
+
+julia> eos = Oceananigans.RoquetIdealizedNonlinearEquationOfState(:cabbeling);
+
+julia> eos.polynomial_coeffs
+(R₀₁₀ = -0.0844, R₁₀₀ = 0.7718, R₀₂₀ = -0.004561, R₀₁₁ = 0.0, R₂₀₀ = 0.0, R₁₀₁ = 0.0, R₁₁₀ = 0.0)
+
+References
+==========
+
+    - Roquet et al., "Defining a Simplified yet 'Realistic' Equation of State for  
+      Seawater", Journal of Physical Oceanography (2015). 
+
+    - "Thermodynamic Equation of State for Seawater" (TEOS-10), http://www.teos-10.org
 """
 function RoquetIdealizedNonlinearEquationOfState(T, flavor=:cabbeling_thermobaricity; 
-                                                 coeffs=roquet_coeffs[flavor], ρ₀=1025)
-    typed_coeffs = type_convert_roquet_coeffs(T, coeffs)
+                                                 polynomial_coeffs=optimized_roquet_coeffs[flavor], ρ₀=1024.6)
+    typed_coeffs = type_convert_roquet_coeffs(T, polynomial_coeffs)
     return RoquetIdealizedNonlinearEquationOfState{flavor, typeof(typed_coeffs), T}(ρ₀, typed_coeffs)
 end
 
@@ -198,22 +226,22 @@ const D = D_aac
 @inline D_aaf(i, j, k, grid) = @inbounds -grid.zF[k]
 
 @inline ρ′(i, j, k, grid, eos::RoquetIdealizedNonlinearEquationOfState, C) = 
-    @inbounds (   eos.coeffs.R₁₀₀ * C.S[i, j, k]
-                + eos.coeffs.R₀₁₀ * C.T[i, j, k]
-                + eos.coeffs.R₀₂₀ * C.T[i, j, k]^2
-                + eos.coeffs.R₀₁₁ * C.T[i, j, k] * D(i, j, k, grid)
-                + eos.coeffs.R₂₀₀ * C.S[i, j, k]^2
-                + eos.coeffs.R₁₀₁ * C.S[i, j, k] * D(i, j, k, grid)
-                + eos.coeffs.R₁₁₀ * C.S[i, j, k] * C.T[i, j, k] )
+    @inbounds (   eos.polynomial_coeffs.R₁₀₀ * C.S[i, j, k]
+                + eos.polynomial_coeffs.R₀₁₀ * C.T[i, j, k]
+                + eos.polynomial_coeffs.R₀₂₀ * C.T[i, j, k]^2
+                + eos.polynomial_coeffs.R₀₁₁ * C.T[i, j, k] * D(i, j, k, grid)
+                + eos.polynomial_coeffs.R₂₀₀ * C.S[i, j, k]^2
+                + eos.polynomial_coeffs.R₁₀₁ * C.S[i, j, k] * D(i, j, k, grid)
+                + eos.polynomial_coeffs.R₁₁₀ * C.S[i, j, k] * C.T[i, j, k] )
 
 @inline thermal_expansion(i, j, k, grid, eos::RoquetIdealizedNonlinearEquationOfState, C) = 
-    @inbounds (   eos.coeffs.R₀₁₀
-                + 2 * eos.coeffs.R₀₂₀ * ▶z_aaf(i, j, k, grid, C.T)
-                + eos.coeffs.R₀₁₁ * D_aaf(i, j, k, grid)
-                + eos.coeffs.R₁₁₀ * ▶z_aaf(i, j, k, grid, C.T) )
+    @inbounds (   eos.polynomial_coeffs.R₀₁₀
+                + 2 * eos.polynomial_coeffs.R₀₂₀ * ▶z_aaf(i, j, k, grid, C.T)
+                + eos.polynomial_coeffs.R₀₁₁ * D_aaf(i, j, k, grid)
+                + eos.polynomial_coeffs.R₁₁₀ * ▶z_aaf(i, j, k, grid, C.T) )
 
 @inline haline_contraction(i, j, k, grid, eos::RoquetIdealizedNonlinearEquationOfState, C) = 
-    @inbounds (   eos.coeffs.R₁₀₀
-                + 2 * eos.coeffs.R₂₀₀ * ▶z_aaf(i, j, k, grid, C.S)
-                + eos.coeffs.R₁₀₁ * D_aaf(i, j, k, grid)
-                + eos.coeffs.R₁₁₀ * ▶z_aaf(i, j, k, grid, C.S) )
+    @inbounds (   eos.polynomial_coeffs.R₁₀₀
+                + 2 * eos.polynomial_coeffs.R₂₀₀ * ▶z_aaf(i, j, k, grid, C.S)
+                + eos.polynomial_coeffs.R₁₀₁ * D_aaf(i, j, k, grid)
+                + eos.polynomial_coeffs.R₁₁₀ * ▶z_aaf(i, j, k, grid, C.S) )
