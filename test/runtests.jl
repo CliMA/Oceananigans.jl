52714559a714bf2386c8d37304f4ad609d033558
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-09-24 07:34:32 -0400

    Split test into separate file

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,5 @@ using Test
 using JULES
 
 @testset "JULES" begin
-    @testset "Time steppers" begin
-        ϕ = 2
-        Δt = 1/5
-        @test RK3(x -> x^2, ϕ, Δt) ≈ 20653834//6328125
-    end
+    include("test_time_steppers.jl")
 end
