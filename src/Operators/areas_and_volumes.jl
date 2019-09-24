3d64a3ae6538e440d9178e7fe87d85ba13afcada
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-09-24 10:22:30 -0400

    Improve comments in areas_and_volumes.jl

--- src/Operators/areas_and_volumes.jl
+++ src/Operators/areas_and_volumes.jl
@@ -1,34 +1,33 @@
-# Grid spacing between cells and faces in the x-direction.
+# Width of the control volumes in the x-direction.
 @inline Δx(i, j, k, grid::AbstractGrid) = grid.Δx
 
-# Grid spacing between cells and faces in the y-direction.
+# Width of the control volumes in the y-direction.
 @inline Δy(i, j, k, grid::AbstractGrid) = grid.Δy
 
-# Grid spacing between cell centers in the z-direction.
+# Height of the control volumes containing the cell centers in the z-direction.
 @inline ΔzC(i, j, k, grid::RegularCartesianGrid) = grid.Δz
-@inline ΔzC(i, j, k, grid::VerticallyStretchedCartesianGrid) = @inbounds grid.ΔzC[k]
+@inline ΔzC(i, j, k, grid::VerticallyStretchedCartesianGrid) = @inbounds grid.ΔzC[i]
 
-# Grid spacing between cell faces in the z-direction.
+# Height of the control volumes containing the cell w-faces in the z-direction.
 @inline ΔzF(i, j, k, grid::RegularCartesianGrid) = grid.Δz
 @inline ΔzF(i, j, k, grid::VerticallyStretchedCartesianGrid) = @inbounds grid.ΔzF[k]
 
-# Area of cell faces surrounding cell center and cell face (i, j, k) in the x-direction.
+# Area of control volume faces surrounding cell center and cell face (i, j, k) in the x-direction
 @inline AxC(i, j, k, grid) = Δy(i, j, k, grid) * ΔzC(i, j, k, grid)
 @inline AxF(i, j, k, grid) = Δy(i, j, k, grid) * ΔzF(i, j, k, grid)
 
-# Area of cell faces surrounding cell center and cell face (i, j, k) in the y-direction.
+# Area of control volume faces surrounding cell center and cell face (i, j, k) in the y-direction
 @inline AyC(i, j, k, grid) = Δx(i, j, k, grid) * ΔzC(i, j, k, grid)
 @inline AyF(i, j, k, grid) = Δx(i, j, k, grid) * ΔzF(i, j, k, grid)
 
-# Area of cell faces surrounding cell center and cell face (i, j, k) in the z-direction.
+# Area of control volume faces surrounding cell center and cell face (i, j, k) in the z-direction
 @inline Az(i, j, k, grid) = Δx(i, j, k, grid) * Δy(i, j, k, grid)
 
-# Volume of a cell.
-@inline VF(i, j, k, grid) = ΔxF(i, j, k, grid) * ΔyF(i, j, k, grid) * Δz(i, j, k, grid)
-@inline VC(i, j, k, grid) = ΔxC(i, j, k, grid) * ΔyC(i, j, k, grid) * Δz(i, j, k, grid)
+# Volume of a control volume surrounding a cell center.
+@inline Vᶜ(i, j, k, grid) = Δx(i, j, k, grid) * Δy(i, j, k, grid) * ΔzC(i, j, k, grid)
 
-# Volume of face-centered cells.
-@inline Vᵘ(i, j, k, grid) = (VF(i, j, k, grid) + VF(i, j, k+1, grid)) / 2
-@inline Vᵛ(i, j, k, grid) = (VF(i, j, k, grid) + VF(i, j+1, k, grid)) / 2
-@inline Vʷ(i, j, k, grid) = (VF(i, j, k, grid) + VF(i+1, j, k, grid)) / 2
+# Volume of a control volume surrounding the cell u-, v-, and w-faces.
+@inline Vᵘ(i, j, k, grid) = Vᶜ(i, j, k, grid)
+@inline Vᵛ(i, j, k, grid) = Vᶜ(i, k, k, grid)
+@inline Vʷ(i, j, k, grid) = Δx(i, j, k, grid) * Δy(i, j, k, grid) * ΔzF(i, j, k, grid)
 
