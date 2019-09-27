a51275d75faadfcc6f6a9f3965c65aeccb74b158
Gregory Wagner <wagner.greg@gmail.com>
2019-09-27 23:09:37 +0100

    Splits forcing functionality into separate file

---
+++ src/forcing.jl
@@ -0,0 +1,17 @@
+"""
+    Forcing(; kwargs...)
+
+Return a named tuple of forcing functions
+for each solution field.
+"""
+Forcings(; u=zerofunk, v=zerofunk, w=zerofunk, T=zerofunk, S=zerofunk) =
+    (u=u, v=v, w=w, T=T, S=S)
+
+struct SpatialForcing{Lx, Ly, Lz, F}
+    func :: F
+end
+
+SpatialForcing(func::Function) = SpatialForcing{Cell, Cell, Cell, typeof(func)}(func)
+
+@inline (forcing::SpatialForcing{X, Y, Z})(i, j, k, grid, time, U, C, params) where {X, Y, Z} =
+    @inbounds forcing.func(xnode(X, i, grid), ynode(Y, j, grid), znode(Z, k, grid))
