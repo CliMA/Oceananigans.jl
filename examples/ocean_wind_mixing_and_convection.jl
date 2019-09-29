f02f04ff923b8e2ba2d3f7d8318d0b0034528abc
Gregory Wagner <wagner.greg@gmail.com>
2019-09-29 15:26:17 -0400

    More improvements to ocean wind mixing and convection example
    Former-commit-id: 334e2ec7cddb7d95ce0aaa63b7466ba71419b00c

--- examples/ocean_wind_mixing_and_convection.jl
+++ examples/ocean_wind_mixing_and_convection.jl
@@ -11,39 +11,40 @@ using Oceananigans, Random, Printf, PyPlot
 # ## Model parameters
 #
 # Here we use an isotropic, cubic grid with `Nz` grid points and grid spacing
-# `Δz = 1` meter. We specify the temperature flux `QT` at the top of the domain,
-# which is related to the heat flux at the top via
+# `Δz = 1` meter. We specify fluxes of heat, momentum, and salinity via
 #
-# ```math
-# Q_T = \frac{Q_h}{\rho_0 c_P} \, ,
-# ```
+#   1. A temperature flux `Qᵀ` at the top of the domain, which is related to heat flux 
+#       by `Qᵀ = Qʰ / (ρ₀ * cᴾ)`, where `Qʰ` is the heat flux, `ρ₀` is a reference density, 
+#       and `cᴾ` is the heat capacity of seawater. With a reference density 
+#       `ρ₀ = 1026 kg m⁻³`and heat capacity `cᴾ = 3991`, our chosen temperature flux of 
+#       `Qᵀ = 10⁻⁴ K m⁻¹ s⁻¹` corresponds to a heat flux of `Qʰ = 409.4 W m⁻²`, a 
+#       relatively powerful cooling rate.
+#       
 #
-# where ``Q_h`` is the heat flux, ``\rho_0`` is a reference density, and ``c_P`` is 
-# the heat capacity of seawater. With a reference density ``\rho_0 = 1026`` and heat 
-# capacity ``c_P = 3991``, our chosen temperature flux of 
-# ``Q_T = 10^{-4} \, \mathrm{K \, m^{-1} \, s^{-1}}`` corresponds to a heat flux of
-# ``Q_h = 409.4 \, \mathrm{W \, m^{-2}}``.
+#   2. A velocity flux `Qᵘ` at the top of the domain, which is related
+#       to the `x` momentum flux `τˣ` via `τˣ = ρ₀ * Qᵘ`, where `ρ₀` is a reference density.
+#       Our chosen value of `Qᵘ = 2 × 10⁻⁵ m² s⁻²` roughly corresponds to atmospheric winds
+#       of `uᵃ = 4.5 m s⁻¹` using the parameterization `τ = 0.0025 * |uᵃ| * uᵃ`.
 #
-# Finally, we use an initial temperature gradient of ``\partial_z T = 0.005``,
-# which implies an iniital buoyancy frequency
+#   3. An evaporation rate `evaporation = 10⁻⁶ m s⁻¹`, or approximately 0.1 millimeter per 
+#       hour.
 #
-# ```math
-# N\^2 = \alpha g \partial_z T = 9.8 \times 10^{-6} \, \mathrm{s^{-2}}
-# ```
-#
-# with a thermal expansion coefficient ``\alpha = 2 \times 10^{-4} \, \mathrm{K^{-1}}`` 
-# and gravitational acceleration ``g = 9.81 \, \mathrm{m \, s^{-2}}``. Note that, by default,
-# the `SeawaterBuoyancy` model uses a gravitational acceleration 
-# ``g_\mathrm{Earth} = 9.80665 \, \mathrm{m \, s^{-2}}``. 
+# Finally, we use an initial temperature gradient of `∂T/∂z = 0.005 K m⁻¹`,
+# which implies an iniital buoyancy frequency `N² = α * g * ∂T/∂z = 9.8 × 10⁻⁶ s⁻²`
+# with a thermal expansion coefficient `α = 2 × 10⁻⁴ K⁻¹` and gravitational acceleration 
+# `g = 9.81 s⁻²`. Note that, by default, the `SeawaterBuoyancy` model uses a gravitational 
+# acceleration `gᴱᵃʳᵗʰ = 9.80665 s⁻²`.
 
-      Nz = 48       # Number of grid points in x, y, z
-      Δz = 1.0      # Grid spacing in x, y, z (meters)         
-      QT = 1e-4     # Temperature flux at surface
-      Qu = -5e-5    # Velocity flux at surface
-    ∂T∂z = 0.005    # Initial vertical temperature gradient
-       f = 1e-4     # Coriolis parameter
-       α = 2e-4     # Thermal expansion coefficient
-end_time = 2hour    # End time for the simulation
+         Nz = 48       # Number of grid points in x, y, z
+         Δz = 1.0      # Grid spacing in x, y, z (meters)         
+         Qᵀ = 5e-5     # Temperature flux at surface
+         Qᵘ = -2e-5    # Velocity flux at surface
+       ∂T∂z = 0.005    # Initial vertical temperature gradient
+evaporation = 1e-7     # Mass-specific evaporation rate [m s⁻¹]
+   end_time = 2hour    # End time for the simulation
+          f = 1e-4     # Coriolis parameter
+          α = 2e-4     # Thermal expansion coefficient
+          β = 8e-4     # Haline contraction coefficient
 
 # ## Boundary conditions
 #
@@ -51,10 +52,15 @@ end_time = 2hour    # End time for the simulation
 # and a `Gradient` boundary condition on `T` that maintains a constant stratification
 # at the bottom.
 
-ubcs = HorizontallyPeriodicBCs(top = BoundaryCondition(Flux, Qu))
+u_bcs = HorizontallyPeriodicBCs(top = BoundaryCondition(Flux, Qᵘ))
 
-Tbcs = HorizontallyPeriodicBCs(    top = BoundaryCondition(Flux, QT),
+T_bcs = HorizontallyPeriodicBCs(   top = BoundaryCondition(Flux, Qᵀ),
                                 bottom = BoundaryCondition(Gradient, ∂T∂z))
+
+## Salinity flux: Qˢ = - E * S
+@inline Qˢ(i, j, grid, time, iter, U, C, p) = @inbounds -p.evaporation * C.S[i, j, 1]
+
+S_bcs = HorizontallyPeriodicBCs(top = BoundaryCondition(Flux, Qˢ))
 
 # ## Model instantiation
 #
@@ -65,16 +71,16 @@ Tbcs = HorizontallyPeriodicBCs(    top = BoundaryCondition(Flux, QT),
 # conditions for `u` and `T`.
 
 model = Model(
-           architecture = CPU(),
-                   grid = RegularCartesianGrid(N=(Nz, Nz, Nz), L=(Δz*Nz, Δz*Nz, Δz*Nz)),
-               coriolis = FPlane(f=f),
-               buoyancy = SeawaterBuoyancy(equation_of_state=LinearEquationOfState(α=α)),
-                closure = AnisotropicMinimumDissipation(),
-    boundary_conditions = BoundaryConditions(u=ubcs, T=Tbcs)
+         architecture = CPU(),
+                 grid = RegularCartesianGrid(N=(Nz, Nz, Nz), L=(Δz*Nz, Δz*Nz, Δz*Nz)),
+             coriolis = FPlane(f=f),
+             buoyancy = SeawaterBuoyancy(equation_of_state=LinearEquationOfState(α=α, β=β)),
+              closure = AnisotropicMinimumDissipation(),
+  boundary_conditions = BoundaryConditions(u=u_bcs, T=T_bcs, S=S_bcs),
+           parameters = (evaporation = evaporation,)
 )
 
-# To use the Smagorinsky-Lilly turbulence closure 
-# (with a constant model coefficient), use
+# To use the Smagorinsky-Lilly turbulence closure (with a constant model coefficient), use
 #
 # ```julia
 # closure = ConstantSmagorinsky()
@@ -96,25 +102,26 @@ model = Model(
 Ξ(z) = randn() * z / model.grid.Lz * (1 + z / model.grid.Lz) # noise
 
 ## Temperature initial condition: a stable density tradient with random noise superposed.
-T₀(x, y, z) = 20 + ∂T∂z * z + ∂T∂z * model.grid.Lz * 1e-6 * Ξ(z)
+T₀(x, y, z) = 20 + ∂T∂z * z + ∂T∂z * model.grid.Lz * 1e-1 * Ξ(z)
 
 ## Velocity initial condition: random noise scaled by the friction velocity.
-u₀(x, y, z) = sqrt(abs(Qu)) * 1e-3 * Ξ(z)
+u₀(x, y, z) = sqrt(abs(Qᵘ)) * 1e-1 * Ξ(z)
 
-set!(model, u=u₀, w=u₀, T=T₀)
+set!(model, u=u₀, w=u₀, T=T₀, S=35)
 
 # ## Set up output
 #
 # We set up an output writer that saves all velocity fields, tracer fields, and the subgrid
 # turbulent diffusivity associated with `model.closure`. The `prefix` keyword argument 
-# to `JLD2OutputWriter` indicates that output will be saved in `wind_mixing_and_convection.jld2`.
+# to `JLD2OutputWriter` indicates that output will be saved in 
+# `ocean_wind_mixing_and_convection.jld2`.
 
 ## Create a NamedTuple containing all the fields to be outputted.
 fields_to_output = merge(model.velocities, model.tracers, (νₑ=model.diffusivities.νₑ,))
 
 ## Instantiate a JLD2OutputWriter to write fields.
 field_writer = JLD2OutputWriter(model, FieldOutputs(fields_to_output); interval=hour/4, 
-                                prefix="wind_mixing_and_convection", force=true)
+                                prefix="ocean_wind_mixing_and_convection", force=true)
                                 
 ## Add the output writer to the models `output_writers`.
 model.output_writers[:fields] = field_writer
@@ -122,19 +129,56 @@ model.output_writers[:fields] = field_writer
 # ## Running the simulation
 #
 # To run the simulation, we instantiate a `TimeStepWizard` to ensure stable time-stepping
-# with a Courant-Freidrichs-Lewy (CFL) number of 0.2. In addition, we create a diagnostic
-# for calculating the maximum vertical velocity, and create a figure for live plotting
-# of simulation results.
+# with a Courant-Freidrichs-Lewy (CFL) number of 0.2. 
 
-wizard = TimeStepWizard(cfl=0.2, Δt=10.0, max_change=1.1, max_Δt=20.0)
+wizard = TimeStepWizard(cfl=0.2, Δt=1.0, max_change=1.1, max_Δt=5.0)
+
+# A diagnostic that returns the maximum absolute value of `w` by calling
+# `wmax(model)`:
 
 wmax = FieldMaximum(abs, model.velocities.w)
 
-fig, axs = subplots()
-x = repeat(model.grid.xC, 1, model.grid.Nz)
-z = repeat(reshape(model.grid.zF[1:end-1], 1, model.grid.Nz), model.grid.Nx, 1)
+# We also create a figure and define a plotting function for live plotting of results.
 
-# Run the model
+fig, axs = subplots(ncols=3, figsize=(12, 5))
+
+"""
+    makeplot!(axs, model)
+
+Make a triptych of x-z slices of vertical velocity, temperature, and salinity 
+associated with `model` in `axs`.
+"""
+function makeplot!(axs, model)
+    jhalf = floor(Int, model.grid.Nz/2) 
+
+    ## Coordinate arrays for plotting
+    xC = repeat(model.grid.xC, 1, model.grid.Nz)
+    zF = repeat(reshape(model.grid.zF[1:end-1], 1, model.grid.Nz), model.grid.Nx, 1)
+    zC = repeat(reshape(model.grid.zC, 1, model.grid.Nz), model.grid.Nx, 1)
+
+    sca(axs[1]); cla()
+    title("Vertical velocity")
+    pcolormesh(xC, zF, data(model.velocities.w)[:, jhalf, :])
+    xlabel("\$ x \$ (m)"); ylabel("\$ z \$ (m)")
+
+    sca(axs[2]); cla() 
+    title("Temperature")
+    pcolormesh(xC, zC, data(model.tracers.T)[:, jhalf, :])
+    xlabel("\$ x \$ (m)")
+
+    sca(axs[3]); cla()
+    title("Salinity")
+    pcolormesh(xC, zC, data(model.tracers.S)[:, jhalf, :])
+    xlabel("\$ x \$ (m)")
+
+    [ax.set_aspect(1) for ax in axs]
+    pause(0.01)
+
+    return nothing
+end
+
+# Finally, we run the the model in a `while` loop.
+
 while model.clock.time < end_time
 
     ## Update the time step associated with `wizard`.
@@ -144,14 +188,9 @@ while model.clock.time < end_time
     walltime = @elapsed time_step!(model, 10, wizard.Δt)
 
     ## Print a progress message
-    @printf("i: %04d, t: %s, Δt: %s, wmax = %.1e ms⁻¹, wall time: %s\n", model.clock.iteration, 
-            prettytime(model.clock.time), prettytime(wizard.Δt), wmax(model), prettytime(walltime))
+    @printf("i: %04d, t: %s, Δt: %s, wmax = %.1e ms⁻¹, wall time: %s\n", 
+            model.clock.iteration, prettytime(model.clock.time), prettytime(wizard.Δt), 
+            wmax(model), prettytime(walltime))           
 
-    ## Make a plot
-    sca(axs); cla(); axs.set_aspect(1)
-    pcolormesh(x, z, data(model.velocities.w)[:, floor(Int, Nz/2), :])
-    xlabel("\$ x \$ (m)")
-    ylabel("\$ z \$ (m)")
-    title("Vertical velocity")
-    pause(0.01)
+    makeplot!(axs, model)
 end
