1d6d83fb65ba6fee39d525fd985836f53477b7d0
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-10-02 13:39:22 -0400

    Change interpolation notation to just \iota

--- src/Operators/interpolation.jl
+++ src/Operators/interpolation.jl
@@ -1,14 +1,14 @@
-@inline ϊx_caa(i, j, k, grid::AbstractGrid{T}, f) where T = @inbounds T(0.5) * (f[i,   j, k] + f[i, j, k+1])
-@inline ϊx_faa(i, j, k, grid::AbstractGrid{T}, f) where T = @inbounds T(0.5) * (f[i, j, k-1] + f[i,   j, k])
+@inline ιx_caa(i, j, k, grid::AbstractGrid{T}, f) where T = @inbounds T(0.5) * (f[i,   j, k] + f[i, j, k+1])
+@inline ιx_faa(i, j, k, grid::AbstractGrid{T}, f) where T = @inbounds T(0.5) * (f[i, j, k-1] + f[i,   j, k])
 
-@inline ϊy_aca(i, j, k, grid::AbstractGrid{T}, f) where T = @inbounds T(0.5) * (f[i, j,   k] + f[i, j+1, k])
-@inline ϊy_afa(i, j, k, grid::AbstractGrid{T}, f) where T = @inbounds T(0.5) * (f[i, j-1, k] + f[i, j,   k])
+@inline ιy_aca(i, j, k, grid::AbstractGrid{T}, f) where T = @inbounds T(0.5) * (f[i, j,   k] + f[i, j+1, k])
+@inline ιy_afa(i, j, k, grid::AbstractGrid{T}, f) where T = @inbounds T(0.5) * (f[i, j-1, k] + f[i, j,   k])
 
-@inline ϊz_aac(i, j, k, grid::RegularCartesianGrid{T}, f) where T = @inbounds T(0.5) * (f[i, j,   k] + f[i+1, j, k])
-@inline ϊz_aaf(i, j, k, grid::RegularCartesianGrid{T}, f) where T = @inbounds T(0.5) * (f[i-1, j, k] + f[i, j,   k])
+@inline ιz_aac(i, j, k, grid::RegularCartesianGrid{T}, f) where T = @inbounds T(0.5) * (f[i, j,   k] + f[i+1, j, k])
+@inline ιz_aaf(i, j, k, grid::RegularCartesianGrid{T}, f) where T = @inbounds T(0.5) * (f[i-1, j, k] + f[i, j,   k])
 
-@inline ϊz_aac(i, j, k, grid::VerticallyStretchedCartesianGrid, f) =
+@inline ιz_aac(i, j, k, grid::VerticallyStretchedCartesianGrid, f) =
     @inbounds ((grid.zC[i] - grid.zF[i]) * f[i, j, k] + (grid.zF[i+1] - grid.zC[i]) * f[i+1, j, k]) / grid.ΔzF[i]
-@inline ϊz_aaf(i, j, k, grid::VerticallyStretchedCartesianGrid, f) =
+@inline ιz_aaf(i, j, k, grid::VerticallyStretchedCartesianGrid, f) =
     @inbounds ((grid.zF[i] - grid.zC[i-1]) * f[i-1, j, k] + (grid.zC[i] - grid.zF[i]) * f[i, j, k]) / grid.ΔzC[i-1]
 
