cee89c7c9ea6a37f598a5f6741b089961b1a8352
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-10-02 13:56:28 -0400

    Add ∂ operators

--- src/Operators/differences.jl
+++ src/Operators/differences.jl
@@ -7,3 +7,15 @@
 @inline δz_aac(i, j, k, grid, f) = @inbounds f[i+1, j, k] - f[i, j,   k]
 @inline δz_aaf(i, j, k, grid, f) = @inbounds f[i, j,   k] - f[i-1, j, k]
 
+@inline ∂x_caa(i, j, k, grid, f) = δx_caa(i, j, k, grid, f) / grid.Δx
+@inline ∂x_faa(i, j, k, grid, f) = δx_faa(i, j, k, grid, f) / grid.Δx
+
+@inline ∂y_aca(i, j, k, grid, f) = δy_caa(i, j, k, grid, f) / grid.Δy
+@inline ∂y_afa(i, j, k, grid, f) = δy_faa(i, j, k, grid, f) / grid.Δy
+
+@inline ∂z_aac(i, j, k, grid::RegularCartesianGrid, f) = δz_aac(i, j, k, grid, f) / grid.Δz
+@inline ∂z_aaf(i, j, k, grid::RegularCartesianGrid, f) = δz_aaf(i, j, k, grid, f) / grid.Δz
+
+@inline ∂z_aac(i, j, k, grid::VerticallyStretchedCartesianGrid, f) = δz_aac(i, j, k, grid, f) / grid.ΔzF[k]
+@inline ∂z_aaf(i, j, k, grid::VerticallyStretchedCartesianGrid, f) = δz_aaf(i, j, k, grid, f) / grid.ΔzC[k]
+
