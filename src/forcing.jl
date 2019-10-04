9e2dbd35e135a4f7c72f72b186ecfe79098a7418
Gregory Wagner <wagner.greg@gmail.com>
2019-10-03 21:24:35 -0400

    Changes SpatialForcing to NaturalForcing
    Former-commit-id: 160110ca3b5f4ae6474b33e70aa8ebafa1d77407

--- src/forcing.jl
+++ src/forcing.jl
@@ -1,17 +1,32 @@
 """
-    Forcing(; kwargs...)
+    ModelForcing(; kwargs...)
 
-Return a named tuple of forcing functions
-for each solution field.
+Return a named tuple of forcing functions for each solution field.
 """
 ModelForcing(; u=zerofunk, v=zerofunk, w=zerofunk, T=zerofunk, S=zerofunk) =
     (u=u, v=v, w=w, T=T, S=S)
 
-struct SpatialForcing{Lx, Ly, Lz, F}
+"""
+    NaturalForcing([location=(Cell, Cell, Cell),] forcing_function::Function)
+
+Construct forcing for a field at `location` from `forcing_function(x, y, z, t)`, 
+where `x, y, z` is position and `t` is time.
+"""
+struct NaturalForcing{Lx, Ly, Lz, F}
     func :: F
 end
 
-SpatialForcing(func::Function) = SpatialForcing{Cell, Cell, Cell, typeof(func)}(func)
+NaturalForcing(location::Tuple, func::Function) = 
+    NaturalForcing{location[1], location[2], location[3], typeof(func)}(func)
 
-@inline (forcing::SpatialForcing{X, Y, Z})(i, j, k, grid, time, U, C, params) where {X, Y, Z} =
-    @inbounds forcing.func(xnode(X, i, grid), ynode(Y, j, grid), znode(Z, k, grid))
+"""
+    NaturalForcing([location=(Cell, Cell, Cell),] forcing::NaturalForcing)
+
+Construct forcing for a field at `location` using `forcing.func`.
+"""
+NaturalForcing(location::Tuple, forcing::NaturalForcing) = NaturalForcing(location, forcing.func)
+
+NaturalForcing(func::Function) = NaturalForcing((Cell, Cell, Cell), func)
+
+@inline (forcing::NaturalForcing{X, Y, Z})(i, j, k, grid, time, U, C, params) where {X, Y, Z} =
+    @inbounds forcing.func(xnode(X, i, grid), ynode(Y, j, grid), znode(Z, k, grid), time)
