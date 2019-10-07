147898777a163764763f1c1145e0a9442df291b2
Gregory Wagner <wagner.greg@gmail.com>
2019-10-07 14:08:07 -0400

    Move model initialization functionality to new file
    Former-commit-id: e7821e846ee81e324b0fab518b8cfa11605bf3ea

---
+++ src/model_initialization.jl
@@ -0,0 +1,129 @@
+#####
+##### Model initialization utilities
+#####
+
+float_type(m::Model) = eltype(model.grid)
+add_bcs!(model::Model; kwargs...) = add_bcs(model.boundary_conditions; kwargs...)
+
+"""
+    with_tracers(tracers, initial_tuple, tracer_default)
+
+Create a tuple corresponding to the solution variables `u`, `v`, `w`, 
+and `tracers`. `initial_tuple` is a `NamedTuple` that at least has
+fields `u`, `v`, and `w`, and may have some fields corresponding to
+the names in `tracers`. `tracer_default` is a function that produces
+a default tuple value for each tracer if not included in `initial_tuple`.
+"""
+function with_tracers(tracers, initial_tuple, tracer_default; with_velocities=false)
+    solution_values = [] # Array{Any, 1}
+
+    if with_velocities
+        push!(solution_values, initial_tuple.u)
+        push!(solution_values, initial_tuple.v)
+        push!(solution_values, initial_tuple.w)
+    end
+
+    for name in tracers
+        tracer_elem = name ∈ propertynames(initial_tuple) ?
+                        getproperty(initial_tuple, name) :
+                        tracer_default(tracers, initial_tuple)
+
+        push!(solution_values, tracer_elem)
+    end
+
+    solution_names = [:u, :v, :w]
+    append!(solution_names, tracers)
+
+    return NamedTuple{Tuple(solution_names)}(Tuple(solution_values))
+end
+
+"""
+    ModelForcing(; kwargs...)
+
+Return a named tuple of forcing functions for each solution field.
+"""
+ModelForcing(; u=zerofunk, v=zerofunk, w=zerofunk, tracer_forcings...) =
+    merge((u=u, v=v, w=w), tracer_forcings)
+
+const Forcing = ModelForcing
+
+default_tracer_forcing(args...) = zerofunk
+ModelForcing(tracers, proposal_forcing) = with_tracers(tracers, proposal_forcing, default_tracer_forcing, 
+                                                       with_velocities=true)
+
+"""
+    VelocityFields(arch, grid)
+
+Return a NamedTuple with fields `u`, `v`, `w` initialized on
+the architecture `arch` and `grid`.
+"""
+function VelocityFields(arch, grid)
+    u = FaceFieldX(arch, grid)
+    v = FaceFieldY(arch, grid)
+    w = FaceFieldZ(arch, grid)
+    return (u=u, v=v, w=w)
+end
+
+"""
+    TracerFields(arch, grid)
+
+Return a NamedTuple with tracer fields initialized
+as `CellField`s on the architecture `arch` and `grid`.
+"""
+function TracerFields(arch, grid, tracernames=(:T, :S))
+    tracerfields = Tuple(CellField(arch, grid) for c in tracernames)
+    return NamedTuple{tracernames}(tracerfields)
+end
+
+TracerFields(tracers::NamedTuple) = tracers
+
+tracernames(names::NTuple{N, Symbol}) where N = :u ∈ names ? names[4:end] : names
+tracernames(::NamedTuple{names}) where names = tracernames(names)
+
+"""
+    PressureFields(arch, grid)
+
+Return a NamedTuple with pressure fields `pHY′` and `pNHS`
+initialized as `CellField`s on the architecture `arch` and `grid`.
+"""
+function PressureFields(arch, grid)
+    pHY′ = CellField(arch, grid)
+    pNHS = CellField(arch, grid)
+    return (pHY′=pHY′, pNHS=pNHS)
+end
+
+"""
+    Tendencies(arch, grid, tracernames)
+
+Return a NamedTuple with tendencies for all solution fields
+(velocity fields and tracer fields), initialized on
+the architecture `arch` and `grid`.
+"""
+function Tendencies(arch, grid, tracernames)
+
+    velocities = (u = FaceFieldX(arch, grid),
+                  v = FaceFieldY(arch, grid),
+                  w = FaceFieldZ(arch, grid))
+
+    tracers = TracerFields(arch, grid, tracernames)
+
+    return merge(velocities, tracers)
+end
+
+"""
+    AdamsBashforthTimestepper(float_type, arch, grid, tracers, χ)
+
+Return an AdamsBashforthTimestepper object with tendency
+fields on `arch` and `grid` and AB2 parameter `χ`.
+"""
+struct AdamsBashforthTimestepper{T, TG}
+      Gⁿ :: TG
+      G⁻ :: TG
+       χ :: T
+end
+
+function AdamsBashforthTimestepper(float_type, arch, grid, tracers, χ)
+   Gⁿ = Tendencies(arch, grid, tracers)
+   G⁻ = Tendencies(arch, grid, tracers)
+   return AdamsBashforthTimestepper{float_type, typeof(Gⁿ)}(Gⁿ, G⁻, χ)
+end
