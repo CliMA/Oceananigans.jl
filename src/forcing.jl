4a921ab3b383822bf009dec6b3afd2362c8601f3
Gregory Wagner <wagner.greg@gmail.com>
2019-10-09 11:39:39 -0400

    Fix error in at_location; new constructor for SimpleForcing

--- src/forcing.jl
+++ src/forcing.jl
@@ -1,12 +1,15 @@
 """
-    SimpleForcing{Lx, Ly, Lz, F, P}
+    SimpleForcing{X, Y, Z, F, P}
 
 Callable object for specifying 'simple' forcings of `x, y, z, t` and optionally
-`parameters` of type `P` at location `Lx, Ly, Lz`.
+`parameters` of type `P` at location `X, Y, Z`.
 """
-struct SimpleForcing{Lx, Ly, Lz, F, P}
+struct SimpleForcing{X, Y, Z, F, P}
           func :: F
     parameters :: P
+    function SimpleForcing{X, Y, Z}(func, parameters) where {X, Y, Z}
+        return new{X, Y, Z, typeof(func), typeof(parameters)}(func, parameters)
+    end
 end
 
 """
@@ -36,7 +39,7 @@ julia> v_forcing = SimpleForcing(parameterized_forcing, parameters=(μ=42, λ=0.
 
 """
 SimpleForcing(location::Tuple, func::Function; parameters=nothing) = 
-    SimpleForcing{location[1], location[2], location[3], typeof(func), typeof(parameters)}(func, parameters)
+    SimpleForcing{location[1], location[2], location[3]}(func, parameters)
 
 SimpleForcing(func::Function; kwargs...) = SimpleForcing((Cell, Cell, Cell), func; kwargs...)
 
@@ -45,11 +48,11 @@ SimpleForcing(location::Tuple, forcing::SimpleForcing) = SimpleForcing(location,
 @inline (f::SimpleForcing{X, Y, Z})(i, j, k, grid, time, U, C, params) where {X, Y, Z} =
     @inbounds f.func(xnode(X, i, grid), ynode(Y, j, grid), znode(Z, k, grid), time, f.parameters)
 
-@inline (f::SimpleForcing{X, Y, Z, F, <:Nothing})(i, j, k, grid, time, U, C, params) where {X, Y, Z, F} =
+@inline (f::SimpleForcing{X, Y, Z, F, <:Nothing})(i, j, k, grid, time, U, C, params) where {X, Y, Z, F} = 
     @inbounds f.func(xnode(X, i, grid), ynode(Y, j, grid), znode(Z, k, grid), time)
 
 at_location(location, u::Function) = u
-at_location(location, u::SimpleForcing) = SimpleForcing(location, u)
+at_location(location, u::SimpleForcing) = SimpleForcing{location[1], location[2], location[3]}(u.func, u.parameters)
 
 zeroforcing(args...) = 0
 
