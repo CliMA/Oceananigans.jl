fbd73765d6f01820a3aab1687a46c34822852523
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2019-10-08 23:20:58 -0400

    Tweaks in calculate_diffusivities for smagorinsky

--- src/TurbulenceClosures/smagorinsky.jl
+++ src/TurbulenceClosures/smagorinsky.jl
@@ -311,19 +311,20 @@ Return the diffusive flux divergence `∇ ⋅ (κ ∇ c)` for the turbulence
     + ∂z_aac(i, j, k, grid, κ_∂z_c, c, tracer_idx, closure, diffusivities.νₑ)
 )
 
-function calculate_diffusivities!(K, arch, grid, closure::AbstractSmagorinsky, args...)
-    @launch device(arch) config=launch_config(grid, 3) calculate_viscosity!(K, grid, closure, args...)
+function calculate_diffusivities!(K, arch, grid, closure::AbstractSmagorinsky, buoyancy, U, C)
+    @launch device(arch) config=launch_config(grid, 3) calculate_viscosity!(K.νₑ, grid, closure, buoyancy, U, C)
     return nothing
 end
 
-function calculate_viscosity!(diffusivities, grid, closure::AbstractSmagorinsky, buoyancy, U, C)
+function calculate_viscosity!(νₑ, grid, closure::AbstractSmagorinsky, buoyancy, U, C)
     @loop for k in (1:grid.Nz; (blockIdx().z - 1) * blockDim().z + threadIdx().z)
         @loop for j in (1:grid.Ny; (blockIdx().y - 1) * blockDim().y + threadIdx().y)
             @loop for i in (1:grid.Nx; (blockIdx().x - 1) * blockDim().x + threadIdx().x)
-                @inbounds diffusivities.νₑ[i, j, k] = ν_ccc(i, j, k, grid, closure, buoyancy, U, C)
+                @inbounds νₑ[i, j, k] = ν_ccc(i, j, k, grid, closure, buoyancy, U, C)
             end
         end
     end
+    return nothing
 end
 
 #####
