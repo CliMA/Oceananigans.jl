fdf3194cb6b644b92c55925244e5a6953ad82134
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2019-10-09 08:18:30 -0400

    Updates compute w continuity test for new syntax

--- test/test_time_stepping.jl
+++ test/test_time_stepping.jl
@@ -42,7 +42,7 @@ function compute_w_from_continuity(arch, FT)
 
     fill_halo_regions!(u.data, bcs.u, arch, grid)
     fill_halo_regions!(v.data, bcs.v, arch, grid)
-    compute_w_from_continuity!(grid, (u=u.data, v=v.data, w=w.data))
+    compute_w_from_continuity!((u=u.data, v=v.data, w=w.data), grid)
 
     fill_halo_regions!(w.data, bcs.w, arch, grid)
     velocity_div!(grid, u.data, v.data, w.data, div_u.data)
