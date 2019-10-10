b6da7bf2c0a341aad496d0ce8245533c78096cc2
Gregory Wagner <wagner.greg@gmail.com>
2019-10-10 00:30:00 -0400

    Nukes unused field addition test function
    Former-commit-id: c57686c54f62e7294a1fb3c744e6841c4f36ba84

--- test/test_fields.jl
+++ test/test_fields.jl
@@ -22,19 +22,6 @@ function correct_field_value_was_set(arch, grid, fieldtype, val::Number)
     return data(f) ≈ val * ones(size(f))
 end
 
-function correct_field_addition(arch, grid, fieldtype, val1::Number, val2::Number)
-    f1 = fieldtype(arch, grid)
-    f2 = fieldtype(arch, grid)
-
-    set!(f1, val1)
-    set!(f2, val2)
-    f3 = f1 + f2
-
-    val3 = convert(mm.float_type, val1) + convert(mm.float_type, val2)
-    f_ans = val3 * ones(size(f1))
-    return f3.data ≈ f_ans
-end
-
 @testset "Fields" begin
     println("Testing fields...")
 
