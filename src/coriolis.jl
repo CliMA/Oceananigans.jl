6a3223a8b4ae5fcd071111edb94f981f55d6ce50
suyashbire1 <suyash309@gmail.com>
2019-10-10 11:57:22 -0400

    zfcrossu for betaplane
    Former-commit-id: d56d755a69f9880cc7f9a09dc59c5b1442a9c75c

--- src/coriolis.jl
+++ src/coriolis.jl
@@ -29,7 +29,7 @@ Returns a parameter object for constant rotation at the angular frequency
 Also called `FPlane`, after the "f-plane" approximation for the local effect of
 Earth's rotation in a planar coordinate system tangent to the Earth's surface.
 """
-function FPlane(T::DataType=Float64; f) 
+function FPlane(T::DataType=Float64; f)
     return FPlane{T}(f)
 end
 
@@ -98,9 +98,9 @@ end
 
 @inline x_f_cross_U(i, j, k, grid, rotation::FPlane, U) = -fv(i, j, k, grid, rotation.f, U.v)
 @inline y_f_cross_U(i, j, k, grid, rotation::FPlane, U) =  fu(i, j, k, grid, rotation.f, U.u)
+@inline z_f_cross_U(i, j, k, grid::AbstractGrid{T}, ::FPlane, U) where T = zero(T)
+
 
 @inline x_f_cross_U(i, j, k, grid, rotation::BetaPlane, U) = -fv(i, j, k, grid, rotation.f₀, rotation.β, U.v)
 @inline y_f_cross_U(i, j, k, grid, rotation::BetaPlane, U) =  fu(i, j, k, grid, rotation.f₀, rotation.β, U.u)
-
-@inline z_f_cross_U(i, j, k, grid::AbstractGrid{T}, ::FPlane, U) where T = zero(T)
-
+@inline z_f_cross_U(i, j, k, grid::AbstractGrid{T}, ::BetaPlane, U) where T = zero(T)
