63f5214d380e06a4bd04c2ae8b00f7812ef617f8
Gregory Wagner <wagner.greg@gmail.com>
2019-10-10 21:47:53 -0400

    Moves generic operator functionality to module top

--- src/AbstractOperations/AbstractOperations.jl
+++ src/AbstractOperations/AbstractOperations.jl
@@ -24,7 +24,79 @@ abstract type AbstractOperation{X, Y, Z, G} <: AbstractLocatedField{X, Y, Z, Not
 data(op::AbstractOperation) = op
 Base.parent(op::AbstractOperation) = op
 
+function validate_grid(a::F, b::F) where F<:AbstractField
+    a.grid === b.grid || throw(ArgumentError("Two fields in a BinaryOperation must be on the same grid."))
+    return a.grid
+end
+
+validate_grid(a::AbstractField, b) = a.grid
+validate_grid(a, b::AbstractField) = b.grid
+validate_grid(a, b) = nothing
+
+function validate_grid(a, b, c...)
+    grids = []
+    push!(grids, validate_grid(a, b))
+    append!(grids, [validate_grid(a, ci) for ci in c])
+
+    for g in grids
+        if g !=== nothing
+            return g
+        end
+    end
+
+    return nothing
+end
+
+@inline identity(i, j, k, grid, ϕ) = @inbounds ϕ[i, j, k]
+@inline identity(i, j, k, grid, ϕ::Number) = ϕ
+
+interp_code(::Type{Face}) = :f
+interp_code(::Type{Cell}) = :c
+interp_code(to::L, from::L) where L = :a
+interp_code(to, from) = interp_code(to)
+
+for ξ in (:x, :y, :z)
+    ▶sym = Symbol(:▶, ξ, :sym)
+    @eval begin
+        $▶sym(s::Symbol) = $▶sym(Val(s))
+        $▶sym(::Union{Val{:f}, Val{:c}}) = string(ξ)
+        $▶sym(::Val{:a}) = ""
+    end
+end
+
+function interp_operator(from, to)
+    x, y, z = (interp_code(t, f) for (t, f) in zip(to, from))
+
+    if all(ξ === :a for ξ in (x, y, z))
+        return identity
+    else 
+        return eval(Symbol(:▶, ▶xsym(x), ▶ysym(y), ▶zsym(z), :_, x, y, z))
+    end
+end
+
+const operators = [:+, :-, :*, :/, :∂x, :∂y, :∂z]
+
+function insert_location!(ex::Expr, location)
+    if ex.head === :call && ex.args[1] ∈ operators
+        push!(ex.args, ex.args[end])
+        ex.args[3:end] .= ex.args[2:end-1]
+        ex.args[2] = location
+    end
+
+    [insert_location!(arg, location) for arg in ex.args]
+
+    return nothing
+end
+
+insert_location!(anything, location) = nothing
+
+macro at(location, ex)
+    insert_location!(ex, location)
+    return esc(ex)
+end
+
 include("binary_operations.jl")
+include("polynary_operations.jl")
 include("derivatives.jl")
 include("computations.jl")
 include("function_fields.jl")
--- src/AbstractOperations/binary_operations.jl
+++ src/AbstractOperations/binary_operations.jl
@@ -8,45 +8,17 @@ struct BinaryOperation{X, Y, Z, A, B, IA, IB, LA, LB, O, G} <: AbstractOperation
       Lb :: LB
     grid :: G
     function BinaryOperation{X, Y, Z}(op, a, b, ▶a, ▶b) where {X, Y, Z}
-        a.grid === b.grid || throw(ArgumentError("Both fields in a BinaryOperation must be on the same grid."))
-
+        grid = validate_grid(a, b)
         La = location(a)
         Lb = location(b)
-
         return new{X, Y, Z, typeof(data(a)), typeof(data(b)), 
                    typeof(▶a), typeof(▶b), typeof(La), typeof(Lb),
-                   typeof(op), typeof(a.grid)}(op, data(a), data(b), ▶a, ▶b, La, Lb, a.grid)
+                   typeof(op), typeof(grid)}(op, data(a), data(b), ▶a, ▶b, La, Lb, grid)
     end
 end
 
 @propagate_inbounds function getindex(β::BinaryOperation, i, j, k) 
     return β.op(β.▶a(i, j, k, β.grid, β.a), β.▶b(i, j, k, β.grid, β.b))
-end
-
-interp_code(::Type{Face}) = :f
-interp_code(::Type{Cell}) = :c
-interp_code(to::L, from::L) where L = :a
-interp_code(to, from) = interp_code(to)
-
-for ξ in (:x, :y, :z)
-    ▶sym = Symbol(:▶, ξ, :sym)
-    @eval begin
-        $▶sym(s::Symbol) = $▶sym(Val(s))
-        $▶sym(::Union{Val{:f}, Val{:c}}) = string(ξ)
-        $▶sym(::Val{:a}) = ""
-    end
-end
-
-@inline identity(i, j, k, grid, ϕ) = @inbounds ϕ[i, j, k]
-
-function interp_operator(to, from)
-    x, y, z = (interp_code(t, f) for (t, f) in zip(to, from))
-
-    if all(ξ === :a for ξ in (x, y, z))
-        return identity
-    else 
-        return eval(Symbol(:▶, ▶xsym(x), ▶ysym(y), ▶zsym(z), :_, x, y, z))
-    end
 end
 
 for op in (:+, :-, :/, :*)
@@ -55,15 +27,21 @@ for op in (:+, :-, :/, :*)
             La = location(a)
             Lb = location(b)
             ▶a = identity
-            ▶b = interp_operator(La, Lb)
-            return BinaryOperation{XA, YA, ZA}($op, a, b, ▶a, ▶b)
+            ▶b = interp_operator(Lb, La)
+            return BinaryOperation{La[1], La[2], La[3]}($op, a, b, ▶a, ▶b)
         end
+
+        $op(a::AbstractLocatedField{X, Y, Z}, b::Number) where {X, Y, Z} =
+            BinaryOperation{X, Y, Z}($op, a, b, identity, identity)
+
+        $op(a::Number, b::AbstractLocatedField{X, Y, Z}) where {X, Y, Z} =
+            BinaryOperation{X, Y, Z}($op, a, b, identity, identity)
 
         function $op(Lop::Tuple, a::F, b::F) where F<:AbstractLocatedField
             La = location(a)
             Lb = location(b)
-            ▶a = interp_operator(Lop, La)
-            ▶b = interp_operator(Lop, Lb)
+            ▶a = interp_operator(La, Lop)
+            ▶b = interp_operator(Lb, Lop)
             return BinaryOperation{Lop[1], Lop[2], Lop[3]}($op, a, b, ▶a, ▶b)
         end
     end
