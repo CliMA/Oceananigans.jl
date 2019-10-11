4ef9cb356c884647087386ffd94db057b7caaa36
Gregory Wagner <wagner.greg@gmail.com>
2019-10-11 00:32:48 -0400

    Bugfix in derivative; Derivative constructor needs location of derivative, not location of field

--- src/AbstractOperations/derivatives.jl
+++ src/AbstractOperations/derivatives.jl
@@ -5,9 +5,8 @@ struct Derivative{X, Y, Z, A, D, I, L, G} <: AbstractOperation{X, Y, Z, G}
       La :: L
     grid :: G
 
-    function Derivative{X, Y, Z}(a, ∂) where {X, Y, Z}
-         L = location(a)
-         ▶ = interp_operator(L, (X, Y, Z))
+    function Derivative{X, Y, Z}(a, ∂, L∂) where {X, Y, Z}
+         ▶ = interp_operator(L∂, (X, Y, Z))
         return new{X, Y, Z, typeof(data(a)), typeof(∂), 
                    typeof(▶), typeof(L), typeof(a.grid)}(data(a), ∂, ▶, L, a.grid)
     end
@@ -18,32 +17,32 @@ flip(::Type{Cell}) = Face
 
 function ∂x(a::AbstractLocatedField{X, Y, Z}) where {X, Y, Z}
     ∂ = eval(Symbol(:∂x_, interp_code(flip(X)), :aa))
-    return Derivative{flip(X), Y, Z}(a, ∂)
+    return Derivative{flip(X), Y, Z}(a, ∂, (flip(X), Y, Z))
 end
 
 function ∂y(a::AbstractLocatedField{X, Y, Z}) where {X, Y, Z}
     ∂ = eval(Symbol(:∂y_a, interp_code(flip(Y)), :a))
-    return Derivative{X, flip(Y), Z}(a, ∂)
+    return Derivative{X, flip(Y), Z}(a, ∂, (X, flip(Y), Z))
 end
 
 function ∂z(a::AbstractLocatedField{X, Y, Z}) where {X, Y, Z}
     ∂ = eval(Symbol(:∂z_aa, interp_code(flip(Z))))
-    return Derivative{X, Y, flip(Z)}(a, ∂)
+    return Derivative{X, Y, flip(Z)}(a, ∂, (X, Y, flip(Z)))
 end
 
 function ∂x(L::Tuple, a::AbstractLocatedField{X, Y, Z}) where {X, Y, Z}
     ∂ = eval(Symbol(:∂x_, interp_code(flip(X)), :aa))
-    return Derivative{L[1], L[2], L[3]}(a, ∂)
+    return Derivative{L[1], L[2], L[3]}(a, ∂, (flip(X), Y, Z))
 end
 
 function ∂y(L::Tuple, a::AbstractLocatedField{X, Y, Z}) where {X, Y, Z}
     ∂ = eval(Symbol(:∂y_a, interp_code(flip(Y)), :a))
-    return Derivative{L[1], L[2], L[3]}(a, ∂)
+    return Derivative{L[1], L[2], L[3]}(a, ∂, (X, flip(Y), Z))
 end
 
 function ∂z(L::Tuple, a::AbstractLocatedField{X, Y, Z}) where {X, Y, Z}
     ∂ = eval(Symbol(:∂z_aa, interp_code(flip(Z))))
-    return Derivative{L[1], L[2], L[3]}(a, ∂)
+    return Derivative{L[1], L[2], L[3]}(a, ∂, (X, Y, flip(Z)))
 end
 
 @propagate_inbounds getindex(d::Derivative, i, j, k) = d.▶(i, j, k, d.grid, d.∂, d.a)
