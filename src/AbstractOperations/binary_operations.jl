8358c6f8a8b8380859bec405511a0fa4fd3b9cb3
Gregory Wagner <wagner.greg@gmail.com>
2019-10-11 09:30:16 -0400

    Requires explicit grid in BinaryOperation constructor

--- src/AbstractOperations/binary_operations.jl
+++ src/AbstractOperations/binary_operations.jl
@@ -8,8 +8,7 @@ struct BinaryOperation{X, Y, Z, A, B, IA, IB, LA, LB, O, G} <: AbstractOperation
       Lb :: LB
     grid :: G
 
-    function BinaryOperation{X, Y, Z}(op, a, b, ▶a, ▶b) where {X, Y, Z}
-        grid = validate_grid(a, b)
+    function BinaryOperation{X, Y, Z}(op, a, b, ▶a, ▶b, grid) where {X, Y, Z}
         La = location(a)
         Lb = location(b)
         return new{X, Y, Z, typeof(data(a)), typeof(data(b)), 
@@ -19,10 +18,12 @@ struct BinaryOperation{X, Y, Z, A, B, IA, IB, LA, LB, O, G} <: AbstractOperation
 end
 
 @inline getindex(β::BinaryOperation, i, j, k) =
-    interpolate_then_operate(i, j, k, β.grid, β.op, β.▶a, β.▶b, β.a, β.b)
+    β.op(▶β.a(i, j, k, β.grid, β.a), β.▶b(i, j, k, β.grid, β.b))
 
-@inline interpolate_then_operate(i, j, k, grid, op, ▶a, ▶b, a, b) =
-    op(▶a(i, j, k, grid, a), ▶b(i, j, k, grid, b))
+    #interpolate_then_operate(i, j, k, β.grid, β.op, β.▶a, β.▶b, β.a, β.b)
+#@inline interpolate_then_operate(i, j, k, grid, op, ▶a, ▶b, a, b) =
+#    op(▶a(i, j, k, grid, a), ▶b(i, j, k, grid, b))
+
 
 for op in (:+, :-, :/, :*)
     @eval begin
@@ -31,21 +32,23 @@ for op in (:+, :-, :/, :*)
             Lb = location(b)
             ▶a = identity
             ▶b = interp_operator(Lb, La)
-            return BinaryOperation{La[1], La[2], La[3]}($op, a, b, ▶a, ▶b)
+            grid = validate_grid(a, b)
+            return BinaryOperation{La[1], La[2], La[3]}($op, a, b, ▶a, ▶b, grid)
         end
 
         $op(a::AbstractLocatedField{X, Y, Z}, b::Number) where {X, Y, Z} =
-            BinaryOperation{X, Y, Z}($op, a, b, identity, identity)
+            BinaryOperation{X, Y, Z}($op, a, b, identity, identity, a.grid)
 
         $op(a::Number, b::AbstractLocatedField{X, Y, Z}) where {X, Y, Z} =
-            BinaryOperation{X, Y, Z}($op, a, b, identity, identity)
+            BinaryOperation{X, Y, Z}($op, a, b, identity, identity, b.grid)
 
         function $op(Lop::Tuple, a, b)
             La = location(a)
             Lb = location(b)
             ▶a = interp_operator(La, Lop)
             ▶b = interp_operator(Lb, Lop)
-            return BinaryOperation{Lop[1], Lop[2], Lop[3]}($op, a, b, ▶a, ▶b)
+            grid = validate_grid(a, b)
+            return BinaryOperation{Lop[1], Lop[2], Lop[3]}($op, a, b, ▶a, ▶b, grid)
         end
     end
 end
