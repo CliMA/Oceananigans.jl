cee7a833f7933ec1814ffed2c78d317ded72d188
Gregory Wagner <wagner.greg@gmail.com>
2019-10-10 21:48:04 -0400

    Adds possibility to interpolate derivatives
    Former-commit-id: 0e07acd6228c164f1daa83b7d5b447f9368e027c

--- src/AbstractOperations/derivatives.jl
+++ src/AbstractOperations/derivatives.jl
@@ -1,9 +1,15 @@
-struct Derivative{X, Y, Z, A, D, G} <: AbstractOperation{X, Y, Z, G}
+struct Derivative{X, Y, Z, A, D, I, L, G} <: AbstractOperation{X, Y, Z, G}
        a :: A
        ∂ :: D
+       ▶ :: I
+      La :: L
     grid :: G
+
     function Derivative{X, Y, Z}(a, ∂) where {X, Y, Z}
-        return new{X, Y, Z, typeof(data(a)), typeof(∂), typeof(a.grid)}(data(a), ∂, a.grid)
+         L = location(a)
+         ▶ = interp_operator(L, (X, Y, Z))
+        return new{X, Y, Z, typeof(data(a)), typeof(∂), 
+                   typeof(▶), typeof(L), typeof(a.grid)}(data(a), ∂, ▶, La, a.grid)
     end
 end
 
@@ -25,4 +31,19 @@ function ∂z(a::AbstractLocatedField{X, Y, Z}) where {X, Y, Z}
     return Derivative{X, Y, flip(Z)}(a, ∂)
 end
 
-@propagate_inbounds getindex(d::Derivative, i, j, k) = d.∂(i, j, k, d.grid, d.a)
+function ∂x(L::Tuple, a::AbstractLocatedField{X, Y, Z}) where {X, Y, Z}
+    ∂ = eval(Symbol(:∂x_, interp_code(flip(X)), :aa))
+    return Derivative{L[1], L[2], L[3]}(a, ∂)
+end
+
+function ∂y(L::Tuple, a::AbstractLocatedField{X, Y, Z}) where {X, Y, Z}
+    ∂ = eval(Symbol(:∂y_a, interp_code(flip(Y)), :a))
+    return Derivative{L[1], L[2], L[3]}(a, ∂)
+end
+
+function ∂z(L::Tuple, a::AbstractLocatedField{X, Y, Z}) where {X, Y, Z}
+    ∂ = eval(Symbol(:∂z_aa, interp_code(flip(Z))))
+    return Derivative{L[1], L[2], L[3]}(a, ∂)
+end
+
+@propagate_inbounds getindex(d::Derivative, i, j, k) = d.▶(i, j, k, grid, d.∂, d.a)
