7f5418f93bb20aa185697e0b203721f600d7b8d3
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-10-13 13:11:20 -0400

    Lay out some structure
    Former-commit-id: 2c8983402a59c803f151f509e70f5689140eb853

--- docs/make.jl
+++ docs/make.jl
@@ -17,17 +17,35 @@ makedocs(
                              canonical = "https://climate-machine.github.io/Oceananigans.jl/latest/"),
    authors = "Ali Ramadhan, Greg Wagner, John Marshall, Jean-Michel Campin, Chris Hill",
   sitename = "Oceananigans.jl",
-     pages = ["Home"         => "index.md",
-              "Examples"     => "examples.md",
-              "Manual"       => ["equations.md",
-                                 "algorithm.md"],
-              "Verification" => "verification.md",
-              "Gallery"      => "gallery.md",
-              "Benchmarks"   => "benchmarks.md",
-              "Internals"    => ["internal/grids.md",
-                                 "internal/fields.md",
-                                 "internal/operators.md"],
-              "Index"        => "subject_index.md"]
+     pages = ["Home"       => "index.md",
+              "Manual"     => ["Continuous equations" => "manual/equations.md",
+                               "Numerical methods" => [
+                                                       "Overview" => "manual/overview.md",
+                                                       "Finite volume method" => "manual/finite_volume.md",
+                                                       "Staggered grid" => "manual/staggered_grid.md",
+                                                       "Fractional step method" => "manual/fractional_step.md",
+                                                       "Time stepping" => "manual/time_stepping.md",
+                                                       "Spatial operators" => "manual/spatial_operators.md",
+                                                       "Poisson solvers" => "manual/poisson_solvers.md",
+                                                       "Boundary conditions" => "manual/boundary_conditions.md",
+                                                       "Turbulence closures" => "manual/turbulence_closures.md",
+                                                       "Large eddy simulation" => "manual/large_eddy_simulation.md"
+                                                      ],
+                               "Examples" => [
+                                              "Rising thermal bubble" => "examples/rising_thermal_bubble.md"
+                                             ],
+                               "Verification" => [
+                                                  "Taylor-Green vortex"     => "verification/taylor_green_vortex.md",
+                                                  "Lid-driven cavity"       => "verification/lid_driven_cavity.md",
+                                                  "Free convection"         => "verification/free_convection.md",
+                                                  "Stratified Couette flow" => "verification/stratified_couette_flow.md"
+                                                 ]
+                              ],
+              "Gallery"    => "gallery.md",
+              "Benchmarks" => "benchmarks.md",
+              "Library"    => "library.md",
+              "Index"      => "subject_index.md"
+             ]
 )
 
 deploydocs(repo = "github.com/climate-machine/Oceananigans.jl.git")
