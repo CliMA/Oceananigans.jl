ee6d893c8be370e3f143dfc511b861b0a38bd29d
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-10-13 12:12:27 -0400

    Reorganize docs and add dummy pages
    Former-commit-id: 7fed009ba9b7012405b040f28890065ec49fdab1

--- docs/make.jl
+++ docs/make.jl
@@ -2,8 +2,11 @@ push!(LOAD_PATH,"../src/")
 push!(LOAD_PATH,"../src/operators/")
 push!(LOAD_PATH,"../src/turbulence_closures/")
 
-using Documenter
-using Oceananigans, Oceananigans.Operators, Oceananigans.TurbulenceClosures
+using
+    Documenter,
+    Oceananigans,
+    Oceananigans.Operators,
+    Oceananigans.TurbulenceClosures
 
 makedocs(
    modules = [Oceananigans, Oceananigans.Operators, Oceananigans.TurbulenceClosures],
@@ -14,14 +17,17 @@ makedocs(
                              canonical = "https://climate-machine.github.io/Oceananigans.jl/latest/"),
    authors = "Ali Ramadhan, Greg Wagner, John Marshall, Jean-Michel Campin, Chris Hill",
   sitename = "Oceananigans.jl",
-     pages = ["Home" => "index.md",
-              "Examples" => "examples.md",
-              "Numerical algorithm" => "algorithm.md",
-              "Performance benchmarks" => "benchmarks.md",
-              "Internals" => ["internal/grids.md",
-                              "internal/fields.md",
-                              "internal/operators.md"],
-              "Index" => "subject_index.md"]
+     pages = ["Home"         => "index.md",
+              "Examples"     => "examples.md",
+              "Manual"       => ["equations.md",
+                                 "algorithm.md"],
+              "Verification" => "verification.md",
+              "Gallery"      => "gallery.md",
+              "Benchmarks"   => "benchmarks.md",
+              "Internals"    => ["internal/grids.md",
+                                 "internal/fields.md",
+                                 "internal/operators.md"],
+              "Index"        => "subject_index.md"]
 )
 
 deploydocs(repo = "github.com/climate-machine/Oceananigans.jl.git")
