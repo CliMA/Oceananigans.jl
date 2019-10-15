7948a803e3dfc91f38acb3a6b65b94ea0c495b05
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-10-15 06:33:15 -0400

    Move to verification folder and bump up to 128^2
    Former-commit-id: 6ae31ced0957a2825d253c33361e61b6d17f322c

--- verification/lid_driven_cavity/lid_driven_cavity.jl
+++ verification/lid_driven_cavity/lid_driven_cavity.jl
@@ -1,7 +1,7 @@
 using Oceananigans, PyPlot, Printf
 using Oceananigans: NoPenetrationBC, NonDimensionalModel
 
-Nx, Ny, Nz = 1, 64, 64
+Nx, Ny, Nz = 1, 128, 128
 Lx, Ly, Lz = 1, 1, 1
 
 vbcs = ChannelBCs(top    = BoundaryCondition(Value, 1),
@@ -21,14 +21,14 @@ model = NonDimensionalModel(N=(Nx, Ny, Nz), L=(Lx, Ly, Lz), Re=Re, Ri=0, Pr=Inf,
 
 fig, ax = subplots(nrows=1, ncols=1, figsize=(10, 10))
 
-Δt = 1e-3
+Δt = 2e-4
 Δ = max(model.grid.Δy, model.grid.Δz)
 CFL = Δt / Δ
 dCFL = (1/Re) * Δt / Δ^2
 @show CFL
 @show dCFL
 
-while model.clock.time < 5
+# while model.clock.time < 1
     time_step!(model; Δt=Δt, Nt=100, init_with_euler=model.clock.time == 0 ? true : false)
     @printf("Time: %.4f\n", model.clock.time)
 
@@ -44,12 +44,12 @@ while model.clock.time < 5
     ζ = reverse(log10.(abs.(ζ)), dims=1)
 
     # ax.streamplot(y, z, v, w)
-    pcolormesh(y, z, ζ, vmin=1e-2, cmap="inferno")
+    pcolormesh(y, z, ζ, vmin=1e-3, cmap="viridis"); title(); xlabel("y"); ylabel("z");
     # fig.colorbar(im, ax=ax)
 
-    ax.set_title(@sprintf("Time: %.4f", model.clock.time))
+    ax.set_title(@sprintf("Lid-driven cavity vorticity log₁₀(ζ): Re=%d, t=%.4f", Re, model.clock.time))
     ax.set_xlabel("\$y\$"); ax.set_ylabel("\$z\$");
     ax.set_xlim([0, 1]); ax.set_ylim([-1, 0]);
     ax.set_aspect(1)
     gcf();
-end
+# end
