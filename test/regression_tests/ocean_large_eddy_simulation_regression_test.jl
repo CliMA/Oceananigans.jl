5a95ba7cda6efbb2844f819e24e58c0fda6755e5
Gregory Wagner <wagner.greg@gmail.com>
2019-10-16 19:25:46 -0400

    Fixes incorrect old checkpointer initialization for generating test data

--- test/regression_tests/ocean_large_eddy_simulation_regression_test.jl
+++ test/regression_tests/ocean_large_eddy_simulation_regression_test.jl
@@ -1,9 +1,9 @@
 function run_ocean_large_eddy_simulation_regression_test(arch, closure)
     name = "ocean_large_eddy_simulation_" * string(typeof(closure).name.wrapper)
 
-    spinup_steps = 1000
-      test_steps = 100
-              Δt = 10.0
+    spinup_steps = 10000
+      test_steps = 10
+              Δt = 2.0
 
     #=
     # Parameters
@@ -34,8 +34,12 @@ function run_ocean_large_eddy_simulation_regression_test(arch, closure)
     set!(model, u=u₀, w=u₀, T=T₀, S=35)
 
     time_step!(model, spinup_steps-test_steps, Δt)
-    model.output_writers[:checkpointer] = Checkpointer(model; frequency=test_steps, 
-                                                       prefix=name * "_", dir=dirname(@__FILE__))
+
+    model.output_writers[:checkpointer] = Checkpointer(model;
+                                                       frequency = test_steps,
+                                                          prefix = name * "_",
+                                                             dir = joinpath(dirname(@__FILE__), "data")
+                                                      )
                                                        
     time_step!(model, 2test_steps, Δt)
     =#
@@ -46,11 +50,11 @@ function run_ocean_large_eddy_simulation_regression_test(arch, closure)
 
     checkpointed_model = restore_from_checkpoint(joinpath(dirname(@__FILE__), "data", name * "_$(spinup_steps+test_steps).jld2"))
 
-    @test all(Array(data(checkpointed_model.velocities.u) .≈ Array(data(test_model.velocities.u))))
-    @test all(Array(data(checkpointed_model.velocities.v) .≈ Array(data(test_model.velocities.v))))
-    @test all(Array(data(checkpointed_model.velocities.w) .≈ Array(data(test_model.velocities.w))))
-    @test all(Array(data(checkpointed_model.tracers.T)    .≈ Array(data(test_model.tracers.T))))
-    @test all(Array(data(checkpointed_model.tracers.S)    .≈ Array(data(test_model.tracers.S))))
+    @test all(Array(data(checkpointed_model.velocities.u)) .≈ Array(data(test_model.velocities.u)))
+    @test all(Array(data(checkpointed_model.velocities.v)) .≈ Array(data(test_model.velocities.v)))
+    @test all(Array(data(checkpointed_model.velocities.w)) .≈ Array(data(test_model.velocities.w)))
+    @test all(Array(data(checkpointed_model.tracers.T))    .≈ Array(data(test_model.tracers.T)))
+    @test all(Array(data(checkpointed_model.tracers.S))    .≈ Array(data(test_model.tracers.S)))
 
     return nothing
 end
