1e1171f7c106a4bbe94b5ae90fb2b48462fa8def
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-10-18 10:59:33 -0400

    Add `@inline` and `@inbounds`
    Co-Authored-By: Raphael Rousseau-Rizzi <r.rousseaurizzi@gmail.com>

--- src/Operators/weno.jl
+++ src/Operators/weno.jl
@@ -2,17 +2,17 @@
 #### WENO-5 face interpolation functions
 ####
 
-p₀(i, f) =  1/3 * f[i-1] + 5/6 * f[i]   -  1/6 * f[i+1]
-p₁(i, f) = -1/6 * f[i-2] + 5/6 * f[i-1] +  1/3 * f[i]
-p₂(i, f) =  1/3 * f[i-3] + 7/6 * f[i-2] + 11/6 * f[i-1]
+@inline p₀(i, f) = @inbounds  1/3 * f[i-1] + 5/6 * f[i]   -  1/6 * f[i+1]
+@inline p₁(i, f) = @inbounds -1/6 * f[i-2] + 5/6 * f[i-1] +  1/3 * f[i]
+@inline p₂(i, f) = @inbounds  1/3 * f[i-3] + 7/6 * f[i-2] + 11/6 * f[i-1]
 
 ####
 #### WENO-5 weight calculation
 ####
 
-β₀(i, f) = 13/12 * (f[i-1] - 2f[i]   + f[i+1])^2 + 1/4 * (3f[i-1] - 4f[i] + f[i+1])^2
-β₁(i, f) = 13/12 * (f[i-2] - 2f[i-1] +   f[i])^2 + 1/4 * (f[i-2]  - f[i])^2
-β₂(i, f) = 13/12 * (f[i-3] - 2f[i-2] + f[i-1])^2 + 1/4 * (f[i-3]  - 4f[i-2] + 3f[i-1])^2
+@inline β₀(i, f) = @inbounds 13/12 * (f[i-1] - 2f[i]   + f[i+1])^2 + 1/4 * (3f[i-1] - 4f[i] + f[i+1])^2
+@inline β₁(i, f) = @inbounds 13/12 * (f[i-2] - 2f[i-1] +   f[i])^2 + 1/4 * (f[i-2]  - f[i])^2
+@inline β₂(i, f) = @inbounds 13/12 * (f[i-3] - 2f[i-2] + f[i-1])^2 + 1/4 * (f[i-3]  - 4f[i-2] + 3f[i-1])^2
 
 ###
 ### WENO-5 (stencil size 3) optimal weights
@@ -29,9 +29,9 @@ const C3₂ = 1/10
 const ϵ = 1e-6
 const n = 2
 
-α₀(i, f) = C3₀ / (β₀(i, f) + ϵ)^n
-α₁(i, f) = C3₁ / (β₁(i, f) + ϵ)^n
-α₂(i, f) = C3₂ / (β₂(i, f) + ϵ)^n
+@inline α₀(i, f) = C3₀ / (β₀(i, f) + ϵ)^n
+@inline α₁(i, f) = C3₁ / (β₁(i, f) + ϵ)^n
+@inline α₂(i, f) = C3₂ / (β₂(i, f) + ϵ)^n
 
 ####
 #### WENO-5 normalized weights
@@ -48,4 +48,11 @@ function weno_weights(i, f)
     return w₁, w₂, w₂
 end
 
-F(i, f) = w₀(i, f) * p₀(i, f) + w₁(i, f) * p₁(i, f) + w₂(i, f) * p₂(i, f)
+####
+#### WENO-5 flux reconstruction
+####
+
+function F(i, f)
+    w₁, w₂, w₃ = weno_weights(i, f)
+    return w₀ * p₀(i, f) + w₁ * p₁(i, f) + w₂ * p₂(i, f)
+end
