4844e65c401dd1323a32fc07000ba0915cf8be11
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-10-20 13:11:59 -0400

    Functions after (i, j, k, grid) to allow for double composition
    Former-commit-id: d197439b661040288f9e139f3c4432320f2a6493

--- src/operators/differences.jl
+++ src/operators/differences.jl
@@ -41,25 +41,24 @@
 #### Derivative operators acting on functions
 ####
 
-@inline ∂x_caa(f::F, i, j, k, grid, args...) where F<:Function = (f(i+1, j, k, grid, args...) - f(i,   j, k, grid, args...)) / Δx(i, j, k, grid)
-@inline ∂x_faa(f::F, i, j, k, grid, args...) where F<:Function = (f(i,   j, k, grid, args...) - f(i-1, j, k, grid, args...)) / Δx(i, j, k, grid)
+@inline ∂x_caa(i, j, k, grid, f::F, args...) where F<:Function = (f(i+1, j, k, grid, args...) - f(i,   j, k, grid, args...)) / Δx(i, j, k, grid)
+@inline ∂x_faa(i, j, k, grid, f::F, args...) where F<:Function = (f(i,   j, k, grid, args...) - f(i-1, j, k, grid, args...)) / Δx(i, j, k, grid)
 
-@inline ∂y_aca(f::F, i, j, k, grid, args...) where F<:Function = (f(i, j+1, k, grid, args...) - f(i, j,   k, grid, args...)) / Δy(i, j, k, grid)
-@inline ∂y_afa(f::F, i, j, k, grid, args...) where F<:Function = (f(i, j,   k, grid, args...) - f(i, j-1, k, grid, args...)) / Δy(i, j, k, grid)
+@inline ∂y_aca(i, j, k, grid, f::F, args...) where F<:Function = (f(i, j+1, k, grid, args...) - f(i, j,   k, grid, args...)) / Δy(i, j, k, grid)
+@inline ∂y_afa(i, j, k, grid, f::F, args...) where F<:Function = (f(i, j,   k, grid, args...) - f(i, j-1, k, grid, args...)) / Δy(i, j, k, grid)
 
-@inline ∂z_aac(f::F, i, j, k, grid, args...) where F<:Function = (f(i, j, k+1, grid, args...) - f(i, j, k,   grid, args...)) / ΔzF(i, j, k, grid)
-@inline ∂z_aaf(f::F, i, j, k, grid, args...) where F<:Function = (f(i, j, k,   grid, args...) - f(i, j, k-1, grid, args...)) / ΔzC(i, j, k, grid)
+@inline ∂z_aac(i, j, k, grid, f::F, args...) where F<:Function = (f(i, j, k+1, grid, args...) - f(i, j, k,   grid, args...)) / ΔzF(i, j, k, grid)
+@inline ∂z_aaf(i, j, k, grid, f::F, args...) where F<:Function = (f(i, j, k,   grid, args...) - f(i, j, k-1, grid, args...)) / ΔzC(i, j, k, grid)
 
 ####
 #### Second derivatives
 ####
 
-@inline ∂x²_caa(i, j, k, grid, c) = ∂x_caa(∂x_faa, i, j, k, grid, c)
-@inline ∂x²_faa(i, j, k, grid, u) = ∂x_faa(∂x_caa, i, j, k, grid, u)
+@inline ∂x²_caa(i, j, k, grid, c) = ∂x_caa(i, j, k, grid, ∂x_faa, c)
+@inline ∂x²_faa(i, j, k, grid, u) = ∂x_faa(i, j, k, grid, ∂x_caa, u)
 
-@inline ∂y²_aca(i, j, k, grid, c) = ∂y_aca(∂y_afa, i, j, k, grid, c)
-@inline ∂y²_afa(i, j, k, grid, v) = ∂y_afa(∂y_aca, i, j, k, grid, v)
+@inline ∂y²_aca(i, j, k, grid, c) = ∂y_aca(i, j, k, grid, ∂y_afa, c)
+@inline ∂y²_afa(i, j, k, grid, v) = ∂y_afa(i, j, k, grid, ∂y_aca, v)
 
-@inline ∂z²_aac(i, j, k, grid, c) = ∂z_aac(∂z_aaf, i, j, k, grid, c)
-@inline ∂z²_aaf(i, j, k, grid, w) = ∂z_aaf(∂z_aac, i, j, k, grid, w)
-
+@inline ∂z²_aac(i, j, k, grid, c) = ∂z_aac(i, j, k, grid, ∂z_aaf, c)
+@inline ∂z²_aaf(i, j, k, grid, w) = ∂z_aaf(i, j, k, grid, ∂z_aac, w)
