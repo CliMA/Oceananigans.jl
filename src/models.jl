fc9321cae611eef6bdd974197e29ffa9b138af64
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-10-20 10:57:58 -0400

    Add mixing ratio and perturbation fields

--- src/models.jl
+++ src/models.jl
@@ -1,23 +1,27 @@
 using Oceananigans
 
-mutable struct CompressibleModel{A, FT, G, M, P, T, F, R} <: AbstractModel
+mutable struct CompressibleModel{A, FT, G, M, E, ρ, T, MR, F, R, P} <: AbstractModel
     architecture     :: A
     grid             :: G
     surface_pressure :: FT
     momenta          :: M
-    densities        :: P
-    tracers          :: T
+    entropy          :: E
+    densities        :: ρ
+    mixing_ratios    :: MR
     slow_forcings    :: F
     fast_forcings    :: R
+    perturbations    :: P
 end
 
 function CompressibleModel(; architecture=CPU(), grid, surface_pressure = 100000)
-    Ũ = MomentumFields(arch, grid) 
+    Ũ = MomentumFields(arch, grid)
+    S = CellField(arch, grid)
     ρ = DensityFields(arch, grid)
-    C = TracerFields(arch, grid)
+    Q = MixingRatioFields(arch, grid)
     F = SlowForcingFields(arch, grid)
     R = FastForcingFields(arch, grid)
-    return CompressibleModel(architecture, grid, surface_pressure, Ũ, ρ, C, F, R)
+    P = PerturbationFields(arch, grid)
+    return CompressibleModel(architecture, grid, surface_pressure, Ũ, S, ρ, Q, F, R, P)
 end
 
 function MomentumFields(arch, grid)
@@ -33,12 +37,11 @@ function DensityFields(arch, grid)
     return (d=ρd, m=ρm)
 end
 
-function TracerFields(arch, grid)
-    S = CellField(arch, grid)
+function MixingRatioFields(arch, grid)
     Qv = CellField(arch, grid)
     Ql = CellField(arch, grid)
     Qi = CellField(arch, grid)
-    return (S=S, Qv=Qv, Ql=Ql, Qi=Qi)
+    return (Qv=Qv, Ql=Ql, Qi=Qi)
 end
 
 function SlowForcingFields(arch, grid)
@@ -64,4 +67,11 @@ function FastForcingFields(arch, grid)
     return (U=U, V=V, W=W, ρd=ρd, S=S, Qv=Qv, Ql=Ql, Qi=Qi)
 end
 
-function 
+function PerturbationFields(arch, grid)
+    U″ = FaceFieldX(arch, grid)
+    V″ = FaceFieldY(arch, grid)
+    W″ = FaceFieldZ(arch, grid)
+    S″ = CellField(arch, grid)
+    ρ″ = CellField(arch, grid)
+    return (U=U″, V=V″, W=W″, S=S″, ρ=ρ″)
+end
