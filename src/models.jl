98010e90b14a824c93087cefcced499afe486766
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2019-10-23 13:50:49 -0400

    Moves ordered_dict_show to utils
    Former-commit-id: 0fda5e5965cb7f2642dc08d7aee078249bea2790

--- src/models.jl
+++ src/models.jl
@@ -84,23 +84,7 @@ function Model(;
                  poisson_solver, diffusivities, output_writers, diagnostics, parameters)
 end
 
-function ordered_dict_show(dict, padchar) 
-    if length(dict) == 0
-        return string(typeof(dict), " with no entries")
-    elseif length(dict) == 1
-        return string(typeof(dict), " with 1 entry:", '\n',
-                      padchar, "   └── ", dict.keys[1], " => ", typeof(dict.vals[1]))
-    else
-        return string(typeof(dict), " with $(length(dict)) entries:", '\n',
-                      Tuple(string(padchar, 
-                                   "   ├── ", name, " => ", typeof(dict[name]), '\n') 
-                            for name in dict.keys[1:end-1]
-                           )...,
-                           padchar, "   └── ", dict.keys[end], " => ", typeof(dict.vals[end])
-                      )
-    end
-end
-
+"""Show the innards of a `Model` in the REPL."""
 Base.show(io::IO, model::Model) =
     print(io,
               "Oceananigans.Model on a ", typeof(model.architecture), " architecture (time = ", 
