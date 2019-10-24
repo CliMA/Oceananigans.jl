27c86e1d3dee55ecd4b275f8d64ab26d399bef7d
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2019-10-24 19:15:33 -0400

    Use new name for _compute_w_from...

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,7 +18,7 @@ using GPUifyLoops: @launch, @loop
 using NCDatasets: Dataset
 
 using Oceananigans: PoissonSolver, PPN, PNN, solve_poisson_3d!,
-                    velocity_div!, compute_w_from_continuity!,
+                    velocity_div!, _compute_w_from_continuity!,
                     launch_config, datatuples, device, with_tracers,
                     parentdata, fill_halo_regions!, run_diagnostic,
                     TracerFields, buoyancy_frequency_squared, thermal_expansion, haline_contraction, ρ′,
