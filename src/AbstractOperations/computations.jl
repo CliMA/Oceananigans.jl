abefe0c4526f3d3531f67b9f055257db6a28b3b4
Gregory Wagner <wagner.greg@gmail.com>
2019-10-23 23:08:41 -0400

    Docstrings for Computations
    Former-commit-id: 9d66c3e715b895bf224909b58457ecfec395c482

--- src/AbstractOperations/computations.jl
+++ src/AbstractOperations/computations.jl
@@ -1,3 +1,8 @@
+"""
+    Computation{R, T, O, G}
+
+Represents an operation performed over the elements of a field.
+"""
 struct Computation{R, T, O, G}
       operation :: O
          result :: R
@@ -5,12 +10,25 @@ struct Computation{R, T, O, G}
     return_type :: T
 end
 
-Computation(op, result; return_type=Array) = Computation(op, result, op.grid, return_type)
+"""
+    Computation(operation, result; return_type=Array)
+
+Returns a `Computation` representing an `operation` performed over the elements of `operation.grid`
+and stored in `result`. `return_type` specifies the output type when the `Computation` instances
+is called as a function.
+"""
+Computation(operation, result; return_type=Array) = 
+    Computation(operation, result, operation.grid, return_type)
 
 architecture(comp::Computation) = architecture(comp.result)
-Base.parent(comp::Computation) = comp
+Base.parent(comp::Computation) = comp # this enables taking a "horizontal average" of a computation
 
-function compute!(comp::Computation)
+"""
+    compute!(computation::Computation)
+
+Perform a `computation`. The result is stored in `computation.result`.
+"""
+function compute!(computation::Computation)
     arch = architecture(comp.result)
     result_data = data(comp.result)
     @launch device(arch) config=launch_config(comp.grid, 3) _compute!(result_data,
@@ -19,6 +37,7 @@ function compute!(comp::Computation)
     return nothing
 end
 
+"""Compute an `operation` over `grid` and store in `result`."""
 function _compute!(result, grid, operation)
     @loop for k in (1:grid.Nz; (blockIdx().z - 1) * blockDim().z + threadIdx().z)
         @loop for j in (1:grid.Ny; (blockIdx().y - 1) * blockDim().y + threadIdx().y)
@@ -30,28 +49,51 @@ function _compute!(result, grid, operation)
     return nothing
 end
 
-function (comp::Computation{<:Array, <:Field})(args...)
-    compute!(comp)
-    return comp.return_type(interior(comp.result))
+"""
+    (computation::Computation)(args...)
+
+Performs the `compute(computation)` and returns the result converted to type `computationutation.return_type`.
+"""
+function (computation::Computation)(args...)
+    computationute!(computation)
+    return computation.return_type(interior(computation.result))
 end
 
-function (comp::Computation{<:Nothing})(args...)
-    compute!(comp)
-    return comp.result
+"""
+    (computation::Computation{<:Nothing})(args...)
+
+Performs `compute!(computation)` and returns `computation.result`.
+"""
+function (computation::Computation{<:Nothing})(args...)
+    computationute!(computation)
+    return computation.result
 end
 
 #####
 ##### Functionality for using computations with HorizontalAverage
 #####
 
+"""
+    HorizontalAverage(op::AbstractOperation, result; kwargs...)
+
+Returns the representation of a `HorizontalAverage` over the operation `op`, using
+`result` as a temporary array to store the result of `operation` computed on `op.grid`.
+"""
 function HorizontalAverage(op::AbstractOperation, result; kwargs...)
     computation = Computation(op, result)
     return HorizontalAverage(computation; kwargs...)
 end
 
+"""
+    HorizontalAverage(op::AbstractOperation, model; kwargs...)
+
+Returns the representation of a `HorizontalAverage` over the operation `op`, using
+`model.pressures.pHY′` as a temporary array to store the result of `operation` computed on `op.grid`.
+"""
 HorizontalAverage(op::AbstractOperation, model::AbstractModel; kwargs...) = 
     HorizontalAverage(op, model.pressures.pHY′; kwargs...)
 
+"""Compute the horizontal average of a computation."""
 function run_diagnostic(model, havg::HorizontalAverage{<:Computation})
     compute!(havg.field)
     zero_halo_regions!(parent(havg.field.result), model.grid)
