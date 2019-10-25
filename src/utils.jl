07b6fd11bac009be0e70bda36cfccab57cab8ee9
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2019-10-24 20:20:41 -0400

    No more type piracy on NamedTuples for range indexing
    Former-commit-id: 55696deb188118a23ed37e36510ec239ce509dce

--- src/utils.jl
+++ src/utils.jl
@@ -202,21 +202,6 @@ parenttuple(obj) = Tuple(f.data.parent for f in obj)
 @inline datatuple(obj::NamedTuple) = NamedTuple{propertynames(obj)}(datatuple(o) for o in obj)
 @inline datatuples(objs...) = (datatuple(obj) for obj in objs)
 
-function getindex(t::NamedTuple, r::AbstractUnitRange{<:Real})
-    n = length(r)
-    n == 0 && return NamedTuple()
-    elems = Vector{eltype(t)}(undef, n)
-    names = Vector{Symbol}(undef, n)
-    o = first(r) - 1
-    for i = 1:n
-        elem = t[o + i]
-        name = propertynames(t)[o + i]
-        @inbounds elems[i] = elem
-        @inbounds names[i] = name
-    end
-    NamedTuple{Tuple(names)}(Tuple(elems))
-end
-
 ####
 #### Dynamic launch configuration
 ####
