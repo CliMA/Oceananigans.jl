344f3a58cc3865414251a02e15d1489f0b23beaf
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-10-25 10:13:57 -0400

    Update areas and volumes to use aaf-type notation

--- src/Operators/areas_and_volumes.jl
+++ src/Operators/areas_and_volumes.jl
@@ -6,34 +6,30 @@
 @inline Δy(i, j, k, grid) = grid.Δy
 
 @inline ΔzC(i, j, k, grid::RegularCartesianGrid) = grid.Δz
-@inline ΔzC(i, j, k, grid::VerticallyStretchedCartesianGrid) = @inbounds grid.ΔzC[k]
+# @inline ΔzC(i, j, k, grid::VerticallyStretchedCartesianGrid) = @inbounds grid.ΔzC[k]
 
 @inline ΔzF(i, j, k, grid::RegularCartesianGrid) = grid.Δz
-@inline ΔzF(i, j, k, grid::VerticallyStretchedCartesianGrid) = @inbounds grid.ΔzF[k]
+# @inline ΔzF(i, j, k, grid::VerticallyStretchedCartesianGrid) = @inbounds grid.ΔzF[k]
 
 ####
 #### Areas
 ####
 
-# Area of cell faces surrounding cell center and cell face (i, j, k) in the x-direction.
-@inline AxC(i, j, k, grid) = Δy(i, j, k, grid) * ΔzC(i, j, k, grid)
-@inline AxF(i, j, k, grid) = Δy(i, j, k, grid) * ΔzF(i, j, k, grid)
+@inline Ax_aac(i, j, k, grid) = Δy(i, j, k, grid) * ΔzF(i, j, k, grid)
+@inline Ax_aaf(i, j, k, grid) = Δy(i, j, k, grid) * ΔzC(i, j, k, grid)
 
-# Area of cell faces surrounding cell center and cell face (i, j, k) in the y-direction.
-@inline AyC(i, j, k, grid) = Δx(i, j, k, grid) * ΔzC(i, j, k, grid)
-@inline AyF(i, j, k, grid) = Δx(i, j, k, grid) * ΔzF(i, j, k, grid)
+@inline Ay_aac(i, j, k, grid) = Δx(i, j, k, grid) * ΔzF(i, j, k, grid)
+@inline Ay_aaf(i, j, k, grid) = Δx(i, j, k, grid) * ΔzC(i, j, k, grid)
 
-# Area of cell faces surrounding cell center and cell face (i, j, k) in the z-direction.
 @inline Az(i, j, k, grid) = Δx(i, j, k, grid) * Δy(i, j, k, grid)
 
 ####
 #### Volumes
 ####
 
-@inline VF(i, j, k, grid) = ΔxF(i, j, k, grid) * ΔyF(i, j, k, grid) * Δz(i, j, k, grid)
-@inline VC(i, j, k, grid) = ΔxC(i, j, k, grid) * ΔyC(i, j, k, grid) * Δz(i, j, k, grid)
+@inline V_aac(i, j, k, grid) = Δx(i, j, k, grid) * Δy(i, j, k, grid) * ΔzF(i, j, k, grid)
+@inline V_aaf(i, j, k, grid) = Δx(i, j, k, grid) * Δy(i, j, k, grid) * ΔzC(i, j, k, grid)
 
-@inline Vᵘ(i, j, k, grid::AbstractGrid{FT}) where FT = FT(0.5) * (VF(i, j, k, grid) + VF(i+1, j, k, grid))
-@inline Vᵛ(i, j, k, grid::AbstractGrid{FT}) where FT = FT(0.5) * (VF(i, j, k, grid) + VF(i, j+1, k, grid))
-@inline Vʷ(i, j, k, grid::AbstractGrid{FT}) where FT = FT(0.5) * (VF(i, j, k, grid) + VF(i, j, k+1, grid))
-
+const Vᵘ = V_aac
+const Vᵛ = V_aac
+const Vʷ = V_aaf
