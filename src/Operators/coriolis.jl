c419ece6a50fb58322ecdfac14509ea4958bbee1
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-10-26 15:08:54 -0400

    Fix dumb typos
    Former-commit-id: e63d8971eb1943fd0c8df6f81d7004a9f944b845

--- src/Operators/coriolis.jl
+++ src/Operators/coriolis.jl
@@ -5,6 +5,6 @@
 @inline fv̅ʸ(i, j, k, grid, v, f) = f * ℑy_aca(i, j, k, grid, v)
 @inline fu̅ˣ(i, j, k, grid, u, f) = f * ℑx_caa(i, j, k, grid, u)
 
-@inline fv(i, j, k, grid, v, f) = ℑx_faa(i, j, k, grid, fVv̅ʸ, v, f)
-@inline fu(i, j, k, grid, u, f) = ℑy_afa(i, j, k, grid, fVu̅ˣ, u, f)
+@inline fv(i, j, k, grid, v, f) = ℑx_faa(i, j, k, grid, fv̅ʸ, v, f)
+@inline fu(i, j, k, grid, u, f) = ℑy_afa(i, j, k, grid, fu̅ˣ, u, f)
 
