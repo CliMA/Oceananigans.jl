b89918ec4b0505cf2b46360560117c6daa079611
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-10-27 15:47:32 -0400

    Bit more structure for time stepping

--- src/time_stepping.jl
+++ src/time_stepping.jl
@@ -1,24 +1,51 @@
+####
+#### Utilities for acoustic time stepping
+####
+
+function acoustic_time_steps(rk3_iter, nₛ, Δt)
+    rk3_iter == 1 && return 1,         Δt/3
+    rk3_iter == 2 && return Int(nₛ/2), (Δt/2)/(nₛ/2)
+    rk3_iter == 3 && return nₛ,        Δt/nₛ
+end
+
+acoustic_time_stepping!(args...) = nothing
+
+####
+#### Time-stepping algorithm
+####
+
 function time_step!(model; Δt, nₛ)
     arch = model.arch
     grid = model.grid
 
     Ũ = model.momenta
     ρ = model.densities
-    C = model.tracers
     F = model.slow_forcings
     R = model.fast_forcings
 
-    compute_slow_forcings!(F, Ũ, ρ)
-    compute_fast_forcings!(R, Ũ, ρ, F)
-    acoustic_time_stepping!(Ũ, ρ, C, F, R; n=1, Δτ=Δt/3)
-    scalar_transport!(C, Ũ, args...)
-    compute_fast_forcings!(R, Ũ, ρ, F)
-    acoustic_time_stepping!(Ũ, ρ, C, F, R; n=Int(nₛ/2), Δτ=Δt/nₛ)
-    scalar_transport!(C, Ũ, args...)
-    compute_fast_forcings!(R, Ũ, ρ, F)
-    acoustic_time_stepping!(Ũ, ρ, C, F, R; n=nₛ, Δτ=Δt/nₛ)
-    scalar_transport!(C, Ũ, args...)
+    C = model.tracers
+    S = model.tracers.S
+
+    # TODO: Fill halo regions for U, V, W, C
+    compute_slow_forcings!(F, Ũ, ρ, C)
+
+    # RK3 time-stepping
+    for rk3_iter in 1:3
+        # TODO: Fill halo regions for U, V, W, ρ, C
+        compute_fast_forcings!(R, Ũ, ρ, F)
+
+        n, Δτ = acoustic_time_steps(rk3_iter)
+        acoustic_time_stepping!(Ũ, ρ, C, F, R; n=n, Δτ=Δτ)
+
+        scalar_transport!(C, Ũ, args...)
+    end
+
+    return nothing
 end
+
+####
+#### Calculation of "slow" forcings
+####
 
 @inline FU(i, j, k, ρ, Ũ) = @inbounds -(ρ.d[i, j, k] / ρ.m[i, j, k]) * x_f_cross_U(i, j, k, grid, rotation, Ũ) + μ∇²u(i, j, k, Ũ.U)
 @inline FV(i, j, k, ρ, Ũ) = @inbounds -(ρ.d[i, j, k] / ρ.m[i, j, k]) * y_f_cross_U(i, j, k, grid, rotation, Ũ) + μ∇²v(i, j, k, Ũ.V)
@@ -26,6 +53,9 @@ end
 
 @inline FC(i, j, k, C) = κ∇²(i, j, k, C)
 
+"""
+Slow forcings include viscous dissipation, diffusion, and Coriolis terms.
+"""
 function compute_slow_forcings!(F, Ũ, ρ, C)
     for k in 1:grid.Nz, j in 1:grid.Ny, i in 1:grid.Nx
         @inbounds F.U[i, j, k] = FU(i, j, k, ρ, Ũ)
@@ -39,9 +69,11 @@ function compute_slow_forcings!(F, Ũ, ρ, C)
     end
 end
 
-# TODO
-@inline buoyancy_perturbation(i, j, k, buoyancy::DryIdealGasBuoyancy, args...)   = nothing
-@inline buoyancy_perturbation(i, j, k, buoyancy::MoistIdealGasBuoyancy, args...) = nothing
+####
+#### Calculation of "fast" forcings
+####
+
+@inline buoyancy_perturbation(i, j, k, buoyancy::DryIdealGas, args...) = nothing
 
 @inline RU(i, j, k, Ũ, ρ, S, FU) = @inbounds -∇ρũu(i, j, k, Ũ) - (ρ.d[i, j, k]/ρ.m[i, j, k]) * ∂x_p′(i, j, k, S, ρ) + FU[i, j, k]
 @inline RV(i, j, k, Ũ, ρ, S, FU) = @inbounds -∇ρũv(i, j, k, Ũ) - (ρ.d[i, j, k]/ρ.m[i, j, k]) * ∂y_p′(i, j, k, S, ρ) + FV[i, j, k]
@@ -51,6 +83,9 @@ end
 
 @inline RC(i, j, k, Ũ, C) = -div_flux(i, j, k, Ũ, C)
 
+"""
+Fast forcings include advection, pressure gradient, and buoyancy terms.
+"""
 function compute_fast_forcings!(R, Ũ, ρ, F)
     for k in 1:grid.Nz, j in 1:grid.Ny, i in 1:grid.Nx
         @inbounds R.U[i, j, k] = RU(i, j, k, ρ, Ũ)
