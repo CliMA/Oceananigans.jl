c2e90b24df9abfd9401fb9837025d700fad5003e
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-10-27 09:42:45 -0400

    Integrate Solvers submodule.
    Former-commit-id: 0a756d5116ede9b70bffea3285497e581cf5ea6b

--- src/Solvers/Solvers.jl
+++ src/Solvers/Solvers.jl
@@ -1,9 +1,12 @@
 module Solvers
 
-using Oceananigans.Grids
+export
+    PoissonSolver, PoissonBCs, solve_poisson_3d!
 
 abstract type PoissonBCs end
 
 include("utils.jl")
+include("poisson_solver_cpu.jl")
+include("poisson_solver_gpu.jl")
 
 end
