7536ea7efe9251c59c44d02372c9a63adbe43a9c
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-10-28 07:25:53 -0400

    Fix stuff based on PR review

--- src/Operators/coriolis.jl
+++ src/Operators/coriolis.jl
@@ -2,9 +2,6 @@
 #### Coriolis terms following Marshall et al. (1997) equation (25).
 ####
 
-@inline fv̅ʸ(i, j, k, grid, v, f) = f * ℑy_aca(i, j, k, grid, v)
-@inline fu̅ˣ(i, j, k, grid, u, f) = f * ℑx_caa(i, j, k, grid, u)
-
-@inline fv(i, j, k, grid, v, f) = ℑx_faa(i, j, k, grid, fv̅ʸ, v, f)
-@inline fu(i, j, k, grid, u, f) = ℑy_afa(i, j, k, grid, fu̅ˣ, u, f)
+@inline fv(i, j, k, grid, v, f) = f * ℑxy_fca(i, j, k, grid, v)
+@inline fu(i, j, k, grid, u, f) = f * ℑxy_cfa(i, j, k, grid, u)
 
--- src/Operators/interpolation.jl
+++ src/Operators/interpolation.jl
@@ -2,33 +2,34 @@
 #### Convinient aliases
 ####
 
+const AG  = AbstractGrid
 const RCG = RegularCartesianGrid
 
 ####
 #### Base interpolation operators
 ####
 
-@inline ℑx_caa(i, j, k, grid::RCG{FT}, u) where FT = @inbounds FT(0.5) * (u[i,   j, k] + u[i+1, j, k])
-@inline ℑx_faa(i, j, k, grid::RCG{FT}, c) where FT = @inbounds FT(0.5) * (c[i-1, j, k] + c[i,   j, k])
+@inline ℑx_caa(i, j, k, grid::AG{FT}, u) where FT = @inbounds FT(0.5) * (u[i,   j, k] + u[i+1, j, k])
+@inline ℑx_faa(i, j, k, grid::AG{FT}, c) where FT = @inbounds FT(0.5) * (c[i-1, j, k] + c[i,   j, k])
 
-@inline ℑy_aca(i, j, k, grid::RCG{FT}, v) where FT = @inbounds FT(0.5) * (v[i, j,   k] + v[i,  j+1, k])
-@inline ℑy_afa(i, j, k, grid::RCG{FT}, c) where FT = @inbounds FT(0.5) * (c[i, j-1, k] + c[i,  j,   k])
+@inline ℑy_aca(i, j, k, grid::AG{FT}, v) where FT = @inbounds FT(0.5) * (v[i, j,   k] + v[i,  j+1, k])
+@inline ℑy_afa(i, j, k, grid::AG{FT}, c) where FT = @inbounds FT(0.5) * (c[i, j-1, k] + c[i,  j,   k])
 
-@inline ℑz_aac(i, j, k, grid::RCG{FT}, w) where FT = @inbounds FT(0.5) * (w[i, j,   k] + w[i, j, k+1])
-@inline ℑz_aaf(i, j, k, grid::RCG{FT}, c) where FT = @inbounds FT(0.5) * (c[i, j, k-1] + c[i, j,   k])
+@inline ℑz_aac(i, j, k, grid::AG{FT}, w) where FT = @inbounds FT(0.5) * (w[i, j,   k] + w[i, j, k+1])
+@inline ℑz_aaf(i, j, k, grid::AG{FT}, c) where FT = @inbounds FT(0.5) * (c[i, j, k-1] + c[i, j,   k])
 
 ####
 #### Interpolation operators acting on functions
 ####
 
-@inline ℑx_caa(i, j, k, grid::RCG{FT}, f::F, args...) where {FT, F<:Function} = FT(0.5) * (f(i,   j, k, grid, args...) + f(i+1, j, k, grid, args...))
-@inline ℑx_faa(i, j, k, grid::RCG{FT}, f::F, args...) where {FT, F<:Function} = FT(0.5) * (f(i-1, j, k, grid, args...) + f(i,   j, k, grid, args...))
+@inline ℑx_caa(i, j, k, grid::AG{FT}, f::F, args...) where {FT, F<:Function} = FT(0.5) * (f(i,   j, k, grid, args...) + f(i+1, j, k, grid, args...))
+@inline ℑx_faa(i, j, k, grid::AG{FT}, f::F, args...) where {FT, F<:Function} = FT(0.5) * (f(i-1, j, k, grid, args...) + f(i,   j, k, grid, args...))
 
-@inline ℑy_aca(i, j, k, grid::RCG{FT}, f::F, args...) where {FT, F<:Function} = FT(0.5) * (f(i, j,   k, grid, args...) + f(i, j+1, k, grid, args...))
-@inline ℑy_afa(i, j, k, grid::RCG{FT}, f::F, args...) where {FT, F<:Function} = FT(0.5) * (f(i, j-1, k, grid, args...) + f(i, j,   k, grid, args...))
+@inline ℑy_aca(i, j, k, grid::AG{FT}, f::F, args...) where {FT, F<:Function} = FT(0.5) * (f(i, j,   k, grid, args...) + f(i, j+1, k, grid, args...))
+@inline ℑy_afa(i, j, k, grid::AG{FT}, f::F, args...) where {FT, F<:Function} = FT(0.5) * (f(i, j-1, k, grid, args...) + f(i, j,   k, grid, args...))
 
-@inline ℑz_aac(i, j, k, grid::RCG{FT}, f::F, args...) where {FT, F<:Function} = FT(0.5) * (f(i, j, k,   grid, args...) + f(i, j, k+1, grid, args...))
-@inline ℑz_aaf(i, j, k, grid::RCG{FT}, f::F, args...) where {FT, F<:Function} = FT(0.5) * (f(i, j, k-1, grid, args...) + f(i, j, k,   grid, args...))
+@inline ℑz_aac(i, j, k, grid::AG{FT}, f::F, args...) where {FT, F<:Function} = FT(0.5) * (f(i, j, k,   grid, args...) + f(i, j, k+1, grid, args...))
+@inline ℑz_aaf(i, j, k, grid::AG{FT}, f::F, args...) where {FT, F<:Function} = FT(0.5) * (f(i, j, k-1, grid, args...) + f(i, j, k,   grid, args...))
 
 ####
 #### "Flux interpolation" operators of the form ℑ(A*f) where A is an area and f is an array.
