7d96baaa52a8f15640b9677520df5b22f06fe41f
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-10-28 08:07:46 -0400

    tracer_flux -> advective_tracer_flux
    Former-commit-id: 07871ad4c21f57888a76d5ffeede40ccd01e2d3c

--- src/Operators/tracer_advection_operators.jl
+++ src/Operators/tracer_advection_operators.jl
@@ -1,8 +1,8 @@
 # Calculate the flux of a tracer quantity c through the faces of a cell.
 # In this case, the fluxes are given by u*Ax*T̅ˣ, v*Ay*T̅ʸ, and w*Az*T̅ᶻ.
-@inline tracer_flux_x(i, j, k, grid, u, c) = Ax_u(i, j, k, grid, u) * ℑxᶠᵃᵃ(i, j, k, grid, c)
-@inline tracer_flux_y(i, j, k, grid, v, c) = Ay_v(i, j, k, grid, v) * ℑyᵃᶠᵃ(i, j, k, grid, c)
-@inline tracer_flux_z(i, j, k, grid, w, c) = Az_w(i, j, k, grid, w) * ℑzᵃᵃᶠ(i, j, k, grid, c)
+@inline advective_tracer_flux_x(i, j, k, grid, u, c) = Ax_u(i, j, k, grid, u) * ℑxᶠᵃᵃ(i, j, k, grid, c)
+@inline advective_tracer_flux_y(i, j, k, grid, v, c) = Ay_v(i, j, k, grid, v) * ℑyᵃᶠᵃ(i, j, k, grid, c)
+@inline advective_tracer_flux_z(i, j, k, grid, w, c) = Az_w(i, j, k, grid, w) * ℑzᵃᵃᶠ(i, j, k, grid, c)
 
 """
     div_flux(i, j, k, grid, U, c)
@@ -15,8 +15,8 @@ a velocity field U = (u, v, w), ∇·(Uc),
 which will end up at the location `ccc`.
 """
 @inline function div_flux(i, j, k, grid, u, v, w, c)
-    1/Vᵃᵃᶜ(i, j, k, grid) * (δxᶜᵃᵃ(i, j, k, grid, tracer_flux_x, u, c) +
-                             δyᵃᶜᵃ(i, j, k, grid, tracer_flux_y, v, c) +
-                             δzᵃᵃᶜ(i, j, k, grid, tracer_flux_z, w, c))
+    1/Vᵃᵃᶜ(i, j, k, grid) * (δxᶜᵃᵃ(i, j, k, grid, advective_tracer_flux_x, u, c) +
+                             δyᵃᶜᵃ(i, j, k, grid, advective_tracer_flux_y, v, c) +
+                             δzᵃᵃᶜ(i, j, k, grid, advective_tracer_flux_z, w, c))
 end
 
