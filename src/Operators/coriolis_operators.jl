af3040d93e99ebd04bc27e08115076e4053c2f5c
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-10-28 20:57:53 -0400

    Nuke Coriolis operators
    Former-commit-id: ae4426c8dfdd29c39638fe9fa9375003d6f19566

--- src/Operators/coriolis_operators.jl
+++
@@ -1,7 +0,0 @@
-####
-#### Coriolis terms following Marshall et al. (1997) equation (25).
-####
-
-@inline fv(i, j, k, grid, v, f) = f * ℑxyᶠᶜᵃ(i, j, k, grid, v)
-@inline fu(i, j, k, grid, u, f) = f * ℑxyᶜᶠᵃ(i, j, k, grid, u)
-
