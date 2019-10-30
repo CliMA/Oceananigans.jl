af1bdf1b8be86d7aeee0c9375273d70623ff4f8e
Gregory Wagner <wagner.greg@gmail.com>
2019-10-30 09:34:17 -0400

    Implements utilities and kernel functions for tupled closures
    Former-commit-id: 5297e136e0e933624279724ff9dcc5e5899df372

--- src/TurbulenceClosures/TurbulenceClosures.jl
+++ src/TurbulenceClosures/TurbulenceClosures.jl
@@ -92,6 +92,57 @@ dissipation principle with model parameters stored as properties of type `FT`.
 abstract type AbstractAnisotropicMinimumDissipation{FT} <: IsotropicDiffusivity{FT} end
 
 ####
+#### 'Tupled closure' implementation
+####
+
+for stress_div in (:∂ⱼ_2ν_Σ₁ⱼ, :∂ⱼ_2ν_Σ₂ⱼ, :∂ⱼ_2ν_Σ₃ⱼ)
+    @eval begin
+        @inline function $stress_div(i, j, k, grid::AbstractGrid{FT}, closure_tuple::Tuple, U, 
+                                     K_tuple, args...) where FT
+
+            stress_div_ijk = zero(FT)
+
+            ntuple(Val(length(closure_tuple))) do α
+                @inbounds closure = closure_tuple[α]
+                @inbounds K = K_tuple[α]
+                stress_div_ijk += $stress_div(i, j, k, grid, closure, U, K, args...)
+            end
+
+            return stress_div_ijk
+        end
+    end
+end
+
+@inline function ∇_κ_∇c(i, j, k, grid::AbstractGrid{FT}, closure_tuple::Tuple, 
+                        c, tracer_index, K_tuple, args...) where FT
+    flux_div_ijk = zero(FT)
+
+    ntuple(Val(length(closure_tuple))) do α
+        @inbounds closure = closure_tuple[α]
+        @inbounds K = K_tuple[α]
+        flux_div_ijk +=  ∇_κ_∇c(i, j, k, grid, closure, c, tracer_index, K, args...)
+    end
+
+    return flux_div_ijk
+end
+
+function calculate_diffusivities!(K_tuple::Tuple, arch, grid, closure_tuple::Tuple, args...)
+    ntuple(Val(length(closure_tuple))) do α
+        @inbounds closure = closure_tuple[α]
+        @inbounds K = K_tuple[α]
+        calculate_diffusivities!(K, arch, grid, closure, args...)
+    end
+
+    return nothing
+end
+
+TurbulentDiffusivities(arch::AbstractArchitecture, grid::AbstractGrid, tracers, closure_tuple::Tuple) =
+    Tuple(TurbulentDiffusivities(arch, grid, tracers, closure) for closure in closure_tuple)
+
+with_tracers(tracers, closure_tuple::Tuple) =
+    Tuple(with_tracers(tracers, closure) for closure in closure_tuple)
+
+####
 #### Include module code
 ####
 
