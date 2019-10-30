1aa2fd93065035202ea1b13c01c496ecdee5cb8f
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2019-10-30 16:01:57 -0400

    Adds comment and cleans up code for two-tuples
    Former-commit-id: ef90765d788ce0dd16c0852f375246c4d3c022d9

--- src/TurbulenceClosures/closure_tuples.jl
+++ src/TurbulenceClosures/closure_tuples.jl
@@ -1,33 +1,26 @@
-####
-#### 'Tupled closure' implementation
-####
+#####
+##### 'Tupled closure' implementation.
+#####
+##### For now, we have hacked an implementation for tuples of two closures.
+##### We cannot do the case of arbitrary closure tuples until we figure out how to
+##### obtain the length of a closure tuple at compile-time on the GPU.
+#####
 
 # Stress divergences
 
 for stress_div in (:∂ⱼ_2ν_Σ₁ⱼ, :∂ⱼ_2ν_Σ₂ⱼ, :∂ⱼ_2ν_Σ₃ⱼ)
-    stress_div_tuple = Symbol(stress_div, :_tuple)
     @eval begin
-        @inline function $stress_div(i, j, k, grid, closures::Tuple, U, Ks)
-            return $stress_div_tuple(i, j, k, grid, closures, U, Ks)
-        end
-
-        @inline function $stress_div_tuple(i, j, k, grid, ct::Tuple{C1, C2}, U, Ks) where {C1, C2}
-            return (  $stress_div(i, j, k, grid, closures[1], U, Ks[1])
-                    + $stress_div(i, j, k, grid, closures[2], U, Ks[2]))
-        end
+        @inline $stress_div(i, j, k, grid, closures::Tuple{C1, C2}, U, Ks) where {C1, C2} = (
+              $stress_div(i, j, k, grid, closures[1], U, Ks[1])
+            + $stress_div(i, j, k, grid, closures[2], U, Ks[2]))
     end
 end
 
 # Tracer flux divergences
 
-@inline function ∇_κ_∇c(i, j, k, grid, closures::Tuple, c, iᶜ, Ks, C, buoyancy) 
-    return ∇_κ_∇c_tuple(i, j, k, grid, closures, c, iᶜ, Ks, C, buoyancy)
-end
-                        
-@inline function ∇_κ_∇c_tuple(i, j, k, grid, closures::Tuple{C1, C2}, c, iᶜ, Ks, C, buoyancy) where {C1, C2}
-    return (  ∇_κ_∇c(i, j, k, grid, closures[1], c, iᶜ, Ks[1], C, buoyancy)
-            + ∇_κ_∇c(i, j, k, grid, closures[2], c, iᶜ, Ks[2], C, buoyancy))
-end
+@inline ∇_κ_∇c(i, j, k, grid, closures::Tuple{C1, C2}, c, iᶜ, Ks, C, buoyancy) where {C1, C2} = (
+      ∇_κ_∇c(i, j, k, grid, closures[1], c, iᶜ, Ks[1], C, buoyancy)
+    + ∇_κ_∇c(i, j, k, grid, closures[2], c, iᶜ, Ks[2], C, buoyancy))
 
 # Utilities for closures
 
