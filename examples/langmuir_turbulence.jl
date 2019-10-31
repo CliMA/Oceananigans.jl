c91d2a98feb4c88ddc0ad773ee64b3be82a8bd0c
Gregory Wagner <wagner.greg@gmail.com>
2019-10-31 17:34:08 -0400

    Updates the comments for langmuir turbulence example

--- examples/langmuir_turbulence.jl
+++ examples/langmuir_turbulence.jl
@@ -1,49 +1,33 @@
 # # Langmuir turbulence example
 #
 # This example implements the Langmuir turbulence simulation reported in section
-# 4 of McWilliams and Sullivan (1997). This example demonstrates:
+# 4 of McWilliams, J. C. et al., "Langmuir Turbulence in the ocean," Journal of 
+# Fluid Mechanics (1997). This example demonstrates:
 #   
 #   * how to run a simulation with surface wave effects via the Craik-Leibovich
 #     approximation
-#
-#   * Other stuff?
 #
 # In addition to `Oceananigans.jl` we need `PyPlot` for plotting, `Random` for
 # generating random initial conditions, and `Printf` for printing progress messages.
 
 using Oceananigans, Random, Printf
 using Oceananigans: g_Earth
-
 using Oceananigans.SurfaceWaves: UniformStokesDrift
 
 # ## Model parameters
 #
-# Here we use an anisotropic grid with `Nh` horizontal grid points and `Nz` vertical
-# grid points. 
-
-# `Δz = 1` meter. We specify fluxes of heat, momentum, and salinity via
+# Here we use an anisotropic grid with `Nh` horizontal grid points, `Nz` vertical
+# grid points, `Δh` horizontal grid spacing, and `Δz` vertical grid spacing. 
+# We specify fluxes of buouyancy and momentum that match the specifications in
+# McWilliams and Sullivan (1997) via
 #
-#   1. A temperature flux `Qᵀ` at the top of the domain, which is related to heat flux
-#       by `Qᵀ = Qʰ / (ρ₀ * cᴾ)`, where `Qʰ` is the heat flux, `ρ₀` is a reference density,
-#       and `cᴾ` is the heat capacity of seawater. With a reference density
-#       `ρ₀ = 1026 kg m⁻³`and heat capacity `cᴾ = 3991`, our chosen temperature flux of
-#       `Qᵀ = 5 × 10⁻⁵ K m⁻¹ s⁻¹` corresponds to a heat flux of `Qʰ = 204.7 W m⁻²`, a
-#       relatively powerful cooling rate.
+#   1. A buoyancy flux `Qᵇ` at the top of the domain.
 #
 #   2. A velocity flux `Qᵘ` at the top of the domain, which is related
 #       to the `x` momentum flux `τˣ` via `τˣ = ρ₀ * Qᵘ`, where `ρ₀` is a reference density.
-#       Our chosen value of `Qᵘ = -2 × 10⁻⁵ m² s⁻²` roughly corresponds to atmospheric winds
-#       of `uᵃ = 2.9 m s⁻¹` in the positive `x`-direction, using the parameterization
-#       `τ = 0.0025 * |uᵃ| * uᵃ`.
 #
-#   3. An evaporation rate `evaporation = 10⁻⁷ m s⁻¹`, or approximately 0.1 millimeter per
-#       hour.
-#
-# Finally, we use an initial temperature gradient of `∂T/∂z = 0.005 K m⁻¹`,
-# which implies an iniital buoyancy frequency `N² = α * g * ∂T/∂z = 9.8 × 10⁻⁶ s⁻²`
-# with a thermal expansion coefficient `α = 2 × 10⁻⁴ K⁻¹` and gravitational acceleration
-# `g = 9.81 s⁻²`. Note that, by default, the `SeawaterBuoyancy` model uses a gravitational
-# acceleration `gᴱᵃʳᵗʰ = 9.80665 s⁻²`.
+# We also choose the wavenumber and amplitude of the surface wave field, and use an
+# initial buoyancy gradient of `N² = 1.936e-5`.
 
       Nh = 48       # Number of grid points in x, y, z
       Nz = 48       # Number of grid points in x, y, z
@@ -55,7 +39,7 @@ using Oceananigans.SurfaceWaves: UniformStokesDrift
        f = 1e-4     # [s⁻¹] Coriolis parameter
      kˢʷ = 0.105    # [m⁻¹] Surface wave wavenumber
      aˢʷ = 0.8      # [m] Surface wave amplitude
-end_time = 2day     # End time for the simulation
+end_time = 1day     # End time for the simulation
 
 # Surface wave stokes drift profile
 const Uˢ = aˢʷ^2 * kˢʷ * sqrt(g_Earth * kˢʷ)
@@ -79,12 +63,9 @@ b_bcs = HorizontallyPeriodicBCs(top = BoundaryCondition(Flux, Qᵇ),
 # ## Model instantiation
 #
 # We instantiate a horizontally-periodic `Model` on the CPU with on a `RegularCartesianGrid`,
-# using a `FPlane` model for rotation (constant rotation rate), a linear equation
-# of state for temperature and salinity, the Anisotropic Minimum Dissipation closure
-# to model the effects of unresolved turbulence, and the previously defined boundary
-# conditions for `u`, `T`, and `S`. We also pass the evaporation rate to the container
-# model.parameters for use in the boundary condition function that calculates the salinity
-# flux.
+# using a `FPlane` model for rotation (constant rotation rate), the Anisotropic Minimum 
+# Dissipation closure to model the effects of unresolved turbulence, and the previously defined 
+# boundary conditions for `u` and `b`.
 
 model = Model(
          architecture = CPU(),
@@ -95,14 +76,10 @@ model = Model(
   boundary_conditions = BoundaryConditions(u=u_bcs, b=b_bcs),
 )
 
-makeplot = true
-
-#
 # ## Initial conditions
 #
-# Out initial condition for temperature consists of a linear stratification superposed with
-# random noise damped at the walls, while our initial condition for velocity consists
-# only of random noise.
+# Out initial condition for buoyancy consists of a linear stratification superposed with
+# random noise damped at the walls.
 
 ## Random noise damped at top and bottom
 Ξ(z) = randn() * z / model.grid.Lz * (1 + z / model.grid.Lz) # noise
@@ -110,17 +87,18 @@ makeplot = true
 ## Temperature initial condition: a stable density tradient with random noise superposed.
 b₀(x, y, z) = N² * z + 1e-1 * Ξ(z) * N² * model.grid.Lz
 
-## Velocity initial condition: note that McWilliams and Sullivan (1997) use a model
-## that is formulated in terms of the Eulerian-mean velocity field. Due to this, 
-## their 'resting' initial condition actually consists of a strongly-sheared, 
-## unbalanced Lagrangian-mean velocity field uˢ(z). To this we add noise.
-u₀(x, y, z) = -uˢ(z) + sqrt(abs(Qᵘ)) * 1e-1 * Ξ(z)
+# Velocity initial condition: note that McWilliams and Sullivan (1997) use a model
+# that is formulated in terms of the Eulerian-mean velocity field. Due to this, 
+# their 'resting' initial condition actually consists of a sheared and 
+# unbalanced Lagrangian-mean velocity field uˢ(z). To this we add noise.
+u₀(x, y, z) = uˢ(z) + sqrt(abs(Qᵘ)) * 1e-1 * Ξ(z)
+
+# Finally, we introduce noise into the vertical velocity:
 
 w₀(x, y, z) = sqrt(abs(Qᵘ)) * 1e-1 * Ξ(z)
 
 set!(model, u=u₀, w=w₀, b=b₀)
 
-#=
 # ## Set up output
 #
 # We set up an output writer that saves all velocity fields, tracer fields, and the subgrid
@@ -133,11 +111,10 @@ fields_to_output = merge(model.velocities, model.tracers, (νₑ=model.diffusivi
 
 ## Instantiate a JLD2OutputWriter to write fields.
 field_writer = JLD2OutputWriter(model, FieldOutputs(fields_to_output); interval=hour/4,
-                                prefix="ocean_wind_mixing_and_convection", force=true)
+                                prefix="langmuir_turbulence", force=true)
 
 ## Add the output writer to the models `output_writers`.
 model.output_writers[:fields] = field_writer;
-=#
 
 # ## Running the simulation
 #
@@ -154,6 +131,8 @@ vmax = FieldMaximum(abs, model.velocities.v)
 wmax = FieldMaximum(abs, model.velocities.w)
 
 # We also create a figure and define a plotting function for live plotting of results.
+makeplot = false
+
 using PyPlot
 
 fig, axs = subplots(ncols=3, figsize=(12, 5))
