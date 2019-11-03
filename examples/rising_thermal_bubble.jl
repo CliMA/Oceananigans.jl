2cf31b4bd72ba20105da65295e9fd3f1bd68363c
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-11-03 16:39:10 -0500

    Start testing rising thermal bubble example

--- examples/rising_thermal_bubble.jl
+++ examples/rising_thermal_bubble.jl
@@ -1,6 +1,6 @@
 using JULES, Oceananigans
 
-N = 16
+N = 8
 L = 100
 
 grid = RegularCartesianGrid(size=(N, N, N), x=(0, L), y=(0, L), z=(0, L))
@@ -13,4 +13,7 @@ j1, j2 = round(Int, Ny/4), round(Int, 3Ny/4)
 k1, k2 = round(Int, Nz/4), round(Int, 3Nz/4)
 model.tracers.Θᵐ.data[i1:i2, j1:j2, k1:k2] .+= 0.01
 
+model.density.data .= 1.2
+
+time_step!(model; Δt=1, nₛ=1)
 
