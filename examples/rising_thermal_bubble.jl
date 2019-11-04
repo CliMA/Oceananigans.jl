56e389f834a93e4f81d076637f0e171a82b73608
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-11-03 20:47:22 -0500

    Initial condition must reflect reference density profile

--- examples/rising_thermal_bubble.jl
+++ examples/rising_thermal_bubble.jl
@@ -8,29 +8,36 @@ L = 100
 grid = RegularCartesianGrid(size=(Nx, Ny, Nz), x=(-L/2, L/2), y=(-L/2, L/2), z=(-L/2, L/2))
 model = CompressibleModel(grid=grid)
 
-Θ₀(x, y, z) = 20 + 0.01 * exp(- (x^2 + z^2) / L)
-set!(model.tracers.Θᵐ, Θ₀)
-set!(model.density, 1.2)
+p₀ = model.surface_pressure
+Rᵈ = model.buoyancy.Rᵈ
 
-Δtp = 1e-4
+# ρ₀(x, y, z) = 1.2 * (1 -  (z - L/2) / 10L)
+Θ₀(x, y, z) = 300 + 0.01 * exp(- (x^2 + z^2) / L)
+ρ₀(x, y, z) = p₀ / (Rᵈ * Θ₀(x, y, z))
+
+set!(model.density, ρ₀)
+set!(model.tracers.Θᵐ, Θ₀)
+
+Δtp = 1e-3
 time_step!(model; Δt=Δtp, nₛ=1)
 
-l = @layout [a; b]
-for i=1:5
+for i=1:100
     time_step!(model; Δt=Δtp, nₛ=1)
 
-    j = Int(Ny/2)
-    U_slice = model.momenta.U.data[1:Nx, j, 1:Nz]
-    W_slice = model.momenta.W.data[1:Nx, j, 1:Nz]
-    ρ_slice = model.density.data[1:Nx, j, 1:Nz] .- 1.2
-    Θ_slice = model.tracers.Θᵐ.data[1:Nx, j, 1:Nz] .- 20
+    xC, yC, zC = model.grid.xC, model.grid.yC, model.grid.zC
+    xF, yF, zF = model.grid.xF, model.grid.yF, model.grid.zF
 
-    x, z = model.grid.xC, model.grid.zC
-    pU = contour(x, z, U_slice; fill=true, levels=10, color=:balance, clims=(-1e-3, 1e-3))
-    pW = contour(x, z, W_slice; fill=true, levels=10, color=:balance, clims=(-1e-3, 1e-3))
-    pρ = contour(x, z, ρ_slice; fill=true, levels=10, color=:balance, clims=(-1e-3, 1e-3))
-    pΘ = contour(x, z, Θ_slice; fill=true, levels=10, color=:thermal, clims=(0, 0.01))
-    display(plot(pU, pW, pρ, pΘ, title=["U" "W" "rho" "Theta_m"], show=true))
-    sleep(0.1)
+    j = Int(Ny/2)
+    U_slice = rotr90(model.momenta.U.data[1:Nx, j, 1:Nz])
+    W_slice = rotr90(model.momenta.W.data[1:Nx, j, 1:Nz])
+    ρ_slice = rotr90(model.density.data[1:Nx, j, 1:Nz] .- ρ₀.(xC, 1, zC'))
+    Θ_slice = rotr90(model.tracers.Θᵐ.data[1:Nx, j, 1:Nz]) .- 300
+    
+    pU = contour(xC, zC, U_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:balance, clims=(-5e-3, 5e-3))
+    pW = contour(xC, zC, W_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:balance, clims=(-0.1, 0.1))
+    pρ = contour(xC, zC, ρ_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:balance, clims=(-1e-5, 1e-5))
+    pΘ = contour(xC, zC, Θ_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:thermal, clims=(0, 0.01))
+    
+    display(plot(pU, pW, pρ, pΘ, title=["U" "W" "rho_prime" "Theta_m_prime"], show=true))
 end
 
