98bfaa6a6bc3ee2c5a65d223dac7a1f600589224
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-11-03 21:58:02 -0500

    Base state probably incompatible with initial conditions

--- examples/rising_thermal_bubble.jl
+++ examples/rising_thermal_bubble.jl
@@ -1,5 +1,5 @@
 using JULES, Oceananigans
-using Plots
+using Plots, Printf
 
 Nx = Nz = 32
 Ny = 8
@@ -24,6 +24,7 @@ time_step!(model; Δt=Δtp, nₛ=1)
 for i=1:100
     time_step!(model; Δt=Δtp, nₛ=1)
 
+    t = @sprintf("%.3f s", model.clock.time)
     xC, yC, zC = model.grid.xC, model.grid.yC, model.grid.zC
     xF, yF, zF = model.grid.xF, model.grid.yF, model.grid.zF
 
@@ -32,12 +33,12 @@ for i=1:100
     W_slice = rotr90(model.momenta.W.data[1:Nx, j, 1:Nz])
     ρ_slice = rotr90(model.density.data[1:Nx, j, 1:Nz] .- ρ₀.(xC, 1, zC'))
     Θ_slice = rotr90(model.tracers.Θᵐ.data[1:Nx, j, 1:Nz]) .- 300
-    
+
     pU = contour(xC, zC, U_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:balance, clims=(-5e-3, 5e-3))
-    pW = contour(xC, zC, W_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:balance, clims=(-0.1, 0.1))
-    pρ = contour(xC, zC, ρ_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:balance, clims=(-1e-5, 1e-5))
-    pΘ = contour(xC, zC, Θ_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:thermal, clims=(0, 0.01))
-    
-    display(plot(pU, pW, pρ, pΘ, title=["U" "W" "rho_prime" "Theta_m_prime"], show=true))
+    pW = contour(xC, zC, W_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:balance, clims=(-5e-3, 5e-3))
+    pρ = contour(xC, zC, ρ_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:balance, clims=(-5e-5, 5e-5))
+    pΘ = contour(xC, zC, Θ_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:thermal, clims=(-0.005, 0.01))
+
+    display(plot(pU, pW, pρ, pΘ, title=["U (m/s), t=$t" "W (m/s)" "rho_prime (kg/m^3)" "Theta_m_prime (K)"], show=true))
 end
 
