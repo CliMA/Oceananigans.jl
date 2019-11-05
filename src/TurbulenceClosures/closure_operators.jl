6ca5feda7a8e6f6d1dc10f520eacc6af9d086257
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-11-05 08:18:36 -0500

    Update closure_operators.jl to use new operators

--- src/TurbulenceClosures/closure_operators.jl
+++ src/TurbulenceClosures/closure_operators.jl
@@ -20,14 +20,14 @@ Multiply the array `ν` located at `ᶜᶜᶜ` by a function
 at index `i, j, k` and location `ᶠᶠᶜ`.
 """
 @inline ν_σᶠᶠᶜ(i, j, k, grid, ν::TN, σᶠᶠᶜ::TS, u, v, w) where {TN<:AbstractArray, TS} =
-    @inbounds ▶xy_ffa(i, j, k, grid, ν) * σᶠᶠᶜ(i, j, k, grid, u, v, w)
+    @inbounds ℑxyᶠᶠᵃ(i, j, k, grid, ν) * σᶠᶠᶜ(i, j, k, grid, u, v, w)
 
 # These functions are analogous to the two above, but for different locations:
 @inline ν_σᶠᶜᶠ(i, j, k, grid, ν::TN, σᶠᶜᶠ::TS, u, v, w) where {TN<:AbstractArray, TS} =
-    @inbounds ▶xz_faf(i, j, k, grid, ν) * σᶠᶜᶠ(i, j, k, grid, u, v, w)
+    @inbounds ℑxzᶠᵃᶠ(i, j, k, grid, ν) * σᶠᶜᶠ(i, j, k, grid, u, v, w)
 
 @inline ν_σᶜᶠᶠ(i, j, k, grid, ν::TN, σᶜᶠᶠ::TS, u, v, w) where {TN<:AbstractArray, TS} =
-    @inbounds ▶yz_aff(i, j, k, grid, ν) * σᶜᶠᶠ(i, j, k, grid, u, v, w)
+    @inbounds ℑyzᵃᶠᶠ(i, j, k, grid, ν) * σᶜᶠᶠ(i, j, k, grid, u, v, w)
 
 #####
 ##### Stress divergences
@@ -35,33 +35,33 @@ at index `i, j, k` and location `ᶠᶠᶜ`.
 
 # At fcc
 @inline ∂x_2ν_Σ₁₁(i, j, k, grid, closure, U, diffusivities) =
-    2 * ∂x_faa(i, j, k, grid, ν_σᶜᶜᶜ, diffusivities.νₑ, Σ₁₁, U.u, U.v, U.w)
+    2 * ∂xᶠᵃᵃ(i, j, k, grid, ν_σᶜᶜᶜ, diffusivities.νₑ, Σ₁₁, U.u, U.v, U.w)
 
 @inline ∂y_2ν_Σ₁₂(i, j, k, grid, closure, U, diffusivities) =
-    2 * ∂y_aca(i, j, k, grid, ν_σᶠᶠᶜ, diffusivities.νₑ, Σ₁₂, U.u, U.v, U.w)
+    2 * ∂yᵃᶜᵃ(i, j, k, grid, ν_σᶠᶠᶜ, diffusivities.νₑ, Σ₁₂, U.u, U.v, U.w)
 
 @inline ∂z_2ν_Σ₁₃(i, j, k, grid, closure, U, diffusivities) =
-    2 * ∂z_aac(i, j, k, grid, ν_σᶠᶜᶠ, diffusivities.νₑ, Σ₁₃, U.u, U.v, U.w)
+    2 * ∂zᵃᵃᶜ(i, j, k, grid, ν_σᶠᶜᶠ, diffusivities.νₑ, Σ₁₃, U.u, U.v, U.w)
 
 # At cfc
 @inline ∂x_2ν_Σ₂₁(i, j, k, grid, closure, U, diffusivities) =
-    2 * ∂x_caa(i, j, k, grid, ν_σᶠᶠᶜ, diffusivities.νₑ, Σ₂₁, U.u, U.v, U.w)
+    2 * ∂xᶜᵃᵃ(i, j, k, grid, ν_σᶠᶠᶜ, diffusivities.νₑ, Σ₂₁, U.u, U.v, U.w)
 
 @inline ∂y_2ν_Σ₂₂(i, j, k, grid, closure, U, diffusivities) =
-    2 * ∂y_afa(i, j, k, grid, ν_σᶜᶜᶜ, diffusivities.νₑ, Σ₂₂, U.u, U.v, U.w)
+    2 * ∂yᵃᶠᵃ(i, j, k, grid, ν_σᶜᶜᶜ, diffusivities.νₑ, Σ₂₂, U.u, U.v, U.w)
 
 @inline ∂z_2ν_Σ₂₃(i, j, k, grid, closure, U, diffusivities) =
-    2 * ∂z_aac(i, j, k, grid, ν_σᶜᶠᶠ, diffusivities.νₑ, Σ₂₃, U.u, U.v, U.w)
+    2 * ∂zᵃᵃᶜ(i, j, k, grid, ν_σᶜᶠᶠ, diffusivities.νₑ, Σ₂₃, U.u, U.v, U.w)
 
 # At ccf
 @inline ∂x_2ν_Σ₃₁(i, j, k, grid, closure, U, diffusivities) =
-    2 * ∂x_caa(i, j, k, grid, ν_σᶠᶜᶠ, diffusivities.νₑ, Σ₃₁, U.u, U.v, U.w)
+    2 * ∂xᶜᵃᵃ(i, j, k, grid, ν_σᶠᶜᶠ, diffusivities.νₑ, Σ₃₁, U.u, U.v, U.w)
 
 @inline ∂y_2ν_Σ₃₂(i, j, k, grid, closure, U, diffusivities) =
-    2 * ∂y_aca(i, j, k, grid, ν_σᶜᶠᶠ, diffusivities.νₑ, Σ₃₂, U.u, U.v, U.w)
+    2 * ∂yᵃᶜᵃ(i, j, k, grid, ν_σᶜᶠᶠ, diffusivities.νₑ, Σ₃₂, U.u, U.v, U.w)
 
 @inline ∂z_2ν_Σ₃₃(i, j, k, grid, closure, U, diffusivities) =
-    2 * ∂z_aaf(i, j, k, grid, ν_σᶜᶜᶜ, diffusivities.νₑ, Σ₃₃, U.u, U.v, U.w)
+    2 * ∂zᵃᵃᶠ(i, j, k, grid, ν_σᶜᶜᶜ, diffusivities.νₑ, Σ₃₃, U.u, U.v, U.w)
 
 """
     κ_∂x_c(i, j, k, grid, c, κ, closure, args...)
@@ -71,8 +71,8 @@ diffusivity at cell centers (location `ccc`), and `c` is an array of scalar
 data located at cell centers.
 """
 @inline function κ_∂x_c(i, j, k, grid, κ, c, closure, args...)
-    κ = ▶x_faa(i, j, k, grid, κ, closure, args...)
-    ∂x_c = ∂x_faa(i, j, k, grid, c)
+    κ = ℑxᶠᵃᵃ(i, j, k, grid, κ, closure, args...)
+    ∂x_c = ∂xᶠᵃᵃ(i, j, k, grid, c)
     return κ * ∂x_c
 end
 
@@ -84,8 +84,8 @@ diffusivity at cell centers (location `ccc`), and `c` is an array of scalar
 data located at cell centers.
 """
 @inline function κ_∂y_c(i, j, k, grid, κ, c, closure, args...)
-    κ = ▶y_afa(i, j, k, grid, κ, closure, args...)
-    ∂y_c = ∂y_afa(i, j, k, grid, c)
+    κ = ℑyᵃᶠᵃ(i, j, k, grid, κ, closure, args...)
+    ∂y_c = ∂yᵃᶠᵃ(i, j, k, grid, c)
     return κ * ∂y_c
 end
 
@@ -97,8 +97,8 @@ diffusivity at cell centers (location `ccc`), and `c` is an array of scalar
 data located at cell centers.
 """
 @inline function κ_∂z_c(i, j, k, grid, κ, c, closure, args...)
-    κ = ▶z_aaf(i, j, k, grid, κ, closure, args...)
-    ∂z_c = ∂z_aaf(i, j, k, grid, c)
+    κ = ℑzᵃᵃᶠ(i, j, k, grid, κ, closure, args...)
+    ∂z_c = ∂zᵃᵃᶠ(i, j, k, grid, c)
     return κ * ∂z_c
 end
 
