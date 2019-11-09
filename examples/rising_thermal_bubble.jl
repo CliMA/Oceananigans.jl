8d0803ccff2f2258b3d9472d7382aae2ba980fba
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-11-09 12:27:30 -0500

    Separate base state and initial conditions for thermal bubble

--- examples/rising_thermal_bubble.jl
+++ examples/rising_thermal_bubble.jl
@@ -1,23 +1,32 @@
 using JULES, Oceananigans
 using Plots, Printf
+using OffsetArrays
 
 Nx = Nz = 32
 Ny = 8
 L = 100
 
-pₛ = 100000
+grid = RegularCartesianGrid(size=(Nx, Ny, Nz), x=(-L/2, L/2), y=(-L/2, L/2), z=(0, L))
+
+const pₛ = 100000
 buoyancy = IdealGas()
-Rᵈ = buoyancy.Rᵈ
-γ  = buoyancy.γ
+const Rᵈ = buoyancy.Rᵈ
+const γ  = buoyancy.γ
 
-# ρ₀(x, y, z) = 1.2 * (1 -  (z - L/2) / 10L)
-Θ₀(x, y, z) = 300 + 0.01 * exp(- (x^2 + z^2) / L)
-p₀(x, y, z) = pₛ * (Rᵈ * Θ₀(x, y, z) / pₛ)^γ
-ρ₀(x, y, z) = p₀(x, y, z) / (Rᵈ * Θ₀(x, y, z))
+Θᵇ(x, y, z) = 300 
+pᵇ(x, y, z) = pₛ * (Rᵈ * Θᵇ(x, y, z) / pₛ)^γ
+ρᵇ(x, y, z) = pᵇ(x, y, z) / (Rᵈ * Θᵇ(x, y, z))
 
-grid = RegularCartesianGrid(size=(Nx, Ny, Nz), x=(-L/2, L/2), y=(-L/2, L/2), z=(-L/2, L/2))
-base_state = BaseState(p=p₀, ρ=ρ₀, θ=Θ₀)
+pᵇ_a = @. pᵇ(0, 0, grid.zC)
+pᵇ_oa = OffsetArray([pᵇ_a[1]; pᵇ_a; pᵇ_a[end]], 0:Nz+1)
+@show pᵇ_oa
+
+base_state = BaseState(p=pᵇ_oa, ρ=ρᵇ, θ=Θᵇ)
 model = CompressibleModel(grid=grid, buoyancy=buoyancy, surface_pressure=pₛ, base_state=base_state)
+
+Θ₀(x, y, z) = Θᵇ(x, y, z) * (1 + 0.01 * exp(- (x^2 + (z-50)^2) / L))
+p₀(x, y, z) = pₛ * (Rᵈ * Θ₀(x, y, z) / pₛ)^γ
+ρ₀(x, y, z) = pᵇ(x, y, z) / (Rᵈ * Θ₀(x, y, z))
 
 set!(model.density, ρ₀)
 set!(model.tracers.Θᵐ, Θ₀)
@@ -26,7 +35,7 @@ set!(model.tracers.Θᵐ, Θ₀)
 time_step!(model; Δt=Δtp, nₛ=1)
 
 #anim = @animate for i=1:10
-for i = 1:10
+for i = 1:30
     @show i
     time_step!(model; Δt=Δtp, nₛ=1)
 
@@ -38,12 +47,12 @@ for i = 1:10
     U_slice = rotr90(model.momenta.U.data[1:Nx, j, 1:Nz])
     W_slice = rotr90(model.momenta.W.data[1:Nx, j, 1:Nz])
     ρ_slice = rotr90(model.density.data[1:Nx, j, 1:Nz] .- ρ₀.(xC, 1, zC'))
-    Θ_slice = rotr90(model.tracers.Θᵐ.data[1:Nx, j, 1:Nz]) .- 300
+    Θ_slice = rotr90(model.tracers.Θᵐ.data[1:Nx, j, 1:Nz])
 
     pU = contour(xC, zC, U_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:balance, clims=(-5e-3, 5e-3))
     pW = contour(xC, zC, W_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:balance, clims=(-5e-3, 5e-3))
     pρ = contour(xC, zC, ρ_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:balance, clims=(-5e-5, 5e-5))
-    pΘ = contour(xC, zC, Θ_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:thermal, clims=(-0.005, 0.01))
+    pΘ = contour(xC, zC, Θ_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:thermal)
 
     display(plot(pU, pW, pρ, pΘ, title=["U (m/s), t=$t" "W (m/s)" "rho_prime (kg/m^3)" "Theta_m_prime (K)"], show=true))
 end
