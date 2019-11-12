9d6cb407216a960fd36430d7fd4a238fca66dcbf
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-11-11 23:37:25 -0500

    Dry thermal bubble with isothermal base state

--- examples/rising_thermal_bubble.jl
+++ examples/rising_thermal_bubble.jl
@@ -9,33 +9,39 @@ L = 100
 grid = RegularCartesianGrid(size=(Nx, Ny, Nz), x=(-L/2, L/2), y=(-L/2, L/2), z=(0, L))
 
 const pₛ = 100000
+const Tₐ = 293.15
+const g  = 9.80665
+
 buoyancy = IdealGas()
 const Rᵈ = buoyancy.Rᵈ
 const γ  = buoyancy.γ
+const ρₛ = pₛ / (Rᵈ*Tₐ)
 
-Θᵇ(x, y, z) = 300 
-pᵇ(x, y, z) = pₛ * (Rᵈ * Θᵇ(x, y, z) / pₛ)^γ
-ρᵇ(x, y, z) = pᵇ(x, y, z) / (Rᵈ * Θᵇ(x, y, z))
+# Isothermal atmosphere
+pᵇ(x, y, z) = pₛ * exp(- g*z / (Rᵈ*Tₐ))
+ρᵇ(x, y, z) = ρₛ * exp(- g*z / (Rᵈ*Tₐ))
+Tᵇ(x, y, z) = ρᵇ(x, y, z) * Tₐ
 
-pᵇ_a = @. pᵇ(0, 0, grid.zC)
-pᵇ_oa = OffsetArray([pᵇ_a[1]; pᵇ_a; pᵇ_a[end]], 0:Nz+1)
-@show pᵇ_oa
+ρ₀(x, y, z) = ρᵇ(x, y, z) + 1e-4 * exp(- (x^2 + (z-50)^2) / L)
+T₀(x, y, z) = ρ₀(x, y, z) * Tₐ
+p₀(x, y, z) = Rᵈ * T₀(x, y, z)
 
-base_state = BaseState(p=pᵇ_oa, ρ=ρᵇ, θ=Θᵇ)
-model = CompressibleModel(grid=grid, buoyancy=buoyancy, surface_pressure=pₛ, base_state=base_state)
+pᵇa = @. pᵇ(0, 0, grid.zC)
+pᵇoa = OffsetArray([pᵇa[1]; pᵇa; pᵇa[end]], 0:Nz+1)
 
-Θ₀(x, y, z) = Θᵇ(x, y, z) * (1 + 0.01 * exp(- (x^2 + (z-50)^2) / L))
-p₀(x, y, z) = pₛ * (Rᵈ * Θ₀(x, y, z) / pₛ)^γ
-ρ₀(x, y, z) = pᵇ(x, y, z) / (Rᵈ * Θ₀(x, y, z))
+base_state = BaseState(p=pᵇoa, ρ=ρᵇ)
+
+model = CompressibleModel(grid=grid, buoyancy=buoyancy, surface_pressure=pₛ,
+                          prognostic_temperature=Temperature(), base_state=base_state)
 
 set!(model.density, ρ₀)
-set!(model.tracers.Θᵐ, Θ₀)
+set!(model.tracers.Θᵐ, T₀)
 
 Δtp = 1e-3
 time_step!(model; Δt=Δtp, nₛ=1)
 
 #anim = @animate for i=1:10
-for i = 1:30
+for i = 1:1
     @show i
     time_step!(model; Δt=Δtp, nₛ=1)
 
@@ -46,15 +52,15 @@ for i = 1:30
     j = Int(Ny/2)
     U_slice = rotr90(model.momenta.U.data[1:Nx, j, 1:Nz])
     W_slice = rotr90(model.momenta.W.data[1:Nx, j, 1:Nz])
-    ρ_slice = rotr90(model.density.data[1:Nx, j, 1:Nz] .- ρ₀.(xC, 1, zC'))
-    Θ_slice = rotr90(model.tracers.Θᵐ.data[1:Nx, j, 1:Nz])
+    ρ_slice = rotr90(model.density.data[1:Nx, j, 1:Nz] .- ρᵇ.(xC, 1, zC'))
+    Θ_slice = rotr90(model.tracers.Θᵐ.data[1:Nx, j, 1:Nz] .- Tᵇ.(xC, 1, zC'))
 
     pU = contour(xC, zC, U_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:balance, clims=(-5e-3, 5e-3))
     pW = contour(xC, zC, W_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:balance, clims=(-5e-3, 5e-3))
     pρ = contour(xC, zC, ρ_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:balance, clims=(-5e-5, 5e-5))
-    pΘ = contour(xC, zC, Θ_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:thermal)
+    pΘ = contour(xC, zC, Θ_slice; xlabel="x", ylabel="z", fill=true, levels=10, color=:thermal, clims=(0, 0.01))
 
-    display(plot(pU, pW, pρ, pΘ, title=["U (m/s), t=$t" "W (m/s)" "rho_prime (kg/m^3)" "Theta_m_prime (K)"], show=true))
+    display(plot(pU, pW, pρ, pΘ, title=["rho*u, t=$t" "rho*w" "rho_prime" "T_prime"], show=true))
 end
 
 # gif(anim, "sad_thermal_bubble.gif", fps=10)
