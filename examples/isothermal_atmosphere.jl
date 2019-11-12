fc6f7cb285dedea65d51e6ef86b1bdccd919e5b8
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-11-11 20:37:20 -0500

    Much more hydrostatic

--- examples/isothermal_atmosphere.jl
+++ examples/isothermal_atmosphere.jl
@@ -16,15 +16,16 @@ const Rᵈ = buoyancy.Rᵈ
 const γ  = buoyancy.γ
 const ρₛ = pₛ / (Rᵈ*Tₐ)
 
-model = CompressibleModel(grid=grid, buoyancy=buoyancy, surface_pressure=pₛ)
+model = CompressibleModel(grid=grid, buoyancy=buoyancy, surface_pressure=pₛ,
+                          prognostic_temperature=Temperature())
 
 # Isothermal atmosphere
 p₀(x, y, z) = pₛ * exp(- g*z / (Rᵈ*Tₐ))
 ρ₀(x, y, z) = ρₛ * exp(- g*z / (Rᵈ*Tₐ))
-Θ₀(x, y, z) = pₛ/Rᵈ * (pₛ / p₀(x, y, z))^γ
+T₀(x, y, z) = p₀(x, y, z) / Rᵈ
 
 set!(model.density, ρ₀)
-set!(model.tracers.Θᵐ, Θ₀)
+set!(model.tracers.Θᵐ, T₀)
 
 Δtp = 1e-3
 time_step!(model; Δt=Δtp, nₛ=1)
