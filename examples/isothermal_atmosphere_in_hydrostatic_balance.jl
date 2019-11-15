a6992692c92359539fca1f6b7dd40944dedf0a41
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-11-15 18:57:01 -0500

    Isothermal atmosphere in hydrostatic balance, no base state

--- examples/isothermal_atmosphere_in_hydrostatic_balance.jl
+++ examples/isothermal_atmosphere_in_hydrostatic_balance.jl
@@ -3,36 +3,31 @@ using Plots, Printf
 using OffsetArrays
 
 Nx = Ny = Nz = 8
-L = 100
+L = 10e3
 
 grid = RegularCartesianGrid(size=(Nx, Ny, Nz), x=(0, L), y=(0, L), z=(0, L))
 
-const pₛ = 100000
-const Tₐ = 293.15
-const g  = 9.80665
+pₛ = 100000
+Tₐ = 293.15
+g  = 9.80665
 
 buoyancy = IdealGas()
-const Rᵈ = buoyancy.Rᵈ
-const γ  = buoyancy.γ
-const ρₛ = pₛ / (Rᵈ*Tₐ)
+Rᵈ = buoyancy.Rᵈ
+cₚ = buoyancy.cₚ
+ρₛ = pₛ / (Rᵈ*Tₐ)
 
 # Isothermal atmosphere
-p₀(x, y, z) = pₛ * exp(- g*z / (Rᵈ*Tₐ))
-ρ₀(x, y, z) = ρₛ * exp(- g*z / (Rᵈ*Tₐ))
-T₀(x, y, z) = ρ₀(x, y, z) * Tₐ
+H = Rᵈ * Tₐ / g
+p₀(x, y, z) = pₛ * exp(-z/H)
+ρ₀(x, y, z) = ρₛ * exp(-z/H)
 
-zC = grid.zC
-p₀a = @. p₀(0, 0, zC)
-p₀oa = OffsetArray([p₀a[1]; p₀a; p₀a[end]], 0:Nz+1)
+θ₀(x, y, z) = Tₐ * exp(z/H * Rᵈ/cₚ)
+Θ₀(x, y, z) = ρ₀(x, y, z) * θ₀(x, y, z)
 
-base_state = BaseState(p=p₀oa, ρ=ρ₀)
-
-model = CompressibleModel(grid=grid, buoyancy=buoyancy, surface_pressure=pₛ,
-                          prognostic_temperature=Temperature(), base_state=base_state)
+model = CompressibleModel(grid=grid, buoyancy=buoyancy, surface_pressure=pₛ)
 
 set!(model.density, ρ₀)
-set!(model.tracers.Θᵐ, T₀)
+set!(model.tracers.Θᵐ, Θ₀)
 
 Δtp = 1e-3
 time_step!(model; Δt=Δtp, nₛ=1)
-
