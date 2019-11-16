e0bd83d8d050620f391f8c367441e9b41754f030
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-11-16 16:30:19 -0500

    Clean up pressure.jl

--- src/pressure.jl
+++ src/pressure.jl
@@ -9,20 +9,20 @@ const IG = IdealGas
 const MPT = ModifiedPotentialTemperature
 
 ####
-#### Exner function
+#### Pressure gradient ∇p terms for regular temperature ρT
 ####
 
-@inline Π(i, j, k, grid, pt::MPT, gas::IG, p₀, Θᵐ) = @inbounds (gas.Rᵈ * Θᵐ[i, j, k] / p₀)^(gas.Rᵈ/gas.cᵥ)
+@inline ∂p∂x(i, j, k, grid, pt::Temperature, gas::IG, pₛ, C) = gas.Rᵈ * ∂xᶠᵃᵃ(i, j, k, grid, C.T)
+@inline ∂p∂y(i, j, k, grid, pt::Temperature, gas::IG, pₛ, C) = gas.Rᵈ * ∂yᵃᶠᵃ(i, j, k, grid, C.T)
+@inline ∂p∂z(i, j, k, grid, pt::Temperature, gas::IG, pₛ, C) = gas.Rᵈ * ∂zᵃᵃᶠ(i, j, k, grid, C.T)
 
 ####
-#### Pressure gradient ∇p terms
+#### Pressure gradient ∇p terms for modified potential temperature Θᵐ = ρθᵐ
 ####
 
-@inline ∂p∂x(i, j, k, grid, pt::Temperature, gas::IG, pₛ, C) = gas.Rᵈ * ∂xᶠᵃᵃ(i, j, k, grid, C.Θᵐ)
-@inline ∂p∂y(i, j, k, grid, pt::Temperature, gas::IG, pₛ, C) = gas.Rᵈ * ∂yᵃᶠᵃ(i, j, k, grid, C.Θᵐ)
-@inline ∂p∂z(i, j, k, grid, pt::Temperature, gas::IG, pₛ, C) = gas.Rᵈ * ∂zᵃᵃᶠ(i, j, k, grid, C.Θᵐ)
+@inline Π(i, j, k, grid, gas::IG, pₛ, Θᵐ) = @inbounds (gas.Rᵈ * Θᵐ[i, j, k] / pₛ)^(gas.Rᵈ/gas.cᵥ)
 
-@inline ∂p∂x(i, j, k, grid, pt::MPT, gas::IG, p₀, C) = gas.γ * gas.Rᵈ * Π(i, j, k, grid, pt, gas, p₀, C.Θᵐ) * ∂xᶠᵃᵃ(i, j, k, grid, C.Θᵐ)
-@inline ∂p∂y(i, j, k, grid, pt::MPT, gas::IG, p₀, C) = gas.γ * gas.Rᵈ * Π(i, j, k, grid, pt, gas, p₀, C.Θᵐ) * ∂yᵃᶠᵃ(i, j, k, grid, C.Θᵐ)
-@inline ∂p∂z(i, j, k, grid, pt::MPT, gas::IG, p₀, C) = gas.γ * gas.Rᵈ * Π(i, j, k, grid, pt, gas, p₀, C.Θᵐ) * ∂zᵃᵃᶠ(i, j, k, grid, C.Θᵐ)
+@inline ∂p∂x(i, j, k, grid, pt::MPT, gas::IG, pₛ, C) = gas.γ * gas.Rᵈ * Π(i, j, k, grid, gas, pₛ, C.Θᵐ) * ∂xᶠᵃᵃ(i, j, k, grid, C.Θᵐ)
+@inline ∂p∂y(i, j, k, grid, pt::MPT, gas::IG, pₛ, C) = gas.γ * gas.Rᵈ * Π(i, j, k, grid, gas, pₛ, C.Θᵐ) * ∂yᵃᶠᵃ(i, j, k, grid, C.Θᵐ)
+@inline ∂p∂z(i, j, k, grid, pt::MPT, gas::IG, pₛ, C) = gas.γ * gas.Rᵈ * Π(i, j, k, grid, gas, pₛ, C.Θᵐ) * ∂zᵃᵃᶠ(i, j, k, grid, C.Θᵐ)
 
