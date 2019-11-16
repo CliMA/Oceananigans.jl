76ef22ffdb22cad5f3b1846092c579627eefbdae
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-11-16 15:12:49 -0500

    Accurately rename moist_air to water_vapor

--- src/buoyancy.jl
+++ src/buoyancy.jl
@@ -12,16 +12,18 @@ using Oceananigans: AbstractBuoyancy, AbstractEquationOfState
 # https://en.wikipedia.org/wiki/Heat_capacity_ratio#Real-gas_relations
 
 const gas_constant = 8.31446261815324  # Universal gas constant [J/mol/K]
-const molar_mass_dry_air = 28.97e-3    # Molar mass of dry air [kg/mol]
-const molar_mass_moist_air = 18.015e-3 # Molar mass of moist air [kg/mol]
-const κᵈ = 2//7                        # Adiabatic exponent for dry air
-const γᵈ = 7//5                        # Heat capacity ratio for a diatomic ideal gas
 
-const Rᵈ_air = gas_constant / molar_mass_dry_air # Specific gas constant for dry air [J/kg/K]
-const cₚ_dry = Rᵈ_air / κᵈ                       # Isobaric specific heat for dry air [J/kg/K]
-const cᵥ_dry = cₚ_dry - Rᵈ_air                   # Isochoric specific heat for dry air [J/kg/K]
+const molar_mass_dry_air     = 28.97e-3  # Molar mass of dry air [kg/mol]
+const molar_mass_water_vapor = 18.015e-3 # Molar mass of water vapor [kg/mol]
 
-const Rᵛ_air = gas_constant / molar_mass_moist_air  # Specific gas constant for moist air [J/kg/K]
+const κᵈ = 2//7  # Poisson constant (?) for a diatomic ideal gas
+const γᵈ = 7//5  # Heat capacity ratio for a diatomic ideal gas
+
+const Rᵈ_air = gas_constant / molar_mass_dry_air      # Specific gas constant for dry air [J/kg/K]
+const Rᵛ_air = gas_constant / molar_mass_water_vapor  # Specific gas constant for water vapor [J/kg/K]
+
+const cₚ_dry = Rᵈ_air / κᵈ      # Isobaric specific heat for dry air [J/kg/K]
+const cᵥ_dry = cₚ_dry - Rᵈ_air  # Isochoric specific heat for dry air [J/kg/K]
 
 ####
 #### Dry ideal gas
