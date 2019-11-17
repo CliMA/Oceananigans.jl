2114e7e1efedf27bd4fdd5c0ec7743025178469f
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-11-16 19:11:14 -0500

    More plots of hydrostatic adjustment

--- examples/isothermal_atmosphere_hydrostatic_adjustment.jl
+++ examples/isothermal_atmosphere_hydrostatic_adjustment.jl
@@ -32,7 +32,7 @@ p₀(x, y, z) = pₛ * exp(-z/H)
 #### Sponge layer forcing to damp vertically propagating acoustic waves at the top.
 ####
 
-const τ⁻¹ = 1    # Damping/relaxation time scale [s⁻¹].
+const τ⁻¹ = 1    # Damping/relaxation time scale [s⁻¹]. This is very strong damping.
 const Δμ = 0.1L  # Sponge layer width [m] set to 10% of the domain height.
 @inline μ(z, Lz) = τ⁻¹ * exp(-(Lz-z) / Δμ)
 
@@ -54,27 +54,56 @@ model = CompressibleModel(grid=grid, buoyancy=buoyancy, reference_pressure=pₛ,
 set!(model.density, ρ₀)
 set!(model.tracers.Θᵐ, Θ₀)
 
+# Initial profiles
 Θ₀_prof = model.tracers.Θᵐ[1, 1, 1:Nz]
 ρ₀_prof = model.density[1, 1, 1:Nz]
+
+# Arrays we will use to store a time series of ρw(z = L/2).
+times = [model.clock.time]
+ρw_ts = [model.momenta.W[1, 1, Int(Nz/2)]]
 
 ####
 #### Time step and keep plotting vertical profiles of ρθ′, ρw, and ρ′.
 ####
 
-for i = 1:500
+while model.clock.time < 500
     time_step!(model; Δt=0.5, Nt=10)
 
     Θ_prof = model.tracers.Θᵐ[1, 1, 1:Nz] .- Θ₀_prof
     W_prof = model.momenta.W[1, 1, 1:Nz+1]
     ρ_prof = model.density[1, 1, 1:Nz] .- ρ₀_prof
 
-    Θ_plot = plot(Θ_prof, grid.zC, xlim=(-2.5, 2.5), label="")
-    W_plot = plot(W_prof, grid.zF, xlim=(-1.2, 1.2), label="")
-    ρ_plot = plot(ρ_prof, grid.zC, xlim=(-0.01, 0.01), label="")
+    Θ_plot = plot(Θ_prof, grid.zC, xlim=(-2.5, 2.5), xlabel="Theta_prime", ylabel="z (m)", label="")
+    W_plot = plot(W_prof, grid.zF, xlim=(-1.2, 1.2), xlabel="rho*w", label="")
+    ρ_plot = plot(ρ_prof, grid.zC, xlim=(-0.01, 0.01), xlabel="rho", label="")
 
     t_str = @sprintf("t = %d s", model.clock.time)
-    display(plot(Θ_plot, W_plot, ρ_plot, title=["Theta_prime" "W" "rho, $t_str"], layout=(1, 3), show=true))
+    display(plot(Θ_plot, W_plot, ρ_plot, title=["" "$t_str" ""], layout=(1, 3), show=true))
 
-    # CFL = maximum(abs, model.momenta.W.data) * Δtp / grid.Δz
-    # @show CFL
+    push!(times, model.clock.time)
+    push!(ρw_ts, model.momenta.W[1, 1, Int(Nz/2)])
 end
+
+####
+#### Plot the initial profile and the hydrostatically balanced profile.
+####
+
+ρ∞_prof = model.density[1, 1, 1:Nz]
+
+θ₀_prof = Θ₀_prof ./ ρ₀_prof
+θ∞_prof = model.tracers.Θᵐ[1, 1, 1:Nz] ./ ρ∞_prof
+
+θ_plot = plot(θ₀_prof, grid.zC, xlabel="theta (K)", ylabel="z (m)", label="initial", legend=:topleft)
+plot!(θ_plot, θ∞_prof, grid.zC, label="balanced")
+
+ρ_plot = plot(ρ₀_prof, grid.zC, xlabel="ρ (kg/m³)", ylabel="z (m)", label="initial")
+plot!(ρ_plot, ρ∞_prof, grid.zC, label="balanced")
+
+display(plot(θ_plot, ρ_plot, show=true))
+
+
+####
+#### Plot timeseries of maximum ρw
+####
+
+plot(times, ρw_ts, linewidth=2, xlabel="time (s)", ylabel="rho*w(z=$(Int(L/2)) m)", label="", show=true)
