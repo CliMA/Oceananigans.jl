80e44ec10add456dec4bea0abe45dc1ed76b7d7a
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-12-02 09:51:58 -0500

    Integrate with Grid submodule.

--- src/Grids/Grids.jl
+++ src/Grids/Grids.jl
@@ -1,10 +1,11 @@
 module Grids
 
-export RegularCartesianGrid
+export RegularCartesianGrid, VerticallyStretchedCartesianGrid
 
 using Oceananigans
 
 include("grid_utils.jl")
 include("regular_cartesian_grid.jl")
+include("verticall_stretched_cartesian_grid.jl")
 
 end
