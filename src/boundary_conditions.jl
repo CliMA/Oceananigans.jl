bcfbb41c9906b6d531e04668dc49eb01b055e7c9
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-12-03 20:05:10 -0500

    Add north and south aliases for y BCs.

--- src/boundary_conditions.jl
+++ src/boundary_conditions.jl
@@ -185,17 +185,22 @@ const CBC = CoordinateBoundaryConditions
 PeriodicBCs() = CBC(PeriodicBC(), PeriodicBC())
 
 # Here we overload setproperty! and getproperty to permit users to call
-# the 'left' and 'right' bcs in the z-direction 'bottom' and 'top'.
+# the 'left' and 'right' bcs in the z-direction 'bottom' and 'top'
+# and the 'left' and 'right' bcs in the y-direction 'south' and 'north'.
 # Note that 'right' technically corresponds to face point N+1.
 Base.setproperty!(cbc::CBC, side::Symbol, bc) = setbc!(cbc, Val(side), bc)
 setbc!(cbc::CBC, ::Val{S}, bc) where S = setfield!(cbc, S, bc)
 setbc!(cbc::CBC, ::Val{:bottom}, bc) = setfield!(cbc, :left, bc)
 setbc!(cbc::CBC, ::Val{:top}, bc) = setfield!(cbc, :right, bc)
+setbc!(cbc::CBC, ::Val{:south}, bc) = setfield!(cbc, :left, bc)
+setbc!(cbc::CBC, ::Val{:north}, bc) = setfield!(cbc, :right, bc)
 
 Base.getproperty(cbc::CBC, side::Symbol) = getbc(cbc, Val(side))
 getbc(cbc::CBC, ::Val{S}) where S = getfield(cbc, S)
 getbc(cbc::CBC, ::Val{:bottom}) = getfield(cbc, :left)
 getbc(cbc::CBC, ::Val{:top}) = getfield(cbc, :right)
+getbc(cbc::CBC, ::Val{:south}) = getfield(cbc, :left)
+getbc(cbc::CBC, ::Val{:north}) = getfield(cbc, :right)
 
 #####
 ##### Boundary conditions for Fields
