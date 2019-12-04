0d763a8a4bcd1e2729bae84e58eae912acb8b10b
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-12-03 19:34:06 -0500

    2D loop macros

--- src/utils.jl
+++ src/utils.jl
@@ -23,6 +23,28 @@ macro loop_xyz(i, j, k, grid, expr)
         end)
 end
 
+macro loop_xy(i, j, grid, expr)
+    return esc(
+        quote
+            @loop for $j in (1:$grid.Ny; (blockIdx().y - 1) * blockDim().y + threadIdx().y)
+                @loop for $i in (1:$grid.Nx; (blockIdx().x - 1) * blockDim().x + threadIdx().x)
+                    $expr
+                end
+            end
+        end)
+end
+
+macro loop_xz(i, k, grid, expr)
+    return esc(
+        quote
+            @loop for $k in (1:$grid.Nz; (blockIdx().y - 1) * blockDim().y + threadIdx().y)
+                @loop for $i in (1:$grid.Nx; (blockIdx().x - 1) * blockDim().x + threadIdx().x)
+                    $expr
+                end
+            end
+        end)
+end
+
 ####
 #### Convinient definitions
 ####
