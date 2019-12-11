b7bfd311b1215e482fdf50c06e964a3af369bbce
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-12-10 21:28:16 -0500

    Perform backslash Tridiagonal solves on CPU to avoid GPU scalar ops.

--- test/test_solvers.jl
+++ test/test_solvers.jl
@@ -182,13 +182,17 @@ end
 function can_solve_single_tridiagonal_system(arch, N)
     ArrayType = array_type(arch)
 
-    a = rand(N-1) |> ArrayType
-    b = 3 .+ rand(N) |> ArrayType  # +3 to ensure diagonal dominance.
-    c = rand(N-1) |> ArrayType
-    f = rand(N) |> ArrayType
+    a = rand(N-1)
+    b = 3 .+ rand(N) # +3 to ensure diagonal dominance.
+    c = rand(N-1)
+    f = rand(N)
 
+    # Solve the system with backslash on the CPU to avoid scalar operations on the GPU.
     M = Tridiagonal(a, b, c)
-    ϕ_correct = M \ f  # This solve probably invokes scalar CuArray operations on the GPU!
+    ϕ_correct = M \ f
+
+    # Convert to CuArray if needed.
+    a, b, c, f = ArrayType.([a, b, c, f])
 
     ϕ = reshape(zeros(N), (1, 1, N)) |> ArrayType
 
@@ -197,7 +201,7 @@ function can_solve_single_tridiagonal_system(arch, N)
 
     solve_batched_tridiagonal_system!(ϕ, arch, btsolver)
 
-    return ϕ[:] ≈ ϕ_correct
+    return Array(ϕ[:]) ≈ ϕ_correct
 end
 
 function can_solve_single_tridiagonal_system_with_functions(arch, N)
@@ -205,17 +209,21 @@ function can_solve_single_tridiagonal_system_with_functions(arch, N)
 
     grid = RegularCartesianGrid(size=(1, 1, N), length=(1, 1, 1))
 
-    a = rand(N-1) |> ArrayType
-    c = rand(N-1) |> ArrayType
+    a = rand(N-1)
+    c = rand(N-1)
 
     @inline b(i, j, k, grid, p) = 3 .+ cos(2π*grid.zC[k])  # +3 to ensure diagonal dominance.
     @inline f(i, j, k, grid, p) = sin(2π*grid.zC[k])
 
-    bₐ = [b(1, 1, k, grid, nothing) for k in 1:N] |> ArrayType
-    fₐ = [f(1, 1, k, grid, nothing) for k in 1:N] |> ArrayType
+    bₐ = [b(1, 1, k, grid, nothing) for k in 1:N]
+    fₐ = [f(1, 1, k, grid, nothing) for k in 1:N]
 
+    # Solve the system with backslash on the CPU to avoid scalar operations on the GPU.
     M = Tridiagonal(a, bₐ, c)
-    ϕ_correct = M \ fₐ  # This solve probably invokes scalar CuArray operations on the GPU!
+    ϕ_correct = M \ fₐ
+
+    # Convert to CuArray if needed.
+    a, c = ArrayType.([a, c])
 
     ϕ = reshape(zeros(N), (1, 1, N)) |> ArrayType
 
@@ -223,23 +231,27 @@ function can_solve_single_tridiagonal_system_with_functions(arch, N)
 
     solve_batched_tridiagonal_system!(ϕ, arch, btsolver)
 
-    return ϕ[:] ≈ ϕ_correct
+    return Array(ϕ[:]) ≈ ϕ_correct
 end
 
 function can_solve_batched_tridiagonal_system_with_3D_RHS(arch, Nx, Ny, Nz)
     ArrayType = array_type(arch)
 
-    a = rand(Nz-1) |> ArrayType
-    b = 3 .+ rand(Nz) |> ArrayType  # +3 to ensure diagonal dominance.
-    c = rand(Nz-1) |> ArrayType
-    f = rand(Nx, Ny, Nz) |> ArrayType
+    a = rand(Nz-1)
+    b = 3 .+ rand(Nz) # +3 to ensure diagonal dominance.
+    c = rand(Nz-1)
+    f = rand(Nx, Ny, Nz)
 
     M = Tridiagonal(a, b, c)
-    ϕ_correct = zeros(Nx, Ny, Nz) |> ArrayType
+    ϕ_correct = zeros(Nx, Ny, Nz)
 
+    # Solve the systems with backslash on the CPU to avoid scalar operations on the GPU.
     for i = 1:Nx, j = 1:Ny
         ϕ_correct[i, j, :] .= M \ f[i, j, :]
     end
+
+    # Convert to CuArray if needed.
+    a, b, c, f = ArrayType.([a, b, c, f])
 
     grid = RegularCartesianGrid(size=(Nx, Ny, Nz), length=(1, 1, 1))
     btsolver = BatchedTridiagonalSolver(arch; dl=a, d=b, du=c, f=f, grid=grid)
@@ -248,7 +260,7 @@ function can_solve_batched_tridiagonal_system_with_3D_RHS(arch, Nx, Ny, Nz)
 
     solve_batched_tridiagonal_system!(ϕ, arch, btsolver)
 
-    return ϕ ≈ ϕ_correct
+    return Array(ϕ) ≈ ϕ_correct
 end
 
 function can_solve_batched_tridiagonal_system_with_3D_functions(arch, Nx, Ny, Nz)
@@ -256,28 +268,32 @@ function can_solve_batched_tridiagonal_system_with_3D_functions(arch, Nx, Ny, Nz
 
     grid = RegularCartesianGrid(size=(Nx, Ny, Nz), length=(1, 1, 1))
 
-    a = rand(Nz-1) |> ArrayType
-    c = rand(Nz-1) |> ArrayType
+    a = rand(Nz-1)
+    c = rand(Nz-1)
 
     @inline b(i, j, k, grid, p) = 3 + grid.xC[i]*grid.yC[j] * cos(2π*grid.zC[k])
     @inline f(i, j, k, grid, p) = (grid.xC[i] + grid.yC[j]) * sin(2π*grid.zC[k])
 
-    ϕ_correct = zeros(Nx, Ny, Nz) |> ArrayType
+    ϕ_correct = zeros(Nx, Ny, Nz)
 
+    # Solve the system with backslash on the CPU to avoid scalar operations on the GPU.
     for i = 1:Nx, j = 1:Ny
-        bₐ = [b(i, j, k, grid, nothing) for k in 1:Nz] |> ArrayType
+        bₐ = [b(i, j, k, grid, nothing) for k in 1:Nz]
         M = Tridiagonal(a, bₐ, c)
 
-        fₐ = [f(i, j, k, grid, nothing) for k in 1:Nz] |> ArrayType
+        fₐ = [f(i, j, k, grid, nothing) for k in 1:Nz]
         ϕ_correct[i, j, :] .= M \ fₐ
     end
+
+    # Convert to CuArray if needed.
+    a, c = ArrayType.([a, c])
 
     btsolver = BatchedTridiagonalSolver(arch; dl=a, d=b, du=c, f=f, grid=grid)
 
     ϕ = zeros(Nx, Ny, Nz) |> ArrayType
     solve_batched_tridiagonal_system!(ϕ, arch, btsolver)
 
-    return ϕ ≈ ϕ_correct
+    return Array(ϕ) ≈ ϕ_correct
 end
 
 @testset "Solvers" begin
