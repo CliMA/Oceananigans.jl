7ca306ce786486ec656586ece5cdab29ca62ae5e
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-12-14 22:47:15 -0500

    `DistributedModel` constructor that checks for consistent ranks

--- src/distributed_model.jl
+++ src/distributed_model.jl
@@ -17,3 +17,34 @@ function validate_tupled_argument(arg, argtype, argname)
     all(arg .> 0)           || throw(ArgumentError("Elements of $argname=$arg must be > 0!"))
     return nothing
 end
+
+function DistributedModel(; ranks, model_kwargs...)
+    validate_tupled_argument(ranks, Int, "ranks")
+    Rx, Ry, Rz = ranks
+    total_ranks = Rx*Ry*Rz
+
+    MPI.Init()
+    comm = MPI.COMM_WORLD
+
+    mpi_ranks = MPI.Comm_size(comm)
+    my_rank   = MPI.Comm_rank(comm)
+
+    if my_rank == 0
+        if total_ranks != mpi_ranks
+            throw(ArgumentError("ranks=($Rx, $Ry, $Rz) [$total_ranks total] inconsistent " *
+                                "with number of MPI ranks: $mpi_ranks. Exiting with code 1."))
+            MPI.Finalize()
+            exit(code=1)
+        end
+    end
+
+    # Ensure that ranks 1:N don't go ahead if total_ranks != mpi_ranks.
+    MPI.Barrier(comm)
+
+    model_id = my_rank + 1
+    println("Model #$my_rank reporting in")
+
+    return DistributedModel(ranks, nothing, nothing, comm)
+end
+
+dm = DistributedModel(ranks=(2, 2, 1))
