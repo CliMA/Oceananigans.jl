66cb913016d2f8484c1baf2735d83b2ad9dab2b0
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-01-04 03:49:57 -0500

    Isolate pressure solve related functions into Oceananigans.Solvers

--- src/Solvers/Solvers.jl
+++ src/Solvers/Solvers.jl
@@ -2,7 +2,8 @@ module Solvers
 
 export
     PoissonSolver, PoissonBCs, solve_poisson_3d!,
-    BatchedTridiagonalSolver, solve_batched_tridiagonal_system!
+    BatchedTridiagonalSolver, solve_batched_tridiagonal_system!,
+    solve_for_pressure!, calculate_poisson_right_hand_side!, idct_permute!
 
 
 using Oceananigans.Grids
@@ -16,5 +17,7 @@ include("solver_utils.jl")
 include("poisson_solver_cpu.jl")
 include("poisson_solver_gpu.jl")
 include("batched_tridiagonal_solver.jl")
+
+include("tmp.jl")
 
 end
