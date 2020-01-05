db394c05d4e6add4661c813e6cfc454af9d1e906
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-01-05 10:09:36 -0500

    No need to `MPI.Finalize()` anymore

--- src/Distributed/test_distributed_model.jl
+++ src/Distributed/test_distributed_model.jl
@@ -14,5 +14,3 @@ dm = DistributedModel(ranks = (2, 2, 2), size = (16, 16, 16),
 my_rank = MPI.Comm_rank(MPI.COMM_WORLD)
 @info "Rank $my_rank: $(dm.connectivity), $(dm.model.grid.zF[end])"
 @info "u.x BCs: $(dm.model.boundary_conditions.solution.u.x)"
-
-MPI.Finalize()
