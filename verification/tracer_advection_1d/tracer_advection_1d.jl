e7be5b04ec196b8745dcf1a48f7a51b5ade6f01a
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-01-12 12:17:36 -0500

    First-order upwind to feel good

--- verification/tracer_advection_1d/tracer_advection_1d.jl
+++ verification/tracer_advection_1d/tracer_advection_1d.jl
@@ -2,25 +2,49 @@ using Printf
 using OffsetArrays
 using Plots
 
+#####
+##### Advection or flux-reconstruction schemes
+#####
+
 abstract type AbstractAdvectionScheme end
 
-# Second order advection
+struct FirstOrderUpwind <: AbstractAdvectionScheme end
+@inline ∂x_advective_flux(i, N, Δx, u, ϕ, ::FirstOrderUpwind) =
+    max(u[i], 0) * (ϕ[i] - ϕ[i-1])/Δx + min(u[i], 0) * (ϕ[i+1] - ϕ[i])/Δx
+
 struct SecondOrderCentered <: AbstractAdvectionScheme end
-@inline advective_flux(i, N, u, ϕ) = u[i] * (ϕ[i-1] + ϕ[i]) / 2
-@inline ∂x_advective_flux(i, N, Δx, u, ϕ) = (advective_flux(i+1, N, u, ϕ) - advective_flux(i, N, u, ϕ)) / Δx
+@inline advective_flux(i, N, u, ϕ, ::SecondOrderCentered) = u[i] * (ϕ[i-1] + ϕ[i]) / 2
+@inline ∂x_advective_flux(i, N, Δx, u, ϕ, scheme) =
+    (advective_flux(i+1, N, u, ϕ, scheme) - advective_flux(i, N, u, ϕ, scheme)) / Δx
+
+#####
+##### Forward Euler time-stepping of advection equation
+#####
 
 function time_step_advection!(ϕ, u, N, Δx, Δt, scheme)
     ϕ[0], ϕ[N+1] = ϕ[N], ϕ[1]  # Fill ghost points to enforce periodic boundary conditions.
     for i in 1:N
-        ϕ[i] = ϕ[i] - Δt * ∂x_advective_flux(i, N, Δx, u, ϕ)
+        ϕ[i] = ϕ[i] - Δt * ∂x_advective_flux(i, N, Δx, u, ϕ, scheme)
     end
 end
+
+#####
+##### Initial conditions and analytic solutions
+#####
 
 # Convert x to periodic range in [-L/2, L/2] assuming u = 1.
 @inline x′(x, t, L) = mod(x + L/2 - t, L) - L/2
 
 # Analytic solutions
 @inline ϕ_Gaussian(x, t, L; a=1, c=1/8) = a*exp(-x′(x, t, L)^2 / (2c^2))
+@inline ϕ_Square(x, t, L; w=0.15) = -w <= x′(x, t, L) <= w ? 1.0 : 0.0
+
+ic_name(::typeof(ϕ_Gaussian)) = "Gaussian"
+ic_name(::typeof(ϕ_Square))   = "Square"
+
+#####
+##### Experiment functions
+#####
 
 function create_figure(N, L, CFL, ϕₐ, scheme)
     Δx = L/N
@@ -40,10 +64,10 @@ function create_figure(N, L, CFL, ϕₐ, scheme)
 
     title = @sprintf("%s N=%d CFL=%.1f", typeof(scheme), N, CFL)
     ϕ_analytic = ϕₐ.(x, T, L)
-    p = plot(x, ϕ_analytic, label="analytic", title=title, xlims=(-0.5, 0.5), ylims=(-0.2, 1.2), dpi=200)
+    p = plot(x, ϕ_analytic, label="Analytic", title=title, xlims=(-0.5, 0.5), ylims=(-0.2, 1.2), dpi=200)
     plot!(p, x, ϕ[1:N], label="$(typeof(scheme))")
 
-    fig_filename = @sprintf("%s_N%d_CFL%.1f.png", typeof(scheme), N, CFL)
+    fig_filename = @sprintf("%s_%s_N%d_CFL%.1f.png", ic_name(ϕₐ), typeof(scheme), N, CFL)
     savefig(p, fig_filename)
 
     return nothing
@@ -79,21 +103,27 @@ function create_animation(N, L, CFL, ϕₐ, scheme)
         plot!(x, ϕ[1:N], label="$(typeof(scheme))")
     end every every(N)
 
-    anim_filename = @sprintf("%s_N%d_CFL%.1f.mp4", typeof(scheme), N, CFL)
+    anim_filename = @sprintf("%s_%s_N%d_CFL%.1f.mp4", ic_name(ϕₐ), typeof(scheme), N, CFL)
     mp4(anim, anim_filename, fps = 30)
 
     return nothing
 end
 
-L = 1
-Ns = [16, 32, 64, 128]
-CFLs = [0.1, 0.3, 0.5]
-schemes = (SecondOrderCentered(),)
+#####
+##### Run 1D tracer advection experiments
+#####
 
-for N in Ns, CFL in CFLs, scheme in schemes
-    @info @sprintf("Creating one-revolution figure [%s, N=%d, CFL=%.1f]...", typeof(scheme), N, CFL)
-    create_figure(N, L, CFL, ϕ_Gaussian, scheme)
-    
+L = 1
+ϕs = (ϕ_Square,)
+schemes = (FirstOrderUpwind(),)
+Ns = [16, 32, 64, 128]
+CFLs = [0.1, 0.5]
+
+for ϕ in ϕs, scheme in schemes, N in Ns, CFL in CFLs
+    @info @sprintf("Creating one-revolution figure [%s, %s, N=%d, CFL=%.1f]...", ic_name(ϕ), typeof(scheme), N, CFL)
+    create_figure(N, L, CFL, ϕ, scheme)
+
     # @info @sprintf("Creating two-revolution animation [%s, N=%d, CFL=%.1f]...", typeof(scheme), N, CFL)
     # create_animation(N, L, CFL, ϕ, scheme)
 end
+
