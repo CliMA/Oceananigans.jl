8a9cc1acbb5054b6046a4ca1a6d70712bd04845e
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-01-13 13:09:37 -0500

    Switch to DifferentialEquations.jl for time stepping

--- verification/tracer_advection_1d/tracer_advection_1d.jl
+++ verification/tracer_advection_1d/tracer_advection_1d.jl
@@ -1,5 +1,6 @@
 using Printf
 using OffsetArrays
+using DifferentialEquations
 using Plots
 
 #####
@@ -21,10 +22,10 @@ struct SecondOrderCentered <: AbstractAdvectionScheme end
 ##### Forward Euler time-stepping of advection equation
 #####
 
-function time_step_advection!(ϕ, u, N, Δx, Δt, scheme)
-    ϕ[0], ϕ[N+1] = ϕ[N], ϕ[1]  # Fill ghost points to enforce periodic boundary conditions.
-    for i in 1:N
-        ϕ[i] = ϕ[i] - Δt * ∂x_advective_flux(i, N, Δx, u, ϕ, scheme)
+function advection!(∂ϕ∂t, ϕ, p, t)
+    ϕ[0], ϕ[p.N+1] = ϕ[p.N], ϕ[1]  # Fill ghost points to enforce periodic boundary conditions.
+    for i in 1:p.N
+        ∂ϕ∂t[i] = ∂x_advective_flux(i, p.N, p.Δx, p.u, ϕ, p.scheme)
     end
 end
 
@@ -46,28 +47,32 @@ ic_name(::typeof(ϕ_Square))   = "Square"
 ##### Experiment functions
 #####
 
-function create_figure(N, L, CFL, ϕₐ, scheme)
+function create_figure(N, L, CFL, ϕₐ, time_stepper, scheme)
     Δx = L/N
     x = range(-L/2 + Δx/2, L/2 - Δx/2; length=N)
     ϕ₀ = ϕₐ.(x, 0, L)
 
     u = OffsetArray(ones(N+2), 0:N+1)
-    ϕ = OffsetArray([0, ϕ₀..., 0], 0:N+1)
+    ϕ₀ = OffsetArray([0, ϕ₀..., 0], 0:N+1)
 
-    T = 1  # one revolution end time
+    T = 1.0  # one revolution end time
     Δt = CFL * Δx / maximum(abs, u)
     nt = ceil(T/Δt)
 
-    for _ in 1:nt
-        time_step_advection!(ϕ, u, N, Δx, Δt, scheme)
-    end
+    tspan = (0.0, T)
+    params = (N=N, Δx=Δx, u=u, scheme=scheme)
+    prob = ODEProblem(advection!, ϕ₀, tspan, params)
+    sol = solve(prob, time_stepper, adaptive=false, dt=Δt)
+    ϕ = sol[:, end]
 
-    title = @sprintf("%s N=%d CFL=%.1f", typeof(scheme), N, CFL)
+    @info "Solver return code: $(sol.retcode)"
+
+    title = @sprintf("%s %s N=%d CFL=%.1f", typeof(scheme), typeof(time_stepper), N, CFL)
     ϕ_analytic = ϕₐ.(x, T, L)
     p = plot(x, ϕ_analytic, label="Analytic", title=title, xlims=(-0.5, 0.5), ylims=(-0.2, 1.2), dpi=200)
     plot!(p, x, ϕ[1:N], label="$(typeof(scheme))")
 
-    fig_filename = @sprintf("%s_%s_N%d_CFL%.1f.png", ic_name(ϕₐ), typeof(scheme), N, CFL)
+    fig_filename = @sprintf("%s_%s_%s_N%d_CFL%.1f.png", ic_name(ϕₐ), typeof(scheme), typeof(time_stepper), N, CFL)
     savefig(p, fig_filename)
 
     return nothing
@@ -119,9 +124,11 @@ schemes = (FirstOrderUpwind(),)
 Ns = [16, 32, 64, 128]
 CFLs = [0.1, 0.5]
 
+create_figure(64, L, 0.1, ϕ_Gaussian, Euler(), SecondOrderCentered())
+
 for ϕ in ϕs, scheme in schemes, N in Ns, CFL in CFLs
-    @info @sprintf("Creating one-revolution figure [%s, %s, N=%d, CFL=%.1f]...", ic_name(ϕ), typeof(scheme), N, CFL)
-    create_figure(N, L, CFL, ϕ, scheme)
+    # @info @sprintf("Creating one-revolution figure [%s, %s, N=%d, CFL=%.1f]...", ic_name(ϕ), typeof(scheme), N, CFL)
+    # create_figure(N, L, CFL, ϕ, scheme)
 
     # @info @sprintf("Creating two-revolution animation [%s, N=%d, CFL=%.1f]...", typeof(scheme), N, CFL)
     # create_animation(N, L, CFL, ϕ, scheme)
