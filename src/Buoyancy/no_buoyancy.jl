df2bfbf8aa5c3097f5650244828b9ec5a7c9e2fa
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2020-01-22 14:47:32 -0500

    Refactor buoyancy submodule

--- src/Buoyancy/no_buoyancy.jl
+++
@@ -1,9 +0,0 @@
-using Oceananigans: AbstractGrid
-
-required_tracers(::Nothing) = ()
-
-@inline buoyancy_perturbation(i, j, k, grid::AbstractGrid{FT}, ::Nothing, C) where FT = zero(FT)
-
-@inline ∂x_b(i, j, k, grid::AbstractGrid{FT}, ::Nothing, C) where FT = zero(FT)
-@inline ∂y_b(i, j, k, grid::AbstractGrid{FT}, ::Nothing, C) where FT = zero(FT)
-@inline ∂z_b(i, j, k, grid::AbstractGrid{FT}, ::Nothing, C) where FT = zero(FT)
