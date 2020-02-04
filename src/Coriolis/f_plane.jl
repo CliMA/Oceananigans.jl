90993caf6327f717968259792f13a24fb4dedc62
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2020-02-04 08:08:53 -0500

    Changes notation for non-traditional Coriolis force, updates documentation and tests

--- src/Coriolis/f_plane.jl
+++ src/Coriolis/f_plane.jl
@@ -57,47 +57,47 @@ A Coriolis implementation that facilitates non-traditional Coriolis terms in the
 and vertical momentum equations along with the traditional Coriolis terms.
 """
 struct NonTraditionalFPlane{FT} <: AbstractRotation
-    f  :: FT
-    f′ :: FT
+    fz :: FT
+    fy :: FT
 end
 
 """
-    NonTraditionalFPlane([FT=Float64;] f=nothing, f′=nothing,
+    NonTraditionalFPlane([FT=Float64;] fz=nothing, fy=nothing,
                                        rotation_rate=Ω_Earth, latitude=nothing)
 
-Returns a parameter object for constant rotation about an arbitrary axis. The two
-perpendicular components of rotation are given by angular frequencies `f/2` and `f′/2`
-and therefore with background vorticities `f` and `f′`, respectively, which can be
-directly specified.
+Returns a parameter object for constant rotation about an axis in the `y-z` plane
+with `y`- and `z`-components `fy/2` and `fz/2`, and the background vorticity is
+`(0, fy, fz)`.
 
-In oceanography `f` and `f′` represent the components of planetary voriticity which
-are perpendicular and parallel to the surface, respectively.
+In oceanography `fz` and `fy` represent the components of planetary voriticity which
+are perpendicular and parallel to the ocean surface in a domain in which `x, y, z` 
+correspond to the directions east, north, and up.
 
-If `f` and `f′` are not specified, they are calculated from `rotation_rate` and `latitude`
-according to the relations `f = 2*rotation_rate*sind(latitude)` and
-`f′ = 2*rotation_rate*cosd(latitude)`, respectively. By default, `rotation_rate`
+If `fz` and `fy` are not specified, they are calculated from `rotation_rate` and `latitude`
+according to the relations `fz = 2*rotation_rate*sind(latitude)` and
+`fy = 2*rotation_rate*cosd(latitude)`, respectively. By default, `rotation_rate`
 is assumed to be Earth's.
 """
-function NonTraditionalFPlane(FT=Float64; f=nothing, f′=nothing, rotation_rate=Ω_Earth, latitude=nothing)
+function NonTraditionalFPlane(FT=Float64; fz=nothing, fy=nothing, rotation_rate=Ω_Earth, latitude=nothing)
 
-    use_f = !isnothing(f) && !isnothing(f′) && isnothing(latitude)
-    use_planet_parameters = !isnothing(latitude) && isnothing(f) && isnothing(f′)
+    use_f = !isnothing(fz) && !isnothing(fy) && isnothing(latitude)
+    use_planet_parameters = !isnothing(latitude) && isnothing(fz) && isnothing(fy)
 
     if (use_planet_parameters && use_f) || (!use_planet_parameters && !use_f)
         throw(ArgumentError("Either both rotation_rate and latitude must be " *
                             "specified, *or* both f and f′ must be specified."))
     elseif use_planet_parameters
-        f  = 2rotation_rate*sind(latitude)
-        f′ = 2rotation_rate*cosd(latitude)
+        fz = 2rotation_rate*sind(latitude)
+        fy = 2rotation_rate*cosd(latitude)
     end
-    return NonTraditionalFPlane{FT}(f, f′)
+    return NonTraditionalFPlane{FT}(fz, fy)
 end
 
-@inline fv_minus_f′w(i, j, k, grid, coriolis::NonTraditionalFPlane, U) = coriolis.f′ * ℑzᵃᵃᶜ(i, j, k, grid, U.w) - coriolis.f * ℑyᵃᶜᵃ(i, j, k, grid, U.v)
+@inline fᶻv_minus_fʸw(i, j, k, grid, coriolis::NonTraditionalFPlane, U) = coriolis.fy * ℑzᵃᵃᶜ(i, j, k, grid, U.w) - coriolis.fz * ℑyᵃᶜᵃ(i, j, k, grid, U.v)
 
-@inline x_f_cross_U(i, j, k, grid, coriolis::NonTraditionalFPlane, U) =   ℑxᶠᵃᵃ(i, j, k, grid, fv_minus_f′w, coriolis, U)
-@inline y_f_cross_U(i, j, k, grid, coriolis::NonTraditionalFPlane, U) =   coriolis.f  * ℑxyᶜᶠᵃ(i, j, k, grid, U.u)
-@inline z_f_cross_U(i, j, k, grid, coriolis::NonTraditionalFPlane, U) = - coriolis.f′ * ℑxzᶜᵃᶠ(i, j, k, grid, U.u)
+@inline x_f_cross_U(i, j, k, grid, coriolis::NonTraditionalFPlane, U) =   ℑxᶠᵃᵃ(i, j, k, grid, fᶻv_minus_fʸw, coriolis, U)
+@inline y_f_cross_U(i, j, k, grid, coriolis::NonTraditionalFPlane, U) =   coriolis.fz * ℑxyᶜᶠᵃ(i, j, k, grid, U.u)
+@inline z_f_cross_U(i, j, k, grid, coriolis::NonTraditionalFPlane, U) = - coriolis.fy * ℑxzᶜᵃᶠ(i, j, k, grid, U.u)
 
 Base.show(io::IO, f_plane::NonTraditionalFPlane{FT}) where FT =
-    println(io, "Non-Traditional FPlane{$FT}:", @sprintf("f = %.2e, f′ = %.2e", f_plane.f, f_plane.f′))
+    println(io, "Non-Traditional FPlane{$FT}:", @sprintf("fz = %.2e, fy = %.2e", f_plane.fz, f_plane.fy))
