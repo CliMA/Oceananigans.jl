9698a1a29c24722d3d514e4c5a8e20c8d5338ee6
Gregory Wagner <wagner.greg@gmail.com>
2020-02-07 08:16:03 -0500

    Fixes file misnaming in Diagnostics.jl

--- src/Diagnostics/Diagnostics.jl
+++ src/Diagnostics/Diagnostics.jl
@@ -12,7 +12,7 @@ using Oceananigans: AbstractDiagnostic
 include("diagnostics_kernels.jl")
 include("nan_checker.jl")
 include("horizontal_average.jl")
-include("timeseries.jl")
+include("time_series.jl")
 include("field_maximum.jl")
 include("cfl.jl")
 
