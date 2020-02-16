2d3f440252185a27894279d567f90b1c9929880f
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-02-15 21:19:48 -0500

    Refactor field tuples so we can pass in data arrays

--- src/Fields/field_tuples.jl
+++ src/Fields/field_tuples.jl
@@ -1,30 +1,32 @@
 """
-    VelocityFields(arch, grid)
+    VelocityFields(arch, grid; u=zeros(arch, grid), v=zeros(arch, grid), w=zeros(arch, grid))
 
-Return a NamedTuple with fields `u`, `v`, `w` initialized on
-the architecture `arch` and `grid`.
+Return a NamedTuple with fields `u`, `v`, `w` initialized on the architecture `arch`
+and `grid`. Data arrays may be passed via the `u`, `v`, and `w` keyword arguments.
 """
-function VelocityFields(arch, grid)
-    u = FaceFieldX(arch, grid)
-    v = FaceFieldY(arch, grid)
-    w = FaceFieldZ(arch, grid)
+function VelocityFields(arch, grid; u=zeros(arch, grid), v=zeros(arch, grid), w=zeros(arch, grid))
+    u = XFaceField(arch, grid, u)
+    v = YFaceField(arch, grid, v)
+    w = ZFaceField(arch, grid, w)
     return (u=u, v=v, w=w)
 end
 
 """
-    TracerFields(arch, grid)
+    TracerFields(arch, grid, tracer_names; kwargs...)
 
-Return a NamedTuple with tracer fields initialized
-as `CellField`s on the architecture `arch` and `grid`.
+Return a NamedTuple with tracer fields specified by `tracer_names` initialized as
+`CellField`s on the architecture `arch` and `grid`. Optional `kwargs` can be specified
+to assign data arrays to each tracer field.
 """
-function TracerFields(arch, grid, tracernames)
-    tracerfields = Tuple(CellField(arch, grid) for c in tracernames)
-    return NamedTuple{tracernames}(tracerfields)
+function TracerFields(arch, grid, tracer_names; kwargs...)
+    tracer_fields = Tuple(c ∈ keys(kwargs) ? CellField(arch, grid, kwargs[c]) : CellField(arch, grid)
+                          for c in tracer_names)
+    return NamedTuple{tracer_names}(tracer_fields)
 end
 
 TracerFields(arch, grid, ::Union{Tuple{}, Nothing}) = NamedTuple{()}(())
-TracerFields(arch, grid, tracer::Symbol) = TracerFields(arch, grid, tuple(tracer))
-TracerFields(arch, grid, tracers::NamedTuple) = tracers
+TracerFields(arch, grid, tracer::Symbol; kwargs...) = TracerFields(arch, grid, tuple(tracer); kwargs...)
+TracerFields(arch, grid, tracers::NamedTuple; kwargs...) = tracers
 
 tracernames(::Nothing) = ()
 tracernames(name::Symbol) = tuple(name)
@@ -32,31 +34,31 @@ tracernames(names::NTuple{N, Symbol}) where N = :u ∈ names ? names[4:end] : na
 tracernames(::NamedTuple{names}) where names = tracernames(names)
 
 """
-    PressureFields(arch, grid)
+    PressureFields(arch, grid; pHY′=zeros(arch, grid), pNHS=zeros(arch, grid))
 
-Return a NamedTuple with pressure fields `pHY′` and `pNHS`
-initialized as `CellField`s on the architecture `arch` and `grid`.
+Return a NamedTuple with pressure fields `pHY′` and `pNHS` initialized as
+`CellField`s on the architecture `arch` and `grid`. Data arrays may be passed via
+the ``pHY′` and `pNHS` keyword arguments.
 """
-function PressureFields(arch, grid)
-    pHY′ = CellField(arch, grid)
-    pNHS = CellField(arch, grid)
+function PressureFields(arch, grid; pHY′=zeros(arch, grid), pNHS=zeros(arch, grid))
+    pHY′ = CellField(arch, grid, pHY′)
+    pNHS = CellField(arch, grid, pNHS)
     return (pHY′=pHY′, pNHS=pNHS)
 end
 
 """
-    Tendencies(arch, grid, tracernames)
+    Tendencies(arch, grid, tracer_names; kwargs...)
 
-Return a NamedTuple with tendencies for all solution fields
-(velocity fields and tracer fields), initialized on
-the architecture `arch` and `grid`.
+Return a NamedTuple with tendencies for all solution fields (velocity fields and
+tracer fields), initialized on the architecture `arch` and `grid`. Optional `kwargs`
+can be specified to assign data arrays to each tendency field.
 """
-function Tendencies(arch, grid, tracernames)
-
-    velocities = (u = FaceFieldX(arch, grid),
-                  v = FaceFieldY(arch, grid),
-                  w = FaceFieldZ(arch, grid))
-
-    tracers = TracerFields(arch, grid, tracernames)
-
+function Tendencies(arch, grid, tracer_names; kwargs...)
+    velocities = (
+        u = :u ∈ keys(kwargs) ? XFaceField(arch, grid, kwargs[:u]) : XFaceField(arch, grid),
+        v = :v ∈ keys(kwargs) ? YFaceField(arch, grid, kwargs[:v]) : YFaceField(arch, grid),
+        w = :w ∈ keys(kwargs) ? ZFaceField(arch, grid, kwargs[:w]) : ZFaceField(arch, grid)
+    )
+    tracers = TracerFields(arch, grid, tracer_names; kwargs...)
     return merge(velocities, tracers)
 end
--- src/Fields/show_fields.jl
+++ src/Fields/show_fields.jl
@@ -10,7 +10,7 @@ short_show(field::Field) = string("Field at ", show_location(field))
 
 show(io::IO, field::Field{X, Y, Z}) where {X, Y, Z} =
     print(io, "$(short_show(field))\n",
-          "├── data: $(typeof(field.data))\n",
+          "├── data: $(typeof(field.data)), size: $(size(field.data))\n",
           "└── grid: $(typeof(field.grid))\n",
           "    ├── size: $(size(field.grid))\n",
-          "    └── domain: $(show_domain(field.grid))\n")
+          "    └── domain: $(show_domain(field.grid))")
