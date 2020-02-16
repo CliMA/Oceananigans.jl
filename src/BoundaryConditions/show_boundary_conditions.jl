f7db6a9385cd5069bdba95101358bbf226ae699f
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-02-16 11:12:25 -0500

    Better pretty printing for fields

--- src/BoundaryConditions/show_boundary_conditions.jl
+++ src/BoundaryConditions/show_boundary_conditions.jl
@@ -1,3 +1,5 @@
+import Oceananigans.Grids: short_show
+
 #####
 ##### BoundaryCondition
 #####
@@ -8,6 +10,18 @@ Base.show(io::IO, bc::BC{C, T}) where {C, T} =
 #####
 ##### FieldBoundaryConditions
 #####
+
+bctype_str(::FBC)  = "Flux"
+bctype_str(::PBC)  = "Periodic"
+bctype_str(::NPBC) = "NoPenetration"
+bctype_str(::VBC)  = "Value"
+bctype_str(::GBC)  = "Gradient"
+bctype_str(::NFBC) = "NoFlux"
+
+short_show(fbcs::FieldBoundaryConditions) =
+    string("x=(west=$(bctype_str(fbcs.x.left)), east=$(bctype_str(fbcs.x.right))), ",
+           "y=(south=$(bctype_str(fbcs.y.left)), north=$(bctype_str(fbcs.y.right))), ",
+           "z=(bottom=$(bctype_str(fbcs.z.left)), top=$(bctype_str(fbcs.z.right)))")
 
 show_field_boundary_conditions(bcs::FieldBoundaryConditions, padding="") =
     string("Oceananigans.FieldBoundaryConditions (NamedTuple{(:x, :y, :z)}), with boundary conditions", '\n',
--- src/Fields/show_fields.jl
+++ src/Fields/show_fields.jl
@@ -1,16 +1,16 @@
 import Base: show
-
+import Oceananigans.Grids: short_show
 using Oceananigans.Grids: show_domain
 
-show_location(X, Y, Z) = "($(typeof(X())), $(typeof(Y())), $(typeof(Z())))"
-
+location_str(::Face) = "Face"
+location_str(::Cell) = "Cell"
+show_location(X, Y, Z) = "($(location_str(X())), $(location_str(Y())), $(location_str(Z())))"
 show_location(field::AbstractField{X, Y, Z}) where {X, Y, Z} = show_location(X, Y, Z)
 
-short_show(field::Field) = string("Field at ", show_location(field))
+short_show(field::Field) = string("Field located at ", show_location(field))
 
-show(io::IO, field::Field{X, Y, Z}) where {X, Y, Z} =
+show(io::IO, field::Field) =
     print(io, "$(short_show(field))\n",
           "├── data: $(typeof(field.data)), size: $(size(field.data))\n",
-          "└── grid: $(typeof(field.grid))\n",
-          "    ├── size: $(size(field.grid))\n",
-          "    └── domain: $(show_domain(field.grid))")
+          "├── grid: $(short_show(field.grid))\n",
+          "└── boundary conditions: $(short_show(field.boundary_conditions))")
