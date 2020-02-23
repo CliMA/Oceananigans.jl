8d68e69a2973769be7b92d3f8209e3f934efab9b
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-02-23 09:43:18 -0500

    Clean up main time stepping loop

--- src/time_stepping.jl
+++ src/time_stepping.jl
@@ -20,64 +20,60 @@ end
 
 # Adding kwargs... so this time_step! can work with Oceananigans.Simulation
 function time_step!(model::CompressibleModel, Δt; kwargs...)
-    arch = model.architecture
-    grid = model.grid
-    time = model.clock.time
-    coriolis = model.coriolis
-    closure = model.closure
-    tvar = model.thermodynamic_variable
-    microphysics = model.microphysics
-    forcing = model.forcing
-
-    Ũ  = model.momenta
     ρ  = model.total_density
-    ρ̃  = model.gases
-    C̃  = model.tracers
+    ρũ = model.momenta
+    ρc̃ = model.tracers
     K̃  = model.diffusivities
-    F  = model.slow_forcings
-    R  = model.right_hand_sides
+    F̃  = model.slow_forcings
+    R̃  = model.right_hand_sides
     IV = model.intermediate_variables
 
-    g  = model.gravity
-
     # On third RK3 step, we update Φ⁺ instead of model.intermediate_variables
-    Φ⁺ = (Ũ..., tracers = C̃)
+    Φ⁺ = (ρũ..., tracers = ρc̃)
 
-    # On the first and second RK3 steps we want to update intermediate Ũ and C̃.
-    Ũ_names = propertynames(Ũ)
-    IV_Ũ_vals = [getproperty(IV, U) for U in Ũ_names]
-    IV_Ũ = NamedTuple{Ũ_names}(IV_Ũ_vals)
+    # On the first and second RK3 steps we want to update intermediate ρũ and ρc̃.
+    ρũ_names = propertynames(ρũ)
+    ρc̃_names = propertynames(ρc̃)
 
-    C̃_names = propertynames(C̃)
-    IV_C̃_vals = [getproperty(IV.tracers, C) for C in C̃_names]
-    IV_C̃ = NamedTuple{C̃_names}(IV_C̃_vals)
+    IV_ρũ_fields = [getproperty(IV, ρu)         for ρu in ρũ_names]
+    IV_ρc̃_fields = [getproperty(IV.tracers, ρc) for ρc in ρc̃_names]
+
+    IV_ρũ = NamedTuple{ρũ_names}(IV_ρũ_fields)
+    IV_ρc̃ = NamedTuple{ρc̃_names}(IV_ρc̃_fields)
 
     @debug "Computing slow forcings..."
-    update_total_density!(ρ.data, grid, ρ̃, C̃)
-    fill_halo_regions!(merge((Σρ=ρ,), Ũ, C̃), arch)
-    compute_slow_forcings!(F, grid, tvar, ρ̃, g, coriolis, closure, ρ, Ũ, C̃, K̃, forcing, time)
-    fill_halo_regions!(F.ρw, arch)
+    update_total_density!(ρ, model.grid, model.gases, ρc̃)
+    fill_halo_regions!(merge((Σρ=ρ,), ρũ, ρc̃), model.architecture)
 
-    # RK3 time-stepping
+    compute_slow_forcings!(
+        F̃, model.grid, model.thermodynamic_variable, model.gases, model.gravity,
+        model.coriolis, model.closure, ρ, ρũ, ρc̃, K̃, model.forcing, model.clock.time)
+
+    fill_halo_regions!(F̃.ρw, model.architecture)
+
     for rk3_iter in 1:3
         @debug "RK3 step #$rk3_iter..."
-
         @debug "  Computing right hand sides..."
+
         if rk3_iter == 1
-            compute_rhs_args = (R, grid, tvar, ρ̃, g, ρ, Ũ, C̃, F)
-            update_total_density!(ρ.data, grid, ρ̃, C̃)
-            fill_halo_regions!(merge((Σρ=ρ,), Ũ, C̃), arch)
+            compute_rhs_args = (R̃, model.grid, model.thermodynamic_variable,
+                                model.gases, model.gravity, ρ, ρũ, ρc̃, F̃)
+
+            update_total_density!(ρ, model.grid, model.gases, ρc̃)
+            fill_halo_regions!(merge((Σρ=ρ,), ρũ, ρc̃), model.architecture)
         else
-            compute_rhs_args = (R, grid, tvar, ρ̃, g, ρ, IV_Ũ, IV_C̃, F)
-            update_total_density!(ρ.data, grid, ρ̃, IV_C̃)
-            fill_halo_regions!(merge((Σρ=ρ,), IV_Ũ, IV_C̃), arch)
+            compute_rhs_args = (R̃, model.grid, model.thermodynamic_variable,
+                                model.gases, model.gravity, ρ, IV_ρũ, IV_ρc̃, F̃)
+
+            update_total_density!(ρ, model.grid, model.gases, IV_ρc̃)
+            fill_halo_regions!(merge((Σρ=ρ,), IV_ρũ, IV_ρc̃), model.architecture)
         end
 
         compute_right_hand_sides!(compute_rhs_args...)
 
         @debug "  Advancing variables..."
         LHS = rk3_iter == 3 ? Φ⁺ : IV
-        advance_variables!(LHS, grid, Ũ, C̃, R; Δt=rk3_time_step(rk3_iter, Δt))
+        advance_variables!(LHS, model.grid, ρũ, ρc̃, R̃; Δt=rk3_time_step(rk3_iter, Δt))
     end
 
     model.clock.iteration += 1
--- src/time_stepping_kernels.jl
+++ src/time_stepping_kernels.jl
@@ -15,17 +15,17 @@ update_total_density!(model) =
 """
 Slow forcings include viscous dissipation, diffusion, and Coriolis terms.
 """
-function compute_slow_forcings!(F, grid, tvar, gases, gravity, coriolis, closure, ρ, ρũ, ρc̃, K̃, forcing, time)
+function compute_slow_forcings!(F̃, grid, tvar, gases, gravity, coriolis, closure, ρ, ρũ, ρc̃, K̃, forcing, time)
     @inbounds begin
         for k in 1:grid.Nz, j in 1:grid.Ny, i in 1:grid.Nx
-            F.ρu[i, j, k] = FU(i, j, k, grid, coriolis, closure, ρ, ρũ, K̃) + forcing.u(i, j, k, grid, time, ρũ, ρc̃, nothing)
-            F.ρv[i, j, k] = FV(i, j, k, grid, coriolis, closure, ρ, ρũ, K̃) + forcing.v(i, j, k, grid, time, ρũ, ρc̃, nothing)
-            F.ρw[i, j, k] = FW(i, j, k, grid, coriolis, closure, ρ, ρũ, K̃) + forcing.w(i, j, k, grid, time, ρũ, ρc̃, nothing)
+            F̃.ρu[i, j, k] = FU(i, j, k, grid, coriolis, closure, ρ, ρũ, K̃) + forcing.u(i, j, k, grid, time, ρũ, ρc̃, nothing)
+            F̃.ρv[i, j, k] = FV(i, j, k, grid, coriolis, closure, ρ, ρũ, K̃) + forcing.v(i, j, k, grid, time, ρũ, ρc̃, nothing)
+            F̃.ρw[i, j, k] = FW(i, j, k, grid, coriolis, closure, ρ, ρũ, K̃) + forcing.w(i, j, k, grid, time, ρũ, ρc̃, nothing)
         end
 
         for (tracer_index, ρc_name) in enumerate(propertynames(ρc̃))
             ρc   = getproperty(ρc̃, ρc_name)
-            F_ρc = getproperty(F.tracers, ρc_name)
+            F_ρc = getproperty(F̃.tracers, ρc_name)
 
             for k in 1:grid.Nz, j in 1:grid.Ny, i in 1:grid.Nx
                 F_ρc[i, j, k] = FC(i, j, k, grid, closure, tracer_index, ρ, ρc, K̃)
@@ -33,7 +33,7 @@ function compute_slow_forcings!(F, grid, tvar, gases, gravity, coriolis, closure
         end
 
         for k in 1:grid.Nz, j in 1:grid.Ny, i in 1:grid.Nx
-            F.tracers[1].data[i, j, k] += FT(i, j, k, grid, closure, tvar, gases, gravity, ρ, ρũ, ρc̃, K̃)
+            F̃.tracers[1].data[i, j, k] += FT(i, j, k, grid, closure, tvar, gases, gravity, ρ, ρũ, ρc̃, K̃)
         end
 
     end
@@ -42,26 +42,26 @@ end
 """
 Fast forcings include advection, pressure gradient, and buoyancy terms.
 """
-function compute_right_hand_sides!(R, grid, tvar, gases, gravity, ρ, ρũ, ρc̃, F)
+function compute_right_hand_sides!(R̃, grid, tvar, gases, gravity, ρ, ρũ, ρc̃, F̃)
     @inbounds begin
         for k in 1:grid.Nz, j in 1:grid.Ny, i in 1:grid.Nx
-            R.ρu[i, j, k] = RU(i, j, k, grid, tvar, gases, gravity, ρ, ρũ, ρc̃, F.ρu)
-            R.ρv[i, j, k] = RV(i, j, k, grid, tvar, gases, gravity, ρ, ρũ, ρc̃, F.ρv)
-            R.ρw[i, j, k] = RW(i, j, k, grid, tvar, gases, gravity, ρ, ρũ, ρc̃, F.ρw)
+            R̃.ρu[i, j, k] = RU(i, j, k, grid, tvar, gases, gravity, ρ, ρũ, ρc̃, F̃.ρu)
+            R̃.ρv[i, j, k] = RV(i, j, k, grid, tvar, gases, gravity, ρ, ρũ, ρc̃, F̃.ρv)
+            R̃.ρw[i, j, k] = RW(i, j, k, grid, tvar, gases, gravity, ρ, ρũ, ρc̃, F̃.ρw)
         end
 
-        for C_name in propertynames(ρc̃)
-            ρc  = getproperty(ρc̃, C_name)
-            R_C = getproperty(R.tracers, C_name)
-            F_C = getproperty(F.tracers, C_name)
+        for ρc_name in propertynames(ρc̃)
+            ρc   = getproperty(ρc̃, ρc_name)
+            R_ρc = getproperty(R̃.tracers, ρc_name)
+            F_ρc = getproperty(F̃.tracers, ρc_name)
 
             for k in 1:grid.Nz, j in 1:grid.Ny, i in 1:grid.Nx
-                R_C[i, j, k] = RC(i, j, k, grid, ρ, ρũ, ρc, F_C)
+                R_ρc[i, j, k] = RC(i, j, k, grid, ρ, ρũ, ρc, F_ρc)
             end
         end
 
         for k in 1:grid.Nz, j in 1:grid.Ny, i in 1:grid.Nx
-            R.tracers[1].data[i, j, k] += RT(i, j, k, grid, tvar, gases, gravity, ρ, ρũ, ρc̃)
+            R̃.tracers[1].data[i, j, k] += RT(i, j, k, grid, tvar, gases, gravity, ρ, ρũ, ρc̃)
         end
 
     end
@@ -73,21 +73,21 @@ Updates variables according to the RK3 time step:
     2. Φ**     = Φᵗ + Δt/2 * R(Φ*)
     3. Φ(t+Δt) = Φᵗ + Δt   * R(Φ**)
 """
-function advance_variables!(I, grid, ρũᵗ, ρc̃ᵗ, R; Δt)
+function advance_variables!(Ĩ, grid, ρũᵗ, ρc̃ᵗ, R̃; Δt)
     @inbounds begin
         for k in 1:grid.Nz, j in 1:grid.Ny, i in 1:grid.Nx
-            I.ρu[i, j, k] = ρũᵗ.ρu[i, j, k] + Δt * R.ρu[i, j, k]
-            I.ρv[i, j, k] = ρũᵗ.ρv[i, j, k] + Δt * R.ρv[i, j, k]
-            I.ρw[i, j, k] = ρũᵗ.ρw[i, j, k] + Δt * R.ρw[i, j, k]
+            Ĩ.ρu[i, j, k] = ρũᵗ.ρu[i, j, k] + Δt * R̃.ρu[i, j, k]
+            Ĩ.ρv[i, j, k] = ρũᵗ.ρv[i, j, k] + Δt * R̃.ρv[i, j, k]
+            Ĩ.ρw[i, j, k] = ρũᵗ.ρw[i, j, k] + Δt * R̃.ρw[i, j, k]
         end
 
-        for C_name in propertynames(ρc̃ᵗ)
-            ρcᵗ  = getproperty(ρc̃ᵗ, C_name)
-            I_C = getproperty(I.tracers,  C_name)
-            R_C = getproperty(R.tracers,  C_name)
+        for ρc_name in propertynames(ρc̃ᵗ)
+            ρcᵗ  = getproperty(ρc̃ᵗ, ρc_name)
+            I_ρc = getproperty(Ĩ.tracers, ρc_name)
+            R_ρc = getproperty(R̃.tracers, ρc_name)
 
             for k in 1:grid.Nz, j in 1:grid.Ny, i in 1:grid.Nx
-                I_C[i, j, k] = ρcᵗ[i, j, k] + Δt * R_C[i, j, k]
+                I_ρc[i, j, k] = ρcᵗ[i, j, k] + Δt * R_ρc[i, j, k]
             end
         end
     end
