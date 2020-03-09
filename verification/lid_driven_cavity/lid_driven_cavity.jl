c8819fb9c8659da43d02ba37c2055fdb4fc111fd
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-03-08 23:31:58 -0400

    Update Lid-driven cavity script to Oceananigans v0.25

--- verification/lid_driven_cavity/lid_driven_cavity.jl
+++ verification/lid_driven_cavity/lid_driven_cavity.jl
@@ -1,19 +1,13 @@
-using Plots, Printf
-
+using Printf
 using Oceananigans
+using Oceananigans: Face, Cell
 using Oceananigans.Diagnostics
+using Oceananigans.OutputWriters
 using Oceananigans.AbstractOperations
 
-using Oceananigans: Face, Cell, NoPenetrationBC
-
-# Workaround for plotting many frames.
-# See: https://github.com/JuliaPlots/Plots.jl/issues/1723
-# import GR
-# GR.inline("png")
-
-####
-#### Data from tables 1 and 2 of Ghia et al. (1982).
-####
+#####
+##### Data from tables 1 and 2 of Ghia et al. (1982).
+#####
 
 j̃ = [1,   8,      9,      10,     14,     23,     37,     59,     65,  80,     95,     110,    123,    124,    156,    126,    129]
 ỹ = [0.0, 0.0547, 0.0625, 0.0703, 0.1016, 0.1719, 0.2813, 0.4531, 0.5, 0.6172, 0.7344, 0.8516, 0.9531, 0.9609, 0.9688, 0.9766, 1.0]
@@ -23,68 +17,73 @@ ũ = Dict(
     400 => [0.0, -0.08186, -0.09266, -0.10338, -0.14612, -0.24299, -0.32726, -0.17119, -0.11477,  0.02135, 0.16256, 0.29093, 0.55892, 0.61756, 0.68439, 0.75837, 1.0]
 )
 
-####
-#### Model setup
-####
+#####
+##### Model setup
+#####
 
-Nx, Ny, Nz = 1, 128, 128
-Lx, Ly, Lz = 1, 1, 1
+Ny = Nz = 128
+Ly = Lz = 1.0
 
-vbcs = ChannelBCs(top    = BoundaryCondition(Value, 1.0),
-                  bottom = BoundaryCondition(Value, 0.0),
-                  north  = NoPenetrationBC(),
-                  south  = NoPenetrationBC())
+topology = (Flat, Bounded, Bounded)
+domain = (x=(0, 1), y=(0, Ly), z=(0, Lz))
+grid = RegularCartesianGrid(topology=topology, size=(1, Ny, Nz); domain...)
 
-wbcs = ChannelBCs(top    = NoPenetrationBC(),
-                  bottom = NoPenetrationBC(),
-                  north  = BoundaryCondition(Value, 0.0),
-                  south  = BoundaryCondition(Value, 0.0))
+v_bcs = VVelocityBoundaryConditions(grid,
+       top = ValueBoundaryCondition(1.0),
+    bottom = ValueBoundaryCondition(0.0)
+)
 
-bcs = ChannelSolutionBCs(v=vbcs, w=wbcs)
+w_bcs = WVelocityBoundaryConditions(grid,
+    north = ValueBoundaryCondition(0.0),
+    south = ValueBoundaryCondition(0.0)
+)
 
-grid = RegularCartesianGrid(size=(Nx, Ny, Nz), x=(0, Lx), y=(0, Ly), z=(0, Lz))
+Re = 100  # Reynolds number
 
-Re = 100
-model = NonDimensionalModel(grid=grid, Re=Re, Pr=Inf, Ro=Inf,
-                            coriolis=nothing, tracers=nothing, buoyancy=nothing,
-                            boundary_conditions=bcs, forcing=forcing)
-
-nan_checker = NaNChecker(model; frequency=10, fields=Dict(:w => model.velocities.w))
-push!(model.diagnostics, nan_checker)
+model = IncompressibleModel(
+    grid=grid,
+    buoyancy=nothing,
+    tracers=nothing,
+    coriolis=nothing,
+    boundary_conditions = (v=v_bcs, w=w_bcs),
+    closure = ConstantIsotropicDiffusivity(ν=1/Re)
+)
 
 u, v, w = model.velocities
 ζ_op = ∂y(w) - ∂z(v)
-ζ = Field(Face, Face, Cell, model.architecture, model.grid)
+ζ = Field(Cell, Face, Face, model.architecture, model.grid, TracerBoundaryConditions(grid))
 ζ_comp = Computation(ζ_op, ζ)
 
-
-y = collect(model.grid.yC)
-z = collect(model.grid.zC)
-# p = heatmap(y, z, zeros(Ny, Nz), color=:viridis, show=true)
-
-# Δt = 0.5e-4
-
-wizard = TimeStepWizard(cfl=0.1, Δt=1e-6, max_change=1.1, max_Δt=1e-5)
+max_Δt = 0.25 * model.grid.Δy^2 * Re / 2
+wizard = TimeStepWizard(cfl=0.1, Δt=1e-6, max_change=1.1, max_Δt=max_Δt)
 cfl = AdvectiveCFL(wizard)
+dcfl = DiffusiveCFL(wizard)
 
-while model.clock.time < 4e-3
-    t = model.clock.time
+function print_progress(simulation)
+    model = simulation.model
 
-    update_Δt!(wizard, model)
+    # Calculate simulation progress in %.
+    progress = 100 * (model.clock.time / simulation.stop_time)
 
-    time_step!(model; Δt=wizard.Δt, Nt=10, init_with_euler = t == 0 ? true : false)
+    # Find maximum velocities.
+    vmax = maximum(abs, model.velocities.v.data.parent)
+    wmax = maximum(abs, model.velocities.w.data.parent)
 
-    compute!(ζ_comp)
-
-    # heatmap!(p, y, z, interior(ζ)[1, :, :], color=:viridis, show=true)
-    # heatmap!(p, y, z, interior(v)[1, :, :], color=:viridis, show=true)
-
-    v_max = maximum(abs, interior(v))
-    w_max = maximum(abs, interior(w))
-    ζ_max = maximum(abs, interior(ζ))
-    CFL = cfl(model)
-    dCFL = (1/Re) * wizard.Δt / Δ^2
-
-    @printf("Time: %1.2e, Δt: %1.2e CFL: %1.2e, dCFL: %1.2e, max (v, w, ζ): %1.2e, %1.2e, %1.2e\n",
-            model.clock.time, wizard.Δt, CFL, dCFL, v_max, w_max, ζ_max)
+    i, t = model.clock.iteration, model.clock.time
+    @printf("[%06.2f%%] i: %d, t: %.3f, U_max: (%.2e, %.2e), CFL: %.2e, dCFL: %.2e, next Δt: %.2e\n",
+            progress, i, t, vmax, wmax, cfl(model), dcfl(model), simulation.Δt.Δt)
 end
+
+simulation = Simulation(model, Δt=wizard, stop_time=10, progress=print_progress, progress_frequency=20)
+
+fields = Dict("v" => model.velocities.v, "w" => model.velocities.w, "ζ" => model -> ζ_comp(model))
+dims = Dict("ζ" => ("xC", "yF", "zF"))
+global_attributes = Dict("Re" => Re)
+output_attributes = Dict("ζ" => Dict("longname" => "vorticity", "units" => "1/s"))
+
+simulation.output_writers[:fields] =
+    NetCDFOutputWriter(model, fields, filename="lid_driven_cavity_Re$Re.nc", interval=0.1,
+                       global_attributes=global_attributes, output_attributes=output_attributes,
+                       dimensions=dims)
+
+run!(simulation)
