b83618c5af56f14210e06232cdcc82aec233231a
Gregory Wagner <wagner.greg@gmail.com>
2020-03-16 14:18:25 -0400

    Revert "Merge branch 'master' into glw/forced-flow-verification"
    This reverts commit 7a57e5fbf5c10bbcf8d77d77fd894e0b997ec87d, reversing
    changes made to 1c843069024c0136d07f98c5096bcef3399d68be.

--- verification/forced_flow/run_free_slip_simulations.jl
+++
@@ -1,6 +0,0 @@
-# Run 4 simulations:
-
-setup_and_run_free_slip_simulation(Nx=16, Nz=16, CFL=1e-3)
-setup_and_run_free_slip_simulation(Nx=32, Nz=32, CFL=1e-3)
-setup_and_run_free_slip_simulation(Nx=64, Nz=64, CFL=1e-3)
-setup_and_run_free_slip_simulation(Nx=128, Nz=128, CFL=1e-3)
