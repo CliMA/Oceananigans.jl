bbfa7d7fe9571e41398d63853bdd67beaca31be6
Gregory Wagner <wagner.greg@gmail.com>
2020-03-18 08:37:20 -0400

    Minor plotting updates in free decay convergence analysis script

--- verification/forced_flow/analyze_free_decay_convergence.jl
+++ verification/forced_flow/analyze_free_decay_convergence.jl
@@ -18,12 +18,12 @@ fig, ax = subplots()
 loglog(Nx, L₁, linestyle="None", marker="o", label="error, \$L_1\$-norm")
 loglog(Nx, L∞, linestyle="None", marker="^", label="error, \$L_\\infty\$-norm")
 
-loglog(Nx, 200 * Nx.^(-2), "k--", linewidth=1, alpha=0.6, label="\$ 200 N_x^{-2} \$")
+#loglog(Nx, 200 * Nx.^(-2), "k--", linewidth=1, alpha=0.6, label="\$ 200 N_x^{-2} \$")
 loglog(Nx, 5 * Nx.^(-1),   "k-", linewidth=1, alpha=0.6, label="\$ 5 N_x^{-1} \$")
 
 xlabel(L"N_x")
-xticks(Nx, ["\$ 2^$(round(Int, log2(N)))$" for N in Nx])
-ylabel("Norms of the absolute error, \$ | u_{\\mathrm{sim}} - u_{\\mathrm{exact}} | \$")
-title("Convergence for forced free slip")
+xticks(Nx, ["\$ 2^$(round(Int, log2(N))) \$" for N in Nx])
+ylabel("Norms of the absolute error, \$ | u_\\mathrm{simulation} - u_\\mathrm{analytical} | \$")
+title("Convergence for free decay")
 
 legend()
