cfc87e90d4d4fb6e0cd40825e271921ad8ecdea1
Gregory Wagner <wagner.greg@gmail.com>
2020-03-19 09:36:50 -0400

    Rename free dacay to faylor green and add more description of the convergence tests

--- verification/simple_convergence_tests/ConvergenceTests/ConvergenceTests.jl
+++ verification/simple_convergence_tests/ConvergenceTests/ConvergenceTests.jl
@@ -1,3 +1,9 @@
+# # ConvergenceTests
+#
+# This module implements a few simple convergence tests for verifying
+# the expected numerical behavior of the spatial discretization 
+# and time-stepping.
+
 module ConvergenceTests
 
 using JLD2, Statistics
@@ -9,19 +15,37 @@ import Oceananigans: RegularCartesianGrid
 include("file_wrangling.jl")
 include("analysis.jl")
 
+# Exponential decay at a point:
+#
 # c = exp(-t) | ∂c/∂t = - c
+#
 # Tests time-stepper convergence.
+#
 include("PointExponentialDecay.jl")
 
+# Advection and diffusion of a 1D Gaussian:
+#
 # c = 1/√(4πκt) * exp( -(x - Ut)^2 / 4κt )
+#
 # Tests x-advection and y-advection.
+#
 include("GaussianAdvectionDiffusion.jl")
 
+# Advection and diffusion of a 1D cosine:
+#
 # c = exp(-κt) * cos(x - Ut)
+#
 # Tests x-advection and y-advection.
+#
 include("CosineAdvectionDiffusion.jl")
 
+# Free decay of a horizontally-advected Taylor-Green vortex:
+#
+# u(x, y, t) = U + cos(x - Ut) sin(y)
+# v(x, y, t) =   - sin(x - Ut) cos(y)
+#
+include("DoublyPeriodicTaylorGreen.jl")
+
 include("ForcedFlowFreeSlip.jl")
-include("DoublyPeriodicFreeDecay.jl")
 
 end
