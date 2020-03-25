03116b599db321b9badf1226ae0ff30dc9e199b1
Gregory Wagner <wagner.greg@gmail.com>
2020-03-25 11:21:31 -0400

    Adds interior_x_indices etc for each dimension in grid_utils

--- src/Grids/grid_utils.jl
+++ src/Grids/grid_utils.jl
@@ -31,9 +31,13 @@ total_length(::Type{Face}, ::Type{Bounded}, N, H=0) = N + 1 + 2H
 #####
 ##### << Nodes >> 
 #####
-#
+
 @inline interior_indices(loc, topo, N) = 1:N
 @inline interior_indices(::Type{Face}, ::Type{Bounded}, N) = 1:N+1
+
+@inline interior_x_indices(loc, grid) = interior_indices(loc, topology(grid, 1), grid.Nx)
+@inline interior_y_indices(loc, grid) = interior_indices(loc, topology(grid, 2), grid.Ny)
+@inline interior_z_indices(loc, grid) = interior_indices(loc, topology(grid, 3), grid.Nz)
 
 @inline interior_parent_indices(loc, topo, N, H) = 1+H:N+H
 @inline interior_parent_indices(::Type{Face}, ::Type{Bounded}, N, H) = 1+H:N+1+H
