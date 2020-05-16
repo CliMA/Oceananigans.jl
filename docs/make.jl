8194342b4422f98a39d34f7cbd4f6ded6e61c8ea
Navid Constantinou <navidcy@gmail.com>
2020-05-16 14:44:30 +1000

    fix repo url

--- docs/make.jl
+++ docs/make.jl
@@ -49,7 +49,7 @@ Timer(t -> println("."), 0, interval=60)
 format = Documenter.HTML(
     collapselevel = 1,
        prettyurls = get(ENV, "CI", nothing) == "true",
-        canonical = "https://climate-machine.github.io/Oceananigans.jl/latest/"
+        canonical = "https://clima.github.io/Oceananigans.jl/latest/"
 )
 
 makedocs(
@@ -145,5 +145,5 @@ makedocs(
      ]
 )
 
-deploydocs(repo = "github.com/climate-machine/Oceananigans.jl.git",
+deploydocs(repo = "github.com/CliMA/Oceananigans.jl.git",
            push_preview = true)
