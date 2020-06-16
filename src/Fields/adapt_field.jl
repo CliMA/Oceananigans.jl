74721684728008d3d111f82880afd6cae1f94054
Gregory Wagner <wagner.greg@gmail.com>
2020-06-16 10:40:03 -0400

    Grid isbits, no need to adapt

--- src/Fields/adapt_field.jl
+++ src/Fields/adapt_field.jl
@@ -2,5 +2,5 @@ using Adapt
 
 Adapt.adapt_structure(to, field::Field{X, Y, Z}) where {X, Y, Z} =
     Field{X, Y, Z}(Adapt.adapt(to, field.data),
-                   Adapt.adapt(to, field.grid),
+                   grid,
                    Adapt.adapt(to, field.boundary_conditions))
