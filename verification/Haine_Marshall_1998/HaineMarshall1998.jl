6ea706bd37e6ce92bb3109dc8e0ac7a98d606f4f
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-06-17 08:58:02 -0400

    Run simulation and write output

--- verification/Haine_Marshall_1998/HaineMarshall1998.jl
+++ verification/Haine_Marshall_1998/HaineMarshall1998.jl
@@ -1,6 +1,9 @@
+using Printf
 using Oceananigans
 using Oceananigans.BoundaryConditions
 using Oceananigans.Fields
+using Oceananigans.OutputWriters
+using Oceananigans.Diagnostics
 using Oceananigans.Utils
 
 arch = CPU()
@@ -25,7 +28,8 @@ coriolis = FPlane(FT, f=f)
 
 α = 2e-4  # Thermal expansion coefficient [K⁻¹]
 eos = LinearEquationOfState(FT, α=α, β=0)
-buoyancy = SeawaterBuoyancy(FT, equation_of_state=eos, constant_salinity=true)
+buoyancy = BuoyancyTracer()
+# buoyancy = SeawaterBuoyancy(FT, equation_of_state=eos, constant_salinity=true)
 
 κh = νh = 5.0   # Horizontal diffusivity and viscosity [m²/s]
 κv = νv = 0.02  # Vertical diffusivity and viscosity [m²/s]
@@ -36,7 +40,7 @@ B_params = (
     B½ = 1.96e-7,    # Buoyancy flux at midchannel [m²/s³]
     Lᶠ = 10kilometer # Characteristic length scale of the forcing [m]
 )
-B(x, y, p) = p.B½ * (tanh(2 * (y - p.Ly/2) / p.Lᶠ) + 1)  # Surface buoyancy flux [m²/s³]
+B(x, y, t, p) = p.B½ * (tanh(2 * (y - p.Ly/2) / p.Lᶠ) + 1)  # Surface buoyancy flux [m²/s³]
 B_bf = BoundaryFunction{:z, Cell, Cell}(B, B_params)
 top_b_bc = FluxBoundaryCondition(B_bf)
 b_bcs = TracerBoundaryConditions(grid, top=top_b_bc)
@@ -51,7 +55,7 @@ model = IncompressibleModel(
                coriolis = coriolis,
                buoyancy = buoyancy,
                 closure = closure,
-                tracers = (:b, :C),
+                tracers = (:b,),
     boundary_conditions = (b=b_bcs,)
 )
 
@@ -59,5 +63,62 @@ Tₛ  = 12.0     # Surface temperature [°C]
 Nₜₕ = 8.37e-4  # Uniform vertical stratification [s⁻¹]
 B₀(x, y, z) = Nₜₕ^2 * z
 
-set!(model, b=B₀, C=0.0)
+set!(model, b=B₀)
+# set!(model, b=B₀, C=0.0)
+
+fields = Dict(
+    "u" => model.velocities.u,
+    "v" => model.velocities.v,
+    "w" => model.velocities.w,
+    "b" => model.tracers.b
+)
+
+surface_output_writer =
+    NetCDFOutputWriter(model, fields, filename="HaineMarshall1998_surface.nc",
+			           interval=1hour, zC=Nz, zF=Nz)
+
+middepth_output_writer =
+    NetCDFOutputWriter(model, fields, filename="HaineMarshall1998_middepth.nc",
+                       interval=1hour, zC=Int(Nz/2), zF=Int(Nz/2))
+
+zonal_output_writer =
+    NetCDFOutputWriter(model, fields, filename="HaineMarshall1998_zonal.nc",
+                       interval=1hour, yC=Int(Ny/2), yF=Int(Ny/2))
+
+meridional_output_writer =
+    NetCDFOutputWriter(model, fields, filename="HaineMarshall1998_meridional.nc",
+                       interval=1hour, xC=Int(Nx/2), xF=Int(Nx/2))
+
+
+Δt_wizard = TimeStepWizard(cfl=0.3, Δt=10.0, max_change=1.2, max_Δt=600.0)
+cfl = AdvectiveCFL(Δt_wizard)
+
+
+# Take Ni "intermediate" time steps at a time before printing a progress
+# statement and updating the time step.
+Ni = 10
+
+function print_progress(simulation)
+    model = simulation.model
+    i, t = model.clock.iteration, model.clock.time
+
+    progress = 100 * (model.clock.time / end_time)
+
+    umax = maximum(abs, model.velocities.u.data.parent)
+    vmax = maximum(abs, model.velocities.v.data.parent)
+    wmax = maximum(abs, model.velocities.w.data.parent)
+
+    @printf("[%05.2f%%] i: %d, t: %.2e days, umax: (%6.3e, %6.3e, %6.3e) m/s, CFL: %6.4e, next Δt: %.1e s\n",
+    	    progress, i, t / day, umax, vmax, wmax, cfl(model), Δt_wizard.Δt)
+end
+
+end_time = 10day
+simulation = Simulation(model, Δt=Δt_wizard, stop_time=end_time, progress=print_progress, progress_frequency=Ni)
+
+simulation.output_writers[:surface] = surface_output_writer
+simulation.output_writers[:middepth] = middepth_output_writer
+simulation.output_writers[:zonal] = zonal_output_writer
+simulation.output_writers[:meridional] = meridional_output_writer
+
+run!(simulation)
 
