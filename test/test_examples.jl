2572835bcf6a57fec0ae9160de8721c8671f7855
Gregory Wagner <wagner.greg@gmail.com>
2020-07-17 12:43:32 -0400

    Fixes printing of example script content on error

--- test/test_examples.jl
+++ test/test_examples.jl
@@ -28,10 +28,10 @@ function run_example(replace_strings, example_name, module_suffix="")
         @printf "% 3d module Test_%s_%s\n" 1 example_name module_suffix
 
         for (number, line) in enumerate(delineated_file_content)
-            @printf "% 3d %s\n" number line
+            @printf "% 3d %s\n" number+1 line
         end
 
-        @printf("% 3d end # module", number+2)
+        @printf("% 3d end # module", length(delineated_file_content)+2)
 
         # Delete the test script
         rm(test_script_filepath)
