cbff61dcf8622643e89d817333cda6b3b801f04d
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-07-21 10:59:22 -0400

    Organize page hierarchies in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -33,18 +33,88 @@ const EXAMPLES_DIR = joinpath(@__DIR__, "..", "examples")
 const OUTPUT_DIR   = joinpath(@__DIR__, "src/generated")
 
 examples = [
-    "one_dimensional_diffusion.jl",
-    "two_dimensional_turbulence.jl",
-    "ocean_wind_mixing_and_convection.jl",
-    "ocean_convection_with_plankton.jl",
-    "internal_wave.jl",
-    "langmuir_turbulence.jl",
+    # "one_dimensional_diffusion.jl",
+    # "two_dimensional_turbulence.jl",
+    # "ocean_wind_mixing_and_convection.jl",
+    # "ocean_convection_with_plankton.jl",
+    # "internal_wave.jl",
+    # "langmuir_turbulence.jl",
 ]
 
 for example in examples
     example_filepath = joinpath(EXAMPLES_DIR, example)
     Literate.markdown(example_filepath, OUTPUT_DIR, documenter=true)
 end
+
+#####
+##### Organize page hierarchies
+#####
+
+example_pages = [
+    "Architecture" => "model_setup/architecture.md",
+    "Number type" => "model_setup/number_type.md",
+    "Grid" => "model_setup/grids.md",
+    "Clock" => "model_setup/clock.md",
+    "Coriolis (rotation)" => "model_setup/coriolis.md",
+    "Tracers" => "model_setup/tracers.md",
+    "Buoyancy and equation of state" => "model_setup/buoyancy_and_equation_of_state.md",
+    "Boundary conditions" => "model_setup/boundary_conditions.md",
+    "Forcing functions" => "model_setup/forcing_functions.md",
+    "Turbulent diffusivity closures and LES models" => "model_setup/turbulent_diffusivity_closures_and_les_models.md",
+    "Diagnostics" => "model_setup/diagnostics.md",
+    "Output writers" => "model_setup/output_writers.md",
+    "Checkpointing" => "model_setup/checkpointing.md",
+    "Time stepping" => "model_setup/time_stepping.md",
+    "Setting initial conditions" => "model_setup/setting_initial_conditions.md"
+]
+
+physics_pages = [
+    "Navier-Stokes and tracer conservation equations" => "physics/navier_stokes_and_tracer_conservation.md",
+    "Coordinate system and notation" => "physics/coordinate_system_and_notation.md",
+    "The Boussinesq approximation" => "physics/boussinesq_approximation.md",
+    "Coriolis forces" => "physics/coriolis_forces.md",
+    "Buoyancy model and equations of state" => "physics/buoyancy_and_equations_of_state.md",
+    "Turbulence closures" => "physics/turbulence_closures.md",
+    "Surface gravity waves and the Craik-Leibovich approximation" => "physics/surface_gravity_waves.md"
+]
+
+numerical_pages = [
+    "Pressure decomposition" => "numerical_implementation/pressure_decomposition.md",
+    "Time stepping" => "numerical_implementation/time_stepping.md",
+    "Finite volume method" => "numerical_implementation/finite_volume.md",
+    "Spatial operators" => "numerical_implementation/spatial_operators.md",
+    "Boundary conditions" => "numerical_implementation/boundary_conditions.md",
+    "Poisson solvers" => "numerical_implementation/poisson_solvers.md",
+    "Large eddy simulation" => "numerical_implementation/large_eddy_simulation.md"
+]
+
+verification_pages = [
+    "Taylor-Green vortex" => "verification/taylor_green_vortex.md",
+    "Stratified Couette flow" => "verification/stratified_couette_flow.md"
+],
+
+appendix_pages = [
+    "Staggered grid" => "appendix/staggered_grid.md",
+    "Fractional step method" => "appendix/fractional_step.md"
+]
+
+pages = [
+    "Home" => "index.md",
+    "Installation instructions" => "installation_instructions.md",
+    "Using GPUs" => "using_gpus.md",
+    "Examples" => example_pages,
+    "Model setup" => model_setup_pages,
+    "Physics" => physics_pages,
+    "Numerical implementation" => numerical_pages,
+    "Verification experiments" => verification_pages,
+    "Gallery" => "gallery.md",
+    "Performance benchmarks" => "benchmarks.md",
+    "Contributor's guide" => "contributing.md",
+    "Library" => "library.md",
+    "Appendix" => appendix_pages,
+    "Function index" => "function_index.md",
+    "References" => "references.md"
+],
 
 #####
 ##### Build and deploy docs
@@ -71,89 +141,13 @@ makedocs(
               Oceananigans.TurbulenceClosures,
               Oceananigans.AbstractOperations],
    doctest = true,
-    strict = true,
+  # strict = true,
    clean   = true,
  checkdocs = :all,
     format = format,
    authors = "Ali Ramadhan, Gregory Wagner, John Marshall, Jean-Michel Campin, Chris Hill",
   sitename = "Oceananigans.jl",
-     pages = [
-                              "Home" => "index.md",
-         "Installation instructions" => "installation_instructions.md",
-                        "Using GPUs" => "using_gpus.md",
-
-         "Examples" => [
-             "One-dimensional diffusion"        => "generated/one_dimensional_diffusion.md",
-             "Two-dimensional turbulence"       => "generated/two_dimensional_turbulence.md",
-             "Ocean wind mixing and convection" => "generated/ocean_wind_mixing_and_convection.md",
-             "Ocean convection with plankton"   => "generated/ocean_convection_with_plankton.md",
-             "Internal wave"                    => "generated/internal_wave.md",
-             "Langmuir turbulence"              => "generated/langmuir_turbulence.md",
-         ],
-
-         "Model setup" => [
-                       "Overview" => "model_setup/overview.md",
-                   "Architecture" => "model_setup/architecture.md",
-                    "Number type" => "model_setup/number_type.md",
-                           "Grid" => "model_setup/grids.md",
-                          "Clock" => "model_setup/clock.md",
-            "Coriolis (rotation)" => "model_setup/coriolis.md",
-                        "Tracers" => "model_setup/tracers.md",
-            "Buoyancy and equation of state" =>
-                                     "model_setup/buoyancy_and_equation_of_state.md",
-            "Boundary conditions" => "model_setup/boundary_conditions.md",
-              "Forcing functions" => "model_setup/forcing_functions.md",
-            "Turbulent diffusivity closures and LES models" =>
-                                     "model_setup/turbulent_diffusivity_closures_and_les_models.md",
-                    "Diagnostics" => "model_setup/diagnostics.md",
-                 "Output writers" => "model_setup/output_writers.md",
-                  "Checkpointing" => "model_setup/checkpointing.md",
-                  "Time stepping" => "model_setup/time_stepping.md",
-            "Setting initial conditions" =>
-                                     "model_setup/setting_initial_conditions.md"
-         ],
-
-         "Physics" => [
-            "Navier-Stokes and tracer conservation equations" =>
-                                                "physics/navier_stokes_and_tracer_conservation.md",
-            "Coordinate system and notation" => "physics/coordinate_system_and_notation.md",
-              "The Boussinesq approximation" => "physics/boussinesq_approximation.md",
-                           "Coriolis forces" => "physics/coriolis_forces.md",
-            "Buoyancy model and equations of state" =>
-                                                "physics/buoyancy_and_equations_of_state.md",
-                       "Turbulence closures" => "physics/turbulence_closures.md",
-            "Surface gravity waves and the Craik-Leibovich approximation" =>
-                                                "physics/surface_gravity_waves.md"
-        ],
-
-         "Numerical implementation" => [
-             "Pressure decomposition" => "numerical_implementation/pressure_decomposition.md",
-             "Time stepping"          => "numerical_implementation/time_stepping.md",
-             "Finite volume method"   => "numerical_implementation/finite_volume.md",
-             "Spatial operators"      => "numerical_implementation/spatial_operators.md",
-             "Boundary conditions"    => "numerical_implementation/boundary_conditions.md",
-             "Poisson solvers"        => "numerical_implementation/poisson_solvers.md",
-             "Large eddy simulation"  => "numerical_implementation/large_eddy_simulation.md"
-         ],
-
-         "Verification experiments" => [
-             "Taylor-Green vortex"     => "verification/taylor_green_vortex.md",
-             "Stratified Couette flow" => "verification/stratified_couette_flow.md"
-         ],
-
-                        "Gallery" => "gallery.md",
-         "Performance benchmarks" => "benchmarks.md",
-            "Contributor's guide" => "contributing.md",
-                        "Library" => "library.md",
-
-         "Appendix" => [
-             "Staggered grid"         => "appendix/staggered_grid.md",
-             "Fractional step method" => "appendix/fractional_step.md",
-         ],
-
-         "Function index" => "function_index.md",
-         "References"     => "references.md"
-     ]
+     pages = pages
 )
 
 withenv("TRAVIS_REPO_SLUG" => "CliMA/OceananigansDocumentation") do
