8d3b9f0e3bad46dfc0d294d0e3c0014fdc48b1a6
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-08-04 17:26:38 -0400

    Remove `diagnostics_kernels.jl`

--- src/Diagnostics/Diagnostics.jl
+++ src/Diagnostics/Diagnostics.jl
@@ -10,7 +10,6 @@ using Oceananigans,
 
 using Oceananigans: AbstractDiagnostic
 
-include("diagnostics_kernels.jl")
 include("nan_checker.jl")
 include("horizontal_average.jl")
 include("time_series.jl")
