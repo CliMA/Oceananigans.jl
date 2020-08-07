cece8a91e8f5cdd1cb763e6017861b1f6f7e4ae4
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2020-08-07 11:30:12 -0400

    Explanation of `Flux` BC in docstring
    Resolves #837

--- src/BoundaryConditions/boundary_condition_types.jl
+++ src/BoundaryConditions/boundary_condition_types.jl
@@ -18,6 +18,18 @@ struct Periodic <: BCType end
     Flux
 
 A type specifying a boundary condition on the flux of a field.
+
+The sign convention is such that a positive flux represents the flux of a quantity in the
+positive direction. For example, a positive vertical flux implies a quantity is fluxed
+upwards, in the +z direction.
+
+Due to this convention, a positive flux applied to the top boundary specifies that a quantity
+is fluxed upwards across the top boundary and thus out of the domain. As a result, a positive
+flux applied to a top boundary leads to a reduction of that quantity in the interior of the
+domain; for example, a positive, upwards flux of heat at the top of the domain acts to cool
+the interior of the domain. Conversely, a positive flux applied to the bottom boundary leads
+to an increase of the quantity in the interior of the domain. The same logic holds for east,
+west, north, and south boundaries.
 """
 struct Flux <: BCType end
 
