3b9e3d5c84d869e2b7282cf37737968b2fdfaeb2
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-08-12 11:42:12 -0400

    Nuke custom log levels and clean up a bit

--- src/Logger.jl
+++ src/Logger.jl
@@ -5,24 +5,6 @@ export ModelLogger, Diagnostic, Setup
 using Dates
 using Logging
 
-#####
-##### Custom LogLevels
-#####
-
-_custom_log_level_docs = """
-    Severity Order:
-    Debug < Diagnostic < Setup < Info
-
-    Usage:
-    @logmsg Logging.LogLevel "Log Message"
-
-    @logmsg comes from Base/Logging
-    LogLevel can be any Base/Logging.LogLevel
-    Log Message can be any expression that evaluates to a string (preferably human readable!)
-"""
-
-const Diagnostic = Logging.LogLevel(-500)  # Sits between Debug and Info
-const Setup      = Logging.LogLevel(-125)
 
 #####
 ##### ModelLogger
@@ -31,11 +13,9 @@ const Setup      = Logging.LogLevel(-125)
 """
     ModelLogger(stream::IO, level::LogLevel)
 
-Based on Logging.SimpleLogger it tries to log all messages in the following format
+Based on Logging.SimpleLogger, it tries to log all messages in the following format:
 
-    message --- [dd/mm/yyyy HH:MM:SS] log_level source_file:line_number
-
-The logger will handle any message from Diagnostic up by default.
+[dd/mm/yyyy HH:MM:SS.sss] message --- log_level source_file:line_number
 """
 struct ModelLogger <: Logging.AbstractLogger
             stream :: IO
@@ -43,7 +23,7 @@ struct ModelLogger <: Logging.AbstractLogger
     message_limits :: Dict{Any,Int}
 end
 
-ModelLogger(stream::IO=stderr, level=Diagnostic) = ModelLogger(stream, level, Dict{Any,Int}())
+ModelLogger(stream::IO=stdout, level=Logging.Info) = ModelLogger(stream, level, Dict{Any,Int}())
 
 Logging.shouldlog(logger::ModelLogger, level, _module, group, id) = get(logger.message_limits, id, 1) > 0
 
@@ -52,23 +32,23 @@ Logging.min_enabled_level(logger::ModelLogger) = logger.min_level
 Logging.catch_exceptions(logger::ModelLogger) = false
 
 function level_to_string(level::Logging.LogLevel)
-    level == Diagnostic   && return "Diagnostic"
-    level == Setup        && return "Setup"
     level == Logging.Warn && return "Warning"
     return string(level)
 end
 
-function Logging.handle_message(logger::ModelLogger, level, message, _module, group, id, filepath, line; maxlog = nothing, kwargs...)
-    if maxlog !== nothing && maxlog isa Integer
+function Logging.handle_message(logger::ModelLogger, level, message, _module, group, id,
+                                filepath, line; maxlog = nothing, kwargs...)
+
+    if !isnothing(maxlog) && maxlog isa Int
         remaining = get!(logger.message_limits, id, maxlog)
         logger.message_limits[id] = remaining - 1
-        remaining > 0 || return
+        remaining > 0 || return nothing
     end
 
     buf = IOBuffer()
     iob = IOContext(buf, logger.stream)
-    level_name = level_to_string(level)
 
+    level_name = level_to_string(level)
     module_name = something(_module, "nothing")
     file_name   = something(filepath, "nothing")
     line_number = something(line, "nothing")
