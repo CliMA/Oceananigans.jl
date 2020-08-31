b68f3f7249a8023a1e9a10e169ce552d8526494c
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-08-31 05:49:08 -0400

    Tighter plots for 1D convergence tests

--- verification/convergence_tests/ConvergenceTests/OneDimensionalUtils.jl
+++ verification/convergence_tests/ConvergenceTests/OneDimensionalUtils.jl
@@ -56,6 +56,8 @@ function plot_solutions!(axs, all_results, names, linestyles, specialcolors)
 
             plot(x, c_sim[i], linestyle, color=defaultcolors[i], alpha=0.8, linewidth=1,
                  label="$name simulated, \$ N_x \$ = $(grids[i].Nx)")
+
+            xlim(minimum(x), maximum(x))
         end
 
         sca(axs[2])
@@ -63,8 +65,10 @@ function plot_solutions!(axs, all_results, names, linestyles, specialcolors)
         for i in 1:length(c_sim)
             x = xnodes(Cell, grids[i])[:]
 
-            plot(x, abs.(c_sim[i] .- c_ana[i]), linestyle, color=defaultcolors[i], alpha=0.8,
-                 label="$name, \$ N_x \$ = $(grids[i].Nx)")
+            semilogy(x, abs.(c_sim[i] .- c_ana[i]), linestyle, color=defaultcolors[i], alpha=0.8,
+                     label="$name, \$ N_x \$ = $(grids[i].Nx)")
+
+            xlim(minimum(x), maximum(x))
         end
 
     end
@@ -76,7 +80,6 @@ function plot_solutions!(axs, all_results, names, linestyles, specialcolors)
     lgd1 = legend(loc="upper left", prop=Dict(:size=>7), bbox_to_anchor=(-0.35, 0, 1.5, 1.0))
 
     sca(axs[2])
-    axs[2].set_yscale("log")
     removespines("top", "right")
     xlabel(L"x")
     ylabel("Absolute error \$ | c_\\mathrm{sim} - c_\\mathrm{analytical} | \$")
@@ -96,27 +99,26 @@ function plot_error_convergence!(axs, Nx, all_results, names)
         @info "u_L∞ = $u_L₁"
 
         common_kwargs = (linestyle="None", color=defaultcolors[j], mfc="None", alpha=0.8)
-        loglog(Nx,  u_L₁; marker="o", label="\$L_1\$-norm, \$u\$ $name", common_kwargs...)
-        loglog(Nx,  v_L₁; marker="2", label="\$L_1\$-norm, \$v\$ $name", common_kwargs...)
-        loglog(Nx, cx_L₁; marker="*", label="\$L_1\$-norm, \$x\$ tracer $name", common_kwargs...)
-        loglog(Nx, cy_L₁; marker="+", label="\$L_1\$-norm, \$y\$ tracer $name", common_kwargs...)
+        loglog(Nx,  u_L₁; basex=2, marker="o", label="\$L_1\$-norm, \$u\$ $name", common_kwargs...)
+        loglog(Nx,  v_L₁; basex=2, marker="2", label="\$L_1\$-norm, \$v\$ $name", common_kwargs...)
+        loglog(Nx, cx_L₁; basex=2, marker="*", label="\$L_1\$-norm, \$x\$ tracer $name", common_kwargs...)
+        loglog(Nx, cy_L₁; basex=2, marker="+", label="\$L_1\$-norm, \$y\$ tracer $name", common_kwargs...)
 
-        loglog(Nx,  u_L∞; marker="1", label="\$L_\\infty\$-norm, \$u\$ $name", common_kwargs...)
-        loglog(Nx,  v_L∞; marker="_", label="\$L_\\infty\$-norm, \$v\$ $name", common_kwargs...)
-        loglog(Nx, cx_L∞; marker="^", label="\$L_\\infty\$-norm, \$x\$ tracer $name", common_kwargs...)
-        loglog(Nx, cy_L∞; marker="s", label="\$L_\\infty\$-norm, \$y\$ tracer $name", common_kwargs...)
+        loglog(Nx,  u_L∞; basex=2, marker="1", label="\$L_\\infty\$-norm, \$u\$ $name", common_kwargs...)
+        loglog(Nx,  v_L∞; basex=2, marker="_", label="\$L_\\infty\$-norm, \$v\$ $name", common_kwargs...)
+        loglog(Nx, cx_L∞; basex=2, marker="^", label="\$L_\\infty\$-norm, \$x\$ tracer $name", common_kwargs...)
+        loglog(Nx, cy_L∞; basex=2, marker="s", label="\$L_\\infty\$-norm, \$y\$ tracer $name", common_kwargs...)
     end
 
     # Guide line to confirm second-order scaling
     u_L₁, v_L₁, cx_L₁, cy_L₁, u_L∞, v_L∞, cx_L∞, cy_L∞  = unpack_errors(all_results[1])
-    loglog(Nx, cx_L₁[1] .* (Nx[1] ./ Nx).^2, "k-", alpha=0.8, label=L"\sim N_x^{-2}")
+    loglog(Nx, cx_L₁[1] .* (Nx[1] ./ Nx).^2, "k-", basex=2, alpha=0.8, label=L"\sim N_x^{-2}")
 
     axs.grid(which="both", linewidth=1)
     xlabel(L"N_x")
     ylabel("\$L\$-norms of \$ | c_\\mathrm{sim} - c_\\mathrm{analytical} |\$")
     removespines("top", "right")
     lgd = legend(loc="upper right", bbox_to_anchor=(1.4, 1.0), prop=Dict(:size=>6))
-    xticks(Nx, ["\$ 2^{$(round(Int, log2(n)))} \$" for n in Nx])
 
     return lgd
 end
