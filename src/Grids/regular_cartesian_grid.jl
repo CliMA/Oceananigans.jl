a841bbb22b85654dfad923da3627f6d8f63ada7d
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-09-03 12:51:18 -0400

    Fix indentation

--- src/Grids/regular_cartesian_grid.jl
+++ src/Grids/regular_cartesian_grid.jl
@@ -177,7 +177,7 @@ end
 
 function show(io::IO, g::RegularCartesianGrid{FT, TX, TY, TZ}) where {FT, TX, TY, TZ}
     print(io, "RegularCartesianGrid{$FT, $TX, $TY, $TZ}\n",
-          "                       domain: $(domain_string(g))\n",
+              "                   domain: $(domain_string(g))\n",
               "                 topology: ", (TX, TY, TZ), '\n',
               "  resolution (Nx, Ny, Nz): ", (g.Nx, g.Ny, g.Nz), '\n',
               "   halo size (Hx, Hy, Hz): ", (g.Hx, g.Hy, g.Hz), '\n',
