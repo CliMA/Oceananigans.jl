8408f0b76738e06b6649ff7fbbd1eea374e555ef
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-09-12 20:46:29 -0400

    Just use a constant velocity field I guess

--- verification/tracer_advection_2d/tracer_advection_2d.jl
+++ verification/tracer_advection_2d/tracer_advection_2d.jl
@@ -31,7 +31,7 @@ ic_name(::typeof(ϕ_Square))   = "Square"
 #####
 
 function setup_simulation(N, T, CFL, ϕₐ, advection_scheme; u, v)
-    topology = (Flat, Bounded, Bounded)
+    topology = (Flat, Periodic, Periodic)
     domain = (x=(0, 1), y=(0, L), z=(0, L))
     grid = RegularCartesianGrid(topology=topology, size=(1, N, N), halo=(3, 3, 3); domain...)
 
@@ -51,13 +51,14 @@ function setup_simulation(N, T, CFL, ϕₐ, advection_scheme; u, v)
 
     simulation = Simulation(model, Δt=Δt, stop_time=T, progress=print_progress, iteration_interval=10)
 
-    filename = @sprintf("stommel_gyre_%s_%s_N%d_CFL%.2f.nc", ic_name(ϕₐ), typeof(advection_scheme), N, CFL)
+    filename = @sprintf("%s_%s_N%d_CFL%.2f.nc", ic_name(ϕₐ), typeof(advection_scheme), N, CFL)
     fields = Dict("v" => model.velocities.v, "w" => model.velocities.w, "c" => model.tracers.c)
+    global_attributes = Dict("N" => N, "CFL" => CFL, "advection_scheme" => string(typeof(advection_scheme)))
     output_attributes = Dict("c" => Dict("longname" => "passive tracer"))
 
     simulation.output_writers[:fields] =
         NetCDFOutputWriter(model, fields, filename=filename, time_interval=0.01,
-                           output_attributes=output_attributes, verbose=true)
+                           global_attributes=global_attributes, output_attributes=output_attributes)
 
     return simulation
 end
@@ -76,63 +77,36 @@ function print_progress(simulation)
                    progress, i, t, v_max, w_max, c_min, c_max)
 end
 
-function create_animation(N, T, CFL, ϕₐ, scheme; u, v)
-    model, Δt = setup_model(N, ϕₐ, scheme, u=u, v=v)
-
-    c = model.tracers.c
-    y, z = ynodes(c), znodes(c)
-    Nt = ceil(Int, T/Δt)
-
-    function every(n)
-          0 < n <= 128 && return 1
-        128 < n <= 256 && return 2
-        256 < n <= 512 && return 4
-        512 < n        && return 8
-    end
-
-    reverse_uv = false
-
-    anim_filename = @sprintf("%s_%s_N%d_CFL%.2f.mp4", ic_name(ϕₐ), typeof(scheme), N, CFL)
-
-    anim = @animate for iter in 0:Nt-1
-        @info "$anim_filename: iter = $(model.clock.iteration)/$Nt"
-
-        time_step!(model, Δt, euler = model.clock.iteration == 0)
-
-        # if !reverse_uv && model.clock.time >= T/2
-        #     set!(model, v = (x, y, z) -> -u(x, y, z), w = (x, y, z) -> -v(x, y, z))
-        #     reverse_uv = true
-        # end
-
-        title = @sprintf("%s N=%d CFL=%.2f", typeof(scheme), N, CFL)
-
-        contourf(y ./ L, z ./ L, dropdims(interior(c), dims=1),
-                 title=title, xlabel="x/L", ylabel="y/L",
-                 xlims=(0, 1), ylims=(0, 1),
-                 levels=20, fill=:true, color=:balance, clims=(-1.0, 1.0),
-                 aspect_ratio=:equal, dpi=300, show= iter % 100 == 0)
-
-    end every 100
-
-    mp4(anim, anim_filename, fps = 15)
-
-    return model
-end
-
 L = τ₀ = β = 1
 r = 0.04β*L
 
 A  = 1
 σˣ = σʸ = 0.05L
 
-N = 64
-CFL = 0.2
-T = 1
+T = 2
 
-u = (x, y, z) -> u_Stommel(y, z, L=L, τ₀=τ₀, β=β, r=r)
-v = (x, y, z) -> v_Stommel(y, z, L=L, τ₀=τ₀, β=β, r=r)
-ϕ = (x, y, z) -> ϕ_Gaussian(y, z, L=L, A=A, σˣ=σˣ, σʸ=σʸ)
+u = (x, y, z) -> 1 # u_Stommel(y, z, L=L, τ₀=τ₀, β=β, r=r)
+v = (x, y, z) -> 1 # v_Stommel(y, z, L=L, τ₀=τ₀, β=β, r=r)
 
-ic_name(::typeof(ϕ)) = ic_name(ϕ_Gaussian)
+ϕ_λ_Gaussian = (x, y, z) -> ϕ_Gaussian(y, z, L=L, A=A, σˣ=σˣ, σʸ=σʸ)
+ϕ_λ_Square   = (x, y, z) -> ϕ_Square(y, z, L=L, A=A, σˣ=σˣ, σʸ=σʸ)
 
-# model, Δt = create_animation(N, T, CFL, ϕ, CenteredSecondOrder(), u=u, v=v)
+ic_name(::typeof(ϕ_λ_Gaussian)) = ic_name(ϕ_Gaussian)
+ic_name(::typeof(ϕ_λ_Square))   = ic_name(ϕ_Square)
+
+ϕ_λs = (ϕ_λ_Gaussian, ϕ_λ_Square)
+schemes = (CenteredSecondOrder(), CenteredFourthOrder(), WENO5())
+Ns = (32, 256)
+CFLs = (0.05, 0.3)
+
+for ϕ in ϕ_λs, scheme in schemes, N in Ns, CFL in CFLs
+    simulation = setup_simulation(N, T, CFL, ϕ, scheme, u=v, v=v)
+
+    # simulation.stop_time = T/2
+    run!(simulation)
+
+    # set!(simulation.model, v=-1, w=-1)
+
+    # simulation.stop_time = T
+    # run!(simulation)
+end
