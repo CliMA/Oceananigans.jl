eb624329c6128e5ffe94ca3cbc70fdaaef1cfc4c
Gregory Wagner <wagner.greg@gmail.com>
2020-09-15 09:08:54 -0400

    Extends convergence tests to RK3TimeStepper

--- verification/convergence_tests/point_exponential_decay.jl
+++ verification/convergence_tests/point_exponential_decay.jl
@@ -14,14 +14,14 @@ removespine(side) = gca().spines[side].set_visible(false)
 removespines(sides...) = [removespine(side) for side in sides]
 
 """ Run advection-diffusion test for all Nx in resolutions. """
-function run_convergence_test(stop_time, proposal_Δt)
+function run_convergence_test(stop_time, proposal_Δt, timestepper)
 
     # Adjust time-step
     stop_iterations = [round(Int, stop_time / dt) for dt in proposal_Δt]
                  Δt = [stop_time / stop_iter for stop_iter in stop_iterations]
 
     # Run the tests
-    results = [run_test(Δt=dt, stop_iteration=stop_iter)
+    results = [run_test(timestepper=timestepper, Δt=dt, stop_iteration=stop_iter)
                for (dt, stop_iter) in zip(Δt, stop_iterations)]
 
     return results, Δt
@@ -31,15 +31,20 @@ unpack_errors(results) = map(r -> r.L₁, results)
 
 stop_time = 3
 Δt = 10 .^ range(-3, 0, length=30)  # Equally spaced in log space.
-results, Δt = run_convergence_test(stop_time, Δt)
-L₁ = unpack_errors(results)
+ab2_results, Δt = run_convergence_test(stop_time, Δt, :AdamsBashforth)
+rk3_results, Δt = run_convergence_test(stop_time, Δt, :RK3)
+
+ab2_L₁ = unpack_errors(ab2_results)
+rk3_L₁ = unpack_errors(rk3_results)
 
 fig, axs = subplots()
 
-loglog(Δt, L₁, "o--", alpha=0.8, linewidth=1, label="Measured error")
+loglog(Δt, ab2_L₁, "o--", alpha=0.8, linewidth=1, label="Measured error, Quasi-second-order Adams-Bashforth")
+loglog(Δt, rk3_L₁, "^-.", alpha=0.8, linewidth=1, label="Measured error, Third-order Runge-Kutta")
 
 # Guide line to confirm second-order scaling
-loglog(Δt, L₁[1] .* Δt / Δt[1], "k-", alpha=0.4, linewidth=2, label=L"\sim \Delta t")
+loglog(Δt, ab2_L₁[1] .* Δt / Δt[1], "k-", alpha=0.4, linewidth=2, label=L"\sim \Delta t")
+loglog(Δt, rk3_L₁[1] .* (Δt / Δt[1]).^3, "r-", alpha=0.4, linewidth=2, label=L"\sim \Delta t^3")
 
 xlabel(L"\Delta t")
 ylabel("\$L_1\$-norm of \$ | c_\\mathrm{simulated}(t) - \\mathrm{e}^{-t} | \$")
@@ -50,4 +55,5 @@ legend()
 filepath = joinpath(@__DIR__, "figs", "point_exponential_decay_time_stepper_convergence.png")
 savefig(filepath, dpi=480)
 
-test_rate_of_convergence(L₁, Δt, data_points=9, expected=1.0, atol=0.01)
+test_rate_of_convergence(ab2_L₁, Δt, data_points=9, expected=1.0, atol=0.01)
+test_rate_of_convergence(rk3_L₁, Δt, data_points=9, expected=3.0, atol=0.01)
