2631a0516161f7cc0af82c636d0b6025334ebcc0
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-09-15 21:00:43 -0400

    Refactor WENO5 to remove grid argument

--- src/Advection/weno5.jl
+++ src/Advection/weno5.jl
@@ -8,33 +8,33 @@ struct WENO5 <: AbstractAdvectionScheme end
 ##### ENO interpolants of size 3
 #####
 
-@inline px₀(i, j, k, grid, f) = @inbounds + 1/3 * f[i-1, j, k] + 5/6 * f[i,   j, k] -  1/6 * f[i+1, j, k]
-@inline px₁(i, j, k, grid, f) = @inbounds - 1/6 * f[i-2, j, k] + 5/6 * f[i-1, j, k] +  1/3 * f[i,   j, k]
-@inline px₂(i, j, k, grid, f) = @inbounds + 1/3 * f[i-3, j, k] - 7/6 * f[i-2, j, k] + 11/6 * f[i-1, j, k]
+@inline px₀(i, j, k, f) = @inbounds + 1/3 * f[i-1, j, k] + 5/6 * f[i,   j, k] -  1/6 * f[i+1, j, k]
+@inline px₁(i, j, k, f) = @inbounds - 1/6 * f[i-2, j, k] + 5/6 * f[i-1, j, k] +  1/3 * f[i,   j, k]
+@inline px₂(i, j, k, f) = @inbounds + 1/3 * f[i-3, j, k] - 7/6 * f[i-2, j, k] + 11/6 * f[i-1, j, k]
 
-@inline py₀(i, j, k, grid, f) = @inbounds + 1/3 * f[i, j-1, k] + 5/6 * f[i, j,   k] -  1/6 * f[i, j+1, k]
-@inline py₁(i, j, k, grid, f) = @inbounds - 1/6 * f[i, j-2, k] + 5/6 * f[i, j-1, k] +  1/3 * f[i, j  , k]
-@inline py₂(i, j, k, grid, f) = @inbounds + 1/3 * f[i, j-3, k] - 7/6 * f[i, j-2, k] + 11/6 * f[i, j-1, k]
+@inline py₀(i, j, k, f) = @inbounds + 1/3 * f[i, j-1, k] + 5/6 * f[i, j,   k] -  1/6 * f[i, j+1, k]
+@inline py₁(i, j, k, f) = @inbounds - 1/6 * f[i, j-2, k] + 5/6 * f[i, j-1, k] +  1/3 * f[i, j  , k]
+@inline py₂(i, j, k, f) = @inbounds + 1/3 * f[i, j-3, k] - 7/6 * f[i, j-2, k] + 11/6 * f[i, j-1, k]
 
-@inline pz₀(i, j, k, grid, f) = @inbounds + 1/3 * f[i, j, k-1] + 5/6 * f[i, j,   k] -  1/6 * f[i, j, k+1]
-@inline pz₁(i, j, k, grid, f) = @inbounds - 1/6 * f[i, j, k-2] + 5/6 * f[i, j, k-1] +  1/3 * f[i, j,   k]
-@inline pz₂(i, j, k, grid, f) = @inbounds + 1/3 * f[i, j, k-3] - 7/6 * f[i, j, k-2] + 11/6 * f[i, j, k-1]
+@inline pz₀(i, j, k, f) = @inbounds + 1/3 * f[i, j, k-1] + 5/6 * f[i, j,   k] -  1/6 * f[i, j, k+1]
+@inline pz₁(i, j, k, f) = @inbounds - 1/6 * f[i, j, k-2] + 5/6 * f[i, j, k-1] +  1/3 * f[i, j,   k]
+@inline pz₂(i, j, k, f) = @inbounds + 1/3 * f[i, j, k-3] - 7/6 * f[i, j, k-2] + 11/6 * f[i, j, k-1]
 
 #####
 ##### Jiang & Shu (1996) WENO smoothness indicators
 #####
 
-@inline βx₀(i, j, k, grid, f) = @inbounds 13/12 * (f[i-1, j, k] - 2f[i,   j, k] + f[i+1, j, k])^2 + 1/4 * (3f[i-1, j, k] - 4f[i,   j, k] +  f[i+1, j, k])^2
-@inline βx₁(i, j, k, grid, f) = @inbounds 13/12 * (f[i-2, j, k] - 2f[i-1, j, k] + f[i,   j, k])^2 + 1/4 * ( f[i-2, j, k]                 -  f[i,   j, k])^2
-@inline βx₂(i, j, k, grid, f) = @inbounds 13/12 * (f[i-3, j, k] - 2f[i-2, j, k] + f[i-1, j, k])^2 + 1/4 * ( f[i-3, j, k] - 4f[i-2, j, k] + 3f[i-1, j, k])^2
+@inline βx₀(i, j, k, f) = @inbounds 13/12 * (f[i-1, j, k] - 2f[i,   j, k] + f[i+1, j, k])^2 + 1/4 * (3f[i-1, j, k] - 4f[i,   j, k] +  f[i+1, j, k])^2
+@inline βx₁(i, j, k, f) = @inbounds 13/12 * (f[i-2, j, k] - 2f[i-1, j, k] + f[i,   j, k])^2 + 1/4 * ( f[i-2, j, k]                 -  f[i,   j, k])^2
+@inline βx₂(i, j, k, f) = @inbounds 13/12 * (f[i-3, j, k] - 2f[i-2, j, k] + f[i-1, j, k])^2 + 1/4 * ( f[i-3, j, k] - 4f[i-2, j, k] + 3f[i-1, j, k])^2
 
-@inline βy₀(i, j, k, grid, f) = @inbounds 13/12 * (f[i, j-1, k] - 2f[i, j,   k] + f[i, j+1, k])^2 + 1/4 * (3f[i, j-1, k] - 4f[i,   j, k] +  f[i, j+1, k])^2
-@inline βy₁(i, j, k, grid, f) = @inbounds 13/12 * (f[i, j-2, k] - 2f[i, j-1, k] + f[i, j,   k])^2 + 1/4 * ( f[i, j-2, k]                 -  f[i,   j, k])^2
-@inline βy₂(i, j, k, grid, f) = @inbounds 13/12 * (f[i, j-3, k] - 2f[i, j-2, k] + f[i, j-1, k])^2 + 1/4 * ( f[i, j-3, k] - 4f[i, j-2, k] + 3f[i, j-1, k])^2
+@inline βy₀(i, j, k, f) = @inbounds 13/12 * (f[i, j-1, k] - 2f[i, j,   k] + f[i, j+1, k])^2 + 1/4 * (3f[i, j-1, k] - 4f[i,   j, k] +  f[i, j+1, k])^2
+@inline βy₁(i, j, k, f) = @inbounds 13/12 * (f[i, j-2, k] - 2f[i, j-1, k] + f[i, j,   k])^2 + 1/4 * ( f[i, j-2, k]                 -  f[i,   j, k])^2
+@inline βy₂(i, j, k, f) = @inbounds 13/12 * (f[i, j-3, k] - 2f[i, j-2, k] + f[i, j-1, k])^2 + 1/4 * ( f[i, j-3, k] - 4f[i, j-2, k] + 3f[i, j-1, k])^2
 
-@inline βz₀(i, j, k, grid, f) = @inbounds 13/12 * (f[i, j, k-1] - 2f[i, j,   k] + f[i, j, k+1])^2 + 1/4 * (3f[i, j, k-1] - 4f[i, j,   k] +  f[i, j, k+1])^2
-@inline βz₁(i, j, k, grid, f) = @inbounds 13/12 * (f[i, j, k-2] - 2f[i, j, k-1] + f[i, j,   k])^2 + 1/4 * ( f[i, j, k-2]                 -  f[i, j,   k])^2
-@inline βz₂(i, j, k, grid, f) = @inbounds 13/12 * (f[i, j, k-3] - 2f[i, j, k-2] + f[i, j, k-1])^2 + 1/4 * ( f[i, j, k-3] - 4f[i, j, k-2] + 3f[i, j, k-1])^2
+@inline βz₀(i, j, k, f) = @inbounds 13/12 * (f[i, j, k-1] - 2f[i, j,   k] + f[i, j, k+1])^2 + 1/4 * (3f[i, j, k-1] - 4f[i, j,   k] +  f[i, j, k+1])^2
+@inline βz₁(i, j, k, f) = @inbounds 13/12 * (f[i, j, k-2] - 2f[i, j, k-1] + f[i, j,   k])^2 + 1/4 * ( f[i, j, k-2]                 -  f[i, j,   k])^2
+@inline βz₂(i, j, k, f) = @inbounds 13/12 * (f[i, j, k-3] - 2f[i, j, k-2] + f[i, j, k-1])^2 + 1/4 * ( f[i, j, k-3] - 4f[i, j, k-2] + 3f[i, j, k-1])^2
 
 #####
 ##### WENO-5 optimal weights
@@ -51,52 +51,58 @@ const C3₂ = 1/10
 const ε = 1e-6
 const ƞ = 2  # WENO exponent
 
-@inline αx₀(i, j, k, grid, f) = C3₀ / (βx₀(i, j, k, grid, f) + ε)^ƞ
-@inline αx₁(i, j, k, grid, f) = C3₁ / (βx₁(i, j, k, grid, f) + ε)^ƞ
-@inline αx₂(i, j, k, grid, f) = C3₂ / (βx₂(i, j, k, grid, f) + ε)^ƞ
+@inline αx₀(i, j, k, f) = C3₀ / (βx₀(i, j, k, f) + ε)^ƞ
+@inline αx₁(i, j, k, f) = C3₁ / (βx₁(i, j, k, f) + ε)^ƞ
+@inline αx₂(i, j, k, f) = C3₂ / (βx₂(i, j, k, f) + ε)^ƞ
 
-@inline αy₀(i, j, k, grid, f) = C3₀ / (βy₀(i, j, k, grid, f) + ε)^ƞ
-@inline αy₁(i, j, k, grid, f) = C3₁ / (βy₁(i, j, k, grid, f) + ε)^ƞ
-@inline αy₂(i, j, k, grid, f) = C3₂ / (βy₂(i, j, k, grid, f) + ε)^ƞ
+@inline αy₀(i, j, k, f) = C3₀ / (βy₀(i, j, k, f) + ε)^ƞ
+@inline αy₁(i, j, k, f) = C3₁ / (βy₁(i, j, k, f) + ε)^ƞ
+@inline αy₂(i, j, k, f) = C3₂ / (βy₂(i, j, k, f) + ε)^ƞ
 
-@inline αz₀(i, j, k, grid, f) = C3₀ / (βz₀(i, j, k, grid, f) + ε)^ƞ
-@inline αz₁(i, j, k, grid, f) = C3₁ / (βz₁(i, j, k, grid, f) + ε)^ƞ
-@inline αz₂(i, j, k, grid, f) = C3₂ / (βz₂(i, j, k, grid, f) + ε)^ƞ
+@inline αz₀(i, j, k, f) = C3₀ / (βz₀(i, j, k, f) + ε)^ƞ
+@inline αz₁(i, j, k, f) = C3₁ / (βz₁(i, j, k, f) + ε)^ƞ
+@inline αz₂(i, j, k, f) = C3₂ / (βz₂(i, j, k, f) + ε)^ƞ
 
 #####
 ##### WENO-5 normalized weights
 #####
 
-@inline function weno5_weights_x(i, j, k, grid, f)
-    α₀ = αx₀(i, j, k, grid, f)
-    α₁ = αx₁(i, j, k, grid, f)
-    α₂ = αx₂(i, j, k, grid, f)
-    Σα = α₀ + α₁ + α₂
+@inline function weno5_weights_x(i, j, k, f)
+    α₀ = αx₀(i, j, k, f)
+    α₁ = αx₁(i, j, k, f)
+    α₂ = αx₂(i, j, k, f)
+    
+    Σα = α₀ + α₁ + α₂ 
     w₀ = α₀ / Σα
     w₁ = α₁ / Σα
     w₂ = α₂ / Σα
+    
     return w₀, w₁, w₂
 end
 
-@inline function weno5_weights_y(i, j, k, grid, f)
-    α₀ = αy₀(i, j, k, grid, f)
-    α₁ = αy₁(i, j, k, grid, f)
-    α₂ = αy₂(i, j, k, grid, f)
+@inline function weno5_weights_y(i, j, k, f)
+    α₀ = αy₀(i, j, k, f)
+    α₁ = αy₁(i, j, k, f)
+    α₂ = αy₂(i, j, k, f)
+
     Σα = α₀ + α₁ + α₂
     w₀ = α₀ / Σα
     w₁ = α₁ / Σα
     w₂ = α₂ / Σα
+    
     return w₀, w₁, w₂
 end
 
-@inline function weno5_weights_z(i, j, k, grid, f)
-    α₀ = αz₀(i, j, k, grid, f)
-    α₁ = αz₁(i, j, k, grid, f)
-    α₂ = αz₂(i, j, k, grid, f)
+@inline function weno5_weights_z(i, j, k, f)
+    α₀ = αz₀(i, j, k, f)
+    α₁ = αz₁(i, j, k, f)
+    α₂ = αz₂(i, j, k, f)
+
     Σα = α₀ + α₁ + α₂
     w₀ = α₀ / Σα
     w₁ = α₁ / Σα
     w₂ = α₂ / Σα
+    
     return w₀, w₁, w₂
 end
 
@@ -104,44 +110,44 @@ end
 ##### WENO-5 flux reconstruction
 #####
 
-@inline function weno5_flux_x(i, j, k, grid, f)
-    w₀, w₁, w₂ = weno5_weights_x(i, j, k, grid, f)
-    return w₀ * px₀(i, j, k, grid, f) + w₁ * px₁(i, j, k, grid, f) + w₂ * px₂(i, j, k, grid, f)
+@inline function weno5_flux_x(i, j, k, f)
+    w₀, w₁, w₂ = weno5_weights_x(i, j, k, f)
+    return w₀ * px₀(i, j, k, f) + w₁ * px₁(i, j, k, f) + w₂ * px₂(i, j, k, f)
 end
 
-@inline function weno5_flux_y(i, j, k, grid, f)
-    w₀, w₁, w₂ = weno5_weights_y(i, j, k, grid, f)
-    return w₀ * py₀(i, j, k, grid, f) + w₁ * py₁(i, j, k, grid, f) + w₂ * py₂(i, j, k, grid, f)
+@inline function weno5_flux_y(i, j, k, f)
+    w₀, w₁, w₂ = weno5_weights_y(i, j, k, f)
+    return w₀ * py₀(i, j, k, f) + w₁ * py₁(i, j, k, f) + w₂ * py₂(i, j, k, f)
 end
 
-@inline function weno5_flux_z(i, j, k, grid, f)
-    w₀, w₁, w₂ = weno5_weights_z(i, j, k, grid, f)
-    return w₀ * pz₀(i, j, k, grid, f) + w₁ * pz₁(i, j, k, grid, f) + w₂ * pz₂(i, j, k, grid, f)
+@inline function weno5_flux_z(i, j, k, f)
+    w₀, w₁, w₂ = weno5_weights_z(i, j, k, f)
+    return w₀ * pz₀(i, j, k, f) + w₁ * pz₁(i, j, k, f) + w₂ * pz₂(i, j, k, f)
 end
 
 #####
 ##### Momentum advection fluxes
 #####
 
-@inline momentum_flux_uu(i, j, k, grid, ::WENO5, u)    = ℑxᶜᵃᵃ(i, j, k, grid, Ax_ψᵃᵃᶠ, u) * weno5_flux_x(i, j, k, grid, u)
-@inline momentum_flux_uv(i, j, k, grid, ::WENO5, u, v) = ℑxᶠᵃᵃ(i, j, k, grid, Ay_ψᵃᵃᶠ, v) * weno5_flux_y(i, j, k, grid, u)
-@inline momentum_flux_uw(i, j, k, grid, ::WENO5, u, w) = ℑxᶠᵃᵃ(i, j, k, grid, Az_ψᵃᵃᵃ, w) * weno5_flux_z(i, j, k, grid, u)
+@inline momentum_flux_uu(i, j, k, grid, ::WENO5, u)    = ℑxᶜᵃᵃ(i, j, k, grid, Ax_ψᵃᵃᶠ, u) * weno5_flux_x(i, j, k, u)
+@inline momentum_flux_uv(i, j, k, grid, ::WENO5, u, v) = ℑxᶠᵃᵃ(i, j, k, grid, Ay_ψᵃᵃᶠ, v) * weno5_flux_y(i, j, k, u)
+@inline momentum_flux_uw(i, j, k, grid, ::WENO5, u, w) = ℑxᶠᵃᵃ(i, j, k, grid, Az_ψᵃᵃᵃ, w) * weno5_flux_z(i, j, k, u)
 
-@inline momentum_flux_vu(i, j, k, grid, ::WENO5, u, v) = ℑyᵃᶠᵃ(i, j, k, grid, Ax_ψᵃᵃᶠ, u) * weno5_flux_x(i, j, k, grid, v)
-@inline momentum_flux_vv(i, j, k, grid, ::WENO5, v)    = ℑyᵃᶜᵃ(i, j, k, grid, Ay_ψᵃᵃᶠ, v) * weno5_flux_y(i, j, k, grid, v)
-@inline momentum_flux_vw(i, j, k, grid, ::WENO5, v, w) = ℑyᵃᶠᵃ(i, j, k, grid, Az_ψᵃᵃᵃ, w) * weno5_flux_z(i, j, k, grid, v)
+@inline momentum_flux_vu(i, j, k, grid, ::WENO5, u, v) = ℑyᵃᶠᵃ(i, j, k, grid, Ax_ψᵃᵃᶠ, u) * weno5_flux_x(i, j, k, v)
+@inline momentum_flux_vv(i, j, k, grid, ::WENO5, v)    = ℑyᵃᶜᵃ(i, j, k, grid, Ay_ψᵃᵃᶠ, v) * weno5_flux_y(i, j, k, v)
+@inline momentum_flux_vw(i, j, k, grid, ::WENO5, v, w) = ℑyᵃᶠᵃ(i, j, k, grid, Az_ψᵃᵃᵃ, w) * weno5_flux_z(i, j, k, v)
 
-@inline momentum_flux_wu(i, j, k, grid, ::WENO5, u, w) = ℑzᵃᵃᶠ(i, j, k, grid, Ax_ψᵃᵃᶠ, u) * weno5_flux_x(i, j, k, grid, w)
-@inline momentum_flux_wv(i, j, k, grid, ::WENO5, v, w) = ℑzᵃᵃᶠ(i, j, k, grid, Ay_ψᵃᵃᶠ, v) * weno5_flux_y(i, j, k, grid, w)
-@inline momentum_flux_ww(i, j, k, grid, ::WENO5, w)    = ℑzᵃᵃᶜ(i, j, k, grid, Az_ψᵃᵃᵃ, w) * weno5_flux_z(i, j, k, grid, w)
+@inline momentum_flux_wu(i, j, k, grid, ::WENO5, u, w) = ℑzᵃᵃᶠ(i, j, k, grid, Ax_ψᵃᵃᶠ, u) * weno5_flux_x(i, j, k, w)
+@inline momentum_flux_wv(i, j, k, grid, ::WENO5, v, w) = ℑzᵃᵃᶠ(i, j, k, grid, Ay_ψᵃᵃᶠ, v) * weno5_flux_y(i, j, k, w)
+@inline momentum_flux_ww(i, j, k, grid, ::WENO5, w)    = ℑzᵃᵃᶜ(i, j, k, grid, Az_ψᵃᵃᵃ, w) * weno5_flux_z(i, j, k, w)
 
 #####
 ##### Advective tracer fluxes
 #####
 
-@inline advective_tracer_flux_x(i, j, k, grid, ::WENO5, u, c) = Ax_ψᵃᵃᶠ(i, j, k, grid, u) * weno5_flux_x(i, j, k, grid, c)
-@inline advective_tracer_flux_y(i, j, k, grid, ::WENO5, v, c) = Ay_ψᵃᵃᶠ(i, j, k, grid, v) * weno5_flux_y(i, j, k, grid, c)
-@inline advective_tracer_flux_z(i, j, k, grid, ::WENO5, w, c) = Az_ψᵃᵃᵃ(i, j, k, grid, w) * weno5_flux_z(i, j, k, grid, c)
+@inline advective_tracer_flux_x(i, j, k, grid, ::WENO5, u, c) = Ax_ψᵃᵃᶠ(i, j, k, grid, u) * weno5_flux_x(i, j, k, c)
+@inline advective_tracer_flux_y(i, j, k, grid, ::WENO5, v, c) = Ay_ψᵃᵃᶠ(i, j, k, grid, v) * weno5_flux_y(i, j, k, c)
+@inline advective_tracer_flux_z(i, j, k, grid, ::WENO5, w, c) = Az_ψᵃᵃᵃ(i, j, k, grid, w) * weno5_flux_z(i, j, k, c)
 
 #####
 ##### Need to advect momentum like tracers
