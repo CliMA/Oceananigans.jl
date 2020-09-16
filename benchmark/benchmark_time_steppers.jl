a24e2e06acfcd1ae74c3bb22bb521f92d3277ac1
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-09-15 23:23:16 -0400

    Time stepper benchmark

--- benchmark/benchmark_time_steppers.jl
+++ benchmark/benchmark_time_steppers.jl
@@ -18,22 +18,21 @@ Nt = 10  # Number of iterations to use for benchmarking time stepping.
          archs = [CPU()]         # Architectures to benchmark on.
 @hascuda archs = [CPU(), GPU()]  # Benchmark GPU on systems with CUDA-enabled GPUs.
 
-Schemes = (CenteredSecondOrder, CenteredFourthOrder, WENO5)
+time_steppers = (:QuasiAdamsBashforth2, :RungeKutta3)
 
 #####
 ##### Run benchmarks
 #####
 
-for arch in archs, FT in float_types, Scheme in Schemes
+for arch in archs, FT in float_types, ts in time_steppers
     N = arch isa CPU ? 64 : 256
     
-    topo = (Periodic, Periodic, Periodic)
-    grid = RegularCartesianGrid(FT, topology=topo, size=(N, N, N), halo=(3, 3, 3), extent=(1, 1, 1))
-    model = IncompressibleModel(architecture=arch, float_type=FT, grid=grid, advection=Scheme())
+    grid = RegularCartesianGrid(FT, size=(N, N, N), extent=(1, 1, 1))
+    model = IncompressibleModel(architecture=arch, float_type=FT, grid=grid, timestepper=ts)
 
     time_step!(model, 1)  # precompile
 
-    bn =  benchmark_name((N, N, N), string(Scheme), arch, FT)
+    bn =  benchmark_name((N, N, N), string(ts), arch, FT)
     @printf("Running benchmark: %s...\n", bn)
     for i in 1:Nt
         @timeit timer bn time_step!(model, 1)
@@ -47,5 +46,5 @@ end
 println()
 println(oceananigans_versioninfo())
 println(versioninfo_with_gpu())
-print_timer(timer, title="Advection scheme benchmarks", sortby=:name)
+print_timer(timer, title="Time stepper benchmarks", sortby=:name)
 println()
