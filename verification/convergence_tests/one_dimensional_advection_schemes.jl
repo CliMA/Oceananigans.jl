33b99b5ae633c7c79b0974b0289be655ac095be2
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-09-19 18:47:28 -0400

    Clean up 1D advection scheme convergence test

--- verification/convergence_tests/one_dimensional_advection_schemes.jl
+++ verification/convergence_tests/one_dimensional_advection_schemes.jl
@@ -33,24 +33,21 @@ end
 #####
 
 advection_schemes = (CenteredSecondOrder(), CenteredFourthOrder(), WENO5())
-# advection_schemes = (WENO5(),)
 
 U = 3
 κ = 1e-8
-
-Nx = Dict(CenteredSecondOrder => 2 .^ (7:10),
-          CenteredFourthOrder => 2 .^ (7:10),
-          WENO5               => 2 .^ (7:10))
+Nx = 2 .^ (7:10)
 
 results = Dict()
 for scheme in advection_schemes
     t_scheme = typeof(scheme)
-    results[t_scheme] = run_convergence_test(κ, U, Nx[t_scheme], scheme)
+    results[t_scheme] = run_convergence_test(κ, U, Nx, scheme)
 end
 
 rate_of_convergence(::CenteredSecondOrder) = 2
 rate_of_convergence(::CenteredFourthOrder) = 4
 rate_of_convergence(::WENO5) = 5
+rate_of_convergence(::WENO{K}) where K = 2K-1
 
 fig, ax = subplots()
 
@@ -58,7 +55,6 @@ for (j, scheme) in enumerate(advection_schemes)
     t_scheme = typeof(scheme)
     name = string(t_scheme)
     roc = rate_of_convergence(scheme)
-    Ns = Nx[t_scheme]
     
     u_L₁, v_L₁, cx_L₁, cy_L₁, u_L∞, v_L∞, cx_L∞, cy_L∞ = unpack_errors(results[typeof(scheme)])
 
@@ -66,14 +62,14 @@ for (j, scheme) in enumerate(advection_schemes)
            t_scheme == CenteredFourthOrder ? 0.12 :
            t_scheme == WENO5               ? 0.40 : Inf
 
-    test_rate_of_convergence(u_L₁,  Ns, expected=-roc, atol=atol, name=name*" u_L₁")
-    test_rate_of_convergence(v_L₁,  Ns, expected=-roc, atol=atol, name=name*" v_L₁")
-    test_rate_of_convergence(cx_L₁, Ns, expected=-roc, atol=atol, name=name*" cx_L₁")
-    test_rate_of_convergence(cy_L₁, Ns, expected=-roc, atol=atol, name=name*" cy_L₁")
-    test_rate_of_convergence(u_L∞,  Ns, expected=-roc, atol=atol, name=name*" u_L∞")
-    test_rate_of_convergence(v_L∞,  Ns, expected=-roc, atol=atol, name=name*" v_L∞")
-    test_rate_of_convergence(cx_L∞, Ns, expected=-roc, atol=atol, name=name*" cx_L∞")
-    test_rate_of_convergence(cy_L∞, Ns, expected=-roc, atol=atol, name=name*" cy_L∞")
+    test_rate_of_convergence(u_L₁,  Nx, expected=-roc, atol=atol, name=name*" u_L₁")
+    test_rate_of_convergence(v_L₁,  Nx, expected=-roc, atol=atol, name=name*" v_L₁")
+    test_rate_of_convergence(cx_L₁, Nx, expected=-roc, atol=atol, name=name*" cx_L₁")
+    test_rate_of_convergence(cy_L₁, Nx, expected=-roc, atol=atol, name=name*" cy_L₁")
+    test_rate_of_convergence(u_L∞,  Nx, expected=-roc, atol=atol, name=name*" u_L∞")
+    test_rate_of_convergence(v_L∞,  Nx, expected=-roc, atol=atol, name=name*" v_L∞")
+    test_rate_of_convergence(cx_L∞, Nx, expected=-roc, atol=atol, name=name*" cx_L∞")
+    test_rate_of_convergence(cy_L∞, Nx, expected=-roc, atol=atol, name=name*" cy_L∞")
 
     @test  u_L₁ ≈  v_L₁
     @test cx_L₁ ≈ cy_L₁
@@ -81,18 +77,18 @@ for (j, scheme) in enumerate(advection_schemes)
     @test cx_L∞ ≈ cy_L∞
 
     common_kwargs = (linestyle="None", color=defaultcolors[j], mfc="None", alpha=0.8)
-    loglog(Ns,  u_L₁; basex=2, marker="o", label="\$L_1\$-norm, \$u\$ $name", common_kwargs...)
-    loglog(Ns,  v_L₁; basex=2, marker="2", label="\$L_1\$-norm, \$v\$ $name", common_kwargs...)
-    loglog(Ns, cx_L₁; basex=2, marker="*", label="\$L_1\$-norm, \$x\$ tracer $name", common_kwargs...)
-    loglog(Ns, cy_L₁; basex=2, marker="+", label="\$L_1\$-norm, \$y\$ tracer $name", common_kwargs...)
+    loglog(Nx,  u_L₁; basex=2, marker="o", label="\$L_1\$-norm, \$u\$ $name", common_kwargs...)
+    loglog(Nx,  v_L₁; basex=2, marker="2", label="\$L_1\$-norm, \$v\$ $name", common_kwargs...)
+    loglog(Nx, cx_L₁; basex=2, marker="*", label="\$L_1\$-norm, \$x\$ tracer $name", common_kwargs...)
+    loglog(Nx, cy_L₁; basex=2, marker="+", label="\$L_1\$-norm, \$y\$ tracer $name", common_kwargs...)
 
-    loglog(Ns,  u_L∞; basex=2, marker="1", label="\$L_\\infty\$-norm, \$u\$ $name", common_kwargs...)
-    loglog(Ns,  v_L∞; basex=2, marker="_", label="\$L_\\infty\$-norm, \$v\$ $name", common_kwargs...)
-    loglog(Ns, cx_L∞; basex=2, marker="^", label="\$L_\\infty\$-norm, \$x\$ tracer $name", common_kwargs...)
-    loglog(Ns, cy_L∞; basex=2, marker="s", label="\$L_\\infty\$-norm, \$y\$ tracer $name", common_kwargs...)
+    loglog(Nx,  u_L∞; basex=2, marker="1", label="\$L_\\infty\$-norm, \$u\$ $name", common_kwargs...)
+    loglog(Nx,  v_L∞; basex=2, marker="_", label="\$L_\\infty\$-norm, \$v\$ $name", common_kwargs...)
+    loglog(Nx, cx_L∞; basex=2, marker="^", label="\$L_\\infty\$-norm, \$x\$ tracer $name", common_kwargs...)
+    loglog(Nx, cy_L∞; basex=2, marker="s", label="\$L_\\infty\$-norm, \$y\$ tracer $name", common_kwargs...)
 
     label = raw"\sim N_x^{-" * "$roc" * raw"}" |> latexstring
-    loglog(Ns, cx_L₁[1] .* (Ns[1] ./ Ns) .^ roc, color=defaultcolors[j], basex=2, alpha=0.8, label=label)
+    loglog(Nx, cx_L₁[1] .* (Nx[1] ./ Nx) .^ roc, color=defaultcolors[j], basex=2, alpha=0.8, label=label)
 
     xlabel(L"N_x")
     ylabel("\$L\$-norms of \$ | c_\\mathrm{sim} - c_\\mathrm{analytical} |\$")
