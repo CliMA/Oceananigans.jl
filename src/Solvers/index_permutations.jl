33becd804795d2f98602beb7dbe8517cbd10394d
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-09-28 18:41:05 -0400

    Better error now

--- src/Solvers/index_permutations.jl
+++ src/Solvers/index_permutations.jl
@@ -8,13 +8,7 @@
 Perform the permutation [a, b, c, d, e, f, g, h] -> [a, c, e, g, h, f, d, b]
 on index `i` out of `N`.
 """
-@inline function _permute_index(i, N)
-    if (i & 1) == 1  # Same as isodd(i)
-        return floor(Int, i/2) + 1
-    else
-        return N - floor(Int, (i-1)/2)
-    end
-end
+@inline _permute_index(i, N) = isodd(i) ? Int(floor(i/2) + 1) : Int(N - floor((i-1)/2))
 
 """
     _permute_index(i, N)
@@ -22,13 +16,7 @@ end
 Undo the permutation [a, b, c, d, e, f, g, h] -> [a, c, e, g, h, f, d, b]
 on index `i` out of `N`.
 """
-@inline function _unpermute_index(i, N)
-    if i <= N/2
-        return 2i-1
-    else
-        return 2(N-i+1)
-    end
-end
+@inline _unpermute_index(i, N) = i <= N/2 ? 2i-1 : 2(N-i+1)
 
 @inline   permute_index(::TriplyPeriodic, ::GPU, i, j, k, Nx, Ny, Nz) = i, j, k
 @inline unpermute_index(::TriplyPeriodic, ::GPU, i, j, k, Nx, Ny, Nz) = i, j, k
