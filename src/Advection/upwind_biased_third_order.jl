37e88861f8c055c7e17c9dca57a2a31d8a31e589
Gregory Wagner <wagner.greg@gmail.com>
2020-09-28 16:58:01 -0400

    Fixes bug in UpwindBiasedThirdOrder advection scheme

--- src/Advection/upwind_biased_third_order.jl
+++ src/Advection/upwind_biased_third_order.jl
@@ -2,12 +2,7 @@
 ##### Upwind-biased 3rd-order advection scheme
 #####
 
-struct UpwindBiasedThirdOrder <: AbstractUpwindBiasedAdvectionScheme
-    function UpwindBiasedThirdOrder()
-        @warn "UpwindBiasedThirdOrder is currently an experimental scheme and may blow up in your face!"
-        return new{}()
-    end
-end
+struct UpwindBiasedThirdOrder <: AbstractUpwindBiasedAdvectionScheme end
 
 const U3 = UpwindBiasedThirdOrder
 
@@ -21,17 +16,17 @@ const U3 = UpwindBiasedThirdOrder
 @inline symmetric_interpolate_yᵃᶜᵃ(i, j, k, grid, ::U3, v) = ℑyᵃᶜᵃ(i, j, k, grid, v)
 @inline symmetric_interpolate_zᵃᵃᶜ(i, j, k, grid, ::U3, w) = ℑzᵃᵃᶜ(i, j, k, grid, w)
 
-@inline left_biased_interpolate_xᶠᵃᵃ(i, j, k, grid, ::U3, c) = @inbounds (6 * ℑxᶠᵃᵃ(i, j, k, grid, c) + 4 * c[i, j, k] - ℑxᶠᵃᵃ(i-1, j, k, grid, c)) / 6 
-@inline left_biased_interpolate_yᵃᶠᵃ(i, j, k, grid, ::U3, c) = @inbounds (6 * ℑyᵃᶠᵃ(i, j, k, grid, c) + 4 * c[i, j, k] - ℑyᵃᶠᵃ(i, j-1, k, grid, c)) / 6 
-@inline left_biased_interpolate_zᵃᵃᶠ(i, j, k, grid, ::U3, c) = @inbounds (6 * ℑzᵃᵃᶠ(i, j, k, grid, c) + 4 * c[i, j, k] - ℑzᵃᵃᶠ(i, j, k-1, grid, c)) / 6 
+@inline left_biased_interpolate_xᶠᵃᵃ(i, j, k, grid, ::U3, c) = @inbounds (2 * c[i, j, k] + 5 * c[i-1, j, k] - c[i-2, j, k]) / 6
+@inline left_biased_interpolate_yᵃᶠᵃ(i, j, k, grid, ::U3, c) = @inbounds (2 * c[i, j, k] + 5 * c[i, j-1, k] - c[i, j-2, k]) / 6
+@inline left_biased_interpolate_zᵃᵃᶠ(i, j, k, grid, ::U3, c) = @inbounds (2 * c[i, j, k] + 5 * c[i, j, k-1] - c[i, j, k-2]) / 6
 
 @inline left_biased_interpolate_xᶜᵃᵃ(i, j, k, grid, scheme::U3, u) = left_biased_interpolate_xᶠᵃᵃ(i+1, j, k, grid, scheme, u)
 @inline left_biased_interpolate_yᵃᶜᵃ(i, j, k, grid, scheme::U3, v) = left_biased_interpolate_yᵃᶠᵃ(i, j+1, k, grid, scheme, v)
 @inline left_biased_interpolate_zᵃᵃᶜ(i, j, k, grid, scheme::U3, w) = left_biased_interpolate_zᵃᵃᶠ(i, j, k+1, grid, scheme, w)
 
-@inline right_biased_interpolate_xᶠᵃᵃ(i, j, k, grid, ::U3, c) = @inbounds (2 * ℑxᶠᵃᵃ(i, j, k, grid, c) + 4 * c[i, j, k] - ℑxᶠᵃᵃ(i+1, j, k, grid, c)) / 6 
-@inline right_biased_interpolate_yᵃᶠᵃ(i, j, k, grid, ::U3, c) = @inbounds (2 * ℑyᵃᶠᵃ(i, j, k, grid, c) + 4 * c[i, j, k] - ℑyᵃᶠᵃ(i, j+1, k, grid, c)) / 6
-@inline right_biased_interpolate_zᵃᵃᶠ(i, j, k, grid, ::U3, c) = @inbounds (2 * ℑzᵃᵃᶠ(i, j, k, grid, c) + 4 * c[i, j, k] - ℑzᵃᵃᶠ(i, j, k+1, grid, c)) / 6
+@inline right_biased_interpolate_xᶠᵃᵃ(i, j, k, grid, ::U3, c) = @inbounds (- c[i+1, j, k] + 5 * c[i, j, k] + 2 * c[i-1, j, k]) / 6
+@inline right_biased_interpolate_yᵃᶠᵃ(i, j, k, grid, ::U3, c) = @inbounds (- c[i, j+1, k] + 5 * c[i, j, k] + 2 * c[i, j-1, k]) / 6
+@inline right_biased_interpolate_zᵃᵃᶠ(i, j, k, grid, ::U3, c) = @inbounds (- c[i, j, k+1] + 5 * c[i, j, k] + 2 * c[i, j, k-1]) / 6
 
 @inline right_biased_interpolate_xᶜᵃᵃ(i, j, k, grid, scheme::U3, u) = right_biased_interpolate_xᶠᵃᵃ(i+1, j, k, grid, scheme, u)
 @inline right_biased_interpolate_yᵃᶜᵃ(i, j, k, grid, scheme::U3, v) = right_biased_interpolate_yᵃᶠᵃ(i, j+1, k, grid, scheme, v)
