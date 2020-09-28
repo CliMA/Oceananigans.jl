465aa149f79af20c2ec2d259578596bcde05277a
Gregory L. Wagner <wagner.greg@gmail.com>
2020-09-28 16:54:22 -0400

    Update new_data.jl
    Resolves #991

--- src/Fields/new_data.jl
+++ src/Fields/new_data.jl
@@ -1,6 +1,6 @@
 using Oceananigans.Grids: total_length, topology
 
-import OffsetArrays: OffsetArray
+using OffsetArrays: OffsetArray
 
 #####
 ##### Creating offset arrays for field data by dispatching on architecture.
@@ -24,13 +24,13 @@ Return a range of indices for a field along a 'reduced' dimension.
 offset_indices(::Type{Nothing}, topo, N, H=0) = 1 : 1
 
 """
-    OffsetArray(underlying_data, grid::AbstractGrid, loc)
+    offset_underlying_data(underlying_data, grid::AbstractGrid, loc)
 
 Returns an `OffsetArray` that maps to `underlying_data` in memory,
 with offset indices appropriate for the `data` of a field on
 a `grid` of `size(grid)` and located at `loc`.
 """
-function OffsetArray(underlying_data, grid::AbstractGrid, loc)
+function offset_underlying_data(underlying_data, grid::AbstractGrid, loc)
     ii = offset_indices(loc[1], topology(grid, 1), grid.Nx, grid.Hx)
     jj = offset_indices(loc[2], topology(grid, 2), grid.Ny, grid.Hy)
     kk = offset_indices(loc[3], topology(grid, 3), grid.Nz, grid.Hz)
@@ -50,7 +50,7 @@ function new_data(FT, ::CPU, grid, loc)
                                 total_length(loc[2], topology(grid, 2), grid.Ny, grid.Hy),
                                 total_length(loc[3], topology(grid, 3), grid.Nz, grid.Hz))
 
-    return OffsetArray(underlying_data, grid, loc)
+    return offset_underlying_data(underlying_data, grid, loc)
 end
 
 """
@@ -67,7 +67,7 @@ function new_data(FT, ::GPU, grid, loc)
 
     underlying_data .= 0 # Ensure data is initially 0.
 
-    return OffsetArray(underlying_data, grid, loc)
+    return offset_underlying_data(underlying_data, grid, loc)
 end
 
 # Default to type of Grid
