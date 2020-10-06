abaaf90a1f79cee22c3981cb9f8484afe94a957c
Gregory L. Wagner <wagner.greg@gmail.com>
2020-10-06 10:06:11 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -57,7 +57,7 @@ example_pages = [
     "Ocean wind mixing and convection" => "generated/ocean_wind_mixing_and_convection.md",
     "Ocean convection with plankton"   => "generated/ocean_convection_with_plankton.md",
     "Internal wave"                    => "generated/internal_wave.md",
-    "Langmuir turbulence"              => "generated/langmuir_turbulence.md"
+    "Langmuir turbulence"              => "generated/langmuir_turbulence.md",
     "Eady turbulence"                  => "generated/eady_turbulence.md"
 ]
 
