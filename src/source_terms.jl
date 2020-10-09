d3c02cee234e4ad3e3b8247cb33224ae4d5497d8
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-10-09 11:07:43 -0400

    Add `WickerSkamarockTimeStepper`

--- src/source_terms.jl
+++ src/source_terms.jl
@@ -72,4 +72,4 @@ end
 @inline ρc_slow_source_term(i, j, k, grid::AbstractGrid{T}, tvar::Entropy, gases, gravity, ρ, ρũ, ρc̃) where T = zero(T)
 
 @inline ρc_slow_source_term(i, j, k, grid, tvar::Energy, gases, gravity, ρ, ρũ, ρc̃) =
-    -∂ⱼpuⱼ(i, j, k, grid, diagnose_p, tvar, gases, gravity, ρ, ρũ, ρc̃)
+    -∂ⱼpuⱼ(i, j, k, grid, diagnose_pressure, tvar, gases, gravity, ρ, ρũ, ρc̃)
