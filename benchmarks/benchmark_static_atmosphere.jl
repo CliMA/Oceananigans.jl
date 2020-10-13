9b9f7ef35a5229a4ef326aba3080248d1e629993
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-10-12 21:29:39 -0400

    GPU benchmarks

--- benchmarks/benchmark_static_atmosphere.jl
+++ benchmarks/benchmark_static_atmosphere.jl
@@ -1,24 +1,31 @@
 using Printf
 using TimerOutputs
+
 using Oceananigans
+using Oceananigans.Architectures
 using JULES
 
 const timer = TimerOutput()
-const Δt = 1
 
-grid = RegularCartesianGrid(size=(32, 32, 32), extent=(1, 1, 1), halo=(2, 2, 2))
-model = CompressibleModel(grid=grid, thermodynamic_variable=Energy())
-time_step!(model, Δt)  # warmup to compile
+Ns = (32, 256)
+Tvars = (Energy, Entropy)
 
-for i in 1:10
-    @timeit timer "32×32×32 [CPU, Float64, Energy]" time_step!(model, Δt)
-end
+Archs = [CPU]
+@hascuda Archs = [CPU, GPU]
 
-model = CompressibleModel(grid=grid, thermodynamic_variable=Entropy())
-time_step!(model, Δt)  # warmup to compile
+for Arch in Archs, N in Ns, Tvar in Tvars
 
-for i in 1:10
-    @timeit timer "32×32×32 [CPU, Float64, Entropy]" time_step!(model, Δt)
+    bname = "$N×$N×$N [$Arch, $Tvar]"
+    @info "Benchmarking $bname..."
+
+    grid = RegularCartesianGrid(size=(N, N, N), extent=(1, 1, 1), halo=(2, 2, 2))
+    model = CompressibleModel(architecture=Arch(), grid=grid, thermodynamic_variable=Tvar())
+    time_step!(model, 1)  # warmup to compile
+
+    for i in 1:10
+        @timeit timer bname time_step!(model, 1)
+    end
+
 end
 
 print_timer(timer, title="Static atmosphere benchmarks", sortby=:name)
