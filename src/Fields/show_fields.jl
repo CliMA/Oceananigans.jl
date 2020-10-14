a64c32f397b9cf349cd8251f07746b6f09962616
Gregory Wagner <wagner.greg@gmail.com>
2020-10-13 23:18:32 -0400

    Better show for AveragedField and ComputedField

--- src/Fields/show_fields.jl
+++ src/Fields/show_fields.jl
@@ -3,7 +3,7 @@ import Oceananigans: short_show
 
 location_str(::Type{Face})    = "Face"
 location_str(::Type{Cell})    = "Cell"
-location_str(::Type{Nothing}) = " -- "
+location_str(::Type{Nothing}) = "⋅"
 
 show_location(X, Y, Z) = "($(location_str(X)), $(location_str(Y)), $(location_str(Z)))"
 
@@ -13,11 +13,8 @@ short_show(m::Missing) = "$m"
 
 short_show(field::Field) = string("Field located at ", show_location(field))
 
-short_show(field::AveragedField) = string("AveragedField located at ", show_location(field), " of ", short_show(field.operand))
+short_show(field::AveragedField) = string("AveragedField over dims=$(field.dims) located at ", show_location(field), " of ", short_show(field.operand))
 short_show(field::ComputedField) = string("ComputedField located at ", show_location(field), " of ", short_show(field.operand))
-
-tree_show(field::AveragedField) = string("AveragedField located at ", show_location(field), " of ", tree_show(field.operand))
-tree_show(field::ComputedField) = string("ComputedField located at ", show_location(field), " of ", tree_show(field.operand))
 
 show(io::IO, field::Field) =
     print(io, "$(short_show(field))\n",
@@ -25,17 +22,23 @@ show(io::IO, field::Field) =
           "├── grid: $(short_show(field.grid))\n",
           "└── boundary conditions: $(short_show(field.boundary_conditions))")
 
+show_status(::Nothing) = "nothing"
+show_status(status) = "time=$(status.time)"
+    
+
 show(io::IO, field::AveragedField) =
     print(io, "$(short_show(field))\n",
           "├── data: $(typeof(field.data)), size: $(size(field.data))\n",
           "├── grid: $(short_show(field.grid))", '\n',
           "├── dims: $(field.dims)", '\n',
-          "└── operand: $(short_show(field.operand))")
+          "├── operand: $(short_show(field.operand))", '\n',
+          "└── status: ", show_status(field.status), '\n')
 
 show(io::IO, field::ComputedField) =
     print(io, "$(short_show(field))\n",
           "├── data: $(typeof(field.data)), size: $(size(field.data))\n",
           "├── grid: $(short_show(field.grid))", '\n',
-          "└── operand: $(short_show(field.operand))")
+          "├── operand: $(short_show(field.operand))", '\n',
+          "└── status: ", show_status(field.status), '\n')
 
 short_show(array::OffsetArray{T, D, A}) where {T, D, A} = string("OffsetArray{$T, $D, $A}")
