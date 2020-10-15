783ae3611b2e753045f20e81e4932a1b1e17779e
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-10-15 08:58:21 -0400

    Nice-ish movie

--- verification/dry_rising_thermal_bubble/animate_dry_rising_thermal_bubble.jl
+++ verification/dry_rising_thermal_bubble/animate_dry_rising_thermal_bubble.jl
@@ -10,13 +10,14 @@ using JULES
 cmocean = pyimport("cmocean")
 
 const plt = PyPlot
-const km = kilometers
+plt.ioff()
 
+km = kilometers
 Tvar = Energy
 
-ds = NCDataset("dry_rising_thermal_bubble_$(Tvar)_bad.nc")
+ds = NCDataset("dry_rising_thermal_bubble_$(Tvar).nc")
 
-function plot_dry_rising_thermal_bubble_frame(n)
+for n in 1:length(ds["time"])
     fig, axes = plt.subplots(nrows=2, ncols=2, sharex=true, sharey=true, figsize=(16, 9), dpi=200)
 
     xC, xF = ds["xC"] / km, ds["xF"] / km
@@ -33,30 +34,34 @@ function plot_dry_rising_thermal_bubble_frame(n)
     fig.suptitle("Dry rising thermal bubble: time = $(prettytime(ds["time"][n]))")
 
     ax_u = axes[1, 1]
-    im = ax_u.pcolormesh(xF, zC, ρu ./ ρ, cmap=cmocean.cm.balance)
+    im = ax_u.pcolormesh(xF, zC, ρu ./ ρ, cmap=cmocean.cm.balance, vmin=-10, vmax=10)
     ax_u.set_title("u(x, z)")
     fig.colorbar(im, ax=ax_u)
 
     ax_w = axes[1, 2]
-    im = ax_w.pcolormesh(xC, zC, ρw ./ ρ, cmap=cmocean.cm.balance)
+    im = ax_w.pcolormesh(xC, zC, ρw ./ ρ, cmap=cmocean.cm.balance, vmin=-10, vmax=10)
     ax_w.set_title("w(x, z)")
     fig.colorbar(im, ax=ax_w)
 
     ax_ρ = axes[2, 1]
-    im = ax_ρ.pcolormesh(xC, zC, ρ - ρ₀, cmap=cmocean.cm.dense)
+    im = ax_ρ.pcolormesh(xC, zC, ρ - ρ₀, cmap=cmocean.cm.dense, vmin=-0.007, vmax=0.007)
     ax_ρ.set_title("ρ′(x, z)")
     fig.colorbar(im, ax=ax_ρ)
 
     ax_e = axes[2, 2]
-    im = ax_e.pcolormesh(xC, zC, (ρe .- ρe₀) ./ ρ, cmap=cmocean.cm.thermal)
+    im = ax_e.pcolormesh(xC, zC, (ρe .- ρe₀) ./ ρ, cmap=cmocean.cm.thermal, vmin=0, vmax=1200)
     ax_e.set_title("e′(x, z)")
     fig.colorbar(im, ax=ax_e)
 
     plt.xlim(-10, 10)
     plt.ylim(0, 10)
 
-    filename = @sprintf("dry_rising_thermal_bubble_%03d.png", n)
+    filename = @sprintf("dry_rising_thermal_bubble_%s_%03d.png", Tvar, n)
+    @info "Saving $filename..."
     plt.savefig(filename)
+    plt.close(fig)
 end
 
-# close(ds)
+close(ds)
+
+run(`ffmpeg -r 15 -f image2 -i dry_rising_thermal_bubble_$(Tvar)_%03d.png -vcodec libx264 -crf 22 -pix_fmt yuv420p dry_rising_thermal_bubble_$Tvar.mp4`)
