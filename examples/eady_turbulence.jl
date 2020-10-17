5e065390b5c759ea2f3133efb57fe3125d972c96
Navid Constantinou <navidcy@gmail.com>
2020-10-17 17:58:08 +1100

    fixes typo and some pending latex issues

--- examples/eady_turbulence.jl
+++ examples/eady_turbulence.jl
@@ -61,15 +61,19 @@
 # positive (upwards) with the velocity at the bottom boundary is negative.
 # This drag term is "quadratic" because the rate at which momentum is removed is proportional
 # to $\boldsymbol{u}_h |\boldsymbol{u}_h|$, where 
-# $\boldsymbol{u}_h = u \boldsymbol{\hat{x}} + v boldsymbol{\hat{y}}$ is the horizontal velocity.
+# $\boldsymbol{u}_h = u \boldsymbol{\hat{x}} + v \boldsymbol{\hat{y}}$ is the horizontal velocity.
 #
 # The $x$-component of the quadratic bottom drag is thus
-#
-# $ \tau_{xz}(z=L_z) = - c^D u \sqrt{u^2 + v^2} \, , $
+# 
+# ```math
+# \tau_{xz}(z=L_z) = - c^D u \sqrt{u^2 + v^2} \, ,
+# ```
 #
 # while the $y$-component is
 #
-# $ \tau_{yz}(z=L_z) = - c^D v \sqrt{u^2 + v^2} \, , 
+# ```math
+# \tau_{yz}(z=L_z) = - c^D v \sqrt{u^2 + v^2} \, , 
+# ``
 #
 # where $c^D$ is a dimensionless drag coefficient and $\tau_{xz}(z=L_z)$ and $\tau_{yz}(z=L_z)$
 # denote the flux of $u$ and $v$ momentum at $z = L_z$, the bottom of the domain.
