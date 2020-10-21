9f006144b664797628fd1dca090ce6c94dcab0e8
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-10-20 20:11:54 -0400

    A docstring and export

--- src/Fields/Fields.jl
+++ src/Fields/Fields.jl
@@ -2,12 +2,14 @@ module Fields
 
 export
     Face, Cell,
-    AbstractField, Field, CellField, XFaceField, YFaceField, ZFaceField,
+    AbstractField, Field,
+    CellField, XFaceField, YFaceField, ZFaceField,
     ReducedField, AveragedField, ComputedField, BackgroundField,
     interior, interiorparent, data,
     xnode, ynode, znode, location,
     set!, compute!, @compute,
-    VelocityFields, TracerFields, tracernames, PressureFields, TendencyFields
+    VelocityFields, TracerFields, tracernames, PressureFields, TendencyFields,
+    interpolate
 
 using Oceananigans.Architectures
 using Oceananigans.Grids
@@ -17,9 +19,9 @@ Base.zeros(FT, ::CPU, Nx, Ny, Nz) = zeros(FT, Nx, Ny, Nz)
 Base.zeros(FT, ::GPU, Nx, Ny, Nz) = zeros(FT, Nx, Ny, Nz) |> CuArray
 Base.zeros(arch, grid, Nx, Ny, Nz) = zeros(eltype(grid), arch, Nx, Ny, Nz)
 
-assumed_field_location(name) = name === :u ? (Face, Cell, Cell) :
-                               name === :v ? (Cell, Face, Cell) :
-                               name === :w ? (Cell, Cell, Face) :
+assumed_field_location(name) = name == :u ? (Face, Cell, Cell) :
+                               name == :v ? (Cell, Face, Cell) :
+                               name == :w ? (Cell, Cell, Face) :
                                              (Cell, Cell, Cell)
 
 include("new_data.jl")
@@ -37,5 +39,6 @@ include("validate_field_tuple_grid.jl")
 include("field_tuples.jl")
 include("background_fields.jl")
 include("show_fields.jl")
+include("interpolate.jl")
 
 end
--- src/Fields/interpolate.jl
+++ src/Fields/interpolate.jl
@@ -1,9 +1,5 @@
-struct Interpolator{F}
-    field :: F
-end
-
 # Converting (x, y, z) coordinates to indices on a RegularCartesianGrid.
-# The returned indices can be floats indicating a location between grid points.
+# The returned indices are floats indicating a location between grid points.
 @inline x_to_index(x, ::Type{Cell}, grid) = @inbounds (x - grid.xC[1]) / grid.Δx
 @inline x_to_index(x, ::Type{Face}, grid) = @inbounds (x - grid.xF[1]) / grid.Δx
 
@@ -20,7 +16,7 @@ end
     return i, j, k
 end
 
-# Trilinear Lagrangian polynomials
+# Trilinear Lagrange polynomials
 @inline ϕ₁(ξ, η, ζ) = (1 - ξ) * (1 - η) * (1 - ζ)
 @inline ϕ₂(ξ, η, ζ) = (1 - ξ) * (1 - η) * ζ
 @inline ϕ₃(ξ, η, ζ) = (1 - ξ) * η * (1 - ζ)
@@ -30,27 +26,29 @@ end
 @inline ϕ₇(ξ, η, ζ) = ξ * η * (1 - ζ)
 @inline ϕ₈(ξ, η, ζ) = ξ * η * ζ
 
-@inline function interpolate(ξ, η, ζ, i, j, k, field)
-    return (  ϕ₁(ξ, η, ζ) * field[i,   j,   k  ]
-            + ϕ₂(ξ, η, ζ) * field[i,   j,   k+1]
-            + ϕ₃(ξ, η, ζ) * field[i,   j+1, k+1]
-            + ϕ₄(ξ, η, ζ) * field[i,   j+1, k+1]
-            + ϕ₅(ξ, η, ζ) * field[i+1, j,   k  ]
-            + ϕ₆(ξ, η, ζ) * field[i+1, j,   k+1]
-            + ϕ₇(ξ, η, ζ) * field[i+1, j+1, k  ]
-            + ϕ₈(ξ, η, ζ) * field[i+1, j+1, k+1])
-end
+@inline _interpolate(ξ, η, ζ, i, j, k, field) =
+    @inbounds (  ϕ₁(ξ, η, ζ) * field[i,   j,   k  ]
+               + ϕ₂(ξ, η, ζ) * field[i,   j,   k+1]
+               + ϕ₃(ξ, η, ζ) * field[i,   j+1, k+1]
+               + ϕ₄(ξ, η, ζ) * field[i,   j+1, k+1]
+               + ϕ₅(ξ, η, ζ) * field[i+1, j,   k  ]
+               + ϕ₆(ξ, η, ζ) * field[i+1, j,   k+1]
+               + ϕ₇(ξ, η, ζ) * field[i+1, j+1, k  ]
+               + ϕ₈(ξ, η, ζ) * field[i+1, j+1, k+1])
 
-@inline function (ℑ::Interpolator)(x, y, z, grid=ℑ.field.grid)
-    i, j, k = coordinates_to_indices(x, y, z, location(ℑ.field), grid)
+"""
+    interpolate(field, x, y, z)
 
-    # Convert fractional indices to integer indices and unit
-    # cell coordinates 0 <= (ξ, η, ζ) <=1 .
+Interpolate `field` to the physical point `(x, y, z)` using trilinear interpolation.
+"""
+@inline function interpolate(field, x, y, z)
+    i, j, k = coordinates_to_indices(x, y, z, location(field), field.grid)
+
+    # Convert fractional indices to unit cell coordinates 0 <= (ξ, η, ζ) <=1
+    # and integer indices.
     ξ, i = modf(i)
     η, j = modf(j)
     ζ, k = modf(k)
 
-    i, j, k = Int(i), Int(j), Int(k)
-
-    return interpolate(ξ, η, ζ, i, j, k, ℑ.field.data)
+    return _interpolate(ξ, η, ζ, Int(i), Int(j), Int(k), field.data)
 end
