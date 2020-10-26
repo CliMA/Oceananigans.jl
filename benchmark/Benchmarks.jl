7f3ac4149f13b38d54f60a0b6239daec6556e82e
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-10-26 13:37:01 -0400

    Convert benchmark suite to data frame and put in a pretty table

--- benchmark/Benchmarks.jl
+++ benchmark/Benchmarks.jl
@@ -28,4 +28,33 @@ function run_benchmark_suite(benchmark_fun; kwargs...)
     return suite
 end
 
+function benchmark_suite_to_dataframe(suite)
+    names = Tuple(Symbol(tag) for tag in suite.tags)
+    df_names = (names..., :min, :median, :mean, :max, :memory, :allocs)
+    empty_cols = Tuple([] for k in df_names)
+    df = DataFrame(; NamedTuple{df_names}(empty_cols)...)
+    
+    for case in keys(suite)
+        trial = suite[case]
+        entry = NamedTuple{names}(case) |> pairs |> Dict
+       
+        entry[:min] = minimum(trial.times) |> prettytime
+        entry[:median] = median(trial.times) |> prettytime
+        entry[:mean] = mean(trial.times) |> prettytime
+        entry[:max] = maximum(trial.times) |> prettytime
+        entry[:memory] = prettymemory(trial.memory)
+        entry[:allocs] = trial.allocs
+
+        push!(df, entry)
+    end
+
+    return df
+end
+
+function summarize_benchmark_suite(df)
+    header = propertynames(df) .|> String
+    pretty_table(df, header, title="Incompressible model benchmarks", nosubheader=true)
+    return nothing
+end
+
 end
