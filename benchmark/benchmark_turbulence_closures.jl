5f85abbc7178771f5dddbee31134411e231ac920
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-11-10 07:17:59 -0500

    Benchmark turbulence closures

--- benchmark/benchmark_turbulence_closures.jl
+++ benchmark/benchmark_turbulence_closures.jl
@@ -1,50 +1,44 @@
-using Printf
-using TimerOutputs
 using Oceananigans
-using Oceananigans.TurbulenceClosures
-using Oceananigans.Utils
+using Benchmarks
 
-include("benchmark_utils.jl")
+# Benchmark function
 
-#####
-##### Benchmark setup and parameters
-#####
+function benchmark_closure(Arch, Closure)
+    grid = RegularCartesianGrid(size=(192, 192, 192), extent=(1, 1, 1))
+    model = IncompressibleModel(architecture=Arch(), grid=grid, advection=Closure())
 
-const timer = TimerOutput()
+    time_step!(model, 1) # warmup
 
-Nt = 10  # Number of iterations to use for benchmarking time stepping.
-
-# Run benchmark across these parameters.
-            Ns = [(32, 32, 32), (256, 256, 128)]
-   float_types = [Float64]       # Float types to benchmark.
-         archs = [CPU()]         # Architectures to benchmark on.
-@hascuda archs = [CPU(), GPU()]  # Benchmark GPU on systems with CUDA-enabled GPUs.
-      closures = [IsotropicDiffusivity, AnisotropicDiffusivity, SmagorinskyLilly,
-	          VerstappenAnisotropicMinimumDissipation]
-
-#####
-##### Run benchmarks
-#####
-
-for arch in archs, FT in float_types, N in Ns, Closure in closures
-	grid = RegularCartesianGrid(FT, size=N, extent=(1, 1, 1))
-    model = IncompressibleModel(architecture=arch, float_type=FT, grid=grid, closure=Closure(FT))
-
-    time_step!(model, 1)  # precompile
-
-    bn =  benchmark_name(N, string(Closure), arch, FT)
-    @printf("Running benchmark: %s...\n", bn)
-    for i in 1:Nt
-        @timeit timer bn time_step!(model, 1)
-    end
+    trial = @benchmark begin
+        @sync_gpu time_step!($model, 1)
+    end samples=10
+    
+    return trial
 end
 
-#####
-##### Print benchmark results
-#####
+# Benchmark parameters
 
-println()
-println(oceananigans_versioninfo())
-println(versioninfo_with_gpu())
-print_timer(timer, title="Turbulence closure benchmarks", sortby=:name)
-println()
+Architectures = has_cuda() ? [CPU, GPU] : [CPU]
+
+Closures = [Nothing,
+            IsotropicDiffusivity,
+            AnisotropicDiffusivity,
+            AnisotropicBiharmonicDiffusivity,
+            TwoDimensionalLeith,
+            SmagorinskyLilly,
+            AnisotropicMinimumDissipation]
+
+# Run and summarize benchmarks
+
+print_machine_info()
+suite = run_benchmarks(benchmark_closure; Architectures, Closures)
+
+df = benchmarks_dataframe(suite)
+sort!(df, [:Architectures, :Closures], by=(string, string))
+benchmarks_pretty_table(df, title="Turbulence closure benchmarks")
+
+if GPU in Architectures
+    df_Δ = gpu_speedups_suite(suite) |> speedups_dataframe
+    sort!(df_Δ, :Closures, by=string)
+    benchmarks_pretty_table(df, title="Turbulence closure CPU -> GPU speedup")
+end
