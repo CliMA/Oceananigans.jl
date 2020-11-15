58d47e3a1b8f70551bfa8f330ab57c08d3f8cb17
Gregory Wagner <wagner.greg@gmail.com>
2020-11-15 09:04:47 -0500

    Experiments with a wavy hello ocean

--- examples/hello_ocean.jl
+++ examples/hello_ocean.jl
@@ -7,29 +7,34 @@
 using Oceananigans, Oceananigans.Grids, Plots
 
 grid = RegularCartesianGrid(size = (1, 64, 64),
-                            x = (0, 1), y = (-4, 4), z = (-4, 4),
-                            topology = (Periodic, Bounded, Bounded))
+                            x = (-6, 6), y = (-6, 6), z = (-3, 3),
+                            topology = (Periodic, Periodic, Bounded))
 
 model = IncompressibleModel(grid = grid,
                             architecture = CPU(),
                             advection = Oceananigans.Advection.WENO5(),
+                            timestepper = :RungeKutta3,
                             tracers = :b,
                             buoyancy = BuoyancyTracer(),
                             closure = IsotropicDiffusivity(ν=1e-3, κ=1e-3))
-         
-@info "Simulating a rising buoyant bubble with" model
 
-set!(model, b = (x, y, z) -> exp(-y^2 - z^2))
+@info "Simulating the ocean with" model
 
-run!(Simulation(model, Δt=0.01, stop_iteration=800))
+set!(model,
+     u = (x, y, z) -> 1 + tanh(z/2),
+     b = (x, y, z) -> z + exp(-x^2) * (tanh(z) - 1))
+
+run!(Simulation(model, Δt=0.1, stop_iteration=1))
 
 # Analyze the data
 
 b = model.tracers.b
 
-plt = contourf(ynodes(b), znodes(b), interior(b)[1, :, :]',
-               xlabel = "y", ylabel = "z", title = "Buoyancy",
-               xlim = (grid.yF[1], grid.yF[end]), ylim = (grid.zF[1], grid.zF[end]),
+@show maximum(abs, b.data)
+
+plt = contourf(xnodes(b), znodes(b), interior(b)[:, 1, :]',
+               xlabel = "x", ylabel = "z", title = "Hello, ocean!",
+               #xlim = (grid.xF[1], grid.xF[end]), ylim = (grid.zF[1], grid.zF[end]),
                aspectratio = :equal, linewidth = 0)
 
 display(plt) # hide
