b45079f9d325192a62a07ba807b1e644d2d0e1c0
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-11-24 08:31:49 -0500

    Run Lagrangian particle tracking test on GPU

--- test/test_lagrangian_particle_tracking.jl
+++ test/test_lagrangian_particle_tracking.jl
@@ -1,13 +1,15 @@
-@testset "Lagrangian particle tracking" begin
-    @info "  Testing Lagrangian particle tacking..."
+function run_simple_particle_tracking_tests(arch)
     topo = (Periodic, Periodic, Bounded)
     domain = (x=(0, 1), y=(0, 1), z=(0, 1))
     grid = RegularCartesianGrid(topology=topo, size=(5, 5, 5); domain...)
 
     P = 10
-    xs, ys, zs = zeros(P), zeros(P), rand(P)
+    xs = convert(array_type(arch), zeros(P))
+    ys = convert(array_type(arch), zeros(P))
+    zs = convert(array_type(arch), zeros(P))
 
-    model = IncompressibleModel(grid=grid, particles=LagrangianParticles(x=xs, y=ys, z=zs))
+    model = IncompressibleModel(architecture=arch, grid=grid,
+                                particles=LagrangianParticles(x=xs, y=ys, z=zs))
 
     set!(model, u=1, v=1)
 
@@ -16,4 +18,13 @@
     # Easy placeholder test!
     @test all(model.particles.x .≈ 0.01)
     @test all(model.particles.y .≈ 0.01)
+
+    return nothing
+end
+
+@testset "Lagrangian particle tracking" begin
+    for arch in archs
+        @info "  Testing Lagrangian particle tacking [$(typeof(arch))]..."
+        run_simple_particle_tracking_tests(arch)
+    end
 end
