e1e083f9b81085d4c6b2cd5b7ac1bef0ac3d624e
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-11-26 12:49:49 -0500

    Print some results and only benchmark Float64

--- benchmark/benchmark_regression.jl
+++ benchmark/benchmark_regression.jl
@@ -6,5 +6,13 @@ baseline = BenchmarkConfig(id="master")
 script = joinpath(@__DIR__, "benchmarkable_incompressible_model.jl")
 resultfile = joinpath(@__DIR__, "regression_benchmarks.json")
 
+print_system_info()
+
 judgement = judge(Oceananigans, baseline, script=script, resultfile=resultfile, verbose=true)
+results = PkgBenchmark.benchmarkgroup(judgement)
+
+for (case, trial) in results
+    println("Results for $case")
+    display(trial)
+end
 
--- benchmark/benchmarkable_incompressible_model.jl
+++ benchmark/benchmarkable_incompressible_model.jl
@@ -6,18 +6,17 @@ using Benchmarks
 # Benchmark parameters
 
 Architectures = has_cuda() ? [CPU, GPU] : [CPU]
-Float_types = [Float32, Float64]
 Ns = [32, 64, 128, 256]
 
 # Define benchmarks
 
 SUITE = BenchmarkGroup()
 
-for Arch in Architectures, FT in Float_types, N in Ns
-    @info "Setting up benchmark: ($Arch, $FT, $N)..."
+for Arch in Architectures, N in Ns
+    @info "Setting up benchmark: ($Arch, $N)..."
 
-    grid = RegularCartesianGrid(FT, size=(N, N, N), extent=(1, 1, 1))
-    model = IncompressibleModel(architecture=Arch(), float_type=FT, grid=grid)
+    grid = RegularCartesianGrid(size=(N, N, N), extent=(1, 1, 1))
+    model = IncompressibleModel(architecture=Arch(), grid=grid)
 
     time_step!(model, 1) # warmup
 
@@ -25,5 +24,6 @@ for Arch in Architectures, FT in Float_types, N in Ns
         @sync_gpu time_step!($model, 1)
     end samples=10
 
-    SUITE[(Arch, FT, N)] = benchmark
+    SUITE[(Arch, N)] = benchmark
 end
+
