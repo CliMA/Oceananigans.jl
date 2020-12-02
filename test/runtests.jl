241df6e371011f30c8c16fbadcc57315e406486c
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-12-02 09:09:06 -0500

    Revert "Temporarily nuke WENO{N}"
    This reverts commit 3a761b645a4a41cacb785eff88bfa12f4edca11a.

--- test/runtests.jl
+++ test/runtests.jl
@@ -88,6 +88,7 @@ group = get(ENV, "TEST_GROUP", :all) |> Symbol
             include("test_coriolis.jl")
             include("test_buoyancy.jl")
             include("test_surface_waves.jl")
+            include("test_weno_reconstruction.jl")
             include("test_utils.jl")
         end
     end
