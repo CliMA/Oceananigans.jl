6d73625aa66fc099d51a73ef91228cc3d30e25fe
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-12-04 11:24:47 -0500

    Update operations benchmark to use new framework

--- benchmark/benchmark_abstract_operations.jl
+++ benchmark/benchmark_abstract_operations.jl
@@ -1,35 +1,32 @@
-using Printf
-using TimerOutputs
+using BenchmarkTools
+using CUDA
 using OrderedCollections
-
 using Oceananigans
-using Oceananigans.Utils
 using Oceananigans.Grids
 using Oceananigans.AbstractOperations
-
-include("benchmark_utils.jl")
-
-#####
-##### Benchmark setup and parameters
-#####
-
-const timer = TimerOutput()
+using Oceananigans.Fields
+using Oceananigans.Utils
+using Benchmarks
 
 FT = Float64
-Nt = 10  # Number of iterations to use for benchmarking time stepping.
 
-         archs = [CPU()]        # Architectures to benchmark on.
-@hascuda archs = [CPU(), GPU()] # Benchmark GPU on systems with CUDA-enabled GPUs.
+         Archs = [CPU]
+@hascuda Archs = [CPU, GPU]
 
 #####
 ##### Run benchmarks
 #####
 
-for arch in archs
-    N = arch isa CPU ? (32, 32, 32) : (256, 256, 256)
+tags = ["Architecture", "ID", "Operation"]
+suite = BenchmarkGroup(tags)
 
-    grid = RegularCartesianGrid(size=N, extent=(1, 1, 1))
-    model = IncompressibleModel(architecture=arch, float_type=FT, grid=grid,
+print_system_info()
+
+for Arch in Archs
+    N = Arch == CPU ? (32, 32, 32) : (256, 256, 256)
+
+    grid = RegularCartesianGrid(FT, size=N, extent=(1, 1, 1))
+    model = IncompressibleModel(architecture=Arch(), float_type=FT, grid=grid,
                                 buoyancy=nothing, tracers=(:α, :β, :γ, :δ, :ζ))
 
     ε(x, y, z) = randn()
@@ -39,7 +36,7 @@ for arch in archs
     u, v, w = model.velocities
     α, β, γ, δ, ζ = model.tracers
 
-    dump_field = Field(Cell, Cell, Cell, arch, grid)
+    dump_field = Field(Cell, Cell, Cell, Arch(), grid)
 
     test_cases = OrderedDict(
         "-α"      => -α,
@@ -53,7 +50,7 @@ for arch in archs
         "α * β * γ * δ"     => α * β * γ * δ,
         "α * β - γ * δ / ζ" => α * β - γ * δ / ζ,
         "u^2 + v^2" => u^2 + v^2,
-        "(u^2 + v^2 + w^2) / 2" => (u^2 + v^2 + w^2) / 2,
+        "√(u^2 + v^2 + w^2)" => √(u^2 + v^2 + w^2),
         "∂x(α)" => ∂x(α),
         "∂y(∂y(β))" => ∂y(∂y(β)),
         "∂z(∂z(∂z(∂z(γ))))" => ∂z(∂z(∂z(∂z(γ)))),
@@ -67,24 +64,27 @@ for arch in archs
     )
 
     for (i, (test_name, op)) in enumerate(test_cases)
-        op_computation = Computation(op, dump_field)
-        compute!(op_computation)  # precompile
+        computed_field = ComputedField(op)
 
-        test_number = @sprintf("%02d", i)
-        bname =  benchmark_name(N, "[$test_number] $test_name", arch, nothing)
-        @info "Running benchmark: $bname..."
-        for _ in 1:Nt
-            @timeit timer bname compute!(op_computation)
-        end
+        compute!(computed_field)  # warmup
+
+        @info "Running abstract operation benchmark: $test_name..."
+
+        trial = @benchmark begin
+            @sync_gpu compute!($computed_field)
+        end samples=10
+
+        suite[(Arch, i, test_name)] = trial
     end
 end
 
-#####
-##### Print benchmark results
-#####
+df = benchmarks_dataframe(suite)
+sort!(df, :ID)
+benchmarks_pretty_table(df, title="Abstract operations benchmarks")
 
-println()
-println(oceananigans_versioninfo())
-println(versioninfo_with_gpu())
-print_timer(timer, title="Abstract operations benchmarks", sortby=:name)
-println()
+for Arch in Archs
+    suite_arch = speedups_suite(suite[@tagged Arch], base_case=(Arch, 1, "-α"))
+    df_arch = speedups_dataframe(suite_arch, slowdown=true)
+    sort!(df_arch, :ID)
+    benchmarks_pretty_table(df_arch, title="Abstract operations relative peformance ($Arch)")
+end
