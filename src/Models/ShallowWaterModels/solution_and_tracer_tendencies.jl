27844ca328d2c85e152c3ce219494d720ce53b5a
francispoulin <fpoulin@uwaterloo.ca>
2020-12-08 11:21:17 -0500

    Updating fluxes to use divergence operators, which allows for different advection schemes.

--- src/Models/ShallowWaterModels/solution_and_tracer_tendencies.jl
+++ src/Models/ShallowWaterModels/solution_and_tracer_tendencies.jl
@@ -2,20 +2,6 @@ using Oceananigans.Advection
 using Oceananigans.Coriolis
 using Oceananigans.Operators
 
-@inline squared(i, j, k, grid, ϕ) = ϕ[i, j, k]^2
-
-@inline uh_transport_x(i, j, k, grid, h, uh, g) =
-    @inbounds ℑxᶜᵃᵃ(i, j, k, grid, squared, uh) / h[i, j, k] + g/2 * h[i, j, k]^2
-
-@inline uh_transport_y(i, j, k, grid, h, uh, vh) =
-    ℑyᵃᶠᵃ(i, j, k, grid, uh) * ℑxᶠᵃᵃ(i, j, k, grid, vh) / ℑxyᶠᶠᵃ(i, j, k, grid, h)
-
-@inline vh_transport_x(i, j, k, grid, h, uh, vh) =
-    ℑyᵃᶠᵃ(i, j, k, grid, uh) * ℑxᶠᵃᵃ(i, j, k, grid, vh) / ℑxyᶠᶠᵃ(i, j, k, grid, h)
-
-@inline vh_transport_y(i, j, k, grid, h, vh, g) =
-    @inbounds ℑyᵃᶜᵃ(i, j, k, grid, squared, vh) / h[i, j, k] + g/2 * h[i, j, k]^2
-
 """
 Compute the tendency for the x-directional transport, uh
 """
@@ -30,10 +16,7 @@ Compute the tendency for the x-directional transport, uh
                                       forcings,
                                       clock)
 
-    g = gravitational_acceleration
-
-    return ( - ∂xᶠᵃᵃ(i, j, k, grid, uh_transport_x, solution.h, solution.uh, gravitational_acceleration)
-             - ∂yᵃᶠᵃ(i, j, k, grid, uh_transport_y, solution.h, solution.uh, solution.vh)
+    return ( - div_hUu(i, j, k, grid, advection, solution)
              - x_f_cross_U(i, j, k, grid, coriolis, solution) )
 end
 
@@ -51,8 +34,7 @@ Compute the tendency for the y-directional transport, vh.
                                       forcings,
                                       clock)
 
-    return ( - ∂xᶠᵃᵃ(i, j, k, grid, vh_transport_x, solution.h, solution.uh, solution.vh)
-             - ∂yᵃᶠᵃ(i, j, k, grid, vh_transport_y, solution.h, solution.vh, gravitational_acceleration)
+    return ( - div_hUv(i, j, k, grid, advection, solution)
              - y_f_cross_U(i, j, k, grid, coriolis, solution) )
 end
 
