50a2ae832b085ae6f3e48e068cbbb54b711a5807
francispoulin <fpoulin@uwaterloo.ca>
2020-12-11 13:52:15 -0500

    Updating script to compute accuracy of center differencing schemes.

--- test/test_accuracy.jl
+++ test/test_accuracy.jl
@@ -4,7 +4,13 @@ using Oceananigans.Grids: Periodic, Bounded
 using Oceananigans.Advection
 using Oceananigans.Grids: xnodes,ynodes
 
-using Plots
+using Plots, LaTeXStrings
+
+using Printf
+
+using Polynomials
+
+using LinearAlgebra
 
 include("../src/Models/ShallowWaterModels/shallow_water_advection_operators.jl")
 using Oceananigans.Operators
@@ -19,18 +25,13 @@ Nrange = 2 .^collect(6:10)
 error  = zeros(length(Nrange));
 
 
-for i in collect(1:length(Nrange))
+for ii in 1:length(Nrange)
 
-    N = Nrange[i]
+    N = Nrange[ii]
     
     grid = RegularCartesianGrid(size=(N, 1, 1), x=(-L/2,L/2), y=(0,1), z=(0,1), topology=(Periodic, Periodic, Bounded));
-    
-    model = ShallowWaterModel(        grid = grid,
-            gravitational_acceleration = 1,
-                          architecture = CPU(),
-                             advection = WENO5(), 
-                              coriolis = FPlane(f=0.0)
-                                  )
+
+    model = ShallowWaterModel(grid = grid, gravitational_acceleration = 1, architecture = CPU())
     
     h(x, y, z) = 1.0 + amp * exp(- x^2 / (2width^2));  
     set!(model, h = h)
@@ -47,24 +48,35 @@ for i in collect(1:length(Nrange))
     set!(model, h = h)
     
     dhdx = zeros(model.grid.Nx, model.grid.Ny, model.grid.Nz)
+
     for k in 1:model.grid.Nz, j in 1:model.grid.Ny, i in 1:model.grid.Nx
+        #dhdx[i,j,k] = ∂x₄ᶠᵃᵃ(i, j, k, grid, model.solution.h)    
         dhdx[i,j,k] = ∂x₂ᶠᵃᵃ(i, j, k, grid, model.solution.h)    
     end
-    
+
     diffdh = dhdx[:,1,1] - dhf
-    #error[cnt] = diffdh[Int(N/2)];
-    error[i] = sqrt(sum(diffdh[2:end].^2))
-    println("N = ", N, " error is ", error[i])
-    
+    error[ii] = norm(diffdh[3:end-2])
+    println("N = ", N, " error is ", error[ii])
+
 end
 
+p = fit(log.(Nrange), log.(error), 1)
 
-plt1 = plot(Nrange, abs.(error), xaxis=:log, yaxis=:log, seriestype = :scatter)
-plt2 = plot!(Nrange, 30*Nrange.^(-2), linewidth=3)
+line_label = @sprintf "%2.1f slope" p[1]
+
+println(line_label)
+
+plt1 = plot(Nrange, abs.(error), xaxis=:log, yaxis=:log, seriestype = :scatter, label="data", markersize=8, title="2nd Order Center Differencing")
+plt2 = plot!(Nrange, 4e2*Nrange.^(-2), linewidth=3, label="-2 slope")
+plt3 = plot!(Nrange, exp(p[0]).*Nrange.^(p[1]), linewidth=3, label=line_label)
 #display(plt2)
-savefig("results")
+savefig("results2nd")
 
 for i in collect(1:length(Nrange)-1)
     println("i = ", i, " and error = ", error[i]/error[i+1])
 end
 
+println("Best fit for the slope of the data is ", p[1])
+
+
+    
