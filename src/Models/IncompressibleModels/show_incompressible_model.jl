2a98dee98622109d4f93db6f8020cddab2d9895d
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2021-01-13 09:29:14 -0500

    Show particles if they exist

--- src/Models/IncompressibleModels/show_incompressible_model.jl
+++ src/Models/IncompressibleModels/show_incompressible_model.jl
@@ -2,11 +2,20 @@ using Oceananigans.Utils: prettytime, ordered_dict_show
 using Oceananigans: short_show
 
 """Show the innards of a `Model` in the REPL."""
-Base.show(io::IO, model::IncompressibleModel{TS, C, A}) where {TS, C, A} =
+function Base.show(io::IO, model::IncompressibleModel{TS, C, A}) where {TS, C, A}
     print(io, "IncompressibleModel{$A, $(eltype(model.grid))}",
         "(time = $(prettytime(model.clock.time)), iteration = $(model.clock.iteration)) \n",
         "├── grid: $(short_show(model.grid))\n",
         "├── tracers: $(tracernames(model.tracers))\n",
         "├── closure: $(typeof(model.closure))\n",
-        "├── buoyancy: $(typeof(model.buoyancy))\n",
-        "└── coriolis: $(typeof(model.coriolis))")
+        "├── buoyancy: $(typeof(model.buoyancy))\n")
+
+    if isnothing(model.particles)
+        print(io, "└── coriolis: $(typeof(model.coriolis))")
+    else
+        particles = model.particles.particles
+        properties = propertynames(particles)
+        print(io, "├── coriolis: $(typeof(model.coriolis))\n")
+        print(io, "└── particles: $(length(particles)) Lagrangian particles with $(length(properties)) properties: $properties")
+    end
+end
