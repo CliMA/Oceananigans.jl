c84a958e708051acd32536a85712e90ab2deae2d
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2021-02-05 09:54:29 -0500

    Some basic tests for slab decomposition models

--- src/Distributed/test_distributed_model.jl
+++ src/Distributed/test_distributed_model.jl
@@ -1,23 +1,140 @@
 using Test
+using MPI
+using Oceananigans
 
-import MPI
+MPI.Initialized() || MPI.Init()
+comm = MPI.COMM_WORLD
 
-using Oceananigans: interior, fill_halo_regions!
+# Right now just testing with 4 ranks!
+mpi_ranks = MPI.Comm_size(comm)
+@assert mpi_ranks == 4
 
-include("distributed_model.jl")
+function test_triply_periodic_connectivity_with_411_ranks()
+    topo = (Periodic, Periodic, Periodic)
+    full_grid = RegularCartesianGrid(topology=topo, size=(8, 8, 8), extent=(1, 2, 3))
+    dm = DistributedModel(grid=full_grid, ranks=(4, 1, 1))
 
-MPI.Init()
+    my_rank = MPI.Comm_rank(MPI.COMM_WORLD)
+    @test my_rank == index2rank(dm.index..., dm.ranks...)
 
-dm = DistributedModel(ranks = (2, 2, 2), size = (16, 16, 16),
-                      x = (0, 1), y = (-0.5, 0.5), z = (-10, 0),
-                      boundary_conditions = HorizontallyPeriodicSolutionBCs(),
-                      poisson_solver = nothing)
+    model = dm.model
+    connectivity = dm.connectivity
 
-my_rank = MPI.Comm_rank(MPI.COMM_WORLD)
-# @info "Rank $my_rank: $(dm.connectivity), $(dm.model.grid.zF[end])"
-# @info "u.x BCs: $(dm.model.boundary_conditions.solution.u.x)"
+    # No communication in y and z.
+    @test isnothing(connectivity.south)
+    @test isnothing(connectivity.north)
+    @test isnothing(connectivity.top)
+    @test isnothing(connectivity.bottom)
 
-interior(dm.model.velocities.u) .= rand(8, 8, 8)
-fill_halo_regions!(dm.model.velocities.u.data, dm.model.boundary_conditions.solution.u, CPU(), dm.model.grid)
+    if my_rank == 0
+        @test connectivity.east == 1
+        @test connectivity.west == 3
+    elseif my_rank == 1
+        @test connectivity.east == 2
+        @test connectivity.west == 0
+    elseif my_rank == 2
+        @test connectivity.east == 3
+        @test connectivity.west == 1
+    elseif my_rank == 3
+        @test connectivity.east == 0
+        @test connectivity.west == 2
+    end
 
-display(interior(dm.model.velocities.u))
+    nx, ny, nz = size(model.grid)
+    @test model.grid.xF[1] == 0.25*my_rank
+    @test model.grid.xF[nx+1] == 0.25*(my_rank+1)
+    @test model.grid.yF[1] == 0
+    @test model.grid.yF[ny+1] == 2
+    @test model.grid.zF[1] == -3
+    @test model.grid.zF[nz+1] == 0
+end
+
+function test_triply_periodic_connectivity_with_141_ranks()
+    topo = (Periodic, Periodic, Periodic)
+    full_grid = RegularCartesianGrid(topology=topo, size=(8, 8, 8), extent=(1, 2, 3))
+    dm = DistributedModel(grid=full_grid, ranks=(1, 4, 1))
+
+    my_rank = MPI.Comm_rank(MPI.COMM_WORLD)
+    @test my_rank == index2rank(dm.index..., dm.ranks...)
+
+    model = dm.model
+    connectivity = dm.connectivity
+
+    # No communication in x and z.
+    @test isnothing(connectivity.east)
+    @test isnothing(connectivity.west)
+    @test isnothing(connectivity.top)
+    @test isnothing(connectivity.bottom)
+
+    if my_rank == 0
+        @test connectivity.north == 1
+        @test connectivity.south == 3
+    elseif my_rank == 1
+        @test connectivity.north == 2
+        @test connectivity.south == 0
+    elseif my_rank == 2
+        @test connectivity.north == 3
+        @test connectivity.south == 1
+    elseif my_rank == 3
+        @test connectivity.north == 0
+        @test connectivity.south == 2
+    end
+
+    nx, ny, nz = size(model.grid)
+    @test model.grid.xF[1] == 0
+    @test model.grid.xF[nx+1] == 1
+    @test model.grid.yF[1] == 0.5*my_rank
+    @test model.grid.yF[ny+1] == 0.5*(my_rank+1)
+    @test model.grid.zF[1] == -3
+    @test model.grid.zF[nz+1] == 0
+end
+
+function test_triply_periodic_connectivity_with_114_ranks()
+    topo = (Periodic, Periodic, Periodic)
+    full_grid = RegularCartesianGrid(topology=topo, size=(8, 8, 8), extent=(1, 2, 3))
+    dm = DistributedModel(grid=full_grid, ranks=(1, 1, 4))
+
+    my_rank = MPI.Comm_rank(MPI.COMM_WORLD)
+    @test my_rank == index2rank(dm.index..., dm.ranks...)
+
+    model = dm.model
+    connectivity = dm.connectivity
+
+    # No communication in x and y.
+    @test isnothing(connectivity.east)
+    @test isnothing(connectivity.west)
+    @test isnothing(connectivity.north)
+    @test isnothing(connectivity.south)
+
+    if my_rank == 0
+        @test connectivity.top == 1
+        @test connectivity.bottom == 3
+    elseif my_rank == 1
+        @test connectivity.top == 2
+        @test connectivity.bottom == 0
+    elseif my_rank == 2
+        @test connectivity.top == 3
+        @test connectivity.bottom == 1
+    elseif my_rank == 3
+        @test connectivity.top == 0
+        @test connectivity.bottom == 2
+    end
+
+    nx, ny, nz = size(model.grid)
+    @test model.grid.xF[1] == 0
+    @test model.grid.xF[nx+1] == 1
+    @test model.grid.yF[1] == 0
+    @test model.grid.yF[ny+1] == 2
+    @test model.grid.zF[1] == -3 + 0.75*my_rank
+    @test model.grid.zF[nz+1] == -3 + 0.75*(my_rank+1)
+end
+
+@testset "Distributed MPI Oceananigans" begin
+    test_triply_periodic_connectivity_with_411_ranks()
+    test_triply_periodic_connectivity_with_141_ranks()
+    test_triply_periodic_connectivity_with_114_ranks()
+end
+
+# MPI.Finalize()
+# @test MPI.Finalized()
+
