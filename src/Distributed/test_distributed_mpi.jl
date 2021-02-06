d18f6700e882344d72d9b29b42a364049ce446bb
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2021-02-05 23:59:56 -0500

    Testing xy decompositions

--- src/Distributed/test_distributed_mpi.jl
+++ src/Distributed/test_distributed_mpi.jl
@@ -31,6 +31,10 @@ function run_triply_periodic_rank_connectivity_tests_with_411_ranks()
     @test isnothing(connectivity.top)
     @test isnothing(connectivity.bottom)
 
+    # +---+---+---+---+
+    # | 0 | 1 | 2 | 3 |
+    # +---+---+---+---+
+
     if my_rank == 0
         @test connectivity.east == 1
         @test connectivity.west == 3
@@ -63,6 +67,16 @@ function run_triply_periodic_rank_connectivity_tests_with_141_ranks()
     @test isnothing(connectivity.west)
     @test isnothing(connectivity.top)
     @test isnothing(connectivity.bottom)
+
+    # +---+
+    # | 0 |
+    # +---+
+    # | 1 |
+    # +---+
+    # | 2 |
+    # +---+
+    # | 3 |
+    # +---+
 
     if my_rank == 0
         @test connectivity.north == 1
@@ -97,6 +111,19 @@ function run_triply_periodic_rank_connectivity_tests_with_114_ranks()
     @test isnothing(connectivity.north)
     @test isnothing(connectivity.south)
 
+    #   /---/
+    #  / 3 /
+    # /---/
+    #   /---/
+    #  / 2 /
+    # /---/
+    #   /---/
+    #  / 1 /
+    # /---/
+    #   /---/
+    #  / 0 /
+    # /---/
+
     if my_rank == 0
         @test connectivity.top == 1
         @test connectivity.bottom == 3
@@ -114,6 +141,51 @@ function run_triply_periodic_rank_connectivity_tests_with_114_ranks()
     return nothing
 end
 
+function run_triply_periodic_rank_connectivity_tests_with_221_ranks()
+    topo = (Periodic, Periodic, Periodic)
+    full_grid = RegularCartesianGrid(topology=topo, size=(8, 8, 8), extent=(1, 2, 3))
+    arch = MultiCPU(grid=full_grid, ranks=(2, 2, 1))
+
+    my_rank = MPI.Comm_rank(MPI.COMM_WORLD)
+    @test my_rank == index2rank(arch.my_index..., arch.ranks...)
+
+    connectivity = arch.connectivity
+
+    # No communication in z.
+    @test isnothing(connectivity.top)
+    @test isnothing(connectivity.bottom)
+
+    # +---+---+
+    # | 0 | 2 |
+    # +---+---+
+    # | 1 | 3 |
+    # +---+---+
+
+    if my_rank == 0
+        @test connectivity.east == 2
+        @test connectivity.west == 2
+        @test connectivity.north == 1
+        @test connectivity.south == 1
+    elseif my_rank == 1
+        @test connectivity.east == 3
+        @test connectivity.west == 3
+        @test connectivity.north == 0
+        @test connectivity.south == 0
+    elseif my_rank == 2
+        @test connectivity.east == 0
+        @test connectivity.west == 0
+        @test connectivity.north == 3
+        @test connectivity.south == 3
+    elseif my_rank == 3
+        @test connectivity.east == 1
+        @test connectivity.west == 1
+        @test connectivity.north == 2
+        @test connectivity.south == 2
+    end
+
+    return nothing
+end
+
 #####
 ##### Local grids for distributed models
 #####
@@ -125,15 +197,15 @@ function run_triply_periodic_local_grid_tests_with_411_ranks()
     dm = DistributedModel(architecture=arch, grid=full_grid)
 
     my_rank = MPI.Comm_rank(MPI.COMM_WORLD)
+    local_grid = dm.model.grid
+    nx, ny, nz = size(local_grid)
 
-    model = dm.model
-    nx, ny, nz = size(model.grid)
-    @test model.grid.xF[1] == 0.25*my_rank
-    @test model.grid.xF[nx+1] == 0.25*(my_rank+1)
-    @test model.grid.yF[1] == 0
-    @test model.grid.yF[ny+1] == 2
-    @test model.grid.zF[1] == -3
-    @test model.grid.zF[nz+1] == 0
+    @test local_grid.xF[1] == 0.25*my_rank
+    @test local_grid.xF[nx+1] == 0.25*(my_rank+1)
+    @test local_grid.yF[1] == 0
+    @test local_grid.yF[ny+1] == 2
+    @test local_grid.zF[1] == -3
+    @test local_grid.zF[nz+1] == 0
 
     return nothing
 end
@@ -145,15 +217,15 @@ function run_triply_periodic_local_grid_tests_with_141_ranks()
     dm = DistributedModel(architecture=arch, grid=full_grid)
 
     my_rank = MPI.Comm_rank(MPI.COMM_WORLD)
+    local_grid = dm.model.grid
+    nx, ny, nz = size(local_grid)
 
-    model = dm.model
-    nx, ny, nz = size(model.grid)
-    @test model.grid.xF[1] == 0
-    @test model.grid.xF[nx+1] == 1
-    @test model.grid.yF[1] == 0.5*my_rank
-    @test model.grid.yF[ny+1] == 0.5*(my_rank+1)
-    @test model.grid.zF[1] == -3
-    @test model.grid.zF[nz+1] == 0
+    @test local_grid.xF[1] == 0
+    @test local_grid.xF[nx+1] == 1
+    @test local_grid.yF[1] == 0.5*my_rank
+    @test local_grid.yF[ny+1] == 0.5*(my_rank+1)
+    @test local_grid.zF[1] == -3
+    @test local_grid.zF[nz+1] == 0
 
     return nothing
 end
@@ -165,21 +237,41 @@ function run_triply_periodic_local_grid_tests_with_114_ranks()
     dm = DistributedModel(architecture=arch, grid=full_grid)
 
     my_rank = MPI.Comm_rank(MPI.COMM_WORLD)
+    local_grid = dm.model.grid
+    nx, ny, nz = size(local_grid)
 
-    model = dm.model
-    nx, ny, nz = size(model.grid)
-    @test model.grid.xF[1] == 0
-    @test model.grid.xF[nx+1] == 1
-    @test model.grid.yF[1] == 0
-    @test model.grid.yF[ny+1] == 2
-    @test model.grid.zF[1] == -3 + 0.75*my_rank
-    @test model.grid.zF[nz+1] == -3 + 0.75*(my_rank+1)
+    @test local_grid.xF[1] == 0
+    @test local_grid.xF[nx+1] == 1
+    @test local_grid.yF[1] == 0
+    @test local_grid.yF[ny+1] == 2
+    @test local_grid.zF[1] == -3 + 0.75*my_rank
+    @test local_grid.zF[nz+1] == -3 + 0.75*(my_rank+1)
+
+    return nothing
+end
+
+function run_triply_periodic_local_grid_tests_with_221_ranks()
+    topo = (Periodic, Periodic, Periodic)
+    full_grid = RegularCartesianGrid(topology=topo, size=(8, 8, 8), extent=(1, 2, 3))
+    arch = MultiCPU(grid=full_grid, ranks=(2, 2, 1))
+    dm = DistributedModel(architecture=arch, grid=full_grid)
+
+    i, j, k = arch.my_index
+    local_grid = dm.model.grid
+    nx, ny, nz = size(local_grid)
+
+    @test local_grid.xF[1] == 0.5*(i-1)
+    @test local_grid.xF[nx+1] == 0.5*i
+    @test local_grid.yF[1] == j-1
+    @test local_grid.yF[ny+1] == j
+    @test local_grid.zF[1] == -3
+    @test local_grid.zF[nz+1] == 0
 
     return nothing
 end
 
 #####
-#####
+##### Injection of halo communication BCs
 #####
 
 function run_triply_periodic_bc_injection_tests_with_411_ranks()
@@ -189,12 +281,13 @@ function run_triply_periodic_bc_injection_tests_with_411_ranks()
     dm = DistributedModel(architecture=arch, grid=full_grid)
 
     for field in fields(dm.model)
-        @test field.boundary_conditions.east isa HaloCommunicationBC
-        @test field.boundary_conditions.west isa HaloCommunicationBC
-        @test !isa(field.boundary_conditions.north, HaloCommunicationBC)
-        @test !isa(field.boundary_conditions.south, HaloCommunicationBC)
-        @test !isa(field.boundary_conditions.top, HaloCommunicationBC)
-        @test !isa(field.boundary_conditions.bottom, HaloCommunicationBC)
+        fbcs = field.boundary_conditions
+        @test fbcs.east isa HaloCommunicationBC
+        @test fbcs.west isa HaloCommunicationBC
+        @test !isa(fbcs.north, HaloCommunicationBC)
+        @test !isa(fbcs.south, HaloCommunicationBC)
+        @test !isa(fbcs.top, HaloCommunicationBC)
+        @test !isa(fbcs.bottom, HaloCommunicationBC)
     end
 end
 
@@ -205,12 +298,13 @@ function run_triply_periodic_bc_injection_tests_with_141_ranks()
     dm = DistributedModel(architecture=arch, grid=full_grid)
 
     for field in fields(dm.model)
-        @test !isa(field.boundary_conditions.east, HaloCommunicationBC)
-        @test !isa(field.boundary_conditions.west, HaloCommunicationBC)
-        @test field.boundary_conditions.north isa HaloCommunicationBC
-        @test field.boundary_conditions.south isa HaloCommunicationBC
-        @test !isa(field.boundary_conditions.top, HaloCommunicationBC)
-        @test !isa(field.boundary_conditions.bottom, HaloCommunicationBC)
+        fbcs = field.boundary_conditions
+        @test !isa(fbcs.east, HaloCommunicationBC)
+        @test !isa(fbcs.west, HaloCommunicationBC)
+        @test fbcs.north isa HaloCommunicationBC
+        @test fbcs.south isa HaloCommunicationBC
+        @test !isa(fbcs.top, HaloCommunicationBC)
+        @test !isa(fbcs.bottom, HaloCommunicationBC)
     end
 end
 
@@ -221,12 +315,30 @@ function run_triply_periodic_bc_injection_tests_with_114_ranks()
     dm = DistributedModel(architecture=arch, grid=full_grid)
 
     for field in fields(dm.model)
-        @test !isa(field.boundary_conditions.east, HaloCommunicationBC)
-        @test !isa(field.boundary_conditions.west, HaloCommunicationBC)
-        @test !isa(field.boundary_conditions.north, HaloCommunicationBC)
-        @test !isa(field.boundary_conditions.south, HaloCommunicationBC)
-        @test field.boundary_conditions.top isa HaloCommunicationBC
-        @test field.boundary_conditions.bottom isa HaloCommunicationBC
+        fbcs = field.boundary_conditions
+        @test !isa(fbcs.east, HaloCommunicationBC)
+        @test !isa(fbcs.west, HaloCommunicationBC)
+        @test !isa(fbcs.north, HaloCommunicationBC)
+        @test !isa(fbcs.south, HaloCommunicationBC)
+        @test fbcs.top isa HaloCommunicationBC
+        @test fbcs.bottom isa HaloCommunicationBC
+    end
+end
+
+function run_triply_periodic_bc_injection_tests_with_221_ranks()
+    topo = (Periodic, Periodic, Periodic)
+    full_grid = RegularCartesianGrid(topology=topo, size=(8, 8, 8), extent=(1, 2, 3))
+    arch = MultiCPU(grid=full_grid, ranks=(2, 2, 1))
+    dm = DistributedModel(architecture=arch, grid=full_grid)
+
+    for field in fields(dm.model)
+        fbcs = field.boundary_conditions
+        @test fbcs.east isa HaloCommunicationBC
+        @test fbcs.west isa HaloCommunicationBC
+        @test fbcs.north isa HaloCommunicationBC
+        @test fbcs.south isa HaloCommunicationBC
+        @test !isa(fbcs.top, HaloCommunicationBC)
+        @test !isa(fbcs.bottom, HaloCommunicationBC)
     end
 end
 
@@ -292,26 +404,29 @@ end
 @testset "Distributed MPI Oceananigans" begin
     @info "Testing distributed MPI Oceananigans..."
 
-    @testset "Multi architectures rank connectivity" begin
-        @info "  Testing multi architecture rank connectivity..."
-        run_triply_periodic_rank_connectivity_tests_with_411_ranks()
-        run_triply_periodic_rank_connectivity_tests_with_141_ranks()
-        run_triply_periodic_rank_connectivity_tests_with_114_ranks()
-    end
+    # @testset "Multi architectures rank connectivity" begin
+    #     @info "  Testing multi architecture rank connectivity..."
+    #     run_triply_periodic_rank_connectivity_tests_with_411_ranks()
+    #     run_triply_periodic_rank_connectivity_tests_with_141_ranks()
+    #     run_triply_periodic_rank_connectivity_tests_with_114_ranks()
+    #     run_triply_periodic_rank_connectivity_tests_with_221_ranks()
+    # end
 
-    @testset "Local grids for distributed models" begin
-        @info "  Testing local grids for distributed models..."
-        run_triply_periodic_local_grid_tests_with_411_ranks()
-        run_triply_periodic_local_grid_tests_with_141_ranks()
-        run_triply_periodic_local_grid_tests_with_114_ranks()
-    end
+    # @testset "Local grids for distributed models" begin
+    #     @info "  Testing local grids for distributed models..."
+    #     run_triply_periodic_local_grid_tests_with_411_ranks()
+    #     run_triply_periodic_local_grid_tests_with_141_ranks()
+    #     run_triply_periodic_local_grid_tests_with_114_ranks()
+    #     run_triply_periodic_local_grid_tests_with_221_ranks()
+    # end
 
-    @testset "Injection of halo communication BCs" begin
-        @info "  Testing injection of halo communication BCs..."
-        run_triply_periodic_bc_injection_tests_with_411_ranks()
-        run_triply_periodic_bc_injection_tests_with_141_ranks()
-        run_triply_periodic_bc_injection_tests_with_114_ranks()
-    end
+    # @testset "Injection of halo communication BCs" begin
+    #     @info "  Testing injection of halo communication BCs..."
+    #     run_triply_periodic_bc_injection_tests_with_411_ranks()
+    #     run_triply_periodic_bc_injection_tests_with_141_ranks()
+    #     run_triply_periodic_bc_injection_tests_with_114_ranks()
+    #     run_triply_periodic_bc_injection_tests_with_221_ranks()
+    # end
 
     # TODO: Larger halos!
     @testset "Halo communication" begin
