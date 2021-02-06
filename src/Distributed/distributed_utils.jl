fff4de7bcad283306b5e42baf4aa94d4e0b60d43
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2021-02-06 00:00:26 -0500

    Add `include_corners` kwarg for halo functions

--- src/Distributed/distributed_utils.jl
+++ src/Distributed/distributed_utils.jl
@@ -1,17 +1,46 @@
-using Oceananigans.Grids: left_halo_indices, right_halo_indices, underlying_left_halo_indices, underlying_right_halo_indices
 using Oceananigans.Fields: AbstractField
+using Oceananigans.Grids:
+    interior_indices,
+    left_halo_indices, right_halo_indices,
+    underlying_left_halo_indices, underlying_right_halo_indices
 
-west_halo(f::AbstractField{LX, LY, LZ}) where {LX, LY, LZ} =
-    view(f.data, left_halo_indices(LX, topology(f, 1), f.grid.Nx, f.grid.Hx), :, :)
+# TODO: Move to Grids/grid_utils.jl
 
-east_halo(f::AbstractField{LX, LY, LZ}) where {LX, LY, LZ} =
-    view(f.data, right_halo_indices(LX, topology(f, 1), f.grid.Nx, f.grid.Hx), :, :)
+west_halo(f::AbstractField{LX, LY, LZ}; include_corners=true) where {LX, LY, LZ} =
+    include_corners ? view(f.data, left_halo_indices(LX, topology(f, 1), f.grid.Nx, f.grid.Hx), :, :) :
+                      view(f.data, left_halo_indices(LX, topology(f, 1), f.grid.Nx, f.grid.Hx),
+                                   interior_indices(LY, topology(f, 2), f.grid.Ny),
+                                   interior_indices(LZ, topology(f, 3), f.grid.Nz))
 
-south_halo(f::AbstractField{LX, LY, LZ}) where {LX, LY, LZ} =
-    view(f.data, :, left_halo_indices(LY, topology(f, 2), f.grid.Ny, f.grid.Hy), :)
+east_halo(f::AbstractField{LX, LY, LZ}; include_corners=true) where {LX, LY, LZ} =
+    include_corners ? view(f.data, right_halo_indices(LX, topology(f, 1), f.grid.Nx, f.grid.Hx), :, :) :
+                      view(f.data, right_halo_indices(LX, topology(f, 1), f.grid.Nx, f.grid.Hx),
+                                   interior_indices(LY, topology(f, 2), f.grid.Ny),
+                                   interior_indices(LZ, topology(f, 3), f.grid.Nz))
 
-north_halo(f::AbstractField{LX, LY, LZ}) where {LX, LY, LZ} =
-    view(f.data, :, right_halo_indices(LY, topology(f, 2), f.grid.Ny, f.grid.Hy), :)
+south_halo(f::AbstractField{LX, LY, LZ}; include_corners=true) where {LX, LY, LZ} =
+    include_corners ? view(f.data, :, left_halo_indices(LY, topology(f, 2), f.grid.Ny, f.grid.Hy), :) :
+                      view(f.data, interior_indices(LX, topology(f, 1), f.grid.Nx),
+                                   left_halo_indices(LY, topology(f, 2), f.grid.Ny, f.grid.Hy),
+                                   interior_indices(LZ, topology(f, 3), f.grid.Nz))
+
+north_halo(f::AbstractField{LX, LY, LZ}; include_corners=true) where {LX, LY, LZ} =
+    include_corners ? view(f.data, :, right_halo_indices(LY, topology(f, 2), f.grid.Ny, f.grid.Hy), :) :
+                      view(f.data, interior_indices(LX, topology(f, 1), f.grid.Nx),
+                                   right_halo_indices(LY, topology(f, 2), f.grid.Ny, f.grid.Hy),
+                                   interior_indices(LZ, topology(f, 3), f.grid.Nz))
+
+bottom_halo(f::AbstractField{LX, LY, LZ}; include_corners=true) where {LX, LY, LZ} =
+    include_corners ? view(f.data, :, :, left_halo_indices(LZ, topology(f, 3), f.grid.Nz, f.grid.Hz)) :
+                      view(f.data, interior_indices(LX, topology(f, 1), f.grid.Nx),
+                                   interior_indices(LY, topology(f, 2), f.grid.Ny),
+                                   left_halo_indices(LZ, topology(f, 3), f.grid.Nz, f.grid.Hz))
+
+top_halo(f::AbstractField{LX, LY, LZ}; include_corners=true) where {LX, LY, LZ} =
+    include_corners ? view(f.data, :, :, right_halo_indices(LZ, topology(f, 3), f.grid.Nz, f.grid.Hz)) :
+                      view(f.data, interior_indices(LX, topology(f, 1), f.grid.Nx),
+                                   interior_indices(LY, topology(f, 2), f.grid.Ny),
+                                   left_halo_indices(LZ, topology(f, 3), f.grid.Nz, f.grid.Hz))
 
 bottom_halo(f::AbstractField{LX, LY, LZ}) where {LX, LY, LZ} =
     view(f.data, :, :, left_halo_indices(LZ, topology(f, 3), f.grid.Nz, f.grid.Hz), :)
