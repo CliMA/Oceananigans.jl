270e8af03c955a3e85fbdc8bf9f67e002be68ed9
Gregory Wagner <wagner.greg@gmail.com>
2021-02-18 16:00:03 -0500

    Fixes formatting issues in kernel_computed_field.jl

--- src/Fields/kernel_computed_field.jl
+++ src/Fields/kernel_computed_field.jl
@@ -11,7 +11,6 @@ struct KernelComputedField{X, Y, Z, S, A, G, K, C, F, P} <: AbstractField{X, Y, 
              parameters :: P
                  status :: S
 
-
     function KernelComputedField{X, Y, Z}(kernel::K, arch, grid;
                                           boundary_conditions = ComputedFieldBoundaryConditions(grid, (X, Y, Z)),
                                           field_dependencies = (),
@@ -39,12 +38,13 @@ struct KernelComputedField{X, Y, Z, S, A, G, K, C, F, P} <: AbstractField{X, Y, 
     end
 end
 
-    """
-        KernelComputedField(X, Y, Z, kernel, model; 
-                            boundary_conditions=ComputedFieldBoundaryConditions(grid, (X, Y, Z)), 
-                            field_dependencies=(), 
-                            parameters=nothing, 
-                            data=nothing, recompute_safely=true)
+"""
+    KernelComputedField(X, Y, Z, kernel, model; 
+                        boundary_conditions = ComputedFieldBoundaryConditions(grid, (X, Y, Z)), 
+                        field_dependencies = (), 
+                        parameters = nothing, 
+                        data = nothing,
+                        recompute_safely = true)
 
 Builds a `KernelComputedField` at `X, Y, Z` computed with `kernel` and `model.architecture` and `model.grid`, with `boundary_conditions`.
 
@@ -64,39 +64,39 @@ Otherwise, `kernel` is launched with the function signature
 
 `recompute_safely` (default: `true`) determines whether the `KernelComputedField` is "recomputed" if embedded in the expression 
 tree of another operation. 
-If `recompute_safely=true`, the `KernelComputedField` is always recomputed. 
-If `recompute_safely=false`, the `KernelComputedField` will not be recomputed if its status is up-to-date. 
-If `data=nothing`, then `recompute_safely` is switched to `false`.
+    - If `recompute_safely=true`, the `KernelComputedField` is always recomputed. 
+    - If `recompute_safely=false`, the `KernelComputedField` will not be recomputed if its status is up-to-date. 
+    - If `data=nothing`, then `recompute_safely` is switched to `false`.
 
-    Example
-    =======
+Example
+=======
 
-    ```julia
-    using KernelAbstractions: @index, @kernel
-    using Oceananigans.Fields: AveragedField, KernelComputedField, compute!
-    using Oceananigans.Grids: Center, Face
-    
-    @inline ψ²(i, j, k, grid, ψ, Ψ) = @inbounds (ψ[i, j, k] - Ψ[i, j, k])^2
-    @kernel function compute_variance!(var, grid, ϕ, Φ)
-        i, j, k = @index(Global, NTuple)
-    
-        @inbounds var[i, j, k] = ψ′²(i, j, k, grid, ϕ, Φ)
-    end
-    
-    u, v, w = model.velocities
-    
-    U = AveragedField(u, dims=(1, 2))
-    V = AveragedField(v, dims=(1, 2))
-    
-    u′² = KernelComputedField(Face, Center, Center, compute_variance!, model; field_dependencies=(u, U,))
-    v′² = KernelComputedField(Center, Face, Center, compute_variance!, model; field_dependencies=(v, V,))
-    w′² = KernelComputedField(Center, Center, Face, compute_variance!, model; field_dependencies=(w, 0,))
-    
-    compute!(u′²)
-    compute!(v′²)
-    compute!(w′²)
-    ```
-    """
+```julia
+using KernelAbstractions: @index, @kernel
+using Oceananigans.Fields: AveragedField, KernelComputedField, compute!
+using Oceananigans.Grids: Center, Face
+
+@inline ψ²(i, j, k, grid, ψ, Ψ) = @inbounds (ψ[i, j, k] - Ψ[i, j, k])^2
+@kernel function compute_variance!(var, grid, ϕ, Φ)
+    i, j, k = @index(Global, NTuple)
+
+    @inbounds var[i, j, k] = ψ′²(i, j, k, grid, ϕ, Φ)
+end
+
+u, v, w = model.velocities
+
+U = AveragedField(u, dims=(1, 2))
+V = AveragedField(v, dims=(1, 2))
+
+u′² = KernelComputedField(Face, Center, Center, compute_variance!, model; field_dependencies=(u, U,))
+v′² = KernelComputedField(Center, Face, Center, compute_variance!, model; field_dependencies=(v, V,))
+w′² = KernelComputedField(Center, Center, Face, compute_variance!, model; field_dependencies=(w, 0,))
+
+compute!(u′²)
+compute!(v′²)
+compute!(w′²)
+```
+"""
 KernelComputedField(X, Y, Z, kernel, model::AbstractModel; kwargs...) =
     KernelComputedField{X, Y, Z}(kernel, model.architecture, model.grid; kwargs...)
 
