b2aad80e8b214c4bc0117c5e227939d0c0b951c9
ali.hh.ramadhan@gmail.com <ali.hh.ramadhan@gmail.com>
2021-02-25 16:10:26 -0500

    Missed a search and replace

--- src/Simulations/run.jl
+++ src/Simulations/run.jl
@@ -3,7 +3,7 @@ using Glob
 using Oceananigans.Utils: initialize_schedule!, align_time_step
 using Oceananigans.Fields: set!
 using Oceananigans.OutputWriters: WindowedTimeAverage, checkpoint_superprefix
-using Oceananigans.TimeSteppers: QuasiAdamsBashforth2TimeStepper, RungeKutta3TimeStepper, update_state!, tick_time, unit_time
+using Oceananigans.TimeSteppers: QuasiAdamsBashforth2TimeStepper, RungeKutta3TimeStepper, update_state!, next_time, unit_time
 
 using Oceananigans: AbstractModel
 
