ed3c6d6f5abf012dd2447c0c8b56f07603227650
Gregory Wagner <wagner.greg@gmail.com>
2021-02-26 13:06:53 -0500

    Cleanliness improvements in solid body rotation

--- validation/solid_body_rotation/solid_body_rotation.jl
+++ validation/solid_body_rotation/solid_body_rotation.jl
@@ -6,7 +6,25 @@
 # > Williamson et al., "A Standard Test Set for Numerical Approximations to the Shallow
 #   Water Equations in Spherical Geometry", Journal of Computational Physics, 1992.
 #
-# The geostrophic flow
+# The problem is posed in spherical strip between 60ᵒS and 60ᵒN latitude on a sphere with
+# unit radius.
+#
+# # Dependencies
+#
+# The validation experiment depends on Oceananigans, JLD2, Printf, and GLMakie for visualization
+
+using Oceananigans
+using Oceananigans.Grids
+using Oceananigans.Coriolis: HydrostaticSphericalCoriolis, VectorInvariantEnergyConserving, VectorInvariantEnstrophyConserving
+using Oceananigans.Models.HydrostaticFreeSurfaceModels: HydrostaticFreeSurfaceModel, VectorInvariant, ExplicitFreeSurface
+using Oceananigans.Utils: prettytime, hours
+using Oceananigans.OutputWriters: JLD2OutputWriter, TimeInterval, IterationInterval
+
+using JLD2
+using Printf
+using GLMakie
+
+# # The geostrophic flow
 #
 # ```math
 # u = U \cos ϕ
@@ -18,130 +36,193 @@
 # acceleration ``g``, corresponding to solid body rotation
 # in the same direction as the "background" rotation rate ``\Omega``.
 # The velocity ``U`` determines the magnitude of the additional rotation.
-#
-# ## A spherical domain
-#
-# We two-dimensional latitude-longitude grid on a sphere of unit radius,
 
-using Oceananigans
-using Oceananigans.Grids: RegularLatitudeLongitudeGrid
-using Oceananigans.Coriolis: HydrostaticSphericalCoriolis, VectorInvariantEnergyConserving, VectorInvariantEnstrophyConserving
-using Oceananigans.Models.HydrostaticFreeSurfaceModels: HydrostaticFreeSurfaceModel, VectorInvariant, ExplicitFreeSurface
-using Oceananigans.Utils: prettytime
-using Oceananigans.OutputWriters: JLD2OutputWriter, TimeInterval, IterationInterval
+const U = 0.1
 
-grid = RegularLatitudeLongitudeGrid(size = (90, 15, 1),
-                                    radius = 1,
-                                    latitude = (-60, 60),
-                                    longitude = (-180, 180),
-                                    z = (-1, 0))
+solid_body_rotation(λ, ϕ) = U * cosd(ϕ)
+solid_body_geostrophic_height(λ, ϕ, R, Ω, g) = (R * Ω * U + U^2 / 2) * sind(ϕ)^2 / g
 
-coriolis = HydrostaticSphericalCoriolis(rotation_rate = 1,
-                                        stencil = VectorInvariantEnstrophyConserving())
+# In addition to the solid body rotation solution, we paint a Gaussian tracer patch
+# on the spherical strip to visualize the rotation.
 
-model = HydrostaticFreeSurfaceModel(grid = grid,
-                                    momentum_advection = VectorInvariant(),
-                                    free_surface = ExplicitFreeSurface(gravitational_acceleration=1),
-                                    coriolis = coriolis,
-                                    tracers = :c,
-                                    buoyancy = nothing,
-                                    closure = nothing)
+function run_solid_body_rotation(; architecture = CPU(),
+                                   resolution = 4, # degrees
+                                   coriolis_scheme = VectorInvariantEnstrophyConserving(),
+                                   super_rotations = 4
+                                )
 
-U = 0.1
-g = model.free_surface.gravitational_acceleration
-R = model.grid.radius
-Ω = model.coriolis.rotation_rate
+    Nx = round(Int, 360 / resolution)
+    Ny = round(Int, 120 / resolution)
 
-uᵢ(λ, ϕ, z) = U * cosd(ϕ)
-ηᵢ(λ, ϕ, z) = (R * Ω * U + U^2 / 2) * sind(ϕ)^2 / g
+    # A spherical domain
+    grid = RegularLatitudeLongitudeGrid(size = (Nx, Ny, 1),
+                                        radius = 1,
+                                        latitude = (-60, 60),
+                                        longitude = (-180, 180),
+                                        z = (-1, 0))
 
-# Tracer patch for visualization
-Gaussian(λ, ϕ, L) = exp(-(λ^2 + ϕ^2) / 2L^2)
+    free_surface = ExplicitFreeSurface(gravitational_acceleration=1)
 
-# Tracer patch parameters
-L = 10 # degree
-ϕ₀ = 5 # degrees
+    coriolis = HydrostaticSphericalCoriolis(rotation_rate = 1,
+                                            scheme = VectorInvariantEnstrophyConserving())
 
-cᵢ(λ, ϕ, z) = Gaussian(λ, ϕ - ϕ₀, L)
+    model = HydrostaticFreeSurfaceModel(grid = grid,
+                                        architecture = architecture,
+                                        momentum_advection = VectorInvariant(),
+                                        free_surface = free_surface,
+                                        coriolis = coriolis,
+                                        tracers = :c,
+                                        buoyancy = nothing,
+                                        closure = nothing)
 
-set!(model, u=uᵢ, η=ηᵢ, c=cᵢ)
+    g = model.free_surface.gravitational_acceleration
+    R = model.grid.radius
+    Ω = model.coriolis.rotation_rate
 
-gravity_wave_speed = sqrt(g * grid.Lz) # hydrostatic (shallow water) gravity wave speed
+    uᵢ(λ, ϕ, z) = solid_body_rotation(λ, ϕ, U)
+    ηᵢ(λ, ϕ, z) = solid_body_geostrophic_height(λ, ϕ, U, R, Ω, g)
 
-# Time-scale for gravity wave propagation across the smallest grid cell
-wave_propagation_time_scale = min(grid.radius * cosd(maximum(abs, grid.ϕᵃᶜᵃ)) * deg2rad(grid.Δλ),
-                                  grid.radius * deg2rad(grid.Δϕ)) / gravity_wave_speed
+    # Tracer patch for visualization
+    Gaussian(λ, ϕ, L) = exp(-(λ^2 + ϕ^2) / 2L^2)
 
-# Calculate stop time based on the time scale for circumglobal tracer advection
+    # Tracer patch parameters
+    L = 10 # degree
+    ϕ₀ = 5 # degrees
 
-circumfrence = 2π * grid.radius
-circumvection_timescale = circumfrence / U
+    cᵢ(λ, ϕ, z) = Gaussian(λ, ϕ - ϕ₀, L)
 
-simulation = Simulation(model,
-                        Δt = 0.1wave_propagation_time_scale,
-                        stop_time = 32 * circumvection_timescale,
-                        iteration_interval = 100,
-                        progress = s -> @info "Time = $(s.model.clock.time) / $(s.stop_time)")
-                                                         
-output_fields = merge(model.velocities, model.tracers, (η=model.free_surface.η,))
+    set!(model, u=uᵢ, η=ηᵢ, c=cᵢ)
 
-output_prefix = "solid_body_rotation_Nx$(grid.Nx)"
+    gravity_wave_speed = sqrt(g * grid.Lz) # hydrostatic (shallow water) gravity wave speed
 
-simulation.output_writers[:fields] = JLD2OutputWriter(model, output_fields,
-                                                      schedule = TimeInterval(circumvection_timescale / 20),
-                                                      prefix = output_prefix,
-                                                      force = true)
+    # Time-scale for gravity wave propagation across the smallest grid cell
+    wave_propagation_time_scale = min(grid.radius * cosd(maximum(abs, grid.ϕᵃᶜᵃ)) * deg2rad(grid.Δλ),
+                                      grid.radius * deg2rad(grid.Δϕ)) / gravity_wave_speed
 
-run!(simulation)
+    super_rotation_period = 2π * grid.radius / U
 
-# ## Visualizing the results
+    simulation = Simulation(model,
+                            Δt = 0.1wave_propagation_time_scale,
+                            stop_time = super_rotations * super_rotation_period,
+                            iteration_interval = 100,
+                            progress = s -> @info "Time = $(s.model.clock.time) / $(s.stop_time)")
+                                                             
+    output_fields = merge(model.velocities, model.tracers, (η=model.free_surface.η,))
 
-using JLD2, Printf, Oceananigans.Grids, GLMakie
-using Oceananigans.Utils: hours
+    output_prefix = "solid_body_rotation_Nx$(grid.Nx)"
 
-λ, ϕ, r = nodes(model.free_surface.η, reshape=true)
+    simulation.output_writers[:fields] = JLD2OutputWriter(model, output_fields,
+                                                          schedule = TimeInterval(super_rotation_period/ 20),
+                                                          prefix = output_prefix,
+                                                          force = true)
 
-λ = λ .+ 180  # Convert to λ ∈ [0°, 360°]
-ϕ = 90 .- ϕ   # Convert to ϕ ∈ [0°, 180°] (0° at north pole)
+    run!(simulation)
 
-filepath = output_prefix * ".jld2"
-file = jldopen(filepath)
+    return simulation.output_writers[:fields].filepath
+end
 
-iterations = parse.(Int, keys(file["timeseries/t"]))
+function visualize_solid_body_rotation(filepath)
 
-iter = Node(0)
-plot_title = @lift @sprintf("Tracer advection by solid body rotation (u, v, η, c): time = %s",
-                            prettytime(file["timeseries/t/" * string($iter)]))
+    @show output_prefix = basename(filepath)[1:end-5]
 
-u = @lift file["timeseries/u/" * string($iter)][:, :, 1]
-v = @lift file["timeseries/v/" * string($iter)][:, :, 1]
-η = @lift file["timeseries/η/" * string($iter)][:, :, 1]
-c = @lift file["timeseries/c/" * string($iter)][:, :, 1]
+    file = jldopen(filepath)
 
-# Plot on the unit sphere to align with the spherical wireframe.
-x3 = @. cosd(λ) * sind(ϕ)
-y3 = @. sind(λ) * sind(ϕ)
-z3 = @. cosd(ϕ) * λ ./ λ
+    iterations = parse.(Int, keys(file["timeseries/t"]))
 
-x = x3[:, :, 1]
-y = y3[:, :, 1]
-z = z3[:, :, 1]
+    Nx = file["grid/Nx"]
+    Ny = file["grid/Ny"]
 
-fig = Figure(resolution = (2400, 1080))
+    grid = RegularLatitudeLongitudeGrid(size = (Nx, Ny, 1),
+                                        radius = 1,
+                                        latitude = (-60, 60),
+                                        longitude = (-180, 180),
+                                        z = (-1, 0))
 
-clims = [(-U, U), (-U, U), (-U, U), (0, 1)]
+    super_rotation_period = 2π * grid.radius / U
 
-for (n, var) in enumerate((u, v, η, c))
-    ax = fig[1, n] = LScene(fig, title="hi")
-    wireframe!(ax, Sphere(Point3f0(0), 0.98f0), show_axis=false) #, color=RGBA(0.0, 0.0, 0.2, 0.4))
-    surface!(ax, x, y, z, color=var, colormap=:balance, colorrange=clims[n])
+    λ = xnodes(Face, grid)
+    ϕ = ynodes(Center, grid)
+    
+    λ = repeat(reshape(λ, Nx, 1), 1, Ny)
+    ϕ = repeat(reshape(ϕ, 1, Ny), Nx, 1)
+
+    λ_azimuthal = λ .+ 180  # Convert to λ ∈ [0°, 360°]
+    ϕ_azimuthal = 90 .- ϕ   # Convert to ϕ ∈ [0°, 180°] (0° at north pole)
+
+    iter = Node(0)
+
+    plot_title = @lift @sprintf("Zonal velocity error in solid body rotation: rotations = %.3f",
+                                file["timeseries/t/" * string($iter)] / super_rotation_period)
+
+    spatial_error = @lift abs.(file["timeseries/u/" * string($iter)][:, :, 1] .- solid_body_rotation.(λ, ϕ)) / U
+    maximum_error = @lift maximum(abs, (file["timeseries/u/" * string($iter)][:, :, 1] .- solid_body_rotation.(λ, ϕ, U)) / U)
+
+    # Plot on the unit sphere to align with the spherical wireframe.
+    x = @. cosd(λ_azimuthal) * sind(ϕ_azimuthal)
+    y = @. sind(λ_azimuthal) * sind(ϕ_azimuthal)
+    z = @. cosd(ϕ_azimuthal)
+
+    fig = Figure(resolution = (1080, 1080))
+
+    ax = fig[1, 1] = LScene(fig, title="")
+    wireframe!(ax, Sphere(Point3f0(0), 0.99f0), show_axis=false)
+    surface!(ax, x, y, z, color=spatial_error, colormap=:thermal, colorrange=(0.0, 0.02))
     rotate_cam!(ax.scene, (-π/4, π/8, 0))
     zoom!(ax.scene, (0, 0, 0), 2, false)
+
+    supertitle = fig[0, :] = Label(fig, plot_title, textsize=30)
+
+    record(fig, output_prefix * ".mp4", iterations, framerate=30) do i
+        @info "Animating iteration $i/$(iterations[end])..."
+        iter[] = i
+    end
+
+    return nothing
 end
 
-supertitle = fig[0, :] = Label(fig, plot_title, textsize=30)
+function analyze_solid_body_rotation(filepath)
 
-record(fig, output_prefix * ".mp4", iterations, framerate=30) do i
-    @info "Animating iteration $i/$(iterations[end])..."
-    iter[] = i
+    @show output_prefix = basename(filepath)[1:end-5]
+
+    file = jldopen(filepath)
+
+    iterations = parse.(Int, keys(file["timeseries/t"]))
+
+    Nx = file["grid/Nx"]
+    Ny = file["grid/Ny"]
+
+    grid = RegularLatitudeLongitudeGrid(size = (Nx, Ny, 1),
+                                        radius = 1,
+                                        latitude = (-60, 60),
+                                        longitude = (-180, 180),
+                                        z = (-1, 0))
+
+    super_rotation_period = 2π * grid.radius / U
+
+    λ = xnodes(Face, grid)
+    ϕ = ynodes(Center, grid)
+    
+    λ = repeat(reshape(λ, Nx, 1), 1, Ny)
+    ϕ = repeat(reshape(ϕ, 1, Ny), Nx, 1)
+
+    maximum_error = [maximum(abs, (file["timeseries/u/$i"][:, :, 1] .- solid_body_rotation.(λ, ϕ, U)) / U)
+                     for i in iterations]
+
+    return iterations, maximum_error
 end
+
+for resolution in (1, 2, 4, 8) # degrees
+    filepath = run_solid_body_rotation(resolution=resolution)
+    visualize_solid_body_rotation(filepath)
+end
+
+#=
+fig = Figure(resolution = (1080, 1080))
+ax = fig[1, 1] = LScene(fig, title="")
+
+for Nx in (45, 90) #, 180, 360)
+    filepath = "solid_body_rotation_Nx$Nx.jld2"
+    iterations, maximum_error = analyze_solid_body_rotation(filepath)
+    lines!(ax, iterations, maximum_error)
+end
+=#
