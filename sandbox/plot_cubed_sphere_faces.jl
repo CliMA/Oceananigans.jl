2111ef9a2f9d60acfe8d129b803e2d83aacaccbf
ali.hh.ramadhan@gmail.com <ali.hh.ramadhan@gmail.com>
2021-02-27 11:41:03 -0500

    Plot points on the cubed sphere faces

---
+++ sandbox/plot_cubed_sphere_faces.jl
@@ -0,0 +1,27 @@
+using PyCall
+
+plt = pyimport("matplotlib.pyplot")
+ccrs = pyimport("cartopy.crs")
+
+include("conformal_cubed_sphere_grid.jl")
+
+Nx_face = Ny_face = 20
+grid = ConformalCubedSphereGrid(face_size=(Nx_face, Ny_face, 1), z=(-1, 0))
+
+## Plot points of each face
+
+fig = plt.figure(figsize=(16, 9))
+ax = fig.add_subplot(1, 1, 1, projection=ccrs.Mollweide())
+
+for (n, face) in enumerate(grid.faces)
+    lons = face.λᶜᶜᶜ[1:Nx_face, 1:Ny_face]
+    lats = face.ϕᶜᶜᶜ[1:Nx_face, 1:Ny_face]
+    ax.scatter(lons, lats, s=4, label="face $n", transform=ccrs.PlateCarree())
+end
+
+ax.legend(loc="lower right")
+ax.coastlines(resolution="50m")
+ax.set_global()
+
+# plt.show()
+plt.savefig("cubed_sphere_points.png", dpi=200)
