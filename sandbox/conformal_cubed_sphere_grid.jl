b1874a7342d821b65a273ecd4aaa83b89158360f
ali.hh.ramadhan@gmail.com <ali.hh.ramadhan@gmail.com>
2021-02-28 11:29:34 -0500

    Reorder faces

--- sandbox/conformal_cubed_sphere_grid.jl
+++ sandbox/conformal_cubed_sphere_grid.jl
@@ -11,39 +11,39 @@ function ConformalCubedSphereGrid(FT=Float64; face_size, z, radius=R_Earth)
     # +z face (face 1)
     z⁺_face_grid = ConformalCubedSphereFaceGrid(FT, size=face_size, z=z, radius=radius, rotation=nothing)
 
-    # -z face (face 2)
-    z⁻_face_grid = ConformalCubedSphereFaceGrid(FT, size=face_size, z=z, radius=radius, rotation=RotX(π))
-
-    # +y face (face 3)
+    # +x face (face 2)
     x⁺_face_grid = ConformalCubedSphereFaceGrid(FT, size=face_size, z=z, radius=radius, rotation=RotX(π/2))
 
-    # -y face (face 4)
-    x⁻_face_grid = ConformalCubedSphereFaceGrid(FT, size=face_size, z=z, radius=radius, rotation=RotX(-π/2))
-
-    # +x face (face 5)
+    # +y face (face 3)
     y⁺_face_grid = ConformalCubedSphereFaceGrid(FT, size=face_size, z=z, radius=radius, rotation=RotY(π/2))
 
-    # -x face (face 6)
+    # -x face (face 4)
+    x⁻_face_grid = ConformalCubedSphereFaceGrid(FT, size=face_size, z=z, radius=radius, rotation=RotX(-π/2))
+
+    # -y face (face 5)
     y⁻_face_grid = ConformalCubedSphereFaceGrid(FT, size=face_size, z=z, radius=radius, rotation=RotY(-π/2))
+
+    # -z face (face 6)
+    z⁻_face_grid = ConformalCubedSphereFaceGrid(FT, size=face_size, z=z, radius=radius, rotation=RotX(π))
 
     faces = (
         z⁺_face_grid,
-        z⁻_face_grid,
         x⁺_face_grid,
-        x⁻_face_grid,
         y⁺_face_grid,
-        y⁻_face_grid
+        x⁻_face_grid,
+        y⁻_face_grid,
+        z⁻_face_grid
     )
 
     # Construct face connectivity:
     #
-    #     +---+
-    #     | 1 |
+    # +---+
+    # | 1 |
     # +---+---+---+---+
     # | 2 | 3 | 4 | 5 |
     # +---+---+---+---+
-    #     | 6 |
-    #     +---+
+    # | 6 |
+    # +---+
 
     face_connectivity = (
         (right=4, left=2, above=5, below=3),
