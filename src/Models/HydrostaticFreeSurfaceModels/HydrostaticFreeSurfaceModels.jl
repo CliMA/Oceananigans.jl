836d5a13ed7dc996966426306486098600a66200
Gregory Wagner <wagner.greg@gmail.com>
2021-03-04 18:00:14 -0500

    Moves PrescribedVelocityFields to HydrostaticFreeSurfaceModel

--- src/Models/HydrostaticFreeSurfaceModels/HydrostaticFreeSurfaceModels.jl
+++ src/Models/HydrostaticFreeSurfaceModels/HydrostaticFreeSurfaceModels.jl
@@ -48,5 +48,6 @@ include("hydrostatic_free_surface_tendency_kernel_functions.jl")
 include("calculate_hydrostatic_free_surface_tendencies.jl")
 include("update_hydrostatic_free_surface_model_state.jl")
 include("hydrostatic_free_surface_ab2_step.jl")
+include("hydrostatic_prescribed_velocity_fields.jl")
 
 end # module
