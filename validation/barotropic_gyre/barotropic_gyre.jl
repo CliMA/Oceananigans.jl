5b877cff29d1263ed0d74789241e2806ace99c52
Gregory Wagner <wagner.greg@gmail.com>
2021-03-05 21:15:38 -0500

    Adds barotropic gyre script

---
+++ validation/barotropic_gyre/barotropic_gyre.jl
@@ -0,0 +1,119 @@
+# # Barotropic gyre
+
+using Oceananigans
+using Oceananigans.Grids
+
+using Oceananigans.Coriolis:
+    HydrostaticSphericalCoriolis,
+    VectorInvariantEnergyConserving,
+    VectorInvariantEnstrophyConserving
+
+using Oceananigans.Models.HydrostaticFreeSurfaceModels:
+    HydrostaticFreeSurfaceModel,
+    VectorInvariant,
+    ExplicitFreeSurface
+
+using Oceananigans.TurbulenceClosures: HorizontallyCurvilinearAnisotropicDiffusivity
+using Oceananigans.Utils: prettytime, hours
+using Oceananigans.OutputWriters: JLD2OutputWriter, TimeInterval, IterationInterval
+
+using Statistics
+using JLD2
+using Printf
+using GLMakie
+
+Nx = 60
+Ny = 30
+
+# A spherical domain
+grid = RegularLatitudeLongitudeGrid(size = (Nx, Ny, 1),
+                                    radius = 1,
+                                    longitude = (0, 60),
+                                    latitude = (30, 60),
+                                    z = (-4000, 0))
+
+free_surface = ExplicitFreeSurface(gravitational_acceleration=1)
+
+coriolis = HydrostaticSphericalCoriolis(scheme = VectorInvariantEnstrophyConserving())
+
+surface_wind_stress = BoundaryCondition(Flux,
+                                        (λ, ϕ, t, p) -> - p.τ * cos(2π * (ϕ - p.ϕ₀) / p.Lϕ),
+                                        parameters = (τ = 5e-4, Lϕ = grid.Ly, ϕ₀ = 45))
+
+u_bcs = UVelocityBoundaryConditions(grid, top = surface_wind_stress)
+                                        
+model = HydrostaticFreeSurfaceModel(grid = grid,
+                                    architecture = CPU(),
+                                    momentum_advection = VectorInvariant(),
+                                    free_surface = free_surface,
+                                    coriolis = coriolis,
+                                    boundary_conditions = (u=u_bcs,),
+                                    buoyancy = nothing,
+                                    closure = HorizontallyCurvilinearAnisotropicDiffusivity(νh=1e3, κh=1e3))
+
+gravity_wave_speed = sqrt(g * grid.Lz) # hydrostatic (shallow water) gravity wave speed
+
+# Time-scale for gravity wave propagation across the smallest grid cell
+wave_propagation_time_scale = min(grid.radius * cosd(maximum(abs, grid.ϕᵃᶜᵃ)) * deg2rad(grid.Δλ),
+                                  grid.radius * deg2rad(grid.Δϕ)) / gravity_wave_speed
+
+super_rotation_period = 2π * grid.radius / U
+
+simulation = Simulation(model,
+                        Δt = 0.1wave_propagation_time_scale,
+                        stop_iterations = 1000,
+                        iteration_interval = 100,
+                        progress = s -> @info "Time = $(s.model.clock.time) / $(s.stop_time)")
+                                                         
+output_fields = merge(model.velocities, (η=model.free_surface.η,))
+
+output_prefix = "barotropic_gyre"
+
+simulation.output_writers[:fields] = JLD2OutputWriter(model, output_fields,
+                                                      schedule = IterationInterval(100),
+                                                      prefix = output_prefix,
+                                                      field_slicer = nothing,
+                                                      force = true)
+
+run!(simulation)
+
+filepath = simulation.output_writers[:fields].filepath
+
+file = jldopen(filepath)
+
+iterations = parse.(Int, keys(file["timeseries/t"]))
+
+λ = xnodes(Face, grid)
+ϕ = ynodes(Center, grid)
+
+λ = repeat(reshape(λ, Nx, 1), 1, Ny)
+ϕ = repeat(reshape(ϕ, 1, Ny), Nx, 1)
+
+λ_azimuthal = λ .+ 180  # Convert to λ ∈ [0°, 360°]
+ϕ_azimuthal = 90 .- ϕ   # Convert to ϕ ∈ [0°, 180°] (0° at north pole)
+
+iter = Node(0)
+
+plot_title = @lift @sprintf("Barotropic gyre: iteration = %d", $iter)
+
+u = @lift file["timeseries/u/" * string($iter)][:, :, 1]
+
+# Plot on the unit sphere to align with the spherical wireframe.
+x = @. cosd(λ_azimuthal) * sind(ϕ_azimuthal)
+y = @. sind(λ_azimuthal) * sind(ϕ_azimuthal)
+z = @. cosd(ϕ_azimuthal)
+
+fig = Figure(resolution = (1080, 1080))
+
+ax = fig[1, 1] = LScene(fig, title="")
+wireframe!(ax, Sphere(Point3f0(0), 0.99f0), show_axis=false)
+surface!(ax, x, y, z, color=spatial_error, colormap=:thermal, colorrange=(0.0, 0.02))
+rotate_cam!(ax.scene, (-π/4, π/8, 0))
+zoom!(ax.scene, (0, 0, 0), 2, false)
+
+supertitle = fig[0, :] = Label(fig, plot_title, textsize=30)
+
+record(fig, output_prefix * ".mp4", iterations, framerate=30) do i
+    @info "Animating iteration $i/$(iterations[end])..."
+    iter[] = i
+end
