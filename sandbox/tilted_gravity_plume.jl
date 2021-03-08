469e3b7614945aaca96f89de6330c71d8789868b
tomchor <tomaschor@gmail.com>
2021-03-07 20:37:01 -0800

    modified sandbox example of tilted plume to test both BuoyancyTracer and SeawaterBuoyancy

--- sandbox/tilted_gravity_plume.jl
+++ sandbox/tilted_gravity_plume.jl
@@ -6,22 +6,38 @@ L = 2000
 topo = (Periodic, Bounded, Bounded)
 grid = RegularRectilinearGrid(topology=topo, size=(1, N, N), extent=(L, L, L))
 
+use_buoyancy_tracer = false
 θ = 45
-g = (0, sind(θ), cosd(θ))
-buoyancy = BuoyancyModel(model=BuoyancyTracer(), gravitational_unit_vector=g)
+
+g̃ = (0, sind(θ), cosd(θ))
+if use_buoyancy_tracer
+    buoyancy = BuoyancyModel(model=BuoyancyTracer(), gravitational_unit_vector=g̃)
+    tracers = :b
+else
+    buoyancy = BuoyancyModel(model=SeawaterBuoyancy(), gravitational_unit_vector=g̃)
+    tracers = :T, :S
+end
 
 model = IncompressibleModel(
            grid = grid,
     timestepper = :RungeKutta3,
        buoyancy = buoyancy,
-        tracers = :b,
+        tracers = tracers,
       advection = UpwindBiasedFifthOrder(),
         closure = IsotropicDiffusivity(ν=0, κ=0)
 )
 
 y₀, z₀ = L/2, -L/2
-b₀(x, y, z) = 0.01 * exp(-100 * ((y - y₀)^2 + (z - z₀)^2) / (L^2 + L^2))
-set!(model, b=b₀)
+anomaly(x, y, z) = 0.01 * exp(-100 * ((y - y₀)^2 + (z - z₀)^2) / (L^2 + L^2))
+
+if use_buoyancy_tracer
+    α = 2e-4; g₀ = 9.81
+    b₀(x, y, z) = α * g₀ * anomaly(x, y, z)
+    set!(model, b=b₀)
+else
+    T₀(x, y, z) = 10 + anomaly(x, y, z)
+    set!(model, T=T₀)
+end
 
 print_progress(sim) = @info "iteration: $(sim.model.clock.iteration), time: $(sim.model.clock.time)"
 simulation = Simulation(model, Δt=10seconds, stop_time=4hours, progress=print_progress, iteration_interval=10)
