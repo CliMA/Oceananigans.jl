2403e94068bd9dfe0bf87a4358b74d01628e5b1b
francispoulin <fpoulin@uwaterloo.ca>
2021-03-09 15:38:00 -0500

    Generalizing the plotting to other topologies.

--- examples/mpi_shallow_water_turbulence.jl
+++ examples/mpi_shallow_water_turbulence.jl
@@ -6,16 +6,16 @@ using Statistics
 using Oceananigans
 using Oceananigans.Distributed
 
+ranks = (2, 2, 1)
 topo = (Periodic, Periodic, Bounded)
 full_grid = RegularRectilinearGrid(topology=topo, size=(128, 128, 1), extent=(4π, 4π, 1), halo=(3, 3, 3))
-arch = MultiCPU(grid=full_grid, ranks=(1, 4, 1))
+arch = MultiCPU(grid=full_grid, ranks=ranks)
 
 model = DistributedShallowWaterModel(
                   architecture = arch,
                           grid = full_grid,
                    timestepper = :RungeKutta3,
                      advection = UpwindBiasedFifthOrder(),
-#                       closure = IsotropicDiffusivity(ν=1e-5),
     gravitational_acceleration = 1.0
 )
 
@@ -42,37 +42,28 @@ using Printf
 using NCDatasets
 using CairoMakie
 
-if arch.my_rank == 0
-    ranks = 4
+nranks = prod(ranks)
 
-    ds = [NCDataset("mpi_shallow_water_turbulence_rank$r.nc") for r in 0:ranks-1]
+if arch.my_rank == 0
+
+    ds = [NCDataset("mpi_shallow_water_turbulence_rank$r.nc") for r in 0:nranks-1]
 
     frame = Node(1)
     plot_title = @lift @sprintf("Oceananigans.jl + MPI: 2D turbulence t = %.2f", ds[1]["time"][$frame])
-    ζ = [@lift ds[r]["ζ"][:, :, 1, $frame] for r in 1:ranks]
-
+    ζ = [@lift ds[r]["ζ"][:, :, 1, $frame] for r in 1:nranks]
+    
     fig = Figure(resolution=(1600, 1200))
-
-    for r in reverse(1:ranks)
-        ax = fig[ranks-r+1, 1] = Axis(fig, ylabel="rank $(r-1)", xticks = MultiplesTicks(9, pi, "π"),  yticks = MultiplesTicks(3, pi, "π"))
+    
+    for rx in 1:ranks[1], ry in 1:ranks[2]
+        ax = fig[ry, rx] = Axis(fig)
+        r = (ry-1)*ranks[2] + rx - 1
         hm = CairoMakie.heatmap!(ax, ds[r]["xF"], ds[r]["yF"], ζ[r], colormap=:balance, colorrange=(-2, 2))
-        r > 1 && hidexdecorations!(ax, grid=false)
-        if r == 1
-            cb = fig[:, 2] = Colorbar(fig, hm, label = "Vorticity ζ = ∂x(v) - ∂y(u)", width=30)
-            cb.height = Relative(2/3)
-        end
-        xlims!(ax, [0, 4π])
-        ylims!(ax, [(r-1)*π, r*π])
     end
-
-    supertitle = fig[0, :] = Label(fig, plot_title, textsize=30)
-
-    trim!(fig.layout)
-
+    
     record(fig, "mpi_shallow_water_turbulence.mp4", 1:length(ds[1]["time"])-1, framerate=30) do n
         @info "Animating MPI turbulence frame $n/$(length(ds[1]["time"]))..."
         frame[] = n
     end
-
+    
     [close(d) for d in ds]
 end
---
+++ examples/mpi_shallow_water_turbulence_plotting.jl
@@ -0,0 +1,42 @@
+#using MPI
+
+#MPI.Initialized() || MPI.Init()
+
+using Printf
+using NCDatasets
+using CairoMakie
+
+ranks = (4,1,1)
+nranks = prod(ranks)
+
+ds = [NCDataset("mpi_shallow_water_turbulence_rank$r.nc") for r in 0:nranks-1]
+
+frame = Node(1)
+plot_title = @lift @sprintf("Oceananigans.jl + MPI: 2D turbulence t = %.2f", ds[1]["time"][$frame])
+ζ = [@lift ds[r]["ζ"][:, :, 1, $frame] for r in 1:nranks]
+
+fig = Figure(resolution=(1600, 1200))
+
+for rx in 1:ranks[1], ry in 1:ranks[2]
+    ax = fig[ry, rx] = Axis(fig)
+    r = (ry-1)*ranks[2] + rx - 1
+    hm = CairoMakie.heatmap!(ax, ds[r]["xF"], ds[r]["yF"], ζ[r], colormap=:balance, colorrange=(-2, 2))
+    #r > 1 && hidexdecorations!(ax, grid=false)
+    #if r == 1
+    #    cb = fig[:, 5] = Colorbar(fig, hm, label = "Vorticity ζ = ∂x(v) - ∂y(u)", width=30)
+    #    cb.height = Relative(2/3)
+    #end
+    #xlims!(ax, [(r-1)*π, r*π])
+    #ylims!(ax, [0, 4π])
+end
+
+#supertitle = fig[0, :] = Label(fig, plot_title, textsize=30)
+
+#trim!(fig.layout)
+
+record(fig, "mpi_shallow_water_turbulence.mp4", 1:length(ds[1]["time"])-1, framerate=30) do n
+    @info "Animating MPI turbulence frame $n/$(length(ds[1]["time"]))..."
+    frame[] = n
+end
+
+[close(d) for d in ds]
