9866858e13e0f49aded60ff88b78da5ff0b024a0
francispoulin <fpoulin@uwaterloo.ca>
2021-03-11 15:51:58 -0500

    First attempt to do strong scaling for shallow water. Does not work.

---
+++ benchmark/strong_scaling_shallow_water_model.jl
@@ -0,0 +1,38 @@
+using JLD2
+using BenchmarkTools 
+using Benchmarks
+
+# Benchmark parameters
+
+Nx = 256
+Ny = 256
+
+ranks = (1, 2, 4, 8, 16)
+
+# Run and collect benchmarks
+
+print_system_info()
+
+for r in ranks
+    @info "Benchmarking distributed shallow water model strong scaling [N=($Nx, $Ny), ranks=$r]..."
+    julia = Base.julia_cmd()
+    run(`mpiexec -np $r $julia --project strong_scaling_shallow_water_model_single.jl $Nx $Ny`)
+end
+
+suite = BenchmarkGroup(["size", "ranks"])
+for r in ranks
+    jldopen("strong_scaling_shallow_water_model_$r.jld2", "r") do file
+        suite[((Nx, Ny), r)] = file["trial"]
+    end
+end
+
+# Summarize benchmarks
+
+df = benchmarks_dataframe(suite)
+sort!(df, :ranks)
+benchmarks_pretty_table(df, title="Shallow water model strong scaling benchmark")
+
+suite_Δ = speedups_suite(suite, base_case=((Nx, Ny), 1))
+df_Δ = speedups_dataframe(suite_Δ)
+sort!(df_Δ, :ranks)
+benchmarks_pretty_table(df_Δ, title="Shallow water model strong scaling speedup")
