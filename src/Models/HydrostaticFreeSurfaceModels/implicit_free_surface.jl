cf7a1559074a3e4aa65630ed0a5bf8bf54b392a5
christophernhill <cnh@mit.edu>
2021-03-10 19:35:06 -0500

    0

--- src/Models/HydrostaticFreeSurfaceModels/implicit_free_surface.jl
+++ src/Models/HydrostaticFreeSurfaceModels/implicit_free_surface.jl
@@ -54,8 +54,8 @@ explicit_barotropic_pressure_y_gradient(i, j, k, grid, free_surface::ImplicitFre
 @kernel function _compute_vertically_integrated_lateral_face_areas!(grid, A )
     i, j = @index(Global, NTuple)
     # U.w[i, j, 1] = 0 is enforced via halo regions.
-    A.Ax[i, j, 1] = 0.
-    A.Ay[i, j, 1] = 0.
+    A.Ax[i, j, 1] = 0
+    A.Ay[i, j, 1] = 0
     @unroll for k in 1:grid.Nz
         @inbounds A.Ax[i, j, 1] += Δyᶠᶜᵃ(i, j, k, grid)*Δzᵃᵃᶜ(i, j, k, grid)
         @inbounds A.Ay[i, j, 1] += Δxᶜᶠᵃ(i, j, k, grid)*Δzᵃᵃᶜ(i, j, k, grid)
