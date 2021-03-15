256219ad59d6d7fb3d7d0e487e23571e80e7df21
Tomas Chor <tomaschor@gmail.com>
2021-03-15 09:52:33 -0700

    Introduce cell_advection_timescale for VerticallyStretched grids

--- src/Utils/cell_advection_timescale.jl
+++ src/Utils/cell_advection_timescale.jl
@@ -11,6 +11,21 @@ function cell_advection_timescale(u, v, w, grid)
     return min(Δx/umax, Δy/vmax, Δz/wmax)
 end
 
+
+function cell_advection_timescale(u, v, w, grid::VerticallyStretchedRectilinearGrid)
+    umax = maximum(abs, u)
+    vmax = maximum(abs, v)
+    wmax = maximum(abs, w)
+
+    Δx = grid.Δx
+    Δy = grid.Δy
+    Δz = minimum(grid.Δzᵃᵃᶜ[1:grid.Nz])
+
+    return min(Δx/umax, Δy/vmax, Δz/wmax)
+end
+
+
+
 cell_advection_timescale(model) =
     cell_advection_timescale(model.velocities.u.data.parent,
                              model.velocities.v.data.parent,
