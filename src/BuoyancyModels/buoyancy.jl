aa504cdee7db95cfd7835fffb385a7b08830baf7
ali.hh.ramadhan@gmail.com <ali.hh.ramadhan@gmail.com>
2021-03-16 20:23:23 -0400

    Update/fix AMD

--- src/BuoyancyModels/buoyancy.jl
+++ src/BuoyancyModels/buoyancy.jl
@@ -15,13 +15,21 @@ function Buoyancy(; model, gravitational_unit_vector=(0, 0, 1))
     return Buoyancy(model, gravitational_unit_vector)
 end
 
+@inline ĝ_x(buoyancy) = @inbounds buoyancy.gravitational_unit_vector[1]
+@inline ĝ_y(buoyancy) = @inbounds buoyancy.gravitational_unit_vector[2]
+@inline ĝ_z(buoyancy) = @inbounds buoyancy.gravitational_unit_vector[3]
+
+#####
+##### For convinience
+#####
+
 @inline required_tracers(bm::Buoyancy) = required_tracers(bm.model)
 
 @inline get_temperature_and_salinity(bm::Buoyancy, C) = get_temperature_and_salinity(bm.model, C)
 
+@inline ∂x_b(i, j, k, grid, b::Buoyancy, C) = ∂x_b(i, j, k, grid, b.model, C)
+@inline ∂y_b(i, j, k, grid, b::Buoyancy, C) = ∂y_b(i, j, k, grid, b.model, C)
+@inline ∂z_b(i, j, k, grid, b::Buoyancy, C) = ∂z_b(i, j, k, grid, b.model, C)
+
 regularize_buoyancy(b) = b
 regularize_buoyancy(b::AbstractBuoyancyModel) = Buoyancy(model=b)
-
-@inline ĝ_x(buoyancy) = @inbounds buoyancy.gravitational_unit_vector[1]
-@inline ĝ_y(buoyancy) = @inbounds buoyancy.gravitational_unit_vector[2]
-@inline ĝ_z(buoyancy) = @inbounds buoyancy.gravitational_unit_vector[3]
