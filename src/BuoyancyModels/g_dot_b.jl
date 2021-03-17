05f9f43aec817d39972a7d56cd7bc3478ebd7255
ali.hh.ramadhan@gmail.com <ali.hh.ramadhan@gmail.com>
2021-03-16 20:39:33 -0400

    Fixes and clean up sandbox

--- src/BuoyancyModels/g_dot_b.jl
+++ src/BuoyancyModels/g_dot_b.jl
@@ -2,6 +2,6 @@
 @inline y_dot_g_b(i, j, k, grid, buoyancy, C) = ĝ_y(buoyancy) * buoyancy_perturbation(i, j, k, grid, buoyancy.model, C)
 @inline z_dot_g_b(i, j, k, grid, buoyancy, C) = ĝ_z(buoyancy) * buoyancy_perturbation(i, j, k, grid, buoyancy.model, C)
 
-@inline x_dot_g_b(i, j, k, grid, ::::Buoyancy{M, VerticalDirection}, C) where M = 0
-@inline y_dot_g_b(i, j, k, grid, ::::Buoyancy{M, VerticalDirection}, C) where M = 0
-@inline z_dot_g_b(i, j, k, grid, ::::Buoyancy{M, VerticalDirection}, C) where M = buoyancy_perturbation(i, j, k, grid, buoyancy.model, C)
+@inline x_dot_g_b(i, j, k, grid,  ::Buoyancy{M, VerticalDirection}, C) where M = 0
+@inline y_dot_g_b(i, j, k, grid,  ::Buoyancy{M, VerticalDirection}, C) where M = 0
+@inline z_dot_g_b(i, j, k, grid, b::Buoyancy{M, VerticalDirection}, C) where M = buoyancy_perturbation(i, j, k, grid, b.model, C)
