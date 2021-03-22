367efe2ee413c82b05b4f6bfc996ee71c42fdb1a
francispoulin <fpoulin@uwaterloo.ca>
2021-03-21 23:43:13 -0400

    Adding lines that will be included later.

--- src/Operators/spacings_and_areas_and_volumes.jl
+++ src/Operators/spacings_and_areas_and_volumes.jl
@@ -52,6 +52,19 @@ The operators in this file fall into three categories:
 @inline Δzᵃᵃᶜ(i, j, k, grid::VerticallyStretchedRectilinearGrid) = @inbounds grid.Δzᵃᵃᶜ[k]
 
 #####
+##### "Spacings" in Flat directions. Here we dispatch to `one`. This abuse of notation
+##### makes volumes correct, and avoids issues with derivatives such as those involved
+##### in the pressure correction step.
+#####
+
+#using Oceananigans.Grids: Flat
+
+#@inline Δx(i, j, k,  grid::AbstractGrid{FT, Flat})         where FT           = one(FT)
+#@inline Δy(i, j, k,  grid::AbstractGrid{FT, TX, Flat})     where {FT, TX}     = one(FT)
+#@inline ΔzC(i, j, k, grid::AbstractGrid{FT, TX, TY, Flat}) where {FT, TX, TY} = one(FT)
+#@inline ΔzF(i, j, k, grid::AbstractGrid{FT, TX, TY, Flat}) where {FT, TX, TY} = one(FT)
+
+#####
 ##### Areas for horizontally-regular algorithms
 #####
 
