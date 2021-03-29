07cfc4cf85dd5a28e7d6c288936e2686dacef19c
ali.hh.ramadhan@gmail.com <ali.hh.ramadhan@gmail.com>
2021-03-29 17:24:26 -0400

    Switch Documenter.jl math engine to MathJax3

--- docs/make.jl
+++ docs/make.jl
@@ -141,7 +141,8 @@ pages = [
 format = Documenter.HTML(
     collapselevel = 1,
        prettyurls = get(ENV, "CI", nothing) == "true",
-        canonical = "https://clima.github.io/OceananigansDocumentation/stable/"
+        canonical = "https://clima.github.io/OceananigansDocumentation/stable/",
+       mathengine = MathJax3()
 )
 
 makedocs(bib,
