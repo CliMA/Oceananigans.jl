dd2f110f05c78798ae080f87b14d8801364db385
ali.hh.ramadhan@gmail.com <ali.hh.ramadhan@gmail.com>
2021-03-30 14:30:30 -0400

    More cubed sphere dispatch and simulation runs!

--- validation/surface_gravity_waves_on_sphere/surface_gravity_waves_on_sphere.jl
+++ validation/surface_gravity_waves_on_sphere/surface_gravity_waves_on_sphere.jl
@@ -22,6 +22,35 @@ import Oceananigans.CubedSpheres: maybe_replace_with_face
 maybe_replace_with_face(free_surface::ExplicitFreeSurface, cubed_sphere_grid, face_number) =
   ExplicitFreeSurface(free_surface.η.faces[face_number], free_surface.gravitational_acceleration)
 
+import Oceananigans.Diagnostics: accurate_cell_advection_timescale
+
+function accurate_cell_advection_timescale(grid::ConformalCubedSphereGrid, velocities)
+
+    min_timescale_on_faces = []
+
+    for (face_number, grid_face) in enumerate(grid.faces)
+        velocities_face = maybe_replace_with_face(velocities, grid, face_number)
+        min_timescale_on_face = accurate_cell_advection_timescale(grid_face, velocities_face)
+        push!(min_timescale_on_faces, min_timescale_on_face)
+        @info "min_timescale on face $face_number = $min_timescale_on_face"
+    end
+
+    return minimum(min_timescale_on_faces)
+end
+
+import Oceananigans.OutputWriters: fetch_output
+
+fetch_output(field::ConformalCubedSphereField, model, field_slicer) =
+    Tuple(fetch_output(field_face, model, field_slicer) for field_face in field.faces)
+
+import Base: minimum, maximum
+
+minimum(field::ConformalCubedSphereField; dims=:) = minimum(minimum(field_face; dims) for field_face in field.faces)
+maximum(field::ConformalCubedSphereField; dims=:) = maximum(maximum(field_face; dims) for field_face in field.faces)
+
+minimum(f, field::ConformalCubedSphereField; dims=:) = minimum(minimum(f, field_face; dims) for field_face in field.faces)
+maximum(f, field::ConformalCubedSphereField; dims=:) = maximum(maximum(f, field_face; dims) for field_face in field.faces)
+
 #####
 ##### Script starts here
 #####
@@ -58,6 +87,7 @@ model = HydrostaticFreeSurfaceModel(
               buoyancy = nothing
 )
 
+## Initial condition:
 ## Very small sea surface height perturbation so the resulting dynamics are well-described
 ## by a linear free surface.
 
@@ -69,6 +99,50 @@ A  = 1e-5 * H  # Amplitude of the perturbation
 
 η′(λ, φ, z) = A * exp(- (λ - λ₀)^2 / Δλ^2) * exp(- (φ - φ₀)^2 / Δφ^2)
 
-# set!(model, η=η′)
+set!(model, η=η′)
 
-set!(model.free_surface.η, η′)
+## Simulation setup
+
+Δt = 20minutes
+
+cfl = CFL(Δt, accurate_cell_advection_timescale)
+
+mutable struct Progress
+    interval_start_time :: Float64
+end
+
+function (p::Progress)(sim)
+    wall_time = (time_ns() - p.interval_start_time) * 1e-9
+
+    @info @sprintf("Time: %s, iteration: %d, max(u⃗): (%.2e, %.2e) m/s, extrema(η): (min=%.2e, max=%.2e), CFL: %.2e, wall time: %s",
+                   prettytime(sim.model.clock.time),
+                   sim.model.clock.iteration,
+                   maximum(abs, sim.model.velocities.u),
+                   maximum(abs, sim.model.velocities.v),
+                   minimum(abs, sim.model.free_surface.η),
+                   maximum(abs, sim.model.free_surface.η),
+                   cfl(model),
+                   prettytime(wall_time))
+
+    p.interval_start_time = time_ns()
+
+    return nothing
+end
+
+simulation = Simulation(model,
+                        Δt = Δt,
+                        stop_time = 1day,
+                        iteration_interval = 1,
+                        progress = Progress(time_ns()))
+
+# TODO: Implement NaNChecker for ConformalCubedSphereField
+empty!(simulation.diagnostics)
+
+output_fields = merge(model.velocities, (η=model.free_surface.η,))
+
+simulation.output_writers[:fields] = JLD2OutputWriter(model, output_fields,
+                                                      schedule = TimeInterval(1hour),
+                                                      prefix = "full_cubed_sphere_gravity_waves",
+                                                      force = true)
+
+run!(simulation)
