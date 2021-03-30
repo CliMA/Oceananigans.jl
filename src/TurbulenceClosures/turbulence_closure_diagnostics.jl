124776d8752cb88d3d9a801f7b7ed20908de368c
francispoulin <fpoulin@uwaterloo.ca>
2021-03-30 15:34:32 -0400

    Making changes suggested by Greg

--- src/TurbulenceClosures/turbulence_closure_diagnostics.jl
+++ src/TurbulenceClosures/turbulence_closure_diagnostics.jl
@@ -1,5 +1,4 @@
 # Timescale for diffusion across one cell
-using Oceananigans.Grids: min_Δx, min_Δy, min_Δz
 using Oceananigans.Grids: topology
 
 function min_Δxyz(grid)
--- src/Utils/cell_advection_timescale.jl
+++ src/Utils/cell_advection_timescale.jl
@@ -29,8 +29,6 @@ function cell_advection_timescale(u, v, w, grid::VerticallyStretchedRectilinearG
 
     Δx = minimum_grid_spacing(grid.Δx,    topo[1])
     Δy = minimum_grid_spacing(grid.Δy,    topo[2])
-    #FJP: this line fails when run on gpu 
-    #FJP: see test_diagnostics.jl for an example
     Δz = minimum_grid_spacing(grid.Δzᵃᵃᶠ, topo[3])
 
     return min(Δx/umax, Δy/vmax, Δz/wmax)
