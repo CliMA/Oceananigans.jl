8bf5feb544ea6600826f9eaf074f7eca22410c9e
francispoulin <fpoulin@uwaterloo.ca>
2021-04-01 11:39:46 -0400

    trying to fix show but failing

--- test_zonal_grid.jl
+++ test_zonal_grid.jl
@@ -1,11 +1,12 @@
 using Revise
 
 using Oceananigans
+using Oceananigans.Grids: short_show, long_show, show
 
 grid1 = ZonallyStretchedRectilinearGrid(size=(8, 8, 8), x=(0,8), y=(0,8), z=(0,8), topology=(Bounded, Bounded, Bounded))
 print(short_show(grid1),"\n")
-print(long_show(grid1),"\n")
-print(show(grid1),"\n")
+#print(long_show(grid1),"\n")
+#print(show(grid1),"\n")
 
 grid2 = ZonallyStretchedRectilinearGrid(size=(8, 8), x=(0,8), y=(0,8), topology=(Bounded, Bounded, Flat))
 print(short_show(grid2), "\n")
