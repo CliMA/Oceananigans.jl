97f27d87727fe90b62c0a573c163f998471514d9
francispoulin <fpoulin@uwaterloo.ca>
2021-04-06 09:30:29 -0400

    Fixing up parameters and adding animation

--- examples/inertially_unstable_jet.jl
+++ examples/inertially_unstable_jet.jl
@@ -3,9 +3,7 @@ using Oceananigans.Units: hours, meters, kilometers, minutes
 
 using JLD2, Plots, Printf
 
-using Revise
-
-   const Ly = 100kilometers
+   const Ly = 1000kilometers
    const Lz = 3kilometers
     const D = Lz/2
 const L_jet = Ly/10
@@ -14,12 +12,11 @@ const H_jet = Lz/10
 grid = RegularRectilinearGrid(size=(128, 128), 
                                  y=(-Ly/2, Ly/2), z=(-Lz, 0),
                           topology=(Flat, Bounded, Bounded),
-                             halo = (3, 3)
-            )
+                             halo = (3, 3))
 
 
-   coriolis = FPlane(latitude=45)
-   const N² = 1.03e-2
+   coriolis = FPlane(0.73e-4)
+   const N² = 1e-2
 const U_max = 14.6 
 
 
@@ -35,9 +32,10 @@ model = IncompressibleModel(
          coriolis = coriolis,
           tracers = :b,
 background_fields = (b=B,),
-         buoyancy = BuoyancyTracer()
-)
+         buoyancy = BuoyancyTracer())
 
+ϵ = 1e-4
+uᵖ = 
 uⁱ(x, y, z) =                                              U_max * sech(y/L_jet)^2 * exp( - (z + D)^2/H_jet^2 )
 bⁱ(x, y, z) = 2 * coriolis.f * L_jet / H_jet^2 * (z + D) * U_max * tanh(y/L_jet)   * exp( - (z + D)^2/H_jet^2 )
 
@@ -53,20 +51,6 @@ kwargs = (
              xlims=(-Ly/2e3, Ly/2e3), 
              ylims=(-Lz/1e3,0)
          )
-
-pltu = contourf(y/1e3, z/1e3, 
-                interior(model.velocities.u)[1,:,:]',
-                title="Zonal Velocity",
-                color=:matter; 
-                kwargs...)
-savefig(pltu, "zonal_velocity.png")
-
-pltb = contourf(y/1e3, z/1e3, 
-                interior(model.tracers.b)[1,:,:]',
-                title="Buoyancy",
-                color=:balance; 
-                kwargs...)
-savefig(pltb, "buoyancy.png")
 
 #wizard = TimeStepWizard(cfl=1.0, Δt=1minutes, max_change=1.1, max_Δt=2minutes)
 
@@ -89,8 +73,7 @@ simulation.output_writers[:fields] = JLD2OutputWriter(model, outputs,
 run!(simulation)
 
 
-#file = jldopen(simulation.output_writers[:fields].filepath)
-file = jldopen("inertial_instability.jld2")
+file = jldopen(simulation.output_writers[:fields].filepath)
 
 iterations = parse.(Int, keys(file["timeseries/t"]))
 
@@ -106,20 +89,17 @@ anim = @animate for (i, iteration) in enumerate(iterations)
    u_snapshot = file["timeseries/u/$iteration"][1, :, :]
    b_snapshot = file["timeseries/b/$iteration"][1, :, :]
 
-   #ω_lim = 2.0
-   #ω_levels = range(-ω_lim, stop=ω_lim, length=20)
-
-   #s_lim = 0.2
-   #s_levels = range(0, stop=s_lim, length=20)
-
    u_plot = contourf(y/1e3, z/1e3, u_snapshot',
                      title = @sprintf("u, at t = %.1f hours", t/hours),
-                     color=:matter; 
+                     color=:matter,
+                     clim=(-1, 13); 
                      kwargs...)
 
+   b_max = maximum(abs, b_snapshot) 
    b_plot = contourf(y/1e3, z/1e3, b_snapshot',
                      title = @sprintf("b, at t = %.1f hours", t/hours),
-                     color=:balance; 
+                     color=:balance;
+                     clim=(-b_max, b_max), 
                      kwargs...)
 
    
@@ -130,7 +110,5 @@ mp4(anim, "Inertial_Instability_2D.mp4", fps=15)
 
 # To-Do
 # 1. Add perturbations
-# 2. Make animation
-# 3. Do 2D simulation (locally)
 # 4. Do 3D simulation on cedar (cpu and then cpu)
 # 5. Try MPI?
