f2dafb6835b55eb544545fb8fc7591284a813fba
francispoulin <fpoulin@uwaterloo.ca>
2021-04-06 13:47:48 -0400

    more touching up

--- examples/inertially_unstable_jet.jl
+++ examples/inertially_unstable_jet.jl
@@ -1,6 +1,5 @@
 using Oceananigans
 using Oceananigans.Units: hours, days, seconds, meters, kilometers, minutes
-
 using JLD2, Plots, Printf
 
    const Ly = 100kilometers
@@ -9,7 +8,7 @@ using JLD2, Plots, Printf
 const L_jet = Ly/10
 const H_jet = Lz/10
 
-grid = RegularRectilinearGrid(size=(128, 128), 
+grid = RegularRectilinearGrid(size=(256, 256), 
                                  y=(-Ly/2, Ly/2), z=(-Lz, 0),
                           topology=(Flat, Bounded, Bounded),
                              halo = (3, 3))
@@ -32,11 +31,11 @@ background_fields = (b=B,),
          buoyancy = BuoyancyTracer(),
           closure = AnisotropicDiffusivity(νh=0, νz=1.27e-2))
 
-ū(x, y, z) =                                              U_max * sech(y/L_jet)^2 * exp( - (z + D)^2/H_jet^2 )
-b̄(x, y, z) = 2 * coriolis.f * L_jet / H_jet^2 * (z + D) * U_max * tanh(y/L_jet)   * exp( - (z + D)^2/H_jet^2 )
+ū(x, y, z) = U_max * sech(y/L_jet)^2 * exp( - (z + D)^2/H_jet^2 )
+b̄(x, y, z) = U_max * tanh(y/L_jet)   * exp( - (z + D)^2/H_jet^2 ) * 2 * coriolis.f * L_jet / H_jet^2 * (z + D)
 
 perturbation(x, y, z) = randn() * sech(y/L_jet)^2 * exp( - (z + D)^2/H_jet^2 )
-          uⁱ(x, y, z) = ū(x, y, z) + 1e-4 * perturbation(x, y, z)
+          uⁱ(x, y, z) = ū(x, y, z) + 1e-2 * perturbation(x, y, z)
           bⁱ(x, y, z) = b̄(x, y, z) + 1e-4 * perturbation(x, y, z)
 
 set!(model, u = uⁱ, b = bⁱ)
@@ -57,14 +56,12 @@ kwargs = (
              xlims=(-Ly/2e3, Ly/2e3), 
              ylims=(-Lz/1e3,0)
          )
-
-#wizard = TimeStepWizard(cfl=1.0, Δt=1minutes, max_change=1.1, max_Δt=2minutes)
-
 progress(sim) = @printf("Iteration: %d, time: %s, Δt: %s\n",
                         sim.model.clock.iteration,
                         prettytime(sim.model.clock.time),
                         prettytime(sim.Δt))
 
+#wizard = TimeStepWizard(cfl=1.0, Δt=1minutes, max_change=1.1, max_Δt=2minutes)
 simulation = Simulation(model, Δt=10, stop_time=2days,
                         iteration_interval=10, progress=progress)
 
@@ -79,9 +76,7 @@ file = jldopen(simulation.output_writers[:fields].filepath)
 
 iterations = parse.(Int, keys(file["timeseries/t"]))
 
-using Plots
-
-@info "Making a neat movie of zonal velocity and buoyancy..."
+@info "Making a movie of perturbation zonal velocity and buoyancy..."
 
 anim = @animate for (i, iteration) in enumerate(iterations)
 
@@ -93,14 +88,14 @@ anim = @animate for (i, iteration) in enumerate(iterations)
 
    u_max = maximum(abs, u_snapshot)
    u_plot = contourf(y/1e3, z/1e3, u_snapshot',
-                     title = @sprintf("ũ, at t = %.1f hours", t/hours),
+                     title = @sprintf("ũ at t = %.1f hours", t/hours),
                      color=:balance,
                      clim=(-u_max, u_max); 
                      kwargs...)
 
    b_max = maximum(abs, b_snapshot) 
    b_plot = contourf(y/1e3, z/1e3, b_snapshot',
-                     title = @sprintf("b̃, at t = %.1f hours", t/hours),
+                     title = @sprintf("b̃ at t = %.1f hours", t/hours),
                      color=:balance;
                      clim=(-b_max, b_max), 
                      kwargs...)
