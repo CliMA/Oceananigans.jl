0d06421e9a998a268129190dadf36c996625ecb4
Gregory Wagner <wagner.greg@gmail.com>
2021-04-09 10:23:59 -0700

    Fixes syntax bug in test_poisson_solvers

--- test/test_poisson_solvers.jl
+++ test/test_poisson_solvers.jl
@@ -267,10 +267,10 @@ two_dimensional_topologies = [
         (Periodic, Periodic, Bounded),
         (Periodic, Bounded,  Bounded),
         (Bounded,  Periodic, Bounded),
-        (Bounded,  Bounded,  Bounded)
-        (Flat,     Bounded,  Bounded)
-        (Flat,     Periodic, Bounded)
-        (Bounded,  Flat,     Bounded)
+        (Bounded,  Bounded,  Bounded),
+        (Flat,     Bounded,  Bounded),
+        (Flat,     Periodic, Bounded),
+        (Bounded,  Flat,     Bounded),
         (Periodic, Flat,     Bounded)
     ]
 
