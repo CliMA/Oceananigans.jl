4a144997f54ea47d31b8026084ad97c775353c45
ali.hh.ramadhan@gmail.com <ali.hh.ramadhan@gmail.com>
2021-04-14 14:52:07 -0400

    Fix corner positions for vortificty field at ffa

--- src/Operators/vorticity_operators.jl
+++ src/Operators/vorticity_operators.jl
@@ -15,18 +15,18 @@
         return Δy_vᶜᶠᵃ(i, j, k, grid, v) - δyᵃᶠᵃ(i, j, k, grid, Δx_uᶠᶜᵃ, u)
 
     # South-east corner
-    elseif i == grid.Nx && j == 1
+    elseif i == grid.Nx+1 && j == 1
         return - Δy_vᶜᶠᵃ(i, j-1, k, grid, v) - δyᵃᶠᵃ(i, j, k, grid, Δx_uᶠᶜᵃ, u)
 
     # North-west corner
-    elseif i == 1 && j == grid.Ny
+    elseif i == 1 && j == grid.Ny+1
         return Δy_vᶜᶠᵃ(i, j, k, grid, v) - δyᵃᶠᵃ(i, j, k, grid, Δx_uᶠᶜᵃ, u)
 
     # North-east corner
-    elseif i == grid.Nx && j == grid.Ny
+    elseif i == grid.Nx+1 && j == grid.Ny+1
         return - Δy_vᶜᶠᵃ(i-1, j, k, grid, v) - δyᵃᶠᵃ(i, j, k, grid, Δx_uᶠᶜᵃ, u)
 
-    # Not at a corner
+    # Not a corner
     else
         return δxᶠᵃᵃ(i, j, k, grid, Δy_vᶜᶠᵃ, v) - δyᵃᶠᵃ(i, j, k, grid, Δx_uᶠᶜᵃ, u)
     end
