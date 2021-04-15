40720991fb5632b7d250021e223466b73bbb0cc0
Gregory L. Wagner <wagner.greg@gmail.com>
2021-04-15 10:05:04 -0700

    PreconditionedConjugateGradientSolver and ImplicitFreeSurface refactor (#1575)
    * Makes eta a ReducedField
    
    * Remove dispatch on Field in regularize_forcing
    
    * Makes eta a ReducedField in ImplicitFreeSurface
    
    * Adds tests for similar(f::Field)
    
    * Adds similar for Field and ReducedField
    
    * Adds back ReducedField constructor for tuple locations
    
    * Disambiguates DefaultBoundaryCondition(Flat, Nothing)
    
    * Centralizes calls to FreeSurfaceDisplacementField and a bit of cleanup
    
    * Generalizes PreconditionedConjugateGradientSolver to work with AbstractField rather than OffsetArray
    
    * Generalizes fill_halo_regions for NormalFlow boundary conditions to work non-function boundary conditions
    
    * Generalizes implicit free surface solver to use ReducedField surface displacement
    
    * Fixes up implicit free surface solver tests for ReducedField surface displacement
    
    * Updates PreconditionedConjugateGradientSolver test to use Field/ReducedField
    
    * Messy updates to PreconditionedConjugateGradientSolver
    
    * Refactoring PreconditionedConjugateGradientSolver
    
    * Some more messy clean up of conjugate gradient solver
    
    * Refactors PreconditionedConjugateGradientSolver
    
    * Refactors tests for PreconditionedConjugateGradientSolver
    
    * Renames test_solvers to test_batched_tridiagonal_solver
    
    * Continues major refactor of ConjugateGradientSolver plus ImplicitFreeSurface
    
    * Refactors the implicit free surface solver
    
    * Fixes bug in implicit free surface solver test
    
    * Fixes up hydrostatic free surface tests
    
    * Uncomments solver tests
    
    * Bugfixes in implicit free surface solver tests
    
    * docs: instantiate main env separately
    
    * Fix LOAD_PATH
    
    * Update versioninfo.jl
    
    * Fixes bug in implicit free surface solver tests
    
    * Fix set! and all_*_nodes for reduced fields
    
    * Fixes bug in implicit free surface RHS
    
    * Removes extraneous parentheses
    
    * Fixes some cosemetic issues
    
    * Use length-1 range for all_*_nodes(::Type{Nothing}, grid)
    
    * Adapt ImplicitFreeSurface for GPU
    
    * Missing plus sign, oh my
    
    * Updates geostrophic adjustment example to use ImplicitFreeSurface
    
    * Reduce tolerance for extrema error in implicit free surface solver tests
    
    * Update test_implicit_free_surface_solver.jl
    
    * Make tests use 3d grid and set LHS to ReducedField
    
    * Update src/Solvers/preconditioned_conjugate_gradient_solver.jl
    
    Co-authored-by: Ali Ramadhan <ali.hh.ramadhan@gmail.com>
    
    * Update test_implicit_free_surface_solver.jl
    
    * Infer floating point type from \chi rather than dt
    
    Co-authored-by: Simon Byrne <simonbyrne@gmail.com>
    Co-authored-by: ali.hh.ramadhan@gmail.com <ali.hh.ramadhan@gmail.com>
    Co-authored-by: christophernhill <cnh@mit.edu>

---
+++ src/Models/HydrostaticFreeSurfaceModels/compute_vertically_integrated_volume_flux.jl
@@ -0,0 +1,46 @@
+"""
+Compute the vertical integrated volume flux from the bottom to z=0 (i.e. linear free-surface)
+
+```
+U★ = ∫ᶻ Ax * u★ dz`
+V★ = ∫ᶻ Ay * v★ dz`
+"""
+### Note - what we really want is RHS = divergence of the vertically integrated volume flux
+###        we can optimize this a bit later to do this all in one go to save using intermediate variables.
+function compute_vertically_integrated_volume_flux!(∫ᶻ_U, model, velocities_update_event)
+
+    wait(device(model.architecture), velocities_update_event)
+
+    fill_halo_regions!(model.velocities, model.architecture, model.clock, fields(model))
+
+    event = launch!(model.architecture,
+                    model.grid,
+                    :xy,
+                    _compute_vertically_integrated_volume_flux!,
+                    ∫ᶻ_U,
+                    model.grid,
+                    model.velocities,
+                    dependencies=Event(device(model.architecture)))
+
+    wait(device(model.architecture), event)
+
+    fill_halo_regions!(∫ᶻ_U, model.architecture, model.clock, fields(model))
+
+    return nothing
+end
+
+@kernel function _compute_vertically_integrated_volume_flux!(∫ᶻ_U, grid, U)
+    i, j = @index(Global, NTuple)
+
+    @inbounds begin
+
+        ∫ᶻ_U.u[i, j, 1] = 0
+        ∫ᶻ_U.v[i, j, 1] = 0
+
+        @unroll for k in 1:grid.Nz
+            ∫ᶻ_U.u[i, j, 1] += Axᶠᶜᶜ(i, j, k, grid) * U.u[i, j, k]
+            ∫ᶻ_U.v[i, j, 1] += Ayᶜᶠᶜ(i, j, k, grid) * U.v[i, j, k]
+        end
+
+     end
+end
--- test/test_preconditioned_conjugate_gradient_solver.jl
+++ test/test_preconditioned_conjugate_gradient_solver.jl
@@ -1,96 +1,69 @@
+using Oceananigans.Solvers: solve!
 using Statistics
 
-@kernel function implicit_η!(grid, f, implicit_η_f)
-    ### Not sure what to call this
-    ### it is for left hand side operator in
-    ### (-g∇ₕ² + 1/Δt )ϕⁿ⁺¹ = ϕⁿ / Δt + ∇ₕHUˢᵗᵃʳ
-
-    #
-    # g= model.free_surface.gravitational_acceleration
-    #
-    g = 9.81
-
-    #
-    # Δt= simulation.Δt
-    #
-    Δt = 9.81
-
-    i, j, k = @index(Global, NTuple)
-    @inbounds implicit_η_f[i, j] = -g * ∇²ᶜᶜᶜ(i, j, grid, f) + f[i, j] / Δt
-
-    # need this for 2d vertically integrated ∇²hᶜᶜᵃ
+function identity_operator!(b, x)
+    parent(b) .= parent(x)
+    return nothing
 end
 
-@kernel function divergence!(grid, u, v, w, div)
-    i, j, k = @index(Global, NTuple)
-    @inbounds div[i, j, k] = divᶜᶜᶜ(i, j, k, grid, u, v, w)
+function run_identity_operator_test(arch, grid)
+    b = Field(Center, Center, Center, arch, grid)
+
+    solver = PreconditionedConjugateGradientSolver(identity_operator!, template_field = b)
+
+    initial_guess = solution = similar(b)
+    set!(initial_guess, (x, y, z) -> rand())
+
+    solve!(initial_guess, solver, b)
+
+    @test norm(solution) .< solver.tolerance
 end
 
-function run_pcg_solver_tests(arch)
-    Lx, Ly, Lz = 4e6, 6e6, 1
-    Nx, Ny, Nz = 100, 150, 1
-    grid = RegularRectilinearGrid(size=(Nx, Ny, Nz), extent=(Lx, Ly, Lz))
+function run_poisson_equation_test(arch, grid)
+    # Solve ∇²ϕ = r
+    ϕ_truth = Field(Center, Center, Center, arch, grid)
 
-    function Amatrix_function!(result, x, arch, grid, bcs; args...)
-        event = launch!(arch, grid, :xyz, ∇²!, grid, x, result, dependencies=Event(device(arch)))
-        wait(device(arch), event)
-        fill_halo_regions!(result, arch)
-        return nothing
-    end
+    # Initialize zero-mean "truth" solution with random numbers
+    set!(ϕ_truth, (x, y, z) -> rand())
+    parent(ϕ_truth) .-= mean(ϕ_truth)
+    fill_halo_regions!(ϕ_truth, arch)
 
-    # Fields for flow, divergence of flow, RHS, and potential to make non-divergent, ϕ
-    velocities = VelocityFields(arch, grid)
-    RHS        = CenterField(arch, grid)
-    ϕ          = CenterField(arch, grid)
+    # Calculate Laplacian of "truth"
+    ∇²ϕ = r = Field(Center, Center, Center, arch, grid)
+    compute_∇²!(∇²ϕ, ϕ_truth, arch, grid)
 
-    # Set divergent flow and calculate divergence
-    u, v, w  = velocities
+    solver = PreconditionedConjugateGradientSolver(compute_∇²!, template_field=ϕ_truth)
 
-    imid = Int(floor(grid.Nx / 2)) + 1
-    jmid = Int(floor(grid.Ny / 2)) + 1
-    CUDA.@allowscalar u.data[imid, jmid, 1] = 1
+    # Solve Poisson equation
+    ϕ_solution = Field(Center, Center, Center, arch, grid)
+    solve!(ϕ_solution, solver, r, arch, grid)
 
-    fill_halo_regions!(u, arch)
+    # Diagnose Laplacian of solution
+    ∇²ϕ_solution = Field(Center, Center, Center, arch, grid)
+    compute_∇²!(∇²ϕ_solution, ϕ_solution, arch, grid)
 
-    event = launch!(arch, grid, :xyz, divergence!, grid, u, v, w, RHS, dependencies=Event(device(arch)))
-    wait(device(arch), event)
-
-    fill_halo_regions!(RHS, arch)
-
-    pcg_params = (
-        PCmatrix_function = nothing,
-        Amatrix_function = Amatrix_function!,
-        Template_field = RHS,
-        maxit = 1000, # grid.Nx * grid.Ny,
-        tol = 1.e-13
-    )
-
-    pcg_solver = PreconditionedConjugateGradientSolver(arch = arch, parameters = pcg_params)
-
-    # Set initial guess and solve
-    parent(ϕ) .= 0
-    @time solve_poisson_equation!(pcg_solver, RHS, ϕ; worda="boo", wordb="cat")
-
-    # Compute ∇² of solution
-    result = similar(ϕ)
-
-    event = launch!(arch, grid, :xyz, ∇²!, grid, ϕ, result, dependencies=Event(device(arch)))
-    wait(device(arch), event)
-
-    fill_halo_regions!(result, arch)
+    # Test
+    extrema_tolerance = 1e-12
+    std_tolerance = 1e-13
 
     CUDA.@allowscalar begin
-        @test abs(minimum(result[1:Nx, 1:Ny, 1] .- RHS.data[1:Nx, 1:Ny, 1])) < 1e-12
-        @test abs(maximum(result[1:Nx, 1:Ny, 1] .- RHS.data[1:Nx, 1:Ny, 1])) < 1e-12
-        @test std(result[1:Nx, 1:Ny, 1] .- RHS.data[1:Nx, 1:Ny, 1]) < 1e-14
+        @test minimum(abs, interior(∇²ϕ_solution) .- interior(∇²ϕ)) < extrema_tolerance
+        @test maximum(abs, interior(∇²ϕ_solution) .- interior(∇²ϕ)) < extrema_tolerance
+        @test          std(interior(∇²ϕ_solution) .- interior(∇²ϕ)) < std_tolerance
+
+        @test   minimum(abs, interior(ϕ_solution) .- interior(ϕ_truth)) < extrema_tolerance
+        @test   maximum(abs, interior(ϕ_solution) .- interior(ϕ_truth)) < extrema_tolerance
+        @test            std(interior(ϕ_solution) .- interior(ϕ_truth)) < std_tolerance
     end
 
     return nothing
 end
 
-@testset "Conjugate gradient solvers" begin
+@testset "PreconditionedConjugateGradientSolver" begin
     for arch in archs
-        @info "Testing conjugate gradient solvers [$(typeof(arch))]..."
-        run_pcg_solver_tests(arch)
+        @info "Testing PreconditionedConjugateGradientSolver [$(typeof(arch))]..."
+        grid = RegularRectilinearGrid(size=(4, 8, 4), extent=(1, 3, 1))
+        run_identity_operator_test(arch, grid)
+        run_poisson_equation_test(arch, grid)
     end
 end
