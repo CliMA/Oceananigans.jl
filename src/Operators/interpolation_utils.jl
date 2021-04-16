5b9bd1a122ac0f5a0d3e7a1d0611f47b99b7a693
Gregory L. Wagner <wagner.greg@gmail.com>
2021-04-16 09:07:59 -0700

    Uses a closure to define identity interpolation

--- src/Operators/interpolation_utils.jl
+++ src/Operators/interpolation_utils.jl
@@ -1,15 +1,6 @@
 using Oceananigans.Utils: instantiate
 using Oceananigans.Grids: Face, Center
 
-import Base: identity
-
-@inline identity(i, j, k, grid, c) = @inbounds c[i, j, k]
-@inline identity(i, j, k, grid, a::Number) = a
-
-"""Evaluate the function `F` with signature `F(i, j, k, grid, args...)` at index `i, j, k` without
-interpolation."""
-@inline identity(i, j, k, grid, F::TF, args...) where TF<:Function = F(i, j, k, grid, args...)
-
 # Utilities for inferring the interpolation function needed to
 # interpolate a field from one location to the next.
 interpolation_code(from, to) = interpolation_code(to)
@@ -44,6 +35,10 @@ function interpolation_operator(from, to)
     from, to = instantiate.(from), instantiate.(to)
     x, y, z = (interpolation_code(X, Y) for (X, Y) in zip(from, to))
 
+    @inline identity(i, j, k, grid, c) = @inbounds c[i, j, k]
+    @inline identity(i, j, k, grid, a::Number) = a
+    @inline identity(i, j, k, grid, F::TF, args...) where TF<:Function = F(i, j, k, grid, args...)
+    
     if all(ξ === :ᵃ for ξ in (x, y, z))
         return identity
     else
@@ -57,7 +52,12 @@ end
 Return the `identity` interpolator function. This is needed to obtain the interpolation
 operator for fields that have no intrinsic location, like numbers or functions.
 """
-interpolation_operator(::Nothing, to) = identity
+function interpolation_operator(::Nothing, to)
+    @inline identity(i, j, k, grid, c) = @inbounds c[i, j, k]
+    @inline identity(i, j, k, grid, a::Number) = a
+    @inline identity(i, j, k, grid, F::TF, args...) where TF<:Function = F(i, j, k, grid, args...)
+    return identity
+end
 
 assumed_field_location(name) = name === :u  ? (Face, Center, Center) :
                                name === :v  ? (Center, Face, Center) :
