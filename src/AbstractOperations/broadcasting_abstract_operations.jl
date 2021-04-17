8be96712a0659de87a40454b41c028d2187d6400
Gregory Wagner <wagner.greg@gmail.com>
2021-04-16 22:08:51 -0400

    Needed eval after all

--- src/AbstractOperations/broadcasting_abstract_operations.jl
+++ src/AbstractOperations/broadcasting_abstract_operations.jl
@@ -6,10 +6,10 @@ import Base.Broadcast: broadcasted
 
 for op in binary_operators
     O = typeof(op)
-    broadcasted(::FieldBroadcastStyle, op::O, a, b) = op(a, b)
+    @eval broadcasted(::FieldBroadcastStyle, op::$O, a, b) = op(a, b)
 end
 
 for op in multiary_operators
     O = typeof(op)
-    broadcasted(::FieldBroadcastStyle, op::O, a, b, c, d...) = op(a, b, c, d...)
+    @eval broadcasted(::FieldBroadcastStyle, op::$O, a, b, c, d...) = op(a, b, c, d...)
 end
