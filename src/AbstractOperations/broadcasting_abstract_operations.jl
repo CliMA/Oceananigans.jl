482f01a690d488b5792e36915fd2d6fdc5153b62
Gregory Wagner <wagner.greg@gmail.com>
2021-04-16 22:18:42 -0400

    AbstractOperations lists are symbols?

--- src/AbstractOperations/broadcasting_abstract_operations.jl
+++ src/AbstractOperations/broadcasting_abstract_operations.jl
@@ -5,11 +5,11 @@ using Base.Broadcast: Broadcasted
 import Base.Broadcast: broadcasted
 
 for op in binary_operators
-    O = typeof(op)
+    O = typeof(eval(op))
     @eval broadcasted(::FieldBroadcastStyle, op::$O, a, b) = op(a, b)
 end
 
 for op in multiary_operators
-    O = typeof(op)
+    O = typeof(eval(op))
     @eval broadcasted(::FieldBroadcastStyle, op::$O, a, b, c, d...) = op(a, b, c, d...)
 end
