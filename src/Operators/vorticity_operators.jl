cc188504dc27b46a95daf399a796cab07dc3621c
ali.hh.ramadhan@gmail.com <ali.hh.ramadhan@gmail.com>
2021-04-20 13:05:23 -0400

    Make `Γᶠᶠᵃ` operator look more like a line integral

--- src/Operators/vorticity_operators.jl
+++ src/Operators/vorticity_operators.jl
@@ -1,5 +1,5 @@
 """ Vertical circulation associated with horizontal velocities u, v. """
-@inline Γᶠᶠᵃ(i, j, k, grid, u, v) = δxᶠᵃᵃ(i, j, k, grid, Δy_vᶜᶠᵃ, v) - δyᵃᶠᵃ(i, j, k, grid, Δx_uᶠᶜᵃ, u)
+@inline Γᶠᶠᵃ(i, j, k, grid, u, v) = Δy_vᶜᶠᵃ(i, j, k, grid, v) - Δy_vᶜᶠᵃ(i-1, j, k, grid, v) - Δx_uᶠᶜᵃ(i, j, k, grid, u) + Δx_uᶠᶜᵃ(i, j-1, k, grid, u)
 
 """ Vertical vorticity associated with horizontal velocities u, v. """
 @inline ζ₃ᶠᶠᵃ(i, j, k, grid, u, v) = Γᶠᶠᵃ(i, j, k, grid, u, v) / Azᶠᶠᵃ(i, j, k, grid)
@@ -12,22 +12,22 @@
 @inline function Γᶠᶠᵃ(i, j, k, grid::ConformalCubedSphereFaceGrid, u, v)
     # South-west corner
     if i == 1 && j == 1
-        return Δy_vᶜᶠᵃ(i, j, k, grid, v) - δyᵃᶠᵃ(i, j, k, grid, Δx_uᶠᶜᵃ, u)
+        return Δy_vᶜᶠᵃ(i, j, k, grid, v) - Δx_uᶠᶜᵃ(i, j, k, grid, u) + Δx_uᶠᶜᵃ(i, j-1, k, grid, u)
 
     # South-east corner
     elseif i == grid.Nx+1 && j == 1
-        return - Δy_vᶜᶠᵃ(i, j-1, k, grid, v) - δyᵃᶠᵃ(i, j, k, grid, Δx_uᶠᶜᵃ, u)
+        return - Δy_vᶜᶠᵃ(i, j-1, k, grid, v) - Δx_uᶠᶜᵃ(i, j, k, grid, u) + Δx_uᶠᶜᵃ(i, j-1, k, grid, u)
 
     # North-west corner
     elseif i == 1 && j == grid.Ny+1
-        return Δy_vᶜᶠᵃ(i, j, k, grid, v) - δyᵃᶠᵃ(i, j, k, grid, Δx_uᶠᶜᵃ, u)
+        return Δy_vᶜᶠᵃ(i, j, k, grid, v) - Δx_uᶠᶜᵃ(i, j, k, grid, u) + Δx_uᶠᶜᵃ(i, j-1, k, grid, u)
 
     # North-east corner
     elseif i == grid.Nx+1 && j == grid.Ny+1
-        return - Δy_vᶜᶠᵃ(i-1, j, k, grid, v) - δyᵃᶠᵃ(i, j, k, grid, Δx_uᶠᶜᵃ, u)
+        return - Δy_vᶜᶠᵃ(i-1, j, k, grid, v) - Δx_uᶠᶜᵃ(i, j, k, grid, u) + Δx_uᶠᶜᵃ(i, j-1, k, grid, u)
 
     # Not a corner
     else
-        return δxᶠᵃᵃ(i, j, k, grid, Δy_vᶜᶠᵃ, v) - δyᵃᶠᵃ(i, j, k, grid, Δx_uᶠᶜᵃ, u)
+        return Δy_vᶜᶠᵃ(i, j, k, grid, v) - Δy_vᶜᶠᵃ(i-1, j, k, grid, v) - Δx_uᶠᶜᵃ(i, j, k, grid, u) + Δx_uᶠᶜᵃ(i, j-1, k, grid, u)
     end
 end
