fe8cccf344b027ed314cc973480390e63e63e7d5
ali.hh.ramadhan@gmail.com <ali.hh.ramadhan@gmail.com>
2021-04-20 13:52:30 -0400

    Clean up DataDeps usage a bit

--- test/data_dependencies.jl
+++ test/data_dependencies.jl
@@ -4,8 +4,7 @@ ENV["DATADEPS_ALWAYS_ACCEPT"] = "true"
 
 dd = DataDep("cubed_sphere_32_grid",
     "Conformal cubed sphere grid with 32×32 grid points on each face",
-    "https://github.com/CliMA/OceananigansArtifacts.jl/raw/main/cubed_sphere_grids/cubed_sphere_32_grid.jld2",
-    "b1dafe4f9142c59a2166458a2def743cd45b20a4ed3a1ae84ad3a530e1eff538" # sha256sum
+    "https://github.com/CliMA/OceananigansArtifacts.jl/raw/main/cubed_sphere_grids/cubed_sphere_32_grid.jld2"
 )
 
 DataDeps.register(dd)
