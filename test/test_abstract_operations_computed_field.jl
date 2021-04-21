a0e65e36bde61c66487aa450458438ae9a8d7f93
Gregory Wagner <wagner.greg@gmail.com>
2021-04-21 06:45:19 -0700

    Test explicit set of unaries

--- test/test_abstract_operations_computed_field.jl
+++ test/test_abstract_operations_computed_field.jl
@@ -410,8 +410,7 @@ end
 
         @testset "Unary operations and derivatives [$FT]" begin
             for ψ in (u, v, w, c)
-                for op_symbol in Oceananigans.AbstractOperations.unary_operators
-                    op = eval(op_symbol)
+                for op in (sqrt, sin, cos, exp, tanh)
                     @test typeof(op(ψ)[2, 2, 2]) <: Number
                 end
 
