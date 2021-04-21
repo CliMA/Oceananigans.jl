cbc854ee0e1c5b72d3c832cd89f51970c0c7c74e
Gregory Wagner <wagner.greg@gmail.com>
2021-04-21 09:08:19 -0400

    Updates broadcasting tests

--- test/test_broadcasting.jl
+++ test/test_broadcasting.jl
@@ -28,24 +28,34 @@
         a2 = CenterField(arch, three_point_grid)
         b2 = ZFaceField(arch, three_point_grid)
         b2 .= 1
-        fill_halo_regions!(b2, arch)
+        fill_halo_regions!(b2, arch) # sets b2[1, 1, 1] = b[1, 1, 4] = 0
 
-        @test b2.data[1, 1, 1] == 0
-        @test b2.data[1, 1, 2] == 1
-        @test b2.data[1, 1, 3] == 1
-        @test b2.data[1, 1, 4] == 0
+        @test b2[1, 1, 1] == 0
+        @test b2[1, 1, 2] == 1
+        @test b2[1, 1, 3] == 1
+        @test b2[1, 1, 4] == 0
 
         a2 .= b2
-        @test a2.data[1, 1, 1] = 0.5
-        @test a2.data[1, 1, 2] = 1.0
-        @test a2.data[1, 1, 3] = 0.5
+        @test a2[1, 1, 1] == 0.5
+        @test a2[1, 1, 2] == 1.0
+        @test a2[1, 1, 3] == 0.5
 
+        a2 .= b2 .+ 1
+        @test a2[1, 1, 1] == 1.5
+        @test a2[1, 1, 2] == 2.0
+        @test a2[1, 1, 3] == 1.5
+
+        # Broadcasting with ReducedField
         r, p, q = [ReducedField(Center, Center, Nothing, arch, grid, dims=3) for i = 1:3]
 
         r .= 2 
         @test all(r .== 2) 
 
         p .= 3 
+
+        q .= r .* p
+        @test all(q .== 6) 
+
         q .= r .* p .+ 1
         @test all(q .== 7) 
     end
