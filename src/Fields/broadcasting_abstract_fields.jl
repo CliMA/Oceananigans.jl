6111e36e1025adeda382ecd40880779e6471bc55
Gregory Wagner <wagner.greg@gmail.com>
2021-04-21 00:51:28 -0400

    Infer whether broadcast requires interpolation or not

--- src/Fields/broadcasting_abstract_fields.jl
+++ src/Fields/broadcasting_abstract_fields.jl
@@ -7,15 +7,11 @@ using Base.Broadcast: Broadcasted
 
 struct FieldBroadcastStyle <: Broadcast.AbstractArrayStyle{3} end
 
-# This code defines BroadcastStyle for AbstractField and associated paraphernalia.
-# It causes problems for simple cases so it's commented out right now.
-# However, we need to use this machinery to ensure proper interpolation between
-# fields at different locations on the staggered grid, so we keep it here, commented out,
-# in the hope that it will be employed someday.
-
+# Preserve location for broadcast style
 Base.Broadcast.BroadcastStyle(::Type{<:AbstractField}) = FieldBroadcastStyle()
-Base.Broadcast.BroadcastStyle(::Type{<:AbstractReducedField}) = FieldBroadcastStyle()
-Base.Broadcast.BroadcastStyle(::FieldBroadcastStyle, b::DefaultArrayStyle{N}) where N = DefaultArrayStyle{3}()
+
+# Precedence rules
+Base.Broadcast.BroadcastStyle(::FieldBroadcastStyle, ::DefaultArrayStyle{N}) where N = DefaultArrayStyle{3}()
 
 "`A = find_field(As)` returns the first AbstractField among the arguments."
 find_field(bc::Broadcasted) = find_field(bc.args)
@@ -32,7 +28,9 @@ function Base.similar(bc::Broadcasted{FieldBroadcastStyle}, ::Type{<:AbstractFlo
     return similar(field)
 end
 
-using Base.Broadcast: Broadcasted
+#####
+##### Kernels
+#####
 
 @kernel function broadcast_xyz!(dest, bc)
     i, j, k = @index(Global, NTuple)
@@ -80,20 +78,33 @@ broadcasted_to_abstract_operation(loc, grid, a) = a
 broadcasted_to_abstract_operation(loc, grid, bc::Broadcasted{<:Any, <:Any, <:Any, <:Any}) =
     bc.f(loc, Tuple(broadcasted_to_abstract_operation(loc, grid, a) for a in bc.args)...)
 
+#####
+##### Hopefully we don't need to interpolate, but if we do...
+#####
+
+needs_interpolation(Xa, Xb) = false
+needs_interpolation(::Type{Center}, ::Type{Face}) = true
+needs_interpolation(::Type{Face}, ::Type{Center}) = true
+
+needs_interpolation(La::Tuple, Lb::Tuple) = any(needs_interpolation.(La, Lb))
+needs_interpolation(La::Tuple, b::AbstractField) = needs_interpolation(La, location(b))
+needs_interpolation(La::Tuple, ::Number) = false
+needs_interpolation(La::Tuple, ::AbstractArray) = false
+needs_interpolation(La::Tuple, bc::Broadcasted) = any(needs_interpolation(La, b) for b in bc.args)
+
+# Non-interpolated broadcasting to Field
 @inline function Base.copyto!(dest::AbstractField{X, Y, Z}, bc::Broadcasted{Nothing}) where {X, Y, Z}
 
-    # Is this needed?
-    # bc′ = Broadcast.preprocess(dest, bc)
+    # It's probably best if you don't need interpolation, but anyways...
+    bc′ = needs_interpolation(dest, bc) ? broadcasted_to_abstract_operation(location(dest), grid, bc) :
+                                          Base.Broadcast.preprocess(dest, bc)
 
     grid = dest.grid
     arch = architecture(dest)
     config = launch_configuration(dest)
     kernel = broadcast_kernel(dest)
 
-    # Convert Broadcasted to AbstractOperation
-    op = broadcasted_to_abstract_operation(location(dest), grid, bc)
-
-    event = launch!(arch, grid, config, kernel, dest, op,
+    event = launch!(arch, grid, config, kernel, dest, bc′,
                     include_right_boundaries = true,
                     location = (X, Y, Z))
 
