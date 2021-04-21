0a9cbe672b0fcaa70860b4fbdd91293d76810489
Gregory Wagner <wagner.greg@gmail.com>
2021-04-21 09:07:56 -0400

    Polish off interpolation for broadcasting

--- src/Fields/broadcasting_abstract_fields.jl
+++ src/Fields/broadcasting_abstract_fields.jl
@@ -11,24 +11,15 @@ struct FieldBroadcastStyle <: Broadcast.AbstractArrayStyle{3} end
 # and result array inference for allocating broadcast operations is figured out.
 Base.Broadcast.BroadcastStyle(::Type{<:AbstractField}) = FieldBroadcastStyle()
 
-# Precedence rules
-Base.Broadcast.BroadcastStyle(::FieldBroadcastStyle, ::DefaultArrayStyle{N}) where N = DefaultArrayStyle{3}()
-Base.Broadcast.BroadcastStyle(::FieldBroadcastStyle, ::FieldBroadcastStyle) = FieldBroadcastStyle()
+# Precedence rule
+Base.Broadcast.BroadcastStyle(::FieldBroadcastStyle, ::DefaultArrayStyle{N}) where N = FieldBroadcastStyle()
 
-"`A = find_field(As)` returns the first AbstractField among the arguments."
-find_field(bc::Broadcasted) = find_field(bc.args)
-find_field(args::Tuple) = find_field(find_field(args[1]), Base.tail(args))
-find_field(x) = x
-find_field(::Tuple{}) = nothing
-find_field(a::AbstractField, rest) = a
-find_field(::Any, rest) = find_field(rest)
-
+# For use in Base.copy when broadcasting with numbers and arrays (useful for comparisons like f::AbstractField .== 0)
 Base.similar(bc::Broadcasted{FieldBroadcastStyle}, ::Type{ElType}) where ElType = similar(Array{ElType}, axes(bc))
 
-function Base.similar(bc::Broadcasted{FieldBroadcastStyle}, ::Type{<:AbstractFloat})
-    field = find_field(bc)
-    return similar(field)
-end
+# Bypass style combining for in-place broadcasting with arrays / scalars to use built-in broadcasting machinery
+@inline Base.Broadcast.materialize!(dest::AbstractField, bc::Broadcasted{<:DefaultArrayStyle}) =
+    Base.Broadcast.materialize!(DefaultArrayStyle{3}(), dest, bc)
 
 #####
 ##### Kernels
@@ -73,9 +64,6 @@ launch_configuration(::AbstractReducedField{<:Loc, <:Loc, Nothing}) = :xy
 
 broadcasted_to_abstract_operation(loc, grid, a) = a
 
-broadcasted_to_abstract_operation(loc, grid, bc::Broadcasted{<:Any, <:Any, <:Any, <:Any}) =
-    bc.f(loc, Tuple(broadcasted_to_abstract_operation(loc, grid, a) for a in bc.args)...)
-
 #####
 ##### Hopefully we don't need to interpolate, but if we do...
 #####
@@ -91,22 +79,19 @@ needs_interpolation(La::Tuple, ::AbstractArray) = false
 needs_interpolation(La::Tuple, bc::Broadcasted) = any(needs_interpolation(La, b) for b in bc.args)
 
 # Broadcasting with interpolation breaks Base's default rules for ABroBbstractOperations 
-@inline Base.Broadcast.materialize!(::Base.Broadcast.BroadcastStyle, dest::AbstractField, bc::Broadcasted) =
-    copyto!(dest, convert(Broadcasted{Nothing}, bc))
+@inline Base.Broadcast.materialize!(::Base.Broadcast.BroadcastStyle,
+                                    dest::AbstractField,
+                                    bc::Broadcasted{<:FieldBroadcastStyle}) = copyto!(dest, convert(Broadcasted{Nothing}, bc))
 
 # Non-interpolated broadcasting to Field
 @inline function Base.copyto!(dest::AbstractField{X, Y, Z}, bc::Broadcasted{Nothing}) where {X, Y, Z}
 
-    # It's probably best if you don't need interpolation, but anyways...
-    #bc′ = needs_interpolation(dest, bc) ? broadcasted_to_abstract_operation(location(dest), grid, bc) :
-    #                                      Base.Broadcast.preprocess(dest, bc)
-                                            
     grid = dest.grid
     arch = architecture(dest)
     config = launch_configuration(dest)
     kernel = broadcast_kernel(dest)
 
-    @show bc′ = broadcasted_to_abstract_operation(location(dest), grid, bc)
+    bc′ = broadcasted_to_abstract_operation(location(dest), grid, bc)
 
     event = launch!(arch, grid, config, kernel, dest, bc′,
                     include_right_boundaries = true,
