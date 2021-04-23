71f0543364bd5352a1274b68062817742a407ee5
Gregory Wagner <wagner.greg@gmail.com>
2021-04-23 11:04:46 -0700

    Adds area metrics and volume metrics

--- src/AbstractOperations/grid_metrics.jl
+++ src/AbstractOperations/grid_metrics.jl
@@ -1,38 +1,61 @@
 using Oceananigans.Operators
 
-# Define aliases for metrics at all locations.
+# Define aliases for some spacings and areas at all locations.
 for LX in (:ᶜ, :ᶠ), LY in (:ᶜ, :ᶠ), LZ in (:ᶜ, :ᶠ)
 
     x_spacing_alias = Symbol(:Δx, LX, LY, LZ)
     y_spacing_alias = Symbol(:Δy, LX, LY, LZ)
     z_spacing_alias = Symbol(:Δz, LX, LY, LZ)
 
+    z_area_alias = Symbol(:Az, LX, LY, LZ)
+
     x_spacing_function = Symbol(:Δx, LX, LY, :ᵃ)
     y_spacing_function = Symbol(:Δy, LX, LY, :ᵃ)
     z_spacing_function = Symbol(:Δz, :ᵃᵃ, LZ)
+
+    z_area_function = Symbol(:Az, LX, LY, :ᵃ)
 
     @eval begin
         const $x_spacing_alias = $x_spacing_function
         const $y_spacing_alias = $y_spacing_function
         const $z_spacing_alias = $z_spacing_function
+        const $z_area_alias = $z_area_function
     end
 end
 
 # Prototype functionality with x, y, z spacings
 abstract type AbstractGridMetric end
 
-struct XSpacing <: AbstractGridMetric end 
-struct YSpacing <: AbstractGridMetric end 
-struct ZSpacing <: AbstractGridMetric end 
+struct XSpacingMetric <: AbstractGridMetric end 
+struct YSpacingMetric <: AbstractGridMetric end 
+struct ZSpacingMetric <: AbstractGridMetric end 
 
-metric_function_prefix(::XSpacing) = :Δx
-metric_function_prefix(::YSpacing) = :Δy
-metric_function_prefix(::ZSpacing) = :Δz
+metric_function_prefix(::XSpacingMetric) = :Δx
+metric_function_prefix(::YSpacingMetric) = :Δy
+metric_function_prefix(::ZSpacingMetric) = :Δz
+
+struct XAreaMetric <: AbstractGridMetric end 
+struct YAreaMetric <: AbstractGridMetric end 
+struct ZAreaMetric <: AbstractGridMetric end 
+
+metric_function_prefix(::XAreaMetric) = :Ax
+metric_function_prefix(::YAreaMetric) = :Ay
+metric_function_prefix(::ZAreaMetric) = :Az
+
+struct VolumeMetric <: AbstractGridMetric end 
+
+metric_function_prefix(::Volume) = :V
 
 # Convenient instances for users
-const Δx = XSpacing()
-const Δy = YSpacing()
-const Δz = ZSpacing()
+const Δx = XSpacingMetric()
+const Δy = YSpacingMetric()
+const Δz = ZSpacingMetric()
+
+const Ax = XAreaMetric()
+const Ay = YAreaMetric()
+const Az = ZAreaMetric()
+
+const Volume = VolumeMetric()
 
 """
     metric_function(loc, metric::AbstractGridMetric)
