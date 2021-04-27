f7408dc08c30ec6a3ced11d70d3458634f0fd071
Gregory L. Wagner <wagner.greg@gmail.com>
2021-04-26 18:27:01 -0700

    Miscellaneous updates to get ConformalCubedSphereGrid simulations running on the GPU (#1608)
    * ConformalCubedSphereGrid and friends running on the GPU...
    
    * Uncomment exchange bcs test
    
    * Updates hydrostatic benchmarks for GPU
    
    * Removes vestigial on_architecture
    
    * Some cleanup and changes all xnode, ynode, znode to use instantiated types
    
    * Goodbye xC yC zC
    
    * Use znode rather than zF in nonlinear equation of state
    
    * Adapt FunctionField grid
    
    * Dont fill halo regions after broadcasting (for now)
    
    * Updates test_field.jl
    
    * Fixes geopotential height function for nonlinear equation of state
    
    * Adds manual fill halo regions in broadcast test
    
    * Allow scalar operations for broadcast of ==
    
    * Imports node into continuous_forcing
    
    * Use AbstractCPUArchitecture etc in zeros instead of CPU
    
    * Use interior(field) in implicit free surface solver test
    
    * Special set! for fields on the same architecture
    
    * Dont test GPU cubed sphere in CPU tests
    
    * Fill eta halos before computing free surface linear operation
    
    * Fixes node for reduced locations
    
    * Free surfaces are two-dimensional
    
    * Update benchmark_hydrostatic_model.jl
    
    * Update fill_halo_regions.jl
    
    * Update cubed_sphere_halo_filling.jl
    
    * Update cubed_sphere_set!.jl
    
    * When StackOverflow doesnt help
    
    * Decompose arguments to fill halo regions into faces

--- benchmark/benchmark_hydrostatic_model.jl
+++ benchmark/benchmark_hydrostatic_model.jl
@@ -19,22 +19,27 @@ DataDeps.register(dd)
 
 # All grids have 6 * 32^2 = 6144 grid points.
 grids = Dict(
-    :RegularRectilinearGrid => RegularRectilinearGrid(size=(128, 48, 1), extent=(1, 1, 1)),
-    :RegularLatitudeLongitudeGrid => RegularLatitudeLongitudeGrid(size=(128, 48, 1), longitude=(-180, 180), latitude=(-80, 80), z=(-1, 0)),
-    :ConformalCubedSphereFaceGrid => ConformalCubedSphereFaceGrid(size=(128, 48, 1), z=(-1, 0)),
-    :ConformalCubedSphereGrid => ConformalCubedSphereGrid(datadep"cubed_sphere_32_grid/cubed_sphere_32_grid.jld2", Nz=1, z=(-1, 0))
+     (CPU, :RegularRectilinearGrid)       => RegularRectilinearGrid(size=(128, 48, 1), extent=(1, 1, 1)),
+     (CPU, :RegularLatitudeLongitudeGrid) => RegularLatitudeLongitudeGrid(size=(128, 48, 1), longitude=(-180, 180), latitude=(-80, 80), z=(-1, 0)),
+     (CPU, :ConformalCubedSphereFaceGrid) => ConformalCubedSphereFaceGrid(size=(128, 48, 1), z=(-1, 0)),
+     (CPU, :ConformalCubedSphereGrid)     => ConformalCubedSphereGrid(datadep"cubed_sphere_32_grid/cubed_sphere_32_grid.jld2", Nz=1, z=(-1, 0)),
+     (GPU, :RegularRectilinearGrid)       => RegularRectilinearGrid(size=(128, 48, 1), extent=(1, 1, 1)),
+     (GPU, :RegularLatitudeLongitudeGrid) => RegularLatitudeLongitudeGrid(size=(128, 48, 1), longitude=(-180, 180), latitude=(-80, 80), z=(-1, 0)),
+     # Uncomment when ConformalCubedSphereFaceGrids of any size can be built natively without loading from file:
+     # (GPU, :ConformalCubedSphereFaceGrid) => ConformalCubedSphereFaceGrid(size=(128, 48, 1), z=(-1, 0), architecture=GPU()),
+     (GPU, :ConformalCubedSphereGrid)     => ConformalCubedSphereGrid(datadep"cubed_sphere_32_grid/cubed_sphere_32_grid.jld2", Nz=1, z=(-1, 0), architecture=GPU()),
 )
 
 free_surfaces = Dict(
     :ExplicitFreeSurface => ExplicitFreeSurface(),
-    :ImplicitFreeSurface => ImplicitFreeSurface(maximum_iterations=1, tolerance=-Inf)  # Force it to take exactly 1 iteration.
+    :ImplicitFreeSurface => ImplicitFreeSurface(maximum_iterations=1, tolerance=-Inf) # Force it to take exactly 1 iteration.
 )
 
 function benchmark_hydrostatic_model(Arch, grid_type, free_surface_type)
 
     model = HydrostaticFreeSurfaceModel(
               architecture = Arch(),
-                      grid = grids[grid_type],
+                      grid = grids[(Arch, grid_type)],
         momentum_advection = VectorInvariant(),
               free_surface = free_surfaces[free_surface_type]
     )
@@ -55,13 +60,15 @@ Architectures = has_cuda() ? [CPU, GPU] : [CPU]
 grid_types = [
     :RegularRectilinearGrid,
     :RegularLatitudeLongitudeGrid,
-    :ConformalCubedSphereFaceGrid,
+    # Uncomment when ConformalCubedSphereFaceGrids of any size can be built natively without loading from file:
+    # :ConformalCubedSphereFaceGrid,
     :ConformalCubedSphereGrid
 ]
 
 free_surface_types = [
     :ExplicitFreeSurface,
-    :ImplicitFreeSurface
+    # ImplicitFreeSurface doesn't yet work on MultiRegionGrids like the ConformalCubedSphereGrid:
+    # :ImplicitFreeSurface
 ]
 
 # Run and summarize benchmarks
---
+++ src/Grids/new_data.jl
@@ -0,0 +1,67 @@
+using Oceananigans.Grids: total_length, topology
+
+using OffsetArrays: OffsetArray
+
+#####
+##### Creating offset arrays for field data by dispatching on architecture.
+#####
+
+"""
+Return a range of indices for a field located at `Center` centers
+`along a grid dimension of length `N` and with halo points `H`.
+"""
+offset_indices(loc, topo, N, H=0) = 1 - H : N + H
+
+"""
+Return a range of indices for a field located at cell `Face`s
+`along a grid dimension of length `N` and with halo points `H`.
+"""
+offset_indices(::Type{Face}, ::Type{Bounded}, N, H=0) = 1 - H : N + H + 1
+
+"""
+Return a range of indices for a field along a 'reduced' dimension.
+"""
+offset_indices(::Type{Nothing}, topo, N, H=0) = 1 : 1
+
+function offset_data(underlying_data::AbstractArray{FT, 3}, loc, topo, N, H) where FT
+    ii = offset_indices(loc[1], topo[1], N[1], H[1])
+    jj = offset_indices(loc[2], topo[2], N[2], H[2])
+    kk = offset_indices(loc[3], topo[3], N[3], H[3])
+
+    return OffsetArray(underlying_data, ii, jj, kk)
+end
+
+function offset_data(underlying_data::AbstractArray{FT, 2}, loc, topo, N, H) where FT
+    ii = offset_indices(loc[1], topo[1], N[1], H[1])
+    jj = offset_indices(loc[2], topo[2], N[2], H[2])
+
+    return OffsetArray(underlying_data, ii, jj)
+end
+
+"""
+    offset_data(underlying_data, grid::AbstractGrid, loc)
+
+Returns an `OffsetArray` that maps to `underlying_data` in memory,
+with offset indices appropriate for the `data` of a field on
+a `grid` of `size(grid)` and located at `loc`.
+"""
+offset_data(underlying_data, grid::AbstractGrid, loc) =
+    offset_data(underlying_data, loc, topology(grid), size(grid), halo_size(grid))
+
+"""
+    new_data([FT=Float64], arch, grid, loc)
+
+Returns an `OffsetArray` of zeros of float type `FT` on `arch`itecture,
+with indices corresponding to a field on a `grid` of `size(grid)` and located at `loc`.
+"""
+function new_data(FT, arch, grid, loc)
+    underlying_data = zeros(FT, arch,
+                            total_length(loc[1], topology(grid, 1), grid.Nx, grid.Hx),
+                            total_length(loc[2], topology(grid, 2), grid.Ny, grid.Hy),
+                            total_length(loc[3], topology(grid, 3), grid.Nz, grid.Hz))
+
+    return offset_data(underlying_data, grid, loc)
+end
+
+# Default to type of Grid
+new_data(arch, grid, loc) = new_data(eltype(grid), arch, grid, loc)
