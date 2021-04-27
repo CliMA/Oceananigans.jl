8df358e97cb699f2c061126f08d0234139b44796
Gregory Wagner <wagner.greg@gmail.com>
2021-04-27 05:07:04 -0400

    Use CUDA macro in FourierTridiagonalPoissonSolver

--- src/Solvers/fourier_tridiagonal_poisson_solver.jl
+++ src/Solvers/fourier_tridiagonal_poisson_solver.jl
@@ -46,10 +46,9 @@ function FourierTridiagonalPoissonSolver(arch, grid, planner_flag=FFTW.PATIENT)
     transforms = plan_transforms(arch, grid, sol_storage, planner_flag)
 
     # Lower and upper diagonals are the same
-    CUDA.allowscalar(true)
-    lower_diagonal = arch_array(arch, [1 / Δzᵃᵃᶠ(1, 1, k, grid) for k in 2:Nz])
+    lower_diagonal = CUDA.@allowscalar [1 / Δzᵃᵃᶠ(1, 1, k, grid) for k in 2:Nz]
+    lower_diagonal = arch_array(arch, lower_diagonal)
     upper_diagonal = lower_diagonal
-    CUDA.allowscalar(false)
 
     # Compute diagonal coefficients for each grid point
     diagonal = arch_array(arch, zeros(Nx, Ny, Nz))
