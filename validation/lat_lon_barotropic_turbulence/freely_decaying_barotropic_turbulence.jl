b521c1cbd418531d09303778f1a76aafe3daeed0
Gregory Wagner <wagner.greg@gmail.com>
2021-04-28 15:22:57 -0400

    Fixes bug in velocity field initializations and enables fast math

--- validation/lat_lon_barotropic_turbulence/freely_decaying_barotropic_turbulence.jl
+++ validation/lat_lon_barotropic_turbulence/freely_decaying_barotropic_turbulence.jl
@@ -3,6 +3,7 @@
 using Oceananigans
 using Oceananigans.Grids
 
+using Oceananigans.BoundaryConditions: fill_halo_regions!
 using Oceananigans.Fields: FunctionField
 
 using Oceananigans.Coriolis:
@@ -30,6 +31,9 @@ using Printf
 
 using Oceananigans.AbstractOperations: AbstractGridMetric, _unary_operation
 
+using CUDA
+CUDA.math_mode!(CUDA.FAST_MATH)
+
 #####
 ##### Grid
 #####
@@ -52,13 +56,13 @@ Ny = round(Int, Δφ / resolution)
 ##### Physics and model setup
 #####
 
-free_surface = ExplicitFreeSurface(gravitational_acceleration=0.2)
+free_surface = ExplicitFreeSurface(gravitational_acceleration=1.0)
 
 equator_Δx = grid.radius * deg2rad(grid.Δλ)
 diffusive_time_scale = 60days
 
 @show const νh₂₀ =        equator_Δx^2 / diffusive_time_scale
-@show const νh₄₀ = 5e-6 * equator_Δx^4 / diffusive_time_scale
+@show const νh₄₀ = 1e-6 * equator_Δx^4 / diffusive_time_scale
 
 @inline νh₂(λ, φ, z, t) = νh₂₀ * cos(π * φ / 180)
 @inline νh₄(λ, φ, z, t) = νh₄₀ * cos(π * φ / 180)
@@ -80,58 +84,51 @@ model = HydrostaticFreeSurfaceModel(grid = grid,
 ##### Initial condition
 #####
 
-g = model.free_surface.gravitational_acceleration
+ψ = Field(Face, Face, Center, model.architecture, model.grid)
+set!(ψ, (x, y, z) -> rand())
+fill_halo_regions!(ψ, model.architecture)
 
-gravity_wave_speed = sqrt(g * grid.Lz) # hydrostatic (shallow water) gravity wave speed
+u, v, w = model.velocities
 
-# Time-scale for gravity wave propagation across the smallest grid cell
-wave_propagation_time_scale = min(grid.radius * cosd(maximum(abs, grid.φᵃᶜᵃ)) * deg2rad(grid.Δλ),
-                                  grid.radius * deg2rad(grid.Δφ)) / gravity_wave_speed
+u .= - ∂y(ψ)
+v .= + ∂x(ψ)
 
-set!(model,
-     u = (x, y, z) -> sqrt(abs(sin(π * y / 180))) * rand(),
-     v = (x, y, z) -> sqrt(abs(sin(π * y / 180))) * rand())
-
+#=
 # Zero out mean motion
 model.velocities.u .-= mean(model.velocities.u)
 model.velocities.v .-= mean(model.velocities.v)
+=#
 
-# Set target velocity to fraction of free surface velocity
-max_u = maximum(model.velocities.u)
-max_v = maximum(model.velocities.v)
-max_speed = sqrt(max_u^2 + max_v^2)
+#####
+##### Rescale velocity to fraction of free surface velocity
+#####
 
-target_speed = 0.1 * gravity_wave_speed
-model.velocities.u ./= target_speed / max_speed
-model.velocities.v ./= target_speed / max_speed
+# Time-scale for gravity wave propagation across the smallest grid cell
+g = model.free_surface.gravitational_acceleration
+gravity_wave_speed = sqrt(g * grid.Lz) # hydrostatic (shallow water) gravity wave speed
+
+minimum_Δx = grid.radius * cosd(maximum(abs, grid.φᵃᶜᵃ)) * deg2rad(grid.Δλ)
+minimum_Δy = grid.radius * deg2rad(grid.Δφ)
+wave_propagation_time_scale = min(minimum_Δx, minimum_Δy) / gravity_wave_speed
+
+@show max_u = maximum(u)
+@show max_v = maximum(v)
+max_speed_ish = sqrt(max_u^2 + max_v^2)
+
+@show target_speed = 0.5 * gravity_wave_speed
+u .*= target_speed / max_speed_ish
+v .*= target_speed / max_speed_ish
+
+@show maximum(u)
+@show maximum(v)
 
 #####
 ##### Simulation setup
 #####
 
-mutable struct Progress; interval_start_time::Float64; end
-
-function (p::Progress)(sim)
-    wall_time = (time_ns() - p.interval_start_time) * 1e-9
-
-    @info @sprintf("Time: %s, iteration: %d, max(u): %.2e m s⁻¹, wall time: %s",
-                   prettytime(sim.model.clock.time),
-                   sim.model.clock.iteration,
-                   maximum(sim.model.velocities.u),
-                   prettytime(wall_time))
-
-    p.interval_start_time = time_ns()
-
-    return nothing
-end
-
 ζ = VerticalVorticityField(model)
 compute!(ζ)
-
-polar_Δx = grid.radius * cosd(maximum(abs, grid.φᵃᶜᵃ)) * deg2rad(grid.Δλ)
-Δt = 0.1 * polar_Δx / target_speed
-
-# Max Rossby number: $(maximum(abs, Ro))
+Δt = 0.1 * minimum_Δx / target_speed
 
 @info """
     Maximum vertical vorticity: $(maximum(ζ))
@@ -140,15 +137,34 @@ polar_Δx = grid.radius * cosd(maximum(abs, grid.φᵃᶜᵃ)) * deg2rad(grid.Δ
     Time step: $(prettytime(Δt))
 """
 
+mutable struct Progress; interval_start_time::Float64; end
+
+function (p::Progress)(sim)
+    wall_time = (time_ns() - p.interval_start_time) * 1e-9
+
+    compute!(ζ)
+
+    @info @sprintf("Time: %s, iteration: %d, max(u): %.2e m s⁻¹, wall time: %s",
+                   prettytime(sim.model.clock.time),
+                   sim.model.clock.iteration,
+                   maximum(abs, ζ),
+                   prettytime(wall_time))
+
+    p.interval_start_time = time_ns()
+
+    return nothing
+end
+
+
 simulation = Simulation(model,
                         Δt = Δt,
-                        stop_time = 20year,
+                        stop_time = 2year,
                         iteration_interval = 100,
                         progress = Progress(time_ns()))
 
 output_fields = merge(model.velocities, (η=model.free_surface.η, ζ=ζ))
 
-output_prefix = "implicit_freely_decaying_barotropic_turbulence_Nx$(grid.Nx)_Ny$(grid.Ny)"
+output_prefix = "freely_decaying_barotropic_turbulence_Nx$(grid.Nx)_Ny$(grid.Ny)"
 
 simulation.output_writers[:fields] = JLD2OutputWriter(model, (ζ = ζ,),
                                                       schedule = TimeInterval(30day),
