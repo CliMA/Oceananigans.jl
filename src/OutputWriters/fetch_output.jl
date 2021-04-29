c827eb71fd9c1cc97ef75964f372b440de7e712b
Gregory L. Wagner <wagner.greg@gmail.com>
2021-04-28 23:06:49 -0700

    Supports FieldSlicer for function output that returns AbstractField
    Resolves #1629 
    
    TODO:
    
    - [ ] Perhaps a test?
    - [ ] OK from @ali-ramadhan that we bypass compute_and_slice_output for `LagrangianParticles`
    
    cc @tomchor

--- src/OutputWriters/fetch_output.jl
+++ src/OutputWriters/fetch_output.jl
@@ -1,18 +1,21 @@
 using CUDA
 
 using Oceananigans.Fields: AbstractField, compute_at!, slice_parent
-using Oceananigans.LagrangianParticleTracking: LagrangianParticles
-
-fetch_output(output, model, field_slicer) = output(model)
+using Oceananigans.LagrangianParticleTracking: LagrangianParticles'
 
 # Needed to support `fetch_output` with `model::Nothing`.
 time(model) = model.clock.time
 time(::Nothing) = nothing
 
-function fetch_output(field::AbstractField, model, field_slicer)
+function compute_and_slice_output(field::AbstractField, model, field_slicer)
     compute_at!(field, time(model))
     return slice_parent(field_slicer, field)
 end
+
+compute_and_slice_output(output, model, field_slicer) = output
+
+fetch_output(output, model, field_slicer) = compute_and_slice_output(output(model), model, field_slicer)
+fetch_output(field::AbstractField, model, field_slicer) = compute_and_slice_output(field, model, field_slicer)
 
 function fetch_output(lagrangian_particles::LagrangianParticles, model, field_slicer)
     particle_properties = lagrangian_particles.properties
