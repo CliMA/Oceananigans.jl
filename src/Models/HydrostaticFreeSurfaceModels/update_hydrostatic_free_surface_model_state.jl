d823ab73b4bdff5252c3e251fa1df5359dbdefa7
Gregory Wagner <wagner.greg@gmail.com>
2021-04-29 01:44:49 -0400

    Calculate auxiliary fields after halos are filled

--- src/Models/HydrostaticFreeSurfaceModels/update_hydrostatic_free_surface_model_state.jl
+++ src/Models/HydrostaticFreeSurfaceModels/update_hydrostatic_free_surface_model_state.jl
@@ -12,12 +12,7 @@ Update peripheral aspects of the model (auxiliary fields, halo regions, diffusiv
 hydrostatic pressure) to the current model state.
 """
 function update_state!(model::HydrostaticFreeSurfaceModel)
-
-    # Compute auxiliary fields
-    for aux_field in model.auxiliary_fields
-        compute!(aux_field)
-    end
-
+    
     # Fill halos for velocities and tracers
     fill_halo_regions!(prognostic_fields(model), model.architecture, model.clock, fields(model))
     fill_horizontal_velocity_halos!(model.velocities.u, model.velocities.v, model.architecture)
@@ -25,6 +20,11 @@ function update_state!(model::HydrostaticFreeSurfaceModel)
     compute_w_from_continuity!(model)
 
     fill_halo_regions!(model.velocities.w, model.architecture, model.clock, fields(model))
+
+    # Compute auxiliary fields
+    for aux_field in model.auxiliary_fields
+        compute!(aux_field)
+    end
 
     # Calculate diffusivities
     calculate_diffusivities!(model.diffusivities, model.architecture, model.grid, model.closure,
