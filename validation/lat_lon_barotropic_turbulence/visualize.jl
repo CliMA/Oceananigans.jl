1613c2f1b80f469f13aa4b6cefdeb6ceace0db61
Gregory Wagner <wagner.greg@gmail.com>
2021-05-03 09:50:36 -0700

    Cleans up scripts and deletes lat-lon scripts with forcing

--- validation/lat_lon_barotropic_turbulence/visualize.jl
+++ validation/lat_lon_barotropic_turbulence/visualize.jl
@@ -4,19 +4,9 @@ using Oceananigans.Utils: prettytime, hours, day, days, years
 using Statistics
 using JLD2
 using Printf
-
-Nx = 360
-Ny = 160
-
-#output_prefix = "freely_decaying_barotropic_turbulence_Nx$(Nx)_Ny$(Ny)"
-#output_prefix = "biharmonic_freely_decaying_barotropic_turbulence_Nx1440_Ny640"
-output_prefix = "biharmonic_freely_decaying_barotropic_turbulence_Nx720_Ny320"
-#output_prefix = "freely_decaying_barotropic_turbulence_Nx2160_Ny960"
-
-#=
-using Makie
 using GLMakie
-function geographic2cartesian(λ, φ, r=1)
+
+function geographic2cartesian(λ, φ; r=1)
     Nλ = length(λ)
     Nφ = length(φ)
 
@@ -32,104 +22,6 @@ function geographic2cartesian(λ, φ, r=1)
 
     return x, y, z
 end
-
-function visualize_makie(filepath)
-
-    file = jldopen(filepath)
-
-    Nx = file["grid/Nx"]
-    Ny = file["grid/Ny"]
-
-    # A spherical domain
-    grid = RegularLatitudeLongitudeGrid(size = (Nx, Ny, 1),
-                                        longitude = (-30, 30),
-                                        latitude = (15, 75),
-                                        z = (-4000, 0))
-
-    iterations = parse.(Int, keys(file["timeseries/t"]))
-
-    xu, yu, zu = geographic2cartesian(xnodes(Face,   grid), ynodes(Center, grid))
-    xv, yv, zv = geographic2cartesian(xnodes(Center, grid), ynodes(Face,   grid))
-    xc, yc, zc = geographic2cartesian(xnodes(Center, grid), ynodes(Center, grid))
-
-    iter = Node(0)
-
-    plot_title = @lift @sprintf("Barotropic gyre: time = %s", prettytime(file["timeseries/t/" * string($iter)]))
-
-    u = @lift file["timeseries/u/" * string($iter)][:, :, 1]
-    v = @lift file["timeseries/v/" * string($iter)][:, :, 1]
-    η = @lift file["timeseries/η/" * string($iter)][:, :, 1]
-
-    u = @lift file["timeseries/u/" * string($iter)][:, :, 1]
-    v = @lift file["timeseries/v/" * string($iter)][:, :, 1]
-    η = @lift file["timeseries/η/" * string($iter)][:, :, 1]
-
-    fig = Figure(resolution = (2160, 1080))
-
-    x = (xu, xv, xc)
-    y = (yu, yv, yc)
-    z = (zu, zv, zc)
-
-    statenames = ["u", "v", "η"]
-    for (n, var) in enumerate([u, v, η])
-        ax = fig[3:7, 3n-2:3n] = LScene(fig) # make plot area wider
-        wireframe!(ax, Sphere(Point3f0(0), 0.99f0), show_axis=false)
-        surface!(ax, x[n], y[n], z[n], color=var, colormap=:balance) #, colorrange=clims[n])
-        rotate_cam!(ax.scene, (3π/4, -π/8, 0))
-        zoom!(ax.scene, (0, 0, 0), 5, false)
-        fig[2, 2 + 3*(n-1)] = Label(fig, statenames[n], textsize = 50) # put names in center
-    end
-
-    supertitle = fig[0, :] = Label(fig, plot_title, textsize=50)
-
-    record(fig, output_prefix * ".mp4", iterations, framerate=30) do i
-        @info "Animating iteration $i/$(iterations[end])..."
-        iter[] = i
-    end
-
-    close(file)
-
-    return nothing
-end
-=#
-
-#=
-using Plots
-
-function visualize_plots(filepath)
-
-    file = jldopen(filepath)
-
-    Nx = file["grid/Nx"]
-    Ny = file["grid/Ny"]
-    Lλ = file["grid/Lx"]
-    Lφ = file["grid/Ly"]
-    Lz = file["grid/Lz"]
-
-    # A spherical domain
-    grid = RegularLatitudeLongitudeGrid(size = (Nx, Ny, 1),
-                                        longitude = (-180, 180),
-                                        latitude = (-Lφ/2, Lφ/2),
-                                        z = (-Lz, 0))
-
-    iterations = parse.(Int, keys(file["timeseries/t"]))
-
-    anim = @animate for (i, iter) in enumerate(iterations)
-        ζ = file["timeseries/ζ/$iter"][:, :, 1]
-        Plots.heatmap(ζ')
-    end
-
-    close(file)
-
-    mp4(anim, "eddying_strip_$(Nx)_$Ny.mp4", fps = 8) # hide
-
-    return nothing
-end
-
-visualize_plots(filepath)
-=#
-
-using GLMakie
 
 function visualize_makie(output_prefix)
 
@@ -151,48 +43,43 @@ function visualize_makie(output_prefix)
 
     iterations = parse.(Int, keys(file["timeseries/t"]))
 
-    λ, ϕ, r = nodes((Face, Face, Center), grid, reshape=true)
-
-    λ = λ .+ 180  # Convert to λ ∈ [0°, 360°]
-    ϕ = 90 .- ϕ   # Convert to ϕ ∈ [0°, 180°] (0° at north pole)
+    λ, ϕ, r = nodes((Face, Face, Center), grid)
+    x, y, z = geographic2cartesian(λ, ϕ, r=1.01)
 
     iter = Node(0)
-
-    plot_title = @lift @sprintf("Free decay of barotropic turbulence: ζ at time = %s", prettytime(file["timeseries/t/" * string($iter)]))
-
-    ζp = @lift file["timeseries/ζ/" * string($iter)][:, :, 1]
-
-    ζp0 = file["timeseries/ζ/" * string(0)][:, :, 1]
-
-    # Plot on the unit sphere to align with the spherical wireframe.
-    # Multiply by 1.01 so the η field is a bit above the wireframe.
-    x = @. 1.01 * cosd(λ) * sind(ϕ)
-    y = @. 1.01 * sind(λ) * sind(ϕ)
-    z = @. 1.01 * cosd(ϕ) * λ ./ λ
-
-    x = x[:, :, 1]
-    y = y[:, :, 1]
-    z = z[:, :, 1]
-
+    ζ′ = @lift file["timeseries/ζ/" * string($iter)][:, :, 1]
+    
     fig = Figure(resolution = (2000, 2000))
 
-    clims = extrema(ζp0) ./ 1000
+    clims = @lift begin
+        scale = 1.5e-1
+        min_clim = 2e-5
+        max_ζ = maximum(abs, file["timeseries/ζ/" * string($iter)][:, :, 1])
+        scale * max_ζ < min_clim && (-min_clim, min_clim)
+        scale .* (-max_ζ, max_ζ)
+    end
 
-    ax = fig[1, :] = LScene(fig) # make plot area wider
+
+    ax = fig[:, :] = LScene(fig) # make plot area wider
     wireframe!(ax, Sphere(Point3f0(0), 1f0), show_axis=false)
-    surface!(ax, x, y, z, color=ζp, colormap=:balance, colorrange=clims)
-    rotate_cam!(ax.scene, (π/2, π/6, 0))
+    surface!(ax, x, y, z, color=ζ′, colormap=:blues, colorrange=clims)
+    rotate_cam!(ax.scene, (π/4, π/6, 0))
     zoom!(ax.scene, (0, 0, 0), 5, false)
 
+    plot_title = @lift @sprintf("Vertical vorticity in decaying, rotating, barotropic turbulence at time = %s",
+                                prettytime(file["timeseries/t/" * string($iter)]))
     supertitle = fig[0, :] = Label(fig, plot_title, textsize=50)
+
     display(fig)
 
     record(fig, output_prefix * ".mp4", iterations, framerate=12) do i
-        @show "Plotting iteration $i of $(iterations[end])..."
+        @info "Plotting iteration $i of $(iterations[end])..."
         iter[] = i
     end
 
     return nothing
 end
+
+output_prefix = "rotating_freely_decaying_barotropic_turbulence_Nx1080_Ny480"
 
 visualize_makie(output_prefix)
