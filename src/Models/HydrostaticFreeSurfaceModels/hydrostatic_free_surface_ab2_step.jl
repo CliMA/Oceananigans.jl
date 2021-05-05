b14aeaec00ff94ad750b3e0bff3773af0ce82ca8
Gregory Wagner <wagner.greg@gmail.com>
2021-05-05 09:11:17 -0700

    Adds placeholder for implicit viscosity and diffusion

--- src/Models/HydrostaticFreeSurfaceModels/hydrostatic_free_surface_ab2_step.jl
+++ src/Models/HydrostaticFreeSurfaceModels/hydrostatic_free_surface_ab2_step.jl
@@ -1,15 +1,14 @@
 using Oceananigans.TimeSteppers: ab2_step_field!
+using KernelAbstractions: NoneEvent
 
 import Oceananigans.TimeSteppers: ab2_step!
 
-combine_events(free_surface_event,
-               velocities_events,
-               tracer_events) = MultiEvent(tuple(free_surface_event, velocities_events..., tracer_events...))
+implicit_vertical_viscosity_step!(u, closure, model; kwargs...) = NoneEvent()
+implicit_vertical_diffusivity_step!(c, closure, model; kwargs...) = NoneEvent()
 
-combine_events(::Nothing, velocities_events, tracer_events) =
-    MultiEvent(tuple(velocities_events..., tracer_events...))
-
-combine_events(::Nothing, ::Nothing, tracer_events) = MultiEvent(tuple(tracer_events...))
+#####
+##### Step everything
+#####
 
 function ab2_step!(model::HydrostaticFreeSurfaceModel, Δt, χ)
 
@@ -17,49 +16,72 @@ function ab2_step!(model::HydrostaticFreeSurfaceModel, Δt, χ)
 
     barrier = Event(device(model.architecture))
 
-    # Launch velocity update kernels
-  
-    velocities_events = []
+    velocities_events = ab2_step_velocities!(model.velocities, model, Δt, χ)
+    tracers_events = ab2_step_tracers!(model.tracers, model, Δt, χ)
+    free_surface_event = ab2_step_free_surface!(model.free_surface, model, Δt, χ, MultiEvent(Tuple(velocities_events)))
 
-    for name in (:u, :v)
-        model.velocities isa PrescribedVelocityFields && break
-
-        Gⁿ = model.timestepper.Gⁿ[name]
-        G⁻ = model.timestepper.G⁻[name]
-        velocity_field = model.velocities[name]
-
-        event = launch!(model.architecture, model.grid, :xyz, ab2_step_field!,
-                        velocity_field, Δt, χ, Gⁿ, G⁻,
-                        dependencies=barrier)
-
-        push!(velocities_events, event)
-    end
-
-    # Tracer update kernels
-
-    tracer_events = []
-
-    for name in tracernames(model.tracers)
-        Gⁿ = model.timestepper.Gⁿ[name]
-        G⁻ = model.timestepper.G⁻[name]
-        tracer_field = model.tracers[name]
-
-        event = launch!(model.architecture, model.grid, :xyz, ab2_step_field!,
-                        tracer_field, Δt, χ, Gⁿ, G⁻,
-                        dependencies=barrier)
-
-        push!(tracer_events, event)
-    end
-
-    velocities_update = MultiEvent(Tuple(velocities_events))
-
-    # Free surface update
-    
-    free_surface_event = ab2_step_free_surface!(model.free_surface, velocities_update, model, Δt, χ)
-
-    prognostic_field_events = combine_events(free_surface_event, velocities_events, tracer_events)
+    prognostic_field_events = MultiEvent(tuple(free_surface_event, velocities_events..., tracer_events...))
 
     wait(device(model.architecture), prognostic_field_events)
 
     return nothing
 end
+
+#####
+##### Step velocities
+#####
+
+function ab2_step_velocities!(velocities, model, Δt, χ)
+
+    # Launch velocity update kernels
+    velocities_events = []
+
+    for name in (:u, :v)
+        Gⁿ = model.timestepper.Gⁿ[name]
+        G⁻ = model.timestepper.G⁻[name]
+        velocity_field = model.velocities[name]
+
+        explicit_velocity_step_event = launch!(model.architecture, model.grid, :xyz,
+                                               ab2_step_field!, velocity_field, Δt, χ, Gⁿ, G⁻,
+                                               dependencies = barrier)
+
+        implicit_velocity_step_event = implicit_vertical_viscosity_step!(velocity_field, model.closure, model;
+                                                                         dependencies = explicit_velocity_step_event)
+
+        push!(velocities_events, explicit_velocity_step_event, implicit_velocity_step_event)
+    end
+
+    return velocities_events
+end
+
+#####
+##### Step velocities
+#####
+
+const EmptyNamedTuple = NamedTuple{(),Tuple{}}
+
+ab2_step_tracers!(::EmptyNamedTuple, model, Δt, χ) = [NoneEvent()]
+
+function ab2_step_tracers!(tracers, model, Δt, χ)
+
+    # Tracer update kernels
+    tracer_events = []
+
+    for name in tracernames(tracers)
+        Gⁿ = model.timestepper.Gⁿ[name]
+        G⁻ = model.timestepper.G⁻[name]
+        tracer_field = tracers[name]
+
+        explicit_tracer_step_event = launch!(model.architecture, model.grid, :xyz,
+                                             ab2_step_field!, tracer_field, Δt, χ, Gⁿ, G⁻,
+                                             dependencies = barrier)
+
+        implicit_tracer_step_event = implicit_vertical_diffusivity_step!(tracer_field, model.closure, model;
+                                                                         dependencies = explicit_tracer_step_event)
+
+        push!(tracer_events, explicit_tracer_step_event, implicit_tracer_step_event)
+    end
+
+    return tracer_events
+end
+
