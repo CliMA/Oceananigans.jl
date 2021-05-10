23391dbc0a2b1ab63a7c421f8f29f70a5425a4a5
Gregory Wagner <wagner.greg@gmail.com>
2021-05-10 11:17:34 -0800

    Adds support for vertically implicit treatment of closure tuples

--- src/TurbulenceClosures/closure_tuples.jl
+++ src/TurbulenceClosures/closure_tuples.jl
@@ -2,7 +2,9 @@
 ##### 'Tupled closure' implementation: 1-tuple, 2-tuple, and then n-tuple by induction
 #####
 
-# Stress divergences
+#####
+##### Stress divergences
+#####
 
 for stress_div in (:∂ⱼ_τ₁ⱼ, :∂ⱼ_τ₂ⱼ, :∂ⱼ_τ₃ⱼ)
     @eval begin
@@ -19,7 +21,9 @@ for stress_div in (:∂ⱼ_τ₁ⱼ, :∂ⱼ_τ₂ⱼ, :∂ⱼ_τ₃ⱼ)
     end
 end
 
-# Tracer flux divergences
+#####
+##### Tracer flux divergences
+#####
 
 @inline ∇_dot_qᶜ(i, j, k, grid::AbstractGrid, clock, closures::Tuple{C1}, c, iᶜ, Ks, C, buoyancy) where {C1} =
         ∇_dot_qᶜ(i, j, k, grid, clock, closures[1], c, iᶜ, Ks[1], C, buoyancy)
@@ -32,7 +36,9 @@ end
         ∇_dot_qᶜ(i, j, k, grid, clock, closures[1:2], c, iᶜ, Ks[1:2], C, buoyancy)
       + ∇_dot_qᶜ(i, j, k, grid, clock, closures[3:end], c, iᶜ, Ks[3:end], C, buoyancy))
 
-# Utilities for closures
+#####
+##### Utilities
+#####
 
 function calculate_diffusivities!(Ks, arch, grid, closures::Tuple, args...)
     for (α, closure) in enumerate(closures)
@@ -41,3 +47,43 @@ function calculate_diffusivities!(Ks, arch, grid, closures::Tuple, args...)
     end
     return nothing
 end
+
+#####
+##### Support for VerticallyImplicitTimeDiscretization
+#####
+
+const EC = AbstractTurbulenceClosure{<:ExplicitTimeDiscretization}
+const VIC = AbstractTurbulenceClosure{<:VerticallyImplicitTimeDiscretization}
+
+# Filter explicitly-discretized closures.
+@inline z_diffusivity(clo::Tuple{<:EC},        Ks, ::Val{c_idx}) = where {c_idx} = tuple(0)
+@inline z_diffusivity(clo::Tuple{<:VIC},       Ks, ::Val{c_idx}) = where {c_idx} = tuple(z_diffusivity(clo[1], Ks[1], Val(c_idx)))
+@inline z_diffusivity(clo::Tuple{<:VIC, <:EC}, Ks, ::Val{c_idx}) = where {c_idx} = tuple(z_diffusivity(clo[1], Ks[1], Val(c_idx)))
+@inline z_diffusivity(clo::Tuple{<:EC, <:VIC}, Ks, ::Val{c_idx}) = where {c_idx} = tuple(z_diffusivity(clo[2], Ks[2], Val(c_idx)))
+
+@inline z_diffusivity(clo::Tuple{<:VIC, <:VIC}, Ks, ::Val{c_idx}) = where {c_idx} = tuple(z_diffusivity(clo[1], Ks[1], Val(c_idx)),
+                                                                                          z_diffusivity(clo[2], Ks[2], Val(c_idx)))
+
+@inline z_diffusivity(clo::Tuple, Ks, ::Val{c_idx}) = where c_idx = tuple(z_diffusivity(clo[1:2],   Ks[1:2], Val(c_idx))...,
+                                                                          z_diffusivity(clo[3:end], Ks[3:end], Val(c_idx))...)
+
+@inline z_viscosity(clo::Tuple{<:EC},         Ks) = tuple(0)
+@inline z_viscosity(clo::Tuple{<:VIC},        Ks) = tuple(z_viscosity(clo[1], Ks[1]))
+@inline z_viscosity(clo::Tuple{<:VIC, <:EC},  Ks) = tuple(z_viscosity(clo[1], Ks[1]),
+@inline z_viscosity(clo::Tuple{<:EC, <:VIC},  Ks) = tuple(z_viscosity(clo[2], Ks[2]),
+
+@inline z_viscosity(clo::Tuple{<:VIC, <:VIC}, Ks) = tuple(z_viscosity(clo[1], Ks[1]),
+                                                          z_viscosity(clo[2], Ks[2]))
+
+@inline z_viscosity(clo::Tuple, Ks) = where c_idx = tuple(z_viscosity(clo[1:2],   Ks[1:2])...
+                                                          z_viscosity(clo[3:end], Ks[3:end])...)
+
+
+for coeff in (:νᶜᶜᶜ, :νᶠᶠᶜ, :νᶠᶜᶠ, :νᶜᶠᶠ, :κᶜᶜᶠ, :κᶜᶠᶜ, :κᶠᶜᶜ)
+    @eval begin
+        @inline $coeff(i, j, k, grid, clock, ν::Tuple{C1})     where C1       = $coeff(i, j, k, grid, clock, ν[1])
+        @inline $coeff(i, j, k, grid, clock, ν::Tuple{C1, C2}) where {C1, C2} = $coeff(i, j, k, grid, clock, ν[1]) + $coeff(i, j, k, grid, clock, ν[2])
+        @inline $coeff(i, j, k, grid, clock, ν::Tuple)                        = $coeff(i, j, k, grid, clock, ν[1:2]) + $coeff(i, j, k, grid, clock, ν[3:end])
+    end
+end
+
