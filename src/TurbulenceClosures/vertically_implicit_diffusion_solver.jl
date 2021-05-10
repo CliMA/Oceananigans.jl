470ebeb7aa89dc7693db1e08d516334f5e542b4f
Gregory Wagner <wagner.greg@gmail.com>
2021-05-09 20:56:02 -0800

    Adds functionality for implicit vertical velocity stepping

--- src/TurbulenceClosures/vertically_implicit_diffusion_solver.jl
+++ src/TurbulenceClosures/vertically_implicit_diffusion_solver.jl
@@ -6,8 +6,9 @@ using Oceananigans.Solvers: BatchedTridiagonalSolver, solve_batched_tridiagonal_
 ##### Vertically implicit solver
 #####
 
-struct VerticallyImplicitDiffusionSolver{B}
-    batched_tridiagonal_solver :: B
+struct VerticallyImplicitDiffusionSolver{H, Z}
+    tracer_solver :: H # also used for horizontal velocities
+    w_velocity_solver :: Z
 end
 
 """
@@ -30,31 +31,60 @@ implicit_tracer_step!(c, ::Nothing, args...; kwargs...) = NoneEvent()
 implicit_diffusion_solver(closure::AbstractTurbulenceClosure, args...) = implicit_solver(time_discretization(closure), closure, args...)
 implicit_diffusion_solver(::ExplicitTimeDiscretization, args...) = nothing
 
-@inline κ_Δz²_ᵏ(i, j, k, grid, κᵏ)     = κᵏ   / Δzᵃᵃᶜ(i, j, k, grid) / Δzᵃᵃᶠ(i, j, k,   grid)
-@inline κ_Δz²_ᵏ⁺¹(i, j, k, grid, κᵏ⁺¹) = κᵏ⁺¹ / Δzᵃᵃᶜ(i, j, k, grid) / Δzᵃᵃᶠ(i, j, k+1, grid)
+#####
+##### Solver kernel functions for tracers / horizontal velocities and for vertical velocities
+##### Note: "ivd" stands for implicit vertical diffusion.
 
-# "ivd" stands for implicit vertical diffusion:
+@inline κ_Δz²(i, j, kᶠ, kᶜ, grid, κ) = κ / Δzᵃᵃᶜ(i, j, kᶜ, grid) / Δzᵃᵃᶠ(i, j, kᶠ, grid)
 
-@inline function ivd_upper_diagonal(i, j, k, grid, clock, Δt, κ⁻⁻ᶠ, κ)
-    k′ = k
-    κᵏ⁺¹ = κ⁻⁻ᶠ(i, j, k′ + 1, grid, clock, κ)
-    return - Δt * κ_Δz²_ᵏ⁺¹(i, j, k′, grid, κᵏ⁺¹)
+# Tracers and horizontal velocities at cell centers in z
+
+@inline function ivd_upper_diagonalᵃᵃᶜ(i, j, k, grid, clock, Δt, κ⁻⁻ᶠ, κ)
+    κᵏ⁺¹ = κ⁻⁻ᶠ(i, j, k+1, grid, clock, κ)
+    return - Δt * κ_Δz²(i, j, k, k+1, grid, κᵏ⁺¹)
 end
 
-@inline function ivd_lower_diagonal(i, j, k, grid, clock, Δt, κ⁻⁻ᶠ, κ)
+@inline function ivd_lower_diagonalᵃᵃᶜ(i, j, k, grid, clock, Δt, κ⁻⁻ᶠ, κ)
     k′ = k + 1 # Shift to adjust for Tridiagonal indexing convenction
     κᵏ = κ⁻⁻ᶠ(i, j, k′, grid, clock, κ)
-    return - Δt * κ_Δz²_ᵏ(i, j, k′, grid, κᵏ)
+    return - Δt * κ_Δz²(i, j, k′, k′, grid, κᵏ)
 end
 
-@inline function ivd_diagonal(i, j, k, grid, clock, Δt, κ⁻⁻ᶠ, κ)
+@inline function ivd_diagonalᵃᵃᶠ(i, j, k, grid::AbstractGrid{FT}, clock, Δt, κ⁻⁻ᶠ, κ) where FT
     κᵏ   = κ⁻⁻ᶠ(i, j, k,   grid, clock, κ)
     κᵏ⁺¹ = κ⁻⁻ᶠ(i, j, k+1, grid, clock, κ)
 
-    return ifelse(k == 1,      1 + Δt *  κ_Δz²_ᵏ⁺¹(i, j, k, grid, κᵏ⁺¹),
-           ifelse(k < grid.Nz, 1 + Δt * (κ_Δz²_ᵏ⁺¹(i, j, k, grid, κᵏ⁺¹) + κ_Δz²_ᵏ(i, j, k, grid, κᵏ)),
-                               1 + Δt *                                   κ_Δz²_ᵏ(i, j, k, grid, κᵏ)))
+    return ifelse(k == 1,      one(FT) + Δt *  κ_Δz²(i, j, k, k+1, grid, κᵏ⁺¹),
+           ifelse(k < grid.Nz, one(FT) + Δt * (κ_Δz²(i, j, k, k+1, grid, κᵏ⁺¹) + κ_Δz²(i, j, k, k, grid, κᵏ)),
+                               one(FT) + Δt *                                    κ_Δz²(i, j, k, k, grid, κᵏ)))
 end
+
+# Vertical velocity kernel functions (at cell interfaces in z)
+
+@inline function ivd_upper_diagonalᵃᵃᶠ(i, j, k, grid::AbstractGrid{FT}, clock, Δt, νᶜᶜᶜ, ν) where FT
+    return ifelse(k == 1,
+                  zero(FT), 
+                  - Δt * κ_Δz²(i, j, k, k, grid, νᶜᶜᶜ(i, j, k, grid, clock, ν)))
+end
+
+@inline function ivd_lower_diagonalᵃᵃᶠ(i, j, k, grid, clock, Δt, νᶜᶜᶜ, ν)
+    k′ = k + 1 # Shift to adjust for Tridiagonal indexing convenction
+    νᵏ⁻¹ = νᶜᶜᶜ(i, j, k′ - 1, grid, clock, κ)
+    return - Δt * κ_Δz²(i, j, k′, k′-1, grid, νᵏ⁻¹)
+end
+
+@inline function ivd_diagonalᵃᵃᶜ(i, j, k, grid::AbstractGrid{FT}, clock, Δt, νᶜᶜᶜ, ν) where FT
+    νᵏ⁻¹ = νᶜᶜᶜ(i, j, k-1, grid, clock, ν)
+    νᵏ   = νᶜᶜᶜ(i, j, k,   grid, clock, ν)
+
+    return ifelse(k == 1,
+                  one(FT),
+                  1 + Δt * (κ_Δz²(i, j, k, k, grid, νᵏ) + κ_Δz²(i, j, k, k-1, grid, νᵏ⁻¹)))
+end
+
+#####
+##### Solver constructor
+#####
 
 """
     implicit_diffusion_solver(::VerticallyImplicitTimeDiscretization, arch, grid)
@@ -67,18 +97,33 @@ Build a tridiagonal solver for the elliptic equation
 
 where `cⁿ⁺¹` and `c★` live at cell `Center`s in the vertical.
 """
-function implicit_diffusion_solver(::VerticallyImplicitTimeDiscretization, arch, grid)
+function implicit_diffusion_solver(::VerticallyImplicitTimeDiscretization, arch, grid; with_vertical_velocity)
 
+    # Share scratch memory
     right_hand_side = arch_array(arch, zeros(grid.Nx, grid.Ny, grid.Nz))
 
-    batched_tridiagonal_solver = BatchedTridiagonalSolver(arch, grid;
-                                                           lower_diagonal = ivd_lower_diagonal,
-                                                                 diagonal = ivd_diagonal,
-                                                           upper_diagonal = ivd_upper_diagonal,
-                                                          right_hand_side = right_hand_side)
+    z_center_solver = BatchedTridiagonalSolver(arch, grid;
+                                                             lower_diagonal = ivd_lower_diagonalᵃᵃᶜ,
+                                                                   diagonal = ivd_diagonalᵃᵃᶜ,
+                                                             upper_diagonal = ivd_upper_diagonalᵃᵃᶜ,
+                                                            right_hand_side = right_hand_side)
 
-    return VerticallyImplicitDiffusionSolver(batched_tridiagonal_solver)
+    if with_vertical_velocity
+        z_face_solver = BatchedTridiagonalSolver(arch, grid;
+                                                  lower_diagonal = ivd_lower_diagonalᵃᵃᶠ,
+                                                        diagonal = ivd_diagonalᵃᵃᶠ,
+                                                  upper_diagonal = ivd_upper_diagonalᵃᵃᶠ,
+                                                 right_hand_side = right_hand_side)
+    else
+        z_face_solver = nothing
+    end
+
+    return VerticallyImplicitDiffusionSolver(tracer_solver, w_velocity_solver)
 end
+
+#####
+##### Implicit step functions
+#####
 
 """
     implicit_step!(field, solver::VerticallyImplicitDiffusionSolver, clock, Δt, κ⁻⁻ᶠ, κ; dependencies)
@@ -88,37 +133,56 @@ tridiagonal system for vertically-implicit diffusion, passing the arguments
 `clock, Δt, κ⁻⁻ᶠ, κ` into the coefficient functions that return coefficients of the
 lower diagonal, diagonal, and upper diagonal of the resulting tridiagonal system.
 """
-function implicit_step!(field, solver::VerticallyImplicitDiffusionSolver,
+function implicit_step!(field,
+                        tridiagonal_solver,
                         clock, Δt, κ⁻⁻ᶠ, κ;
                         dependencies = Event(device(model.architecture)))
 
     field_interior = interior(field)
-    solver.batched_tridiagonal_solver.f .= field_interior
+    tridiagonal_solver.f .= field_interior
 
-    return solve_batched_tridiagonal_system!(field, solver.batched_tridiagonal_solver,
-                                             clock, Δt, κ⁻⁻ᶠ, κ;
+    return solve_batched_tridiagonal_system!(field,
+                                             tridiagonal_solver, clock, Δt, κ⁻⁻ᶠ, κ;
                                              dependencies = dependencies)
 end
                                              
-function implicit_velocity_step!(velocity_field, solver::VerticallyImplicitDiffusionSolver, clock, Δt,
-                                 field_location, closure, diffusivities; dependencies = Event(device(model.architecture)))
+function implicit_velocity_step!(velocity_field,
+                                 implicit_solver::VerticallyImplicitDiffusionSolver,
+                                 clock,
+                                 Δt,
+                                 field_location,
+                                 closure,
+                                 diffusivities;
+                                 dependencies = Event(device(model.architecture)))
 
     vertical_viscous_flux_location = (field_location[1], field_location[2], flip(field_location[3]))
     ν⁻⁻ᶠ = eval(Symbol(:ν, interpolation_code.(vertical_viscous_flux_location)...)) 
     ν = z_viscosity(closure, diffusivities)
 
-    return implicit_step!(velocity_field, solver,
-                          clock, Δt, ν⁻⁻ᶠ, ν;
+    if field_location = (Center, Center, Face) # w velocity
+        solver = implicit_solver.w_velocity_solver
+    else
+        solver = implicit_solver.tracer_solver
+    end
+
+    return implicit_step!(velocity_field,
+                          solver, clock, Δt, ν⁻⁻ᶠ, ν;
                           dependencies = dependencies)
 end
 
-function implicit_tracer_step!(tracer_field, solver::VerticallyImplicitDiffusionSolver, clock, Δt,
-                               closure, diffusivities, tracer_index; dependencies = Event(device(model.architecture)))
+function implicit_tracer_step!(tracer_field,
+                               implicit_solver::VerticallyImplicitDiffusionSolver,
+                               clock,
+                               Δt,
+                               closure,
+                               diffusivities,
+                               tracer_index;
+                               dependencies = Event(device(model.architecture)))
 
     κ = z_diffusivity(closure, diffusivities, Val(tracer_index))
 
-    return implicit_step!(tracer_field, solver,
-                          clock, Δt, κᶜᶜᶠ, κ;
+    return implicit_step!(tracer_field,
+                          implicit_solver.tracer_solver, clock, Δt, κᶜᶜᶠ, κ;
                           dependencies = dependencies)
 end
 
