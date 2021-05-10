70ce17b2f9413098801bca090a8b12cf67e2dba5
Gregory Wagner <wagner.greg@gmail.com>
2021-05-10 10:43:46 -0800

    Better closure tuple implementation

--- src/TurbulenceClosures/closure_tuples.jl
+++ src/TurbulenceClosures/closure_tuples.jl
@@ -1,26 +1,36 @@
 #####
-##### 'Tupled closure' implementation for closures of length 2.
-#####
-##### For now, we hack an implementation for tuples of two closures that compiles on the GPU.
-##### We cannot do the case of arbitrary closure tuples until we figure out how to
-##### obtain the length of a closure tuple at compile-time on the GPU.
+##### 'Tupled closure' implementation: 1-tuple, 2-tuple, and then n-tuple by induction
 #####
 
 # Stress divergences
 
 for stress_div in (:∂ⱼ_τ₁ⱼ, :∂ⱼ_τ₂ⱼ, :∂ⱼ_τ₃ⱼ)
     @eval begin
+        @inline $stress_div(i, j, k, grid::AbstractGrid, clock, closures::Tuple{C1}, U, Ks) where {C1} =
+                    $stress_div(i, j, k, grid, clock, closures[1], U, Ks[1])
+
         @inline $stress_div(i, j, k, grid::AbstractGrid, clock, closures::Tuple{C1, C2}, U, Ks) where {C1, C2} = (
-                $stress_div(i, j, k, grid, clock, closures[1], U, Ks[1])
-              + $stress_div(i, j, k, grid, clock, closures[2], U, Ks[2]))
+                    $stress_div(i, j, k, grid, clock, closures[1], U, Ks[1])
+                  + $stress_div(i, j, k, grid, clock, closures[2], U, Ks[2]))
+
+        @inline $stress_div(i, j, k, grid::AbstractGrid, clock, closures::Tuple, U, Ks, args...) = (
+                    $stress_div(i, j, k, grid, clock, closures[1:2], U, Ks[1:2], args...)
+                  + $stress_div(i, j, k, grid, clock, closures[3:end], U, K[3:end], args...))
     end
 end
 
 # Tracer flux divergences
 
+@inline ∇_dot_qᶜ(i, j, k, grid::AbstractGrid, clock, closures::Tuple{C1}, c, iᶜ, Ks, C, buoyancy) where {C1} =
+        ∇_dot_qᶜ(i, j, k, grid, clock, closures[1], c, iᶜ, Ks[1], C, buoyancy)
+
 @inline ∇_dot_qᶜ(i, j, k, grid::AbstractGrid, clock, closures::Tuple{C1, C2}, c, iᶜ, Ks, C, buoyancy) where {C1, C2} = (
         ∇_dot_qᶜ(i, j, k, grid, clock, closures[1], c, iᶜ, Ks[1], C, buoyancy)
       + ∇_dot_qᶜ(i, j, k, grid, clock, closures[2], c, iᶜ, Ks[2], C, buoyancy))
+
+@inline ∇_dot_qᶜ(i, j, k, grid::AbstractGrid, clock, closures::Tuple, c, iᶜ, Ks, C, buoyancy) = (
+        ∇_dot_qᶜ(i, j, k, grid, clock, closures[1:2], c, iᶜ, Ks[1:2], C, buoyancy)
+      + ∇_dot_qᶜ(i, j, k, grid, clock, closures[3:end], c, iᶜ, Ks[3:end], C, buoyancy))
 
 # Utilities for closures
 
@@ -31,54 +41,3 @@ function calculate_diffusivities!(Ks, arch, grid, closures::Tuple, args...)
     end
     return nothing
 end
-
-#####
-##### Arbitrary length 'tupled closure' implementation
-#####
-
-for stress_div in (:∂ⱼ_τ₁ⱼ, :∂ⱼ_τ₂ⱼ, :∂ⱼ_τ₃ⱼ)
-    @eval begin
-        @inline function $stress_div(i, j, k, grid::AbstractGrid{FT}, clock, closure_tuple::Tuple, U,
-                                     K_tuple, args...) where FT
-
-            stress_div_ijk = zero(FT)
-
-            ntuple(Val(length(closure_tuple))) do α
-                @inbounds closure = closure_tuple[α]
-                @inbounds K = K_tuple[α]
-                stress_div_ijk += $stress_div(i, j, k, grid, clock, closure, U, K, args...)
-            end
-
-            return stress_div_ijk
-        end
-    end
-end
-
-@inline function ∇_dot_qᶜ(i, j, k, grid::AbstractGrid{FT}, clock, closure_tuple::Tuple,
-                          c, tracer_index, K_tuple, args...) where FT
-
-    flux_div_ijk = zero(FT)
-
-    ntuple(Val(length(closure_tuple))) do α
-        @inbounds closure = closure_tuple[α]
-        @inbounds K = K_tuple[α]
-        flux_div_ijk +=  ∇_κ_∇c(i, j, k, grid, clock, closure, c, tracer_index, K, args...)
-    end
-
-    return flux_div_ijk
-end
-
-function calculate_diffusivities!(K_tuple::Tuple, arch, grid, closure_tuple::Tuple, args...)
-    ntuple(Val(length(closure_tuple))) do α
-        @inbounds closure = closure_tuple[α]
-        @inbounds K = K_tuple[α]
-        calculate_diffusivities!(K, arch, grid, closure, args...)
-    end
-
-    return nothing
-end
-
-with_tracers(tracers, closure_tuple::Tuple) =
-    Tuple(with_tracers(tracers, closure) for closure in closure_tuple)
-
-
