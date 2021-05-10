09d255a7c8d983329a451e291b31986e6525d9bb
Gregory Wagner <wagner.greg@gmail.com>
2021-05-10 11:24:51 -0800

    Restores with_tracers for closure tuples

--- src/TurbulenceClosures/closure_tuples.jl
+++ src/TurbulenceClosures/closure_tuples.jl
@@ -40,6 +40,9 @@ end
 ##### Utilities
 #####
 
+with_tracers(tracers, closure_tuple::Tuple) =
+    Tuple(with_tracers(tracers, closure) for closure in closure_tuple)
+
 function calculate_diffusivities!(Ks, arch, grid, closures::Tuple, args...)
     for (α, closure) in enumerate(closures)
         @inbounds K = Ks[α]
