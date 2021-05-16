4f7441b764fcb0c36b2f23bef0049972decc05ad
Gregory Wagner <wagner.greg@gmail.com>
2021-05-15 16:49:51 -0800

    Generalizes advective fluxes to curvilinear grids in principle

--- src/Advection/momentum_advection_operators.jl
+++ src/Advection/momentum_advection_operators.jl
@@ -12,7 +12,7 @@ Calculate the advection of momentum in the x-direction using the conservative fo
 which will end up at the location `fcc`.
 """
 @inline function div_Uu(i, j, k, grid, advection, U, u)
-    return 1/Vᵃᵃᶜ(i, j, k, grid) * (δxᶠᵃᵃ(i, j, k, grid, momentum_flux_uu, advection, U.u, u) +
+    return 1/Vᶠᶜᶜ(i, j, k, grid) * (δxᶠᵃᵃ(i, j, k, grid, momentum_flux_uu, advection, U.u, u) +
                                     δyᵃᶜᵃ(i, j, k, grid, momentum_flux_uv, advection, U.v, u) +
                                     δzᵃᵃᶜ(i, j, k, grid, momentum_flux_uw, advection, U.w, u))
 end
@@ -27,7 +27,7 @@ Calculate the advection of momentum in the y-direction using the conservative fo
 which will end up at the location `cfc`.
 """
 @inline function div_Uv(i, j, k, grid, advection, U, v)
-    return 1/Vᵃᵃᶜ(i, j, k, grid) * (δxᶜᵃᵃ(i, j, k, grid, momentum_flux_vu, advection, U.u, v) +
+    return 1/Vᶜᶠᶜ(i, j, k, grid) * (δxᶜᵃᵃ(i, j, k, grid, momentum_flux_vu, advection, U.u, v) +
                                     δyᵃᶠᵃ(i, j, k, grid, momentum_flux_vv, advection, U.v, v)    +
                                     δzᵃᵃᶜ(i, j, k, grid, momentum_flux_vw, advection, U.w, v))
 end
@@ -42,7 +42,7 @@ Calculate the advection of momentum in the z-direction using the conservative fo
 which will end up at the location `ccf`.
 """
 @inline function div_Uw(i, j, k, grid, advection, U, w)
-    return 1/Vᵃᵃᶠ(i, j, k, grid) * (δxᶜᵃᵃ(i, j, k, grid, momentum_flux_wu, advection, U.u, w) +
+    return 1/Vᶜᶜᶠ(i, j, k, grid) * (δxᶜᵃᵃ(i, j, k, grid, momentum_flux_wu, advection, U.u, w) +
                                     δyᵃᶜᵃ(i, j, k, grid, momentum_flux_wv, advection, U.v, w) +
                                     δzᵃᵃᶠ(i, j, k, grid, momentum_flux_ww, advection, U.w, w))
 end
