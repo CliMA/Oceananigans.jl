be858a1db4823021abbfecf615d4a65643e240d0
Gregory Wagner <wagner.greg@gmail.com>
2021-05-17 14:59:46 -0800

    Off to a meagre start

--- validation/implicit_vertical_diffusion/tke_based_column_model.jl
+++ validation/implicit_vertical_diffusion/tke_based_column_model.jl
@@ -8,7 +8,7 @@ grid = RegularRectilinearGrid(size=128, z=(-128, 0), topology=(Flat, Flat, Bound
 
 closure = TKEBasedVerticalDiffusivity()
 
-Qᵇ = 1e-8
+Qᵇ = 1e-7
 Qᵉ = - closure.dissipation_constant * closure.surface_model.CᵂwΔ * Qᵇ * grid.Δz
 
 convection_bcs = TracerBoundaryConditions(grid; top = FluxBoundaryCondition(Qᵇ))
@@ -30,7 +30,7 @@ b = view(interior(model.tracers.b), 1, 1, :)
 
 b_plot = plot(b, z, linewidth = 2, label = "t = 0", xlabel = "Buoyancy", ylabel = "z", legend=:bottomright)
               
-simulation = Simulation(model, Δt = 1.0, stop_time = 4hour)
+simulation = Simulation(model, Δt = 1.0, stop_time = 1hour)
 
 run!(simulation)
 
