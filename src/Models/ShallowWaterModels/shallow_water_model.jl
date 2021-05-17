6115762ea4de2a1cab33a9a196731bb5134a0c87
Francis J. Poulin <fpoulin@uwaterloo.ca>
2021-05-17 14:48:44 -0400

    Update src/Models/ShallowWaterModels/shallow_water_model.jl
    Co-authored-by: Gregory L. Wagner <wagner.greg@gmail.com>

--- src/Models/ShallowWaterModels/shallow_water_model.jl
+++ src/Models/ShallowWaterModels/shallow_water_model.jl
@@ -39,7 +39,7 @@ function ShallowWaterSolutionFields(arch, grid, bcs)
     return (uh=uh, vh=vh, h=h)
 end
 
-struct ShallowWaterModel{G, A<:AbstractArchitecture, T, V, R, F, E, B, Q, C, K, TS, IB} <: AbstractModel{TS}
+struct ShallowWaterModel{G, A<:AbstractArchitecture, T, V, R, F, E, B, Q, C, K, TS} <: AbstractModel{TS}
 
                           grid :: G         # Grid of physical points on which `Model` is solved
                   architecture :: A         # Computer `Architecture` on which `Model` is run
