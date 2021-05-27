aa5f3f489a2463508e6fd82a52fc5f187ecf1063
Gregory Wagner <wagner.greg@gmail.com>
2021-05-27 13:18:46 -0800

    Converts centered_second_order flux functions to appropriate form

--- src/Advection/centered_second_order.jl
+++ src/Advection/centered_second_order.jl
@@ -8,17 +8,17 @@ boundary_buffer(::CenteredSecondOrder) = 0
 
 const C2 = CenteredSecondOrder
 
-@inline momentum_flux_uu(i, j, k, grid, ::C2, U, u) = ℑxᶜᵃᵃ(i, j, k, grid, Ax_uᶠᶜᶜ, U) * ℑxᶜᵃᵃ(i, j, k, grid, u)
-@inline momentum_flux_uv(i, j, k, grid, ::C2, V, u) = ℑxᶠᵃᵃ(i, j, k, grid, Ay_vᶜᶠᶜ, V) * ℑyᵃᶠᵃ(i, j, k, grid, u)
-@inline momentum_flux_uw(i, j, k, grid, ::C2, W, u) = ℑxᶠᵃᵃ(i, j, k, grid, Az_wᶜᶜᵃ, W) * ℑzᵃᵃᶠ(i, j, k, grid, u)
+@inline advective_momentum_flux_Uu(i, j, k, grid, ::C2, U, u) = ℑxᶜᵃᵃ(i, j, k, grid, Ax_uᶠᶜᶜ, U) * ℑxᶜᵃᵃ(i, j, k, grid, u)
+@inline advective_momentum_flux_Vu(i, j, k, grid, ::C2, V, u) = ℑxᶠᵃᵃ(i, j, k, grid, Ay_vᶜᶠᶜ, V) * ℑyᵃᶠᵃ(i, j, k, grid, u)
+@inline advective_momentum_flux_Wu(i, j, k, grid, ::C2, W, u) = ℑxᶠᵃᵃ(i, j, k, grid, Az_wᶜᶜᵃ, W) * ℑzᵃᵃᶠ(i, j, k, grid, u)
 
-@inline momentum_flux_vu(i, j, k, grid, ::C2, U, v) = ℑyᵃᶠᵃ(i, j, k, grid, Ax_uᶠᶜᶜ, U) * ℑxᶠᵃᵃ(i, j, k, grid, v)
-@inline momentum_flux_vv(i, j, k, grid, ::C2, V, v) = ℑyᵃᶜᵃ(i, j, k, grid, Ay_vᶜᶠᶜ, V) * ℑyᵃᶜᵃ(i, j, k, grid, v)
-@inline momentum_flux_vw(i, j, k, grid, ::C2, W, v) = ℑyᵃᶠᵃ(i, j, k, grid, Az_wᶜᶜᵃ, W) * ℑzᵃᵃᶠ(i, j, k, grid, v)
+@inline advective_momentum_flux_Uv(i, j, k, grid, ::C2, U, v) = ℑyᵃᶠᵃ(i, j, k, grid, Ax_uᶠᶜᶜ, U) * ℑxᶠᵃᵃ(i, j, k, grid, v)
+@inline advective_momentum_flux_Vv(i, j, k, grid, ::C2, V, v) = ℑyᵃᶜᵃ(i, j, k, grid, Ay_vᶜᶠᶜ, V) * ℑyᵃᶜᵃ(i, j, k, grid, v)
+@inline advective_momentum_flux_Wv(i, j, k, grid, ::C2, W, v) = ℑyᵃᶠᵃ(i, j, k, grid, Az_wᶜᶜᵃ, W) * ℑzᵃᵃᶠ(i, j, k, grid, v)
 
-@inline momentum_flux_wu(i, j, k, grid, ::C2, U, w) = ℑzᵃᵃᶠ(i, j, k, grid, Ax_uᶠᶜᶜ, U) * ℑxᶠᵃᵃ(i, j, k, grid, w)
-@inline momentum_flux_wv(i, j, k, grid, ::C2, V, w) = ℑzᵃᵃᶠ(i, j, k, grid, Ay_vᶜᶠᶜ, V) * ℑyᵃᶠᵃ(i, j, k, grid, w)
-@inline momentum_flux_ww(i, j, k, grid, ::C2, W, w) = ℑzᵃᵃᶜ(i, j, k, grid, Az_wᶜᶜᵃ, W) * ℑzᵃᵃᶜ(i, j, k, grid, w)
+@inline advective_momentum_flux_Uw(i, j, k, grid, ::C2, U, w) = ℑzᵃᵃᶠ(i, j, k, grid, Ax_uᶠᶜᶜ, U) * ℑxᶠᵃᵃ(i, j, k, grid, w)
+@inline advective_momentum_flux_Vw(i, j, k, grid, ::C2, V, w) = ℑzᵃᵃᶠ(i, j, k, grid, Ay_vᶜᶠᶜ, V) * ℑyᵃᶠᵃ(i, j, k, grid, w)
+@inline advective_momentum_flux_Ww(i, j, k, grid, ::C2, W, w) = ℑzᵃᵃᶜ(i, j, k, grid, Az_wᶜᶜᵃ, W) * ℑzᵃᵃᶜ(i, j, k, grid, w)
 
 # Calculate the flux of a tracer quantity c through the faces of a cell.
 # In this case, the fluxes are given by u*Ax*c̄ˣ, v*Ay*c̄ʸ, and w*Az*c̄ᶻ.
