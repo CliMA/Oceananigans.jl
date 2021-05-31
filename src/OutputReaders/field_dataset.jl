4558f09bee969bb1a775891b6b1826851c168484
ali.hh.ramadhan@gmail.com <ali.hh.ramadhan@gmail.com>
2021-05-31 10:45:10 -0400

    More output reader quality of life features

--- src/OutputReaders/field_dataset.jl
+++ src/OutputReaders/field_dataset.jl
@@ -36,5 +36,7 @@ function FieldDataset(filepath; architecture=CPU(), backend=InMemory(), metadata
     return FieldDataset(ds, metadata, abspath(filepath))
 end
 
+Base.getindex(fds::FieldDataset, inds...) = Base.getindex(fds.fields, inds...)
+
 Base.show(io::IO, fds::FieldDataset) where {X, Y, Z, K, A} =
     print(io, "FieldDataset with $(length(fds.fields)) fields and $(length(fds.metadata)) metadata entries.")
