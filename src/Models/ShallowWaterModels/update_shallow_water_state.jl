5d0f887217424f319b7ce62e71cf8416a91f0443
Gregory Wagner <wagner.greg@gmail.com>
2021-06-02 10:36:57 -0800

    Fixes bug in update_shallow_water_state

--- src/Models/ShallowWaterModels/update_shallow_water_state.jl
+++ src/Models/ShallowWaterModels/update_shallow_water_state.jl
@@ -11,9 +11,9 @@ Fill halo regions for `model.solution` and `model.tracers`.
 function update_state!(model::ShallowWaterModel)
 
     # Mask immersed fields
-    masking_events = Tuple(mask_immersed_field!(field) for field in merge((uh=model.solution.uh, vh=model.solution.vh), model.tracers))
+    masking_events = Tuple(mask_immersed_field!(field) for field in (model.solution.uh, vh=model.solution.vh))
 
-    wait(device(model.architecture), MultiEvent(tuple(masking_events..., height_masking_event)))
+    wait(device(model.architecture), MultiEvent(masking_events))
 
     # Fill halos for velocities and tracers
     fill_halo_regions!(merge(model.solution, model.tracers),
