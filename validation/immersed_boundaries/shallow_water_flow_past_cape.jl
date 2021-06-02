219e2292f1d5201d5154b668ad6e5a632084aa20
Gregory Wagner <wagner.greg@gmail.com>
2021-06-02 09:50:02 -0800

    Updates

--- validation/immersed_boundaries/shallow_water_flow_past_cape.jl
+++ validation/immersed_boundaries/shallow_water_flow_past_cape.jl
@@ -6,25 +6,26 @@ using Oceananigans.OutputReaders: FieldTimeSeries
 using Oceananigans.Models
 using Oceananigans.ImmersedBoundaries: ImmersedBoundaryGrid, GridFittedBoundary
 
-underlying_grid = RegularRectilinearGrid(size=(256, 64), x=(-5, 15), y=(0, 5), topology=(Periodic, Bounded, Flat))
+underlying_grid = RegularRectilinearGrid(size=(256, 64), x=(-5, 15), y=(0, 5), topology=(Periodic, Bounded, Flat), halo = (3, 3))
                               
 cape(x, y, z)  = y < exp(-x^2)
+
 grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBoundary(cape))
 
-U(x, y, z) = 5 - y
+const Ly = grid.Ly
+U(x, y, z) = 1 - y / Ly
 
 damping_rate = 0.01 # relax fields on a 100 second time-scale
 const x0 = -13.0 # center point of sponge
 const dx = 1.0 # sponge width
 smoothed_step_mask(x, y, z) = 1/2 * (1 + tanh((x - x0) / dx))
 
-uh_sponge = Relaxation(rate=damping_rate, mask=smoothed_step_mask, target=U)
- h_sponge = Relaxation(rate=damping_rate, mask=smoothed_step_mask, target=U)
+uh_sponge = Relaxation(rate = damping_rate, mask = smoothed_step_mask, target = (x, y, z, t) -> U(x, y, z))
+ h_sponge = Relaxation(rate = damping_rate, mask = smoothed_step_mask, target = (x, y, z, t) -> U(x, y, z))
 
-model = ShallowWaterModel(grid = grid, gravitational_acceleration=1,
-                          forcing = (uh=uh_sponge, h=h_sponge))
+model = ShallowWaterModel(grid = grid, gravitational_acceleration = 1, forcing = (uh=uh_sponge, h=h_sponge))
 
-set!(model, h=1, uh=U)
+set!(model, h = 1, uh = U)
 
 wall_clock = [time_ns()]
 
@@ -41,7 +42,7 @@ function progress(sim)
     return nothing
 end
 
-wizard = TimeStepWizard(cfl=0.5, Δt=0.1*grid.Δx, max_change=1.1, max_Δt=0.5)
+wizard = TimeStepWizard(cfl=0.5, Δt=0.001*grid.Δx, max_change=1.1, max_Δt=0.5)
 
 simulation = Simulation(model, Δt=wizard, stop_time=1, progress=progress, iteration_interval=10)
 
