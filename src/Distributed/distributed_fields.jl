7122264994b5990c5de71471d82425035af8be41
Gregory Wagner <wagner.greg@gmail.com>
2021-06-03 14:33:39 -0800

    Use inner constructor for fields on distributed architectures

--- src/Distributed/distributed_fields.jl
+++ src/Distributed/distributed_fields.jl
@@ -6,6 +6,7 @@ function Field(X, Y, Z, arch::AbstractMultiArchitecture, grid::AbstractGrid,
                data = new_data(eltype(grid), arch, grid, (X, Y, Z)))
 
     communicative_bcs = inject_halo_communication_boundary_conditions(bcs, arch.local_rank, arch.connectivity)
+    validate_field_data(X, Y, Z, data, grid)
 
-    return Field(X, Y, Z, arch, grid, communicative_bcs, data)
+    return Field{X, Y, Z}(data, arch, grid, bcs)
 end
