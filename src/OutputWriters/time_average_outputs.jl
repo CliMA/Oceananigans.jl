e5a3d10df0d350af541613b5a47e963252f8f35d
Gregory L. Wagner <wagner.greg@gmail.com>
2021-07-02 17:31:58 -0600

    Create copies of AveragedTimeInterval for each output (#1807)
    * Test concurrent `WindowedTimeAverage` on multiple outputs
    
    Attempting to reproduce #1517 in a test.
    
    * Adds missing comma to time-averaging test
    
    * Omit test groups other than Simulation for now
    
    * Fixes set! in mulitple time averages test
    
    * Adds wait back to pipeline
    
    * Fix reference to tracers.c
    
    * Adds test for time averaging a single NetCDF output
    
    * Creates copies of AveragedTimeInterval for each output
    
    * Restores full buildkite pipeline
    
    * Update Project.toml

--- src/OutputWriters/time_average_outputs.jl
+++ src/OutputWriters/time_average_outputs.jl
@@ -9,7 +9,7 @@ Returns the `TimeInterval` associated with `schedule` and a `NamedTuple` or `Dic
 outputs.
 """
 function time_average_outputs(schedule::AveragedTimeInterval, outputs::Dict, model, field_slicer)
-    averaged_outputs = Dict(name => WindowedTimeAverage(output, model; schedule=schedule, field_slicer=field_slicer)
+    averaged_outputs = Dict(name => WindowedTimeAverage(output, model; schedule=copy(schedule), field_slicer=field_slicer)
                             for (name, output) in outputs)
 
     return TimeInterval(schedule), averaged_outputs
@@ -18,7 +18,7 @@ end
 function time_average_outputs(schedule::AveragedTimeInterval, outputs::NamedTuple, model, field_slicer)
     output_names = Tuple(keys(outputs))
 
-    output_values = Tuple(WindowedTimeAverage(outputs[name], model; schedule=schedule, field_slicer=field_slicer)
+    output_values = Tuple(WindowedTimeAverage(outputs[name], model; schedule=copy(schedule), field_slicer=field_slicer)
                           for name in output_names)
 
     averaged_outputs = NamedTuple{output_names}(output_values)
