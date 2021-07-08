024d5bb67e9b0bc92995b9f1b62f74b3de80d795
Navid Constantinou <navidcy@gmail.com>
2021-07-08 10:39:48 +1000

    change rationals to floats-manyway they are decorated with FT()

--- src/Advection/weno_fifth_order.jl
+++ src/Advection/weno_fifth_order.jl
@@ -20,64 +20,64 @@ const AG = AbstractGrid
 ##### ENO interpolants of size 3
 #####
 
-@inline left_biased_px₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(1//3) * ψ[i-1, j, k] + FT(5//6) * ψ[i,   j, k] -  FT(1//6) * ψ[i+1, j, k]
-@inline left_biased_px₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds - FT(1//6) * ψ[i-2, j, k] + FT(5//6) * ψ[i-1, j, k] +  FT(1//3) * ψ[i,   j, k]
-@inline left_biased_px₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(1//3) * ψ[i-3, j, k] - FT(7//6) * ψ[i-2, j, k] + FT(11//6) * ψ[i-1, j, k]
+@inline left_biased_px₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(1/3) * ψ[i-1, j, k] + FT(5/6) * ψ[i,   j, k] -  FT(1/6) * ψ[i+1, j, k]
+@inline left_biased_px₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds - FT(1/6) * ψ[i-2, j, k] + FT(5/6) * ψ[i-1, j, k] +  FT(1/3) * ψ[i,   j, k]
+@inline left_biased_px₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(1/3) * ψ[i-3, j, k] - FT(7/6) * ψ[i-2, j, k] + FT(11/6) * ψ[i-1, j, k]
 
-@inline left_biased_py₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(1//3) * ψ[i, j-1, k] + FT(5//6) * ψ[i, j,   k] -  FT(1//6) * ψ[i, j+1, k]
-@inline left_biased_py₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds - FT(1//6) * ψ[i, j-2, k] + FT(5//6) * ψ[i, j-1, k] +  FT(1//3) * ψ[i, j  , k]
-@inline left_biased_py₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(1//3) * ψ[i, j-3, k] - FT(7//6) * ψ[i, j-2, k] + FT(11//6) * ψ[i, j-1, k]
+@inline left_biased_py₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(1/3) * ψ[i, j-1, k] + FT(5/6) * ψ[i, j,   k] -  FT(1/6) * ψ[i, j+1, k]
+@inline left_biased_py₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds - FT(1/6) * ψ[i, j-2, k] + FT(5/6) * ψ[i, j-1, k] +  FT(1/3) * ψ[i, j  , k]
+@inline left_biased_py₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(1/3) * ψ[i, j-3, k] - FT(7/6) * ψ[i, j-2, k] + FT(11/6) * ψ[i, j-1, k]
 
-@inline left_biased_pz₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(1//3) * ψ[i, j, k-1] + FT(5//6) * ψ[i, j,   k] -  FT(1//6) * ψ[i, j, k+1]
-@inline left_biased_pz₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds - FT(1//6) * ψ[i, j, k-2] + FT(5//6) * ψ[i, j, k-1] +  FT(1//3) * ψ[i, j,   k]
-@inline left_biased_pz₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(1//3) * ψ[i, j, k-3] - FT(7//6) * ψ[i, j, k-2] + FT(11//6) * ψ[i, j, k-1]
+@inline left_biased_pz₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(1/3) * ψ[i, j, k-1] + FT(5/6) * ψ[i, j,   k] -  FT(1/6) * ψ[i, j, k+1]
+@inline left_biased_pz₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds - FT(1/6) * ψ[i, j, k-2] + FT(5/6) * ψ[i, j, k-1] +  FT(1/3) * ψ[i, j,   k]
+@inline left_biased_pz₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(1/3) * ψ[i, j, k-3] - FT(7/6) * ψ[i, j, k-2] + FT(11/6) * ψ[i, j, k-1]
 
-@inline right_biased_px₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(11//6) * ψ[i,   j, k] - FT(7//6) * ψ[i+1, j, k] + FT(1//3) * ψ[i+2, j, k]
-@inline right_biased_px₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds +  FT(1//3) * ψ[i-1, j, k] + FT(5//6) * ψ[i,   j, k] - FT(1//6) * ψ[i+1, j, k]
-@inline right_biased_px₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds -  FT(1//6) * ψ[i-2, j, k] + FT(5//6) * ψ[i-1, j, k] + FT(1//3) * ψ[i,   j, k]
+@inline right_biased_px₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(11/6) * ψ[i,   j, k] - FT(7/6) * ψ[i+1, j, k] + FT(1/3) * ψ[i+2, j, k]
+@inline right_biased_px₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds +  FT(1/3) * ψ[i-1, j, k] + FT(5/6) * ψ[i,   j, k] - FT(1/6) * ψ[i+1, j, k]
+@inline right_biased_px₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds -  FT(1/6) * ψ[i-2, j, k] + FT(5/6) * ψ[i-1, j, k] + FT(1/3) * ψ[i,   j, k]
 
-@inline right_biased_py₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(11//6) * ψ[i,   j, k] - FT(7//6) * ψ[i, j+1, k] + FT(1//3) * ψ[i, j+2, k]
-@inline right_biased_py₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds +  FT(1//3) * ψ[i, j-1, k] + FT(5//6) * ψ[i,   j, k] - FT(1//6) * ψ[i, j+1, k]
-@inline right_biased_py₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds -  FT(1//6) * ψ[i, j-2, k] + FT(5//6) * ψ[i, j-1, k] + FT(1//3) * ψ[i,   j, k]
+@inline right_biased_py₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(11/6) * ψ[i,   j, k] - FT(7/6) * ψ[i, j+1, k] + FT(1/3) * ψ[i, j+2, k]
+@inline right_biased_py₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds +  FT(1/3) * ψ[i, j-1, k] + FT(5/6) * ψ[i,   j, k] - FT(1/6) * ψ[i, j+1, k]
+@inline right_biased_py₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds -  FT(1/6) * ψ[i, j-2, k] + FT(5/6) * ψ[i, j-1, k] + FT(1/3) * ψ[i,   j, k]
 
-@inline right_biased_pz₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(11//6) * ψ[i, j,   k] - FT(7//6) * ψ[i, j, k+1] + FT(1//3) * ψ[i, j, k+2]
-@inline right_biased_pz₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds +  FT(1//3) * ψ[i, j, k-1] + FT(5//6) * ψ[i, j,   k] - FT(1//6) * ψ[i, j, k+1]
-@inline right_biased_pz₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds -  FT(1//6) * ψ[i, j, k-2] + FT(5//6) * ψ[i, j, k-1] + FT(1//3) * ψ[i, j,   k]
+@inline right_biased_pz₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds + FT(11/6) * ψ[i, j,   k] - FT(7/6) * ψ[i, j, k+1] + FT(1/3) * ψ[i, j, k+2]
+@inline right_biased_pz₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds +  FT(1/3) * ψ[i, j, k-1] + FT(5/6) * ψ[i, j,   k] - FT(1/6) * ψ[i, j, k+1]
+@inline right_biased_pz₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds -  FT(1/6) * ψ[i, j, k-2] + FT(5/6) * ψ[i, j, k-1] + FT(1/3) * ψ[i, j,   k]
 
 #####
 ##### Jiang & Shu (1996) WENO smoothness indicators. See also Equation 2.63 in Shu (1998).
 #####
 
 # We use 32-bit integer to represent the exponent "2" for fast exponentiation.
-# See https://github.com/CliMA/Oceananigans.jl/pull/1770 for more information.
+# See https:/github.com/CliMA/Oceananigans.jl/pull/1770 for more information.
 const two_32 = Int32(2)
 
-@inline left_biased_βx₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i-1, j, k] - 2ψ[i,   j, k] + ψ[i+1, j, k])^two_32 + FT(1//4) * (3ψ[i-1, j, k] - 4ψ[i,   j, k] +  ψ[i+1, j, k])^two_32
-@inline left_biased_βx₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i-2, j, k] - 2ψ[i-1, j, k] + ψ[i,   j, k])^two_32 + FT(1//4) * ( ψ[i-2, j, k]                 -  ψ[i,   j, k])^two_32
-@inline left_biased_βx₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i-3, j, k] - 2ψ[i-2, j, k] + ψ[i-1, j, k])^two_32 + FT(1//4) * ( ψ[i-3, j, k] - 4ψ[i-2, j, k] + 3ψ[i-1, j, k])^two_32
+@inline left_biased_βx₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i-1, j, k] - 2ψ[i,   j, k] + ψ[i+1, j, k])^two_32 + FT(1/4) * (3ψ[i-1, j, k] - 4ψ[i,   j, k] +  ψ[i+1, j, k])^two_32
+@inline left_biased_βx₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i-2, j, k] - 2ψ[i-1, j, k] + ψ[i,   j, k])^two_32 + FT(1/4) * ( ψ[i-2, j, k]                 -  ψ[i,   j, k])^two_32
+@inline left_biased_βx₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i-3, j, k] - 2ψ[i-2, j, k] + ψ[i-1, j, k])^two_32 + FT(1/4) * ( ψ[i-3, j, k] - 4ψ[i-2, j, k] + 3ψ[i-1, j, k])^two_32
 
-@inline left_biased_βy₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i, j-1, k] - 2ψ[i, j,   k] + ψ[i, j+1, k])^two_32 + FT(1//4) * (3ψ[i, j-1, k] - 4ψ[i,   j, k] +  ψ[i, j+1, k])^two_32
-@inline left_biased_βy₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i, j-2, k] - 2ψ[i, j-1, k] + ψ[i, j,   k])^two_32 + FT(1//4) * ( ψ[i, j-2, k]                 -  ψ[i,   j, k])^two_32
-@inline left_biased_βy₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i, j-3, k] - 2ψ[i, j-2, k] + ψ[i, j-1, k])^two_32 + FT(1//4) * ( ψ[i, j-3, k] - 4ψ[i, j-2, k] + 3ψ[i, j-1, k])^two_32
+@inline left_biased_βy₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i, j-1, k] - 2ψ[i, j,   k] + ψ[i, j+1, k])^two_32 + FT(1/4) * (3ψ[i, j-1, k] - 4ψ[i,   j, k] +  ψ[i, j+1, k])^two_32
+@inline left_biased_βy₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i, j-2, k] - 2ψ[i, j-1, k] + ψ[i, j,   k])^two_32 + FT(1/4) * ( ψ[i, j-2, k]                 -  ψ[i,   j, k])^two_32
+@inline left_biased_βy₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i, j-3, k] - 2ψ[i, j-2, k] + ψ[i, j-1, k])^two_32 + FT(1/4) * ( ψ[i, j-3, k] - 4ψ[i, j-2, k] + 3ψ[i, j-1, k])^two_32
 
-@inline left_biased_βz₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i, j, k-1] - 2ψ[i, j,   k] + ψ[i, j, k+1])^two_32 + FT(1//4) * (3ψ[i, j, k-1] - 4ψ[i, j,   k] +  ψ[i, j, k+1])^two_32
-@inline left_biased_βz₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i, j, k-2] - 2ψ[i, j, k-1] + ψ[i, j,   k])^two_32 + FT(1//4) * ( ψ[i, j, k-2]                 -  ψ[i, j,   k])^two_32
-@inline left_biased_βz₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i, j, k-3] - 2ψ[i, j, k-2] + ψ[i, j, k-1])^two_32 + FT(1//4) * ( ψ[i, j, k-3] - 4ψ[i, j, k-2] + 3ψ[i, j, k-1])^two_32
+@inline left_biased_βz₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i, j, k-1] - 2ψ[i, j,   k] + ψ[i, j, k+1])^two_32 + FT(1/4) * (3ψ[i, j, k-1] - 4ψ[i, j,   k] +  ψ[i, j, k+1])^two_32
+@inline left_biased_βz₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i, j, k-2] - 2ψ[i, j, k-1] + ψ[i, j,   k])^two_32 + FT(1/4) * ( ψ[i, j, k-2]                 -  ψ[i, j,   k])^two_32
+@inline left_biased_βz₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i, j, k-3] - 2ψ[i, j, k-2] + ψ[i, j, k-1])^two_32 + FT(1/4) * ( ψ[i, j, k-3] - 4ψ[i, j, k-2] + 3ψ[i, j, k-1])^two_32
 
 # Right-biased smoothness indicators are a reflection or "symmetric modification" of the left-biased smoothness
 # indicators around grid point `i-1/2`.
 
-@inline right_biased_βx₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i,   j, k] - 2ψ[i+1, j, k] + ψ[i+2, j, k])^two_32 + FT(1//4) * ( ψ[i,   j, k] - 4ψ[i+1, j, k] + 3ψ[i+2, j, k])^two_32
-@inline right_biased_βx₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i-1, j, k] - 2ψ[i,   j, k] + ψ[i+1, j, k])^two_32 + FT(1//4) * ( ψ[i-1, j, k]                 -  ψ[i+1, j, k])^two_32
-@inline right_biased_βx₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i-2, j, k] - 2ψ[i-1, j, k] + ψ[i,   j, k])^two_32 + FT(1//4) * (3ψ[i-2, j, k] - 4ψ[i-1, j, k] +  ψ[i,   j, k])^two_32
+@inline right_biased_βx₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i,   j, k] - 2ψ[i+1, j, k] + ψ[i+2, j, k])^two_32 + FT(1/4) * ( ψ[i,   j, k] - 4ψ[i+1, j, k] + 3ψ[i+2, j, k])^two_32
+@inline right_biased_βx₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i-1, j, k] - 2ψ[i,   j, k] + ψ[i+1, j, k])^two_32 + FT(1/4) * ( ψ[i-1, j, k]                 -  ψ[i+1, j, k])^two_32
+@inline right_biased_βx₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i-2, j, k] - 2ψ[i-1, j, k] + ψ[i,   j, k])^two_32 + FT(1/4) * (3ψ[i-2, j, k] - 4ψ[i-1, j, k] +  ψ[i,   j, k])^two_32
 
-@inline right_biased_βy₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i, j,   k] - 2ψ[i, j+1, k] + ψ[i, j+2, k])^two_32 + FT(1//4) * ( ψ[i,   j, k] - 4ψ[i, j+1, k] + 3ψ[i, j+2, k])^two_32
-@inline right_biased_βy₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i, j-1, k] - 2ψ[i, j,   k] + ψ[i, j+1, k])^two_32 + FT(1//4) * ( ψ[i, j-1, k]                 -  ψ[i, j+1, k])^two_32
-@inline right_biased_βy₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i, j-2, k] - 2ψ[i, j-1, k] + ψ[i, j,   k])^two_32 + FT(1//4) * (3ψ[i, j-2, k] - 4ψ[i, j-1, k] +  ψ[i,   j, k])^two_32
+@inline right_biased_βy₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i, j,   k] - 2ψ[i, j+1, k] + ψ[i, j+2, k])^two_32 + FT(1/4) * ( ψ[i,   j, k] - 4ψ[i, j+1, k] + 3ψ[i, j+2, k])^two_32
+@inline right_biased_βy₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i, j-1, k] - 2ψ[i, j,   k] + ψ[i, j+1, k])^two_32 + FT(1/4) * ( ψ[i, j-1, k]                 -  ψ[i, j+1, k])^two_32
+@inline right_biased_βy₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i, j-2, k] - 2ψ[i, j-1, k] + ψ[i, j,   k])^two_32 + FT(1/4) * (3ψ[i, j-2, k] - 4ψ[i, j-1, k] +  ψ[i,   j, k])^two_32
 
-@inline right_biased_βz₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i, j,   k] - 2ψ[i, j, k+1] + ψ[i, j, k+2])^two_32 + FT(1//4) * ( ψ[i, j,   k] - 4ψ[i, j, k+1] + 3ψ[i, j, k+2])^two_32
-@inline right_biased_βz₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i, j, k-1] - 2ψ[i, j,   k] + ψ[i, j, k+1])^two_32 + FT(1//4) * ( ψ[i, j, k-1]                 -  ψ[i, j, k+1])^two_32
-@inline right_biased_βz₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13//12) * (ψ[i, j, k-2] - 2ψ[i, j, k-1] + ψ[i, j,   k])^two_32 + FT(1//4) * (3ψ[i, j, k-2] - 4ψ[i, j, k-1] +  ψ[i, j,   k])^two_32
+@inline right_biased_βz₀(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i, j,   k] - 2ψ[i, j, k+1] + ψ[i, j, k+2])^two_32 + FT(1/4) * ( ψ[i, j,   k] - 4ψ[i, j, k+1] + 3ψ[i, j, k+2])^two_32
+@inline right_biased_βz₁(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i, j, k-1] - 2ψ[i, j,   k] + ψ[i, j, k+1])^two_32 + FT(1/4) * ( ψ[i, j, k-1]                 -  ψ[i, j, k+1])^two_32
+@inline right_biased_βz₂(i, j, k, grid::AG{FT}, ψ) where FT = @inbounds FT(13/12) * (ψ[i, j, k-2] - 2ψ[i, j, k-1] + ψ[i, j,   k])^two_32 + FT(1/4) * (3ψ[i, j, k-2] - 4ψ[i, j, k-1] +  ψ[i, j,   k])^two_32
 
 #####
 ##### WENO-5 optimal weights
@@ -93,7 +93,7 @@ const C3₂ = 1/10
 
 # Note: these constants may need to be changed for smooth solutions and/or fine grid.
 # Note note: we use 32-bit integer to represent the exponent "2" for fast exponentiation.
-# see https://github.com/CliMA/Oceananigans.jl/pull/1770 for more information.
+# see https:/github.com/CliMA/Oceananigans.jl/pull/1770 for more information.
 const ƞ = Int32(2) # WENO exponent
 const ε = 1e-6
 
