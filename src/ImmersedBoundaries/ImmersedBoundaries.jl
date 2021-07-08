74f2230063f3e4aff5b01e390e2e4fa30f9115ff
Gregory Wagner <wagner.greg@gmail.com>
2021-07-08 07:37:48 -0600

    Extend all_x_nodes for ImmersedBoundaryGrid

--- src/ImmersedBoundaries/ImmersedBoundaries.jl
+++ src/ImmersedBoundaries/ImmersedBoundaries.jl
@@ -40,7 +40,7 @@ import Oceananigans.Utils: cell_advection_timescale
 import Oceananigans.Solvers: PressureSolver
 import Oceananigans.Grids: with_halo
 import Oceananigans.Coriolis: φᶠᶠᵃ
-import Oceananigans.Grids: with_halo, xnode, ynode, znode
+import Oceananigans.Grids: with_halo, xnode, ynode, znode, all_x_nodes, all_y_nodes, all_z_nodes
 
 import Oceananigans.Advection:
     _advective_momentum_flux_Uu,
@@ -121,6 +121,10 @@ PressureSolver(arch, ibg::ImmersedBoundaryGrid) = PressureSolver(arch, ibg.grid)
 @inline xnode(LX, LY, LZ, i, j, k, ibg::ImmersedBoundaryGrid) = xnode(LX, LY, LZ, i, j, k, ibg.grid)
 @inline ynode(LX, LY, LZ, i, j, k, ibg::ImmersedBoundaryGrid) = ynode(LX, LY, LZ, i, j, k, ibg.grid)
 @inline znode(LX, LY, LZ, i, j, k, ibg::ImmersedBoundaryGrid) = znode(LX, LY, LZ, i, j, k, ibg.grid)
+
+all_x_nodes(loc, ibg::ImmersedBoundaryGrid) = all_x_nodes(loc, ibg.grid)
+all_y_nodes(loc, ibg::ImmersedBoundaryGrid) = all_x_nodes(loc, ibg.grid)
+all_z_nodes(loc, ibg::ImmersedBoundaryGrid) = all_x_nodes(loc, ibg.grid)
 
 include("immersed_grid_metrics.jl")
 include("grid_fitted_immersed_boundary.jl")
