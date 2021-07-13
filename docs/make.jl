d3d4d6ae4af83cefc593165e9da78f8780b13295
Gregory Wagner <wagner.greg@gmail.com>
2021-07-13 16:45:05 -0400

    Docs despration

--- docs/make.jl
+++ docs/make.jl
@@ -116,14 +116,16 @@ numerical_pages = [
     "Large eddy simulation" => "numerical_implementation/large_eddy_simulation.md"
 ]
 
+#=
 appendix_pages = [
-    "Staggered grid" => "appendix/staggered_grid.md",
-    "Fractional step method" => "appendix/fractional_step.md",
-    "Convergence tests" => "appendix/convergence_tests.md",
-    "Performance benchmarks" => "appendix/benchmarks.md",
-    "Library" => "appendix/library.md",
-    "Function index" => "appendix/function_index.md",
+    #"Staggered grid" => "appendix/staggered_grid.md",
+    #"Fractional step method" => "appendix/fractional_step.md",
+    #"Convergence tests" => "appendix/convergence_tests.md",
+    #"Performance benchmarks" => "appendix/benchmarks.md",
+    # "Library" => "appendix/library.md",
+    # "Function index" => "appendix/function_index.md",
 ]
+=#
 
 pages = [
     "Home" => "index.md",
