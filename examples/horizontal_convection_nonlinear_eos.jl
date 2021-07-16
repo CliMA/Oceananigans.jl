eaa2e916e935c1a555d54aae2c93ddd300d23224
Navid Constantinou <navidcy@users.noreply.github.com>
2021-07-16 11:08:20 +1000

    an attempt to have nonlin eos

---
+++ examples/horizontal_convection_nonlinear_eos.jl
@@ -0,0 +1,404 @@
+# # Horizontal convection example
+#
+# In this example, we initialize flow at rest under the effect of gravity and in which a 
+# non-uniform buoyancy along the top surface is imposed. This problems setup is referred to 
+# as "horizontal convection".
+#
+# This example demonstrates:
+#
+#   * How to use `ComputedField`s for output
+#   * How to post-process saved output
+#
+# ## Install dependencies
+#
+# First let's make sure we have all required packages installed.
+
+# ```julia
+# using Pkg
+# pkg"add Oceananigans, SeawaterPolynomials, JLD2, Plots"
+# ```
+
+# ## Horizontal convection
+#
+# We'll consider here the two-dimensional verion of horizontal convection in which a flow
+# ``\boldsymbol{u} = (u, w)`` on the ``(x, z)``-plane is evolved under the effect of gravity
+# and with a prescribed, non-uniform buoyancy at the top-surface of the domain.
+#
+# ```math
+# \begin{gather}
+# \partial_t \boldsymbol{u} + \boldsymbol{u \cdot \nabla} \boldsymbol{u} + \boldsymbol{\nabla}p = b \hat{\boldsymbol{z}} + \nu \nabla^2 \boldsymbol{u} \, , \\
+# \partial_t b + \boldsymbol{u \cdot \nabla} b = \kappa \nabla^2 b \, , \\
+# \boldsymbol{\nabla \cdot u} = 0 \, .
+# \end{gather}
+# ```
+# 
+# The domain here is ``-L_x/2 \le x \le L_x/2`` and ``-H \le z \le 0``.
+#
+# ### Boundary conditions
+#
+# At the surface, the imposed buoyancy is ``b_s(x, z = 0, t) = - b_* \cos(2 \pi x / L_x)``
+# while zero-flux boundary conditions are imposed on all other boundaries. We use free-slip 
+# boundary conditions on ``u`` and ``w`` at all boundaries.
+#
+# ### Non-dimensional control parameters
+#
+# The problem is characterized by three non-dimensional parameters. The first is the domain's
+# aspect ratio, ``A = L_x / H`` and the other two are the Rayleigh (``Ra``) and Prandtl (``Pr``)
+# numbers:
+#
+# ```math
+# Ra = \frac{L_x^3 b_*}{\nu \kappa} \, , \quad \text{and}\, \quad Pr = \frac{\nu}{\kappa} \, .
+# ```
+#
+# The Prandtl number expresses the ratio of momentum over heat diffusion; the Rayleigh number
+# is a measure of the relative importance of gravity over viscosity in the momentum equation.
+#
+# For a domain with a given extent, the nondimensional values of ``Ra`` and ``Pr`` uniquely
+# determine the viscosity and diffusivity, i.e.,
+# 
+# ```math
+# \nu = \sqrt{\frac{Pr b_* L_x^3}{Ra}} \quad \text{and} \quad \kappa = \sqrt{\frac{b_* L_x^3}{Pr Ra}} \, .
+# ```
+#
+# Now let's code these things up! We start off by importing `Oceananigans` and `Printf`.
+
+using Oceananigans
+using SeawaterPolynomials: RoquetEquationOfState, ρ′
+using Printf
+
+# ## The grid
+#
+# We use a two-dimensional grid with an aspect ratio ``A = L_x / H = 2``.
+
+H = 1.0          # vertical domain extent
+Lx = 2H          # horizontal domain extent
+Nx, Nz = 128, 64 # horizontal, vertical resolution
+
+grid = RegularRectilinearGrid(size = (Nx, Nz),
+                                 x = (-Lx/2, Lx/2),
+                                 z = (-H, 0),
+                              halo = (3, 3),
+                          topology = (Bounded, Flat, Bounded))
+
+# Any attempts for `VerticallyStretchedRectilinearGrid` failed...
+# 
+# ```
+# σ = 0.2  # stretching factor
+# 
+# hyperbolically_spaced_faces(k) = H - H * (1 - tanh(σ * (k - 1) / Nz) / tanh(σ))
+# 
+# grid_stretched = VerticallyStretchedRectilinearGrid(size = (Nx, Nz),
+#                                                 topology = (Bounded, Flat, Bounded),
+#                                                        x = (-Lx/2, Lx/2),
+#                                                     halo = (3, 3),
+#                                                  z_faces = hyperbolically_spaced_faces)
+# 
+# # We plot vertical spacing versus depth to inspect the prescribed grid stretching:
+# using Plots
+# 
+# plot(grid.Δzᵃᵃᶜ[1:Nz], grid.zᵃᵃᶜ[1:Nz],
+#      marker = :circle,
+#      ylabel = "Depth",
+#      xlabel = "Vertical spacing",
+#      legend = nothing)
+# ```
+
+# ## Boundary conditions
+#
+# We impose a surface buoyancy boundary condition,
+#
+# ```math
+# T(x, z=0, t) = - T_* \cos (2π x / L_x) \, .
+# ```
+
+const T★ = 5.0
+const background_temperature = 10 # ᵒC
+
+@inline Tˢ(x, y, t) = - T★ * cos(π * x) + background_temperature
+
+top_bc = ValueBoundaryCondition(Tˢ)
+T_bcs = FieldBoundaryConditions(top = top_bc)
+
+ρ₀ = 1000
+
+roquet_eos = RoquetEquationOfState(:Cabbeling, reference_density=ρ₀)
+buoyancy = SeawaterBuoyancy(equation_of_state=roquet_eos, constant_salinity=0)
+
+b₋ = buoyancy.gravitational_acceleration * ρ′(background_temperature + T★, 0, 0, roquet_eos) / ρ₀
+b₊ = buoyancy.gravitational_acceleration * ρ′(background_temperature - T★, 0, 0, roquet_eos) / ρ₀
+
+Δb = b₊ - b₋
+
+# ## Turbulence closures
+#
+# We use isotropic viscosity and diffusivities, `ν` and `κ` whose values are obtain from the
+# prescribed ``Ra`` and ``Pr`` numbers. Here, we use ``Pr = 1`` and ``Ra = 10^8``:
+
+Pr = 1.0   # The Prandtl number
+Ra = 1e16   # The Rayleigh number
+
+ν = sqrt(Pr * Δb * Lx^3 / Ra)  # Laplacian viscosity
+κ = ν * Pr                     # Laplacian diffusivity
+nothing # hide
+
+t★ = sqrt(H / Δb)
+
+
+# ## Model instantiation
+#
+# We instantiate the model with the fifth-order WENO advection scheme, a 3rd order
+# Runge-Kutta time-stepping scheme, and a `BuoyancyTracer`.
+
+
+model = IncompressibleModel(
+           architecture = CPU(),
+                   grid = grid,
+              advection = WENO5(),
+            timestepper = :RungeKutta3,
+                tracers = :T,
+               buoyancy = buoyancy,
+                closure = IsotropicDiffusivity(ν=ν, κ=κ),
+    boundary_conditions = (T=T_bcs,)
+    )
+
+# ## Simulation set-up
+#
+# We set up a simulation that runs up to ``t = 40`` with a `JLD2OutputWriter` that saves the
+# flow speed, ``\sqrt{u^2 + w^2}``, the vorticity, ``\partial_z u - \partial_x w``, and the 
+# buoyancy dissipation, ``\kappa |\boldsymbol{\nabla} b|^2``.
+#
+# ### The `TimeStepWizard`
+#
+# The TimeStepWizard manages the time-step adaptively, keeping the Courant-Freidrichs-Lewy 
+# (CFL) number close to `0.75` while ensuring the time-step does not increase beyond the 
+# maximum allowable value for numerical stability.
+
+max_Δt = t★/5
+wizard = TimeStepWizard(cfl=0.8, Δt=t★/20, max_change=1.2, max_Δt=max_Δt)
+
+# ### A progress messenger
+#
+# We write a function that prints out a helpful progress message while the simulation runs.
+
+CFL = AdvectiveCFL(wizard)
+
+start_time = time_ns()
+
+progress(sim) = @printf("i: % 6d, sim time: % 1.3f, wall time: % 10s, Δt: % 1.4f, CFL: %.2e\n",
+                        sim.model.clock.iteration,
+                        sim.model.clock.time,
+                        prettytime(1e-9 * (time_ns() - start_time)),
+                        sim.Δt.Δt,
+                        CFL(sim.model))
+nothing # hide
+
+# ### Build the simulation
+#
+# We're ready to build and run the simulation. We ask for a progress message and time-step update
+# every 100 iterations,
+
+simulation = Simulation(model, Δt = wizard, iteration_interval = 20,
+                                                     stop_time = 1000t★,
+                                                      progress = progress)
+
+# ### Output
+#
+# We use `ComputedField`s to diagnose and output the total flow speed, the vorticity, ``\zeta``,
+# and the buoyancy dissipation, ``\chi = \kappa |\boldsymbol{\nabla}b|^2``. Note that 
+# `ComputedField`s take "AbstractOperations"on `Field`s as input:
+
+u, v, w = model.velocities # unpack velocity `Field`s
+b = BuoyancyField(model)   # unpack buoyancy `Field`
+
+## total flow speed
+speed = ComputedField(sqrt(u^2 + w^2))
+
+## y-component of vorticity
+ζ = ComputedField(∂z(u) - ∂x(w))
+
+outputs = (s = speed, b = b, ζ = ζ)
+nothing # hide
+
+# We create a `JLD2OutputWriter` that saves the speed, the vorticity, and the buoyancy dissipation.
+# We then add the `JLD2OutputWriter` to the `simulation`.
+
+saved_output_prefix = "horizontal_convection"
+saved_output_filename = saved_output_prefix * ".jld2"
+
+simulation.output_writers[:fields] = JLD2OutputWriter(model, outputs,
+                                                  field_slicer = nothing,
+                                                      schedule = TimeInterval(t★),
+                                                        prefix = saved_output_prefix,
+                                                         force = true)
+nothing # hide
+
+# Ready to press the big red button:
+
+run!(simulation)
+
+# ## Visualizing horizontal convection
+#
+# We animate the results by opening the JLD2 file, extracting data for the iterations we ended
+# up saving at, and ploting the saved fields. We prepare for animating the flow by creating 
+# coordinate arrays, opening the file, building a vector of the iterations that we saved data at,
+# and defining a function for computing colorbar limits:
+
+using JLD2, Plots
+
+## Open the file with our data
+file = jldopen(saved_output_filename)
+κ = file["closure/κ/T"]
+
+s_timeseries = FieldTimeSeries(saved_output_filename, "s")
+b_timeseries = FieldTimeSeries(saved_output_filename, "b")
+ζ_timeseries = FieldTimeSeries(saved_output_filename, "ζ")
+
+times = b_timeseries.times
+
+## Coordinate arrays
+xs, ys, zs = nodes(s_timeseries[1])
+xb, yb, zb = nodes(b_timeseries[1])
+xζ, yζ, zζ = nodes(ζ_timeseries[1])
+
+
+## Extract a vector of iterations
+iterations = parse.(Int, keys(file["timeseries/t"]))
+
+# The utilities below come handy for calculating nice contour intervals:
+
+function nice_levels(c, clim, nlevels=41)
+    levels = range(0, stop=clim, length=nlevels)
+    cmax = maximum(abs, c)
+    clim < cmax && (levels = vcat(levels, [cmax]))
+    return levels
+end
+
+function nice_divergent_levels(c, clim, nlevels=41)
+    levels = range(-clim, stop=clim, length=nlevels)
+    return levels
+end
+
+nothing # hide
+
+# Now we're ready to animate.
+
+@info "Making an animation from saved data..."
+
+anim = @animate for i in 1:50:length(times)
+
+    ## Load fields from file
+    t = times[i]
+    s_snapshot = interior(s_timeseries[i])[:, 1, :]
+    ζ_snapshot = interior(ζ_timeseries[i])[:, 1, :]
+    
+    b = b_timeseries[i]
+    χ = ComputedField(κ * (∂x(b)^2 + ∂z(b)^2))
+    compute!(χ)
+    
+    b_snapshot = interior(b)[:, 1, :]
+    χ_snapshot = interior(χ)[:, 1, :]
+    
+    ## determine colorbar limits and contour levels
+    slim = 0.6 * maximum(s_timeseries)
+    slevels = vcat(range(0, stop=slim, length=31), [slim])
+
+    blim = 0.6 * maximum(b_timeseries)
+    blevels = vcat([-1], range(-blim, stop=blim, length=51), [1])
+    
+    ζlim = 0.6 * maximum(ζ_timeseries)
+    ζlevels = nice_divergent_levels(ζ_snapshot, ζlim)
+
+    χlim = 0.00025
+    χlevels = nice_levels(χ_snapshot, χlim)
+
+    @info @sprintf("Drawing frame %d:", i)
+
+    kwargs = (      xlims = (-Lx/2, Lx/2),
+                    ylims = (-H, 0),
+                   xlabel = "x / H",
+                   ylabel = "z / H",
+              aspectratio = 1,
+                linewidth = 0)
+
+    s_plot = contourf(xs, zs, s_snapshot';
+                      clims = (0, slim), levels = slevels,
+                      color = :speed, kwargs...)
+    # s_title = @sprintf("speed √[(u²+w²)/(b⋆H)] @ t=%1.2f", t)
+    
+    b_plot = contourf(xb, zb, b_snapshot';
+                      clims = (0, blim), levels = blevels,
+                      color = :thermal, kwargs...)
+    # b_title = @sprintf("buoyancy, b/b⋆ @ t=%1.2f", t)
+    
+    ζ_plot = contourf(xζ, zζ, clamp.(ζ_snapshot', -ζlim, ζlim);
+                      clims=(-ζlim, ζlim), levels = ζlevels,
+                      color = :balance, kwargs...)
+    # ζ_title = @sprintf("vorticity, (∂u/∂z - ∂w/∂x) √(H/b⋆) @ t=%1.2f", t)
+    
+    χ_plot = contourf(xs, zs, χ_snapshot';
+                      clims = (0, χlim), levels = χlevels,
+                      color = :dense, kwargs...)
+    # χ_title = @sprintf("buoyancy dissipation, κ|∇b|² √(H/b⋆⁵) @ t=%1.2f", t)
+    
+    plot(s_plot, b_plot, ζ_plot, χ_plot,
+           size = (700, 1200),
+           link = :x,
+         layout = Plots.grid(4, 1))
+          # title = [s_title, b_title, ζ_title, χ_title])
+end
+
+mp4(anim, "horizontal_convection_nonlin_eos.mp4", fps = 16) # hide
+
+#=
+# ### The Nusselt number
+#
+# We are often interested on how much the flow enhances the mixing. This is quantified by the
+# Nusselt number, which measure how much the fluid flow enhances mixing occurs compared if only
+# diffusion was in operation. The Nusselt number is given by
+#
+# ```math
+# Nu = \frac{\langle \chi \rangle}{\langle \chi_{\rm diff} \rangle} \, ,
+# ```
+#
+# where angle brackets above denote a spatial and time average and  ``\chi_{\rm diff}`` is
+# the buoyancy dissipation without any flow, i.e., it's the buoyancy dissipation associated
+# with the buoyancy distribution that satisfies
+#
+# ```math
+# \kappa \nabla^2 b_{\rm diff} = 0 \, ,
+# ```
+#
+# with the same boundary conditions same as our setup.
+
+using JLD2, Plots
+using Oceananigans
+using Oceananigans.AbstractOperations: volume
+using Oceananigans.Fields
+
+saved_output_prefix = "horizontal_convection"
+saved_output_filename = saved_output_prefix * ".jld2"
+
+file = jldopen(saved_output_filename)
+κ = file["closure/κ/T"]
+
+b_timeseries = FieldTimeSeries(saved_output_filename, "b")
+
+t = b_timeseries.times
+grid = b_timeseries.grid
+
+∫ⱽ_mod_∇b = ReducedField(Nothing, Nothing, Nothing, CPU(), grid, dims=(1, 2, 3))
+Nu_from_b = zeros(length(t))
+
+## Can we broadcast over the time dimension?
+for i = 1:length(t)
+    b = b_timeseries[i]
+    sum!(∫ⱽ_mod_∇b,  κ * (∂x(b)^2+∂z(b)^2) * volume)
+    Nu_from_b[i] = ∫ⱽ_mod_∇b[1, 1, 1] / (κ * π * 2π/Lx * tanh(π))
+end
+
+plot(t, Nu_from_b,
+       xlabel = "time",
+       ylabel = "Nu(t)",
+    linewidth = 3,
+       legend = :none)
+=#
