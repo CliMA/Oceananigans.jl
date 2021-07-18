d86947f96ec20d457d9ac4efe8e64fb1864ae8c5
Navid Constantinou <navidcy@users.noreply.github.com>
2021-07-19 05:55:42 +1000

    incompressible -> nonhydrostatic

---
+++ src/Models/NonhydrostaticModels/update_nonhydrostatic_model_state.jl
@@ -0,0 +1,35 @@
+using Oceananigans.Architectures
+using Oceananigans.BoundaryConditions
+using Oceananigans.TurbulenceClosures: calculate_diffusivities!
+
+import Oceananigans.TimeSteppers: update_state!
+
+"""
+    update_state!(model::NonhydrostaticModel)
+
+Update peripheral aspects of the model (halo regions, diffusivities, hydrostatic pressure) to the current model state.
+"""
+function update_state!(model::NonhydrostaticModel)
+
+    # Fill halos for velocities and tracers
+    fill_halo_regions!(merge(model.velocities, model.tracers), model.architecture,  model.clock, fields(model))
+
+    # Calculate diffusivities
+    calculate_diffusivities!(model.diffusivities, model.architecture, model.grid, model.closure,
+                             model.buoyancy, model.velocities, model.tracers)
+
+    fill_halo_regions!(model.diffusivities, model.architecture, model.clock, fields(model))
+
+    # Calculate hydrostatic pressure
+    pressure_calculation = launch!(model.architecture, model.grid, :xy, update_hydrostatic_pressure!,
+                                   model.pressures.pHY′, model.grid, model.buoyancy, model.tracers,
+                                   dependencies=Event(device(model.architecture)))
+
+    # Fill halo regions for pressure
+    wait(device(model.architecture), pressure_calculation)
+
+    fill_halo_regions!(model.pressures.pHY′, model.architecture)
+
+    return nothing
+end
+
