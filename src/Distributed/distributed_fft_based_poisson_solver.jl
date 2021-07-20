2bd0647926562cd444ca52d8b5f633f819edf796
Gregory Wagner <wagner.greg@gmail.com>
2021-07-20 08:25:00 -0400

    Whos grid is it anyways?"

--- src/Distributed/distributed_fft_based_poisson_solver.jl
+++ src/Distributed/distributed_fft_based_poisson_solver.jl
@@ -32,31 +32,6 @@ function DistributedFFTBasedPoissonSolver(arch, full_grid, local_grid)
     return DistributedFFTBasedPoissonSolver(arch, plan, full_grid, local_grid, eigenvalues, storage)
 end
 
-#=
-function solve_poisson_equation!(solver::DistributedFFTBasedPoissonSolver)
-    λx, λy, λz = solver.eigenvalues
-
-    # Apply forward transforms.
-    solver.plan * solver.storage
-
-    # Solve the discrete Poisson equation.
-    RHS = ϕ = solver.storage[2]
-    @. ϕ = - RHS / (λx + λy + λz)
-
-    # Setting DC component of the solution (the mean) to be zero. This is also
-    # necessary because the source term to the Poisson equation has zero mean
-    # and so the DC component comes out to be ∞.
-    if MPI.Comm_rank(MPI.COMM_WORLD) == 0
-        ϕ[1, 1, 1] = 0
-    end
-
-    # Apply backward transforms.
-    solver.plan \ solver.storage
-
-    return nothing
-end
-=#
-
 function solve!(x, solver::DistributedFFTBasedPoissonSolver, b, r=0)
     λx, λy, λz = solver.eigenvalues
 
--- src/Distributed/distributed_solve_for_pressure.jl
+++ src/Distributed/distributed_solve_for_pressure.jl
@@ -4,7 +4,7 @@ import Oceananigans.Models.NonhydrostaticModels: solve_for_pressure!
 function solve_for_pressure!(pressure, solver::DistributedFFTBasedPoissonSolver, Δt, U★)
     rhs = first(solver.storage)
     arch = solver.architecture
-    grid = solver.grid
+    grid = solver.my_grid
 
     rhs_event = launch!(arch, grid, :xyz, calculate_pressure_source_term_fft_based_solver!,
                         rhs, grid, Δt, U★, dependencies = device_event(arch))
