75fa9a85df2d147cb3896e3b7f9c94821cc9116b
Gregory L. Wagner <wagner.greg@gmail.com>
2021-07-23 21:08:28 -0400

    Introducing Callback for Simulations (#1894)
    * Implements Callback
    
    * Adds simulation time+iteration utilitiy functions
    
    * Hmm lets not return the simulation after run! afterall

--- src/Oceananigans.jl
+++ src/Oceananigans.jl
@@ -70,7 +70,7 @@ export
     Clock, TimeStepWizard, time_step!,
 
     # Simulations
-    Simulation, run!,
+    Simulation, run!, Callback, iteration,
     iteration_limit_exceeded, stop_time_exceeded, wall_time_limit_exceeded,
 
     # Diagnostics
