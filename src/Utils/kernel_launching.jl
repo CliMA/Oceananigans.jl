7fb49bf21fde166cc2e02e2b5c9e390c4991a9ff
Gregory Wagner <wagner.greg@gmail.com>
2021-07-26 10:42:55 -0400

    Use a better heuristic for CPU kernel workgroup

--- src/Utils/kernel_launching.jl
+++ src/Utils/kernel_launching.jl
@@ -6,9 +6,26 @@ using KernelAbstractions
 using Oceananigans.Architectures
 using Oceananigans.Grids
 
-const MAX_THREADS_PER_BLOCK = 256
-
 flatten_reduced_dimensions(worksize, dims) = Tuple(i ∈ dims ? 1 : worksize[i] for i = 1:3)
+
+const MAX_GPU_THREADS_PER_BLOCK = 256
+
+function heuristic_workgroup(arch, Nx, Ny, Nz)
+    # One-dimensional column models:
+    Nx == 1 && Ny == 1 return (1, 1)
+
+    # Two-dimensional y-z slice models:
+    Nx == 1 && return (1, min(MAX_GPU_THREADS_PER_BLOCK, Ny))
+
+    # Two-dimensional x-z slice models:
+    Ny == 1 && return (1, min(MAX_GPU_THREADS_PER_BLOCK, Nx))
+
+    # Three-dimensional GPU models
+    arch isa GPU && return (Int(√MAX_GPU_THREADS_PER_BLOCK), Int(√MAX_GPU_THREADS_PER_BLOCK))
+
+    # Three-dimensional CPU models
+    return (Nx, Ny)
+end
 
 """
     work_layout(grid, dims; include_right_boundaries=false, location=nothing)
@@ -23,28 +40,12 @@ to be specified.
 
 For more information, see: https://github.com/CliMA/Oceananigans.jl/pull/308
 """
-function work_layout(grid, dims; include_right_boundaries=false, location=nothing, reduced_dimensions=())
+function work_layout(arch, grid, dims; include_right_boundaries=false, location=nothing, reduced_dimensions=())
 
     Nx, Ny, Nz = size(grid)
 
-    workgroup = Nx == 1 && Ny == 1 ?
-
-                    # One-dimensional column models:
-                    (1, 1) :
-
-                Nx == 1 ?
-
-                    # Two-dimensional y-z slice models:
-                    (1, min(MAX_THREADS_PER_BLOCK, Ny)) :
-
-                Ny == 1 ?
-
-                    # Two-dimensional x-z slice models:
-                    (1, min(MAX_THREADS_PER_BLOCK, Nx)) :
-
-                    # Three-dimensional models
-                    (Int(√MAX_THREADS_PER_BLOCK), Int(√MAX_THREADS_PER_BLOCK))
-
+    workgroup = heuristic_workgroup(arch, Nx, Ny, Nz)
+   
     Nx′, Ny′, Nz′ = include_right_boundaries ? size(location, grid) : (Nx, Ny, Nz)
 
     Nx′, Ny′, Nz′ = flatten_reduced_dimensions((Nx′, Ny′, Nz′), reduced_dimensions)
@@ -75,10 +76,10 @@ function launch!(arch, grid, dims, kernel!, args...;
                  location = nothing,
                  kwargs...)
 
-    workgroup, worksize = work_layout(grid, dims,
+    workgroup, worksize = work_layout(arch, grid, dims,
                                       include_right_boundaries = include_right_boundaries,
-                                            reduced_dimensions = reduced_dimensions,
-                                                      location = location)
+                                      reduced_dimensions = reduced_dimensions,
+                                      location = location)
 
     loop! = kernel!(Architectures.device(arch), workgroup, worksize)
 
