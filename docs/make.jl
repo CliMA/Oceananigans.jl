d867823b3c76f6aaf31da9bec5d7727ee229329f
Navid Constantinou <navidcy@users.noreply.github.com>
2021-07-26 10:03:16 +1000

    change order of pages in Docs/Numerical Implementation

--- docs/make.jl
+++ docs/make.jl
@@ -108,10 +108,10 @@ physics_pages = [
 ]
 
 numerical_pages = [
-    "Pressure decomposition" => "numerical_implementation/pressure_decomposition.md",
-    "Time stepping" => "numerical_implementation/time_stepping.md",
     "Finite volume method" => "numerical_implementation/finite_volume.md",
     "Spatial operators" => "numerical_implementation/spatial_operators.md",
+    "Pressure decomposition" => "numerical_implementation/pressure_decomposition.md",
+    "Time stepping" => "numerical_implementation/time_stepping.md",
     "Boundary conditions" => "numerical_implementation/boundary_conditions.md",
     "Poisson solvers" => "numerical_implementation/poisson_solvers.md",
     "Large eddy simulation" => "numerical_implementation/large_eddy_simulation.md"
