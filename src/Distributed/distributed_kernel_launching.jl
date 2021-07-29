97dc42a63301ae81d098c8299dae49a4dabd0080
Gregory Wagner <wagner.greg@gmail.com>
2021-07-29 09:48:31 -0400

    Extends heuristic workgroup for distributed architectures

--- src/Distributed/distributed_kernel_launching.jl
+++ src/Distributed/distributed_kernel_launching.jl
@@ -1,7 +1,8 @@
-import Oceananigans.Utils: launch!
+import Oceananigans.Utils: launch!, heuristic_workgroup
 
 function launch!(arch::AbstractMultiArchitecture, args...; kwargs...)
     child_arch = child_architecture(arch)
     return launch!(child_arch, args...; kwargs...)
 end
 
+heuristic_workgroup(arch::AbstractMultiArchitecture, Nx, Ny, Nz) = heuristic_workgroup(child_architecture(arch), Nx, Ny, Nz)
