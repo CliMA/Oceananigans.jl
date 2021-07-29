18f0e196af2e51b63572441120b6d1068648ef3d
tomchor <tomaschor@gmail.com>
2021-07-29 10:40:00 -0700

    hopefully fixed tests

--- test/test_regression.jl
+++ test/test_regression.jl
@@ -71,7 +71,7 @@ include("regression_tests/ocean_large_eddy_simulation_regression_test.jl")
                 run_rayleigh_benard_regression_test(arch, grid_type)
             end
 
-            for closure in (AnisotropicMinimumDissipation(ν=1.05e-6, κ=1.46e-7), SmagorinskyLilly(ν=1.05e-6, κ=1.46e-7))
+            for closure in (AnisotropicMinimumDissipation(ν=1.05e-6, κ=1.46e-7), SmagorinskyLilly(C=0.23, Cb=1, Pr=1, ν=1.05e-6, κ=1.46e-7))
                 closurename = string(typeof(closure).name.wrapper)
                 @testset "Ocean large eddy simulation [$(typeof(arch)), $closurename, $grid_type grid]" begin
                     @info "  Testing oceanic large eddy simulation regression [$(typeof(arch)), $closurename, $grid_type grid]"
