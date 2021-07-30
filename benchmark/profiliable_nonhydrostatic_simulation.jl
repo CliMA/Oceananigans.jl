68ef4470c64f94f463e904fe33fcf09764bd3ab8
Gregory Wagner <wagner.greg@gmail.com>
2021-07-30 12:23:20 -0600

    Adds simple script for profiling NonhydrostaticModel

---
+++ benchmark/profiliable_nonhydrostatic_simulation.jl
@@ -0,0 +1,41 @@
+push!(LOAD_PATH, joinpath(@__DIR__, ".."))
+
+#using BenchmarkTools
+using CUDA
+using Oceananigans
+using Benchmarks
+
+# Benchmark parameters
+
+timestepper = :QuasiAdamsBashforth2
+Arch = CPU
+FT = Float64
+N = 128
+
+print_system_info()
+
+# Define benchmarks
+
+@info "Setting up benchmark: ($Arch, $FT, $N)..."
+
+grid = RegularRectilinearGrid(FT, size=(N, N, N), extent=(1, 1, 1))
+
+model = NonhydrostaticModel(architecture = Arch(),
+                            timestepper = timestepper,
+                            grid = grid,
+                            advection = WENO5(),
+                            tracers = :b,
+                            buoyancy = BuoyancyTracer(),
+                            closure = IsotropicDiffusivity(ν=1, κ=1))
+
+simulation = Simulation(model, Δt=1, stop_iteration=1)
+
+@info "warming up"
+
+run!(simulation)
+
+simulation.stop_iteration += 10
+
+CUDA.@profile run!(simulation)
+
+@info "done profiling ($Arch, $FT, $N)"
