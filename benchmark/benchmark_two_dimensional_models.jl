945de132857e0e9b7e812b831976fb2e1c419751
Gregory Wagner <wagner.greg@gmail.com>
2021-08-03 18:22:02 -0600

    Use pushfirst! and adds a profiling script

--- benchmark/benchmark_two_dimensional_models.jl
+++ benchmark/benchmark_two_dimensional_models.jl
@@ -1,14 +1,18 @@
+pushfirst!(LOAD_PATH, joinpath(@__DIR__, ".."))
+
 using Oceananigans
 using Oceananigans: short_show
 using Oceananigans.TimeSteppers: time_step!
 using BenchmarkTools
 
-xy_grid = RegularRectilinearGrid(size = (512, 512, 1), halo = (3, 3, 3), extent = (2π, 2π, 2π), topology = (Periodic, Periodic, Bounded))
-xz_grid = RegularRectilinearGrid(size = (512, 1, 512), halo = (3, 3, 3), extent = (2π, 2π, 2π), topology = (Periodic, Periodic, Bounded))
-yz_grid = RegularRectilinearGrid(size = (1, 512, 512), halo = (3, 3, 3), extent = (2π, 2π, 2π), topology = (Periodic, Periodic, Bounded))
+Nx = 64
+
+xy_grid = RegularRectilinearGrid(size = (Nx, Nx, 1), halo = (3, 3, 3), extent = (2π, 2π, 2π), topology = (Periodic, Periodic, Bounded))
+xz_grid = RegularRectilinearGrid(size = (Nx, 1, Nx), halo = (3, 3, 3), extent = (2π, 2π, 2π), topology = (Periodic, Periodic, Bounded))
+yz_grid = RegularRectilinearGrid(size = (1, Nx, Nx), halo = (3, 3, 3), extent = (2π, 2π, 2π), topology = (Periodic, Periodic, Bounded))
                               
-function ten_steps!(model)
-    for _ = 1:10
+function nsteps!(model, n)
+    for _ = 1:n
         time_step!(model, 1e-6)
     end
     return nothing
@@ -16,20 +20,21 @@ end
 
 arch = CPU() #for arch in (CPU(), GPU())
 
-for grid in (xy_grid,
-             xz_grid,
-             yz_grid)
+model_kwargs = (architecture = arch,
+                timestepper = :QuasiAdamsBashforth2,
+                advection = nothing,
+                closure = nothing,
+                buoyancy = nothing,
+                tracers = nothing)
 
-    model = NonhydrostaticModel(architecture = arch,
-                                timestepper = :QuasiAdamsBashforth2,
-                                grid = grid,
-                                advection = nothing,
-                                closure = nothing,
-                                buoyancy = nothing,
-                                tracers = nothing)
+grids = [xy_grid, xz_grid, yz_grid]
+models = [NonhydrostaticModel(; grid = grid, model_kwargs...) for grid in grids]
+xy_model, xz_model, yz_model = models
+                              
     
+for model in models
     time_step!(model, 1e-6) # warmup
-
-    @info "Benchmarking model with $(short_show(grid))..."
-    @btime ten_steps!($model)
 end
+
+# @info "Benchmarking model with $(short_show(grid))..."
+# @btime nsteps!($model, 100)
