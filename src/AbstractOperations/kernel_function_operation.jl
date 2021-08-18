777655a2be8b266add598604e3ead718eff3ee8d
tomchor <tomaschor@gmail.com>
2021-08-18 13:04:28 -0700

    moved user-facing function for KernelFunctionOperation out of the struct

--- src/AbstractOperations/kernel_function_operation.jl
+++ src/AbstractOperations/kernel_function_operation.jl
@@ -1,3 +1,19 @@
+struct KernelFunctionOperation{LX, LY, LZ, P, A, G, T, K, D} <: AbstractOperation{LX, LY, LZ, A, G, T}
+    kernel_function :: K
+    computed_dependencies :: D
+    parameters :: P
+    architecture :: A
+    grid :: G
+
+    function KernelFunctionOperation{LX, LY, LZ}(kernel_function::K, computed_dependencies::D,
+                                                 parameters::P, architecture::A, grid::G) where {LX, LY, LZ, K, G, A, D, P}
+        T = eltype(grid)
+        return new{LX, LY, LZ, P, A, G, T, K, D}(kernel_function, computed_dependencies, parameters, architecture, grid)
+    end
+
+end
+
+
 """
     KernelFunctionOperation{LX, LY, LZ}(kernel_function, grid; architecture=nothing,
                                         computed_dependencies=(), parameters=nothing)
@@ -29,7 +45,7 @@ random_kernel_function(i, j, k, grid) = rand() # use CUDA.rand on the GPU
 kernel_op = KernelFunctionOperation{Center, Center, Center}(random_kernel_function, grid; architecture=CPU())
 ```
 
-Constrcut a kernel function operation using the vertical vorticity operator
+Construct a kernel function operation using the vertical vorticity operator
 valid on curvilinear and cubed sphere grids:
 
 ```julia
@@ -38,38 +54,22 @@ using Oceananigans.Operators: ζ₃ᶠᶠᵃ # called with signature ζ₃ᶠᶠ
 grid = model.grid
 u, v, w = model.velocities
 
-ζ_op = KernelFunctionOperation{Face, Face, Center}(ζ₃ᶠᶠᵃ, CPU(), grid, computed_dependencies=(u, v))
+ζ_op = KernelFunctionOperation{Face, Face, Center}(ζ₃ᶠᶠᵃ, grid, computed_dependencies=(u, v))
 ```
 """
-struct KernelFunctionOperation{LX, LY, LZ, P, A, G, T, K, D} <: AbstractOperation{LX, LY, LZ, A, G, T}
-    kernel_function :: K
-    computed_dependencies :: D
-    parameters :: P
-    architecture :: A
-    grid :: G
+function KernelFunctionOperation{LX, LY, LZ}(kernel_function,
+                                             grid;
+                                             architecture = nothing,
+                                             computed_dependencies = (),
+                                             parameters = nothing) where {LX, LY, LZ}
 
-    function KernelFunctionOperation{LX, LY, LZ}(kernel_function::K, computed_dependencies::D,
-                                                 parameters::P, architecture::A, grid::G) where {LX, LY, LZ, K, G, A, D, P}
-        T = eltype(grid)
-        return new{LX, LY, LZ, P, A, G, T, K, D}(kernel_function, computed_dependencies, parameters, architecture, grid)
-    end
+    arch = isnothing(architecture) ?
+        Oceananigans.Architectures.architecture(computed_dependencies...) :
+        architecture
 
-    function KernelFunctionOperation{LX, LY, LZ}(kernel_function::K,
-                                                 grid::G;
-                                                 architecture = nothing,
-                                                 computed_dependencies::D = (),
-                                                 parameters::P = nothing) where {LX, LY, LZ, K, G, D, P}
-        T = eltype(grid)
-
-        arch = isnothing(architecture) ?
-            Oceananigans.Architectures.architecture(computed_dependencies...) :
-            architecture
-
-        A = typeof(arch)
-
-        return new{LX, LY, LZ, P, A, G, T, K, D}(kernel_function, computed_dependencies, parameters, arch, grid)
-    end
+        return KernelFunctionOperation{LX, LY, LZ}(kernel_function, computed_dependencies, parameters, arch, grid)
 end
+
 
 @inline Base.getindex(κ::KernelFunctionOperation, i, j, k) = κ.kernel_function(i, j, k, κ.grid, κ.computed_dependencies..., κ.parameters)
 @inline Base.getindex(κ::KernelFunctionOperation{LX, LY, LZ, <:Nothing}, i, j, k) where {LX, LY, LZ} = κ.kernel_function(i, j, k, κ.grid, κ.computed_dependencies...)
