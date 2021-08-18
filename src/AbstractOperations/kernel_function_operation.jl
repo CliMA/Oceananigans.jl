a59d4d615c494e8901446ca455a673b42188c774
tomchor <tomaschor@gmail.com>
2021-08-18 08:55:51 -0700

    exported KernelFunctionOperation and moved docstring

--- src/AbstractOperations/kernel_function_operation.jl
+++ src/AbstractOperations/kernel_function_operation.jl
@@ -1,3 +1,46 @@
+"""
+    KernelFunctionOperation{LX, LY, LZ}(kernel_function, grid; architecture=nothing,
+                                        computed_dependencies=(), parameters=nothing)
+
+Constructs a `KernelFunctionOperation` at location `(LX, LY, LZ)` on `grid` an with
+an optional iterable of `computed_dependencies` and arbitrary `parameters`.
+
+With `isnothing(parameters)` (the default), `kernel_function` is called
+with
+
+```julia
+kernel_function(i, j, k, grid, computed_dependencies...)
+```
+
+Otherwise `kernel_function` is called with
+
+```julia
+kernel_function(i, j, k, grid, computed_dependencies..., parameters)
+```
+
+Examples
+========
+
+Construct a kernel function operation that returns random numbers:
+
+```julia
+random_kernel_function(i, j, k, grid) = rand() # use CUDA.rand on the GPU
+
+kernel_op = KernelFunctionOperation{Center, Center, Center}(random_kernel_function, grid; architecture=CPU())
+```
+
+Constrcut a kernel function operation using the vertical vorticity operator
+valid on curvilinear and cubed sphere grids:
+
+```julia
+using Oceananigans.Operators: ζ₃ᶠᶠᵃ # called with signature ζ₃ᶠᶠᵃ(i, j, k, grid, u, v)
+
+grid = model.grid
+u, v, w = model.velocities
+
+ζ_op = KernelFunctionOperation{Face, Face, Center}(ζ₃ᶠᶠᵃ, CPU(), grid, computed_dependencies=(u, v))
+```
+"""
 struct KernelFunctionOperation{LX, LY, LZ, P, A, G, T, K, D} <: AbstractOperation{LX, LY, LZ, A, G, T}
     kernel_function :: K
     computed_dependencies :: D
@@ -11,49 +54,6 @@ struct KernelFunctionOperation{LX, LY, LZ, P, A, G, T, K, D} <: AbstractOperatio
         return new{LX, LY, LZ, P, A, G, T, K, D}(kernel_function, computed_dependencies, parameters, architecture, grid)
     end
 
-    """
-        KernelFunctionOperation{LX, LY, LZ}(kernel_function, grid; architecture=nothing,
-                                            computed_dependencies=(), parameters=nothing)
- 
-    Constructs a `KernelFunctionOperation` at location `(LX, LY, LZ)` on `grid` an with
-    an optional iterable of `computed_dependencies` and arbitrary `parameters`.
-
-    With `isnothing(parameters)` (the default), `kernel_function` is called
-    with
-
-    ```julia
-    kernel_function(i, j, k, grid, computed_dependencies...)
-    ```
-
-    Otherwise `kernel_function` is called with
-
-    ```julia
-    kernel_function(i, j, k, grid, computed_dependencies..., parameters)
-    ```
-
-    Examples
-    ========
-
-    Construct a kernel function operation that returns random numbers:
-
-    ```julia
-    random_kernel_function(i, j, k, grid) = rand() # use CUDA.rand on the GPU
-
-    kernel_op = KernelFunctionOperation{Center, Center, Center}(random_kernel_function, grid; architecture=CPU())
-    ```
-
-    Constrcut a kernel function operation using the vertical vorticity operator
-    valid on curvilinear and cubed sphere grids:
-
-    ```julia
-    using Oceananigans.Operators: ζ₃ᶠᶠᵃ # called with signature ζ₃ᶠᶠᵃ(i, j, k, grid, u, v)
-
-    grid = model.grid
-    u, v, w = model.velocities
-
-    ζ_op = KernelFunctionOperation{Face, Face, Center}(ζ₃ᶠᶠᵃ, CPU(), grid, computed_dependencies=(u, v))
-    ```
-    """
     function KernelFunctionOperation{LX, LY, LZ}(kernel_function::K,
                                                  grid::G;
                                                  architecture = nothing,
