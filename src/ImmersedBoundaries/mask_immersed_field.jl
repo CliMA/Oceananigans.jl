77ee18c5a71f61f3f9da899fb3ce67853f20ba56
Gregory L. Wagner <wagner.greg@gmail.com>
2021-08-31 09:56:30 -0600

    Adds optional second argument `value` to `mask_immersed_field!`
    This optional positional argument can be used to mask a field with a value other than 0.
    
    cc @francispoulin @fadaie91

--- src/ImmersedBoundaries/mask_immersed_field.jl
+++ src/ImmersedBoundaries/mask_immersed_field.jl
@@ -5,14 +5,13 @@ using Oceananigans.Fields: location, AbstractDataField
 
 instantiate(X) = X()
 
-mask_immersed_field!(field::AbstractField, loc=location(field)) = mask_immersed_field!(field, field.grid, loc)
-mask_immersed_field!(field, grid, loc) = NoneEvent()
+mask_immersed_field!(field::AbstractField, value=zero(eltype(field.grid))) = mask_immersed_field!(field, field.grid, location(field), value)
+mask_immersed_field!(field, grid, loc, value) = NoneEvent()
 
-function mask_immersed_field!(field::AbstractDataField, grid::ImmersedBoundaryGrid, loc)
+function mask_immersed_field!(field::AbstractDataField, grid::ImmersedBoundaryGrid, loc, value)
     arch = architecture(field)
     loc = instantiate.(loc)
-    mask_value = zero(eltype(grid))
-    return launch!(arch, grid, :xyz, _mask_immersed_field!, field, loc, grid, mask_value; dependencies = device_event(arch))
+    return launch!(arch, grid, :xyz, _mask_immersed_field!, field, loc, grid, value; dependencies = device_event(arch))
 end
 
 @kernel function _mask_immersed_field!(field, loc, grid, value)
