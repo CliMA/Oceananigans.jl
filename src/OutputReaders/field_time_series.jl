a49e87f28011880cdb80be49e84e0b5bdb76855e
Gregory Wagner <wagner.greg@gmail.com>
2021-09-13 13:04:30 -0600

    Adds iterations and times keyword arguments to FieldTimeSeries

--- src/OutputReaders/field_time_series.jl
+++ src/OutputReaders/field_time_series.jl
@@ -31,29 +31,56 @@ end
 @propagate_inbounds Base.getindex(f::FieldTimeSeries{X, Y, Z, InMemory}, i, j, k, n) where {X, Y, Z} = f.data[i, j, k, n]
 
 """
-    FieldTimeSeries(filepath, name; architecture=CPU(), backend=InMemory(); grid=nothing)
+    FieldTimeSeries(filepath, name;
+                    architecture = CPU(),
+                    backend = InMemory(),
+                    grid = nothing,
+                    iterations = nothing,
+                    times = nothing)
 
 Returns a `FieldTimeSeries` for the field `name` describing a field's time history from a JLD2 file
-located at `filepath`. Note that model output must have been saved with halos. The `InMemory` backend
-will store the data fully in memory as a 4D multi-dimensional array while the `OnDisk` backend will
-lazily load field time snapshots when the `FieldTimeSeries` is indexed linearly.
+located at `filepath`. Note that model output must have been saved with halos.
 
-If `grid` is not supplied, it will be reconstructed from file.
+Keyword arguments
+=================
+
+- `archiecture`: The architecture on which to store time series data. CPU() by default.
+
+- `backend`: Whether to load all data in 4D array, or to lazily load data from disk
+             when indexing into `FieldTimeSeries`.
+
+- `grid`: A grid to associated with data, in the case that the native grid
+          was not serialized properly.
+
+- `iterations`: Iterations to load. Defaults to all iterations found in the file.
+
+- `times`: Save times to load, as determined through an approximate floating point
+           comparison to recorded save times. Defaults to times associated with `iterations`.
+           Takes precedence over `iterations` if `times` is specified.
 """
 FieldTimeSeries(filepath, name; architecture=CPU(), backend=InMemory(), kwargs...) =
     FieldTimeSeries(filepath, name, architecture, backend; kwargs...)
 
-function FieldTimeSeries(filepath, name, architecture, backend::InMemory; grid=nothing)
+function FieldTimeSeries(filepath, name, architecture, backend::InMemory; grid=nothing, iterations=nothing, times=nothing)
     file = jldopen(filepath)
 
-    if isnothing(grid)
-        grid = file["serialized/grid"]
+    isnothing(grid) && (grid = file["serialized/grid"])
+
+    if isnothing(times)
+        # times are not specified, but iterations may be
+        isnothing(iterations) && (iterations = parse.(Int, keys(file["timeseries/t"])))
+        times = [file["timeseries/t/$i"] for i in iterations]
+
+    else
+        # times are specified; iterations must be calculated
+        all_iterations = parse.(Int, keys(file["timeseries/t"]))
+        all_times = [file["timeseries/t/$i"] for i in all_iterations]
+
+        indices_to_load = [findfirst(time -> time ≈ time_to_load , all_times) for time_to_load in times]
+
+        iterations = all_iterations[indices_to_load]
+        times = all_times[indices_to_load]
     end
-
-    Hx, Hy, Hz = halo_size(grid)
-
-    iterations = parse.(Int, keys(file["timeseries/t"]))
-    times = [file["timeseries/t/$i"] for i in iterations]
 
     LX, LY, LZ = location = file["timeseries/$name/serialized/location"]
 
