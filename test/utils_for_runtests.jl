a814bafcbc365d6418a4fe2ba41af12bb2d362fa
Simone Silvestri <silvestri.simone0@gmail.com>
2021-11-02 15:09:36 -0400

    cleaned test

--- test/utils_for_runtests.jl
+++ test/utils_for_runtests.jl
@@ -1,3 +1,83 @@
+using Test
+using Printf
+using Random
+using Statistics
+using LinearAlgebra
+using Logging
+
+using CUDA
+using MPI
+using JLD2
+using FFTW
+using OffsetArrays
+using SeawaterPolynomials
+
+using Oceananigans
+using Oceananigans.Architectures
+using Oceananigans.Grids
+using Oceananigans.Operators
+using Oceananigans.Advection
+using Oceananigans.BoundaryConditions
+using Oceananigans.Fields
+using Oceananigans.Coriolis
+using Oceananigans.BuoyancyModels
+using Oceananigans.Forcings
+using Oceananigans.Solvers
+using Oceananigans.Models
+using Oceananigans.Simulations
+using Oceananigans.Diagnostics
+using Oceananigans.OutputWriters
+using Oceananigans.TurbulenceClosures
+using Oceananigans.AbstractOperations
+using Oceananigans.Distributed
+using Oceananigans.Logger
+using Oceananigans.Units
+using Oceananigans.Utils
+using Oceananigans.Architectures: device # to resolve conflict with CUDA.device
+
+using Dates: DateTime, Nanosecond
+using TimesDates: TimeDate
+using Statistics: mean
+using LinearAlgebra: norm
+using NCDatasets: Dataset
+using KernelAbstractions: @kernel, @index, Event
+
+import Oceananigans.Fields: interior
+import Oceananigans.Utils: launch!, datatuple
+
+Logging.global_logger(OceananigansLogger())
+
+#####
+##### Testing parameters
+#####
+
+float_types = (Float32, Float64)
+
+archs = CUDA.has_cuda() ? (GPU(),) : (CPU(),)
+
+closures = (
+    :IsotropicDiffusivity,
+    :AnisotropicDiffusivity,
+    :AnisotropicBiharmonicDiffusivity,
+    :TwoDimensionalLeith,
+    :SmagorinskyLilly,
+    :AnisotropicMinimumDissipation,
+    :HorizontallyCurvilinearAnisotropicDiffusivity,
+    :HorizontallyCurvilinearAnisotropicBiharmonicDiffusivity,
+    :ConvectiveAdjustmentVerticalDiffusivity
+)
+
+#####
+##### Run tests!
+#####
+
+CUDA.allowscalar(true)
+
+include("data_dependencies.jl")
+include("utils_for_runtests.jl")
+
+group = get(ENV, "TEST_GROUP", :all) |> Symbol
+
 using Oceananigans.TimeSteppers: QuasiAdamsBashforth2TimeStepper, RungeKutta3TimeStepper, update_state!
 
 #####
