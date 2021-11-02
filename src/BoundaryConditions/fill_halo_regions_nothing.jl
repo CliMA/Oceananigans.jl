973563b469c4a89bc65047af8d1306cdd0251945
Simone Silvestri <silvestri.simone0@gmail.com>
2021-11-02 18:21:46 -0400

    changed multi_event

--- src/BoundaryConditions/fill_halo_regions_nothing.jl
+++ src/BoundaryConditions/fill_halo_regions_nothing.jl
@@ -2,11 +2,11 @@
 ##### Nothing happens when your boundary condition is nothing
 #####
 
-fill_west_halo!(c, ::Nothing, arch, barrier, grid, args...) = nothing
-fill_east_halo!(c, ::Nothing, arch, barrier, grid, args...) = nothing
+fill_west_halo!(c, ::Nothing, arch, barrier, grid, args...) = NoneEvent()
+fill_east_halo!(c, ::Nothing, arch, barrier, grid, args...) = NoneEvent()
 
-fill_north_halo!(c, ::Nothing, arch, barrier, grid, args...) = nothing
-fill_south_halo!(c, ::Nothing, arch, barrier, grid, args...) = nothing
+fill_north_halo!(c, ::Nothing, arch, barrier, grid, args...) = NoneEvent()
+fill_south_halo!(c, ::Nothing, arch, barrier, grid, args...) = NoneEvent()
 
-fill_top_halo!(c, ::Nothing, arch, barrier, grid, args...)    = nothing
-fill_bottom_halo!(c, ::Nothing, arch, barrier, grid, args...) = nothing
+fill_top_halo!(c, ::Nothing, arch, barrier, grid, args...)    = NoneEvent()
+fill_bottom_halo!(c, ::Nothing, arch, barrier, grid, args...) = NoneEvent()
