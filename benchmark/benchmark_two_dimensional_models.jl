80ea593c555538ebbeda932247ea2f33b320022f
Gregory Wagner <wagner.greg@gmail.com>
2021-11-04 09:57:28 -0400

    Adds script for benchmarking two-dimensional models

---
+++ benchmark/benchmark_two_dimensional_models.jl
@@ -0,0 +1,39 @@
+pushfirst!(LOAD_PATH, joinpath(@__DIR__, ".."))
+
+using Oceananigans
+using Oceananigans: short_show
+using Oceananigans.TimeSteppers: time_step!
+using BenchmarkTools
+
+N = 64
+
+xy_grid = RegularRectilinearGrid(size = (N, N, 1), halo = (3, 3, 3), extent = (2π, 2π, 2π), topology = (Periodic, Periodic, Bounded))
+xz_grid = RegularRectilinearGrid(size = (N, 1, N), halo = (3, 3, 3), extent = (2π, 2π, 2π), topology = (Periodic, Periodic, Bounded))
+yz_grid = RegularRectilinearGrid(size = (1, N, N), halo = (3, 3, 3), extent = (2π, 2π, 2π), topology = (Periodic, Periodic, Bounded))
+                              
+function ten_steps!(model)
+    for _ = 1:10
+        time_step!(model, 1e-6)
+    end
+    return nothing
+end
+
+arch = CPU() #for arch in (CPU(), GPU())
+
+for grid in (xy_grid,
+             xz_grid,
+             yz_grid)
+
+    model = NonhydrostaticModel(architecture = arch,
+                                timestepper = :QuasiAdamsBashforth2,
+                                grid = grid,
+                                advection = nothing,
+                                closure = nothing,
+                                buoyancy = nothing,
+                                tracers = nothing)
+    
+    time_step!(model, 1e-6) # warmup
+
+    @info "Benchmarking model with $(short_show(grid))..."
+    @btime ten_steps!($model)
+end
