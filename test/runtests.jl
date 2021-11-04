a77cb8150e75f601267cb63952e2ea46857f76fd
Simone Silvestri <silvestri.simone0@gmail.com>
2021-11-03 22:44:34 -0400

    restored tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -79,104 +79,104 @@ archs = test_architectures()
 group = get(ENV, "TEST_GROUP", :all) |> Symbol
 
 @testset "Oceananigans" begin
-    # if group == :unit || group == :all
-    #     @testset "Unit tests" begin
-    #         include("test_grids.jl")
-    #         include("test_operators.jl")
-    #         include("test_boundary_conditions.jl")
-    #         include("test_field.jl")
-    #         include("test_reduced_field.jl")
-    #         include("test_averaged_field.jl")
-    #         include("test_kernel_computed_field.jl")
-    #         include("test_halo_regions.jl")
-    #         include("test_coriolis.jl")
-    #         include("test_buoyancy.jl")
-    #         include("test_stokes_drift.jl")
-    #         include("test_utils.jl")
-    #     end
-    # end
+    if group == :unit || group == :all
+        @testset "Unit tests" begin
+            include("test_grids.jl")
+            include("test_operators.jl")
+            include("test_boundary_conditions.jl")
+            include("test_field.jl")
+            include("test_reduced_field.jl")
+            include("test_averaged_field.jl")
+            include("test_kernel_computed_field.jl")
+            include("test_halo_regions.jl")
+            include("test_coriolis.jl")
+            include("test_buoyancy.jl")
+            include("test_stokes_drift.jl")
+            include("test_utils.jl")
+        end
+    end
 
-    # if group == :solvers || group == :all
-    #     @testset "Solvers" begin
-    #         include("test_batched_tridiagonal_solver.jl")
-    #         include("test_preconditioned_conjugate_gradient_solver.jl")
-    #         include("test_poisson_solvers.jl")
-    #     end
-    # end
+    if group == :solvers || group == :all
+        @testset "Solvers" begin
+            include("test_batched_tridiagonal_solver.jl")
+            include("test_preconditioned_conjugate_gradient_solver.jl")
+            include("test_poisson_solvers.jl")
+        end
+    end
 
-    # if group == :time_stepping_1 || group == :all
-    #     @testset "Model and time stepping tests (part 1)" begin
-    #         include("test_nonhydrostatic_models.jl")
-    #         include("test_time_stepping.jl")
-    #     end
-    # end
+    if group == :time_stepping_1 || group == :all
+        @testset "Model and time stepping tests (part 1)" begin
+            include("test_nonhydrostatic_models.jl")
+            include("test_time_stepping.jl")
+        end
+    end
 
-    # if group == :time_stepping_2 || group == :all
-    #     @testset "Model and time stepping tests (part 2)" begin
-    #         include("test_boundary_conditions_integration.jl")
-    #         include("test_forcings.jl")
-    #         include("test_turbulence_closures.jl")
-    #         include("test_dynamics.jl")
-    #     end
-    # end
+    if group == :time_stepping_2 || group == :all
+        @testset "Model and time stepping tests (part 2)" begin
+            include("test_boundary_conditions_integration.jl")
+            include("test_forcings.jl")
+            include("test_turbulence_closures.jl")
+            include("test_dynamics.jl")
+        end
+    end
 
-    # if group == :shallow_water || group == :all
-    #     include("test_shallow_water_models.jl")
-    # end
+    if group == :shallow_water || group == :all
+        include("test_shallow_water_models.jl")
+    end
 
-    # if group == :hydrostatic_free_surface || group == :all
-    #     @testset "HydrostaticFreeSurfaceModel tests" begin
-    #         include("test_hydrostatic_free_surface_models.jl")
-    #         include("test_hydrostatic_free_surface_immersed_boundaries.jl")
-    #         include("test_vertical_vorticity_field.jl")
-    #         include("test_implicit_free_surface_solver.jl")
-    #         include("test_hydrostatic_free_surface_immersed_boundaries_apply_surf_bc.jl")
-    #     end
-    # end
+    if group == :hydrostatic_free_surface || group == :all
+        @testset "HydrostaticFreeSurfaceModel tests" begin
+            include("test_hydrostatic_free_surface_models.jl")
+            include("test_hydrostatic_free_surface_immersed_boundaries.jl")
+            include("test_vertical_vorticity_field.jl")
+            include("test_implicit_free_surface_solver.jl")
+            include("test_hydrostatic_free_surface_immersed_boundaries_apply_surf_bc.jl")
+        end
+    end
 
-    # if group == :abstract_operations || group == :all
-    #     @testset "AbstractOperations and broadcasting tests" begin
-    #         include("test_abstract_operations.jl")
-    #         include("test_computed_field.jl")
-    #         include("test_broadcasting.jl")
-    #     end
-    # end
+    if group == :abstract_operations || group == :all
+        @testset "AbstractOperations and broadcasting tests" begin
+            include("test_abstract_operations.jl")
+            include("test_computed_field.jl")
+            include("test_broadcasting.jl")
+        end
+    end
 
-    # if group == :simulation || group == :all
-    #     @testset "Simulation tests" begin
-    #         include("test_simulations.jl")
-    #         include("test_diagnostics.jl")
-    #         include("test_output_writers.jl")
-    #         include("test_output_readers.jl")
-    #         include("test_lagrangian_particle_tracking.jl")
-    #     end
-    # end
+    if group == :simulation || group == :all
+        @testset "Simulation tests" begin
+            include("test_simulations.jl")
+            include("test_diagnostics.jl")
+            include("test_output_writers.jl")
+            include("test_output_readers.jl")
+            include("test_lagrangian_particle_tracking.jl")
+        end
+    end
 
-    # if group == :cubed_sphere || group == :all
-    #     @testset "Cubed sphere tests" begin
-    #         include("test_cubed_spheres.jl")
-    #         include("test_cubed_sphere_halo_exchange.jl")
-    #         include("test_cubed_sphere_circulation.jl")
-    #     end
-    # end
+    if group == :cubed_sphere || group == :all
+        @testset "Cubed sphere tests" begin
+            include("test_cubed_spheres.jl")
+            include("test_cubed_sphere_halo_exchange.jl")
+            include("test_cubed_sphere_circulation.jl")
+        end
+    end
 
-    # if group == :distributed || group == :all
-    #     MPI.Initialized() || MPI.Init()
-    #     include("test_distributed_models.jl")
-    #     include("test_distributed_poisson_solvers.jl")
-    # end
+    if group == :distributed || group == :all
+        MPI.Initialized() || MPI.Init()
+        include("test_distributed_models.jl")
+        include("test_distributed_poisson_solvers.jl")
+    end
 
     if group == :regression || group == :all
         include("test_regression.jl")
     end
 
-    # if group == :scripts || group == :all
-    #     @testset "Scripts" begin
-    #         include("test_validation.jl")
-    #     end
-    # end
+    if group == :scripts || group == :all
+        @testset "Scripts" begin
+            include("test_validation.jl")
+        end
+    end
 
-    # if group == :convergence
-    #     include("test_convergence.jl")
-    # end
+    if group == :convergence
+        include("test_convergence.jl")
+    end
 end
--- test/test_regression.jl
+++ test/test_regression.jl
@@ -51,25 +51,25 @@ include("regression_tests/hydrostatic_free_turbulence_regression_test.jl")
     @info "Running regression tests..."
 
     for arch in archs
-        # for grid_type in [:regular, :vertically_unstretched]
-        #     @testset "Thermal bubble [$(typeof(arch)), $grid_type grid]" begin
-        #         @info "  Testing thermal bubble regression [$(typeof(arch)), $grid_type grid]"
-        #         run_thermal_bubble_regression_test(arch, grid_type)
-        #     end
+        for grid_type in [:regular, :vertically_unstretched]
+            @testset "Thermal bubble [$(typeof(arch)), $grid_type grid]" begin
+                @info "  Testing thermal bubble regression [$(typeof(arch)), $grid_type grid]"
+                run_thermal_bubble_regression_test(arch, grid_type)
+            end
 
-        #     @testset "Rayleigh–Bénard tracer [$(typeof(arch)), $grid_type grid]]" begin
-        #         @info "  Testing Rayleigh–Bénard tracer regression [$(typeof(arch)), $grid_type grid]"
-        #         run_rayleigh_benard_regression_test(arch, grid_type)
-        #     end
+            @testset "Rayleigh–Bénard tracer [$(typeof(arch)), $grid_type grid]]" begin
+                @info "  Testing Rayleigh–Bénard tracer regression [$(typeof(arch)), $grid_type grid]"
+                run_rayleigh_benard_regression_test(arch, grid_type)
+            end
 
-        #     for closure in (AnisotropicMinimumDissipation(ν=1.05e-6, κ=1.46e-7), SmagorinskyLilly(C=0.23, Cb=1, Pr=1, ν=1.05e-6, κ=1.46e-7))
-        #         closurename = string(typeof(closure).name.wrapper)
-        #         @testset "Ocean large eddy simulation [$(typeof(arch)), $closurename, $grid_type grid]" begin
-        #             @info "  Testing oceanic large eddy simulation regression [$(typeof(arch)), $closurename, $grid_type grid]"
-        #             run_ocean_large_eddy_simulation_regression_test(arch, grid_type, closure)
-        #         end
-        #     end
-        # end
+            for closure in (AnisotropicMinimumDissipation(ν=1.05e-6, κ=1.46e-7), SmagorinskyLilly(C=0.23, Cb=1, Pr=1, ν=1.05e-6, κ=1.46e-7))
+                closurename = string(typeof(closure).name.wrapper)
+                @testset "Ocean large eddy simulation [$(typeof(arch)), $closurename, $grid_type grid]" begin
+                    @info "  Testing oceanic large eddy simulation regression [$(typeof(arch)), $closurename, $grid_type grid]"
+                    run_ocean_large_eddy_simulation_regression_test(arch, grid_type, closure)
+                end
+            end
+        end
 
         explicit_free_surface = ExplicitFreeSurface(gravitational_acceleration=1.0)
         implicit_free_surface = ImplicitFreeSurface(gravitational_acceleration = 1.0,
