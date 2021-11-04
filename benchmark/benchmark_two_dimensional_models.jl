97549749b790f0135b0e1110bea80cc30f61cbf0
Gregory Wagner <wagner.greg@gmail.com>
2021-11-04 09:58:59 -0400

    Benchmark CPU and GPU

--- benchmark/benchmark_two_dimensional_models.jl
+++ benchmark/benchmark_two_dimensional_models.jl
@@ -5,7 +5,7 @@ using Oceananigans: short_show
 using Oceananigans.TimeSteppers: time_step!
 using BenchmarkTools
 
-N = 64
+N = 256
 
 xy_grid = RegularRectilinearGrid(size = (N, N, 1), halo = (3, 3, 3), extent = (2π, 2π, 2π), topology = (Periodic, Periodic, Bounded))
 xz_grid = RegularRectilinearGrid(size = (N, 1, N), halo = (3, 3, 3), extent = (2π, 2π, 2π), topology = (Periodic, Periodic, Bounded))
@@ -18,22 +18,23 @@ function ten_steps!(model)
     return nothing
 end
 
-arch = CPU() #for arch in (CPU(), GPU())
+for arch in (CPU(), GPU())
 
-for grid in (xy_grid,
-             xz_grid,
-             yz_grid)
+    for grid in (xy_grid,
+                 xz_grid,
+                 yz_grid)
 
-    model = NonhydrostaticModel(architecture = arch,
-                                timestepper = :QuasiAdamsBashforth2,
-                                grid = grid,
-                                advection = nothing,
-                                closure = nothing,
-                                buoyancy = nothing,
-                                tracers = nothing)
-    
-    time_step!(model, 1e-6) # warmup
+        model = NonhydrostaticModel(architecture = arch,
+                                    timestepper = :QuasiAdamsBashforth2,
+                                    grid = grid,
+                                    advection = nothing,
+                                    closure = nothing,
+                                    buoyancy = nothing,
+                                    tracers = nothing)
+        
+        time_step!(model, 1e-6) # warmup
 
-    @info "Benchmarking model with $(short_show(grid))..."
-    @btime ten_steps!($model)
+        @info "Benchmarking model with $(short_show(grid)) on $arch..."
+        @btime ten_steps!($model)
+    end
 end
