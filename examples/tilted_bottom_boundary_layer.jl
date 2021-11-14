4788f3307f23af25d9190110a3b7c7bf6e11ab10
tomchor <tomaschor@gmail.com>
2021-11-14 11:28:12 -0800

    made progress in the example

--- examples/tilted_bottom_boundary_layer.jl
+++ examples/tilted_bottom_boundary_layer.jl
@@ -17,8 +17,7 @@
 
 # ## Load `Oceananigans.jl` and define problem constants
 #
-# Since the simulation is taken from Wenegrat et al. (2020), we can set a named tuple
-# with every simulation parameter that we use:
+# We set a named tuple that defines the necessary simulation parametrs:
 
 using Oceananigans
 using Oceananigans.Units
@@ -36,20 +35,193 @@ params = (f₀ = 1e-4, #1/s
           ν = 5e-4, # m²/s
           sponge_frac = 1/5,
           sponge_rate = √1e-5, # 1/s
-          z_0 = 0.1, # m (roughness length)
+          z₀ = 0.1, # m (roughness length)
           )
 
 arch = CPU()
-ĝ = [sin(params.θ_rad), 0, cos(params.θ_rad)]
 
 
 # Here `f₀` is the Coriolis frequency, `V∞` in the interior `v`-velocity, `N²∞` is the
 # interior stratification, `θ_rad` is the bottom slope in radians, `ν` is the eddy viscosity,
-# and `z_0` is rthe roughness length (needed for the drag at the bottom).
+# and `z₀` is the roughness length (needed for the drag at the bottom).
 #
-# ## Instantiating and configuring a model
 #
-# A core Oceananigans type is `NonhydrostaticModel`. We build an `NonhydrostaticModel`
-# by passing it a `grid`, plus information about the equations we would like to solve.
+# ## Creating the grid
+#
+# Since we anticipate most of the energy in the flow to be located close to the bottom, we
+# can save computational resources by creating a grid that has finer spacings near the bottom
+# which gets progressively coarser near the top. Such a grid can be achieved with the following
+# equations:
 #
 
+
+refinement = 1.8 # controls spacing near surface (higher means finer spaced)
+stretching = 10   # controls rate of stretching at bottom 
+
+h₁(k) = ((-k+params.Nz)+1) / params.Nz
+
+## Linear near-surface generator
+ζ₁(k) = 1 + (h₁(k) - 1) / refinement
+
+## Bottom-intensified stretching function 
+Σ₁(k) = (1 - exp(-stretching * h₁(k))) / (1 - exp(-stretching))
+
+## Generating function
+z_faces(k) = -params.Lz * (ζ₁(k) * Σ₁(k) - 1)
+
+
+grid = VerticallyStretchedRectilinearGrid(topology=topo,
+                                          architecture = arch,
+                                          size=(params.Nx, params.Nz),
+                                          x=(0, params.Lx), z_faces=z_faces,
+                                          halo=(3,3),
+                                          )
+@info grid
+
+
+# We plot vertical spacing versus depth to inspect the prescribed grid stretching. Note that
+# the spacing near the bottom is relatively uniform with height, which is a desired property from
+# a numerical perspective.
+#
+#
+# ## Setting up a buoyancy model in a tilted domain
+#
+# We set-up our domain in a way that the coordinates align with the bottom. That means that, from
+# the coordinate's point of view, gravity needs to be tilted by `θ_rad` radians, which we can do by
+# passing the `vertical_units_vector` parameter to `Buoyancy()`:
+
+ĝ = [sin(params.θ_rad), 0, cos(params.θ_rad)]
+buoyancy = Buoyancy(model=BuoyancyTracer(), vertical_unit_vector=ĝ)
+
+# Since we are simulating a period domain that is tilted, we need to set-up the background
+# stratification as a `BackgroundField` and solve for the buoyancy perturbation. This avoids mean
+# horizontal buoyant gradients, which cannot exist in a horizontally-periodic domain:
+
+b∞(x, y, z, t, p) = p.N²∞ * (x * sin(p.θ_rad) + z * cos(p.θ_rad))
+B_field = BackgroundField(b∞, parameters=(; params.N²∞, params.θ_rad))
+
+## As a boundary condition
+
+#db∞dz = params.N²∞ * ĝ[3]
+#grad_bc_b = GradientBoundaryCondition(-db∞dz) # db/dz + db∞/dz = 0 @ z=0
+#b_bcs = FieldBoundaryConditions(bottom = grad_bc_b)
+
+
+# ## Bottom drag
+#
+# We also set-up a bottom drag that follows Monin-Obukhov theory, which can be implemented following https://doi.org/10.1029/2005WR004685.
+# Note that we need to include`V∞` in the velocity, since we will model it as a background field.
+#
+
+const κ = 0.4 # von Karman constant
+z₁ = CUDA.@allowscalar znodes(Center, grid)[1] # Closest grid center to the bottom
+cᴰ = (κ / log(z₁/params.z₀))^2 # Drag coefficient
+
+@inline drag_u(x, y, t, u, v, p) = - p.cᴰ * √(u^2 + (v + p.V∞)^2) * u
+@inline drag_v(x, y, t, u, v, p) = - p.cᴰ * √(u^2 + (v + p.V∞)^2) * (v + p.V∞)
+
+drag_bc_u = FluxBoundaryCondition(drag_u, field_dependencies=(:u, :v), parameters=(cᴰ=cᴰ, V∞=params.V∞))
+drag_bc_v = FluxBoundaryCondition(drag_v, field_dependencies=(:u, :v), parameters=(cᴰ=cᴰ, V∞=params.V∞))
+
+u_bcs = FieldBoundaryConditions(bottom = drag_bc_u)
+v_bcs = FieldBoundaryConditions(bottom = drag_bc_v)
+
+V_bg(x, y, z, t, p) = p.V∞
+V_field = BackgroundField(V_bg, parameters=(; V∞=params.V∞))
+#-----
+
+bcs = 
+#-----
+
+
+#++++ Sponge layer definition
+@info "Defining sponge layer"
+@inline heaviside(X) = ifelse(X < 0, zero(X), one(X))
+
+const sp_frac = params.sponge_frac
+const Lz = params.Lz
+
+function top_mask_2nd(x, y, z)
+    z₁ = +Lz; z₀ = z₁ - Lz * sp_frac 
+    return heaviside((z - z₀)/(z₁ - z₀)) * ((z - z₀)/(z₁ - z₀))^2
+end
+
+full_sponge_0 = Relaxation(rate=params.sponge_rate, mask=top_mask_2nd, target=0)
+forcing = (u=full_sponge_0, v=full_sponge_0, w=full_sponge_0,)
+#----
+
+
+#++++ Turbulence closure
+closure = IsotropicDiffusivity(ν=params.ν, κ=params.ν)
+#----
+
+
+#++++ Model and ICs
+@info "Creating model"
+model = NonhydrostaticModel(grid = grid, timestepper = :RungeKutta3,
+                            architecture = arch,
+                            advection = WENO5(),
+                            buoyancy = buoyancy,
+                            coriolis = ConstantCartesianCoriolis(f=params.f₀, rotation_axis=ĝ),
+                            tracers = :b,
+                            closure = closure,
+                            boundary_conditions = (u=u_bcs, v=v_bcs,),
+                            background_fields = (b=B_field, v=V_field,),
+                            forcing=forcing,
+                           )
+@info "" model
+if has_cuda() run(`nvidia-smi`) end
+
+noise(z, kick) = kick * randn() * exp(-z / (params.Lz/5))
+u_ic(x, y, z) = noise(z, 1e-3)
+set!(model, b=0, u=u_ic, w=u_ic)
+
+ū = sum(model.velocities.u.data.parent) / (grid.Nx * grid.Ny * grid.Nz)
+v̄ = sum(model.velocities.v.data.parent) / (grid.Nx * grid.Ny * grid.Nz)
+w̄ = sum(model.velocities.w.data.parent) / (grid.Nx * grid.Ny * grid.Nz)
+
+model.velocities.u.data.parent .-= ū
+model.velocities.v.data.parent .-= v̄
+model.velocities.w.data.parent .-= w̄
+#----
+
+
+#++++ Create simulation
+using Oceananigans.Grids: min_Δz
+if ndims==3
+    cfl=0.85
+else
+    cfl=0.5
+end
+wizard = TimeStepWizard(max_change=1.03, cfl=cfl)
+
+# Print a progress message
+progress_message(sim) =
+    @printf("i: %04d, t: %s, Δt: %s, wmax = %.1e ms⁻¹, wall time: %s\n",
+            iteration(sim), prettytime(time(sim)),
+            prettytime(sim.Δt), maximum(abs, sim.model.velocities.w),
+            prettytime((time_ns() - start_time) * 1e-9))
+
+simulation = Simulation(model, Δt=0.5*min_Δz(grid)/params.V∞, 
+                        stop_time=3days, 
+                       )
+simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(4))
+simulation.callbacks[:progress] = Callback(progress_message, IterationInterval(10))
+#----
+
+
+#++++ Outputs
+u, v, w = model.velocities
+b_tot = ComputedField(model.tracers.b + model.background_fields.tracers.b)
+v_tot = ComputedField(v + model.background_fields.velocities.v)
+ω_y = ComputedField(∂z(u)-∂x(w))
+fields = merge((; u, v_tot, w, b_tot, ω_y))
+simulation.output_writers[:fields] =
+NetCDFOutputWriter(model, fields, filepath = "out.tilted_bbl.nc",
+                   schedule = TimeInterval(20minutes),
+                   mode = "c")
+#----
+#
+
+start_time = time_ns() # so we can print the total elapsed wall time
+run!(simulation)
