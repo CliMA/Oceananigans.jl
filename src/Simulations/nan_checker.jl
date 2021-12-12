23cf5a7cdf0d06e0146e13f75a93d4934f28f400
Gregory L. Wagner <wagner.greg@gmail.com>
2021-12-11 20:13:55 -0700

    Allow NaNChecker.erroring (#2087)
    * Allow NaNChecker.erroring
    
    * Add positional argument constructor with erroring=false
    
    * Adds a test for the erroring nan checker
    
    * Add function API erroring_NaNChecker!

--- src/Simulations/nan_checker.jl
+++ src/Simulations/nan_checker.jl
@@ -1,25 +1,40 @@
-struct NaNChecker{F}
+mutable struct NaNChecker{F}
     fields :: F
+    erroring :: Bool
 end
 
+NaNChecker(fields) = NaNChecker(fields, false) # default
+
 """
-    NaNChecker(; fields)
+    NaNChecker(; fields, erroring=false)
 
 Return a `NaNChecker`, which sets `sim.running=false` if a `NaN` is detected
 in any member of `fields` when `NaNChecker(sim)` is called. `fields` should be
 a container with key-value pairs like a dictionary or `NamedTuple`.
+
+If `erroring=true`, the `NaNChecker` will throw an error on NaN detection.
 """
-NaNChecker(; fields) = NaNChecker(fields)
+NaNChecker(; fields, erroring=false) = NaNChecker(fields, erroring)
 
 hasnan(field) = any(isnan, parent(field)) 
 
 function (nc::NaNChecker)(simulation)
     for (name, field) in pairs(nc.fields)
         if hasnan(field)
-            @info "NaN found in field $name. Stopping simulation."
             simulation.running = false
+
+            if nc.erroring
+                clock = simulation.model.clock
+                error("time = $(clock.time), iteration = $(clock.iteration): NaN found in field $name. Aborting simulation.")
+            else
+                @info "NaN found in field $name. Stopping simulation."
+            end
         end
     end
     return nothing
 end
 
+function erroring_NaNChecker!(simulation)
+    simulation.callbacks[:nan_checker].func.erroring = true
+    return nothing
+end
