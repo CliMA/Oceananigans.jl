084657149d43bd22d138c5b3f12a2132b745bdfa
Simone Silvestri <silvestri.simone0@gmail.com>
2021-12-15 17:45:03 -0500

    fixed interaction prescribed-velocity/single-column-grid

--- src/Models/HydrostaticFreeSurfaceModels/single_column_model_mode.jl
+++ src/Models/HydrostaticFreeSurfaceModels/single_column_model_mode.jl
@@ -29,8 +29,10 @@ const SingleColumnGrid = AbstractGrid{<:AbstractFloat, <:Flat, <:Flat, <:Bounded
 #####
 
 PressureField(arch, ::SingleColumnGrid) = (pHY′ = nothing,)
-FreeSurface(free_surface::ExplicitFreeSurface{Nothing}, velocities, arch, ::SingleColumnGrid) = nothing
-FreeSurface(free_surface::ImplicitFreeSurface{Nothing}, velocities, arch, ::SingleColumnGrid) = nothing
+FreeSurface(free_surface::ExplicitFreeSurface{Nothing}, velocities,                 arch, ::SingleColumnGrid) = nothing
+FreeSurface(free_surface::ImplicitFreeSurface{Nothing}, velocities,                 arch, ::SingleColumnGrid) = nothing
+FreeSurface(free_surface::ExplicitFreeSurface{Nothing}, ::PrescribedVelocityFields, arch, ::SingleColumnGrid) = nothing
+FreeSurface(free_surface::ImplicitFreeSurface{Nothing}, ::PrescribedVelocityFields, arch, ::SingleColumnGrid) = nothing
 
 validate_momentum_advection(momentum_advection, ::SingleColumnGrid) = nothing
 validate_tracer_advection(tracer_advection::AbstractAdvectionScheme, ::SingleColumnGrid) = nothing, NamedTuple()
