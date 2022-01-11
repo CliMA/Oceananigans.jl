eddd501dc521ac2af778dfe9c84a1aced9b63fcb
Simone Silvestri <silvestri.simone0@gmail.com>
2022-01-11 10:30:22 -0500

    fixed tests

--- test/data_dependencies.jl
+++ test/data_dependencies.jl
@@ -16,7 +16,7 @@ datadep"cubed_sphere_32_grid"
 
 # Downloading the regression fields
 
-path = "https://github.com/CliMA/OceananigansArtifacts.jl/raw/ss/new_regression_data/data_for_regression_tests_new_hhydrostatic/"
+path = "https://github.com/CliMA/OceananigansArtifacts.jl/raw/main/data_for_regression_tests/"
 
 dh = DataDep("regression_test_data",
     "Data for Regression tests",
