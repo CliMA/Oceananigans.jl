89eb639393690f47a19f58c6687e36f35e1acdf6
Gregory Wagner <wagner.greg@gmail.com>
2022-01-13 12:29:53 -0700

    Update DistributedField constructor

--- src/Distributed/distributed_fields.jl
+++ src/Distributed/distributed_fields.jl
@@ -4,10 +4,10 @@ import Oceananigans.BoundaryConditions: fill_halo_regions!
 function Field((LX, LY, LZ)::Tuple, grid::DistributedGrid, data, old_bcs, op, status)
     arch = architecture(grid)
     new_bcs = inject_halo_communication_boundary_conditions(old_bcs, arch.local_rank, arch.connectivity)
-    return Field{LX, LY, LZ}(grid, data, new_bcs, op, status, child_architecture(arch))
+    return Field{LX, LY, LZ}(grid, data, new_bcs, op, status)
 end
 
-const DistributedField = Field{<:Any, <:Any, <:Any, <:Any, <:Any, <:DistributedGrid}
+const DistributedField = Field{<:Any, <:Any, <:Any, <:Any, <:DistributedGrid}
 
 fill_halo_regions!(field::DistributedField, arch, args...; kwargs...) =
     fill_halo_regions!(field.data,
