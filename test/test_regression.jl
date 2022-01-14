24f7e8c22c897364746b9eedd584bb8393ede423
Gregory Wagner <wagner.greg@gmail.com>
2022-01-14 15:44:15 -0500

    Restore all regression tests

--- test/test_regression.jl
+++ test/test_regression.jl
@@ -66,7 +66,6 @@ include("regression_tests/hydrostatic_free_turbulence_regression_test.jl")
 
     for arch in archs
         for grid_type in [:regular, :vertically_unstretched]
-            #=
             @testset "Thermal bubble [$(typeof(arch)), $grid_type grid]" begin
                 @info "  Testing thermal bubble regression [$(typeof(arch)), $grid_type grid]"
                 run_thermal_bubble_regression_test(arch, grid_type)
@@ -76,7 +75,6 @@ include("regression_tests/hydrostatic_free_turbulence_regression_test.jl")
                 @info "  Testing Rayleigh–Bénard tracer regression [$(typeof(arch)), $grid_type grid]"
                 run_rayleigh_benard_regression_test(arch, grid_type)
             end
-            =#
 
             for closure in (AnisotropicMinimumDissipation(ν=1.05e-6, κ=1.46e-7), SmagorinskyLilly(C=0.23, Cb=1, Pr=1, ν=1.05e-6, κ=1.46e-7))
                 closurename = string(typeof(closure).name.wrapper)
@@ -89,7 +87,6 @@ include("regression_tests/hydrostatic_free_turbulence_regression_test.jl")
 
         # Hydrostatic regression test
 
-        #=
         longitude = ((-180, 180), collect(-180:2:180), (-160, 160), collect(-160:2:160))
         latitude  = ((-60, 60),   collect(-60:2:60))
         zcoord    = ((-90, 0) ,   collect(-90:30:0))
@@ -125,6 +122,5 @@ include("regression_tests/hydrostatic_free_turbulence_regression_test.jl")
                 end
             end
 	    end   
-        =#
 	end
 end
