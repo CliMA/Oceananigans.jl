97ad4cdb93343d2e3986e54be34dd1f69402111d
Gregory L. Wagner <wagner.greg@gmail.com>
2022-01-21 11:34:21 -0700

    Update constant_field.jl

--- src/Fields/constant_field.jl
+++ src/Fields/constant_field.jl
@@ -2,7 +2,7 @@ struct ZeroField{T, N} <: AbstractField{Nothing, Nothing, Nothing, Nothing, T, N
 struct OneField{T, N} <: AbstractField{Nothing, Nothing, Nothing, Nothing, T, N} end
 
 ZeroField(T=Int) = ZeroField{T, 3}() # default 3D, integer 0
-OneField(T=Int) = OneField{T, 3}() # default 3D, integer 0
+OneField(T=Int) = OneField{T, 3}() # default 3D, integer 1
 
 @inline Base.getindex(::ZeroField{T, N}, ind...) where {N, T} = zero(T)
 @inline Base.getindex(::OneField{T, N}, ind...) where {N, T} = one(T)
