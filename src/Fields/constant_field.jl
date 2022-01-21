2d44c6c74e5f50f7b82e75018859847dfaa19df0
Gregory Wagner <wagner.greg@gmail.com>
2022-01-21 11:30:23 -0700

    Adds ConstantField

---
+++ src/Fields/constant_field.jl
@@ -0,0 +1,16 @@
+struct ZeroField{T, N} <: AbstractField{Nothing, Nothing, Nothing, Nothing, T, N} end
+
+ZeroField(T=Int) = ZeroField{T, 3}() # default 3D, integer 0
+
+@inline Base.getindex(::ZeroField{T, N}, ind...) where {N, T} = zero(T)
+
+struct ConstantField{T, N} <: AbstractField{Nothing, Nothing, Nothing, Nothing, T, N}
+    constant :: T
+    ConstantField{N}(constant::T) where T = new{T, N}(constant)
+end
+
+# Default 3-dimensional field
+ConstantField(constant) = ConstantField{3}(constant)
+
+@inline Base.getindex(f::ConstantField, ind...) = f.constant
+
