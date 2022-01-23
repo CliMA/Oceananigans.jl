97293a74e14414ffe2b4623324880a209b84f6f6
Gregory Wagner <wagner.greg@gmail.com>
2022-01-23 13:14:05 -0700

    More indices functions for Flat grids

--- src/Grids/grid_utils.jl
+++ src/Grids/grid_utils.jl
@@ -136,30 +136,46 @@ regular_dimensions(grid) = ()
 @inline underlying_right_halo_indices(::Type{Face}, ::Type{Bounded}, N, H) = N+2+H:N+1+2H
 @inline underlying_right_halo_indices(::Type{Nothing}, topo, N, H) = 1:0 # empty
 
-@inline interior_indices(loc, topo, N) = 1:N
-@inline interior_indices(::Type{Face}, ::Type{Bounded}, N) = 1:N+1
-@inline interior_indices(::Type{Nothing}, topo, N) = 1:1
+@inline interior_indices(loc,             topo,            N) = 1:N
+@inline interior_indices(::Type{Face},    ::Type{Bounded}, N) = 1:N+1
+@inline interior_indices(::Type{Nothing}, topo,            N) = 1:1
+
+@inline interior_indices(::Type{Nothing}, topo::Type{Flat}, N) = 1:N
+@inline interior_indices(::Type{Face},    topo::Type{Flat}, N) = 1:N
+@inline interior_indices(::Type{Center},  topo::Type{Flat}, N) = 1:N
 
 @inline interior_x_indices(loc, grid) = interior_indices(loc, topology(grid, 1), grid.Nx)
 @inline interior_y_indices(loc, grid) = interior_indices(loc, topology(grid, 2), grid.Ny)
 @inline interior_z_indices(loc, grid) = interior_indices(loc, topology(grid, 3), grid.Nz)
 
-@inline interior_parent_indices(loc, topo, N, H) = 1+H:N+H
-@inline interior_parent_indices(::Type{Face}, ::Type{Bounded}, N, H) = 1+H:N+1+H
-@inline interior_parent_indices(::Type{Nothing}, topo, N, H) = 1:1
+@inline interior_parent_indices(loc,             topo,            N, H) = 1+H:N+H
+@inline interior_parent_indices(::Type{Face},    ::Type{Bounded}, N, H) = 1+H:N+1+H
+@inline interior_parent_indices(::Type{Nothing}, topo,            N, H) = 1:1
+
+@inline interior_parent_indices(::Type{Nothing}, topo::Type{Flat}, N, H) = 1:N
+@inline interior_parent_indices(::Type{Face},    topo::Type{Flat}, N, H) = 1:N
+@inline interior_parent_indices(::Type{Center},  topo::Type{Flat}, N, H) = 1:N
 
 # All indices including halos.
-@inline all_indices(loc, topo, N, H) = 1-H:N+H
-@inline all_indices(::Type{Face}, ::Type{Bounded}, N, H) = 1-H:N+1+H
-@inline all_indices(::Type{Nothing}, topo, N, H) = 1:1
+@inline all_indices(loc,             topo,            N, H) = 1-H:N+H
+@inline all_indices(::Type{Face},    ::Type{Bounded}, N, H) = 1-H:N+1+H
+@inline all_indices(::Type{Nothing}, topo,            N, H) = 1:1
+
+@inline all_indices(::Type{Nothing}, ::Type{Flat}, N, H) = 1:N
+@inline all_indices(::Type{Face},    ::Type{Flat}, N, H) = 1:N
+@inline all_indices(::Type{Center},  ::Type{Flat}, N, H) = 1:N
 
 @inline all_x_indices(loc, grid) = all_indices(loc, topology(grid, 1), grid.Nx, grid.Hx)
 @inline all_y_indices(loc, grid) = all_indices(loc, topology(grid, 2), grid.Ny, grid.Hy)
 @inline all_z_indices(loc, grid) = all_indices(loc, topology(grid, 3), grid.Nz, grid.Hz)
 
-@inline all_parent_indices(loc, topo, N, H) = 1:N+2H
-@inline all_parent_indices(::Type{Face}, ::Type{Bounded}, N, H) = 1:N+1+2H
-@inline all_parent_indices(::Type{Nothing}, topo, N, H) = 1:1
+@inline all_parent_indices(loc,             topo,            N, H) = 1:N+2H
+@inline all_parent_indices(::Type{Face},    ::Type{Bounded}, N, H) = 1:N+1+2H
+@inline all_parent_indices(::Type{Nothing}, topo,            N, H) = 1:1
+
+@inline all_parent_indices(::Type{Nothing}, ::Type{Flat}, N, H) = 1:N
+@inline all_parent_indices(::Type{Face},    ::Type{Flat}, N, H) = 1:N
+@inline all_parent_indices(::Type{Center},  ::Type{Flat}, N, H) = 1:N
 
 @inline all_parent_x_indices(loc, grid) = all_parent_indices(loc, topology(grid, 1), grid.Nx, grid.Hx)
 @inline all_parent_y_indices(loc, grid) = all_parent_indices(loc, topology(grid, 2), grid.Ny, grid.Hy)
