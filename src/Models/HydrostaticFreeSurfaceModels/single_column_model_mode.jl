a314f1f64e80181ecff84d6bab1e5182644d95cc
Gregory L. Wagner <wagner.greg@gmail.com>
2022-01-27 08:16:08 -0700

    Fixes `FreeSurface` constructor for `SingleColumnGrid` and adds a test (#2196)
    * Fixes FreeSurface constructor for SingleColumnGrid and adds a test
    
    * Test isnothing(model.free_surface)
    
    * Bump to 0.68.8
    
    * Bugfix in test
    
    * fix test
    
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>

--- src/Models/HydrostaticFreeSurfaceModels/single_column_model_mode.jl
+++ src/Models/HydrostaticFreeSurfaceModels/single_column_model_mode.jl
@@ -30,10 +30,10 @@ const SingleColumnGrid = AbstractGrid{<:AbstractFloat, <:Flat, <:Flat, <:Bounded
 #####
 
 PressureField(arch, ::SingleColumnGrid) = (pHY′ = nothing,)
-FreeSurface(free_surface::ExplicitFreeSurface{Nothing}, velocities,                 arch, ::SingleColumnGrid) = nothing
-FreeSurface(free_surface::ImplicitFreeSurface{Nothing}, velocities,                 arch, ::SingleColumnGrid) = nothing
-FreeSurface(free_surface::ExplicitFreeSurface{Nothing}, ::PrescribedVelocityFields, arch, ::SingleColumnGrid) = nothing
-FreeSurface(free_surface::ImplicitFreeSurface{Nothing}, ::PrescribedVelocityFields, arch, ::SingleColumnGrid) = nothing
+FreeSurface(free_surface::ExplicitFreeSurface{Nothing}, velocities,                 ::SingleColumnGrid) = nothing
+FreeSurface(free_surface::ImplicitFreeSurface{Nothing}, velocities,                 ::SingleColumnGrid) = nothing
+FreeSurface(free_surface::ExplicitFreeSurface{Nothing}, ::PrescribedVelocityFields, ::SingleColumnGrid) = nothing
+FreeSurface(free_surface::ImplicitFreeSurface{Nothing}, ::PrescribedVelocityFields, ::SingleColumnGrid) = nothing
 
 validate_momentum_advection(momentum_advection, ::SingleColumnGrid) = nothing
 validate_tracer_advection(tracer_advection::AbstractAdvectionScheme, ::SingleColumnGrid) = nothing, NamedTuple()
