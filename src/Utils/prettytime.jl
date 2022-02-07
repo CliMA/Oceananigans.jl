9b75942bd0275f47d4e5b2bb2726958068386e15
Gregory Wagner <wagner.greg@gmail.com>
2022-02-07 08:54:43 -0700

    Adds longform option to prettytime

--- src/Utils/prettytime.jl
+++ src/Utils/prettytime.jl
@@ -13,42 +13,56 @@ human-friendly formatted string with three decimal places. Depending on the valu
 the string will be formatted to show `t` in nanoseconds (ns), microseconds (μs),
 milliseconds (ms), seconds, minutes, hours, days, or years.
 """
-function prettytime(t)
+function prettytime(t, longform=true)
     # Modified from: https://github.com/JuliaCI/BenchmarkTools.jl/blob/master/src/trials.jl
+    
+    # Some shortcuts
     iszero(t) && return "0 seconds"
+    t < 1e-9 && return @sprintf("%.3e seconds", t)
 
     t = maybe_int(t)
-
-    if t < 1e-9
-        # No point going to picoseconds, just return something readable.
-        return @sprintf("%.3e seconds", t)
-    elseif t < 1e-6
-        value, units = t * 1e9, "ns"
-    elseif t < 1e-3
-        value, units = t * 1e6, "μs"
-    elseif t < 1
-        value, units = t * 1e3, "ms"
-    elseif t < minute
-        value = t
-        units = value == 1 ? "second" : "seconds"
-    elseif t < hour
-        value = maybe_int(t / minute)
-        units = value == 1 ? "minute" : "minutes"
-    elseif t < day
-        value = maybe_int(t / hour)
-        units = value == 1 ? "hour" : "hours"
-    elseif t < year
-        value = maybe_int(t / day)
-        units = value == 1 ? "day" : "days"
-    else
-        value = maybe_int(t / year)
-        units = value == 1 ? "year" : "years"
-    end
+    value, units = prettyunits(t, longform)
 
     if isinteger(value)
         return @sprintf("%d %s", value, units)
     else
         return @sprintf("%.3f %s", value, units)
+    end
+end
+
+function prettyunits(t, longform)
+    if t < 1e-9
+        return ""
+    elseif t < 1e-6
+        return = t * 1e9, "ns"
+    elseif t < 1e-3
+        return = t * 1e6, "μs"
+    elseif t < 1
+        return = t * 1e3, "ms"
+    elseif t < minute
+        value = t
+        longform && return value, "s"
+        units = value == 1 ? "second" : "seconds"
+        return value, units
+    elseif t < hour
+        value = maybe_int(t / minute)
+        longform && return value, "m"
+        units = value == 1 ? "minute" : "minutes"
+    elseif t < day
+        value = maybe_int(t / hour)
+        units = value == 1 ? (longform ? "hour" : "hr") :
+                             (longform ? "hrs" : "hours")
+        return value, units
+    elseif t < year
+        value = maybe_int(t / day)
+        longform && return value, "d"
+        units = value == 1 ? "day" : "days"
+        return value, units
+    else
+        value = maybe_int(t / year)
+        units = value == 1 ? (longform ? "year" : "yr") :
+                             (longform ? "years" : "yrs")
+        return value, units
     end
 end
 
