ecb2d29b497294c4862a43b828712b2b161210e7
Simone Silvestri <silvestri.simone0@gmail.com>
2022-02-16 18:29:16 -0500

    small bugfix

--- src/Operators/laplacian_operators.jl
+++ src/Operators/laplacian_operators.jl
@@ -39,19 +39,19 @@ which will end up at the location `ccc`.
                                     δzᵃᵃᶜ(i, j, k, grid, Az_∂zᶜᶜᶠ, c))
 end
 
-@inline function _∇²ᶠᶜᶜ(i, j, k, grid, u)
+@inline function ∇²ᶠᶜᶜ(i, j, k, grid, u)
     return 1/Vᶠᶜᶜ(i, j, k, grid) * (δxᶠᵃᵃ(i, j, k, grid, Ax_∂xᶜᶜᶜ, u) +
                                     δyᵃᶜᵃ(i, j, k, grid, Ay_∂yᶠᶠᶜ, u) +
                                     δzᵃᵃᶜ(i, j, k, grid, Az_∂zᶠᶜᶠ, u))
 end
 
-@inline function _∇²ᶜᶠᶜ(i, j, k, grid, v)
+@inline function ∇²ᶜᶠᶜ(i, j, k, grid, v)
     return 1/Vᶜᶠᶜ(i, j, k, grid) * (δxᶜᵃᵃ(i, j, k, grid, Ax_∂xᶠᶠᶜ, v) +
                                     δyᵃᶠᵃ(i, j, k, grid, Ay_∂yᶜᶜᶜ, v) +
                                     δzᵃᵃᶜ(i, j, k, grid, Az_∂zᶜᶠᶠ, v))
 end
 
-@inline function _∇²ᶜᶜᶠ(i, j, k, grid, w)
+@inline function ∇²ᶜᶜᶠ(i, j, k, grid, w)
     return 1/Vᶜᶜᶠ(i, j, k, grid) * (δxᶜᵃᵃ(i, j, k, grid, Ax_∂xᶠᶜᶠ, w) +
                                     δyᵃᶜᵃ(i, j, k, grid, Ay_∂yᶜᶠᶠ, w) +
                                     δzᵃᵃᶠ(i, j, k, grid, Az_∂zᶜᶜᶜ, w))
