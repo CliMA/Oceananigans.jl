8d214b1b978ccc40f7cc3e594e304431141bddcb
Simone Silvestri <silvestri.simone0@gmail.com>
2022-02-24 14:21:07 -0500

    No need for adapt!

--- src/MultiRegion/multi_region_object.jl
+++ src/MultiRegion/multi_region_object.jl
@@ -13,9 +13,6 @@ switch_device!(mo::MultiRegionObject, i) = switch_device!(getdevice(mo, i))
 getregion(mo::MultiRegionObject, i) = mo.regions[i]
 getdevice(mo::MultiRegionObject, i) = mo.devices[i]
 
-Adapt.adapt_structure(to, mo::MultiRegionObject) = 
-                MultiRegionObject(Adapt.adapt(to, regions),  nothing)
-
 regions(mo::MultiRegionObject) = 1:length(mo.regions)
 devices(mo::MultiRegionObject) = mo.devices
 
