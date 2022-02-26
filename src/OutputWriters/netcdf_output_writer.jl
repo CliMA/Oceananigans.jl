b034594db5075ce52969867a38370864ca964c2a
Gregory Wagner <wagner.greg@gmail.com>
2022-02-26 08:18:32 -0700

    Counterattack hacks with comments

--- src/OutputWriters/netcdf_output_writer.jl
+++ src/OutputWriters/netcdf_output_writer.jl
@@ -300,7 +300,9 @@ function NetCDFOutputWriter(model, outputs; filepath, schedule,
     # Default to create/clobber.
     isnothing(mode) && (mode = "c")
 
-    # We need to convert to a Dict with String keys if user provides a named tuple.
+    # TODO: This call to dictify is only necessary because "dictify" is hacked to help
+    # with LagrangianParticles output (see the end of the file).
+    # We shouldn't support this in the future; we should require users to 'name' LagrangianParticles output.
     outputs = dictify(outputs)
     outputs = Dict(string(name) => construct_output(outputs[name], model.grid, indices, with_halos) for name in keys(outputs))
     output_attributes = dictify(output_attributes)
