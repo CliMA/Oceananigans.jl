0458969f02b139e2e9a2fd2c58cd03448feebc93
Francis Poulin <fpoulin@uwaterloo.ca>
2022-03-03 13:52:24 -0500

    cleaning up partial cell code

--- src/ImmersedBoundaries/partial_cell_immersed_boundaries.jl
+++ src/ImmersedBoundaries/partial_cell_immersed_boundaries.jl
@@ -21,7 +21,6 @@ struct PartialCellBottom{B, E} <: AbstractGridFittedBoundary
     minimum_fractional_partial_Δz :: E
 end
 
-
 @inline get_bottom_height(i, j, k, grid, bottom_height::AbstractArray) = @inbounds bottom_height[i, j]
 
 @inline function get_bottom_height(i, j, k, underlying_grid, bottom_height)
@@ -46,23 +45,4 @@ bottom_cell(i, j, k, ibg::PCIBG) = !is_immersed(i, j, k, ibg.grid, ibg.immersed_
     x, y, z = node(c, c, c, i, j, k, underlying_grid)
     Δzᵃᵃᶜ = ibg.immersed_boundary.bottom_height(x, y)
     return Δzᵃᵃᶜ
-end
-
-#height = (topography.(grid.yᵃᶠᵃ[1:grid.Ny]) + 4*topography.(grid.yᵃᶜᵃ[1:grid.Ny]) + topography.(grid.yᵃᶠᵃ[2:grid.Ny+1]))/6
-
-# FJP: Need to add a second argument???
-const ArrayPartialCellBottom = PartialCellBottom{<:Array}
-const CuArrayPartialCellBottom = PartialCellBottom{<:CuArray}
-
-function ImmersedBoundaryGrid(grid, ib::Union{ArrayPartialCellBottom, CuArrayPartialCellBottom})
-
-    # Wrap bathymetry in an OffsetArray with halos
-    arch = grid.architecture
-    bottom_field = Field{Center, Center, Nothing}(grid)
-    bottom_data = arch_array(arch, ib.bottom_height)
-    bottom_field .= bottom_data
-    fill_halo_regions!(bottom_field, arch)
-    offset_bottom_array = dropdims(bottom_field.data, dims=3)
-    new_ib = PartialCellBottom(offset_bottom_array, ib.minimum_fractional_partial_Δz)
-    return ImmersedBoundaryGrid(grid, new_ib)
 end
