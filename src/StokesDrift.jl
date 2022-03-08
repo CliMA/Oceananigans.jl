e41276c526447847847325dbbc034abe363582ec
Gregory Wagner <wagner.greg@gmail.com>
2022-03-07 19:43:57 -0700

    Fix bug and add a docstring

--- src/StokesDrift.jl
+++ src/StokesDrift.jl
@@ -60,7 +60,13 @@ a uniform surface gravity wave field.
 UniformStokesDrift(; ∂z_uˢ=addzero, ∂z_vˢ=addzero, ∂t_uˢ=addzero, ∂t_vˢ=addzero) =
     UniformStokesDrift(∂z_uˢ, ∂z_vˢ, ∂t_uˢ, ∂t_vˢ)
 
-#∂z_uˢ=addzero, ∂z_vˢ=addzero, ∂t_uˢ=addzero, ∂t_vˢ=addzero) =
+# TODO: add docstring 
+"""
+    UniformStokesDrift(grid)
+
+Construct a set of `Field`s that describe the Stokes drift shear
+and tendency beneath a uniform surface gravity wave field.
+"""
 function UniformStokesDrift(grid::AbstractGrid)
                             
     ∂z_uˢ = Field{Nothing, Nothing, Face}(grid)
@@ -80,20 +86,20 @@ const USD = UniformStokesDrift
 @inline get_stokes_shearᶠ(i, j, k, grid, ∂z_Uˢ::AbstractArray, time) = @inbounds ∂z_Uˢ[i, j, k]
 
 @inline get_stokes_tendencyᶜ(i, j, k, grid, time, ∂t_Uˢ) = ∂t_Uˢ(znode(Center(), k, grid), time)
+@inline get_stokes_tendencyᶜ(i, j, k, grid, time, ∂t_Uˢ::AbstractArray) = @inbounds ∂t_Uˢ[i, j, k]
 
 @inline ∂t_uˢ(i, j, k, grid, sw::USD, time) = get_stokes_tendencyᶜ(i, j, k, grid, sw.∂t_uˢ, time)
 @inline ∂t_vˢ(i, j, k, grid, sw::USD, time) = get_stokes_tendencyᶜ(i, j, k, grid, sw.∂t_vˢ, time)
 @inline ∂t_wˢ(i, j, k, grid::AbstractGrid{FT}, sw::USD, time) where FT = zero(FT)
 
 @inline x_curl_Uˢ_cross_U(i, j, k, grid, sw::USD, U, time) =
-    @inbounds ℑxzᶠᵃᶜ(i, j, k, grid, U.w) * get_stokes_shearᶜ(i, j, k, grid, sw.∂z_uˢ, time)
+    ℑxzᶠᵃᶜ(i, j, k, grid, U.w) * get_stokes_shearᶜ(i, j, k, grid, sw.∂z_uˢ, time)
 
 @inline y_curl_Uˢ_cross_U(i, j, k, grid, sw::USD, U, time) =
-    @inbounds ℑyzᵃᶠᶜ(i, j, k, grid, U.w) * get_stokes_shearᶜ(i, j, k, grid, sw.∂z_vˢ, time)
+    ℑyzᵃᶠᶜ(i, j, k, grid, U.w) * get_stokes_shearᶜ(i, j, k, grid, sw.∂z_vˢ, time)
 
-@inline z_curl_Uˢ_cross_U(i, j, k, grid, sw::USD, U, time) = @inbounds begin (
+@inline z_curl_Uˢ_cross_U(i, j, k, grid, sw::USD, U, time) = (
     - ℑxzᶜᵃᶠ(i, j, k, grid, U.u) * get_stokes_shearᶠ(i, j, k, grid, sw.∂z_uˢ, time)
     - ℑyzᵃᶜᶠ(i, j, k, grid, U.v) * get_stokes_shearᶠ(i, j, k, grid, sw.∂z_vˢ, time))
-end
 
 end # module
