2894fc7a339e95e33b409f2dc6ac3437d23056b8
Simone Silvestri <silvestri.simone0@gmail.com>
2022-03-11 15:24:33 -0500

    added loop to fill_halo

--- src/BoundaryConditions/fill_halo_regions_periodic.jl
+++ src/BoundaryConditions/fill_halo_regions_periodic.jl
@@ -65,33 +65,36 @@ end
 
 @kernel function fill_periodic_west_and_east_halo!(c::NTuple{M}, H::Int, N) where M 
   j, k = @index(Global, NTuple)
-  
-  @unroll for i = 1:H
+  @unroll for n = 1:M
+    @unroll for i = 1:H
       @inbounds begin
-          c[i, j, k]     = c[N+i, j, k] # west
-          c[N+H+i, j, k] = c[H+i, j, k] # east
+          c[n][i, j, k]     = c[n][N+i, j, k] # west
+          c[n][N+H+i, j, k] = c[n][H+i, j, k] # east
       end
+    end
   end
 end
 
 @kernel function fill_periodic_south_and_north_halo!(c::NTuple{M}, H::Int, N) where M
   i, k = @index(Global, NTuple)
-  
-  @unroll for j = 1:H
+  @unroll for n = 1:M
+   @unroll for j = 1:H
       @inbounds begin
-          c[i, j, k] = c[i, N+j, k]     # south
-          c[i, N+H+j, k] = c[i, H+j, k] # north
+          c[n][i, j, k]     = c[n][i, N+j, k]     # south
+          c[n][i, N+H+j, k] = c[n][i, H+j, k] # north
       end
+    end
   end
 end
 
 @kernel function fill_periodic_bottom_and_top_halo!(c::NTuple{M}, H::Int, N) where M
   i, j = @index(Global, NTuple)
-  
-  @unroll for k = 1:H
+  @unroll for n = 1:M
+   @unroll for k = 1:H
       @inbounds begin
-          c[i, j, k] = c[i, j, N+k]        # top
-          c[i, j, N+H+k] = c[i, j, H+k] # bottom
+          c[n][i, j, k]     = c[n][i, j, N+k]        # top
+          c[n][i, j, N+H+k] = c[n][i, j, H+k] # bottom
       end
+    end
   end
 end
