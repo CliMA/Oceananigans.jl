2926c1c117632ca74371b63dfa42034a5d1a898d
Gregory Wagner <wagner.greg@gmail.com>
2022-03-11 22:43:42 -0500

    Bugfix in immersed boundary ivd

--- src/ImmersedBoundaries/grid_fitted_immersed_boundaries.jl
+++ src/ImmersedBoundaries/grid_fitted_immersed_boundaries.jl
@@ -65,31 +65,33 @@ Adapt.adapt_structure(to, ib::GridFittedBottom) = GridFittedBottom(adapt(to, ib.
 #####
 
 ####
-#### For a center solver we have to check the interface "solidity" at faces k+1 in both the Upper diagonal and the Lower diagonal 
+#### For a center solver we have to check the interface "solidity" at faces k+1 in both the
+#### Upper diagonal and the Lower diagonal 
 #### (because of tridiagonal convention where lower_diagonal on row k is found at k-1)
 #### Same goes for the face solver, where we check at centers k in both Upper and lower diagonal
 ####
 
-@inline ivd_immersed_solid_interface(LX, LY, ::Center, i, j, k, ibg) = solid_interface(LX, LY, Face(), i, j, k+1, ibg)
-@inline ivd_immersed_solid_interface(LX, LY, ::Face, i, j, k, ibg)   = solid_interface(LX, LY, Center(), i, j, k, ibg)
+@inline immersed_ivd_solid_interface(LX, LY, ::Center, i, j, k, ibg) = solid_interface(LX, LY, Face(), i, j, k+1, ibg)
+@inline immersed_ivd_solid_interface(LX, LY, ::Face, i, j, k, ibg)   = solid_interface(LX, LY, Center(), i, j, k, ibg)
 
 # extending the upper and lower diagonal functions of the batched tridiagonal solver
 
 for location in (:upper_, :lower_)
-    alt_func = Symbol(:_ivd_, location, :diagonal)
-    func     = Symbol(:ivd_ , location, :diagonal)
+    immersed_func = Symbol(:immersed_ivd_, location, :diagonal)
+    func = Symbol(:ivd_ , location, :diagonal)
     @eval begin
-        @inline function $alt_func(i, j, k, ibg::GFIBG, closure, K, id, LX, LY, LZ, clock, Δt, κz)
-            return ifelse(ivd_immersed_solid_interface(LX, LY, LZ, i, j, k, ibg),
-                          zero(eltype(ibg.grid)),
-                          $func(i, j, k, ibg.grid, closure, K, id, LX, LY, LZ, clock, Δt, κz))
-        end
-
+        # Disambiguation
         @inline $func(i, j, k, ibg::GFIBG, closure, K, id, LX, LY, LZ::Face, clock, Δt, κz) =
                 $alt_func(i, j, k, ibg::GFIBG, closure, K, id, LX, LY, LZ, clock, Δt, κz)
 
         @inline $func(i, j, k, ibg::GFIBG, closure, K, id, LX, LY, LZ::Center, clock, Δt, κz) =
-                $alt_func(i, j, k, ibg::GFIBG, closure, K, id, LX, LY, LZ, Δt, κz)
+                $alt_func(i, j, k, ibg::GFIBG, closure, K, id, LX, LY, LZ, clock, Δt, κz)
+
+        @inline function $immersed_func(i, j, k, ibg::GFIBG, closure, K, id, LX, LY, LZ, clock, Δt, κz)
+            return ifelse(immersed_ivd_solid_interface(LX, LY, LZ, i, j, k, ibg),
+                          zero(eltype(ibg.grid)),
+                          $func(i, j, k, ibg.grid, closure, K, id, LX, LY, LZ, clock, Δt, κz))
+        end
     end
 end
 
