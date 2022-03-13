5127e26c4d06d0fc0f5391d7e503a4be8d6db392
Gregory Wagner <wagner.greg@gmail.com>
2022-03-13 16:59:51 -0600

    Hmm, cant use functions to set!

--- validation/distributed_simulations/mpi_nonhydrostatic_two_dimensional_turbulence.jl
+++ validation/distributed_simulations/mpi_nonhydrostatic_two_dimensional_turbulence.jl
@@ -2,11 +2,16 @@
 #
 # $ mpiexec -n 2 julia --project mpi_nonhydrostatic_two_dimensional_turbulence.jl
 #
-# For example.
+# for example.
+#
+# You also probably should set
+#
+# $ export JULIA_NUM_THREADS=1
 
 using MPI
 using Oceananigans
 using Oceananigans.Distributed
+using Statistics
 
 MPI.Init()
 
@@ -27,19 +32,42 @@ grid = RectilinearGrid(arch; topology, size=(Nx, Ny), halo=(3, 3), x=(0, 2π), y
 
 model = NonhydrostaticModel(; grid, advection=WENO5(), closure=ScalarDiffusivity(ν=1e-4, κ=1e-4))
 
-ϵ(x, y, z) = 2rand() - 1 # ∈ (-1, 1)
-set!(model, u=ϵ, v=ϵ)
+# This doesn't work?
+# ϵ(x, y, z) = 2rand() - 1 # ∈ (-1, 1)
+# set!(model, u=ϵ, v=ϵ)
+
+uᵢ = rand(size(grid)...)
+vᵢ = rand(size(grid)...)
+uᵢ .-= mean(uᵢ)
+vᵢ .-= mean(vᵢ)
+set!(model, u=uᵢ, v=vᵢ)
 
 u, v, w = model.velocities
 e_op = @at (Center, Center, Center) 1/2 * (u^2 + v^2)
 e = Field(e_op)
+ζ = Field(∂x(v) - ∂y(u))
 compute!(e)
-@show e
+compute!(ζ)
 
-simulation = Simulation(model, Δt=0.01, stop_time=10)
+simulation = Simulation(model, Δt=0.01, stop_iteration=1000)
+
+function progress(sim)
+    comm = MPI.COMM_WORLD
+    rank = MPI.Comm_rank(comm)
+    compute!(ζ)
+    compute!(e)
+
+    if rank === 0
+        @info "Iteration: " * iteration(sim) * ", time: " * prettytime(sim)
+    end
+
+    @info @sprintf("Rank %d: max|ζ|: %.2e, max(e): %.2e",
+                   MPI.Comm_rank(comm), maximum(abs, ζ), maximum(abs, e))
+
+    return nothing
+end
+
+simulation.callbacks[:progress] = Callback(progress, IterationInterval(10))
 
 run!(simulation)
-
-compute!(e)
-@show e
 
