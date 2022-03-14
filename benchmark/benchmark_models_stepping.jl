338c06f13ba27765e5ed34b1242539a85ead8ac2
Simone Silvestri <silvestri.simone0@gmail.com>
2022-03-14 00:19:10 -0400

    restored benchmark

--- benchmark/benchmark_models_stepping.jl
+++ benchmark/benchmark_models_stepping.jl
@@ -66,34 +66,35 @@ for (model, name) in zip((:nonhydrostatic, :hydrostatic, :shallowwater), ("Nonhy
     @eval begin
         suite = run_benchmarks($benchmark_func; Architectures, Float_types, Ns)
     end
-    # plot_num = length(Ns)
-    # CPU_Float32 = zeros(Float64, plot_num)
-    # CPU_Float64 = zeros(Float64, plot_num)
-    # GPU_Float32 = zeros(Float64, plot_num)
-    # GPU_Float64 = zeros(Float64, plot_num)
-    # plot_keys = collect(keys(suite))
-    # sort!(plot_keys, by = v -> [Symbol(v[1]), Symbol(v[2]), v[3]])
 
-    # for i in 1:plot_num
-    #     CPU_Float32[i] = mean(suite[plot_keys[i]].times) / 1.0e6
-    #     CPU_Float64[i] = mean(suite[plot_keys[i+plot_num]].times) / 1.0e6
-    #     GPU_Float32[i] = mean(suite[plot_keys[i+2plot_num]].times) / 1.0e6
-    #     GPU_Float64[i] = mean(suite[plot_keys[i+3plot_num]].times) / 1.0e6
-    # end
+    plot_num = length(Ns)
+    CPU_Float32 = zeros(Float64, plot_num)
+    CPU_Float64 = zeros(Float64, plot_num)
+    GPU_Float32 = zeros(Float64, plot_num)
+    GPU_Float64 = zeros(Float64, plot_num)
+    plot_keys = collect(keys(suite))
+    sort!(plot_keys, by = v -> [Symbol(v[1]), Symbol(v[2]), v[3]])
 
-    # plt = plot(Ns, CPU_Float32, lw=4, label="CPU Float32", xaxis=:log2, yaxis=:log, legend=:topleft,
-    #         xlabel="Nx", ylabel="Times (ms)", title= name * "Model Benchmarks: CPU vs GPU")
-    # plot!(plt, Ns, CPU_Float64, lw=4, label="CPU Float64")
-    # plot!(plt, Ns, GPU_Float32, lw=4, label="GPU Float32")
-    # plot!(plt, Ns, GPU_Float64, lw=4, label="GPU Float64")
-    # display(plt)
-    # savefig(plt, "nonhydrostatic_times.png")
+    for i in 1:plot_num
+        CPU_Float32[i] = mean(suite[plot_keys[i]].times) / 1.0e6
+        CPU_Float64[i] = mean(suite[plot_keys[i+plot_num]].times) / 1.0e6
+        GPU_Float32[i] = mean(suite[plot_keys[i+2plot_num]].times) / 1.0e6
+        GPU_Float64[i] = mean(suite[plot_keys[i+3plot_num]].times) / 1.0e6
+    end
 
-    # plt2 = plot(Ns, CPU_Float32./GPU_Float32, lw=4, xaxis=:log2, legend=:topleft, label="Float32",
-    #             xlabel="Nx", ylabel="Speedup Ratio", title= name * "Model Benchmarks: CPU/GPU")
-    # plot!(plt2, Ns, CPU_Float64./GPU_Float64, lw=4, label="Float64")
-    # display(plt2)
-    # savefig(plt2, "nonhydrostatic_speedup.png")
+    plt = plot(Ns, CPU_Float32, lw=4, label="CPU Float32", xaxis=:log2, yaxis=:log, legend=:topleft,
+            xlabel="Nx", ylabel="Times (ms)", title= name * "Model Benchmarks: CPU vs GPU")
+    plot!(plt, Ns, CPU_Float64, lw=4, label="CPU Float64")
+    plot!(plt, Ns, GPU_Float32, lw=4, label="GPU Float32")
+    plot!(plt, Ns, GPU_Float64, lw=4, label="GPU Float64")
+    display(plt)
+    savefig(plt, "nonhydrostatic_times.png")
+
+    plt2 = plot(Ns, CPU_Float32./GPU_Float32, lw=4, xaxis=:log2, legend=:topleft, label="Float32",
+                xlabel="Nx", ylabel="Speedup Ratio", title= name * "Model Benchmarks: CPU/GPU")
+    plot!(plt2, Ns, CPU_Float64./GPU_Float64, lw=4, label="Float64")
+    display(plt2)
+    savefig(plt2, "nonhydrostatic_speedup.png")
 
     df = benchmarks_dataframe(suite)
     benchmarks_pretty_table(df, title=name * " benchmarks")
