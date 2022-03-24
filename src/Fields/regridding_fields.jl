6489770419c052cd5972007cf8daa80222aa31a1
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-03-24 14:53:41 +1100

    fix doctests

--- src/Fields/regridding_fields.jl
+++ src/Fields/regridding_fields.jl
@@ -30,23 +30,20 @@ input_grid = RectilinearGrid(size=Nz, z = [0, Lz/3, Lz], topology=topology, halo
 input_field = CenterField(input_grid)
 input_field[1, 1, 1:Nz] = [2, 3]
 
-output_grid = RectilinearGrid(size=Nz, z=(0, Lz), topology=topology)
+output_grid = RectilinearGrid(size=Nz, z=(0, Lz), topology=topology, halo=1)
 output_field = CenterField(output_grid)
 
 regrid!(output_field, input_field)
 
-julia> output_field[1, 1, :]
+output_field[1, 1, :]
+
 # output
-8-element OffsetArray(::Vector{Float64}, -2:5) with eltype Float64 with indices -2:5:
- 0.0
- 0.0
+4-element OffsetArray(::Vector{Float64}, 0:3) with eltype Float64 with indices 0:3:
  0.0
  2.333333333333333
  3.0
  0.0
- 0.0
- 0.0
-```
+ ```
 """
 regrid!(a, b) = regrid!(a, a.grid, b.grid, b)
 
