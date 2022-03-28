7374f198da034309a0bb8555a7c5f0447e8b225f
tomchor <tomaschor@gmail.com>
2022-03-28 12:41:58 -0700

    improved plotting to all directions

--- sandbox/plot_drag.py
+++ sandbox/plot_drag.py
@@ -6,44 +6,65 @@ import pynanigans as pn
 
 #++++ Options
 plot = True
-parallel = False
+parallel = True
+directions = ["x", "y", "z"]
+velocities = ["u", "v", "w"]
 #----
 
 
 #++++ Define plotting function
-def plotfunc(ds, fig, tt, *args, **kwargs):
+def plotfunc(ds, fig, tt, framedim, y_dir = "z", vels=["u", "v"]):
     ds0 = ds.isel(time=tt)
-    ax1 = fig.subplots(ncols=1,)
+    axes = fig.subplots(ncols=2, sharey=True)
 
     # Time series
-    ds0.u_cdrag.pnplot(ax=ax1, y='z', label="u (no IBM w/ drag)")
-    ds0.u_imsd.pnplot(ax=ax1, y='z', label="u (w/ IBM)", ls="--")
-    ax1.set_xlim(0, None)
-    ax1.legend()
+    ds0[f"{vels[0]}_ddrag"].pnplot(ax=axes[0], y=y_dir, label="u (no IBM w/ drag)")
+    ds0[f"{vels[0]}_immsd"].pnplot(ax=axes[0], y=y_dir, label="u (w/ IBM)", ls="--")
 
+    ds0[f"{vels[1]}_ddrag"].pnplot(ax=axes[1], y=y_dir, label="u (no IBM w/ drag)")
+    ds0[f"{vels[1]}_immsd"].pnplot(ax=axes[1], y=y_dir, label="u (w/ IBM)", ls="--")
+
+    for i, ax in enumerate(axes):
+        ax.set_xlim(0, None)
+        ax.legend()
+        ax.set_title(vels[i])
     return None, None
 #----
 
+for bounded_dir in directions:
 
-#++++ Open dataset
-vid_cdrag = xr.load_dataset("control_drag_model.nc", decode_times=False, engine="netcdf4")
-vid_imsd = xr.load_dataset("immersed_model.nc", decode_times=False, engine="netcdf4")
-ds = xr.Dataset(dict(u_cdrag=vid_cdrag.u, u_imsd=vid_imsd.u)).pnmean(('x', 'y'))
-if parallel:
-    ds = ds.chunk(dict(time=1))
-#----
+    #++++ Get directions right
+    per_directions = [ direc for direc in directions if direc is not bounded_dir ]
+    per_velocities = [ veloc for direc, veloc in zip(directions, velocities) if direc is not bounded_dir ]
+    #----
+    
+    #++++ Open dataset
+    vid_ddrag = xr.load_dataset(f"{bounded_dir}_control_drag_model.nc", decode_times=False, engine="netcdf4")
+    vid_immsd = xr.load_dataset(f"{bounded_dir}_immersed_model.nc", decode_times=False, engine="netcdf4")
 
-#++++
-if plot:
-    from matplotlib import pyplot as plt
-    from xmovie import Movie
-    from dask.diagnostics import ProgressBar
-    plt.rcParams['figure.constrained_layout.use'] = True
+    vels_ddrag = { f"{vel}_ddrag" : vid_ddrag[vel] for vel in per_velocities }
+    vels_immsd = { f"{vel}_immsd" : vid_immsd[vel] for vel in per_velocities }
+    ds = xr.Dataset(vels_ddrag | vels_immsd).squeeze()
 
+    if parallel:
+        ds = ds.chunk(dict(time=1))
+    #----
+    
+    #++++
+    if plot:
+        from matplotlib import pyplot as plt
+        from xmovie import Movie
+        from dask.diagnostics import ProgressBar
+        plt.rcParams['figure.constrained_layout.use'] = True
+    
+    
+        mov = Movie(ds, plotfunc, input_check=False, 
+                    y_dir=bounded_dir,
+                    vels=per_velocities)
 
-    mov = Movie(ds, plotfunc, input_check=False)
-    with ProgressBar():
-        mov.save(f"u_drag.mp4", 
-                 parallel=parallel, parallel_compute_kwargs=dict(), 
-                 overwrite_existing=True)
-#----
+        with ProgressBar():
+            mov.save(f"{bounded_dir}_drag.mp4", 
+                     parallel=parallel, parallel_compute_kwargs=dict(), 
+                     overwrite_existing=True,
+                     )
+    #----
