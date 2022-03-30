e86a014a47273fd5dc1692bf480c52cc3b2d5af9
Gregory Wagner <wagner.greg@gmail.com>
2022-03-30 16:42:16 -0600

    Updates

--- validation/elliptic_solvers/doubly_bounded_poisson.jl
+++ validation/elliptic_solvers/doubly_bounded_poisson.jl
@@ -8,18 +8,19 @@ grid = RectilinearGrid(size=(128, 128), x=(-4, 4), y=(-4, 4), topology=(Bounded,
 # periodic_grid = RectilinearGrid(size=(128, 128), x=(0, 2π), y=(0, 2π), topology=(Periodic, Periodic, Flat))
 
 r = CenterField(grid)
-ϕ = CenterField(grid)
+ϕ_fft = CenterField(grid)
+ϕ_hd = CenterField(grid)
 
 r₀(x, y, z) = exp(-x^2 - y^2)
 set!(r, r₀)
 fill_halo_regions!(r, grid.architecture)
 
-# Solve ∇²ϕ = r with `FFTBasedPoissonSolver`
+# Solve ∇²ϕ_fft = r with `FFTBasedPoissonSolver`
 fft_solver = FFTBasedPoissonSolver(grid)
 fft_solver.storage .= interior(r)
-@time solve!(ϕ, fft_solver, fft_solver.storage)
+@time solve!(ϕ_fft, fft_solver, fft_solver.storage)
 
-# Solve ∇²ϕ = r with `HeptadiagonalIterativeSolver`
+# Solve ∇²ϕ_fft = r with `HeptadiagonalIterativeSolver`
 Nx, Ny, Nz = size(grid)
 C = zeros(Nx, Ny, Nz)
 D = zeros(Nx, Ny, Nz)
@@ -27,15 +28,13 @@ Ax = [Δzᵃᵃᶜ(i, j, k, grid) * Δyᶠᶜᵃ(i, j, k, grid) / Δxᶠᶜᵃ(i
 Ay = [Δzᵃᵃᶜ(i, j, k, grid) * Δxᶜᶠᵃ(i, j, k, grid) / Δyᶜᶠᵃ(i, j, k, grid) for i=1:Nx, j=1:Ny, k=1:Nz]
 Az = [Δxᶜᶜᵃ(i, j, k, grid) * Δyᶜᶜᵃ(i, j, k, grid) / Δzᵃᵃᶠ(i, j, k, grid) for i=1:Nx, j=1:Ny, k=1:Nz]
 
-solver = HeptadiagonalIterativeSolver((Ax, Ay, Az, C, D); grid, preconditioner_method = nothing)
+hd_solver = HeptadiagonalIterativeSolver((Ax, Ay, Az, C, D); grid, preconditioner_method = nothing)
+solve!(ϕ_hd, hd_solver, interior(r), 1.0)
 
-
-#=
 fig = Figure(resolution=(1200, 600))
 ax_r = Axis(fig[1, 1], title="RHS")
-ax_ϕ = Axis(fig[1, 2], title="Solution")
+ax_ϕ_fft = Axis(fig[1, 2], title="Solution")
 heatmap!(ax_r, interior(r, :, :, 1))
-heatmap!(ax_ϕ, interior(ϕ, :, :, 1))
+heatmap!(ax_ϕ_fft, interior(ϕ_fft, :, :, 1))
 
 display(fig)
-=#
