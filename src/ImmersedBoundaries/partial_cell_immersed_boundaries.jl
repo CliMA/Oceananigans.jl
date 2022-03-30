cccbdc12609f25ad6298060fdbf96565706dc227
Gregory Wagner <wagner.greg@gmail.com>
2022-03-30 10:35:59 -0600

    Fleshes out partial cell implementation

--- src/ImmersedBoundaries/partial_cell_immersed_boundaries.jl
+++ src/ImmersedBoundaries/partial_cell_immersed_boundaries.jl
@@ -28,6 +28,17 @@ end
     return bottom_height(x, y)
 end
 
+"""
+
+        --x--
+          ∘   k+1
+    k+1 --x--    ↑      <- node z
+          ∘   k  | Δz
+      k --x--    ↓
+      
+Criterion is h >= z - ϵ Δz
+
+"""
 @inline function is_immersed(i, j, k, underlying_grid, ib::PartialCellBottom)
     # Face node above current cell
     x, y, z = node(c, c, f, i, j, k+1, underlying_grid)
@@ -42,7 +53,25 @@ const PCIBG = ImmersedBoundaryGrid{<:Any, <:Any, <:Any, <:Any, <:Any, <:PartialC
 bottom_cell(i, j, k, ibg::PCIBG) = !is_immersed(i, j, k, ibg.grid, ibg.immersed_boundary) & is_immersed(i, j, k-1, ibg.grid, ibg.immersed_boundary)
 
 @inline function Δzᶜᶜᶜ(i, j, k, ibg::PCIBG)
-    x, y, z = node(c, c, c, i, j, k, underlying_grid)
-    Δzᶜᶜᶜ = ibg.immersed_boundary.bottom_height(x, y)
-    return Δzᶜᶜᶜ
+    underlying_grid = ibg.grid
+    ib = ibg.immersed_boundary
+    # Get node at face above
+    x, y, z = node(c, c, c, i, j, k+1, underlying_grid)
+
+    # Get bottom height and fractional Δz parameter
+    h = get_bottom_height(i, j, k, underlying_grid, ib.bottom_height)
+    ϵ = ibg.immersed_boundary.minimum_fractional_partial_Δz
+
+    # Are we in a bottom cell?
+    at_the_bottom = bottom_cell(i, j, k, ibg)
+
+    full_Δz = Δzᶜᶜᶜ(i, j, k, ibg.grid)
+    partial_Δz = max(ϵ * full_Δz, z - h)
+
+    return ifelse(at_the_bottom, partial_Δz, full_Δz)
 end
+
+@inline Δzᶠᶜᶜ(i, j, k, ibg::PCIBG) = min(Δzᶜᶜᶜ(i-1, j, k, ibg), Δzᶜᶜᶜ(i, j, k, ibg))
+@inline Δzᶜᶠᶜ(i, j, k, ibg::PCIBG) = min(Δzᶜᶜᶜ(i, j-1, k, ibg), Δzᶜᶜᶜ(i, j, k, ibg))
+
+# TODO define Δzᶜᶜᶠ
