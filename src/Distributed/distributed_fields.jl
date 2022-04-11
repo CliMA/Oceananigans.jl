372807a724cb4c6c404badc696f9d40f3495a45e
Simone Silvestri <silvestri.simone0@gmail.com>
2022-04-11 06:10:08 -0400

    bugfix in distributed

--- src/Distributed/distributed_fields.jl
+++ src/Distributed/distributed_fields.jl
@@ -1,10 +1,11 @@
-import Oceananigans.Fields: Field, location
+import Oceananigans.Fields: Field, FieldBoundaryBuffers, location
 import Oceananigans.BoundaryConditions: fill_halo_regions!
 
 function Field((LX, LY, LZ)::Tuple, grid::DistributedGrid, data, old_bcs, indices::Tuple, op, status)
     arch = architecture(grid)
     new_bcs = inject_halo_communication_boundary_conditions(old_bcs, arch.local_rank, arch.connectivity)
-    return Field{LX, LY, LZ}(grid, data, new_bcs, indices, op, status)
+    buffers = FieldBoundaryBuffers(grid, data, new_bcs)
+    return Field{LX, LY, LZ}(grid, data, new_bcs, indices, op, status, buffers)
 end
 
 const DistributedField      = Field{<:Any, <:Any, <:Any, <:Any, <:DistributedGrid}
