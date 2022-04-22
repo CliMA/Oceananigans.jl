0d68a2acc081979b318e7f9a594d5bb831919527
Gregory Wagner <wagner.greg@gmail.com>
2022-04-22 13:34:14 -0800

    Starts developing simple surface relaxation

--- validation/near_global_lat_lon/one_degree_setups/near_global_one_degree.jl
+++ validation/near_global_lat_lon/one_degree_setups/near_global_one_degree.jl
@@ -21,59 +21,50 @@ output_prefix = "near_global_lat_lon_$(Nx)_$(Ny)_$(Nz)"
 
 bathymetry = jldopen("bathymetry-360x150-latitude-75.0.jld2")["bathymetry"]
 
-τˣ = zeros(Nx, Ny, Nmonths)
-τʸ = zeros(Nx, Ny, Nmonths)
-T★ = zeros(Nx, Ny, Nmonths)
-S★ = zeros(Nx, Ny, Nmonths)
-
 include("one_degree_interface_heights.jl")
 z = one_degree_interface_heights()
 
 # A spherical domain
 @show underlying_grid = LatitudeLongitudeGrid(arch; size = (Nx, Ny, Nz), halo = (4, 4, 4),
-                                              latitude, 
+                                              latitude, z,
                                               longitude = (-180, 180),
-                                              z,
                                               precompute_metrics = true)
 
 grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(bathymetry))
 
-
-#####
-##### Physics and model setup
-#####
-
-νh = 1e+1
-νz = 5e-3
-κh = 1e+1
-κz = 1e-4
-
-κ_skew = 1000.0 * 1e-3       # [m² s⁻¹] skew diffusivity
-κ_symmetric = 900.0 * 1e-3   # [m² s⁻¹] symmetric diffusivity
-
 using Oceananigans.Operators: Δx, Δy
 using Oceananigans.TurbulenceClosures
 
-@inline νhb(i, j, k, grid, lx, ly, lz) = (1 / (1 / Δx(i, j, k, grid, lx, ly, lz)^2 + 1 / Δy(i, j, k, grid, lx, ly, lz)^2))^2 / 5days
+@inline ν₄(i, j, k, grid, lx, ly, lz) = (1 / (1 / Δx(i, j, k, grid, lx, ly, lz)^2 + 1 / Δy(i, j, k, grid, lx, ly, lz)^2))^2 / 5days
 
-horizontal_diffusivity = HorizontalScalarDiffusivity(ν=νh, κ=κh)
-vertical_diffusivity = VerticalScalarDiffusivity(VerticallyImplicitTimeDiscretization(), ν=νz, κ=κz)
-convective_adjustment = RiBasedVerticalDiffusivity(VerticallyImplicitTimeDiscretization(), κ₀=1.0)
-biharmonic_viscosity = HorizontalScalarBiharmonicDiffusivity(ν=νhb, discrete_form=true)
+horizontal_diffusivity = HorizontalScalarDiffusivity(ν=1e1, κ=1e1)
+background_vertical_diffusivity = VerticalScalarDiffusivity(VerticallyImplicitTimeDiscretization(), ν=1e-2, κ=1e-4)
+dynamic_vertical_diffusivity = RiBasedVerticalDiffusivity()
+biharmonic_viscosity = HorizontalScalarBiharmonicDiffusivity(ν=ν₄, discrete_form=true)
 
+κ_skew = 1000.0      # [m² s⁻¹] skew diffusivity
+κ_symmetric = 1000.0 # [m² s⁻¹] symmetric diffusivity
 gerdes_koberle_willebrand_tapering = FluxTapering(1e-2)
 gent_mcwilliams_diffusivity = IsopycnalSkewSymmetricDiffusivity(κ_skew = κ_skew,
                                                                 κ_symmetric = κ_symmetric,
                                                                 slope_limiter = gerdes_koberle_willebrand_tapering)
 
-free_surface = ImplicitFreeSurface(solver_method = :HeptadiagonalIterativeSolver,
-                                   preconditioner_method = :SparseInverse,
-                                   preconditioner_settings = (ε=0.01, nzrel=10))
+free_surface = ImplicitFreeSurface(solver_method = :HeptadiagonalIterativeSolver)
+                                   # preconditioner_method = :SparseInverse,
+                                   # preconditioner_settings = (ε=0.01, nzrel=10))
 
 equation_of_state = LinearEquationOfState()
 buoyancy = SeawaterBuoyancy(; equation_of_state, constant_salinity=35.0)
 
-closures = (horizontal_diffusivity, vertical_diffusivity, convective_adjustment, biharmonic_viscosity, gent_mcwilliams_diffusivity)
+closures = (horizontal_diffusivity, background_vertical_diffusivity, dynamic_vertical_diffusivity, gent_mcwilliams_diffusivity)
+# closures = (horizontal_diffusivity, vertical_diffusivity, convective_adjustment, biharmonic_viscosity, gent_mcwilliams_diffusivity)
+
+#=
+@inline reference_temperature(φ) = 30.0 * cos(2π * φ / 180)
+@inline temperature_relaxation(λ, φ, t, T, λ) = λ * (T - reference_temperature(φ))
+T_top_bc = FluxBoundaryCondition(temperature_relaxation, field_dependencies=:T, parameters=30days)
+T_bcs = FieldBoundaryConditions(top=T_top_bc)
+=#
 
 model = HydrostaticFreeSurfaceModel(; grid, free_surface, buoyancy,
                                     momentum_advection = VectorInvariant(),
@@ -87,11 +78,13 @@ simulation = Simulation(model; Δt=20minutes, stop_iteration=10)
 wall_clock = Ref(time_ns())
 function progress(sim)
     elapsed = 1e-9 * (time_ns() - wall_clock[])
+
     @info string("Iteration: ", iteration(sim),
                  ", time: ", prettytime(sim),
                  ", wall time: ", prettytime(elapsed))
 
     wall_clock[] = time_ns()
+
     return nothing
 end
 
