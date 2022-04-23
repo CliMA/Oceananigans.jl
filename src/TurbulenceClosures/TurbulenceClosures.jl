de8255ce019174bd4950611798f61844e665af4f
Gregory Wagner <wagner.greg@gmail.com>
2022-04-22 18:28:28 -0800

    Export RiBased and FluxTapering

--- src/TurbulenceClosures/TurbulenceClosures.jl
+++ src/TurbulenceClosures/TurbulenceClosures.jl
@@ -12,7 +12,9 @@ export
     SmagorinskyLilly,
     AnisotropicMinimumDissipation,
     ConvectiveAdjustmentVerticalDiffusivity,
+    RiBasedVerticalDiffusivity,
     IsopycnalSkewSymmetricDiffusivity,
+    FluxTapering,
 
     ExplicitTimeDiscretization,
     VerticallyImplicitTimeDiscretization,
