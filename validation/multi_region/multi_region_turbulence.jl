c6c61227983930dbf114fca9d06e58c0c9a9ae4e
Simone Silvestri <silvestri.simone0@gmail.com>
2022-04-27 11:04:43 -0400

    made fair comparison

--- validation/multi_region/multi_region_turbulence.jl
+++ validation/multi_region/multi_region_turbulence.jl
@@ -1,7 +1,8 @@
 using Oceananigans
 using Oceananigans.Advection: VelocityStencil
-using Oceananigans.MultiRegion: reconstruct_global_field
+using Oceananigans.MultiRegion: reconstruct_global_field, multi_region_object_from_array
 using GLMakie
+using Random
 
 Nh = 128
 grid = RectilinearGrid(size=(Nh, Nh, 1), halo=(4, 4, 4), x=(0, 2π), y=(0, 2π), z=(0, 1), topology=(Periodic, Periodic, Bounded))
@@ -10,25 +11,33 @@ mrg  = MultiRegionGrid(grid, partition=XPartition(2))
 Δh = 2π / grid.Nx
 Δt = 0.1 * Δh
 
-momentum_advection = WENO5()
-#momentum_advection = WENO5(vector_invariant=VelocityStencil())
+ϵ(x, y, z)  =  2rand() - 1
+u_init = interior(set!(Field(Face, Center, Center), grid), ϵ)
+v_init = interior(set!(Field(Face, Center, Center), grid), ϵ)
 
-model = HydrostaticFreeSurfaceModel(; grid = mrg, momentum_advection,
+u_init_mrg = multi_region_object_from_array(u_init, mrg)
+v_init_mrg = multi_region_object_from_array(v_init, mrg)
+
+momentum_advection = WENO5()
+free_surface = ImplicitFreeSurface(gravitational_acceleration=1)
+#momentum_advection = WENO5(vector_invariant=VelocityStencil())
+progress(sim) = @info "Iteration: $(iteration(sim)), time: $(time(sim))"
+
+## Multi region model
+
+model = HydrostaticFreeSurfaceModel(; grid = mrg, momentum_advection, free_surface,
                                     tracers = (),
                                     buoyancy = nothing,
-                                    free_surface = ImplicitFreeSurface(gravitational_acceleration=1),
                                     closure = ScalarDiffusivity(ν=1e-4))
 
-
-ϵ(x, y, z)  =  2rand() - 1
-set!(model, u=ϵ, v=ϵ)
+Random.seed!(1234)
+set!(model, u=u_init_mrg, v=v_init_mrg)
 
 simulation = Simulation(model; Δt, stop_iteration=10)
 run!(simulation)
 
 simulation.stop_iteration += 1000
 
-progress(sim) = @info "Iteration: $(iteration(sim)), time: $(time(sim))"
 simulation.callbacks[:progress] = Callback(progress, IterationInterval(100))
 
 start_time = time_ns()
@@ -38,20 +47,12 @@ elapsed_time = 1e-9 * (time_ns() - start_time)
 
 u, v, w = model.velocities
 
-# @show u
-
 u = reconstruct_global_field(u)
 v = reconstruct_global_field(v)
 
 ζ_1 = compute!(Field(∂x(v) - ∂y(u)))
 
-# fig = Figure()
-# ax = Axis(fig[1, 1])
-# heatmap!(ax, interior(ζ, :, :, 1))
-# display(fig)
-
-momentum_advection = WENO5()
-#momentum_advection = WENO5(vector_invariant=VelocityStencil())
+## Single region model
 
 model = HydrostaticFreeSurfaceModel(; grid, momentum_advection,
                                     tracers = (),
@@ -59,9 +60,9 @@ model = HydrostaticFreeSurfaceModel(; grid, momentum_advection,
                                     free_surface = ImplicitFreeSurface(gravitational_acceleration=1),
                                     closure = ScalarDiffusivity(ν=1e-4))
 
-
+Random.seed!(1234)                                    
 ϵ(x, y, z)  =  2rand() - 1
-set!(model, u=ϵ, v=ϵ)
+set!(model, u=u_init, v=v_init)
 
 simulation = Simulation(model; Δt, stop_iteration=10)
 run!(simulation)
@@ -78,12 +79,9 @@ elapsed_time = 1e-9 * (time_ns() - start_time)
 
 u, v, w = model.velocities
 
-# @show u
-
-if grid isa MultiRegionGrid
-    u = reconstruct_global_field(u)
-    v = reconstruct_global_field(v)
-    # @show u
-end
-
 ζ_2 = compute!(Field(∂x(v) - ∂y(u)))
+
+# fig = Figure()
+# ax = Axis(fig[1, 1])
+# heatmap!(ax, interior(ζ, :, :, 1))
+# display(fig)
