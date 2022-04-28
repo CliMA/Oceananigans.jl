ed53024da6a4cc49bb26491753c736304d263ba6
Gregory Wagner <wagner.greg@gmail.com>
2022-04-28 07:54:29 -0400

    Implement fallbacks for coefficient extractors, plus comment

--- src/TurbulenceClosures/vertically_implicit_diffusion_solver.jl
+++ src/TurbulenceClosures/vertically_implicit_diffusion_solver.jl
@@ -3,6 +3,31 @@ using Oceananigans.AbstractOperations: flip
 using Oceananigans.Solvers: BatchedTridiagonalSolver, solve!
 
 #####
+##### implicit_step! interface
+#####
+##### Closures with `VerticallyImplicitTimeDiscretization` can define
+#####
+##### 1. "Coefficient extractors" `νz` and `κz` to support vertically-implicit
+#####    treatment of a diffusive term iwth the form `∂z κz ∂z ϕ` for a variable `ϕ`. 
+#####    There are four extractors for momentum (`νz`) and three for tracers (`κz`).
+#####
+##### 2. `implicit_linear_coefficient` to support the implicit treament of a _linear_ term.
+#####
+
+# Fallbacks: extend these function for `closure` to support.
+# TODO: docstring
+@inline implicit_linear_coefficient(i, j, k, grid, closure, diffusivity_fields, tracer_index, LX, LY, LZ, clock, Δt, κz) =
+    zero(grid)
+
+@inline νzᶜᶜᶜ(i, j, k, grid, closure, diffusivity_fields, clock) = zero(grid)
+@inline νzᶠᶠᶜ(i, j, k, grid, closure, diffusivity_fields, clock) = zero(grid)
+@inline νzᶠᶜᶠ(i, j, k, grid, closure, diffusivity_fields, clock) = zero(grid)
+@inline νzᶜᶠᶠ(i, j, k, grid, closure, diffusivity_fields, clock) = zero(grid)
+@inline κzᶠᶜᶜ(i, j, k, grid, closure, diffusivity_fields, tracer_index, clock) = zero(grid)
+@inline κzᶜᶠᶜ(i, j, k, grid, closure, diffusivity_fields, tracer_index, clock) = zero(grid)
+@inline κzᶜᶜᶠ(i, j, k, grid, closure, diffusivity_fields, tracer_index, clock) = zero(grid)
+
+#####
 ##### Batched Tridiagonal solver for implicit diffusion
 #####
 
@@ -72,9 +97,6 @@ end
 @inline maybe_tupled_implicit_linear_coefficient(args...) = implicit_linear_coefficient(args...)
 @inline maybe_tupled_ivd_upper_diagonal(args...) = ivd_upper_diagonal(args...)
 @inline maybe_tupled_ivd_lower_diagonal(args...) = ivd_lower_diagonal(args...)
-
-# Default
-@inline implicit_linear_coefficient(i, j, k, grid, closure, args...) = zero(eltype(grid))
 
 #####
 ##### Solver constructor
