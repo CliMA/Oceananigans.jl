f4bc28b9c2d8d480a1b985cf85ba72c5e8d95d4c
Matin Raayai <30674652+matinraayai@users.noreply.github.com>
2022-04-28 16:36:26 +0000

    Update src/Architectures.jl
    Co-authored-by: Gregory L. Wagner <gregory.leclaire.wagner@gmail.com>

--- src/Architectures.jl
+++ src/Architectures.jl
@@ -35,7 +35,7 @@ variable `JULIA_NUM_THREADS` is set.
 struct CPU <: AbstractArchitecture end
 
 """
-    CUDAGPU <: AbstractArchitecture
+    GPU <: AbstractArchitecture
 
 Run Oceananigans on a single NVIDIA CUDA GPU.
 """
