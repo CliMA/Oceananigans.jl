b694b371227ebf8efc1233cbb9ea476d2fcb807d
Simone Silvestri <silvestri.simone0@gmail.com>
2022-04-29 15:58:43 -0400

    fixed typing

--- src/Grids/grid_generation.jl
+++ src/Grids/grid_generation.jl
@@ -11,14 +11,20 @@ get_coord_face(coord::Nothing, i) = 1
 get_coord_face(coord::Function, i) = coord(i)
 get_coord_face(coord::AbstractVector, i) = CUDA.@allowscalar coord[i]
 
-lower_exterior_Δcoordᶠ(topology,        Fi, Hcoord) = [Fi[end - Hcoord + i] - Fi[end - Hcoord + i - 1] for i = 1:Hcoord]
-lower_exterior_Δcoordᶠ(::Type{Bounded}, Fi, Hcoord) = [Fi[2]  - Fi[1] for i = 1:Hcoord]
+lower_exterior_Δcoordᶠ(topology, Fi, Hcoord) = [Fi[end - Hcoord + i] - Fi[end - Hcoord + i - 1] for i = 1:Hcoord]
+lower_exterior_Δcoordᶠ(::Type{<:BoundedTopology}, Fi, Hcoord) = [Fi[2]  - Fi[1] for i = 1:Hcoord]
 
-upper_exterior_Δcoordᶠ(topology,        Fi, Hcoord) = [Fi[i + 1] - Fi[i] for i = 1:Hcoord]
-upper_exterior_Δcoordᶠ(::Type{Bounded}, Fi, Hcoord) = [Fi[end]   - Fi[end - 1] for i = 1:Hcoord]
+upper_exterior_Δcoordᶠ(topology, Fi, Hcoord) = [Fi[i + 1] - Fi[i] for i = 1:Hcoord]
+upper_exterior_Δcoordᶠ(::Type{<:BoundedTopology}, Fi, Hcoord) = [Fi[end]   - Fi[end - 1] for i = 1:Hcoord]
+
+upper_interior_F(topology, coord, Δ)               = coord - Δ
+upper_interior_F(::Type{<:BoundedTopology}, coord) = coord
+
+total_interior_length(topology, N)                  = N
+total_interior_length(::Type{<:BoundedTopology}, N) = N + 1
 
 # generate a stretched coordinate passing the explicit coord faces as vector of functionL
-function generate_coordinate(FT, topology, N, H, coord, architecture)
+function generate_coordinate(FT, topology, N, H, coord, arch)
 
     # Ensure correct type for F and derived quantities
     interiorF = zeros(FT, N+1)
@@ -56,15 +62,15 @@ function generate_coordinate(FT, topology, N, H, coord, architecture)
         Δᶠ[i] = Δᶠ[i-1]
     end
 
-    Δᶜ = OffsetArray(arch_array(architecture, Δᶜ), -H)
-    Δᶠ = OffsetArray(arch_array(architecture, Δᶠ), -H-1)
+    Δᶜ = OffsetArray(arch_array(arch, Δᶜ), -H)
+    Δᶠ = OffsetArray(arch_array(arch, Δᶠ), -H-1)
 
     F = OffsetArray(F, -H)
     C = OffsetArray(C, -H)
 
     # Convert to appropriate array type for arch
-    F = OffsetArray(arch_array(architecture, F.parent), F.offsets...)
-    C = OffsetArray(arch_array(architecture, C.parent), C.offsets...)
+    F = OffsetArray(arch_array(arch, F.parent), F.offsets...)
+    C = OffsetArray(arch_array(arch, C.parent), C.offsets...)
 
     return L, F, C, Δᶠ, Δᶜ
 end
@@ -78,11 +84,14 @@ function generate_coordinate(FT, topology, N, H, coord::Tuple{<:Number, <:Number
     @assert c₁ < c₂
     L = c₂ - c₁
 
-    F = range(c₁, c₂, length = N + 1)
-    F = StepRangeLen(F.ref, F.step, F.len + 2 * H, F.offset + H)
-
     # Convert to get the correct type also when using single precision
     Δᶠ = Δᶜ = Δ = L / N
+    
+    F₋ = c₁
+    F₊ = upper_interior_F(topology, c₂, Δ)
+
+    F = range(c₁, c₂, length = total_interior_length(topology, N))
+    F = StepRangeLen(F.ref, F.step, F.len + 2 * H, F.offset + H)
 
     C₋ = c₁ + Δ / 2
     C₊ = c₂ - Δ / 2
@@ -90,13 +99,13 @@ function generate_coordinate(FT, topology, N, H, coord::Tuple{<:Number, <:Number
     C = range(C₋, C₊, length = N)
     C = StepRangeLen(C.ref, C.step, C.len + 2 * H, C.offset + H)
 
-    F = OffsetArray(F, -H)
-    C = OffsetArray(C, -H)
+    F = OffsetArray(FT.(F), -H)
+    C = OffsetArray(FT.(C), -H)
         
-    return L, F, C, Δᶠ, Δᶜ
+    return FT(L), F, C, FT(Δᶠ), FT(Δᶜ)
 end
 
 # Flat domains
-function generate_coordinate(FT, ::Type{Flat}, N, H, coord::Tuple{<:Number, <:Number}, architecture)
+function generate_coordinate(FT, ::Type{Flat}, N, H, coord::Tuple{<:Number, <:Number}, arch)
     return FT(1), range(1, 1, length=N), range(1, 1, length=N), FT(1), FT(1)
 end
