62d2b52054187a6e9da4f36e8e164164468b2611
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2022-04-30 11:46:15 -0400

    Update grid_generation.jl

--- src/Grids/grid_generation.jl
+++ src/Grids/grid_generation.jl
@@ -12,16 +12,16 @@ get_coord_face(coord::Function, i) = coord(i)
 get_coord_face(coord::AbstractVector, i) = CUDA.@allowscalar coord[i]
 
 lower_exterior_Δcoordᶠ(topology, Fi, Hcoord) = [Fi[end - Hcoord + i] - Fi[end - Hcoord + i - 1] for i = 1:Hcoord]
-lower_exterior_Δcoordᶠ(::Type{<:BoundedTopology}, Fi, Hcoord) = [Fi[2]  - Fi[1] for i = 1:Hcoord]
+lower_exterior_Δcoordᶠ(::Type{<:Bounded}, Fi, Hcoord) = [Fi[2]  - Fi[1] for i = 1:Hcoord]
 
 upper_exterior_Δcoordᶠ(topology, Fi, Hcoord) = [Fi[i + 1] - Fi[i] for i = 1:Hcoord]
-upper_exterior_Δcoordᶠ(::Type{<:BoundedTopology}, Fi, Hcoord) = [Fi[end]   - Fi[end - 1] for i = 1:Hcoord]
+upper_exterior_Δcoordᶠ(::Type{<:Bounded}, Fi, Hcoord) = [Fi[end]   - Fi[end - 1] for i = 1:Hcoord]
 
-upper_interior_F(topology, coord, Δ)               = coord - Δ
-upper_interior_F(::Type{<:BoundedTopology}, coord) = coord
+upper_interior_F(topology, coord, Δ)       = coord - Δ
+upper_interior_F(::Type{<:Bounded}, coord) = coord
 
-total_interior_length(topology, N)                  = N
-total_interior_length(::Type{<:BoundedTopology}, N) = N + 1
+total_interior_length(topology, N)          = N
+total_interior_length(::Type{<:Bounded}, N) = N + 1
 
 # generate a stretched coordinate passing the explicit coord faces as vector of functionL
 function generate_coordinate(FT, topology, N, H, coord, arch)
