c3e276fc6ac2abffcdb73727258dddd050d08cc2
Gregory Wagner <wagner.greg@gmail.com>
2022-05-04 11:42:26 -0400

    Update resting stratified bump=y ocean script

--- validation/immersed_boundaries/resting_stratified_bumpy_ocean.jl
+++ validation/immersed_boundaries/resting_stratified_bumpy_ocean.jl
@@ -6,6 +6,7 @@ using GLMakie
 
 arch = CPU()
 tracer_advection = CenteredSecondOrder()
+momentum_advection = CenteredSecondOrder()
 
 underlying_grid = RectilinearGrid(arch,
                                   size=(128, 64), halo=(3, 3), 
@@ -23,31 +24,94 @@ seamount_field = Field{Center, Center, Nothing}(underlying_grid)
 set!(seamount_field, seamount)
 fill_halo_regions!(seamount_field)
 
-grid = ImmersedBoundaryGrid(underlying_grid, PartialCellBottom(seamount_field.data))
-#grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(seamount_field.data))
+immersed_boundaries = [
+                       PartialCellBottom(seamount_field.data,
+                                         minimum_fractional_partial_Δz=1.0),
+                       GridFittedBottom(seamount_field.data)
+                      ]
 
-model = HydrostaticFreeSurfaceModel(; grid,
-                                    tracer_advection,
-                                    tracers = :b,
-                                    buoyancy = BuoyancyTracer())
+b = []
+v = []
 
-N² = 1e-1
-bᵢ(x, y, z) = N² * z
-set!(model, b = bᵢ)
+function progress(sim)
+    vmax = maximum(abs, sim.model.velocities.v)
+    @info @sprintf("Iter: %d, time: %.2e, max|v|: %.2e",
+                   iteration(sim), time(sim), vmax)
 
-simulation = Simulation(model; Δt=1e-3, stop_iteration=10)
+    return nothing
+end
 
-run!(simulation)
+for ib in immersed_boundaries
+    grid = ImmersedBoundaryGrid(underlying_grid, ib)
 
-fig = Figure()
-ax_b = Axis(fig[1, 1])
-ax_v = Axis(fig[1, 1])
+    @show grid
 
-b = model.tracers.b
-u, v, w = model.velocities
+    model = HydrostaticFreeSurfaceModel(; grid,
+                                        tracer_advection,
+                                        momentum_advection,
+                                        coriolis = FPlane(f=0.1),
+                                        tracers = :b,
+                                        buoyancy = BuoyancyTracer())
 
-heatmap!(ax_b, interior(b, 1, :, :))
-heatmap!(ax_v, interior(v, 1, :, :))
+    N² = 1
+    bᵢ(x, y, z) = N² * z
+    set!(model, b = bᵢ)
+
+    simulation = Simulation(model; Δt=1e-3, stop_iteration=1000)
+    simulation.callbacks[:p] = Callback(progress, IterationInterval(100))
+
+
+    run!(simulation)
+
+    push!(b, Array(interior(model.tracers.b, 1, :, :)))
+    push!(v, Array(interior(model.velocities.v, 1, :, :)))
+end
+
+b_partial = b[1]
+b_full    = b[2]
+Δb = b_full .- b_partial
+
+v_partial = v[1]
+v_full    = v[2]
+Δv = v_full .- v_partial
+
+fig = Figure(resolution=(800, 1200))
+
+ax_bp = Axis(fig[1, 2])
+ax_bf = Axis(fig[2, 2])
+ax_bd = Axis(fig[3, 2])
+
+# ax_vp = Axis(fig[1, 3])
+# ax_vf = Axis(fig[2, 3])
+# ax_vd = Axis(fig[3, 3])
+
+color = (:black, 0.5)
+linewidth = 3
+levels = 15
+
+hmbp = heatmap!(ax_bp, b_partial)
+contour!(ax_bp, b_partial; levels, color, linewidth)
+Colorbar(fig[1, 1], hmbp)
+
+hmbf = heatmap!(ax_bf, b_full)
+contour!(ax_bf, b_full; levels, color, linewidth)
+Colorbar(fig[2, 1], hmbf)
+
+hmbd = heatmap!(ax_bd, Δb)
+Colorbar(fig[3, 1], hmbd)
+
+#=
+hmvp = heatmap!(ax_vp, v_partial)
+#contour!(ax_vp, v_partial, levels=15)
+#Colorbar(fig[1, 1], hmvp)
+
+hmvf = heatmap!(ax_vf, v_full)
+#contour!(ax_vf, v_full, levels=15)
+#Colorbar(fig[2, 1], hmvf)
+
+hmvd = heatmap!(ax_vd, Δv)
+#Colorbar(fig[3, 1], hmvd)
+=#
 
 display(fig)
 
