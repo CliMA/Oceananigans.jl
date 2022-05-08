c1778398c72c44deabad4981179d749d7231e4f9
Christopher Hill <christophernhill@gmail.com>
2022-05-08 23:43:13 +0000

    Add failing set for debugging

--- test/test_quick_amd.jl
+++ test/test_quick_amd.jl
@@ -1,4 +1,5 @@
 include("dependencies_for_runtests.jl")
+include("dependencies_for_poisson_solvers.jl")
 using GPUArrays
 
 #####
@@ -13,8 +14,8 @@ using GPUArrays
     @info "Testing AMD basics..."
 
     for arch in archs
-        @testset "Quick AMD instantiation [$(typeof(arch))]" begin
-            @info "  Quick AMD instantiation [$(typeof(arch))]..."
+        @testset "Quick AMD set field [$(typeof(arch))]" begin
+            @info "  Quick AMD set field from func [$(typeof(arch))]..."
             N = 3
             topo = (Bounded, Bounded, Bounded)
             FT=Float64
@@ -31,6 +32,28 @@ using GPUArrays
             ans_c=@allowscalar u .- 0
 
             @test ans ≈ ans_c
+
+            @info "  Quick AMD set field from array [$(typeof(arch))]..."
+            N = 3
+            topo = (Bounded, Bounded, Bounded)
+            FT=Float64
+            grid = RectilinearGrid(arch, FT, topology=topo, size=(N, N, N), x=(-1, 1), y=(0, 2π), z=(-1, 1))
+            Nx, Ny, Nz = size(grid)
+            f = rand(Nx, Ny, Nz)
+            default_bcs = FieldBoundaryConditions()
+            u_bcs = regularize_field_boundary_conditions(default_bcs, grid, :u)
+            u = CenterField(grid,boundary_conditions=u_bcs)
+            set!(u, f )
+            ans=@allowscalar u .- 0
+
+            # Check against CPU
+            grid = RectilinearGrid(CPU(), FT, topology=topo, size=(N, N, N), x=(-1, 1), y=(0, 2π), z=(-1, 1))
+            u = XFaceField(grid)
+            set!(u, f )
+            ans_c=@allowscalar u .- 0
+
+            @test ans ≈ ans_c
+
         end
     end
 end
