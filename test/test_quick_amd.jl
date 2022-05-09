e4340a7f2ef10391b46e4b71bf3f74ffbd4bd945
Christopher Hill <christophernhill@gmail.com>
2022-05-09 00:31:49 +0000

    More set! debugging

--- test/test_quick_amd.jl
+++ test/test_quick_amd.jl
@@ -15,6 +15,7 @@ using GPUArrays
 
     for arch in archs
         @testset "Quick AMD set field [$(typeof(arch))]" begin
+
             @info "  Quick AMD set field from func [$(typeof(arch))]..."
             N = 3
             topo = (Bounded, Bounded, Bounded)
@@ -24,16 +25,35 @@ using GPUArrays
             u = XFaceField(grid)
             set!(u, f )
             ans=@allowscalar u .- 0
-
             # Check against CPU
             grid = RectilinearGrid(CPU(), FT, topology=topo, size=(N, N, N), x=(-1, 1), y=(0, 2π), z=(-1, 1))
             u = XFaceField(grid)
             set!(u, f )
             ans_c=@allowscalar u .- 0
-
             @test ans ≈ ans_c
 
-            @info "  Quick AMD set field from array [$(typeof(arch))]..."
+            @info "  Quick AMD set field from GPU array [$(typeof(arch))]..."
+            N = 3
+            topo = (Bounded, Bounded, Bounded)
+            FT=Float64
+            grid = RectilinearGrid(arch, FT, topology=topo, size=(N, N, N), x=(-1, 1), y=(0, 2π), z=(-1, 1))
+            Nx, Ny, Nz = size(grid)
+            f = rand(Nx, Ny, Nz)
+            f_d = ROCArray(f)
+            default_bcs = FieldBoundaryConditions()
+            u_bcs = regularize_field_boundary_conditions(default_bcs, grid, :u)
+            u = CenterField(grid,boundary_conditions=u_bcs)
+            set!(u, f_d )
+            ans=@allowscalar u .- 0
+            # Check against CPU
+            grid = RectilinearGrid(CPU(), FT, topology=topo, size=(N, N, N), x=(-1, 1), y=(0, 2π), z=(-1, 1))
+            Nx, Ny, Nz = size(grid)
+            u = CenterField(grid,boundary_conditions=u_bcs)
+            set!(u, f )
+            ans_c=@allowscalar u .- 0
+            @test ans ≈ ans_c
+
+            @info "  Quick AMD set field from CPU array [$(typeof(arch))]..."
             N = 3
             topo = (Bounded, Bounded, Bounded)
             FT=Float64
@@ -45,10 +65,11 @@ using GPUArrays
             u = CenterField(grid,boundary_conditions=u_bcs)
             set!(u, f )
             ans=@allowscalar u .- 0
-
             # Check against CPU
             grid = RectilinearGrid(CPU(), FT, topology=topo, size=(N, N, N), x=(-1, 1), y=(0, 2π), z=(-1, 1))
-            u = XFaceField(grid)
+            default_bcs = FieldBoundaryConditions()
+            u_bcs = regularize_field_boundary_conditions(default_bcs, grid, :u)
+            u = CenterField(grid,boundary_conditions=u_bcs)
             set!(u, f )
             ans_c=@allowscalar u .- 0
 
