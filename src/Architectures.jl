f1c0b8ca110ee1b0870aae32cea9541e6d9ccc56
simone-silvestri <silvestri.simone0@gmail.com>
2022-05-11 11:14:38 -0400

    removed type passing

--- src/Architectures.jl
+++ src/Architectures.jl
@@ -124,14 +124,11 @@ function unified_array(::GPU, arr::AbstractArray)
 end
 
 ## Only for contiguous data!! (i.e. the offset is always 1)
-@inline function device_copy_to!(dst::CuArray{T}, src::CuArray{T}; async::Bool = false) where T
+@inline function device_copy_to!(dst::CuArray, src::CuArray; async::Bool = false) 
     n = length(src)
     context!(context(src)) do
         GC.@preserve src dst begin
             unsafe_copyto!(pointer(dst, 1), pointer(src, 1), n; async)
-            if Base.isbitsunion(T)
-                unsafe_copyto!(typetagdata(dst, 1), typetagdata(src, 1), n; async)
-            end
         end
     end
     return dst
