320a8c55184adac8a5cb6bf11c381db6ec43c629
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-05-14 10:16:14 -0700

    simpler

--- examples/two_dimensional_turbulence.jl
+++ examples/two_dimensional_turbulence.jl
@@ -151,15 +151,17 @@ s = @lift(Array(file["timeseries/s/"   * string($iter)][:, :, 1]))
 
 x, y, z = nodes((Center, Center, Center), grid)
 
+kwargs = (extendlow = :auto, extendhigh = :auto)
+
 ω_lim = 2.0
 ω_levels = range(-ω_lim, stop=ω_lim, length=20)
+
+contourf!(ax_ω, x, y, ω; levels = ω_levels, colormap=:balance, colorrange=(-ω_lim, ω_lim), kwargs...)
 
 s_lim = 0.2
 s_levels = range(0, stop=s_lim, length=20)
 
-contourf!(ax_ω, x, y, ω; levels = ω_levels, colormap=:balance, colorrange=(-ω_lim, ω_lim), extendlow = :auto, extendhigh = :auto)
-
-contourf!(ax_s, x, y, s; levels = s_levels, colormap=:speed, colorrange=(0, s_lim), extendlow = :auto, extendhigh = :auto)
+contourf!(ax_s, x, y, s; levels = s_levels, colormap=:speed, colorrange=(0, s_lim), kwargs...)
 
 iterations = parse.(Int, keys(file["timeseries/t"]))
 
