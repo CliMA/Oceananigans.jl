878c015dfefcf12740807b192f92adc620b0e403
Simone Silvestri <silvestri.simone0@gmail.com>
2022-05-16 20:48:47 -0400

    added z-direction

--- src/Advection/positivity_preserving_tracer_advection_operators.jl
+++ src/Advection/positivity_preserving_tracer_advection_operators.jl
@@ -11,8 +11,9 @@ const BoundPreservingScheme = PositiveWENO
 
     div_x = bounded_tracer_flux_divergence_x(i, j, k, grid, advection, U.u, c)
     div_y = bounded_tracer_flux_divergence_y(i, j, k, grid, advection, U.v, c)
+    div_z = bounded_tracer_flux_divergence_z(i, j, k, grid, advection, U.w, c)
 
-    return 1/Vᶜᶜᶜ(i, j, k, grid) * (div_x + div_y + δzᵃᵃᶜ(i, j, k, grid, advective_tracer_flux_z, advection, U.w, c))
+    return 1/Vᶜᶜᶜ(i, j, k, grid) * (div_x + div_y + div_z)
 end
 
 @inline function bounded_tracer_flux_divergence_x(i, j, k, grid, advection::BoundPreservingScheme, u, c)
@@ -61,4 +62,28 @@ end
 
     return Ayᶜᶠᶜ(i, j+1, k, grid) * upwind_biased_product(v[i, j+1, k], c₊ᴸ, c₊ᴿ) - 
            Ayᶜᶠᶜ(i, j,   k, grid) * upwind_biased_product(v[i, j,   k], c₋ᴸ, c₋ᴿ)
+end
+
+@inline function bounded_tracer_flux_divergence_z(i, j, k, grid, advection::BoundPreservingScheme, w, c)
+
+    lower_limit = advection.bounds[1]
+    upper_limit = advection.bounds[2]
+
+    cᵢⱼ  = c[i, j, k]
+
+    c₊ᴸ =  _left_biased_interpolate_zᵃᵃᶠ(i, j, k+1, grid, advection, c)
+    c₊ᴿ = _right_biased_interpolate_zᵃᵃᶠ(i, j, k+1, grid, advection, c)
+    c₋ᴸ =  _left_biased_interpolate_zᵃᵃᶠ(i, j, k,   grid, advection, c)
+    c₋ᴿ = _right_biased_interpolate_zᵃᵃᶠ(i, j, k,   grid, advection, c)
+
+    p̃   =  (cᵢⱼ - ω̂₁ * c₋ᴿ - ω̂ₙ * c₊ᴸ) / (1 - 2ω̂₁)
+    M   = max(p̃, c₊ᴸ, c₋ᴿ) 
+    m   = min(p̃, c₊ᴸ, c₋ᴿ) 
+    θ   = min(abs((upper_limit - cᵢⱼ)/(M - cᵢⱼ + ε₂)), abs((lower_limit - cᵢⱼ)/(m - cᵢⱼ + ε₂)), 1.0)
+
+    c₊ᴸ = θ * (c₊ᴸ - cᵢⱼ) + cᵢⱼ
+    c₋ᴿ = θ * (c₋ᴿ - cᵢⱼ) + cᵢⱼ
+
+    return Azᶜᶜᶠ(i, j, k+1, grid) * upwind_biased_product(w[i, j, k+1], c₊ᴸ, c₊ᴿ) - 
+           Azᶜᶜᶠ(i, j, k,   grid) * upwind_biased_product(w[i, j, k],   c₋ᴸ, c₋ᴿ)
 end
