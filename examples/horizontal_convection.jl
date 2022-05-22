eeaabe0152725a220ff783cb8c5af90ef8813e03
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-05-23 06:23:52 +1000

    hide current_figure()

--- examples/horizontal_convection.jl
+++ examples/horizontal_convection.jl
@@ -341,4 +341,4 @@ lines!(ax_KE, t, kinetic_energy; linewidth = 3)
 ax_Nu = Axis(fig[2, 1], xlabel = "time", ylabel = "Nu")
 lines!(ax_Nu, t, Nu; linewidth = 3)
 
-current_figure()
+current_figure() # hide
