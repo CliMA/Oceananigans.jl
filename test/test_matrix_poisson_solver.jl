3f0fc6f67d92e91a6495a2efd63067e5e27b19ea
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2022-05-23 14:24:16 -0400

    Update test_matrix_poisson_solver.jl

--- test/test_matrix_poisson_solver.jl
+++ test/test_matrix_poisson_solver.jl
@@ -32,7 +32,7 @@ function run_identity_operator_test(grid)
 
     b = arch_array(architecture(grid), rand(M))
 
-    storage = arch_array(arch, zeros(size(b))
+    storage = arch_array(arch, zeros(size(b)))
     solve!(storage, solver, b, 1.0)
 
     @test norm(Array(storage) .- Array(b)) .< solver.tolerance
@@ -89,7 +89,7 @@ function run_poisson_equation_test(grid)
     # Solve Poisson equation
     ϕ_solution = CenterField(grid)
 
-    storage = arch_array(arch, zeros(size(rhs))
+    storage = arch_array(arch, zeros(size(rhs)))
     solve!(storage, solver, rhs, 1.0)
     set!(ϕ_solution, reshape(storage, solver.problem_size...))
     fill_halo_regions!(ϕ_solution) 
