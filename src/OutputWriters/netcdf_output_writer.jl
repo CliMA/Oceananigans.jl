4b1e29af546cf219ac6ba87bf294d124750d9dcc
tomchor <tomaschor@gmail.com>
2022-05-24 17:52:52 -0700

    only return length 1 if that's not the default

--- src/OutputWriters/netcdf_output_writer.jl
+++ src/OutputWriters/netcdf_output_writer.jl
@@ -28,21 +28,21 @@ dictify(outputs::NamedTuple) = Dict(string(k) => dictify(v) for (k, v) in zip(ke
 
 Base.length(::Colon) = Inf
 
-xdim(::Type{Face}, inds) = length(inds) == 1 ? () : ("xF",)
-ydim(::Type{Face}, inds) = length(inds) == 1 ? () : ("yF",)
-zdim(::Type{Face}, inds) = length(inds) == 1 ? () : ("zF",)
+xdim(::Type{Face}, inds, user_inds) = (length(inds) == 1) & (length(user_inds) != 1) ? () : ("xF",)
+ydim(::Type{Face}, inds, user_inds) = (length(inds) == 1) & (length(user_inds) != 1) ? () : ("yF",)
+zdim(::Type{Face}, inds, user_inds) = (length(inds) == 1) & (length(user_inds) != 1) ? () : ("zF",)
 
-xdim(::Type{Center}, inds) = length(inds) == 1 ? () : ("xC",)
-ydim(::Type{Center}, inds) = length(inds) == 1 ? () : ("yC",)
-zdim(::Type{Center}, inds) = length(inds) == 1 ? () : ("zC",)
+xdim(::Type{Center}, inds, user_inds) = (length(inds) == 1) & (length(user_inds) != 1) ? () : ("xC",)
+ydim(::Type{Center}, inds, user_inds) = (length(inds) == 1) & (length(user_inds) != 1) ? () : ("yC",)
+zdim(::Type{Center}, inds, user_inds) = (length(inds) == 1) & (length(user_inds) != 1) ? () : ("zC",)
 
-xdim(::Type{Nothing}, inds) = ()
-ydim(::Type{Nothing}, inds) = ()
-zdim(::Type{Nothing}, inds) = ()
+xdim(::Type{Nothing}, inds, user_inds) = ()
+ydim(::Type{Nothing}, inds, user_inds) = ()
+zdim(::Type{Nothing}, inds, user_inds) = ()
 
-function netcdf_spatial_dimensions(afield::AbstractField{LX, LY, LZ}) where {LX, LY, LZ}
+function netcdf_spatial_dimensions(afield::AbstractField{LX, LY, LZ}, user_def_indices) where {LX, LY, LZ}
     indices = afield.indices
-    tuple(xdim(LX, indices[1])..., ydim(LY, indices[2])..., zdim(LZ, indices[3])...)
+    tuple(xdim(LX, indices[1], user_def_indices[1])..., ydim(LY, indices[2], user_def_indices[2])..., zdim(LZ, indices[3], user_def_indices[3])...)
 end
 
 
@@ -377,7 +377,7 @@ function NetCDFOutputWriter(model, outputs; filename, schedule,
 
         for (name, output) in outputs
             attributes = try output_attributes[name]; catch; Dict(); end
-            define_output_variable!(dataset, output, name, array_type, compression, attributes, dimensions)
+            define_output_variable!(dataset, output, name, array_type, compression, attributes, dimensions, indices)
         end
 
         sync(dataset)
@@ -393,7 +393,7 @@ end
 #####
 
 """ Defines empty variables for 'custom' user-supplied `output`. """
-function define_output_variable!(dataset, output, name, array_type, compression, output_attributes, dimensions)
+function define_output_variable!(dataset, output, name, array_type, compression, output_attributes, dimensions, user_def_indices)
     name ∉ keys(dimensions) && error("Custom output $name needs dimensions!")
 
     defVar(dataset, name, eltype(array_type), (dimensions[name]..., "time"),
@@ -404,9 +404,9 @@ end
 
 
 """ Defines empty field variable. """
-function define_output_variable!(dataset, output::AbstractField, name, array_type, compression, output_attributes, dimensions)
+function define_output_variable!(dataset, output::AbstractField, name, array_type, compression, output_attributes, dimensions, user_def_indices)
     defVar(dataset, name, eltype(array_type),
-           (netcdf_spatial_dimensions(output)..., "time"),
+           (netcdf_spatial_dimensions(output, user_def_indices)..., "time"),
            compression=compression, attrib=output_attributes)
 end
 
@@ -521,7 +521,7 @@ end
 #####
 
 """ Defines empty variable for particle trackting. """
-function define_output_variable!(dataset, output::LagrangianParticles, name, array_type, compression, output_attributes, dimensions)
+function define_output_variable!(dataset, output::LagrangianParticles, name, array_type, compression, output_attributes, dimensions, user_def_indices)
     particle_fields = eltype(output.properties) |> fieldnames .|> string
     for particle_field in particle_fields
         defVar(dataset, particle_field, eltype(array_type),
