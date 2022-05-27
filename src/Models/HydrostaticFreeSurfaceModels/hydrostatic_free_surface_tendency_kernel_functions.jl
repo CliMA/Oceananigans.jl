c5d5dd71b3ae12b199e342b2705d251bee90a330
simone-silvestri <silvestri.simone0@gmail.com>
2022-05-26 21:10:19 -0400

    bugfix

--- src/Models/HydrostaticFreeSurfaceModels/hydrostatic_free_surface_tendency_kernel_functions.jl
+++ src/Models/HydrostaticFreeSurfaceModels/hydrostatic_free_surface_tendency_kernel_functions.jl
@@ -153,7 +153,7 @@ end
     @inbounds e = tracers[tracer_index]
 
     return ( - div_Uc(i, j, k, grid, advection, velocities, e)
-             - ∇_dot_qᶜ(i, j, k, grid, closure, diffusivities, val_tracer_index, clock, tracers, model_fields, buoyancy)
+             - ∇_dot_qᶜ(i, j, k, grid, closure, diffusivities, val_tracer_index, tracers, clock, model_fields, buoyancy)
              - immersed_∇_dot_qᶜ(i, j, k, grid, e, e_immersed_bc, closure, diffusivities, val_tracer_index, clock, tracers, model_fields)
              + shear_production(i, j, k, grid, closure, velocities, diffusivities)
              + buoyancy_flux(i, j, k, grid, closure, velocities, tracers, buoyancy, diffusivities)
