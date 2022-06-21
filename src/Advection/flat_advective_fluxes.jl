ee500d8fd81cc5f63abb1438edc10b0dd2f13dab
Simone Silvestri <silvestri.simone0@gmail.com>
2022-06-21 09:12:54 -0400

    hotfix

--- src/Advection/flat_advective_fluxes.jl
+++ src/Advection/flat_advective_fluxes.jl
@@ -1,26 +1,27 @@
+
 const CenteredOrUpwind = Union{AbstractCenteredAdvectionScheme, AbstractUpwindBiasedAdvectionScheme}
 
 #####
 ##### Flat Topologies
 #####
 
-@inline advective_momentum_flux_Uu(i, j, k, grid::AbstractGrid{FT, Flat}, scheme::CenteredorUpwind, U, u) where FT = zero(grid)
-@inline advective_momentum_flux_Vu(i, j, k, grid::AbstractGrid{FT, Flat}, scheme::CenteredorUpwind, V, u) where FT = zero(grid)
-@inline advective_momentum_flux_Wu(i, j, k, grid::AbstractGrid{FT, Flat}, scheme::CenteredorUpwind, W, u) where FT = zero(grid)
+@inline advective_momentum_flux_Uu(i, j, k, grid::AbstractGrid{FT, Flat}, scheme::CenteredOrUpwind, U, u) where FT = zero(grid)
+@inline advective_momentum_flux_Vu(i, j, k, grid::AbstractGrid{FT, Flat}, scheme::CenteredOrUpwind, V, u) where FT = zero(grid)
+@inline advective_momentum_flux_Wu(i, j, k, grid::AbstractGrid{FT, Flat}, scheme::CenteredOrUpwind, W, u) where FT = zero(grid)
 
 @inline advective_momentum_flux_Uv(i, j, k, grid::AbstractGrid{FT, TX, Flat}, scheme::CenteredOrUpwind, U, v) where {FT, TX} = zero(grid)
 @inline advective_momentum_flux_Vv(i, j, k, grid::AbstractGrid{FT, TX, Flat}, scheme::CenteredOrUpwind, V, v) where {FT, TX} = zero(grid)
 @inline advective_momentum_flux_Wv(i, j, k, grid::AbstractGrid{FT, TX, Flat}, scheme::CenteredOrUpwind, W, v) where {FT, TX} = zero(grid)
 
-@inline advective_momentum_flux_Uw(i, j, k, grid::AbstractGrid{FT, TX, TY, Flat}, scheme::CenteredorUpwind, U, w) where {FT, TX, TY} = zero(grid)
-@inline advective_momentum_flux_Vw(i, j, k, grid::AbstractGrid{FT, TX, TY, Flat}, scheme::CenteredorUpwind, V, w) where {FT, TX, TY} = zero(grid)
-@inline advective_momentum_flux_Ww(i, j, k, grid::AbstractGrid{FT, TX, TY, Flat}, scheme::CenteredorUpwind, W, w) where {FT, TX, TY} = zero(grid)
+@inline advective_momentum_flux_Uw(i, j, k, grid::AbstractGrid{FT, TX, TY, Flat}, scheme::CenteredOrUpwind, U, w) where {FT, TX, TY} = zero(grid)
+@inline advective_momentum_flux_Vw(i, j, k, grid::AbstractGrid{FT, TX, TY, Flat}, scheme::CenteredOrUpwind, V, w) where {FT, TX, TY} = zero(grid)
+@inline advective_momentum_flux_Ww(i, j, k, grid::AbstractGrid{FT, TX, TY, Flat}, scheme::CenteredOrUpwind, W, w) where {FT, TX, TY} = zero(grid)
 
 @inline advective_momentum_flux_Uv(i, j, k, grid::AbstractGrid{FT, Flat}, scheme::CenteredOrUpwind, U, v) where {FT} = zero(grid)
 @inline advective_momentum_flux_Uw(i, j, k, grid::AbstractGrid{FT, Flat}, scheme::CenteredorUpwind, U, w) where {FT} = zero(grid)
 
-@inline advective_momentum_flux_Vu(i, j, k, grid::AbstractGrid{FT, TX, Flat}, scheme::CenteredorUpwind, V, u) where {FT, TX} = zero(grid)
-@inline advective_momentum_flux_Vw(i, j, k, grid::AbstractGrid{FT, TX, Flat}, scheme::CenteredorUpwind, V, w) where {FT, TX} = zero(grid)
+@inline advective_momentum_flux_Vu(i, j, k, grid::AbstractGrid{FT, TX, Flat}, scheme::CenteredOrUpwind, V, u) where {FT, TX} = zero(grid)
+@inline advective_momentum_flux_Vw(i, j, k, grid::AbstractGrid{FT, TX, Flat}, scheme::CenteredOrUpwind, V, w) where {FT, TX} = zero(grid)
 
-@inline advective_momentum_flux_Wu(i, j, k, grid::AbstractGrid{FT, TX, TY, Flat}, scheme::CenteredorUpwind, W, u) where {FT, TX, TY} = zero(grid)
+@inline advective_momentum_flux_Wu(i, j, k, grid::AbstractGrid{FT, TX, TY, Flat}, scheme::CenteredOrUpwind, W, u) where {FT, TX, TY} = zero(grid)
 @inline advective_momentum_flux_Wv(i, j, k, grid::AbstractGrid{FT, TX, TY, Flat}, scheme::CenteredOrUpwind, W, v) where {FT, TX, TY} = zero(grid)
