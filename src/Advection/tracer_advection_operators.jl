0689f91481735afa79755794dee49b0f0f2703a3
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-06-29 14:29:04 +1000

    docstring cleanup

--- src/Advection/tracer_advection_operators.jl
+++ src/Advection/tracer_advection_operators.jl
@@ -21,7 +21,7 @@ const ZeroU = NamedTuple{(:u, :v, :w), Tuple{ZeroField, ZeroField, ZeroField}}
 """
     div_uc(i, j, k, grid, advection, U, c)
 
-Calculates the divergence of the flux of a tracer quantity ``c`` being advected by
+Calculate the divergence of the flux of a tracer quantity ``c`` being advected by
 a velocity field, ``𝛁⋅(𝐯 c)``,
 
     1/V * [δxᶜᵃᵃ(Ax * u * ℑxᶠᵃᵃ(c)) + δyᵃᶜᵃ(Ay * v * ℑyᵃᶠᵃ(c)) + δzᵃᵃᶜ(Az * w * ℑzᵃᵃᶠ(c))]
