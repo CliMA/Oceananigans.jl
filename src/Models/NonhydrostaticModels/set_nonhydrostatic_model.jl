d9b6790f05ca48d7c2ec047a6eeb86a5fe12441a
tomchor <tomaschor@gmail.com>
2022-07-31 20:19:19 -0700

    lets try not masking velocities

--- src/Models/NonhydrostaticModels/set_nonhydrostatic_model.jl
+++ src/Models/NonhydrostaticModels/set_nonhydrostatic_model.jl
@@ -43,8 +43,8 @@ function set!(model::NonhydrostaticModel; enforce_incompressibility=true, kwargs
 
     # Apply a mask
     #tracer_masking_events = Tuple(mask_immersed_field!(c) for c in model.tracers)
-    velocity_masking_events = mask_immersed_velocities!(model.velocities, model.architecture, model.grid)
-    wait(device(model.architecture), MultiEvent(tuple(velocity_masking_events...)))
+    #velocity_masking_events = mask_immersed_velocities!(model.velocities, model.architecture, model.grid)
+    #wait(device(model.architecture), MultiEvent(tuple(velocity_masking_events...)))
 
     update_state!(model)
 
