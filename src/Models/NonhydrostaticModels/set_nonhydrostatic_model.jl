7561c2d5e99eebb58b9ef4da34621dcf0e19fb75
tomchor <tomaschor@gmail.com>
2022-08-17 10:38:43 -0700

    cleaned up old code

--- src/Models/NonhydrostaticModels/set_nonhydrostatic_model.jl
+++ src/Models/NonhydrostaticModels/set_nonhydrostatic_model.jl
@@ -41,11 +41,6 @@ function set!(model::NonhydrostaticModel; enforce_incompressibility=true, kwargs
         set!(ϕ, value)
     end
 
-    # Apply a mask
-    #tracer_masking_events = Tuple(mask_immersed_field!(c) for c in model.tracers)
-    #velocity_masking_events = mask_immersed_velocities!(model.velocities, model.architecture, model.grid)
-    #wait(device(model.architecture), MultiEvent(tuple(velocity_masking_events...)))
-
     update_state!(model)
 
     if enforce_incompressibility
