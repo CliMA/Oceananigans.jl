cc749026c98e098f5df86ae1a540b2a22c5189b2
tomchor <tomaschor@gmail.com>
2022-08-22 18:59:34 -0700

    remove masking from some hydrostatic free surface routines

--- src/Models/HydrostaticFreeSurfaceModels/update_hydrostatic_free_surface_model_state.jl
+++ src/Models/HydrostaticFreeSurfaceModels/update_hydrostatic_free_surface_model_state.jl
@@ -2,7 +2,6 @@ using Oceananigans.Architectures
 using Oceananigans.Architectures: device_event
 using Oceananigans.BoundaryConditions
 using Oceananigans.TurbulenceClosures: calculate_diffusivities!
-using Oceananigans.ImmersedBoundaries: mask_immersed_field!, mask_immersed_reduced_field_xy!
 using Oceananigans.Models.NonhydrostaticModels: update_hydrostatic_pressure!
 
 import Oceananigans.TimeSteppers: update_state!
@@ -19,8 +18,6 @@ update_state!(model::HydrostaticFreeSurfaceModel) = update_state!(model, model.g
 
 function update_state!(model::HydrostaticFreeSurfaceModel, grid)
 
-    @apply_regionally masking_actions!(model)
-
     fill_halo_regions!(prognostic_fields(model), model.clock, fields(model))
     fill_horizontal_velocity_halos!(model.velocities.u, model.velocities.v, model.architecture)
 
@@ -31,15 +28,6 @@ function update_state!(model::HydrostaticFreeSurfaceModel, grid)
     fill_halo_regions!(model.pressure.pHY′)
     
     return nothing
-end
-
-# Mask immersed fields
-function masking_actions!(model)
-    η = displacement(model.free_surface)
-    masking_events = Any[mask_immersed_field!(field)
-                         for field in merge(model.auxiliary_fields, prognostic_fields(model)) if field !== η]
-    push!(masking_events, mask_immersed_reduced_field_xy!(η, k=size(model.grid, 3)))    
-    wait(device(model.architecture), MultiEvent(Tuple(masking_events)))
 end
 
 function update_state_actions!(model) 
