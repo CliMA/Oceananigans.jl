699d323c93e0b6a77f326c69f75a6c699002b0ff
matinraayai <raayaiardani.m@northeastern.edu>
2022-08-29 17:33:14 -0400

    Rebased to master + ported some new features.

--- src/MultiRegion/multi_region_utils.jl
+++ src/MultiRegion/multi_region_utils.jl
@@ -3,20 +3,35 @@ validate_devices(partition, ::CPU, devices) = nothing
 validate_devices(p, ::CPU, ::Nothing) = nothing
 
 # If no device is specified on the GPU, use only the default device
-validate_devices(p, ::GPU, ::Nothing) = 1
+validate_devices(p, ::CUDAGPU, ::Nothing) = 1
+validate_devices(p, ::ROCMGPU, ::Nothing) = 1
 
-function validate_devices(partition, ::GPU, devices)
+function validate_devices(partition, ::CUDAGPU, devices)
     @assert length(unique(devices)) <= length(CUDA.devices())
     @assert maximum(devices) <= length(CUDA.devices())
     @assert length(devices) <= length(partition)
     return devices
 end
 
-function validate_devices(partition, ::GPU, devices::Number)
+function validate_devices(partition, ::ROCMGPU, devices)
+    @assert length(unique(devices)) <= length(AMDGPU.get_agents(:gpu))
+    @assert maximum(devices) <= length(AMDGPU.get_agents(:gpu))
+    @assert length(devices) <= length(partition)
+    return devices
+end
+
+function validate_devices(partition, ::CUDAGPU, devices::Number)
     @assert devices <= length(CUDA.devices())
     @assert devices <= length(partition)
     return devices
 end
+
+function validate_devices(partition, ::ROCMGPU, devices::Number)
+    @assert devices <= length(AMDGPU.get_agents(:gpu))
+    @assert devices <= length(partition)
+    return devices
+end
+
 assign_devices(p, ::Nothing) = Tuple(CPU() for i in 1:length(p))
 
 function assign_devices(p::AbstractPartition, dev::Number) 
@@ -25,13 +40,15 @@ function assign_devices(p::AbstractPartition, dev::Number)
     leftover = mod(part, dev)
     devices  = []
 
+    PROVIDER = AMDGPU.has_rocm_gpu() ? AMDGPU : CUDA
+
     for i in 1:dev
-        CUDA.device!(i-1)
+        PROVIDER.device!(i-1)
         for j in 1:repeat
-            push!(devices, CUDA.device())
+            push!(devices, PROVIDER.device())
         end
         if i <= leftover
-            push!(devices, CUDA.device())
+            push!(devices, PROVIDER.device())
         end
     end
     return Tuple(devices)
@@ -43,13 +60,15 @@ function assign_devices(p::AbstractPartition, dev::Tuple)
     leftover = mod(part, length(dev))
     devices  = []
 
+    PROVIDER = AMDGPU.has_rocm_gpu() ? AMDGPU : CUDA
+
     for i in 1:length(dev)
-        CUDA.device!(dev[i])
+        PROVIDER.device!(dev[i])
         for j in 1:repeat
-            push!(devices, CUDA.device())
+            push!(devices, PROVIDER.device())
         end
         if i <= leftover
-            push!(devices, CUDA.device())
+            push!(devices, PROVIDER.device())
         end
     end
     return Tuple(devices)
