95c531cf7e524d952a493d5422ba279a735f49a4
Gregory Wagner <wagner.greg@gmail.com>
2022-09-01 16:40:18 -0600

    Update

--- src/TurbulenceClosures/turbulence_closure_implementations/CATKEVerticalDiffusivities/turbulent_kinetic_energy_equation.jl
+++ src/TurbulenceClosures/turbulence_closure_implementations/CATKEVerticalDiffusivities/turbulent_kinetic_energy_equation.jl
@@ -59,6 +59,7 @@ end
     κᶻ = ℑzᵃᵃᶜ(i, j, k, grid, diffusivities.Kᶜ)
     N² = ℑzᵃᵃᶜ(i, j, k, grid, ∂z_b, buoyancy, tracers)
     eⁱʲᵏ = @inbounds tracers.e[i, j, k]
+
     # "Patankar trick" for buoyancy production (cf Patankar 1980 or Burchard et al. 2003)
     # If buoyancy flux is a _sink_ of TKE, we treat it implicitly.
     return ifelse((N² > 0) & (eⁱʲᵏ > 0), zero(grid), - κᶻ * N²)
