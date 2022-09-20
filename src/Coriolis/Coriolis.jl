d7b30fcb5ad57269e68d9d2bb66362afb4f29636
simone-silvestri <silvestri.simone0@gmail.com>
2022-09-20 17:36:39 -0400

    export correct object

--- src/Coriolis/Coriolis.jl
+++ src/Coriolis/Coriolis.jl
@@ -2,7 +2,7 @@ module Coriolis
 
 export
     FPlane, ConstantCartesianCoriolis, BetaPlane, NonTraditionalBetaPlane,
-    HydrostaticSphericalCoriolis, WetPointEnstrophyConservingScheme,
+    HydrostaticSphericalCoriolis, WetCellEnstrophyConservingScheme,
     x_f_cross_U, y_f_cross_U, z_f_cross_U
 
 using Printf
