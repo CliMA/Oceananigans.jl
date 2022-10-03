4ec70347bd4267a62d9262423191288558c4c06b
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-10-03 16:33:10 +0300

    add example with field with indices

--- src/Fields/field.jl
+++ src/Fields/field.jl
@@ -102,23 +102,56 @@ location in `(x, y, z)` respectively.
 Keyword arguments
 =================
 
-- `data :: OffsetArray`: An offset array with the fields data. If nothing is providet the
+- `data :: OffsetArray`: An offset array with the fields data. If nothing is provided the
   field is filled with zeros.
-- `boundary_conditions`: If nothing is provided, then field is created using the default
+  - `boundary_conditions`: If nothing is provided, then field is created using the default
   boundary conditions via [`FieldBoundaryConditions`](@ref).
+- `indices`: Used to prescribe where a reduced field lives on. For example, at which `k` index
+  does a two-dimensional ``x``-``y`` field lives on. Default: `(:, :, :)`.
 
 Example
 =======
 
-```jldoctest
+A field at location `(Face, Face, Center)`.
+
+```jldoctest fields
 julia> using Oceananigans
 
-julia> ω = Field{Face, Face, Center}(RectilinearGrid(size=(1, 1, 1), extent=(1, 1, 1)))
-1×1×1 Field{Face, Face, Center} on RectilinearGrid on CPU
-├── grid: 1×1×1 RectilinearGrid{Float64, Periodic, Periodic, Bounded} on CPU with 3×3×3 halo
+julia> grid = RectilinearGrid(size=(2, 3, 4), extent=(1, 1, 1))
+
+julia> ω = Field{Face, Face, Center}(grid)
+2×3×4 Field{Face, Face, Center} on RectilinearGrid on CPU
+├── grid: 2×3×4 RectilinearGrid{Float64, Periodic, Periodic, Bounded} on CPU with 3×3×3 halo
 ├── boundary conditions: FieldBoundaryConditions
 │   └── west: Periodic, east: Periodic, south: Periodic, north: Periodic, bottom: ZeroFlux, top: ZeroFlux, immersed: ZeroFlux
-└── data: 7×7×7 OffsetArray(::Array{Float64, 3}, -2:4, -2:4, -2:4) with eltype Float64 with indices -2:4×-2:4×-2:4
+└── data: 8×9×10 OffsetArray(::Array{Float64, 3}, -2:5, -2:6, -2:7) with eltype Float64 with indices -2:5×-2:6×-2:7
+    └── max=0.0, min=0.0, mean=0.0
+```
+
+Using `indices` to create a two dimensional ``x``-``y`` field at location `(Face, Face, Center)`
+that lives at the grid's surface, `k = Nz`.
+
+```jldoctest fields
+julia> u = XFaceField(grid); v = YFaceField(grid);
+
+julia> ωₛ = Field(∂x(v) - ∂y(u), indices=(:, :, grid.Nz))
+2×3×1 Field{Face, Face, Center} on RectilinearGrid on CPU
+├── grid: 2×3×4 RectilinearGrid{Float64, Periodic, Periodic, Bounded} on CPU with 3×3×3 halo
+├── boundary conditions: FieldBoundaryConditions
+│   └── west: Periodic, east: Periodic, south: Periodic, north: Periodic, bottom: Nothing, top: Nothing, immersed: ZeroFlux
+├── operand: BinaryOperation at (Face, Face, Center)
+├── status: time=0.0
+└── data: 8×9×1 OffsetArray(::Array{Float64, 3}, -2:5, -2:6, 4:4) with eltype Float64 with indices -2:5×-2:6×4:4
+    └── max=0.0, min=0.0, mean=0.0
+
+julia> compute!(ωₛ)
+2×3×1 Field{Face, Face, Center} on RectilinearGrid on CPU
+├── grid: 2×3×4 RectilinearGrid{Float64, Periodic, Periodic, Bounded} on CPU with 3×3×3 halo
+├── boundary conditions: FieldBoundaryConditions
+│   └── west: Periodic, east: Periodic, south: Periodic, north: Periodic, bottom: Nothing, top: Nothing, immersed: ZeroFlux
+├── operand: BinaryOperation at (Face, Face, Center)
+├── status: time=0.0
+└── data: 8×9×1 OffsetArray(::Array{Float64, 3}, -2:5, -2:6, 4:4) with eltype Float64 with indices -2:5×-2:6×4:4
     └── max=0.0, min=0.0, mean=0.0
 ```
 """
