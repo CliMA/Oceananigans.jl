2cf09966f1c95f666a1699ae5e88361e9064ef7e
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-10-03 17:34:57 +0300

    Update src/Advection/flat_advective_fluxes.jl
    Co-authored-by: Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>

--- src/Advection/flat_advective_fluxes.jl
+++ src/Advection/flat_advective_fluxes.jl
@@ -18,3 +18,11 @@ for SchemeType in [:CenteredScheme, :UpwindScheme]
         @inline advective_momentum_flux_Ww(i, j, k, grid::AbstractGrid{FT, TX, TY, Flat}, scheme::$SchemeType, W, w) where {FT, TX, TY} = zero(grid)
     end
 end
+
+@inline inner_right_biased_interpolate_xᶠᵃᵃ(i, j, k, grid::XFlatGrid, scheme, ψ, args...) = ψ[i, j, k]
+@inline inner_right_biased_interpolate_yᵃᶠᵃ(i, j, k, grid::YFlatGrid, scheme, ψ, args...) = ψ[i, j, k]
+@inline inner_right_biased_interpolate_zᵃᵃᶠ(i, j, k, grid::ZFlatGrid, scheme, ψ, args...) = ψ[i, j, k]
+
+@inline inner_right_biased_interpolate_xᶠᵃᵃ(i, j, k, grid::XFlatGrid, scheme, ψ::Function, idx, loc, args...) = ψ(i, j, k, grid, args...)
+@inline inner_right_biased_interpolate_yᵃᶠᵃ(i, j, k, grid::YFlatGrid, scheme, ψ::Function, idx, loc, args...) = ψ(i, j, k, grid, args...)
+@inline inner_right_biased_interpolate_zᵃᵃᶠ(i, j, k, grid::ZFlatGrid, scheme, ψ::Function, idx, loc, args...) = ψ(i, j, k, grid, args...)
