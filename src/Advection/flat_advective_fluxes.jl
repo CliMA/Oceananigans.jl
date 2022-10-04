ef49d8453a3cbc162b1d661123c64c53c56f28d1
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-10-04 17:39:45 +0300

    import XFlatGrid, YFlatGrid, ZFlatGrid

--- src/Advection/flat_advective_fluxes.jl
+++ src/Advection/flat_advective_fluxes.jl
@@ -3,25 +3,23 @@
 ##### Flat Topologies
 #####
 
+using Oceananigans.Operators: XFlatGrid, YFlatGrid, ZFlatGrid
+
 for SchemeType in [:CenteredScheme, :UpwindScheme]
     @eval begin
-        @inline advective_momentum_flux_Uu(i, j, k, grid::AbstractGrid{FT, Flat}, scheme::$SchemeType, U, u) where FT = zero(grid)
-        @inline advective_momentum_flux_Uv(i, j, k, grid::AbstractGrid{FT, Flat}, scheme::$SchemeType, U, v) where {FT} = zero(grid)
-        @inline advective_momentum_flux_Uw(i, j, k, grid::AbstractGrid{FT, Flat}, scheme::$SchemeType, U, w) where {FT} = zero(grid)
+        @inline advective_momentum_flux_Uu(i, j, k, grid::XFlatGrid, scheme::$SchemeType, U, u) where FT = zero(grid)
+        @inline advective_momentum_flux_Uv(i, j, k, grid::XFlatGrid, scheme::$SchemeType, U, v) where {FT} = zero(grid)
+        @inline advective_momentum_flux_Uw(i, j, k, grid::XFlatGrid, scheme::$SchemeType, U, w) where {FT} = zero(grid)
 
-        @inline advective_momentum_flux_Vv(i, j, k, grid::AbstractGrid{FT, TX, Flat}, scheme::$SchemeType, V, v) where {FT, TX} = zero(grid)
-        @inline advective_momentum_flux_Vu(i, j, k, grid::AbstractGrid{FT, TX, Flat}, scheme::$SchemeType, V, u) where {FT, TX} = zero(grid)
-        @inline advective_momentum_flux_Vw(i, j, k, grid::AbstractGrid{FT, TX, Flat}, scheme::$SchemeType, V, w) where {FT, TX} = zero(grid)
+        @inline advective_momentum_flux_Vv(i, j, k, grid::YFlatGrid, scheme::$SchemeType, V, v) where {FT, TX} = zero(grid)
+        @inline advective_momentum_flux_Vu(i, j, k, grid::YFlatGrid, scheme::$SchemeType, V, u) where {FT, TX} = zero(grid)
+        @inline advective_momentum_flux_Vw(i, j, k, grid::YFlatGrid, scheme::$SchemeType, V, w) where {FT, TX} = zero(grid)
 
-        @inline advective_momentum_flux_Wu(i, j, k, grid::AbstractGrid{FT, TX, TY, Flat}, scheme::$SchemeType, W, u) where {FT, TX, TY} = zero(grid)
-        @inline advective_momentum_flux_Wv(i, j, k, grid::AbstractGrid{FT, TX, TY, Flat}, scheme::$SchemeType, W, v) where {FT, TX, TY} = zero(grid)
-        @inline advective_momentum_flux_Ww(i, j, k, grid::AbstractGrid{FT, TX, TY, Flat}, scheme::$SchemeType, W, w) where {FT, TX, TY} = zero(grid)
+        @inline advective_momentum_flux_Wu(i, j, k, grid::ZFlatGrid, scheme::$SchemeType, W, u) where {FT, TX, TY} = zero(grid)
+        @inline advective_momentum_flux_Wv(i, j, k, grid::ZFlatGrid, scheme::$SchemeType, W, v) where {FT, TX, TY} = zero(grid)
+        @inline advective_momentum_flux_Ww(i, j, k, grid::ZFlatGrid, scheme::$SchemeType, W, w) where {FT, TX, TY} = zero(grid)
     end
 end
-
-const XFlatGrid = AG{<:Any, Flat}
-const YFlatGrid = AG{<:Any, <:Any, Flat}
-const ZFlatGrid = AG{<:Any, <:Any, <:Any, Flat}
 
 @inline inner_right_biased_interpolate_xᶠᵃᵃ(i, j, k, ::XFlatGrid, scheme, ψ, args...) = @inbounds ψ[i, j, k]
 @inline inner_right_biased_interpolate_yᵃᶠᵃ(i, j, k, ::YFlatGrid, scheme, ψ, args...) = @inbounds ψ[i, j, k]
