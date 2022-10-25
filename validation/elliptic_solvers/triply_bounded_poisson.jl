c634f6c9dd46244408a38b2a71b5cb83d0d21954
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-10-26 09:19:54 +1100

    bench and plot

--- validation/elliptic_solvers/triply_bounded_poisson.jl
+++ validation/elliptic_solvers/triply_bounded_poisson.jl
@@ -12,7 +12,8 @@ using Oceananigans.Solvers: solve!,
 
 using BenchmarkTools,
       CUDA,
-      IterativeSolvers
+      IterativeSolvers,
+      GLMakie
 
 using KernelAbstractions: @kernel, @index, Event
 using Statistics: mean
@@ -33,7 +34,7 @@ arch = CPU()
 
 Nx, Ny, Nz = 24, 24, 12
 
-z_slice = 2
+z_slice = 6
 
 underlying_grid = RectilinearGrid(arch,
                                   size =(Nx, Ny, Nz),
@@ -57,8 +58,9 @@ set!(r, r₀)
 r .-= mean(r)
 fill_halo_regions!(r)
 
-ax2 = Axis(fig[1, 2])
-heatmap!(ax2, x[1:Nx], y[1:Ny], interior(r)[:, :, z_slice])
+fig = Figure()
+ax = Axis(fig[1, 2]; title="right-hand-side")
+heatmap!(ax, x[1:Nx], y[1:Ny], interior(r)[:, :, z_slice])
 current_figure()
 
 @kernel function ∇²!(∇²f, grid, f)
@@ -85,11 +87,10 @@ cg_solver = PreconditionedConjugateGradientSolver(compute_∇²!, template_field
 
 fill_halo_regions!(φ_cg)
 
-ax_cg = Axis(fig[2, 1])
+ax_cg = Axis(fig[2, 1]; title="cg")
 heatmap!(ax_cg, x[1:Nx], y[1:Ny], interior(φ_cg)[:, :, z_slice])
 current_figure()
 
-#=
 function bench_solve()
     parent(φ_cg) .= 0
     solve!(φ_cg, cg_solver, r, arch, grid)
@@ -103,7 +104,6 @@ elseif arch == GPU()
 end
 
 fill_halo_regions!(φ_cg)
-=#
 
 # Solve ∇²φ = r with `AlgebraicMultigrid` solver
 φ_mg = CenterField(grid)
@@ -114,23 +114,26 @@ fill_halo_regions!(φ_cg)
 @info "Solving the Poisson equation with the Algebraic Multigrid solver..."
 @time solve!(φ_mg, mgs, r)
 
-# function bench_solve()
-#     parent(φ_mg) .= 0
-#     solve!(φ_mg, mgs, r)
-#     return nothing
-# end
+function bench_solve()
+    parent(φ_mg) .= 0
+    solve!(φ_mg, mgs, r)
+    return nothing
+end
 
-# if arch == CPU()
-#     bench_mg = @benchmark bench_solve();
-# elseif arch == GPU()
-#     bench_mg = @benchmark CUDA.@sync bench_solve();
-# end
+if arch == CPU()
+    bench_mg = @benchmark bench_solve();
+elseif arch == GPU()
+    bench_mg = @benchmark CUDA.@sync bench_solve();
+end
 
 φ_mg .-= mean(φ_mg)
 
 fill_halo_regions!(φ_mg)
 finalize_solver!(mgs)
 
+ax_mg = Axis(fig[2, 2]; title="mg")
+heatmap!(ax_mg, x[1:Nx], y[1:Ny], interior(φ_mg)[:, :, z_slice])
+current_figure()
 
 # Solve ∇²φ = r with `PreconditionedConjugateGradientSolver` solver using the AlgebraicMultigrid as preconditioner
 
@@ -145,6 +148,7 @@ end
 
 """
     precondition!(z, mgp::MultigridPreconditioner, r, args...)
+
 Return `z` (Field)
 """
 function precondition!(z, mg_solver::MultigridSolver, r, args...)
@@ -155,7 +159,7 @@ function precondition!(z, mg_solver::MultigridSolver, r, args...)
 end
 
 # construct a multigrid solver to use as a preconditioner
-maxiter = 1;
+maxiter = 1
 mgs = MultigridSolver(compute_∇²!, arch, grid; template_field = r, maxiter)
 
 φ_cgmg = CenterField(grid)
@@ -166,18 +170,23 @@ cgmg_solver = PreconditionedConjugateGradientSolver(compute_∇²!, template_fie
 @info "Solving the Poisson equation with a conjugate gradient preconditioned iterative solver w/ AMG as preconditioner..."
 @time solve!(φ_cgmg, cgmg_solver, r, arch, grid)
 
-# function bench_solve()
-#     parent(φ_cgmg) .= 0
-#     solve!(φ_cgmg, cgmg_solver, r, arch, grid)
+function bench_solve()
+    parent(φ_cgmg) .= 0
+    solve!(φ_cgmg, cgmg_solver, r, arch, grid)
 
-#     return nothing
-# end
+    return nothing
+end
 
-# if arch == CPU()
-#     bench_mgpcg = @benchmark bench_solve();
-# elseif arch == GPU()
-#     bench_mgpcg = @benchmark CUDA.@sync bench_solve();
-# end
+if arch == CPU()
+    bench_mgpcg = @benchmark bench_solve();
+elseif arch == GPU()
+    bench_mgpcg = @benchmark CUDA.@sync bench_solve();
+end
 
 fill_halo_regions!(φ_cgmg)
+
+ax_cgmg = Axis(fig[2, 3]; title="cgmg")
+heatmap!(ax_cgmg, x[1:Nx], y[1:Ny], interior(φ_cgmg)[:, :, z_slice])
+current_figure()
+
 finalize_solver!(cgmg_solver)
