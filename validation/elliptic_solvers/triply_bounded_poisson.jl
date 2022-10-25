f3cbbe686316e9b850f2c05ddd56ec7b4650e24d
Elise Palethorpe <ellypaley@gmail.com>
2022-10-25 11:00:26 +1100

    tidying up

--- validation/elliptic_solvers/triply_bounded_poisson.jl
+++ validation/elliptic_solvers/triply_bounded_poisson.jl
@@ -48,20 +48,10 @@ grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(land))
 
 x, y = grid.underlying_grid.xᶜᵃᵃ, grid.underlying_grid.yᵃᶜᵃ
 
-# grid = underlying_grid
-
-# x, y = grid.xᶜᵃᵃ, grid.yᵃᶜᵃ
-
-using GLMakie
-
-fig = Figure()
-ax1 = Axis(fig[1, 1])
-heatmap!(ax1, x[1:Nx], y[1:Ny], grid.immersed_boundary.bottom_height[1:Nx, 1:Ny])
-current_figure()
 
 # Select RHS
 r = CenterField(grid)
-r₀(x, y, z) = (x^2 + y^2) < 1 ? 1 : 0 # exp(-x^2 - y^2 - z^2)
+r₀(x, y, z) = (x^2 + y^2) < 1 ? 1 : 0
 # r₀(x, y, z) = exp(-x^2 - y^2 - z^2)
 set!(r, r₀)
 r .-= mean(r)
@@ -85,31 +75,6 @@ function compute_∇²!(∇²φ, φ, arch, grid)
 
     return nothing
 end
-
-#=
-# Solve ∇²φ = r with `FFTBasedPoissonSolver`
-φ_fft = CenterField(grid)
-fft_solver = FFTBasedPoissonSolver(grid)
-fft_solver.storage .= interior(r)
-
-@info "Solving the Poisson equation with an FFT-based solver..."
-@time solve!(φ_fft, fft_solver, fft_solver.storage)
-
-function bench_solve()
-    parent(φ_fft) .= 0
-    fft_solver.storage .= interior(r)
-    solve!(φ_fft, fft_solver, fft_solver.storage)
-    return nothing
-end
-
-if arch == CPU()
-    bench_fft = @benchmark bench_solve();
-elseif arch == GPU()
-    bench_fft = @benchmark CUDA.@sync bench_solve();
-end
-
-fill_halo_regions!(φ_fft)
-=#
 
 # Solve ∇²φ = r with `PreconditionedConjugateGradientSolver`
 φ_cg = CenterField(grid)
@@ -149,47 +114,23 @@ fill_halo_regions!(φ_cg)
 @info "Solving the Poisson equation with the Algebraic Multigrid solver..."
 @time solve!(φ_mg, mgs, r)
 
-φ_mg .-= mean(φ_mg)
+# function bench_solve()
+#     parent(φ_mg) .= 0
+#     solve!(φ_mg, mgs, r)
+#     return nothing
+# end
 
-fill_halo_regions!(φ_mg)
-finalize_solver!(mgs)
-
-
-∇²φ_mg = CenterField(grid)
-compute_∇²!(∇²φ_mg, φ_mg, arch, grid.underlying_grid)
-
-∇²φ_cg = CenterField(grid)
-compute_∇²!(∇²φ_cg, φ_cg, arch, grid.underlying_grid)
-
-ax_mg = Axis(fig[2, 2])
-heatmap!(ax_mg, x[1:Nx], y[1:Ny], interior(φ_mg)[:, :, z_slice])
-current_figure()
-
-ax_lapcg = Axis(fig[3, 1])
-heatmap!(ax_lapcg, x[1:Nx], y[1:Ny], interior(∇²φ_cg)[:, :, z_slice])
-current_figure()
-
-ax_lapmg = Axis(fig[3, 2])
-heatmap!(ax_lapmg, x[1:Nx], y[1:Ny], interior(∇²φ_mg)[:, :, z_slice])
-current_figure()
-
-#=
-function bench_solve()
-    parent(φ_mg) .= 0
-    solve!(φ_mg, mgs, r)
-    return nothing
-end
-
-if arch == CPU()
-    bench_mg = @benchmark bench_solve();
-elseif arch == GPU()
-    bench_mg = @benchmark CUDA.@sync bench_solve();
-end
+# if arch == CPU()
+#     bench_mg = @benchmark bench_solve();
+# elseif arch == GPU()
+#     bench_mg = @benchmark CUDA.@sync bench_solve();
+# end
 
 φ_mg .-= mean(φ_mg)
 
 fill_halo_regions!(φ_mg)
 finalize_solver!(mgs)
+
 
 # Solve ∇²φ = r with `PreconditionedConjugateGradientSolver` solver using the AlgebraicMultigrid as preconditioner
 
@@ -225,128 +166,18 @@ cgmg_solver = PreconditionedConjugateGradientSolver(compute_∇²!, template_fie
 @info "Solving the Poisson equation with a conjugate gradient preconditioned iterative solver w/ AMG as preconditioner..."
 @time solve!(φ_cgmg, cgmg_solver, r, arch, grid)
 
-function bench_solve()
-    parent(φ_cgmg) .= 0
-    solve!(φ_cgmg, cgmg_solver, r, arch, grid)
+# function bench_solve()
+#     parent(φ_cgmg) .= 0
+#     solve!(φ_cgmg, cgmg_solver, r, arch, grid)
 
-    return nothing
-end
+#     return nothing
+# end
 
-if arch == CPU()
-    bench_mgpcg = @benchmark bench_solve();
-elseif arch == GPU()
-    bench_mgpcg = @benchmark CUDA.@sync bench_solve();
-end
+# if arch == CPU()
+#     bench_mgpcg = @benchmark bench_solve();
+# elseif arch == GPU()
+#     bench_mgpcg = @benchmark CUDA.@sync bench_solve();
+# end
 
 fill_halo_regions!(φ_cgmg)
 finalize_solver!(cgmg_solver)
-
-#=
-using  GLMakie
-∇²φ_fft = CenterField(grid)
-∇²φ_cg = CenterField(grid)
-∇²φ_mg = CenterField(grid)
-∇²φ_cgmg = CenterField(grid)
-
-compute_∇²!(∇²φ_fft, φ_fft, arch, grid)
-compute_∇²!(∇²φ_cg, φ_cg, arch, grid)
-compute_∇²!(∇²φ_mg, φ_mg, arch, grid)
-compute_∇²!(∇²φ_cgmg, φ_cgmg, arch, grid)
-
-# Plot results
-fig = Figure(resolution=(1600, 1200))
-
-ax_r = Axis(fig[1, 3], aspect=1, title="RHS")
-
-ax_φ_fft = Axis(fig[2, 1], aspect=1, title="FFT-based solution")
-ax_φ_cg = Axis(fig[2, 3], aspect=1, title="CG solution")
-ax_φ_mg = Axis(fig[2, 5], aspect=1, title="Multigrid solution")
-ax_φ_cgmg = Axis(fig[2, 7], aspect=1, title="PreconditionedCG MG solution")
-
-ax_∇²φ_fft = Axis(fig[3, 1], aspect=1, title="FFT-based ∇²φ")
-ax_∇²φ_cg = Axis(fig[3, 3], aspect=1, title="CG ∇²φ")
-ax_∇²φ_mg = Axis(fig[3, 5], aspect=1, title="Multigrid ∇²φ")
-ax_∇²φ_cgmg = Axis(fig[3, 7], aspect=1, title="PreconditionedCG MG ∇²φ")
-
-ax_err_fft = Axis(fig[4, 1], aspect=1, title="error FFT-based")
-ax_err_cg = Axis(fig[4, 3], aspect=1, title="error CG")
-ax_err_mg = Axis(fig[4, 5], aspect=1, title="error Multigrid")
-ax_err_cgmg = Axis(fig[4, 7], aspect=1, title="error PreconditionedCG MG")
-
-hm_r = heatmap!(ax_r, interior(r, :, :, 1))
-Colorbar(fig[1, 4], hm_r)
-
-hm_fft = heatmap!(ax_φ_fft, interior(φ_fft, :, :, 1))
-Colorbar(fig[2, 2], hm_fft)
-hm_cg = heatmap!(ax_φ_cg, interior(φ_cg, :, :, 1))
-Colorbar(fig[2, 4], hm_cg)
-hm_mg = heatmap!(ax_φ_mg, interior(φ_mg, :, :, 1))
-Colorbar(fig[2, 6], hm_mg)
-hm_cgmg = heatmap!(ax_φ_cgmg, interior(φ_cgmg, :, :, 1))
-Colorbar(fig[2, 8], hm_cgmg)
-
-hm_∇²fft = heatmap!(ax_∇²φ_fft, interior(∇²φ_fft, :, :, 1))
-Colorbar(fig[3, 2], hm_∇²fft)
-hm_∇²cg = heatmap!(ax_∇²φ_cg, interior(∇²φ_cg, :, :, 1))
-Colorbar(fig[3, 4], hm_∇²cg)
-hm_∇²mg = heatmap!(ax_∇²φ_mg, interior(∇²φ_mg, :, :, 1))
-Colorbar(fig[3, 6], hm_∇²mg)
-hm_∇²cgmg = heatmap!(ax_∇²φ_cgmg, interior(∇²φ_cgmg, :, :, 1))
-Colorbar(fig[3, 8], hm_∇²cgmg)
-
-hm_err_cg = heatmap!(ax_err_cg, interior(∇²φ_cg, :, :, 1) - interior(∇²φ_fft, :, :, 1))
-Colorbar(fig[4, 4], hm_err_cg)
-hm_err_mg = heatmap!(ax_err_mg, interior(∇²φ_mg, :, :, 1) - interior(∇²φ_fft, :, :, 1))
-Colorbar(fig[4, 6], hm_err_mg)
-hm_err_cgmg = heatmap!(ax_err_cgmg, interior(∇²φ_cgmg, :, :, 1) - interior(∇²φ_fft, :, :, 1))
-Colorbar(fig[4, 8], hm_err_cgmg)
-
-display(fig)
-=#
-
-using CairoMakie
-
-mean_time = mean.([bench_fft.times, bench_pcg.times, bench_mg.times, bench_mgpcg.times]) ./ 1e9 # convert ns -> s
-allocations = [bench_fft.allocs, bench_pcg.allocs, bench_mg.allocs, bench_mgpcg.allocs]
-memory = [bench_fft.memory, bench_pcg.memory, bench_mg.memory, bench_mgpcg.memory] ./ 1024 # convert b -> kb
-
-tbl = (x = [1, 2, 3, 4],
-       mean_time = mean_time,
-       allocations = allocations,
-       memory = memory
-       )
-
-colors = Makie.wong_colors()
-
-fig = Figure(resolution = (600, 800))
-ax1 = Axis(fig[1, 1];
-           xticks = (1:4, ["fft", "pcg", "mg", "mg-pcg"]),
-           ylabel = "μs",
-           limits = (nothing, (-0.1 * maximum(mean_time), 1.2 * maximum(mean_time))),
-           title = "mean time")
-
-ax2 = Axis(fig[2, 1];
-           xticks = (1:4, ["fft", "pcg", "mg", "mg-pcg"]),
-           limits = (nothing, (-0.1 * maximum(allocations), 1.2 * maximum(allocations))),
-           title = "memory allocations")
-
-ax3 = Axis(fig[3, 1];
-           xticks = (1:4, ["fft", "pcg", "mg", "mg-pcg"]),
-           ylabel = "kb",
-           limits = (nothing, (-0.1 * maximum(memory), 1.2 * maximum(memory))),
-           title = "memory used")
-
-barplot!(ax1, tbl.x, tbl.mean_time;
-         bar_labels = prettytime.(tbl.mean_time),
-         color = colors[tbl.x])
-
-barplot!(ax2, tbl.x, tbl.allocations;
-         bar_labels = string.(tbl.allocations),
-         color = colors[tbl.x])
-
-barplot!(ax3, tbl.x, tbl.memory;
-         bar_labels = string.(Int.(round.(tbl.memory))) .* " kb",
-         color = colors[tbl.x])
-
-save("solvers_benchmark_" * string(typeof(arch)) * ".png", fig)
-=#
