21cf8fb4991ac4f715bcac1fd46d01ae4b2403a7
Gregory Wagner <wagner.greg@gmail.com>
2022-10-29 08:29:37 -0600

    Add comment about the "other" update state

--- src/Models/HydrostaticFreeSurfaceModels/update_hydrostatic_free_surface_model_state.jl
+++ src/Models/HydrostaticFreeSurfaceModels/update_hydrostatic_free_surface_model_state.jl
@@ -9,14 +9,16 @@ import Oceananigans.TimeSteppers: update_state!
 
 compute_auxiliary_fields!(auxiliary_fields) = Tuple(compute!(a) for a in auxiliary_fields)
 
+# Note: see single_column_model_mode.jl for a "reduced" version of update_state! for
+# single column models.
+update_state!(model::HydrostaticFreeSurfaceModel) = update_state!(model, model.grid)
+
 """
     update_state!(model::HydrostaticFreeSurfaceModel)
 
 Update peripheral aspects of the model (auxiliary fields, halo regions, diffusivities,
 hydrostatic pressure) to the current model state.
 """
-update_state!(model::HydrostaticFreeSurfaceModel) = update_state!(model, model.grid)
-
 function update_state!(model::HydrostaticFreeSurfaceModel, grid)
 
     @apply_regionally masking_actions!(model)
@@ -24,7 +26,7 @@ function update_state!(model::HydrostaticFreeSurfaceModel, grid)
     fill_halo_regions!(prognostic_fields(model), model.clock, fields(model))
     fill_horizontal_velocity_halos!(model.velocities.u, model.velocities.v, model.architecture)
 
-    @apply_regionally update_state_actions!(model)
+    @apply_regionally compute_w_diffusivities_pressure!(model)
 
     fill_halo_regions!(model.velocities.w, model.clock, fields(model))
     fill_halo_regions!(model.diffusivity_fields, model.clock, fields(model))
@@ -42,8 +44,9 @@ function masking_actions!(model)
     wait(device(model.architecture), MultiEvent(Tuple(masking_events)))
 end
 
-function update_state_actions!(model) 
+function compute_w_diffusivities_pressure!(model) 
     compute_w_from_continuity!(model)
     calculate_diffusivities!(model.diffusivity_fields, model.closure, model)
     update_hydrostatic_pressure!(model.pressure.pHY′, model.architecture, model.grid, model.buoyancy, model.tracers)
 end
+
