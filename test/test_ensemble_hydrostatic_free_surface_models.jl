b1997713b35f41b42a1f5cc5660d6ceb2ec3e305
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-11-01 10:10:54 +1100

    better tests?

--- test/test_ensemble_hydrostatic_free_surface_models.jl
+++ test/test_ensemble_hydrostatic_free_surface_models.jl
@@ -7,13 +7,17 @@ const CAVD = ConvectiveAdjustmentVerticalDiffusivity
 @testset "Ensembles of `HydrostaticFreeSurfaceModel` with different closures" begin
 
     Nz = 16
-    Hz = 1 
-    grid = RectilinearGrid(size=Nz, z=(-10, 10), topology=(Flat, Flat, Bounded), halo=1)
+    Hz = 1
+    topology = (Flat, Flat, Bounded)
+    grid = RectilinearGrid(; size=Nz, z=(-10, 10), topology, halo=Hz)
 
     closures = [CAVD(background_κz=1.0) CAVD(background_κz=1.1)
-                CAVD(background_κz=1.2) CAVD(background_κz=1.3)]
-    
-    @test size(closures) == (2, 2)
+                CAVD(background_κz=1.2) CAVD(background_κz=1.3)
+                CAVD(background_κz=1.4) CAVD(background_κz=1.5)]
+
+    ensemble_size = size(closures)
+
+    @test size(closures) == (3, 2)
     @test closures[2, 1].background_κz == 1.2 
 
     Δt = 0.01 * grid.Δzᵃᵃᶜ^2
@@ -21,7 +25,7 @@ const CAVD = ConvectiveAdjustmentVerticalDiffusivity
     model_kwargs = (; tracers=:c, buoyancy=nothing, coriolis=nothing)
     simulation_kwargs = (; Δt, stop_iteration=100)
 
-    models = [HydrostaticFreeSurfaceModel(; grid, closure=closures[i, j], model_kwargs...) for i=1:2, j=1:2]
+    models = [HydrostaticFreeSurfaceModel(; grid, closure=closures[i, j], model_kwargs...) for i=1:ensemble_size[1], j=1:ensemble_size[2]]
 
     set_ic!(model) = set!(model, c = (x, y, z) -> exp(-z^2)) 
 
@@ -31,29 +35,29 @@ const CAVD = ConvectiveAdjustmentVerticalDiffusivity
         run!(simulation)
     end 
 
-    ensemble_size = ColumnEnsembleSize(Nz=Nz, ensemble=(2, 2), Hz=1)
-    ensemble_grid = RectilinearGrid(size=ensemble_size, z=(-10, 10), topology=(Flat, Flat, Bounded), halo=1)
+    ensemble_grid = RectilinearGrid(; size=ColumnEnsembleSize(; Nz, ensemble=ensemble_size, Hz),
+                                      z=(-10, 10), topology, halo=Hz)
 
-    @test size(ensemble_grid) == (2, 2, Nz) 
+    @test size(ensemble_grid) == (ensemble_size[1], ensemble_size[2], Nz)
 
     ensemble_model = HydrostaticFreeSurfaceModel(; grid=ensemble_grid, closure=closures, model_kwargs...)
     set_ic!(ensemble_model)
 
-    @test size(parent(ensemble_model.tracers.c)) == (2, 2, Nz+2)
+    @test size(parent(ensemble_model.tracers.c)) == (ensemble_size[1], ensemble_size[2], Nz+2)
 
     ensemble_simulation = Simulation(ensemble_model; simulation_kwargs...)
     run!(ensemble_simulation)
 
-    for i = 1:2, j = 1:2 
-        @info "Testing ScalarDiffusivity ensemble member ($i, $j)..."
+    for i=1:ensemble_size[1], j=1:ensemble_size[2]
+        @info "Testing ConvectiveAdjustmentVerticalDiffusivity ensemble member ($i, $j)..."
         @test parent(ensemble_model.tracers.c)[i, j, :] == parent(models[i, j].tracers.c)[1, 1, :]
-    end 
+    end
 
 end
 
 @testset "Ensembles of column `HydrostaticFreeSurfaceModel`s with different Coriolis parameters" begin
 
-    Nz = 4
+    Nz = 2
     Hz = 1
     topology = (Flat, Flat, Bounded)
 
