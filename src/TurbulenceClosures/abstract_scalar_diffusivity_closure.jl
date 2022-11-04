f5947f7e6ec3e4a96eea6458bfd87e341e62c3e2
tomchor <tomaschor@gmail.com>
2022-11-04 16:07:53 -0700

    oops

--- src/TurbulenceClosures/abstract_scalar_diffusivity_closure.jl
+++ src/TurbulenceClosures/abstract_scalar_diffusivity_closure.jl
@@ -58,7 +58,7 @@ function diffusivity end
 @inline diffusivity_location(::AbstractScalarDiffusivity) = (Center(), Center(), Center())
 
 # For tuples (note that kernel functions are "untupled", so these are for the user API)
-viscosity(closure::Tuple, K) = Tuple(viscosity(closure[n], K[n]) for n = 1:length(closure)
+viscosity(closure::Tuple, K) = Tuple(viscosity(closure[n], K[n]) for n = 1:length(closure))
 diffusivity(closure::Tuple, K, id) = Tuple(diffusivity(closure[n], K[n], id) for n = 1:length(closure))
 
 @inline formulation(::AbstractScalarDiffusivity{TD, F}) where {TD, F} = F()
