bef131d80f3750559e9926c7e6a6c120c3a8b18f
Simone Silvestri <silvestri.simone0@gmail.com>
2022-11-11 16:20:20 -0500

    bugfix

--- src/ImmersedBoundaries/conditional_fluxes.jl
+++ src/ImmersedBoundaries/conditional_fluxes.jl
@@ -202,7 +202,7 @@ for bias in (:symmetric, :left_biased, :right_biased)
                            $interp(i, j, k, ibg, scheme, args...))
             
                 # Conditional high-order interpolation for Vector Invariant WENO in Bounded directions
-                @inline $alt_interp(i, j, k, ibg::ImmersedBoundaryGrid, scheme::WENO, ζ, VI<:SmoothnessStencil, u, v) =
+                @inline $alt_interp(i, j, k, ibg::ImmersedBoundaryGrid, scheme::WENO, ζ, VI::Type{<:SmoothnessStencil}, u, v) =
                     ifelse($near_boundary(i, j, k, ibg, scheme),
                             $alt_interp(i, j, k, ibg, scheme.buffer_scheme, ζ, VI, u, v),
                             $interp(i, j, k, ibg, scheme, ζ, VI, u, v))
