a5455963c31135251e71012bb8136a54ac506cfa
Gregory Wagner <wagner.greg@gmail.com>
2022-11-16 11:04:45 -0700

    Simplify MixingLength

--- src/TurbulenceClosures/turbulence_closure_implementations/CATKEVerticalDiffusivities/mixing_length.jl
+++ src/TurbulenceClosures/turbulence_closure_implementations/CATKEVerticalDiffusivities/mixing_length.jl
@@ -19,7 +19,7 @@ Convective mixing length
 ========================
 
 ```math
-ℓʰ = convecting ? Cᴬ * Δz : 0
+ℓʰ = convecting ? Cᶜ * Δz : 0
 ```
 
 Stably-stratified mixing length
@@ -51,16 +51,13 @@ and ``step`` is the piecewise linear function
 
 """
 Base.@kwdef struct MixingLength{FT}
-    Cᵇu  :: FT = Inf
-    Cᵇc  :: FT = Inf
-    Cᵇe  :: FT = Inf
-    Cˢu  :: FT = Inf
-    Cˢc  :: FT = Inf
-    Cˢe  :: FT = Inf
-    Cᴬu  :: FT = 0.0
-    Cᴬc  :: FT = 0.0
-    Cᴬe  :: FT = 0.0
-    Cᴬˢ  :: FT = 0.0
+    Cᵇ   :: FT = Inf
+    Cˢ   :: FT = Inf
+    Cᶜc  :: FT = 0.0
+    Cᶜe  :: FT = 0.0
+    Cᵉc  :: FT = 0.0
+    Cᵉe  :: FT = 0.0
+    Cᶜˢ  :: FT = 0.0
     Cᴷu⁻ :: FT = 1.0
     Cᴷu⁺ :: FT = 1.0
     Cᴷc⁻ :: FT = 1.0
@@ -102,14 +99,20 @@ end
     return min(d, ℓᵇ, ℓˢ)
 end
 
-@inline function convective_mixing_lengthᶜᶜᶠ(i, j, k, grid, Cᴬ::Number, Cᴬˢ::Number,
+@inline function convective_mixing_lengthᶜᶜᶠ(i, j, k, grid, Cᶜ::Number, Cᵉ::Number, Cᶜˢ::Number,
                                              velocities, tracers, buoyancy, clock, tracer_bcs)
 
     # A kind of convective adjustment...
     N² = ∂z_b(i, j, k, grid, buoyancy, tracers) # buoyancy frequency
     N²_above = ∂z_b(i, j, k+1, grid, buoyancy, tracers) # buoyancy frequency
-    convecting = (N² < 0) | (N²_above < 0)
-    ℓʰ = Cᴬ * Δzᶜᶜᶠ(i, j, k, grid)
+
+    convecting = N² < 0
+    entraining = (!convecting) & (N²_above < 0)
+
+    C = ifelse(convecting, Cᶜ,
+        ifelse(entraining, Cᵉ, zero(grid)))
+
+    ℓʰ = C * Δzᶜᶜᶠ(i, j, k, grid)
 
     # "Sheared convection number"
     Qᵇ = top_buoyancy_flux(i, j, grid, buoyancy, tracer_bcs, clock, merge(velocities, tracers))
@@ -120,10 +123,10 @@ end
     α = ifelse(Qᵇ > 0, S * Qᵇ / e⁺, zero(S))
 
     # "Shear aware" mising length
-    ℓʰ *= 1 - Cᴬˢ * α
+    ℓʰ *= 1 - Cᶜˢ * α
     ℓʰ = max(zero(grid), ℓʰ)
     
-    return ifelse(convecting, ℓʰ, zero(grid))
+    return ℓʰ
 end
 
 """Piecewise linear function between 0 (when x < c) and 1 (when x - c > w)."""
@@ -139,48 +142,46 @@ end
 end
 
 @inline function momentum_mixing_lengthᶜᶜᶠ(i, j, k, grid, closure, velocities, tracers, buoyancy, clock, tracer_bcs)
-    Cᴬ = closure.mixing_length.Cᴬu
-    Cᴬˢ = closure.mixing_length.Cᴬˢ
-    ℓʰ = convective_mixing_lengthᶜᶜᶠ(i, j, k, grid, Cᴬ, Cᴬˢ, velocities, tracers, buoyancy, clock, tracer_bcs)
-
     Cᴷ⁻ = closure.mixing_length.Cᴷu⁻
     Cᴷ⁺ = closure.mixing_length.Cᴷu⁺
     σ = stable_mixing_scale(i, j, k, grid, Cᴷ⁻, Cᴷ⁺, closure, velocities, tracers, buoyancy)
 
-    Cᵇ = closure.mixing_length.Cᵇu
-    Cˢ = closure.mixing_length.Cˢu
+    Cᵇ = closure.mixing_length.Cᵇ
+    Cˢ = closure.mixing_length.Cˢ
     ℓ★ = σ * stable_mixing_lengthᶜᶜᶠ(i, j, k, grid, Cᵇ, Cˢ, tracers.e, velocities, tracers, buoyancy)
 
-    return ℓ★ + ℓʰ
+    return ℓ★
 end
 
 @inline function tracer_mixing_lengthᶜᶜᶠ(i, j, k, grid, closure, velocities, tracers, buoyancy, clock, tracer_bcs)
-    Cᴬ = closure.mixing_length.Cᴬc
-    Cᴬˢ = closure.mixing_length.Cᴬˢ
-    ℓʰ = convective_mixing_lengthᶜᶜᶠ(i, j, k, grid, Cᴬ, Cᴬˢ, velocities, tracers, buoyancy, clock, tracer_bcs)
+    Cᶜ = closure.mixing_length.Cᶜc
+    Cᵉ = closure.mixing_length.Cᵉc
+    Cᶜˢ = closure.mixing_length.Cᶜˢ
+    ℓʰ = convective_mixing_lengthᶜᶜᶠ(i, j, k, grid, Cᶜ, Cᵉ, Cᶜˢ, velocities, tracers, buoyancy, clock, tracer_bcs)
 
     Cᴷ⁻ = closure.mixing_length.Cᴷc⁻
     Cᴷ⁺ = closure.mixing_length.Cᴷc⁺
     σ = stable_mixing_scale(i, j, k, grid, Cᴷ⁻, Cᴷ⁺, closure, velocities, tracers, buoyancy)
 
-    Cᵇ = closure.mixing_length.Cᵇc
-    Cˢ = closure.mixing_length.Cˢc
+    Cᵇ = closure.mixing_length.Cᵇ
+    Cˢ = closure.mixing_length.Cˢ
     ℓ★ = σ * stable_mixing_lengthᶜᶜᶠ(i, j, k, grid, Cᵇ, Cˢ, tracers.e, velocities, tracers, buoyancy)
 
     return ℓ★ + ℓʰ
 end
 
 @inline function TKE_mixing_lengthᶜᶜᶠ(i, j, k, grid, closure, velocities, tracers, buoyancy, clock, tracer_bcs)
-    Cᴬ = closure.mixing_length.Cᴬe
-    Cᴬˢ = closure.mixing_length.Cᴬˢ
-    ℓʰ = convective_mixing_lengthᶜᶜᶠ(i, j, k, grid, Cᴬ, Cᴬˢ, velocities, tracers, buoyancy, clock, tracer_bcs)
+    Cᶜ = closure.mixing_length.Cᶜe
+    Cᵉ = closure.mixing_length.Cᵉe
+    Cᶜˢ = closure.mixing_length.Cᶜˢ
+    ℓʰ = convective_mixing_lengthᶜᶜᶠ(i, j, k, grid, Cᶜ, Cᵉ, Cᶜˢ, velocities, tracers, buoyancy, clock, tracer_bcs)
 
     Cᴷ⁻ = closure.mixing_length.Cᴷe⁻
     Cᴷ⁺ = closure.mixing_length.Cᴷe⁺
     σ = stable_mixing_scale(i, j, k, grid, Cᴷ⁻, Cᴷ⁺, closure, velocities, tracers, buoyancy)
 
-    Cᵇ = closure.mixing_length.Cᵇe
-    Cˢ = closure.mixing_length.Cˢe
+    Cᵇ = closure.mixing_length.Cᵇ
+    Cˢ = closure.mixing_length.Cˢ
     ℓ★ = σ * stable_mixing_lengthᶜᶜᶠ(i, j, k, grid, Cᵇ, Cˢ, tracers.e, velocities, tracers, buoyancy)
 
     return ℓ★ + ℓʰ
@@ -188,21 +189,18 @@ end
 
 Base.show(io::IO, ML::MixingLength) =
     print(io, "CATKEVerticalDiffusivities.MixingLength parameters:", "\n",
-              "    Cᵇu  = $(ML.Cᵇu)",   "\n",
-              "    Cᵇc  = $(ML.Cᵇc)",   "\n",
-              "    Cᵇe  = $(ML.Cᵇe)",   "\n",
-              "    Cˢu  = $(ML.Cˢu)",   "\n",
-              "    Cˢc  = $(ML.Cˢc)",   "\n",
-              "    Cˢe  = $(ML.Cˢe)",   "\n",
-              "    Cᴬu  = $(ML.Cᴬu)",   "\n",
-              "    Cᴬc  = $(ML.Cᴬc)",   "\n",
-              "    Cᴬe  = $(ML.Cᴬe)",   "\n",
-              "    Cᴷu⁻ = $(ML.Cᴷu⁻)",  "\n",
-              "    Cᴷc⁻ = $(ML.Cᴷc⁻)",  "\n",
-              "    Cᴷe⁻ = $(ML.Cᴷe⁻)",  "\n",
-              "    Cᴷu⁺ = $(ML.Cᴷu⁺)",  "\n",
-              "    Cᴷc⁺ = $(ML.Cᴷc⁺)",  "\n",
-              "    Cᴷe⁺ = $(ML.Cᴷe⁺)",  "\n",
+              "    Cᵇ   = $(ML.Cᵇ)",   "\n",
+              "    Cˢ   = $(ML.Cˢu)",  "\n",
+              "    Cᶜc  = $(ML.Cᶜc)",  "\n",
+              "    Cᶜe  = $(ML.Cᶜe)",  "\n",
+              "    Cᵉc  = $(ML.Cᵉc)",  "\n",
+              "    Cᵉe  = $(ML.Cᵉe)",  "\n",
+              "    Cᴷu⁻ = $(ML.Cᴷu⁻)", "\n",
+              "    Cᴷc⁻ = $(ML.Cᴷc⁻)", "\n",
+              "    Cᴷe⁻ = $(ML.Cᴷe⁻)", "\n",
+              "    Cᴷu⁺ = $(ML.Cᴷu⁺)", "\n",
+              "    Cᴷc⁺ = $(ML.Cᴷc⁺)", "\n",
+              "    Cᴷe⁺ = $(ML.Cᴷe⁺)", "\n",
               "    CRiʷ = $(ML.CRiʷ)", "\n",
               "    CRiᶜ = $(ML.CRiᶜ)")
 
