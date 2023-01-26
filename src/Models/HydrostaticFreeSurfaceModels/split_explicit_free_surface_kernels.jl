88bc58a5732b71eebd53d0a773b0f084a6250781
Simone Silvestri <silvestri.simone0@gmail.com>
2023-01-25 20:59:05 -0500

    fixed immersed boundary?

--- src/Models/HydrostaticFreeSurfaceModels/split_explicit_free_surface_kernels.jl
+++ src/Models/HydrostaticFreeSurfaceModels/split_explicit_free_surface_kernels.jl
@@ -54,10 +54,10 @@ the free surface and a `NoPenetration` boundary condition for velocity
 @inline div_yᶜᶜᶠ_bound(i, j, k, grid, TY, f, args...) = 
     1 / Azᶜᶜᶠ(i, j, k, grid) * δyᵃᶜᵃ_bound(i, j, k, grid, TY, Δx_qᶜᶠᶠ, f, args...) 
 
-using Oceananigans.ImmersedBoundaries: immersed_peripheral_node, IBG, c, f
+using Oceananigans.ImmersedBoundaries: immersed_peripheral_node, inative_node, IBG, c, f
 
-@inline conditional_value_fcf(i, j, k, grid, ibg, f::Function, args...) = ifelse(immersed_peripheral_node(i, j, k, ibg, f, c, f), zero(ibg), f(i, j, k, grid, args...))
-@inline conditional_value_cff(i, j, k, grid, ibg, f::Function, args...) = ifelse(immersed_peripheral_node(i, j, k, ibg, c, f, f), zero(ibg), f(i, j, k, grid, args...))
+@inline conditional_value_fcf(i, j, k, grid, ibg, f::Function, args...) = ifelse(inactive_node(i, j, k, ibg, f, c, f), zero(ibg), f(i, j, k, grid, args...))
+@inline conditional_value_cff(i, j, k, grid, ibg, f::Function, args...) = ifelse(inactive_node(i, j, k, ibg, c, f, f), zero(ibg), f(i, j, k, grid, args...))
 
 @inline conditional_∂x_bound_f(LY, LZ, i, j, k, ibg::IBG{FT}, ∂x, args...) where FT = ifelse(immersed_peripheral_node(i, j, k, ibg, c, LY, LZ) | immersed_peripheral_node(i+1, j, k, ibg, c, LY, LZ), zero(ibg), ∂x(i, j, k, ibg.underlying_grid, args...))
 @inline conditional_∂y_bound_f(LY, LZ, i, j, k, ibg::IBG{FT}, ∂y, args...) where FT = ifelse(immersed_peripheral_node(i, j, k, ibg, f, LY, LZ) | immersed_peripheral_node(i, j+1, k, ibg, f, LY, LZ), zero(ibg), ∂y(i, j, k, ibg.underlying_grid, args...))
@@ -82,8 +82,8 @@ end
     TX, TY, _ = topology(grid)
 
     @inbounds begin        
-        η[i, j, k_top] -= Δτ * (div_xᶜᶜᶠ_bound(i, j, 1, grid, TX, U★, U, Uᵐ⁻¹, Uᵐ⁻²) +
-                                div_yᶜᶜᶠ_bound(i, j, 1, grid, TY, U★, V, Vᵐ⁻¹, Vᵐ⁻²))
+        η[i, j, k_top] -= Δτ * (div_xᶜᶜᶠ_bound(i, j, k_top, grid, TX, U★, U, Uᵐ⁻¹, Uᵐ⁻²) +
+                                div_yᶜᶜᶠ_bound(i, j, k_top, grid, TY, U★, V, Vᵐ⁻¹, Vᵐ⁻²))
         
         Ũ[i, j, 1] +=  mass_flux_weight * U★(i, j, 1, grid, U, Uᵐ⁻¹, Uᵐ⁻²)
         Ṽ[i, j, 1] +=  mass_flux_weight * U★(i, j, 1, grid, V, Vᵐ⁻¹, Vᵐ⁻²)
