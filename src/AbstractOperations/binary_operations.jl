41e383e95d3e1503591413913b9ca6b44a95a61d
Gregory Wagner <wagner.greg@gmail.com>
2023-02-06 00:24:48 -0500

    Reorganize abstract ops

--- src/AbstractOperations/binary_operations.jl
+++ src/AbstractOperations/binary_operations.jl
@@ -195,40 +195,6 @@ macro binary(ops...)
 end
 
 #####
-##### BinaryOperations with ZeroField
-#####
-
-import Base: +, -, *, /, ==
-using Oceananigans.Fields: ZeroField, ConstantField
-
-==(::ZeroField, ::ZeroField) = true
-
-==(zf::ZeroField, cf::ConstantField) = 0 == cf.constant
-==(cf::ConstantField, zf::ZeroField) = 0 == cf.constant
-==(c1::ConstantField, c2::ConstantField) = c1.constant == c2.constant
-
-+(a::ZeroField, b::AbstractField) = b
-+(a::AbstractField, b::ZeroField) = a
-+(a::ZeroField, b::Number) = ConstantField(b)
-+(a::Number, b::ZeroField) = ConstantField(a)
-
--(a::ZeroField, b::AbstractField) = -b
--(a::AbstractField, b::ZeroField) = a
--(a::ZeroField, b::Number) = ConstantField(-b)
--(a::Number, b::ZeroField) = ConstantField(a)
-
-*(a::ZeroField, b::AbstractField) = a
-*(a::AbstractField, b::ZeroField) = b
-*(a::ZeroField, b::Number) = a
-*(a::Number, b::ZeroField) = b
-
-/(a::ZeroField, b::AbstractField) = a
-/(a::AbstractField, b::ZeroField) = ConstantField(convert(eltype(a), Inf))
-/(a::ZeroField, b::Number) = a
-/(a::Number, b::ZeroField) = ConstantField(a / convert(eltype(a), 0))
-
-
-#####
 ##### Nested computations
 #####
 
