76786762630c200261d9ebd597485ee1b8c2cd2a
Simone Silvestri <silvestri.simone0@gmail.com>
2023-02-14 18:47:32 -0500

    fixing connectivity

--- src/MultiRegion/cubed_sphere_partitions.jl
+++ src/MultiRegion/cubed_sphere_partitions.jl
@@ -52,15 +52,19 @@ utilities to get the index of the panel the index within the panel and the globa
 
 @inline Rx(panel_idx, partition::RegularCubedSpherePartition)  = partition.Rx    
 @inline Rx(panel_idx, partition::XRegularCubedSpherePartition) = partition.Rx    
-@inline Rx(panel_idx, partition::YRegularCubedSpherePartition) = partition.Rx[panel_idx] 
+@inline Rx(panel_idx, partition::CubedSpherePartition)         = partition.Rx[panel_idx] 
 
-@inline panel_index(r, partition)         = r ÷ div_per_panel(r, partition) + 1
-@inline intra_panel_index(r, partition)   = mod(r, div_per_panel(r, partition)) + 1
-@inline intra_panel_index_x(r, partition) = mod(intra_panel_index(r, partition), Rx(r, partition)) + 1
-@inline intra_panel_index_y(r, partition) = intra_panel_index(r, partition) ÷ Rx(r, partition) + 1 
+@inline Ry(panel_idx, partition::RegularCubedSpherePartition)  = partition.Ry    
+@inline Ry(panel_idx, partition::YRegularCubedSpherePartition) = partition.Ry    
+@inline Ry(panel_idx, partition::CubedSpherePartition)         = partition.Ry[panel_idx] 
+
+@inline panel_index(r, partition)         = (r - 1) ÷ div_per_panel(r, partition) + 1
+@inline intra_panel_index(r, partition)   = mod(r - 1, div_per_panel(r, partition)) + 1
+@inline intra_panel_index_x(r, partition) = mod(intra_panel_index(r, partition) - 1, Rx(r, partition)) + 1
+@inline intra_panel_index_y(r, partition) = (intra_panel_index(r, partition) - 1) ÷ Rx(r, partition) + 1
 
 @inline rank_from_panel_idx(pᵢ, pⱼ, panel_idx, partition::CubedSpherePartition) =
-            panel_idx * div_per_panel(panel_idx, partition) + Rx(panel_idx, partition) * (pⱼ - 1) + pᵢ
+            (panel_idx - 1) * div_per_panel(panel_idx, partition) + Rx(panel_idx, partition) * (pⱼ - 1) + pᵢ
 
 @inline function region_corners(r, p::CubedSpherePartition)
     pᵢ = intra_panel_index_x(r, p)
@@ -134,24 +138,24 @@ function inject_west_boundary(region, p::CubedSpherePartition, global_bc)
     pᵢ = intra_panel_index_x(region, p)
     pⱼ = intra_panel_index_y(region, p)
 
-    panel_idx = panel_index(region, p)
+    pidx = panel_index(region, p)
 
     if pᵢ == 1
-        if mod(panel_idx, 2) == 0
+        if mod(pidx, 2) == 0
             from_side = :east
-            from_panel = panel_index - 1
-            from_pᵢ   = p.div_per_side
-            from_pⱼ   = pⱼ
+            from_panel = pidx - 1
+            from_pᵢ    = Rx(from_panel, p)
+            from_pⱼ    = pⱼ
         else    
-            from_side = :north
-            from_panel = mod(panel_idx + 3, 6) + 1
-            from_pᵢ   = p.div_per_side - pⱼ + 1
-            from_pⱼ   = p.div_per_side
+            from_side  = :north
+            from_panel = mod(pidx + 3, 6) + 1
+            from_pᵢ    = Rx(from_panel, p) - pⱼ + 1
+            from_pⱼ    = Ry(from_panel, p)
         end
         from_rank = rank_from_panel_idx(from_pᵢ, from_pⱼ, from_panel, p)
     else
         from_side = :east
-        from_rank = rank_from_panel_idx(pᵢ - 1, pⱼ, panel_idx, p)
+        from_rank = rank_from_panel_idx(pᵢ - 1, pⱼ, pidx, p)
     end
 
     bc = CommunicationBoundaryCondition(CubedSphereConnectivity(region, from_rank, :west, from_side))
@@ -164,24 +168,24 @@ function inject_east_boundary(region, p::CubedSpherePartition, global_bc)
     pᵢ = intra_panel_index_x(region, p)
     pⱼ = intra_panel_index_y(region, p)
 
-    panel_idx = panel_index(region, p)
+    pidx = panel_index(region, p)
 
-    if pᵢ == p.div_per_side
-        if mod(panel_idx, 2) != 0
-            from_side = :west
-            from_panel = panel_index + 1
-            from_pᵢ   = 1
-            from_pⱼ   = pⱼ
+    if pᵢ == p.Rx
+        if mod(pidx, 2) != 0
+            from_side  = :west
+            from_panel = pidx + 1
+            from_pᵢ    = 1
+            from_pⱼ    = pⱼ
         else    
-            from_side = :south
-            from_panel = mod(panel_idx + 1, 6) + 1
-            from_pᵢ   = p.div_per_side - pⱼ + 1
-            from_pⱼ   = 1
+            from_side  = :south
+            from_panel = mod(pidx + 1, 6) + 1
+            from_pᵢ    = Rx(from_panel, p) - pⱼ + 1
+            from_pⱼ    = 1
         end
         from_rank = rank_from_panel_idx(from_pᵢ, from_pⱼ, from_panel, p)
     else
         from_side = :west
-        from_rank = rank_from_panel_idx(pᵢ + 1, pⱼ, panel_idx, p)
+        from_rank = rank_from_panel_idx(pᵢ + 1, pⱼ, pidx, p)
     end
 
     bc = CommunicationBoundaryCondition(CubedSphereConnectivity(region, from_rank, :east, from_side))
@@ -193,24 +197,24 @@ function inject_south_boundary(region, p::CubedSpherePartition, global_bc)
     pᵢ = intra_panel_index_x(region, p)
     pⱼ = intra_panel_index_y(region, p)
 
-    panel_idx = panel_index(region, p)
+    pidx = panel_index(region, p)
 
     if pⱼ == 1
-        if mod(panel_idx, 2) != 0
-            from_side = :north
-            from_panel = mod(panel_index + 4, 6) + 1
-            from_pᵢ   = pᵢ 
-            from_pⱼ   = p.div_per_side
+        if mod(pidx, 2) != 0
+            from_side  = :north
+            from_panel = mod(pidx + 4, 6) + 1
+            from_pᵢ    = pᵢ 
+            from_pⱼ    = Ry(from_panel, p)
         else    
-            from_side = :east
-            from_panel = mod(panel_idx + 3, 6) + 1
-            from_pᵢ   = p.div_per_side
-            from_pⱼ   = p.div_per_side - pᵢ + 1
+            from_side  = :east
+            from_panel = mod(pidx + 3, 6) + 1
+            from_pᵢ    = Rx(from_panel, p)
+            from_pⱼ    = Ry(from_panel, p) - pᵢ + 1
         end
         from_rank = rank_from_panel_idx(from_pᵢ, from_pⱼ, from_panel, p)
     else
         from_side = :north
-        from_rank = rank_from_panel_idx(pᵢ, pⱼ - 1, panel_idx, p)
+        from_rank = rank_from_panel_idx(pᵢ, pⱼ - 1, pidx, p)
     end
 
     bc = CommunicationBoundaryCondition(CubedSphereConnectivity(region, from_rank, :south, from_side))
@@ -222,27 +226,63 @@ function inject_north_boundary(region, p::CubedSpherePartition, global_bc)
     pᵢ = intra_panel_index_x(region, p)
     pⱼ = intra_panel_index_y(region, p)
 
-    panel_idx = panel_index(region, p)
+    pidx = panel_index(region, p)
 
-    if pⱼ == p.div_per_side
-        if mod(panel_idx, 2) == 0
-            from_side = :south
-            from_panel = mod(panel_index, 6) + 1
-            from_pᵢ   = pᵢ 
-            from_pⱼ   = 1
+    if pⱼ == p.Ry
+        if mod(pidx, 2) == 0
+            from_side  = :south
+            from_panel = mod(pidx, 6) + 1
+            from_pᵢ    = pᵢ 
+            from_pⱼ    = 1
         else    
-            from_side = :west
-            from_panel = mod(panel_idx + 1, 6) + 1
-            from_pᵢ   = 1
-            from_pⱼ   = p.div_per_side - pᵢ + 1
+            from_side  = :west
+            from_panel = mod(pidx + 1, 6) + 1
+            from_pᵢ    = 1
+            from_pⱼ    = Rx(from_panel, p) - pᵢ + 1
         end
         from_rank = rank_from_panel_idx(from_pᵢ, from_pⱼ, from_panel, p)
     else
         from_side = :south
-        from_rank = rank_from_panel_idx(pᵢ, pⱼ + 1, panel_idx, p)
+        from_rank = rank_from_panel_idx(pᵢ, pⱼ + 1, pidx, p)
     end
 
     bc = CommunicationBoundaryCondition(CubedSphereConnectivity(region, from_rank, :south, from_side))
 
     return bc
 end
+
+"""
+Partition with 4 regions per panel (2 divisions in x and 2 in y)
+
+part = CubedSpherePartition(Rx = 2, Ry = 2)
+
+testing the correct injection of `west` boundary conditions
+
+for i in 1:24
+    @show i, inject_west_boundary(i, part2, 1).condition
+end
+(i, (inject_west_boundary(i, part2, 1)).condition) = (1, Oceananigans.MultiRegion.CubedSphereConnectivity(1, 20, :west, :north))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (2, Oceananigans.MultiRegion.CubedSphereConnectivity(2, 1, :west, :east))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (3, Oceananigans.MultiRegion.CubedSphereConnectivity(3, 19, :west, :north))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (4, Oceananigans.MultiRegion.CubedSphereConnectivity(4, 3, :west, :east))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (5, Oceananigans.MultiRegion.CubedSphereConnectivity(5, 2, :west, :east))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (6, Oceananigans.MultiRegion.CubedSphereConnectivity(6, 5, :west, :east))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (7, Oceananigans.MultiRegion.CubedSphereConnectivity(7, 4, :west, :east))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (8, Oceananigans.MultiRegion.CubedSphereConnectivity(8, 7, :west, :east))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (9, Oceananigans.MultiRegion.CubedSphereConnectivity(9, 4, :west, :north))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (10, Oceananigans.MultiRegion.CubedSphereConnectivity(10, 9, :west, :east))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (11, Oceananigans.MultiRegion.CubedSphereConnectivity(11, 3, :west, :north))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (12, Oceananigans.MultiRegion.CubedSphereConnectivity(12, 11, :west, :east))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (13, Oceananigans.MultiRegion.CubedSphereConnectivity(13, 10, :west, :east))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (14, Oceananigans.MultiRegion.CubedSphereConnectivity(14, 13, :west, :east))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (15, Oceananigans.MultiRegion.CubedSphereConnectivity(15, 12, :west, :east))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (16, Oceananigans.MultiRegion.CubedSphereConnectivity(16, 15, :west, :east))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (17, Oceananigans.MultiRegion.CubedSphereConnectivity(17, 12, :west, :north))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (18, Oceananigans.MultiRegion.CubedSphereConnectivity(18, 17, :west, :east))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (19, Oceananigans.MultiRegion.CubedSphereConnectivity(19, 11, :west, :north))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (20, Oceananigans.MultiRegion.CubedSphereConnectivity(20, 19, :west, :east))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (21, Oceananigans.MultiRegion.CubedSphereConnectivity(21, 18, :west, :east))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (22, Oceananigans.MultiRegion.CubedSphereConnectivity(22, 21, :west, :east))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (23, Oceananigans.MultiRegion.CubedSphereConnectivity(23, 20, :west, :east))
+(i, (inject_west_boundary(i, part2, 1)).condition) = (24, Oceananigans.MultiRegion.CubedSphereConnectivity(24, 23, :west, :east))
+"""
