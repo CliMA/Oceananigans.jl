022d97eca1f471e996e47b839329b6b0aa98b6f3
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-02-25 08:35:26 -0600

    fix typo

--- src/Models/NonhydrostaticModels/nonhydrostatic_model.jl
+++ src/Models/NonhydrostaticModels/nonhydrostatic_model.jl
@@ -78,7 +78,7 @@ Keyword arguments
 
   - `grid`: (required) The resolution and discrete geometry on which the `model` is solved. The
             architecture (CPU/GPU) that the model is solve on is inferred from the architecture
-            of the `grid`. Note that the grid needs to be regurarly spaced in the horizontal
+            of the `grid`. Note that the grid needs to be regularly spaced in the horizontal
             dimensions, ``x`` and ``y``.
   - `advection`: The scheme that advects velocities and tracers. See `Oceananigans.Advection`.
   - `buoyancy`: The buoyancy model. See `Oceananigans.BuoyancyModels`.
