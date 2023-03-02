79a4f22697c72422e33bd1c25a8bad879d77283a
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-03-02 10:49:14 -0700

    new ConformalCubedSphereGrid

--- src/MultiRegion/multi_region_cubed_sphere_grid.jl
+++ src/MultiRegion/multi_region_cubed_sphere_grid.jl
@@ -1,3 +1,5 @@
+using Oceananigans.Grids: R_Earth
+
 using Rotations
 
 rotation_from_panel_index(idx) = idx == 1 ? RotX(π/2)*RotY(π/2) :
@@ -6,14 +8,40 @@ rotation_from_panel_index(idx) = idx == 1 ? RotX(π/2)*RotY(π/2) :
                                  idx == 4 ? RotX(π)*RotY(-π/2) :
                                  idx == 5 ? RotY(π/2)*RotX(π/2) :
                                  RotZ(π/2)*RotX(π)
-                                    
-function MultiRegionGrid(arch::AbstractArchitecture, FT=Float64;
-                         panel_size, z,
-                         panel_halo = (1, 1, 1),
-                         panel_topology = (FullyConnected, FullyConnected, Bounded),
-                         radius = R_Earth,
-                         partition = CubedSpherePartition(), 
-                         devices = nothing)
+
+"""
+    ConformalCubedSphereGrid(arch::AbstractArchitecture, FT=Float64;
+                             panel_size,
+                             z,
+                             panel_halo = (1, 1, 1),
+                             panel_topology = (FullyConnected, FullyConnected, Bounded),
+                             radius = R_Earth,
+                             partition = CubedSpherePartition(), 
+                             devices = nothing)
+
+Return a ConformalCubedSphereGrid.
+
+Example
+=======
+
+```julia
+julia> using Oceananigans
+
+julia> grid = ConformalCubedSphereGrid(CPU(); panel_size = (10, 10, 1), z = (0, 1), radius = 1.0)
+ConformalCubedSphereGrid{Float64, FullyConnected, FullyConnected, Bounded} partitioned on CPU(): 
+├── grids: 10×10×1 OrthogonalSphericalShellGrid{Float64, Bounded, Bounded, Bounded} on CPU with 1×1×1 halo and with precomputed metrics 
+├── partitioning: CubedSpherePartition with (1 region in each panel) 
+└── devices: (CPU(), CPU(), CPU(), CPU(), CPU(), CPU())
+```
+"""
+function ConformalCubedSphereGrid(arch::AbstractArchitecture, FT=Float64;
+                                  panel_size,
+                                  z,
+                                  panel_halo = (1, 1, 1),
+                                  panel_topology = (FullyConnected, FullyConnected, Bounded),
+                                  radius = R_Earth,
+                                  partition = CubedSpherePartition(; Rx=1),
+                                  devices = nothing)
 
     devices = validate_devices(partition, arch, devices)
     devices = assign_devices(partition, devices)
@@ -22,11 +50,14 @@ function MultiRegionGrid(arch::AbstractArchitecture, FT=Float64;
     region_η    = []
     region_ξ    = []
     region_rot  = []
+
     for r in 1:length(partition)
         Δξ = 2 ./ Rx(r, partition)
         Δη = 2 ./ Ry(r, partition)
+
         pᵢ = intra_panel_index_x(r, partition)
         pⱼ = intra_panel_index_y(r, partition)
+
         push!(region_size, (panel_size[1] ÷ Rx(r, partition), panel_size[2] ÷ Ry(r, partition), panel_size[3]))
         push!(region_ξ,    (-1 + Δξ * (pᵢ - 1), -1 + Δξ * pᵢ))
         push!(region_η,    (-1 + Δη * (pⱼ - 1), -1 + Δη * pⱼ))
@@ -50,9 +81,14 @@ function MultiRegionGrid(arch::AbstractArchitecture, FT=Float64;
     return MultiRegionGrid{FT, panel_topology[1], panel_topology[2], panel_topology[3]}(arch, partition, region_grids, devices)
 end
 
+Base.show(io::IO, mrg::MultiRegionGrid{FT, TX, TY, TZ, <:CubedSpherePartition}) where {FT, TX, TY, TZ} =  
+    print(io, "ConformalCubedSphereGrid{$FT, $TX, $TY, $TZ} partitioned on $(architecture(mrg)): \n",
+              "├── grids: $(summary(mrg.region_grids[1])) \n",
+              "├── partitioning: $(summary(mrg.partition)) \n",
+              "└── devices: $(devices(mrg))")
+
 """
 Constructing a MultiRegionGrid
-grid = MultiRegionGrid(CPU(); partition = CubedSpherePartition(), panel_size = (10, 10, 1), z = (0, 1), radius = 1.0)
 
 to set a field
 
