7f2dea47433bfd6416e02dfacca6e832a3ccf1f7
Gregory Wagner <wagner.greg@gmail.com>
2023-03-15 11:42:53 -0600

    Fix bug in mask_immersed_field

--- src/ImmersedBoundaries/mask_immersed_field.jl
+++ src/ImmersedBoundaries/mask_immersed_field.jl
@@ -20,7 +20,7 @@ function mask_immersed_field!(field::Field, value=zero(field.grid); blocking=tru
     end
 end
 
-function mask_immersed_field_xy!(field value=zero(field.grid); k, blocking, mask=peripheral_node)
+function mask_immersed_field_xy!(field, value=zero(field.grid); k, blocking, mask=peripheral_node)
     if blocking
         event = mask_immersed_field_xy!(field, field.grid, location(field), value; k, mask)
         wait(device(architecture(field)), event)
