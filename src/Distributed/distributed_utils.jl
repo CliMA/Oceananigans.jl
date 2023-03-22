bba6448494526cc10c884c086d225b02b2edaeba
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-03-22 16:29:01 +1100

    fix

--- src/Distributed/distributed_utils.jl
+++ src/Distributed/distributed_utils.jl
@@ -46,23 +46,26 @@ top_halo(f::AbstractField{LX, LY, LZ}; include_corners=true) where {LX, LY, LZ} 
                                    interior_indices(LY, topology(f, 2), f.grid.Ny),
                                    right_halo_indices(LZ, topology(f, 3), f.grid.Nz, f.grid.Hz))
 
+instantiate(T::Type) = T()
+instantiate(t) = t
+
 underlying_west_halo(f, grid, location) =
-    view(f.parent, underlying_left_halo_indices(location(), topology(grid, 1)(), grid.Nx, grid.Hx), :, :)
+    view(f.parent, underlying_left_halo_indices(instantiate(location), instantiate(topology(grid, 1)), grid.Nx, grid.Hx), :, :)
 
 underlying_east_halo(f, grid, location) =
-    view(f.parent, underlying_right_halo_indices(location(), topology(grid, 1)(), grid.Nx, grid.Hx), :, :)
+    view(f.parent, underlying_right_halo_indices(instantiate(location), instantiate(topology(grid, 1)), grid.Nx, grid.Hx), :, :)
 
 underlying_south_halo(f, grid, location) =
-    view(f.parent, :, underlying_left_halo_indices(location(), topology(grid, 2)(), grid.Ny, grid.Hy), :)
+    view(f.parent, :, underlying_left_halo_indices(instantiate(location), instantiate(topology(grid, 2)), grid.Ny, grid.Hy), :)
 
 underlying_north_halo(f, grid, location) =
-    view(f.parent, :, underlying_right_halo_indices(location(), topology(grid, 2)(), grid.Ny, grid.Hy), :)
+    view(f.parent, :, underlying_right_halo_indices(instantiate(location), instantiate(topology(grid, 2)), grid.Ny, grid.Hy), :)
 
 underlying_bottom_halo(f, grid, location) =
-    view(f.parent, :, :, underlying_left_halo_indices(location(), topology(grid, 3)(), grid.Nz, grid.Hz))
+    view(f.parent, :, :, underlying_left_halo_indices(instantiate(location), instantiate(topology(grid, 3)), grid.Nz, grid.Hz))
 
 underlying_top_halo(f, grid, location) =
-    view(f.parent, :, :, underlying_right_halo_indices(location(), topology(grid, 3)(), grid.Nz, grid.Hz))
+    view(f.parent, :, :, underlying_right_halo_indices(instantiate(location), instantiate(topology(grid, 3)), grid.Nz, grid.Hz))
 
 #####
 ##### Viewing boundary grid points (used to fill other halos)
