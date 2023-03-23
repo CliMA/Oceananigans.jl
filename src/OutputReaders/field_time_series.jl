49f3ef98b0db1b5d240ab584b420c76e229db9cc
Gregory Wagner <wagner.greg@gmail.com>
2023-03-23 15:30:38 -0600

    Adds back important constructor

--- src/OutputReaders/field_time_series.jl
+++ src/OutputReaders/field_time_series.jl
@@ -43,6 +43,27 @@ struct UnspecifiedBoundaryConditions end
 #####
 
 """
+    FieldTimeSeries{LX, LY, LZ}(grid, times, [FT=eltype(grid);]
+                                indices = (:, :, :),
+                                boundary_conditions = nothing)
+
+Return a `FieldTimeSeries` at location `(LX, LY, LZ)`, on `grid`, at `times`.
+"""
+function FieldTimeSeries{LX, LY, LZ}(grid, times, FT=eltype(grid);
+                                     indices = (:, :, :),
+                                     boundary_conditions = nothing) where {LX, LY, LZ}
+
+    Nt = length(times)
+    arch = architecture(grid)
+    loc = (LX, LY, LZ)
+    space_size = total_size(loc, grid, indices)
+    underlying_data = zeros(FT, arch, space_size..., Nt)
+    data = offset_data(underlying_data, grid, loc, indices)
+
+    return FieldTimeSeries{LX, LY, LZ, InMemory}(data, grid, boundary_conditions, times, indices)
+end
+
+"""
     FieldTimeSeries(path, name;
                     backend = InMemory(),
                     grid = nothing,
