f25a3d2e38975efde1296ccb4522460dd71f3867
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-03-26 11:46:51 +1100

    remove CubedSphere module

--- validation/multi_region/multi_region_cubedsphere.jl
+++ validation/multi_region/multi_region_cubedsphere.jl
@@ -9,10 +9,46 @@ using Oceananigans.BoundaryConditions: fill_halo_regions!
 using GeoMakie, GLMakie
 GLMakie.activate!()
 
+function heatsphere!(ax::Axis3, field::Field, k=1; kwargs...)
+    LX, LY, LZ = location(field)
+    grid = field.grid
 
-Nx, Ny = 8, 8
+    _, (xvertices, yvertices, zvertices) = get_cartesian_nodes_and_vertices(grid, LX(), LY(), LZ())
 
-grid = ConformalCubedSphereGrid(panel_size=(Nx, Ny, 1), z=(-1, 0), radius=1, panel_halo = (3, 3, 1), panel_topology=(Bounded, Bounded, Bounded))
+    quad_points3 = vcat([Point3.(xvertices[:, i, j], yvertices[:, i, j], zvertices[:, i, j]) for i in axes(xvertices, 2), j in axes(xvertices, 3)]...)
+    quad_faces = vcat([begin; j = (i-1) * 4 + 1; [j j+1  j+2; j+2 j+3 j]; end for i in 1:length(quad_points3)÷4]...)
+    
+    colors_per_point = vcat(fill.(vec(interior(field, :, :, k)), 4)...)
+    
+    mesh!(ax, quad_points3, quad_faces; color = colors_per_point, shading = false, kwargs...)
+
+    return ax
+end
+
+function heatlatlon!(ax::Axis, field::Field, k=1; kwargs...)
+    LX, LY, LZ = location(field)
+    grid = field.grid
+
+    _, (λvertices, φvertices) = get_lat_lon_nodes_and_vertices(grid, LX(), LY(), LZ())
+
+    quad_points = vcat([Point2.(λvertices[:, i, j], φvertices[:, i, j]) for i in axes(λvertices, 2), j in axes(λvertices, 3)]...)
+    quad_faces = vcat([begin; j = (i-1) * 4 + 1; [j j+1  j+2; j+2 j+3 j]; end for i in 1:length(quad_points)÷4]...)
+
+    colors_per_point = vcat(fill.(vec(interior(field, :, :, k)), 4)...)
+
+    mesh!(ax, quad_points, quad_faces; color = colors_per_point, shading = false, kwargs...)
+
+    xlims!(ax, (-180, 180))
+    ylims!(ax, (-90, 90))
+
+    return ax
+end
+
+
+
+Nx, Ny, Nz = 16, 16, 2
+
+grid = ConformalCubedSphereGrid(panel_size=(Nx, Ny, Nz), z=(-1, 0), radius=1, panel_halo = (3, 3, 1), panel_topology=(Bounded, Bounded, Bounded))
 
 c = CenterField(grid)
 
@@ -44,17 +80,7 @@ fig = Figure()
 ax = Axis3(fig[1, 1], aspect=(1, 1, 1), limits=((-1, 1), (-1, 1), (-1, 1)))
 
 for region in [1, 2, 3, 4, 5, 6]
-    (xc, yc, zc), (xvertices, yvertices, zvertices) = get_cartesian_nodes_and_vertices(Center(), Center(), getregion(c.grid, region))
-
-    quad_points3 = vcat([Point3{eltype(grid)}.(xvertices[:, i, j], yvertices[:, i, j], zvertices[:, i, j]) for i in 1:size(xvertices, 2), j in 1:size(xvertices, 3)]...)
-    quad_faces = vcat([begin; j = (i-1) * 4 + 1; [j j+1  j+2; j+2 j+3 j]; end for i in 1:length(quad_points3)÷4]...)
-
-    colors_per_point = vcat(fill.(vec(getregion(c, region)), 4)...)
-
-    mesh!(ax, quad_points3, quad_faces; color = colors_per_point, shading = false, colorrange, colormap)
-    cl = lines!(ax, GeoMakie.coastlines(), color = :black, linewidth=0.85)
-
-    translate!(cl, 0, 0, 1000)
+    heatsphere!(ax, getregion(c, region); colorrange, colormap)
 end
 
 fig
@@ -64,17 +90,8 @@ fig = Figure()
 ax = Axis(fig[1, 1])
 
 for region in [1, 2, 3, 4, 5, 6]
-    (λc, φc), (λvertices, φvertices) = get_lat_lon_nodes_and_vertices(Center(), Center(), getregion(c.grid, region))
-
-    quad_points = vcat([Point2{eltype(grid)}.(λvertices[:, i, j], φvertices[:, i, j]) for i in 1:size(λvertices, 2), j in 1:size(λvertices, 3)]...)
-    quad_faces = vcat([begin; j = (i-1) * 4 + 1; [j j+1  j+2; j+2 j+3 j]; end for i in 1:length(quad_points)÷4]...)
-
-    colors_per_point = vcat(fill.(vec(getregion(c, region)), 4)...)
-
-    mesh!(ax, quad_points, quad_faces; color = colors_per_point, shading = false, colorrange, colormap)
+    heatlatlon!(ax, getregion(c, region); colorrange, colormap)
 end
-xlims!(ax, (-180, 180))
-ylims!(ax, (-90, 90))
 
 fig
 
@@ -86,17 +103,7 @@ ax = GeoAxis(fig[1, 1],
              lonlims = automatic)
 
 for region in [1, 2, 3, 4, 5, 6]
-    (λc, φc), (λvertices, φvertices) = get_lat_lon_nodes_and_vertices(Center(), Center(), getregion(c.grid, region))
-
-    quad_points = vcat([Point2{eltype(grid)}.(λvertices[:, i, j], φvertices[:, i, j]) for i in 1:size(λvertices, 2), j in 1:size(λvertices, 3)]...)
-    quad_faces = vcat([begin; j = (i-1) * 4 + 1; [j j+1  j+2; j+2 j+3 j]; end for i in 1:length(quad_points)÷4]...)
-
-    colors_per_point = vcat(fill.(vec(getregion(c, region)), 4)...)
-
-    mesh!(ax, quad_points, quad_faces; color = colors_per_point, shading = false, colorrange, colormap)
-    cl = lines!(ax, GeoMakie.coastlines(), color = :black, linewidth=0.85)
-
-    translate!(cl, 0, 0, 1000)
+    heatlatlon!(ax, getregion(c, region); colorrange, colormap)
 end
 
 fig
