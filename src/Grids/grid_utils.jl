7370348af2a85b10fbd308e874ca9eabf619e049
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-03-27 15:49:32 +1100

    better docs for nodes

--- src/Grids/grid_utils.jl
+++ src/Grids/grid_utils.jl
@@ -297,15 +297,18 @@ julia> zC = znodes(horz_periodic_grid, Center(), Center(), Center(), with_halos=
     nodes(grid, (ℓx, ℓy, ℓz); reshape=false, with_halos=false)
     nodes(grid, ℓx, ℓy, ℓz; reshape=false, with_halos=false)
 
-Return a 3-tuple of views over the interior nodes
-at the locations in `loc=(ℓx, ℓy, ℓz)` in `x, y, z`.
+Return a 3-tuple of views over the interior nodes of the `grid`'s
+native coordinates at the locations in `loc=(ℓx, ℓy, ℓz)` in `x, y, z`.
 
-If `reshape=true`, the views are reshaped to 3D arrays
-with non-singleton dimensions 1, 2, 3 for `x, y, z`, respectively.
-These reshaped arrays can then be used in broadcast operations with 3D fields
-or arrays.
+If `reshape=true`, the views are reshaped to 3D arrays with non-singleton
+dimensions 1, 2, 3 for `x, y, z`, respectively. These reshaped arrays can then
+be used in broadcast operations with 3D fields or arrays.
 
-See [`xnodes`](@ref), [`ynodes`](@ref), and [`znodes`](@ref).
+For `RectilinearGrid`s the native coordinates are `x, y, z`; for curvilinear grids,
+like `LatitudeLongitudeGrid` or `OrthogonalSphericalShellGrid` the native coordinates
+are `λ, φ, z`.
+
+See [`xnodes`](@ref), [`ynodes`](@ref), [`znodes`](@ref), [`λnodes`](@ref), and [`φnodes`](@ref).
 """
 nodes(grid::AbstractGrid, (ℓx, ℓy, ℓz); reshape=false, with_halos=false) = nodes(grid, ℓx, ℓy, ℓz; reshape, with_halos)
 
