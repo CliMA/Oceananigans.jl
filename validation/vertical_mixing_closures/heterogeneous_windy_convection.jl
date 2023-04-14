47d089186721267f9c3cd4681eb968427b020843
Gregory Wagner <wagner.greg@gmail.com>
2023-04-13 22:16:24 -0800

    Update to homogeneous windy convection

--- validation/vertical_mixing_closures/heterogeneous_windy_convection.jl
+++ validation/vertical_mixing_closures/heterogeneous_windy_convection.jl
@@ -7,8 +7,10 @@ using Oceananigans.Units
 using Oceananigans.TurbulenceClosures: CATKEVerticalDiffusivity
 using Oceananigans.ImmersedBoundaries: GridFittedBottom, PartialCellBottom
 
+import Oceananigans.TurbulenceClosures.CATKEVerticalDiffusivities
+
 Nx = 1
-Ny = 100
+Ny = 200
 
 const Lx = 1000kilometers
 const Ly = Lx
@@ -45,17 +47,20 @@ u_top_bc = FluxBoundaryCondition(Qᵘ)
 b_bcs = FieldBoundaryConditions(top=b_top_bc)
 u_bcs = FieldBoundaryConditions(top=u_top_bc)
 
-vertical_mixing = CATKEVerticalDiffusivity()
+vertical_mixing = CATKEVerticalDiffusivity(; minimum_turbulent_kinetic_energy=1e-9)
 #vertical_mixing = RiBasedVerticalDiffusivity()
 
+@show vertical_mixing
+
 Δy = Ly / Ny
-ν₄ = Δy^4 / 70minutes
+ν₄ = Δy^4 / 1hours
 hyperviscosity = HorizontalScalarBiharmonicDiffusivity(ν=ν₄)
 
 #closure = vertical_mixing
 closure = (vertical_mixing, hyperviscosity)
 
 filename = "heterogeneous_cooling_with_hyperviscosity.jld2"
+#filename = "heterogeneous_cooling.jld2"
 
 model = HydrostaticFreeSurfaceModel(; grid, closure,
                                     momentum_advection = WENO(),
@@ -65,11 +70,11 @@ model = HydrostaticFreeSurfaceModel(; grid, closure,
                                     boundary_conditions = (; b=b_bcs, u=u_bcs),
                                     buoyancy = BuoyancyTracer())
 
-N² = 1e-5
-bᵢ(x, y, z) = N² * z
-set!(model, b=bᵢ, e=1e-6)
+N²ᵢ = 1e-5
+bᵢ(x, y, z) = N²ᵢ * z
+set!(model, b=bᵢ, e=1e-9)
 
-simulation = Simulation(model, Δt=5minute, stop_time=2days)
+simulation = Simulation(model, Δt=5minute, stop_time=10days)
 
 κᶜ = if model.closure isa Tuple
     model.diffusivity_fields[1].κᶜ
@@ -77,7 +82,9 @@ else
     model.diffusivity_fields.κᶜ
 end
 
-outputs = (; model.velocities..., model.tracers..., κᶜ=κᶜ)
+b = model.tracers.b
+N² = ∂z(b)
+outputs = (; model.velocities..., model.tracers..., κᶜ=κᶜ, N²=N²)
 
 simulation.output_writers[:fields] = JLD2OutputWriter(model, outputs;
                                                       filename,
@@ -93,8 +100,8 @@ function progress(sim)
                    iteration(sim), prettytime(sim),
                    maximum(abs, u), maximum(abs, v), maximum(abs, w))
 
-    msg *= @sprintf(", extrema(e): (%6.2e, %6.2e) m² s⁻²", minimum(e), maximum(e))
-    msg *= @sprintf(", extrema(κᶜ): (%6.2e, %6.2e) m² s⁻²", minimum(κᶜ), maximum(κᶜ))
+    msg *= @sprintf(", max(e): %6.2e m² s⁻²", maximum(e))
+    msg *= @sprintf(", max(κᶜ): %6.2e m² s⁻¹", maximum(κᶜ))
 
     @info msg
     
@@ -110,10 +117,11 @@ e_ts = FieldTimeSeries(filename, "e")
 u_ts = FieldTimeSeries(filename, "u")
 v_ts = FieldTimeSeries(filename, "v")
 w_ts = FieldTimeSeries(filename, "w")
-#κ_ts = FieldTimeSeries(filename, "κᶜ")
+κ_ts = FieldTimeSeries(filename, "κᶜ")
+N_ts = FieldTimeSeries(filename, "N²")
 Nt = length(b_ts.times)
 
-for ψ in (b_ts, e_ts, u_ts, v_ts, w_ts)
+for ψ in (b_ts, e_ts, u_ts, v_ts, w_ts, κ_ts) #, N_ts)
     ψp = parent(ψ)
     ψp[ψp .== 0] .= NaN
 end
@@ -121,16 +129,17 @@ end
 fig = Figure(resolution=(1600, 800))
 
 ax_uyz = Axis(fig[1, 1], title="u(y, z) - <u(y, z)>")
-ax_vyz = Axis(fig[1, 2], title="v(y, z)")
-ax_wyz = Axis(fig[1, 3], title="w(y, z)")
+#ax_vyz = Axis(fig[1, 2], title="v(y, z)")
+ax_wyz = Axis(fig[1, 2], title="w(y, z)")
+ax_Nyz = Axis(fig[1, 3], title="N²(y, z)")
 ax_eyz = Axis(fig[1, 4], title="e(y, z)")
-#ax_κyz = Axis(fig[1, 4], title="κ(y, z)")
+ax_κyz = Axis(fig[1, 5], title="κ(y, z)")
 
-ax_bz = Axis(fig[2, 1], title="b(z)", xlabel="y")
+ax_bz = Axis(fig[2, 1], title="b(z)", xlabel="z")
 ax_uz = Axis(fig[2, 2], title="u(z)", ylabel="z")
 ax_vz = Axis(fig[2, 3], title="v(z)", ylabel="z")
 ax_ez = Axis(fig[2, 4], title="e(z)", ylabel="z")
-#ax_κz = Axis(fig[2, 4], title="κ(z)", ylabel="z")
+ax_κz = Axis(fig[2, 5], title="κ(z)", ylabel="z")
 
 slider = Slider(fig[3, :], range=1:Nt, startvalue=1)
 n = slider.value
@@ -149,7 +158,8 @@ end
 v_yz = @lift interior(v_ts[$n], 1, :, :)
 w_yz = @lift interior(w_ts[$n], 1, :, :)
 w_yz = @lift interior(w_ts[$n], 1, :, :)
-#κ_yz = @lift interior(κ_ts[$n], 1, :, :)
+N_yz = @lift interior(N_ts[$n], 1, :, :)
+κ_yz = @lift interior(κ_ts[$n], 1, :, :)
 
 Nx, Ny, Nz = size(grid)
 
@@ -161,9 +171,9 @@ e_z1 = @lift interior(e_ts[$n], 1, 16, :)
 e_z2 = @lift interior(e_ts[$n], 1, 32, :)
 e_z3 = @lift interior(e_ts[$n], 1, 8, :)
 
-# κ_z1 = @lift interior(κ_ts[$n], 1, 16, :)
-# κ_z2 = @lift interior(κ_ts[$n], 1, 32, :)
-# κ_z3 = @lift interior(κ_ts[$n], 1, 8, :)
+κ_z1 = @lift interior(κ_ts[$n], 1, 16, :)
+κ_z2 = @lift interior(κ_ts[$n], 1, 32, :)
+κ_z3 = @lift interior(κ_ts[$n], 1, 8, :)
 
 u_z1 = @lift interior(u_ts[$n], 1, 16, :)
 u_z2 = @lift interior(u_ts[$n], 1, 32, :)
@@ -174,28 +184,31 @@ v_z2 = @lift interior(v_ts[$n], 1, 32, :)
 v_z3 = @lift interior(v_ts[$n], 1, 8, :)
 
 x, y, z = nodes(b_ts)
-#xκ, yκ, zκ = nodes(κ_ts)
+xκ, yκ, zκ = nodes(κ_ts)
 
-elim = 6e-4
+elim = 1e-4
 ulim = 0.2
 vlim = 2e-2
-wlim = 2e-4
-κlim = 1e1
+wlim = 1e-5
+κlim = 1e-3 # 1e1
 
 heatmap!(ax_eyz, y, z, e_yz, colormap=:solar, colorrange=(0, elim), nan_color=:gray)
 contour!(ax_eyz, y, z, b_yz, levels=15, color=:black)
 
-#heatmap!(ax_κyz, y, zκ κ_yz, colormap=:thermal, colorrange=(0, κlim), nan_color=:gray)
-#contour!(ax_κyz, y, z, b_yz, levels=15, color=:black)
+heatmap!(ax_κyz, y, zκ, κ_yz, colormap=:thermal, colorrange=(0, κlim), nan_color=:gray)
+contour!(ax_κyz, y, z, b_yz, levels=15, color=:black)
 
 heatmap!(ax_uyz, y, z, u_yz, colormap=:balance, colorrange=(-ulim, ulim), nan_color=:gray)
 contour!(ax_uyz, y, z, b_yz, levels=15, color=:black)
 
-heatmap!(ax_vyz, y, z, v_yz, colormap=:balance, colorrange=(-vlim, vlim), nan_color=:gray)
-contour!(ax_vyz, y, z, b_yz, levels=15, color=:black)
+# heatmap!(ax_vyz, y, z, v_yz, colormap=:balance, colorrange=(-vlim, vlim), nan_color=:gray)
+# contour!(ax_vyz, y, z, b_yz, levels=15, color=:black)
 
 heatmap!(ax_wyz, y, z, w_yz, colormap=:balance, colorrange=(-wlim, wlim), nan_color=:gray)
 contour!(ax_wyz, y, z, b_yz, levels=15, color=:black)
+
+heatmap!(ax_Nyz, y, z, N_yz, colormap=:thermal, colorrange=(1e-6, 1e-5), nan_color=:gray)
+contour!(ax_Nyz, y, z, b_yz, levels=15, color=:black)
 
 lines!(ax_bz, b_z1, z)
 lines!(ax_bz, b_z2, z)
@@ -204,6 +217,10 @@ lines!(ax_bz, b_z3, z)
 lines!(ax_ez, e_z1, z)
 lines!(ax_ez, e_z2, z)
 lines!(ax_ez, e_z3, z)
+
+lines!(ax_κz, κ_z1, zκ)
+lines!(ax_κz, κ_z2, zκ)
+lines!(ax_κz, κ_z3, zκ)
 
 lines!(ax_uz, u_z1, z)
 lines!(ax_uz, u_z2, z)
@@ -216,14 +233,20 @@ lines!(ax_vz, v_z3, z)
 xlims!(ax_ez, -elim/10, 2elim)
 xlims!(ax_uz, -2ulim, 2ulim)
 xlims!(ax_vz, -2vlim, 2vlim)
+xlims!(ax_κz, -κlim/10, 2κlim)
+
 ylims!(ax_bz, -1020, 20)
 ylims!(ax_uz, -1020, 20)
 ylims!(ax_vz, -1020, 20)
 ylims!(ax_ez, -1020, 20)
+ylims!(ax_κz, -1020, 20)
 
 display(fig)
 
+#=
 record(fig, filename[1:end-5] * ".mp4", 1:Nt, framerate=24) do nn
     @info "Plotting frame $nn of $Nt..."
     n[] = nn
 end
+=#
+
