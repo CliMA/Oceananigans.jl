92df7cd3b485154328d63d63100c2a237c0c9f93
Gregory Wagner <wagner.greg@gmail.com>
2023-04-21 07:25:46 -0800

    Fix bug in buoyancy implementation

--- src/BuoyancyModels/g_dot_b.jl
+++ src/BuoyancyModels/g_dot_b.jl
@@ -1,7 +1,8 @@
-@inline x_dot_g_b(i, j, k, grid, buoyancy, C) = ĝ_x(buoyancy) * buoyancy_perturbation(i, j, k, grid, buoyancy.model, C)
-@inline y_dot_g_b(i, j, k, grid, buoyancy, C) = ĝ_y(buoyancy) * buoyancy_perturbation(i, j, k, grid, buoyancy.model, C)
-@inline z_dot_g_b(i, j, k, grid, buoyancy, C) = ĝ_z(buoyancy) * buoyancy_perturbation(i, j, k, grid, buoyancy.model, C)
+@inline x_dot_g_b(i, j, k, grid, buoyancy, C) = ĝ_x(buoyancy) * ℑxᶠᵃᵃ(i, j, k, grid, buoyancy_perturbation, buoyancy.model, C)
+@inline y_dot_g_b(i, j, k, grid, buoyancy, C) = ĝ_y(buoyancy) * ℑyᵃᶠᵃ(i, j, k, grid, buoyancy_perturbation, buoyancy.model, C)
+@inline z_dot_g_b(i, j, k, grid, buoyancy, C) = ĝ_z(buoyancy) * ℑzᵃᵃᶠ(i, j, k, grid, buoyancy_perturbation, buoyancy.model, C)
 
 @inline x_dot_g_b(i, j, k, grid,  ::Buoyancy{M, NegativeZDirection}, C) where M = 0
 @inline y_dot_g_b(i, j, k, grid,  ::Buoyancy{M, NegativeZDirection}, C) where M = 0
-@inline z_dot_g_b(i, j, k, grid, b::Buoyancy{M, NegativeZDirection}, C) where M = buoyancy_perturbation(i, j, k, grid, b.model, C)
+@inline z_dot_g_b(i, j, k, grid, b::Buoyancy{M, NegativeZDirection}, C) where M = ℑzᵃᵃᶠ(i, j, k, grid, buoyancy_perturbation, b.model, C)
+
