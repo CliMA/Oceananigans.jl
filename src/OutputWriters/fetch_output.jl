2a117d603eaf5a8afbdf510e3b097048d76693a0
Gregory Wagner <wagner.greg@gmail.com>
2023-04-24 14:49:08 -0800

    Add output for ZeroField

--- src/OutputWriters/fetch_output.jl
+++ src/OutputWriters/fetch_output.jl
@@ -1,6 +1,6 @@
 using CUDA
 
-using Oceananigans.Fields: AbstractField, compute_at!
+using Oceananigans.Fields: AbstractField, compute_at!, ZeroField
 using Oceananigans.LagrangianParticleTracking: LagrangianParticles
 
 # Needed to support `fetch_output` with `model::Nothing`.
@@ -41,3 +41,6 @@ function fetch_and_convert_output(output, model, writer)
     fetched = fetch_output(output, model)
     return convert_output(fetched, writer)
 end
+
+fetch_and_convert_output(output::ZeroField, model, writer) = zero(eltype(output))
+
