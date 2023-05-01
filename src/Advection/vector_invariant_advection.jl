b27d2b7359b35b1fba2c300b6737ec8560b40656
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-05-01 02:07:04 -0400

    try like this

--- src/Advection/vector_invariant_advection.jl
+++ src/Advection/vector_invariant_advection.jl
@@ -241,8 +241,10 @@ end
     Sζ = scheme.vorticity_stencil
 
     @inbounds v̂ = ℑxᶠᵃᵃ(i, j, k, grid, ℑyᵃᶜᵃ, Δx_qᶜᶠᶜ, v) / Δxᶠᶜᶜ(i, j, k, grid) 
-    ζᴸ =  _left_biased_interpolate_yᵃᶜᵃ(i, j, k, grid, scheme.vorticity_scheme, Az_qᶠᶠᶜ, ζ₃ᶠᶠᶜ, Sζ, u, v) / Azᶠᶜᶜ(i, j, k, grid)
-    ζᴿ = _right_biased_interpolate_yᵃᶜᵃ(i, j, k, grid, scheme.vorticity_scheme, Az_qᶠᶠᶜ, ζ₃ᶠᶠᶜ, Sζ, u, v) / Azᶠᶜᶜ(i, j, k, grid)
+    ζᴸ =  _left_biased_interpolate_yᵃᶜᵃ(i, j, k, grid, scheme.vorticity_scheme, ζ₃ᶠᶠᶜ, Sζ, u, v)
+    ζᴿ = _right_biased_interpolate_yᵃᶜᵃ(i, j, k, grid, scheme.vorticity_scheme, ζ₃ᶠᶠᶜ, Sζ, u, v)
+    # ζᴸ =  _left_biased_interpolate_yᵃᶜᵃ(i, j, k, grid, scheme.vorticity_scheme, Az_qᶠᶠᶜ, Sζ, ζ₃ᶠᶠᶜ, u, v) / Azᶠᶜᶜ(i, j, k, grid)
+    # ζᴿ = _right_biased_interpolate_yᵃᶜᵃ(i, j, k, grid, scheme.vorticity_scheme, Az_qᶠᶠᶜ, Sζ, ζ₃ᶠᶠᶜ, u, v) / Azᶠᶜᶜ(i, j, k, grid)
 
     Sδ = scheme.divergence_stencil
     
@@ -258,8 +260,10 @@ end
     Sζ = scheme.vorticity_stencil
 
     @inbounds û  =  ℑyᵃᶠᵃ(i, j, k, grid, ℑxᶜᵃᵃ, Δy_qᶠᶜᶜ, u) / Δyᶜᶠᶜ(i, j, k, grid)
-    ζᴸ =  _left_biased_interpolate_xᶜᵃᵃ(i, j, k, grid, scheme.vorticity_scheme, Az_qᶠᶠᶜ, ζ₃ᶠᶠᶜ, Sζ, u, v) / Azᶜᶠᶜ(i, j, k, grid)
-    ζᴿ = _right_biased_interpolate_xᶜᵃᵃ(i, j, k, grid, scheme.vorticity_scheme, Az_qᶠᶠᶜ, ζ₃ᶠᶠᶜ, Sζ, u, v) / Azᶜᶠᶜ(i, j, k, grid)
+    ζᴸ =  _left_biased_interpolate_xᶜᵃᵃ(i, j, k, grid, scheme.vorticity_scheme, ζ₃ᶠᶠᶜ, Sζ, u, v)
+    ζᴿ = _right_biased_interpolate_xᶜᵃᵃ(i, j, k, grid, scheme.vorticity_scheme, ζ₃ᶠᶠᶜ, Sζ, u, v)
+    # ζᴸ =  _left_biased_interpolate_xᶜᵃᵃ(i, j, k, grid, scheme.vorticity_scheme, Az_qᶠᶠᶜ, Sζ, ζ₃ᶠᶠᶜ, u, v) / Azᶜᶠᶜ(i, j, k, grid)
+    # ζᴿ = _right_biased_interpolate_xᶜᵃᵃ(i, j, k, grid, scheme.vorticity_scheme, Az_qᶠᶠᶜ, Sζ, ζ₃ᶠᶠᶜ, u, v) / Azᶜᶠᶜ(i, j, k, grid)
 
     Sδ = scheme.divergence_stencil
 
