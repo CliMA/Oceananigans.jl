9b0fc62fdbe92fc4a043a8a5609bd6c4f8d77210
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-05-02 10:10:50 -0400

    test

--- src/Advection/vector_invariant_advection.jl
+++ src/Advection/vector_invariant_advection.jl
@@ -159,18 +159,26 @@ const VectorInvariantVerticallyEnergyConserving  = VectorInvariant{<:Any, <:Any,
 
 @inline function advective_momentum_flux_Wu(i, j, k, grid, scheme::VectorInvariant, W, u)
 
-    w̃  =    ℑxᶠᵃᵃ(i, j, k, grid, Az_qᶜᶜᶠ, W)
-    uᴸ =  _left_biased_interpolate_zᵃᵃᶠ(i, j, k, grid, scheme.vertical_scheme, u)
-    uᴿ = _right_biased_interpolate_zᵃᵃᶠ(i, j, k, grid, scheme.vertical_scheme, u)
+    û  =     _symmetric_interpolate_zᵃᵃᶠ(i, j, k, grid, scheme.vertical_scheme, u)
+    w̃ᴸ =   _left_biased_interpolate_xᶠᵃᵃ(i, j, k, grid, scheme.divergence_scheme, Az_qᶜᶜᶠ, W)
+    w̃ᴿ =  _right_biased_interpolate_xᶠᵃᵃ(i, j, k, grid, scheme.divergence_scheme, Az_qᶜᶜᶠ, W)
+    uᴸ =   _left_biased_interpolate_zᵃᵃᶠ(i, j, k, grid, scheme.vertical_scheme, u)
+    uᴿ =  _right_biased_interpolate_zᵃᵃᶠ(i, j, k, grid, scheme.vertical_scheme, u)
+
+    w̃ = upwind_biased_product(û, w̃ᴸ, w̃ᴿ) / abs(û)
 
     return upwind_biased_product(w̃, uᴸ, uᴿ)
 end
 
 @inline function advective_momentum_flux_Wv(i, j, k, grid, scheme::VectorInvariant, W, v)
 
-    w̃  =   ℑyᵃᶠᵃ(i, j, k, grid, Az_qᶜᶜᶠ, W)
-    vᴸ =  _left_biased_interpolate_zᵃᵃᶠ(i, j, k, grid, scheme.vertical_scheme, v)
-    vᴿ = _right_biased_interpolate_zᵃᵃᶠ(i, j, k, grid, scheme.vertical_scheme, v)
+    v̂  =     _symmetric_interpolate_zᵃᵃᶠ(i, j, k, grid, scheme.vertical_scheme, v)
+    w̃ᴸ =   _left_biased_interpolate_xᶠᵃᵃ(i, j, k, grid, scheme.divergence_scheme, Az_qᶜᶜᶠ, W)
+    w̃ᴿ =  _right_biased_interpolate_xᶠᵃᵃ(i, j, k, grid, scheme.divergence_scheme, Az_qᶜᶜᶠ, W)
+    vᴸ =   _left_biased_interpolate_zᵃᵃᶠ(i, j, k, grid, scheme.vertical_scheme, v)
+    vᴿ =  _right_biased_interpolate_zᵃᵃᶠ(i, j, k, grid, scheme.vertical_scheme, v)
+
+    w̃ = upwind_biased_product(v̂, w̃ᴸ, w̃ᴿ) / abs(v̂)
 
     return upwind_biased_product(w̃, vᴸ, vᴿ)
 end
