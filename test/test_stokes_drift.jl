49edd6cdaf5f2e29c72083b542146aa76f1e509f
Tomas Chor <tomaschor@gmail.com>
2023-05-10 13:06:35 -0300

    Update test/test_stokes_drift.jl
    Co-authored-by: Gregory L. Wagner <wagner.greg@gmail.com>

--- test/test_stokes_drift.jl
+++ test/test_stokes_drift.jl
@@ -1,10 +1,10 @@
 include("dependencies_for_runtests.jl")
 
 function instantiate_stokes_drift()
-    ∂t_uˢ(z, t, p) = exp(z/p.vertical_scale) * cos(t)
-    ∂t_vˢ(z, t, p) = exp(z/p.vertical_scale) * cos(t)
-    ∂z_uˢ(z, t, p) = exp(z/p.vertical_scale) * cos(t)
-    ∂z_vˢ(z, t, p) = exp(z/p.vertical_scale) * cos(t)
+    ∂t_uˢ(z, t, h) = exp(z/h) * cos(t)
+    ∂t_vˢ(z, t, h) = exp(z/h) * cos(t)
+    ∂z_uˢ(z, t, h) = exp(z/h) * cos(t)
+    ∂z_vˢ(z, t, h) = exp(z/h) * cos(t)
     stokes_drift = UniformStokesDrift(∂t_uˢ=∂t_uˢ, ∂t_vˢ=∂t_vˢ,
                                       ∂z_uˢ=∂z_uˢ, ∂z_vˢ=∂z_vˢ, parameters=20)
 
