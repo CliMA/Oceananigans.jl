0436f5f12c87d9cc1dd2b6cb23c0b5c085d54646
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-05-13 09:34:31 +1000

    add free surface in set!(::HydrostaticFreeSurfaceModel) docs

--- src/Models/HydrostaticFreeSurfaceModels/set_hydrostatic_free_surface_model.jl
+++ src/Models/HydrostaticFreeSurfaceModels/set_hydrostatic_free_surface_model.jl
@@ -5,24 +5,27 @@ import Oceananigans.Fields: set!
 using Oceananigans.Utils: @apply_regionally, apply_regionally!
 
 """
-    set!(model; kwargs...)
+    set!(model::HydrostaticFreeSurfaceModel; kwargs...)
 
-Set velocity and tracer fields of `model`. The keyword arguments
-`kwargs...` take the form `name=data`, where `name` refers to one of the
-fields of `model.velocities` or `model.tracers`, and the `data` may be an array,
-a function with arguments `(x, y, z)`, or any data type for which a
-`set!(ϕ::AbstractField, data)` function exists.
+Set velocity and tracer fields of `model`. The keyword arguments `kwargs...`
+take the form `name = data`, where `name` refers to one of the fields of either:
+(i) `model.velocities`, (ii) `model.tracers`, or (iii) `model.free_surface.η`,
+and the `data` may be an array, a function with arguments `(x, y, z)`, or any data type
+for which a `set!(ϕ::AbstractField, data)` function exists.
 
 Example
 =======
-```julia
-model = HydrostaticFreeSurfaceModel(grid=RectilinearGrid(size=(32, 32, 32), length=(1, 1, 1))
+
+```jldoctest
+using Oceananigans
+
+model = HydrostaticFreeSurfaceModel(grid = RectilinearGrid(size=(32, 32, 32), extent=(1, 1, 1)))
 
 # Set u to a parabolic function of z, v to random numbers damped
 # at top and bottom, and T to some silly array of half zeros,
 # half random numbers.
 
-u₀(x, y, z) = z/model.grid.Lz * (1 + z/model.grid.Lz)
+u₀(x, y, z) = z / model.grid.Lz * (1 + z / model.grid.Lz)
 v₀(x, y, z) = 1e-3 * rand() * u₀(x, y, z)
 
 T₀ = rand(size(model.grid)...)
