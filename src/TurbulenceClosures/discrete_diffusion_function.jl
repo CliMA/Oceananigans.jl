8ce9429a51ff1c8f7ff352bad9cbc0b24c2782fd
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-06-04 14:16:33 +1000

    move docstring to construction + enhance

--- src/TurbulenceClosures/discrete_diffusion_function.jl
+++ src/TurbulenceClosures/discrete_diffusion_function.jl
@@ -5,46 +5,68 @@ using Oceananigans.Utils: instantiate
     struct DiscreteDiffusionFunction{LX, LY, LZ, P, F} 
 
 A wrapper for a diffusivity functions with optional parameters at a specified locations.
-
-If LX == LY == LZ == nothing the function call requires locations in the signature
-When `parameters=nothing`, the diffusivity `func` is called with the signature
-
-```
-func(i, j, k, grid, lx, ly, lz, clock, model_fields)
-```
-
-where `i, j, k` are the indices,
-where `grid` is `model.grid`, `clock.time` is the current simulation time and
-`clock.iteration` is the current model iteration, and
-`model_fields` is a `NamedTuple` with `u, v, w`, the fields in `model.tracers` and the `model.auxiliary_fields`,
-
-When `parameters` is not `nothing`, `func` is called with the signature
-
-```
-func(i, j, k, grid, lx, ly, lz, clock, model_fields, parameters)
-```
-
-If LX, LY, LZ != (nothing, nothing, nothing) the function call does not require locations in the signature
-and the output will be automatically interpolated on the correct location
-
-without parameters
-```
-func(i, j, k, grid, clock, model_fields)
-```
-with parameters
-```
-func(i, j, k, grid, clock, model_fields, parameters)
-```
 """
 struct DiscreteDiffusionFunction{LX, LY, LZ, P, F} 
     func :: F
     parameters :: P
 
-    function DiscreteDiffusionFunction{LX, LY, LZ}(func::F, parameters::P) where {LX, LY, LZ, F, P}
-        return new{LX, LY, LZ, P, F}(func, parameters)
-    end
+    DiscreteDiffusionFunction{LX, LY, LZ}(func::F, parameters::P) where {LX, LY, LZ, F, P} =
+        new{LX, LY, LZ, P, F}(func, parameters)
 end
 
+"""
+    DiscreteDiffusionFunction(func; parameters, loc)
+
+Return a discrete representation of a diffusivity `func`tion with optional `parameters`
+at specified `loc`ations.
+
+Keyword Arguments
+=================
+
+* `parameters`: A named tuple with parameters used by `func`; default: `nothing`.
+
+* `loc`: A tuple with the locations that the diffusivity `func` is applied on.
+
+  **Without locations**
+
+  If `LX == LY == LZ == nothing` the function call requires locations in the signature.
+  In this case, the diffusivity `func` is called with the signature:
+
+  - Without parameters:
+
+    ```julia
+    func(i, j, k, grid, ℓx, ℓy, ℓz, clock, model_fields)
+    ```
+
+    where `i, j, k` are the indices, `grid` is `model.grid`, `ℓx, ℓy, ℓz` are the
+    instantiated versions of `LX, LY, LZ`, `clock.time` is the current simulation time,
+    `clock.iteration` is the current model iteration, and `model_fields` is a
+    `NamedTuple` with `u, v, w`, the fields in `model.tracers` and the `model.auxiliary_fields`.
+
+  - With `parameters` is not `nothing`, `func` is called with the signature
+
+    ```julia
+    func(i, j, k, grid, ℓx, ℓy, ℓz, clock, model_fields, parameters)
+    ```
+
+  **With locations**
+
+  If `LX, LY, LZ != (nothing, nothing, nothing)` the function call *does not require*
+  locations in the signature and the output is automatically interpolated on the correct
+  location. In this case, the diffusivity `func` is called with the signature:
+
+  1. Without parameters:
+
+    ```julia
+    func(i, j, k, grid, clock, model_fields)
+    ```
+
+  2. With `parameters` is not `nothing`, `func` is called with the signature
+
+    ```julia
+    func(i, j, k, grid, clock, model_fields, parameters)
+    ```
+"""
 function DiscreteDiffusionFunction(func; parameters, loc)
     loc = instantiate.(loc)
     return DiscreteDiffusionFunction{typeof(loc[1]), typeof(loc[2]), typeof(loc[3])}(func, parameters)
@@ -54,22 +76,22 @@ const UnparameterizedDDF{LX, LY, LZ} = DiscreteDiffusionFunction{LX, LY, LZ, <:N
 const UnlocalizedDDF                 = DiscreteDiffusionFunction{<:Nothing, <:Nothing, <:Nothing}
 const UnlocalizedUnparametrizedDDF   = DiscreteDiffusionFunction{<:Nothing, <:Nothing, <:Nothing, <:Nothing}
 
-@inline function getdiffusivity(dd::DiscreteDiffusionFunction{LX, LY, LZ}, 
+@inline function getdiffusivity(dd::DiscreteDiffusionFunction{LX, LY, LZ},
                               i, j, k, grid, location, clock, fields) where {LX, LY, LZ} 
-        from = (LX(), LY(), LZ())
-        return ℑxyz(i, j, k, grid, from, location, dd.func, clock, fields, dd.parameters)
+    from = (LX(), LY(), LZ())
+    return ℑxyz(i, j, k, grid, from, location, dd.func, clock, fields, dd.parameters)
 end
 
-@inline function getdiffusivity(dd::UnparameterizedDDF{LX, LY, LZ}, 
-                              i, j, k, grid, location, clock, fields) where {LX, LY, LZ} 
-        from = (LX(), LY(), LZ())
+@inline function getdiffusivity(dd::UnparameterizedDDF{LX, LY, LZ},
+                              i, j, k, grid, location, clock, fields) where {LX, LY, LZ}
+    from = (LX(), LY(), LZ())
     return ℑxyz(i, j, k, grid, from, location, dd.func, clock, fields)
 end
 
-@inline getdiffusivity(dd::UnlocalizedDDF, i, j, k, grid, location, clock, fields) = 
+@inline getdiffusivity(dd::UnlocalizedDDF, i, j, k, grid, location, clock, fields) =
         dd.func(i, j, k, grid, location..., clock, fields, dd.parameters)
 
-@inline getdiffusivity(dd::UnlocalizedUnparametrizedDDF, i, j, k, grid, location, clock, fields) = 
+@inline getdiffusivity(dd::UnlocalizedUnparametrizedDDF, i, j, k, grid, location, clock, fields) =
         dd.func(i, j, k, grid, location..., clock, fields)
 
 Adapt.adapt_structure(to, dd::DiscreteDiffusionFunction{LX, LY, LZ}) where {LX, LY, LZ} =
