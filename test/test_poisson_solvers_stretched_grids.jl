d4826519d2f3cfefb552548630452753550e9b15
tomchor <tomaschor@gmail.com>
2023-06-19 13:21:39 -0600

    more general tests for poisson solver

--- test/test_poisson_solvers_stretched_grids.jl
+++ test/test_poisson_solvers_stretched_grids.jl
@@ -14,6 +14,9 @@ include("dependencies_for_poisson_solvers.jl")
         (Periodic, Bounded,  Bounded),
         (Bounded,  Periodic, Bounded),
         (Bounded,  Bounded,  Bounded),
+        (Periodic, Bounded,  Periodic),
+        (Bounded,  Periodic, Periodic),
+        (Bounded,  Bounded,  Periodic),
         (Flat,     Bounded,  Bounded),
         (Flat,     Periodic, Bounded),
         (Bounded,  Flat,     Bounded),
@@ -25,20 +28,21 @@ include("dependencies_for_poisson_solvers.jl")
 
             faces_even = [1, 2, 4, 7, 11, 16, 22, 29, 37]      # Nz = 8
             faces_odd  = [1, 2, 4, 7, 11, 16, 22, 29, 37, 51]  # Nz = 9
-            for irregular_axis in (1, 2, 3)
-                if topo[irregular_axis] == Bounded
-                    @info "  Testing stretched Poisson solver [FACR, $(typeof(arch)), $topo, $irregular_axis]..."
-                    @test stretched_poisson_solver_correct_answer(Float64, arch, topo, 8, 8, 1:8; irregular_axis)
-                    @test stretched_poisson_solver_correct_answer(Float64, arch, topo, 7, 7, 1:7; irregular_axis)
-                    @test stretched_poisson_solver_correct_answer(Float32, arch, topo, 8, 8, 1:8; irregular_axis)
+            for stretched_axis in (3,)
+                if topo[stretched_axis] == Bounded
+                    @info "  Testing stretched Poisson solver [FACR, $(typeof(arch)), $topo, stretched axis = $stretched_axis]..."
+                    @test stretched_poisson_solver_correct_answer(Float64, arch, topo, 4, 5, 1:4; stretched_axis)
+                    @test stretched_poisson_solver_correct_answer(Float64, arch, topo, 8, 8, 1:8; stretched_axis)
+                    @test stretched_poisson_solver_correct_answer(Float64, arch, topo, 7, 7, 1:7; stretched_axis)
+                    @test stretched_poisson_solver_correct_answer(Float32, arch, topo, 8, 8, 1:8; stretched_axis)
 
                     for faces in [faces_even, faces_odd]
-                        @test stretched_poisson_solver_correct_answer(Float64, arch, topo, 8,  8, faces; irregular_axis)
-                        @test stretched_poisson_solver_correct_answer(Float64, arch, topo, 16, 8, faces; irregular_axis)
-                        @test stretched_poisson_solver_correct_answer(Float64, arch, topo, 8, 16, faces; irregular_axis)
-                        @test stretched_poisson_solver_correct_answer(Float64, arch, topo, 8, 11, faces; irregular_axis)
-                        @test stretched_poisson_solver_correct_answer(Float64, arch, topo, 5,  8, faces; irregular_axis)
-                        @test stretched_poisson_solver_correct_answer(Float64, arch, topo, 7, 13, faces; irregular_axis)
+                        @test stretched_poisson_solver_correct_answer(Float64, arch, topo, 8,  8, faces; stretched_axis)
+                        @test stretched_poisson_solver_correct_answer(Float64, arch, topo, 16, 8, faces; stretched_axis)
+                        @test stretched_poisson_solver_correct_answer(Float64, arch, topo, 8, 16, faces; stretched_axis)
+                        @test stretched_poisson_solver_correct_answer(Float64, arch, topo, 8, 11, faces; stretched_axis)
+                        @test stretched_poisson_solver_correct_answer(Float64, arch, topo, 5,  8, faces; stretched_axis)
+                        @test stretched_poisson_solver_correct_answer(Float64, arch, topo, 7, 13, faces; stretched_axis)
                     end
                 end
             end
