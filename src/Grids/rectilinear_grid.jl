87e3d770cf6b4502a891dbc587e1036968f36cb2
Tomas Chor <tomaschor@gmail.com>
2023-06-20 16:10:07 -0700

    Allowing `NonhydrostaticModel` with grids that are irregular in either `x` or `y` (#3111)
    * add slow but general method for getting the diagonals
    
    * dumb but fast way
    
    * generalized FourierTridiagonalPoissonSolver constructor function
    
    * generalized set_source_term! function
    
    * Update src/Solvers/fourier_tridiagonal_poisson_solver.jl
    
    * small cleanup
    
    * generalize fourier tridiagonal poisson solver and
    
    * Update src/Models/NonhydrostaticModels/NonhydrostaticModels.jl
    
    Co-authored-by: Gregory L. Wagner <wagner.greg@gmail.com>
    
    * bugfix
    
    * get correction directions in plan_transforms
    
    * m, n, q, -> i, j, k
    
    * better error message
    
    * test poisson solver in x, y, and z irregularly spaced grids
    
    * rename file
    
    * correct source term calculation
    
    * try to fix plan_transforms for GPUs
    
    * better comments
    
    * reshape storage twice
    
    * irregular_dimension -> stretched_dimensions
    
    * renaming for clarity
    
    * irregular -> stretched
    
    * attempt at fixing plan_transforms
    
    * more general tests for poisson solver
    
    * rework dependencies_for_poisson_solvers
    
    * vs_grid -> stretched_grid
    
    * polish wording
    
    * remove unnecessary statement
    
    * no need for getindex
    
    * no need for tridiagonal_direction in FourierTridiagSolver
    
    * size(grid)[dim] -> size(grid, dim)
    
    * Revert "no need for tridiagonal_direction in FourierTridiagSolver"
    
    This reverts commit 8f6c5410fab5183f0773528d6217156c7819ce58.
    
    * avoid calls to StretchedDirection(grid)
    
    * remove tridiagonal_direction from FourierTrdiagSolver again
    
    ---------
    
    Co-authored-by: Gregory L. Wagner <wagner.greg@gmail.com>

--- src/Grids/rectilinear_grid.jl
+++ src/Grids/rectilinear_grid.jl
@@ -63,6 +63,11 @@ regular_dimensions(::XZRegRectilinearGrid) = (1, 3)
 regular_dimensions(::YZRegRectilinearGrid) = (2, 3)
 regular_dimensions(::RegRectilinearGrid)   = (1, 2, 3)
 
+stretched_dimensions(::YZRegRectilinearGrid) = tuple(1)
+stretched_dimensions(::XZRegRectilinearGrid) = tuple(2)
+stretched_dimensions(::XYRegRectilinearGrid) = tuple(3)
+
+
 """
     RectilinearGrid([architecture = CPU(), FT = Float64];
                     size,
