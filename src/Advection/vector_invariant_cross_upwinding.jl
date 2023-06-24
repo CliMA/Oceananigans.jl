e2ea6455249f53e057de271786af5bd41b550398
Simone Silvestri <silvestri.simone0@gmail.com>
2023-06-24 12:03:24 +0100

    Update src/Advection/vector_invariant_cross_upwinding.jl
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>

--- src/Advection/vector_invariant_cross_upwinding.jl
+++ src/Advection/vector_invariant_cross_upwinding.jl
@@ -9,7 +9,7 @@ const VectorInvariantCrossVerticalUpwinding = VectorInvariant{<:Any, <:Any, <:An
 
 ##### 
 ##### Due to the presence of cross derivative terms that generate excessive noise and result in 
-##### numerical instabilities, it is not possible to perform a complete upwinding of the Kinetic 
+##### numerical instabilities, it is undesirable to perform a complete upwinding of the Kinetic 
 ##### Energy gradient. Consequently, the `OnlySelfUpwinding` scheme is implemented for the Kinetic 
 ##### Energy gradient in the case of `CrossAndSelfUpwinding`.
 ##### For details on the implementation refer to the file `vector_invariant_self_upwinding.jl` 
