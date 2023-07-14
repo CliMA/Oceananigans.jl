d1d1b05bb8aa64cb8b54940273135ae05c34ad1c
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-07-14 14:06:00 +0300

    update Julia version warning

--- src/Oceananigans.jl
+++ src/Oceananigans.jl
@@ -4,8 +4,8 @@ data-driven, ocean-flavored fluid dynamics on CPUs and GPUs.
 """
 module Oceananigans
 
-if VERSION < v"1.8"
-    @warn "Oceananigans is tested on Julia v1.8 and therefore it is strongly recommended you run Oceananigans on Julia v1.8 or newer."
+if VERSION < v"1.9"
+    @warn "Oceananigans is tested on Julia v1.9 and therefore it is strongly recommended you run Oceananigans on Julia v1.9 or newer."
 end
 
 export
