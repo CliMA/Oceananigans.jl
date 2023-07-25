45bdebca3955b91a5035d13859a1bc994e19c36d
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-07-25 19:40:22 -0400

    small bugfix

--- src/MultiRegion/multi_region_field.jl
+++ src/MultiRegion/multi_region_field.jl
@@ -117,6 +117,7 @@ set!(mrf::MultiRegionField, v)  = apply_regionally!(set!,  mrf, v)
 fill!(mrf::MultiRegionField, v) = apply_regionally!(fill!, mrf, v)
 
 set!(mrf::MultiRegionField, f::Function)  = apply_regionally!(set!, mrf, f)
+set!(u::MultiRegionField, v::MultiRegionField)  = apply_regionally!(set!, u, v)
 
 compute_at!(mrf::GriddedMultiRegionField, time)  = apply_regionally!(compute_at!, mrf, time)
 compute_at!(mrf::MultiRegionComputedField, time) = apply_regionally!(compute_at!, mrf, time)
