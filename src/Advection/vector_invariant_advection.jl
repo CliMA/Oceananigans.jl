9e1728fce3a3096f5abcea0dbdb882a393bc1cc9
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-08-22 09:08:20 -0400

    change

--- src/Advection/vector_invariant_advection.jl
+++ src/Advection/vector_invariant_advection.jl
@@ -300,10 +300,7 @@ const UZ{N} = UpwindBiased{N, <:Any, <:Any, <:Any, <:Nothing}
 for buffer in 1:6
     @eval begin
         @inline inner_symmetric_biased_interpolate_xᶠᵃᵃ(i, j, k, grid, scheme::Centered{$buffer}, f::Function, idx, loc, VI::AbstractSmoothnessStencil, args...) = inner_symmetric_biased_interpolate_xᶠᵃᵃ(i, j, k, grid, scheme, f, idx, loc, args...)
-        @inline inner_symmetric_biased_interpolate_xᶠᵃᵃ(i, j, k, grid, scheme::Centered{$buffer}, f::Function, idx, loc, VI::AbstractSmoothnessStencil, args...) = inner_symmetric_biased_interpolate_xᶠᵃᵃ(i, j, k, grid, scheme, f, idx, loc, args...)
         @inline inner_symmetric_biased_interpolate_yᵃᶠᵃ(i, j, k, grid, scheme::Centered{$buffer}, f::Function, idx, loc, VI::AbstractSmoothnessStencil, args...) = inner_symmetric_biased_interpolate_yᵃᶠᵃ(i, j, k, grid, scheme, f, idx, loc, args...)
-        @inline inner_symmetric_biased_interpolate_yᵃᶠᵃ(i, j, k, grid, scheme::Centered{$buffer}, f::Function, idx, loc, VI::AbstractSmoothnessStencil, args...) = inner_symmetric_biased_interpolate_yᵃᶠᵃ(i, j, k, grid, scheme, f, idx, loc, args...)
-        @inline inner_symmetric_biased_interpolate_zᵃᵃᶠ(i, j, k, grid, scheme::Centered{$buffer}, f::Function, idx, loc, VI::AbstractSmoothnessStencil, args...) = inner_symmetric_biased_interpolate_zᵃᵃᶠ(i, j, k, grid, scheme, f, idx, loc, args...)
         @inline inner_symmetric_biased_interpolate_zᵃᵃᶠ(i, j, k, grid, scheme::Centered{$buffer}, f::Function, idx, loc, VI::AbstractSmoothnessStencil, args...) = inner_symmetric_biased_interpolate_zᵃᵃᶠ(i, j, k, grid, scheme, f, idx, loc, args...)
 
         @inline inner_left_biased_interpolate_xᶠᵃᵃ(i, j, k, grid, scheme::U{$buffer},  f::Function, idx, loc, VI::AbstractSmoothnessStencil, args...) = inner_left_biased_interpolate_xᶠᵃᵃ(i, j, k, grid, scheme, f, idx, loc, args...)
