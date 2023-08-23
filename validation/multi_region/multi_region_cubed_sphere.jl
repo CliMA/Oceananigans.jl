0764ff2f84323a19bef9e3b8000a88fc0483d4a6
Sid <siddhartha.bishnu@gmail.com>
2023-08-23 16:15:26 +0530

    Removed plots of specific velocity profiles
    Removed plots of velocity profiles corresponding to a constant field and solid
    body rotation.

--- validation/multi_region/multi_region_cubed_sphere.jl
+++ validation/multi_region/multi_region_cubed_sphere.jl
@@ -78,18 +78,26 @@ end
 heatlatlon!(ax::Axis, field::CubedSphereField, k=1; kwargs...)  = apply_regionally!(heatlatlon!, ax, field, k; kwargs...)
 heatsphere!(ax::Axis3, field::CubedSphereField, k=1; kwargs...) = apply_regionally!(heatsphere!, ax, field, k; kwargs...)
 
-function panel_wise_visualization(profile, field, field_name, k; # Here k represents the vertical index of the field.
-                                  hide_decorations = false, extrema_reduction_factor = 0.75)
-
+function panel_wise_visualization(field_type, field, field_name, function_type, k; # Here k represents the vertical index of the field.
+                                  hide_decorations = false, extrema_reduction_factor = 1.0)
+    
+    if field_name == "c"
+        title_part = "cca"
+    elseif field_name == "u"
+        title_part = "fca"
+    elseif field_name == "v"
+        title_part = "cfa"
+    end
+    
     # Create figure.
-    fig = Figure(resolution = (1650, 1200))
+    fig = Figure(resolution = (1800, 1200))
     colorrange = (minimum(field) * extrema_reduction_factor, maximum(field) * extrema_reduction_factor)
-    colormap = :YlOrRd_9
+    colormap = :balance
     
     # Plot Panel 1.
     ax_1 = Axis(fig[3,1]; xlabel = "Local x direction", ylabel = "Local y direction", xlabelsize = 22.5, 
                 ylabelsize = 22.5, xticklabelsize = 17.5, yticklabelsize = 17.5, xlabelpadding = 10, ylabelpadding = 10, 
-                aspect = 1.0, title = "Panel 1", titlesize = 27.5, titlegap = 15, titlefont = :bold)
+                aspect = 1.0, title = "Panel 1: " * title_part, titlesize = 27.5, titlegap = 15, titlefont = :bold)
     if hide_decorations
         hidedecorations!(ax_1)
     end
@@ -99,7 +107,7 @@ function panel_wise_visualization(profile, field, field_name, k; # Here k repres
     # Plot Panel 2.
     ax_2 = Axis(fig[3,3]; xlabel = "Local x direction", ylabel = "Local y direction", xlabelsize = 22.5, 
                 ylabelsize = 22.5, xticklabelsize = 17.5, yticklabelsize = 17.5, xlabelpadding = 10, ylabelpadding = 10, 
-                aspect = 1.0, title = "Panel 2", titlesize = 27.5, titlegap = 15, titlefont = :bold)
+                aspect = 1.0, title = "Panel 2: " * title_part, titlesize = 27.5, titlegap = 15, titlefont = :bold)
     if hide_decorations
         hidedecorations!(ax_2)
     end
@@ -109,7 +117,7 @@ function panel_wise_visualization(profile, field, field_name, k; # Here k repres
     # Plot Panel 3.
     ax_3 = Axis(fig[2,3]; xlabel = "Local x direction", ylabel = "Local y direction", xlabelsize = 22.5, 
                 ylabelsize = 22.5, xticklabelsize = 17.5, yticklabelsize = 17.5, xlabelpadding = 10, ylabelpadding = 10, 
-                aspect = 1.0, title = "Panel 3", titlesize = 27.5, titlegap = 15, titlefont = :bold)
+                aspect = 1.0, title = "Panel 3: " * title_part, titlesize = 27.5, titlegap = 15, titlefont = :bold)
     if hide_decorations
         hidedecorations!(ax_3)
     end
@@ -119,7 +127,7 @@ function panel_wise_visualization(profile, field, field_name, k; # Here k repres
     # Plot Panel 4.
     ax_4 = Axis(fig[2,5]; xlabel = "Local x direction", ylabel = "Local y direction", xlabelsize = 22.5, 
                 ylabelsize = 22.5, xticklabelsize = 17.5, yticklabelsize = 17.5, xlabelpadding = 10, ylabelpadding = 10, 
-                aspect = 1.0, title = "Panel 4", titlesize = 27.5, titlegap = 15, titlefont = :bold)
+                aspect = 1.0, title = "Panel 4: " * title_part, titlesize = 27.5, titlegap = 15, titlefont = :bold)
     if hide_decorations
         hidedecorations!(ax_4)
     end
@@ -129,7 +137,7 @@ function panel_wise_visualization(profile, field, field_name, k; # Here k repres
     # Plot Panel 5.
     ax_5 = Axis(fig[1,5]; xlabel = "Local x direction", ylabel = "Local y direction", xlabelsize = 22.5, 
                 ylabelsize = 22.5, xticklabelsize = 17.5, yticklabelsize = 17.5, xlabelpadding = 10, ylabelpadding = 10, 
-                aspect = 1.0, title = "Panel 5", titlesize = 27.5, titlegap = 15, titlefont = :bold)
+                aspect = 1.0, title = "Panel 5: " * title_part, titlesize = 27.5, titlegap = 15, titlefont = :bold)
     if hide_decorations
         hidedecorations!(ax_5)
     end
@@ -139,7 +147,7 @@ function panel_wise_visualization(profile, field, field_name, k; # Here k repres
     # Plot Panel 6.
     ax_6 = Axis(fig[1,7]; xlabel = "Local x direction", ylabel = "Local y direction", xlabelsize = 22.5, 
                 ylabelsize = 22.5, xticklabelsize = 17.5, yticklabelsize = 17.5, xlabelpadding = 10, ylabelpadding = 10, 
-                aspect = 1.0, title = "Panel 6", titlesize = 27.5, titlegap = 15, titlefont = :bold)
+                aspect = 1.0, title = "Panel 6: " * title_part, titlesize = 27.5, titlegap = 15, titlefont = :bold)
     if hide_decorations
         hidedecorations!(ax_6)
     end
@@ -147,12 +155,13 @@ function panel_wise_visualization(profile, field, field_name, k; # Here k repres
     Colorbar(fig[1,8], hm_6)    
     
     # Save figure.
-    figure_name = profile * "_panel_wise_visualization_" * field_name * "_k_" * string(k) * ".png"
+    figure_name = field_type * "_panel_wise_visualization_" * field_name * "_" * function_type * ".png"
     save(figure_name, fig) 
     
 end
 
-function multi_region_cubed_sphere_plots()
+function multi_region_cubed_sphere_latitude_longitude_plots(coordinate_type, function_type, 
+                                                            set_function_fill_halos)
 
     Nx, Ny, Nz = 5, 5, 1
     cubed_sphere_radius = 1
@@ -161,149 +170,132 @@ function multi_region_cubed_sphere_plots()
     Hx, Hy, Hz = grid.Hx, grid.Hy, grid.Hz
     
     c = CenterField(grid)
-     
-    initial_tracer_profile = "latitude"
-    # Choose initial_tracer_profile to be "latitude" or "longitude".
-    
-    if initial_tracer_profile == "latitude"
+    u = XFaceField(grid)
+    v = YFaceField(grid)
+
+    if coordinate_type == "latitude"
         
-        set!(c, (λ, φ, z) -> φ)
+        if function_type == "set"
+        
+            set!(c, (λ, φ, z) -> φ)
+            set!(u, (λ, φ, z) -> φ)
+            set!(v, (λ, φ, z) -> φ)
+            
+            if set_function_fill_halos
+                fill_halo_regions!(c)
+                fill_halo_regions!(u)
+                fill_halo_regions!(v)
+            end
+            
+        elseif function_type == "nodes"
+        
+            for region in 1:6
+                for i in 1-Hx:Nx+Hx, j in 1-Hy:Ny+Hy
+                    getregion(c, region).data[i, j, 1] = φnodes(getregion(grid, region), Center(), Center(), Center();
+                                                                with_halos=true)[i, j, 1]
+                    getregion(u, region).data[i, j, 1] = φnodes(getregion(grid, region), Face(), Center(), Center(); 
+                                                                with_halos=true)[i, j, 1]
+                    getregion(v, region).data[i, j, 1] = φnodes(getregion(grid, region), Center(), Face(), Center(); 
+                                                                with_halos=true)[i, j, 1]
+                end
+            end
+            
+        end
+
         colorrange = (-90, 90)
         
-    elseif initial_tracer_profile == "longitude"
+    elseif coordinate_type == "longitude"
     
-        set!(c, (λ, φ, z) -> λ)
+        if function_type == "set"
+        
+            set!(c, (λ, φ, z) -> λ)
+            set!(u, (λ, φ, z) -> λ)
+            set!(v, (λ, φ, z) -> λ)
+            
+            if set_function_fill_halos
+                fill_halo_regions!(c)
+                fill_halo_regions!(u)
+                fill_halo_regions!(v)
+            end
+            
+        elseif function_type == "nodes"
+        
+            for region in 1:6
+                for i in 1-Hx:Nx+Hx, j in 1-Hy:Ny+Hy
+                    getregion(c, region).data[i, j, 1] = λnodes(getregion(grid, region), Center(), Center(), Center();
+                                                                with_halos=true)[i, j, 1]
+                    getregion(u, region).data[i, j, 1] = λnodes(getregion(grid, region), Face(), Center(), Center(); 
+                                                                with_halos=true)[i, j, 1]
+                    getregion(v, region).data[i, j, 1] = λnodes(getregion(grid, region), Center(), Face(), Center(); 
+                                                                with_halos=true)[i, j, 1]
+                end
+            end
+            
+        end
+
         colorrange = (-180, 180)
         
     end
     
+    k = grid.region_grids[1].Hz + (Nz + 1)÷2
+    
+    panel_wise_visualization(coordinate_type, c, "c", function_type, k)
+    panel_wise_visualization(coordinate_type, u, "u", function_type, k)
+    panel_wise_visualization(coordinate_type, v, "v", function_type, k)
+    
     colormap = :balance
-    fill_halo_regions!(c)
     
     fig = Figure()
     ax = Axis3(fig[1, 1], aspect=(1, 1, 1), limits=((-1, 1), (-1, 1), (-1, 1)))
     heatsphere!(ax, c; colorrange, colormap)
-    save(initial_tracer_profile * "_heatsphere_c" * ".png", fig)
+    save(coordinate_type * "_heatsphere_c" * "_" * function_type * ".png", fig)
     
     fig = Figure()
     ax = Axis(fig[1, 1])
     heatlatlon!(ax, c; colorrange, colormap)
-    save(initial_tracer_profile * "_heatlatlon_c" * ".png", fig)
+    save(coordinate_type * "_heatlatlon_c" * "_" * function_type * ".png", fig)
 
     fig = Figure(resolution = (1200, 600))
     ax = GeoAxis(fig[1, 1], coastlines = true, lonlims = automatic)
     heatlatlon!(ax, c; colorrange, colormap)
-    save(initial_tracer_profile * "_geolatlon_c" * ".png", fig)
-    
-    k = grid.region_grids[1].Hz + (Nz + 1)÷2
-    panel_wise_visualization(initial_tracer_profile, c, "c", k)
-    
-    u = XFaceField(grid)
-    v = YFaceField(grid)
-    
-    initial_velocity_profile = "latitude"
-    # Choose initial_velocity_profile to be "latitude" or "longitude" or "constant" or "solid_body_rotation".
-    
-    if initial_velocity_profile == "latitude"
-    
-        for region in 1:6
-        
-            for i in 1-Hx:Nx+Hx, j in 1-Hy:Ny+Hy
-                getregion(u, region).data[i, j, 1] = φnodes(getregion(grid, region), Face(), Center(), Center(); 
-                                                            with_halos=true)[i, j, 1]
-                getregion(v, region).data[i, j, 1] = φnodes(getregion(grid, region), Center(), Face(), Center(); 
-                                                            with_halos=true)[i, j, 1]
-            end
-    
-        end
-
-        colorrange = (-90, 90)
-        
-    elseif initial_velocity_profile == "longitude"
-    
-        for region in 1:6
-        
-            for i in 1-Hx:Nx+Hx, j in 1-Hy:Ny+Hy
-                getregion(u, region).data[i, j, 1] = λnodes(getregion(grid, region), Face(), Center(), Center(); 
-                                                            with_halos=true)[i, j, 1]
-                getregion(v, region).data[i, j, 1] = λnodes(getregion(grid, region), Center(), Face(), Center(); 
-                                                            with_halos=true)[i, j, 1]
-            end
-    
-        end
-
-        colorrange = (-180, 180)
-        
-    elseif initial_velocity_profile == "constant"
-    
-        u₀ = 10
-        v₀ = 100
-        U(λ, φ, z) = u₀ 
-        V(λ, φ, z) = v₀
-        u₀_v₀_abs_max = max(abs(u₀), abs(v₀))
-        colorrange = (-u₀_v₀_abs_max, u₀_v₀_abs_max)
-    
-    elseif initial_velocity_profile == "solid_body_rotation"
-    
-        time_period = 10
-        u_advection = (2π * cubed_sphere_radius)/time_period
-        U(λ, φ, z) =   u_advection * cosd(λ) * sind(φ)
-        V(λ, φ, z) = - u_advection * sind(λ)
-        colorrange = (-u_advection, u_advection)
-        
-    end
-    
-    if !(initial_velocity_profile == "latitude" || initial_velocity_profile == "longitude")
-    
-        set!(u, U)
-        set!(v, V)
-        
-        for _ in 1:2
-            fill_halo_regions!(u)
-            fill_halo_regions!(v)
-            @apply_regionally replace_horizontal_velocity_halos!((; u = u, v = v, w = nothing), grid)
-        end
-        
-    end
+    save(coordinate_type * "_geolatlon_c" * "_" * function_type * ".png", fig)
     
     fig = Figure()
     ax = Axis3(fig[1, 1], aspect=(1, 1, 1), limits=((-1, 1), (-1, 1), (-1, 1)))
     heatsphere!(ax, u; colorrange, colormap)
-    save(initial_velocity_profile * "_heatsphere_u" * ".png", fig)
+    save(coordinate_type * "_heatsphere_u" * "_" * function_type * ".png", fig)
     
     fig = Figure()
     ax = Axis(fig[1, 1])
     heatlatlon!(ax, u; colorrange, colormap)
-    save(initial_velocity_profile * "_heatlatlon_u" * ".png", fig)
+    save(coordinate_type * "_heatlatlon_u" * "_" * function_type * ".png", fig)
 
     fig = Figure(resolution = (1200, 600))
     ax = GeoAxis(fig[1, 1], coastlines = true, lonlims = automatic)
     heatlatlon!(ax, u; colorrange, colormap)
-    save(initial_velocity_profile * "_geolatlon_u" * ".png", fig)
+    save(coordinate_type * "_geolatlon_u" * "_" * function_type * ".png", fig)
     
     fig = Figure()
     ax = Axis3(fig[1, 1], aspect=(1, 1, 1), limits=((-1, 1), (-1, 1), (-1, 1)))
     heatsphere!(ax, v; colorrange, colormap)
-    save(initial_velocity_profile * "_heatsphere_v" * ".png", fig)
+    save(coordinate_type * "_heatsphere_v" * "_" * function_type * ".png", fig)
     
     fig = Figure()
     ax = Axis(fig[1, 1])
     heatlatlon!(ax, v; colorrange, colormap)
-    save(initial_velocity_profile * "_heatlatlon_v" * ".png", fig)
+    save(coordinate_type * "_heatlatlon_v" * "_" * function_type * ".png", fig)
 
     fig = Figure(resolution = (1200, 600))
     ax = GeoAxis(fig[1, 1], coastlines = true, lonlims = automatic)
     heatlatlon!(ax, v; colorrange, colormap)
-    save(initial_velocity_profile * "_geolatlon_v" * ".png", fig)  
-    
-    k = grid.region_grids[1].Hz + (Nz + 1)÷2
-    
-    panel_wise_visualization(initial_velocity_profile, u, "u", k)
-    panel_wise_visualization(initial_velocity_profile, v, "v", k) 
+    save(coordinate_type * "_geolatlon_v" * "_" * function_type * ".png", fig)  
     
 end
 
 test_multi_region_cubed_sphere_plots = false
 if test_multi_region_cubed_sphere_plots
-    multi_region_cubed_sphere_plots()
+    coordinate_type = "longitude" # Choose between "latitude" and "longitude".
+    function_type = "nodes" # Choose between "set" and "nodes".
+    set_function_fill_halos = false # Choose between true and false.
+    multi_region_cubed_sphere_latitude_longitude_plots(coordinate_type, function_type, set_function_fill_halos)
 end
