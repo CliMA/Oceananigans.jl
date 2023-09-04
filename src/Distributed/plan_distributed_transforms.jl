1a303545dc01f10255bd5a40fe133a1725ef379d
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-09-04 18:51:03 -0400

    correction

--- src/Distributed/plan_distributed_transforms.jl
+++ src/Distributed/plan_distributed_transforms.jl
@@ -1,28 +1,31 @@
+using Oceananigans.Architectures: array_type
 using Oceananigans.Solvers: plan_forward_transform, plan_backward_transform, DiscreteTransform
 using Oceananigans.Solvers: Forward, Backward
 
 @inline reshaped_size(grid) = size(grid, 2), size(grid, 1), size(grid, 3)
 
-function plan_distributed_transforms(global_grid::RegRectilinearGrid, storage::ParallelFields, planner_flag)
-    TX, TY, TZ = topo = topology(global_grid)
+function plan_distributed_transforms(global_grid, storage::ParallelFields, planner_flag)
+    topo = topology(global_grid)
     arch = architecture(global_grid)
 
     grids = (storage.zfield.grid, storage.yfield.grid, storage.xfield.grid)
 
     Ny = reshaped_size(grids[2])
-    rs_storage = reshape(interior(storage.yfield), Ny)
+    AT = array_type(arch)
 
-    forward_plan_x  = plan_forward_transform(interior(storage.xfield),  topo[1](), [1], planner_flag)
-    forward_plan_z  = plan_forward_transform(interior(storage.zfield),  topo[3](), [3], planner_flag)
-    backward_plan_x = plan_backward_transform(interior(storage.xfield), topo[1](), [1], planner_flag)
-    backward_plan_z = plan_backward_transform(interior(storage.zfield), topo[3](), [3], planner_flag)
+    rs_storage = reshape(AT(interior(storage.yfield), Ny))
+
+    forward_plan_x  = plan_forward_transform(AT(interior(storage.xfield)),  topo[1](), [1], planner_flag)
+    forward_plan_z  = plan_forward_transform(AT(interior(storage.zfield)),  topo[3](), [3], planner_flag)
+    backward_plan_x = plan_backward_transform(AT(interior(storage.xfield)), topo[1](), [1], planner_flag)
+    backward_plan_z = plan_backward_transform(AT(interior(storage.zfield)), topo[3](), [3], planner_flag)
 
     if arch isa GPU
         forward_plan_y  = plan_forward_transform(rs_storage,  topo[2](), [1], planner_flag) 
         backward_plan_y = plan_backward_transform(rs_storage, topo[2](), [1], planner_flag) 
     else
-        forward_plan_y  = plan_forward_transform(interior(storage.yfield),  topo[2](), [2], planner_flag) 
-        backward_plan_y = plan_backward_transform(interior(storage.yfield), topo[2](), [2], planner_flag) 
+        forward_plan_y  = plan_forward_transform(AT(interior(storage.yfield)),  topo[2](), [2], planner_flag) 
+        backward_plan_y = plan_backward_transform(AT(interior(storage.yfield)), topo[2](), [2], planner_flag) 
     end
 
     forward_operations = (
