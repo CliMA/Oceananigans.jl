a5a5aa0657df6bb91bbc16271161a9dd5928e5c9
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-09-04 19:30:25 -0400

    removed

--- src/Models/NonhydrostaticModels/solve_for_pressure.jl
+++ src/Models/NonhydrostaticModels/solve_for_pressure.jl
@@ -3,14 +3,9 @@ using Oceananigans.Solvers: FFTBasedPoissonSolver, FourierTridiagonalPoissonSolv
 using Oceananigans.Distributed: DistributedFFTBasedPoissonSolver
 using Oceananigans.Grids: XDirection, YDirection, ZDirection
 
-using PencilArrays: Permutation
-
 #####
 ##### Calculate the right-hand-side of the non-hydrostatic pressure Poisson equation.
 #####
-
-const ZXYPermutation = Permutation{(3, 1, 2), 3}
-const ZYXPermutation = Permutation{(3, 2, 1), 3}
 
 @kernel function calculate_pressure_source_term_fft_based_solver!(rhs, grid, Δt, U★)
     i, j, k = @index(Global, NTuple)
