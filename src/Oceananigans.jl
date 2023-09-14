41fb5ce3ca01677496c04350c884e64c3d1536f4
Gregory Wagner <wagner.greg@gmail.com>
2023-09-14 09:46:05 -0600

    Generate TODO to refactor multi region to appear before Models

--- src/Oceananigans.jl
+++ src/Oceananigans.jl
@@ -201,6 +201,11 @@ include("AbstractOperations/AbstractOperations.jl")
 include("Advection/Advection.jl")
 include("Solvers/Solvers.jl")
 
+# TODO: move here
+#include("ImmersedBoundaries/ImmersedBoundaries.jl")
+#include("Distributed/Distributed.jl")
+#include("MultiRegion/MultiRegion.jl")
+
 # Physics, time-stepping, and models
 include("Coriolis/Coriolis.jl")
 include("BuoyancyModels/BuoyancyModels.jl")
@@ -209,8 +214,10 @@ include("TurbulenceClosures/TurbulenceClosures.jl")
 include("Forcings/Forcings.jl")
 include("Biogeochemistry.jl")
 
+# TODO: move above
 include("ImmersedBoundaries/ImmersedBoundaries.jl")
 include("Distributed/Distributed.jl")
+
 include("TimeSteppers/TimeSteppers.jl")
 include("Models/Models.jl")
 
