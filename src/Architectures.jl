bac7f4e03843adb04766191772962928e56b68c8
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-09-19 12:26:49 +0200

    fix comment

--- src/Architectures.jl
+++ src/Architectures.jl
@@ -93,7 +93,7 @@ function unified_array(::GPU, arr::AbstractArray)
     return vec
 end
 
-## Only for contiguous data!! (i.e. only if the offset for pointer(dst::CuArray, offset::Int) is 1)
+## GPU to GPU copy of contiguous data
 @inline function device_copy_to!(dst::CuArray, src::CuArray; async::Bool = false) 
     n = length(src)
     context!(context(src)) do
