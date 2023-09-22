27bda7562cbd18bd621b63e814a6fbd19f394544
simone-silvestri <silvestri.simone0@gmail.com>
2023-09-22 12:37:59 -0400

    fixed the distributed FFT tests

--- src/DistributedComputations/halo_communication_bcs.jl
+++ src/DistributedComputations/halo_communication_bcs.jl
@@ -17,21 +17,21 @@ function inject_halo_communication_boundary_conditions(field_bcs, local_rank, co
     rank_north  = connectivity.north
     rank_south  = connectivity.south
 
-    east_comm_ranks   = HaloCommunicationRanks(from=local_rank, to=rank_east)
-    west_comm_ranks   = HaloCommunicationRanks(from=local_rank, to=rank_west)
-    north_comm_ranks  = HaloCommunicationRanks(from=local_rank, to=rank_north)
-    south_comm_ranks  = HaloCommunicationRanks(from=local_rank, to=rank_south)
+    east_comm_ranks  = HaloCommunicationRanks(from=local_rank, to=rank_east)
+    west_comm_ranks  = HaloCommunicationRanks(from=local_rank, to=rank_west)
+    north_comm_ranks = HaloCommunicationRanks(from=local_rank, to=rank_north)
+    south_comm_ranks = HaloCommunicationRanks(from=local_rank, to=rank_south)
 
-    east_comm_bc   = DistributedCommunicationBoundaryCondition(east_comm_ranks)
-    west_comm_bc   = DistributedCommunicationBoundaryCondition(west_comm_ranks)
-    north_comm_bc  = DistributedCommunicationBoundaryCondition(north_comm_ranks)
-    south_comm_bc  = DistributedCommunicationBoundaryCondition(south_comm_ranks)
+    east_comm_bc  = DistributedCommunicationBoundaryCondition(east_comm_ranks)
+    west_comm_bc  = DistributedCommunicationBoundaryCondition(west_comm_ranks)
+    north_comm_bc = DistributedCommunicationBoundaryCondition(north_comm_ranks)
+    south_comm_bc = DistributedCommunicationBoundaryCondition(south_comm_ranks)
 
-    west     = isnothing(rank_west)   ? field_bcs.west   : west_comm_bc
-    east     = isnothing(rank_east)   ? field_bcs.east   : east_comm_bc
-    south    = isnothing(rank_south)  ? field_bcs.south  : south_comm_bc
-    north    = isnothing(rank_north)  ? field_bcs.north  : north_comm_bc
-    
+    west  = isnothing(rank_west)  ? field_bcs.west   : west_comm_bc
+    east  = isnothing(rank_east)  ? field_bcs.east   : east_comm_bc
+    south = isnothing(rank_south) ? field_bcs.south  : south_comm_bc
+    north = isnothing(rank_north) ? field_bcs.north  : north_comm_bc
+
     bottom   = field_bcs.bottom 
     top      = field_bcs.top    
     immersed = field_bcs.immersed
--- test/test_distributed_poisson_solvers.jl
+++ test/test_distributed_poisson_solvers.jl
@@ -2,14 +2,14 @@ using MPI
 MPI.Init()
 
 include("dependencies_for_runtests.jl")
+include("dependencies_for_poisson_solvers.jl")
 
-
-# # Distributed model tests
+# # Distributed Poisson Solver tests
 #
 # These tests are meant to be run on 4 ranks. This script may be run
 # stand-alone (outside the test environment) via
 #
-# mpiexec -n 4 julia --project test_distributed_models.jl
+# mpiexec -n 4 julia --project test_distributed_poisson_solver.jl
 #
 # provided that a few packages (like TimesDates.jl) are in your global environment.
 #
@@ -19,27 +19,37 @@ include("dependencies_for_runtests.jl")
 #
 # then later:
 # 
-# julia> include("test_distributed_models.jl")
+# julia> include("test_distributed_poisson_solver.jl")
 #
 # When running the tests this way, uncomment the following line
 
 # to initialize MPI.
 
-using Oceananigans.DistributedComputations: reconstruct_global_grid
+using Oceananigans.DistributedComputations: reconstruct_global_grid, DistributedGrid
+using Oceananigans.Models.NonhydrostaticModels: solve_for_pressure!
 
-function random_divergent_source_term(grid)
-    # Generate right hand side from a random (divergent) velocity field.
-    Ru = XFaceField(grid)
-    Rv = YFaceField(grid)
-    Rw = ZFaceField(grid)
+function random_divergent_source_term(grid::DistributedGrid)
+    arch = architecture(grid)
+    default_bcs = FieldBoundaryConditions()
+
+    u_bcs = regularize_field_boundary_conditions(default_bcs, grid, :u)
+    v_bcs = regularize_field_boundary_conditions(default_bcs, grid, :v)
+    w_bcs = regularize_field_boundary_conditions(default_bcs, grid, :w)
+
+    u_bcs = inject_halo_communication_boundary_conditions(u_bcs, arch.local_rank, arch.connectivity)
+    v_bcs = inject_halo_communication_boundary_conditions(v_bcs, arch.local_rank, arch.connectivity)
+    w_bcs = inject_halo_communication_boundary_conditions(w_bcs, arch.local_rank, arch.connectivity)
+
+    Ru = CenterField(grid, boundary_conditions=u_bcs)
+    Rv = CenterField(grid, boundary_conditions=v_bcs)
+    Rw = CenterField(grid, boundary_conditions=w_bcs)
     U = (u=Ru, v=Rv, w=Rw)
 
     Nx, Ny, Nz = size(grid)
-    set!(Ru, (x, y, z) -> rand())
-    set!(Rv, (x, y, z) -> rand())
-    set!(Rw, (x, y, z) -> rand())
+    set!(Ru, rand(Nx, Ny, Nz))
+    set!(Rv, rand(Nx, Ny, Nz))
+    set!(Rw, rand(Nx, Ny, Nz))
 
-    arch = architecture(grid)
     fill_halo_regions!(Ru)
     fill_halo_regions!(Rv)
     fill_halo_regions!(Rw)
@@ -49,34 +59,31 @@ function random_divergent_source_term(grid)
     R = zeros(Nx, Ny, Nz) |> ArrayType
     launch!(arch, grid, :xyz, divergence!, grid, U.u.data, U.v.data, U.w.data, R)
     
-    return R
+    return R, U
 end
 
 function divergence_free_poisson_solution(grid_points, ranks, topo)
     arch = Distributed(CPU(), ranks=ranks, topology=topo)
     local_grid = RectilinearGrid(arch, topology=topo, size=grid_points, extent=(2π, 2π, 2π))
 
-    bcs = FieldBoundaryConditions(local_grid, (Center, Center, Center))
-    bcs = inject_halo_communication_boundary_conditions(bcs, arch.local_rank, arch.connectivity)
+    p_bcs = FieldBoundaryConditions(local_grid, (Center, Center, Center))
+    p_bcs = inject_halo_communication_boundary_conditions(p_bcs, arch.local_rank, arch.connectivity)
 
     # The test will solve for ϕ, then compare R to ∇²ϕ.
-    ϕ   = CenterField(local_grid, boundary_conditions=bcs)
-    ∇²ϕ = CenterField(local_grid, boundary_conditions=bcs)
-    R   = random_divergent_source_term(local_grid)
+    ϕ   = CenterField(local_grid, boundary_conditions=p_bcs)
+    ∇²ϕ = CenterField(local_grid, boundary_conditions=p_bcs)
+    R, U = random_divergent_source_term(local_grid)
 
     global_grid = reconstruct_global_grid(local_grid)
     solver = DistributedFFTBasedPoissonSolver(global_grid, local_grid)
-    solver.storage.zfield .= R
-
-    # Solve it
-    solve!(ϕ, solver)
-
-    fill_halo_regions!(ϕ)
-
+    
+    # Using Δt = 1.
+    solve_for_pressure!(ϕ, solver, 1, U)
+    
     # "Recompute" ∇²ϕ
     compute_∇²!(∇²ϕ, ϕ, arch, local_grid)
 
-    return R ≈ interior(∇²ϕ)
+    return CUDA.@allowscalar interior(∇²ϕ) ≈ R
 end
 
 @testset "Distributed FFT-based Poisson solver" begin
@@ -85,11 +92,8 @@ end
                      (Periodic, Bounded, Bounded),
                      (Bounded, Bounded, Bounded))
         @info "  Testing 3D distributed FFT-based Poisson solver with topology $topology..."
-        @test divergence_free_poisson_solution((44, 11, 8), (1, 4, 1), topology)
-        @test divergence_free_poisson_solution((44,  4, 8), (1, 4, 1), topology)
-        @test divergence_free_poisson_solution((16, 11, 8), (1, 4, 1), topology)
-        @test divergence_free_poisson_solution((22,  8, 8), (2, 2, 1), topology)
-        @test divergence_free_poisson_solution(( 8, 22, 8), (2, 2, 1), topology)
+        @test divergence_free_poisson_solution((11, 44, 8), (4, 1, 1), topology)
+        @test divergence_free_poisson_solution(( 4, 44, 8), (4, 1, 1), topology)
         @test divergence_free_poisson_solution((44, 11, 8), (1, 4, 1), topology)
         @test divergence_free_poisson_solution((44,  4, 8), (1, 4, 1), topology)
         @test divergence_free_poisson_solution((16, 11, 8), (1, 4, 1), topology)
