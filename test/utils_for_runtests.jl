709042a2aac6fdf6c6c1a55eda11e255100f60ce
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-10-13 17:57:46 -0400

    equal not defined

--- test/utils_for_runtests.jl
+++ test/utils_for_runtests.jl
@@ -6,7 +6,7 @@ using Test
 using Printf
 using Test
 using Oceananigans.TimeSteppers: QuasiAdamsBashforth2TimeStepper, RungeKutta3TimeStepper, update_state!
-using Oceananigans.DistributedComputations: Distributed, Partition, child_architecture, Relative
+using Oceananigans.DistributedComputations: Distributed, Partition, child_architecture, Relative, Equal
 using MPI
 
 import Oceananigans.Fields: interior
