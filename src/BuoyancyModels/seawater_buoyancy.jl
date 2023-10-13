e49861c83355774e05494e9af5a13e177523938e
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-10-13 15:02:18 -0400

    fix doctests

--- src/BuoyancyModels/seawater_buoyancy.jl
+++ src/BuoyancyModels/seawater_buoyancy.jl
@@ -62,23 +62,39 @@ Examples
 ========
 
 ```jldoctest
-Suggested change
-using SeawaterPolynomials.TEOS10: TEOS10EquationOfState
-# The "TEOS10" equation of state, see https://www.teos-10.org
-teos10 = TEOS10EquationOfState()
-
-# Buoyancy that depends on both temperature and salinity
-buoyancy = SeawaterBuoyancy(equation_of_state=teos10)
-
-# Buoyancy that depends only on salinity with temperature held at 20 degrees Celsius
-salinity_dependent_buoyancy = SeawaterBuoyancy(equation_of_state=teos10, constant_temperature=20) 
-
-# Buoyancy that depends only on temperature with salinity held at 35 psu
-temperature_dependent_buoyancy = SeawaterBuoyancy(equation_of_state=teos10, constant_salinity=35)
-julia> using Oceananigans
 julia> using SeawaterPolynomials.TEOS10: TEOS10EquationOfState
 
 # The "TEOS10" equation of state, see https://www.teos-10.org
+julia> teos10 = TEOS10EquationOfState()
+BoussinesqEquationOfState{Float64}:
+    ├── seawater_polynomial: TEOS10SeawaterPolynomial{Float64}
+    └── reference_density: 1020.0
+
+# Buoyancy that depends on both temperature and salinity
+julia> using Oceananigans
+
+julia> buoyancy = SeawaterBuoyancy(equation_of_state=teos10)
+SeawaterBuoyancy{Float64}:
+├── gravitational_acceleration: 9.80665
+└── equation of state: BoussinesqEquationOfState{Float64}
+
+# Buoyancy that depends only on salinity with temperature held at 20 degrees Celsius
+julia> salinity_dependent_buoyancy = SeawaterBuoyancy(equation_of_state=teos10, constant_temperature=20) 
+SeawaterBuoyancy{Float64}:
+├── gravitational_acceleration: 9.80665
+├── constant_temperature: 20
+└── equation of state: BoussinesqEquationOfState{Float64}
+
+# Buoyancy that depends only on temperature with salinity held at 35 psu
+julia> temperature_dependent_buoyancy = SeawaterBuoyancy(equation_of_state=teos10, constant_salinity=35)
+SeawaterBuoyancy{Float64}:
+├── gravitational_acceleration: 9.80665
+├── constant_salinity: 35
+└── equation of state: BoussinesqEquationOfState{Float64}
+
+# The "TEOS10" equation of state, see https://www.teos-10.org
+julia> using SeawaterPolynomials.TEOS10: TEOS10EquationOfState
+
 julia> teos10 = TEOS10EquationOfState()
 BoussinesqEquationOfState{Float64}:
     ├── seawater_polynomial: TEOS10SeawaterPolynomial{Float64}
