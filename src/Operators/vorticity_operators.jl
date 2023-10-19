8b962b379ec4ddab62ced075fafb9f2252808dc4
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-10-19 14:42:27 -0600

    don't use short-circuit but ensure operator presedence is taken care of

--- src/Operators/vorticity_operators.jl
+++ src/Operators/vorticity_operators.jl
@@ -17,10 +17,10 @@ end
 @inline on_corner(i, j, grid) = on_south_west_corner(i, j, grid) | on_south_east_corner(i, j, grid) | on_north_west_corner(i, j, grid) | on_north_east_corner(i, j, grid)
 
 # South-west, south-east, north-west, north-east corners
-@inline on_south_west_corner(i, j, grid) = (i == 1 && j == 1)
-@inline on_south_east_corner(i, j, grid) = (i == grid.Nx+1 && j == 1)
-@inline on_north_east_corner(i, j, grid) = (i == grid.Nx+1 && j == grid.Ny+1)
-@inline on_north_west_corner(i, j, grid) = (i == 1 && j == grid.Ny+1)
+@inline on_south_west_corner(i, j, grid) = ((i == 1) & (j == 1))
+@inline on_south_east_corner(i, j, grid) = ((i == grid.Nx+1) & (j == 1))
+@inline on_north_east_corner(i, j, grid) = ((i == grid.Nx+1) & (j == grid.Ny+1))
+@inline on_north_west_corner(i, j, grid) = ((i == 1) & (j == grid.Ny+1))
 
 #####
 ##### Vertical circulation at the corners of the cubed sphere needs to treated in a special manner.
@@ -28,13 +28,14 @@ end
 #####
 
 """
-     Γᶠᶠᶜ(i, j, k, grid, u, v)
+    Γᶠᶠᶜ(i, j, k, grid, u, v)
 
  The vertical circulation associated with horizontal velocities ``u`` and ``v``.
  """
-@inline Γᶠᶠᶜ(i, j, k, grid::OrthogonalSphericalShellGrid, u, v) = 
+@inline function Γᶠᶠᶜ(i, j, k, grid::OrthogonalSphericalShellGrid, u, v)
     ifelse(on_south_west_corner(i, j, grid) | on_north_west_corner(i, j, grid),
-           Δy_qᶜᶠᶜ(i, j, k, grid, v) - Δx_qᶠᶜᶜ(i, j, k, grid, u) + Δx_qᶠᶜᶜ(i, j-1, k, grid, u), 
+           Δy_qᶜᶠᶜ(i, j, k, grid, v) - Δx_qᶠᶜᶜ(i, j, k, grid, u) + Δx_qᶠᶜᶜ(i, j-1, k, grid, u),
            ifelse(on_south_east_corner(i, j, grid) | on_north_east_corner(i, j, grid),
-                  - Δy_qᶜᶠᶜ(i-1, j, k, grid, v) - Δx_qᶠᶜᶜ(i, j, k, grid, u) + Δx_qᶠᶜᶜ(i, j-1, k, grid, u), 
+                  - Δy_qᶜᶠᶜ(i-1, j, k, grid, v) - Δx_qᶠᶜᶜ(i, j, k, grid, u) + Δx_qᶠᶜᶜ(i, j-1, k, grid, u),
                   δxᶠᵃᵃ(i, j, k, grid, Δy_qᶜᶠᶜ, v) - δyᵃᶠᵃ(i, j, k, grid, Δx_qᶠᶜᶜ, u)))
+end
