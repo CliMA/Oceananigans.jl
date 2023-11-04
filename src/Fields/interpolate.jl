32e0c984dca9eebe0a80e7164bc415ac298d52aa
Gregory Wagner <wagner.greg@gmail.com>
2023-11-04 10:31:05 -0600

    Add utility for interpolating from one field to another

--- src/Fields/interpolate.jl
+++ src/Fields/interpolate.jl
@@ -293,3 +293,39 @@ Interpolate `field` to the physical point `(x, y, z)` using trilinear interpolat
     return interpolate(to_node, from_field, from_loc, from_field.grid)
 end
 
+@kernel function _interpolate!!(to_field, to_grid, to_location,
+                                from_field, from_grid, from_location)
+
+    i, j, k = @index(Global, NTuple)
+    to_node = node(i, j, k, to_grid, to_location...)
+    @inbounds to_field[i, j, k] = interpolate(to_node, from_field, from_location, from_grid)
+end
+
+"""
+    interpolate!(to_field::Field, from_field::AbstractField)
+
+Interpolate `from_field` `to_field` and then fill the halo regions of `to_field`.
+"""
+function interpolate!(to_field::Field, from_field::AbstractField)
+    to_grid = to_field.grid
+
+    from_arch = architecture(from_field)
+    to_arch = architecture(to_field)
+    if !isnothing(from_arch) && to_arch == from_arch
+        msg = "Cannot interpolate! because from_field is on $from_arch while to_field is on $to_field."
+        throw(ArgumentError(msg))
+    end
+
+    # Make locations
+    from_location = Tuple(L() for L in location(from_field))
+    to_location   = Tuple(L() for L in location(to_field))
+
+    launch!(arch, to_grid, size(to_field),
+            _interpolate!, to_field, to_grid, to_location,
+            from_field, from_grid, from_location)
+
+    fill_halo_regions!(to_field)
+
+    return nothing
+end
+
