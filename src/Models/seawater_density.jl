4d2e5d024668aa56f87b69e1d09d4822ab7226ef
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-11-08 14:50:05 -0500

    add Seawater density

--- src/Models/seawater_density.jl
+++ src/Models/seawater_density.jl
@@ -9,6 +9,9 @@ import SeawaterPolynomials.ρ
 **note** `eos` must be `BoussinesqEquationOfState` because a reference density is needed for the computation."
 @inline ρ(i, j, k, grid, eos, T, S, Z) = @inbounds ρ(T[i, j, k], S[i, j, k], Z[i, j, k], eos)
 
+# Fallback to calculate potential density, i.e. density at a reference height
+@inline ρ(i, j, k, grid, eos, T, S, Z::Number) = @inbounds ρ(T[i, j, k], S[i, j, k], Z, eos)
+
 "Return a `KernelFunctionOperation` to compute the in-situ `seawater_density`."
 seawater_density(grid, eos, temperature, salinity, geopotential_height) =
     KernelFunctionOperation{Center, Center, Center}(ρ, grid, eos, temperature, salinity, geopotential_height)
