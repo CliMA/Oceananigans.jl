0183c651cf5a2acb138d43e138c4e18334c0a700
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-11-20 09:54:04 -0500

    first commit

--- src/Models/ShallowWaterModels/solution_and_tracer_tendencies.jl
+++ src/Models/ShallowWaterModels/solution_and_tracer_tendencies.jl
@@ -4,20 +4,13 @@ using Oceananigans.Coriolis
 using Oceananigans.Operators
 using Oceananigans.TurbulenceClosures: ∇_dot_qᶜ, ∂ⱼ_τ₁ⱼ, ∂ⱼ_τ₂ⱼ
 
-@inline half_g_h²(i, j, k, grid, h, g)  = @inbounds 1/2 * g * h[i, j, k]^2
 @inline h_plus_hB(i, j, k, grid, h, hB) = @inbounds h[i, j, k] + hB[i, j, k]
 
-@inline x_pressure_gradient(i, j, k, grid, g, h, hB, formulation) = ∂xᶠᶜᶜ(i, j, k, grid, half_g_h², h, g)
-@inline y_pressure_gradient(i, j, k, grid, g, h, hB, formulation) = ∂yᶜᶠᶜ(i, j, k, grid, half_g_h², h, g)
+@inline x_pressure_gradient(i, j, k, grid, g, h, hB, ::ConservativeFormulation) = g * ℑxᶠᶜᶜ(i, j, k, grid, h) * ∂xᶠᶜᶜ(i, j, k, grid, h_plus_hB, h, hB)
+@inline y_pressure_gradient(i, j, k, grid, g, h, hB, ::ConservativeFormulation) = g * ℑyᶜᶠᶜ(i, j, k, grid, h) * ∂yᶜᶠᶜ(i, j, k, grid, h_plus_hB, h, hB)
 
 @inline x_pressure_gradient(i, j, k, grid, g, h, hB, ::VectorInvariantFormulation) = g * ∂xᶠᶜᶜ(i, j, k, grid, h_plus_hB, h, hB)
 @inline y_pressure_gradient(i, j, k, grid, g, h, hB, ::VectorInvariantFormulation) = g * ∂yᶜᶠᶜ(i, j, k, grid, h_plus_hB, h, hB)
-
-@inline bathymetry_contribution_x(i, j, k, grid, g, h, hB, formulation) = g * h[i, j, k] * ∂xᶠᶜᶜ(i, j, k, grid, hB)
-@inline bathymetry_contribution_y(i, j, k, grid, g, h, hB, formulation) = g * h[i, j, k] * ∂yᶜᶠᶜ(i, j, k, grid, hB)
-
-@inline bathymetry_contribution_x(i, j, k, grid, g, h, hB, ::VectorInvariantFormulation) = zero(grid) 
-@inline bathymetry_contribution_y(i, j, k, grid, g, h, hB, ::VectorInvariantFormulation) = zero(grid) 
 
 """
 Compute the tendency for the x-directional transport, uh
