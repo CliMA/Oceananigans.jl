959ddaba5c7a87ef42a3d7a7c4878761c359c8ba
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2023-12-28 18:02:24 -0500

    Fix order of operations in step_lagrangian_particles! function

--- src/Models/LagrangianParticleTracking/LagrangianParticleTracking.jl
+++ src/Models/LagrangianParticleTracking/LagrangianParticleTracking.jl
@@ -140,12 +140,12 @@ step_lagrangian_particles!(::Nothing, model, Δt) = nothing
 function step_lagrangian_particles!(particles::LagrangianParticles, model, Δt)
     # Update the properties of the Lagrangian particles
     update_lagrangian_particle_properties!(particles, model, Δt)
-    
-    # Compute dynamics
-    particles.dynamics(particles, model, Δt)
 
     # Advect particles
     advect_lagrangian_particles!(particles, model, Δt)
+
+    # Compute dynamics
+    particles.dynamics(particles, model, Δt)
 end
 
 end # module
