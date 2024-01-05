15611961f33038b42a944909643e60d81c56e6dc
Gregory Wagner <wagner.greg@gmail.com>
2024-01-05 11:12:22 -0700

    Better docstring for Callback

--- src/Simulations/callback.jl
+++ src/Simulations/callback.jl
@@ -21,13 +21,27 @@ function initialize!(callback::Callback, sim)
 end
 
 """
-    Callback(func, schedule=IterationInterval(1); parameters=nothing)
+    Callback(func, schedule=IterationInterval(1);
+             parameters=nothing, callsite=TimeStepCallsite())
 
 Return `Callback` that executes `func` on `schedule`
-with optional `parameters`. `schedule = IterationInterval(1)` by default.
+at the `callsite` with optional `parameters`. By default,
+`schedule = IterationInterval(1)` and `callsite = TimeStepCallsite()`.
 
 If `isnothing(parameters)`, `func(sim::Simulation)` is called.
 Otherwise, `func` is called via `func(sim::Simulation, parameters)`.
+
+The `callsite` determines where `Callback` is executed. The possible values for 
+`callsite` are
+
+ * `TimeStepCallsite()`: after a time-step.
+
+ * `TendencyCallsite()`: after tendencies are calculated, but before taking
+    a time-step (useful for modifying tendency calculations).
+
+ * `UpdateStateCallsite()`: within `update_state!`, after auxiliary variables have
+    been computed (for multi-stage time-steppers, `update_state!` may be called multiple
+    times per time-step).
 """
 function Callback(func, schedule=IterationInterval(1);
                   parameters = nothing,
@@ -88,6 +102,8 @@ is big enough for the name to be unique.
 
 If `name` is supplied, it may be modified if `simulation.callbacks[name]`
 already exists.
+
+`callback_kw` are passed to the constructor for [`Callback`](@ref).
 
 The `callback` (which contains a schedule) can also be supplied directly.
 """
