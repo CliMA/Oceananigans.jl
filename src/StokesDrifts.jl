094f180df6b080a167d97158a7e5206769c3f50d
Gregory L. Wagner <wagner.greg@gmail.com>
2024-01-09 09:40:57 -0700

    Update src/StokesDrifts.jl
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>

--- src/StokesDrifts.jl
+++ src/StokesDrifts.jl
@@ -137,7 +137,7 @@ of the "psuedovorticity",
 ∇ × uˢ = x̂ (∂y_wˢ - ∂z_vˢ) + ŷ (∂z_uˢ - ∂x_wˢ) + ẑ (∂x_vˢ - ∂y_uˢ)
 ```
 
-as well as time-derivatives of uˢ, vˢ, and wˢ.
+as well as time-derivatives of ``uˢ``, ``vˢ``, and ``wˢ``.
 
 Note that each function (e.g. ∂z_uˢ) is a function of horizontal coordinates and time.
 Thus, the correct function signature depends on the grid, since `Flat` horizontal directions
