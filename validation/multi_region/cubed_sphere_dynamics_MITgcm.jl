212fc1603fb71cb9c55dfabcfeb0c88ecbcda3f3
Jean-Michel Campin <jmc@mit.edu>
2024-01-20 14:33:07 -0500

    Add Coriolis
    also save SSH field and simplify initial condition setting (so that it would
    work with zero planet rotation)

--- validation/multi_region/cubed_sphere_dynamics_MITgcm.jl
+++ validation/multi_region/cubed_sphere_dynamics_MITgcm.jl
@@ -2,7 +2,7 @@
 Download:
 (a) the file old_code_metrics.jld2 from
     https://www.dropbox.com/scl/fo/qu7nfr94wqc6ym6izpfqw/h?rlkey=zd4o5134u64ibyxggy64tiygt&dl=0; and
-(b) the directory grid_cs32+ol4 from 
+(b) the directory grid_cs32+ol4 from
     https://www.dropbox.com/scl/fo/c0pex0u8yvao6ehd3rqtp/h?rlkey=uq8bojrrsa7c4pb4n9ou8wvcs&dl=0;
 and place them in the path validation/multi_region/. Then run this script from the same path as:
 include("cubed_sphere_dynamics_MITgcm.jl")
@@ -23,11 +23,11 @@ using CairoMakie
 
 include("cubed_sphere_visualization.jl")
 
-g = 9.81
+g = 10.
 
 Lz = 1
-R  = 6370e3            # sphere's radius
-U  = 38.60328935834681 # velocity scale
+R  = 6370e3        # sphere's radius
+U  = 40.           # velocity scale
 
 load_cs32_grid = false
 
@@ -38,7 +38,7 @@ if load_cs32_grid
                    "b1dafe4f9142c59a2166458a2def743cd45b20a4ed3a1ae84ad3a530e1eff538")
     DataDeps.register(dd32)
     grid_filepath = datadep"cubed_sphere_32_grid/cubed_sphere_32_grid.jld2"
-    grid = ConformalCubedSphereGrid(grid_filepath; 
+    grid = ConformalCubedSphereGrid(grid_filepath;
                                     Nz = 1,
                                     z = (-Lz, 0),
                                     panel_halo = (1, 1, 1),
@@ -94,7 +94,7 @@ end
 π_MITgcm = 3.14159265358979323844
 Ω = 2π_MITgcm/86400
 
-ψᵣ(λ, φ, z) = -R^2*Ω_prime/(2Ω)*2Ω*sind(φ) # ψᵣ(λ, φ, z) = -U * R * sind(φ)
+ψᵣ(λ, φ, z) = -R^2*Ω_prime*sind(φ) # ψᵣ(λ, φ, z) = -U * R * sind(φ)
 
 #=
 for φʳ = 90; ψᵣ(λ, φ, z) = - U * R * sind(φ)
@@ -108,8 +108,8 @@ for φʳ = 90; ψᵣ(λ, φ, z) = - U * R * sind(φ)
 set!(ψ, ψᵣ)
 
 #=
-Note: fill_halo_regions! works for (Face, Face, Center) field, *except* for the two corner points that do not correspond 
-to an interior point! We need to manually fill the Face-Face halo points of the two corners that do not have a 
+Note: fill_halo_regions! works for (Face, Face, Center) field, *except* for the two corner points that do not correspond
+to an interior point! We need to manually fill the Face-Face halo points of the two corners that do not have a
 corresponding interior point.
 =#
 
@@ -143,9 +143,9 @@ for region in 1:number_of_regions(grid)
         λ = λnode(i, j, k, grid[region], Face(), Face(), Center())
         φ = φnode(i, j, k, grid[region], Face(), Face(), Center())
         ψ[region][i, j, k] = ψᵣ(λ, φ, 0)
-        #= 
-        At the halo points, both latitude (φ) and longitude (λ) assume zero values, which in turn causes the 
-        streamfunction (ψ) to be zero. Therefore, to guarantee accurate values at these points, we opted to reinstate 
+        #=
+        At the halo points, both latitude (φ) and longitude (λ) assume zero values, which in turn causes the
+        streamfunction (ψ) to be zero. Therefore, to guarantee accurate values at these points, we opted to reinstate
         the fill halos for the streamfunction (ψ) after setting its interior values.
         =#
     end
@@ -165,7 +165,7 @@ for region in 1:number_of_regions(grid)
         #=
         # Debugging option 1
         u[region][i, j, k] = 1
-        v[region][i, j, k] = 2 
+        v[region][i, j, k] = 2
         =#
         #=
         # Debugging option 2
@@ -194,14 +194,14 @@ using KernelAbstractions: @kernel, @index
 end
 
 #=
-Upon examining the initial vorticity field plot, it was noted that NANs unexpectedly appear along the halos. 
-Additionally, the vorticity values along the boundaries are significantly higher compared to those within the domain's 
+Upon examining the initial vorticity field plot, it was noted that NANs unexpectedly appear along the halos.
+Additionally, the vorticity values along the boundaries are significantly higher compared to those within the domain's
 interior. These issues likely contribute to the instability in the solution. By replacing the line
 @inbounds ζ[i, j, k] = ζ₃ᶠᶠᶜ(i, j, k, grid, u, v)
 with
 @inbounds ζ[i, j, k] = (-1)^(i+j+k)*(i + j + k)
-we observe that all points are populated with finite values, effectively eliminating the appearance of NANs. 
-Consequently, it's evident that the flaw lies within the vorticity computation function, which seems to be producing 
+we observe that all points are populated with finite values, effectively eliminating the appearance of NANs.
+Consequently, it's evident that the flaw lies within the vorticity computation function, which seems to be producing
 erroneous results.
 =#
 
@@ -267,7 +267,7 @@ jldopen("new_code_metrics.jld2", "w") do file
         file["Azᶜᶜᵃ/" * string(region)] = grid[region].Azᶜᶜᵃ
         file["Azᶠᶜᵃ/" * string(region)] = grid[region].Azᶠᶜᵃ
         file["Azᶜᶠᵃ/" * string(region)] = grid[region].Azᶜᶠᵃ
-        file["Azᶠᶠᵃ/" * string(region)] = grid[region].Azᶠᶠᵃ       
+        file["Azᶠᶠᵃ/" * string(region)] = grid[region].Azᶠᶠᵃ
     end
 end
 
@@ -283,7 +283,7 @@ if compare_old_and_new_code_metrics
     old_Azᶠᶜᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
     old_Azᶜᶠᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
     old_Azᶠᶠᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
-    
+
     new_Δxᶠᶜᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
     new_Δxᶜᶠᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
     new_Δyᶠᶜᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
@@ -292,7 +292,7 @@ if compare_old_and_new_code_metrics
     new_Azᶠᶜᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
     new_Azᶜᶠᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
     new_Azᶠᶠᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
-    
+
     if old_code_metrics_JMC
         for region in 1:6
             old_Δxᶠᶜᵃ_parent[:, :, region] = read_big_endian_coordinates("grid_cs32+ol4/dXc.00$(region).001.data", 32, 4)[1+4-nHalo:end-4+nHalo,1+4-nHalo:end-4+nHalo]
@@ -304,7 +304,7 @@ if compare_old_and_new_code_metrics
             old_Azᶜᶠᵃ_parent[:, :, region] = read_big_endian_coordinates("grid_cs32+ol4/rAs.00$(region).001.data", 32, 4)[1+4-nHalo:end-4+nHalo,1+4-nHalo:end-4+nHalo]
             old_Azᶠᶠᵃ_parent[:, :, region] = read_big_endian_coordinates("grid_cs32+ol4/rAz.00$(region).001.data", 32, 4)[1+4-nHalo:end-4+nHalo,1+4-nHalo:end-4+nHalo]
         end
-    else    
+    else
         old_file = jldopen("old_code_metrics.jld2")
         for region in 1:6
             old_Δxᶠᶜᵃ_parent[:, :, region] = parent(old_file["Δxᶠᶜᵃ/" * string(region)][1-Hx:Nx+Hx, :])
@@ -344,11 +344,11 @@ if compare_old_and_new_code_metrics
             end
         end
     end
-    
+
     if !old_code_metrics_JMC
         close(old_file)
     end
-    
+
     new_file = jldopen("new_code_metrics.jld2")
     for region in 1:6
         new_Δxᶠᶜᵃ_parent[:, :, region] = parent(new_file["Δxᶠᶜᵃ/" * string(region)][:, :, 1])
@@ -361,7 +361,7 @@ if compare_old_and_new_code_metrics
         new_Azᶠᶠᵃ_parent[:, :, region] = parent(new_file["Azᶠᶠᵃ/" * string(region)][:, :, 1])
     end
     close(new_file)
-    
+
     Δxᶠᶜᵃ_difference = new_Δxᶠᶜᵃ_parent - old_Δxᶠᶜᵃ_parent
     Δxᶜᶠᵃ_difference = new_Δxᶜᶠᵃ_parent - old_Δxᶜᶠᵃ_parent
     Δyᶠᶜᵃ_difference = new_Δyᶠᶜᵃ_parent - old_Δyᶠᶜᵃ_parent
@@ -370,7 +370,7 @@ if compare_old_and_new_code_metrics
     Azᶠᶜᵃ_difference = new_Azᶠᶜᵃ_parent - old_Azᶠᶜᵃ_parent
     Azᶜᶠᵃ_difference = new_Azᶜᶠᵃ_parent - old_Azᶜᶠᵃ_parent
     Azᶠᶠᵃ_difference = new_Azᶠᶠᵃ_parent - old_Azᶠᶠᵃ_parent
-    
+
     Δxᶠᶜᵃ_relative_difference = Δxᶠᶜᵃ_difference ./ old_Δxᶠᶜᵃ_parent
     Δxᶜᶠᵃ_relative_difference = Δxᶜᶠᵃ_difference ./ old_Δxᶜᶠᵃ_parent
     Δyᶠᶜᵃ_relative_difference = Δyᶠᶜᵃ_difference ./ old_Δyᶠᶜᵃ_parent
@@ -379,12 +379,16 @@ if compare_old_and_new_code_metrics
     Azᶠᶜᵃ_relative_difference = Azᶠᶜᵃ_difference ./ old_Azᶠᶜᵃ_parent
     Azᶜᶠᵃ_relative_difference = Azᶜᶠᵃ_difference ./ old_Azᶜᶠᵃ_parent
     Azᶠᶠᵃ_relative_difference = Azᶠᶠᵃ_difference ./ old_Azᶠᶠᵃ_parent
-    
+
 end
+
+my_Coriolis = HydrostaticSphericalCoriolis( rotation_rate = Ω ,
+                                            scheme = EnstrophyConserving())
 
 model = HydrostaticFreeSurfaceModel(; grid,
                                     momentum_advection = VectorInvariant(),
                                     free_surface = ExplicitFreeSurface(; gravitational_acceleration = g),
+                                    coriolis = my_Coriolis,
                                     buoyancy = nothing)
 
 #=
@@ -393,24 +397,23 @@ model.timestepper.χ = -0.5
 
 # Initial conditions
 
-fac = -(R^2) * Ω_prime * (Ω + 0.5Ω_prime) / (4g * Ω^2)
- 
+fac = -(R^2) * Ω_prime * (Ω + 0.5Ω_prime) / g
+
 for region in 1:number_of_regions(grid)
     #=
     The following operations only set the interior values of the model velocities.
     model.velocities.u[region] .= u[region]
     model.velocities.v[region] .= v[region]
     =#
-    
+
     for j in 1-Hy:grid.Ny+Hy, i in 1-Hx:grid.Nx+Hx, k in 1:grid.Nz
         model.velocities.u[region][i,j,k] = u[region][i, j, k]
         model.velocities.v[region][i,j,k] = v[region][i, j, k]
     end
-    
+
     for j in 1:grid.Ny, i in 1:grid.Nx, k in grid.Nz+1:grid.Nz+1
         φ = φnode(i, j, k, grid[region], Center(), Center(), Center())
-        f = 2 * Ω * sind(φ)
-        model.free_surface.η[region][i, j, k] = fac * f^2
+        model.free_surface.η[region][i, j, k] = fac * (sind(φ))^2
     end
 end
 
@@ -419,7 +422,7 @@ for passes in 1:3
 end
 
 Δt = 1200
-stop_time = 86400 # 1 day
+stop_time = 5*86400 # 5 day
 simulation = Simulation(model; Δt, stop_time)
 
 # Print a progress message
@@ -428,13 +431,16 @@ progress_message(sim) = @printf("Iteration: %04d, time: %s, Δt: %s, max(|u|): %
                                 maximum(abs, sim.model.velocities.u),
                                 prettytime(sim.run_wall_time))
 
-simulation.callbacks[:progress] = Callback(progress_message, IterationInterval(20))
+simulation.callbacks[:progress] = Callback(progress_message, IterationInterval(6))
 
 u_fields = Field[]
 save_u(sim) = push!(u_fields, deepcopy(sim.model.velocities.u))
 
 v_fields = Field[]
 save_v(sim) = push!(v_fields, deepcopy(sim.model.velocities.v))
+
+eta_fields = Field[]
+save_eta(sim) = push!(eta_fields, deepcopy(sim.model.free_surface.η))
 
 #=
 using Oceananigans.Models.HydrostaticFreeSurfaceModels: fill_velocity_halos!
@@ -453,7 +459,7 @@ end
 
 u₀ = deepcopy(simulation.model.velocities.u)
 v₀ = deepcopy(simulation.model.velocities.v)
-ζ₀ = deepcopy(ζ) 
+ζ₀ = deepcopy(ζ)
 
 # Plot the relative difference of the grid metrics with halos.
 
@@ -517,29 +523,30 @@ function save_vorticity(sim)
     fill_velocity_halos!(sim.model.velocities)
 
     u, v, _ = sim.model.velocities
-    
+
     offset = -1 .* halo_size(grid)
-    
+
     @apply_regionally begin
         params = KernelParameters(total_size(ζ[1]), offset)
         launch!(CPU(), grid, params, _compute_vorticity!, ζ, grid, u, v)
     end
 
     push!(ζ_fields, deepcopy(ζ))
-    
+
     Δζ_field = deepcopy(ζ)
     for region in 1:number_of_regions(grid)
         for i in 1:grid.Nx, j in 1:grid.Ny, k in 1:grid.Nz
             Δζ_field[region][i, j, k] -= ζ₀[region][i, j, k]
         end
     end
-    
+
     push!(Δζ_fields, Δζ_field)
 end
 
-save_fields_iteration_interval = 3
+save_fields_iteration_interval = 9
 simulation.callbacks[:save_u] = Callback(save_u, IterationInterval(save_fields_iteration_interval))
 simulation.callbacks[:save_v] = Callback(save_v, IterationInterval(save_fields_iteration_interval))
+simulation.callbacks[:save_eta] = Callback(save_eta, IterationInterval(save_fields_iteration_interval))
 simulation.callbacks[:save_vorticity] = Callback(save_vorticity, IterationInterval(save_fields_iteration_interval))
 
 run!(simulation)
