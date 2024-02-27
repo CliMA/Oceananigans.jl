0ab0a1d6787de79ed28591b14bfbf95d4f7f7058
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-02-27 17:28:27 -0500

    going ahead

--- src/Advection/shared_tracer_advection.jl
+++ src/Advection/shared_tracer_advection.jl
@@ -5,24 +5,36 @@ function tracer_advection(Gⁿ, tracers, grid, tracer_advection, velocities; par
     return nothing
 end
 
-@kernel function tracer_advection_x(Gⁿ, tracers, grid, tracer_advection, U)
-    i, j, k = @index(Global, NTuple)
-   
-    glo_id = @index(Global)
-    loc_id = @index(Local)
+@inline retrieve_x_advection(tracer_advection) = tracer_advection
+@inline retrieve_y_advection(tracer_advection) = tracer_advection
+@inline retrieve_z_advection(tracer_advection) = tracer_advection
 
-    tracer_flux = @localmem FT (N, grp_size)
+@inline retrieve_x_advection(tracer_advection::TracerAdvection) = tracer_advection.x
+@inline retrieve_y_advection(tracer_advection::TracerAdvection) = tracer_advection.y
+@inline retrieve_z_advection(tracer_advection::TracerAdvection) = tracer_advection.z
 
-    @unroll for tracer_name in tracer_names
-        advection = retrieve_x_advection(tracer_advection, tracer_name)
+@kernel function tracer_advection_x(Gⁿ, tracers, grid, tracer_advection, U, ::Val{Ntracers}) where Ntracers
+    global_id = @index(Global, Linear)
+    block_id  = 
+    thread_id = @index(Local)
+
+    k_id = 
+
+    tracer_flux = @localmem FT (, )
+
+    @unroll for t in 1:Ntracers
+        tracer_name = @inbounds tracer_names[t]
+        tracer    = @inbounds tracers[t]
+        advection = @inbounds tracer_advection[tracer_name]
+        tendency  = @inbounds Gⁿ[tracer_name]
+        advection = retrieve_x_advection(advection)
 
         # calculate fluxes into the shared memory
         tracer_flux[] = _advective_tracer_flux_x(i, j, k, grid, advection, U, c)
 
         @synchronize
         # put div in memory
-        Gⁿ[i, j, k] = (tracer_flux[t] - tracer_flux[t-1]) / Vᶜᶜᶜ(i, j, k, grid)
-        # exit
+        tendency[i, j, k] = (tracer_flux[t] - tracer_flux[t-1]) / Vᶜᶜᶜ(i, j, k, grid)
     end
 end
 
