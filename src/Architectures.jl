aa3bf068b49a26af21f00f006884a1556c8262e9
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-03-03 14:58:16 -0500

    no inline for tuples

--- src/Architectures.jl
+++ src/Architectures.jl
@@ -60,9 +60,9 @@ array_type(::GPU) = CuArray
 on_architecture(arch, a) = a
 
 # Tupled implementation
-@inline on_architecture(arch, t::Tuple{}) = ()
-@inline on_architecture(arch, t::Tuple)   = Tuple(on_architecture(arch, elem) for elem in t)
-@inline on_architecture(arch, nt::NamedTuple)  = NamedTuple{keys(nt)}(on_architecture(arch, Tuple(nt)))
+on_architecture(arch, t::Tuple{}) = ()
+on_architecture(arch, t::Tuple)   = Tuple(on_architecture(arch, elem) for elem in t)
+on_architecture(arch, nt::NamedTuple)  = NamedTuple{keys(nt)}(on_architecture(arch, Tuple(nt)))
 
 # On architecture for array types
 on_architecture(::CPU, a::Array)   = a
