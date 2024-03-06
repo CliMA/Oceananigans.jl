0a30050b7645a022759f29b1cc6b4672895deb7e
Gregory L. Wagner <wagner.greg@gmail.com>
2024-03-06 08:09:03 -0700

    Update src/Forcings/forcing.jl
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>

--- src/Forcings/forcing.jl
+++ src/Forcings/forcing.jl
@@ -167,7 +167,7 @@ end
 """
     Forcing(array::AbstractArray)
 
-Return a `Forcing` by `array`, which can be added to the tendency of an Oceananigans model field.
+Return a `Forcing` by `array`, which can be added to the tendency of a model field.
 
 Forcing is computed by calling `array[i, j, k]`, so `array` must be 3D with `size(grid)`.
 """
