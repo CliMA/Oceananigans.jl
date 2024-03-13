af1603b8ffd80abe9cf084762bd73e090baf234d
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-03-13 09:14:03 -0400

    wenos till 5

---
+++ src/Advection/weno_2.jl
@@ -0,0 +1,143 @@
+for side in [:left, :right], (dir, val) in zip([:xᶠᵃᵃ, :yᵃᶠᵃ, :zᵃᵃᶠ], [1, 2, 3])
+    biased_interpolate = Symbol(:inner_, side, :_biased_interpolate_, dir)
+    biased_β  = Symbol(side, :_biased_β)
+    biased_p  = Symbol(side, :_biased_p)
+    coeff     = Symbol(:coeff_, side) 
+    stencil   = Symbol(side, :_stencil_, dir)
+    stencil_u = Symbol(:tangential_, side, :_stencil_u)
+    stencil_v = Symbol(:tangential_, side, :_stencil_v)
+
+    @eval begin
+        @inline function $biased_interpolate(i, j, k, grid, 
+                                            scheme::WENO{2, FT}, 
+                                            ψ, idx, loc, args...) where {FT}
+        
+            ψs = $stencil(i, j, k, scheme, Val(1), ψ, grid, args...)
+            β  = $biased_β(ψs, scheme, Val(0))
+            C  = FT($coeff(scheme, Val(0)))
+            α  = @fastmath C / (β + FT(ε))^2
+            ψ̅  = $biased_p(scheme, Val(0), ψs, Nothing, Val($val), idx, loc) 
+            glob = β
+            sol1 = ψ̅ * C
+            wei1 = C
+            sol2 = ψ̅ * α  
+            wei2 = α
+
+            ψs = $stencil(i, j, k, scheme, Val(2), ψ, grid, args...)
+            β  = $biased_β(ψs, scheme, Val(1))
+            C  = FT($coeff(scheme, Val(1)))
+            α  = @fastmath C / (β + FT(ε))^2
+            ψ̅  = $biased_p(scheme, Val(1), ψs, Nothing, Val($val), idx, loc) 
+            glob += add_global_smoothness(glob, β, Val(N), Val(1))
+            sol1 += ψ̅ * C
+            wei1 += C
+            sol2 += ψ̅ * α  
+            wei2 += α
+
+            # Is glob squared here?
+            return (sol1 + sol2 * glob) / (wei1 + wei2 * glob)
+        end
+
+        @inline function $biased_interpolate(i, j, k, grid, 
+                                            scheme::WENO{2, FT}, 
+                                            ψ, idx, loc, ::AbstractSmoothnessStencil, args...) where {FT}
+        
+            ψs = $stencil(i, j, k, scheme, Val(1), ψ, grid, args...)
+            β  = $biased_β(ψs, scheme, Val(0))
+            C  = FT($coeff(scheme, Val(0)))
+            α  = @fastmath C / (β + FT(ε))^2
+            ψ̅  = $biased_p(scheme, Val(0), ψs, Nothing, Val($val), idx, loc) 
+            glob = β
+            sol1 = ψ̅ * C
+            wei1 = C
+            sol2 = ψ̅ * α  
+            wei2 = α
+
+            ψs = $stencil(i, j, k, scheme, Val(2), ψ, grid, args...)
+            β  = $biased_β(ψs, scheme, Val(1))
+            C  = FT($coeff(scheme, Val(1)))
+            α  = @fastmath C / (β + FT(ε))^2
+            ψ̅  = $biased_p(scheme, Val(1), ψs, Nothing, Val($val), idx, loc) 
+            glob += add_global_smoothness(glob, β, Val(N), Val(1))
+            sol1 += ψ̅ * C
+            wei1 += C
+            sol2 += ψ̅ * α  
+            wei2 += α
+
+            # Is glob squared here?
+            return (sol1 + sol2 * glob) / (wei1 + wei2 * glob)
+        end
+
+        @inline function $biased_interpolate(i, j, k, grid, 
+                                             scheme::WENO{2, FT}, 
+                                             ψ, idx, loc, ::VelocityStencil, u, v, args...) where {FT}
+
+
+            ψs = $stencil(i, j, k, scheme, Val(1), ψ, grid, u, v, args...)
+            us = $stencil_u(i, j, k, scheme, Val(1), Val($val), grid, u)
+            vs = $stencil_v(i, j, k, scheme, Val(1), Val($val), grid, v)
+            βu = $biased_β(us, scheme, Val(0))
+            βv = $biased_β(vs, scheme, Val(0))
+            βU = 0.5 * (βu + βv)
+            C  = FT($coeff(scheme, Val(0)))
+            α  = @fastmath C / (βU + FT(ε))^2
+            ψ̅  = $biased_p(scheme, Val(0), ψs, Nothing, Val($val), idx, loc) 
+            glob = β
+            sol1 = ψ̅ * C
+            wei1 = C
+            sol2 = ψ̅ * α  
+            wei2 = α
+
+            ψs = $stencil(i, j, k, scheme, Val(2), ψ, grid, u, v, args...)
+            us = $stencil_u(i, j, k, scheme, Val(2), Val($val), grid, u)
+            vs = $stencil_v(i, j, k, scheme, Val(2), Val($val), grid, v)
+            βu = $biased_β(us, scheme, Val(1))
+            βv = $biased_β(vs, scheme, Val(1))
+            βU = 0.5 * (βu + βv)
+            C  = FT($coeff(scheme, Val(1)))
+            α  = @fastmath C / (βU + FT(ε))^2
+            ψ̅  = $biased_p(scheme, Val(1), ψs, Nothing, Val($val), idx, loc) 
+            glob += add_global_smoothness(glob, β, Val(N), Val(1))
+            sol1 += ψ̅ * C
+            wei1 += C
+            sol2 += ψ̅ * α  
+            wei2 += α
+
+
+            # Is glob squared here?
+            return (sol1 + sol2 * glob) / (wei1 + wei2 * glob)
+        end
+
+        @inline function $biased_interpolate(i, j, k, grid, 
+                                             scheme::WENO{2, FT}, 
+                                             ψ, idx, loc, VI::FunctionStencil, args...) where {FT}
+
+            ψs = $stencil(i, j, k, scheme, Val(1), ψ, grid, args...)
+            ϕs = $stencil(i, j, k, scheme, Val(1), VI.func, grid, args...)
+            βϕ = $biased_β(ϕs, scheme, Val(0))
+            C  = FT($coeff(scheme, Val(0)))
+            α  = @fastmath C / (βϕ + FT(ε))^2
+            ψ̅  = $biased_p(scheme, Val(0), ψs, Nothing, Val($val), idx, loc) 
+            glob = β
+            sol1 = ψ̅ * C
+            wei1 = C
+            sol2 = ψ̅ * α  
+            wei2 = α
+
+            ψs = $stencil(i, j, k, scheme, Val(2), ψ, grid, args...)
+            ϕs = $stencil(i, j, k, scheme, Val(2), VI.func, grid, args...)
+            βϕ = $biased_β(ϕs, scheme, Val(1))
+            C  = FT($coeff(scheme, Val(1)))
+            α  = @fastmath C / (βϕ + FT(ε))^2
+            ψ̅  = $biased_p(scheme, Val(1), ψs, Nothing, Val($val), idx, loc) 
+            glob += add_global_smoothness(glob, β, Val(N), Val(1))
+            sol1 += ψ̅ * C
+            wei1 += C
+            sol2 += ψ̅ * α  
+            wei2 += α
+
+            # Is glob squared here?
+            return (sol1 + sol2 * glob) / (wei1 + wei2 * glob)
+        end
+    end
+end
