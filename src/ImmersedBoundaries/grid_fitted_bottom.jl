de4ed16d4c9a6164b67cba9326be9dc83fe424bd
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-03-14 16:23:24 +0200

    elaborate bit more on immersed_condition

--- src/ImmersedBoundaries/grid_fitted_bottom.jl
+++ src/ImmersedBoundaries/grid_fitted_bottom.jl
@@ -42,11 +42,15 @@ Keyword Arguments
 * `bottom_height`: an array or function that gives the height of the
                    bottom in absolute ``z`` coordinates.
 
-* `immersed_condition`: determine whether the part of the domain that is 
+* `immersed_condition`: Determine whether the part of the domain that is 
                         immersed are all the cell centers that lie below
                         `bottom_height` (`CenterImmersedCondition()`; default)
                         or all the cell faces that lie below `bottom_height`
-                        (`InterfaceImmersedCondition()`).
+                        (`InterfaceImmersedCondition()`). The only purpose of
+                        `immersed_condition` to allow `GridFittedBottom` and
+                        `PartialCellBottom` to have the same behavior when the
+                        minimum fractional cell height for partial cells is set
+                        to 0.
 """
 GridFittedBottom(bottom_height) = GridFittedBottom(bottom_height, CenterImmersedCondition())
 
