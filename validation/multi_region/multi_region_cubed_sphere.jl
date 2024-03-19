c43ed70c880f8f8635f235155d31bc6865d3e7c0
Sid <siddhartha.bishnu@gmail.com>
2024-03-18 22:28:01 -0400

    Computed difference in cs grid metrics with MITgcm

--- validation/multi_region/multi_region_cubed_sphere.jl
+++ validation/multi_region/multi_region_cubed_sphere.jl
@@ -5,7 +5,7 @@ using Oceananigans.Models.HydrostaticFreeSurfaceModels: fill_cubed_sphere_halo_r
 Install Imaginocean.jl from GitHub:
 using Pkg; Pkg.add(url="https://github.com/navidcy/Imaginocean.jl", rev="main")
 =#
-using CairoMakie, Imaginocean, JLD2
+using OffsetArrays, CairoMakie, Imaginocean, JLD2
 
 # First create a conformal cubed sphere grid.
 
@@ -234,3 +234,55 @@ jldopen("cs_grid_difference.jld2", "w") do file
 end
 
 close(cubed_sphere_dynamics_branch_cs_grid_file)
+
+Nx, Ny, Nz = 32, 32, 1
+grid = ConformalCubedSphereGrid(; panel_size = (Nx, Ny, Nz), z = (-1, 0), radius=6370e3, horizontal_direction_halo = 4,
+                                  z_halo = 1)
+Hx, Hy, Hz = grid.Hx, grid.Hy, grid.Hz
+
+MITgcm_cs_grid_file = jldopen("jmc_cubed_sphere_32_grid_with_4_halos.jld2")
+
+λᶜᶜᵃ_difference_MITgcm  = zeros(Nx+2Hx, Ny+2Hy, 6)
+λᶠᶠᵃ_difference_MITgcm  = zeros(Nx+2Hx, Ny+2Hy, 6)
+φᶜᶜᵃ_difference_MITgcm  = zeros(Nx+2Hx, Ny+2Hy, 6)
+φᶠᶠᵃ_difference_MITgcm  = zeros(Nx+2Hx, Ny+2Hy, 6)
+Δxᶠᶜᵃ_difference_MITgcm = zeros(Nx+2Hx, Ny+2Hy, 6)
+Δxᶜᶠᵃ_difference_MITgcm = zeros(Nx+2Hx, Ny+2Hy, 6)
+Δyᶠᶜᵃ_difference_MITgcm = zeros(Nx+2Hx, Ny+2Hy, 6)
+Δyᶜᶠᵃ_difference_MITgcm = zeros(Nx+2Hx, Ny+2Hy, 6)
+Δyᶠᶠᵃ_difference_MITgcm = zeros(Nx+2Hx, Ny+2Hy, 6)
+Azᶜᶜᵃ_difference_MITgcm = zeros(Nx+2Hx, Ny+2Hy, 6)
+Azᶠᶜᵃ_difference_MITgcm = zeros(Nx+2Hx, Ny+2Hy, 6)
+Azᶜᶠᵃ_difference_MITgcm = zeros(Nx+2Hx, Ny+2Hy, 6)
+Azᶠᶠᵃ_difference_MITgcm = zeros(Nx+2Hx, Ny+2Hy, 6)
+
+jldopen("MITgcm_cs_grid_difference.jld2", "w") do file
+    for region in 1:6
+        λᶜᶜᵃ_difference_MITgcm[:, :, region]  =  grid[region].λᶜᶜᵃ - OffsetArray(MITgcm_cs_grid_file["face" * string(region) * "/λᶜᶜᵃ" ], 1-Hx:Nx+Hx, 1-Hy:Ny+Hy)
+        λᶠᶠᵃ_difference_MITgcm[:, :, region]  =  grid[region].λᶠᶠᵃ - OffsetArray(MITgcm_cs_grid_file["face" * string(region) * "/λᶠᶠᵃ" ], 1-Hx:Nx+Hx, 1-Hy:Ny+Hy)
+        φᶜᶜᵃ_difference_MITgcm[:, :, region]  =  grid[region].φᶜᶜᵃ - OffsetArray(MITgcm_cs_grid_file["face" * string(region) * "/φᶜᶜᵃ" ], 1-Hx:Nx+Hx, 1-Hy:Ny+Hy)
+        φᶠᶠᵃ_difference_MITgcm[:, :, region]  =  grid[region].φᶠᶠᵃ - OffsetArray(MITgcm_cs_grid_file["face" * string(region) * "/φᶠᶠᵃ" ], 1-Hx:Nx+Hx, 1-Hy:Ny+Hy)
+        Δxᶠᶜᵃ_difference_MITgcm[:, :, region] = grid[region].Δxᶠᶜᵃ - OffsetArray(MITgcm_cs_grid_file["face" * string(region) * "/Δxᶠᶜᵃ"], 1-Hx:Nx+Hx, 1-Hy:Ny+Hy)
+        Δxᶜᶠᵃ_difference_MITgcm[:, :, region] = grid[region].Δxᶜᶠᵃ - OffsetArray(MITgcm_cs_grid_file["face" * string(region) * "/Δxᶜᶠᵃ"], 1-Hx:Nx+Hx, 1-Hy:Ny+Hy)
+        Δyᶠᶜᵃ_difference_MITgcm[:, :, region] = grid[region].Δyᶠᶜᵃ - OffsetArray(MITgcm_cs_grid_file["face" * string(region) * "/Δyᶠᶜᵃ"], 1-Hx:Nx+Hx, 1-Hy:Ny+Hy)
+        Δyᶜᶠᵃ_difference_MITgcm[:, :, region] = grid[region].Δyᶜᶠᵃ - OffsetArray(MITgcm_cs_grid_file["face" * string(region) * "/Δyᶜᶠᵃ"], 1-Hx:Nx+Hx, 1-Hy:Ny+Hy)
+        Azᶜᶜᵃ_difference_MITgcm[:, :, region] = grid[region].Azᶜᶜᵃ - OffsetArray(MITgcm_cs_grid_file["face" * string(region) * "/Azᶜᶜᵃ"], 1-Hx:Nx+Hx, 1-Hy:Ny+Hy)
+        Azᶠᶜᵃ_difference_MITgcm[:, :, region] = grid[region].Azᶠᶜᵃ - OffsetArray(MITgcm_cs_grid_file["face" * string(region) * "/Azᶠᶜᵃ"], 1-Hx:Nx+Hx, 1-Hy:Ny+Hy)
+        Azᶜᶠᵃ_difference_MITgcm[:, :, region] = grid[region].Azᶜᶠᵃ - OffsetArray(MITgcm_cs_grid_file["face" * string(region) * "/Azᶜᶠᵃ"], 1-Hx:Nx+Hx, 1-Hy:Ny+Hy)
+        Azᶠᶠᵃ_difference_MITgcm[:, :, region] = grid[region].Azᶠᶠᵃ - OffsetArray(MITgcm_cs_grid_file["face" * string(region) * "/Azᶠᶠᵃ"], 1-Hx:Nx+Hx, 1-Hy:Ny+Hy)
+        file["λᶜᶜᵃ_difference_MITgcm/" * string(region)]  =  λᶜᶜᵃ_difference_MITgcm[:, :, region]
+        file["λᶠᶠᵃ_difference_MITgcm/" * string(region)]  =  λᶠᶠᵃ_difference_MITgcm[:, :, region]
+        file["φᶜᶜᵃ_difference_MITgcm/" * string(region)]  =  φᶜᶜᵃ_difference_MITgcm[:, :, region]
+        file["φᶠᶠᵃ_difference_MITgcm/" * string(region)]  =  φᶠᶠᵃ_difference_MITgcm[:, :, region]
+        file["Δxᶠᶜᵃ_difference_MITgcm/" * string(region)] = Δxᶠᶜᵃ_difference_MITgcm[:, :, region]
+        file["Δxᶜᶠᵃ_difference_MITgcm/" * string(region)] = Δxᶜᶠᵃ_difference_MITgcm[:, :, region]
+        file["Δyᶠᶜᵃ_difference_MITgcm/" * string(region)] = Δyᶠᶜᵃ_difference_MITgcm[:, :, region]
+        file["Δyᶜᶠᵃ_difference_MITgcm/" * string(region)] = Δyᶜᶠᵃ_difference_MITgcm[:, :, region]
+        file["Azᶜᶜᵃ_difference_MITgcm/" * string(region)] = Azᶜᶜᵃ_difference_MITgcm[:, :, region]
+        file["Azᶠᶜᵃ_difference_MITgcm/" * string(region)] = Azᶠᶜᵃ_difference_MITgcm[:, :, region]
+        file["Azᶜᶠᵃ_difference_MITgcm/" * string(region)] = Azᶜᶠᵃ_difference_MITgcm[:, :, region]
+        file["Azᶠᶠᵃ_difference_MITgcm/" * string(region)] = Azᶠᶠᵃ_difference_MITgcm[:, :, region]
+    end
+end
+
+close(MITgcm_cs_grid_file)
