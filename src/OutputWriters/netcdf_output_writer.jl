71dab3a1981436ea815daff7586b0ecbe383c6ff
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-03-24 07:14:50 +0200

    fix doctest

--- src/OutputWriters/netcdf_output_writer.jl
+++ src/OutputWriters/netcdf_output_writer.jl
@@ -268,7 +268,7 @@ NetCDFOutputWriter scheduled on TimeInterval(1 minute):
 ├── 2 outputs: (c, u)
 └── array type: Array{Float64}
 ├── file_splitting: NoFileSplitting
-└── file size: 14.9 KiB
+└── file size: 14.8 KiB
 ```
 
 ```jldoctest netcdf1
