cca34059817c48bb02631e0648cbb228f0296ed1
Francis Poulin <fpoulin@uwaterloo.ca>
2024-04-01 22:00:02 -0400

    resting example runs

--- validation/immersed_boundaries/resting_stratified_bumpy_ocean.jl
+++ validation/immersed_boundaries/resting_stratified_bumpy_ocean.jl
@@ -5,12 +5,9 @@ using Oceananigans.ImmersedBoundaries: PartialCellBottom
 using Printf
 using GLMakie
 
-arch = CPU()
-tracer_advection = CenteredSecondOrder()
-momentum_advection = CenteredSecondOrder()
-
-underlying_grid = RectilinearGrid(arch,
-                                  size=(128, 64), halo=(3, 3), 
+underlying_grid = RectilinearGrid(CPU(),
+                                  size = (128, 64), 
+                                  halo=(4, 4), 
                                   y = (-1, 1),
                                   z = (-1, 0),
                                   topology=(Flat, Periodic, Bounded))
@@ -21,9 +18,10 @@ L = 0.25 # bump width
 @inline h(y) = h₀ * exp(- y^2 / L^2)
 @inline seamount(y) = - 1 + h(y)
 
+minimum_fractional_cell_height = 0.2
 immersed_boundaries = [
                        PartialCellBottom(seamount;
-                                         minimum_fractional_cell_height = 0.2),
+                                         minimum_fractional_cell_height),
                        GridFittedBottom(seamount)
                       ]
 
@@ -32,7 +30,7 @@ v = []
 
 function progress(sim)
     vmax = maximum(abs, sim.model.velocities.v)
-    @info @sprintf("Iter: %d, time: %.2e, max|v|: %.2e",
+    @info @sprintf("Iter: %d, time: %.2e, max|v|: %.8e",
                    iteration(sim), time(sim), vmax)
 
     return nothing
@@ -44,19 +42,18 @@ for ib in immersed_boundaries
     @show grid
 
     model = HydrostaticFreeSurfaceModel(; grid,
-                                        tracer_advection,
-                                        momentum_advection,
-                                        coriolis = FPlane(f=0.1),
+                                        tracer_advection = WENO(),
+                                        momentum_advection = WENO(),
+                                        coriolis = FPlane(f=1),
                                         tracers = :b,
                                         buoyancy = BuoyancyTracer())
 
     N² = 1
     bᵢ(y, z) = N² * z
-    set!(model, b = bᵢ)
+    set!(model, u = 0, b = bᵢ)
 
-    simulation = Simulation(model; Δt=1e-3, stop_iteration=1000)
+    simulation = Simulation(model; Δt=1e-3, stop_iteration=3000)
     simulation.callbacks[:p] = Callback(progress, IterationInterval(100))
-
 
     run!(simulation)
 
@@ -74,14 +71,10 @@ v_full    = v[2]
 
 fig = Figure(size=(1200, 1800))
 
-partial_cell_title = @sprintf("PartialCellBottom with ϵ = %.1f", minimum_fractional_Δz)
-ax_bp = Axis(fig[1, 2], title=partial_cell_title)
-ax_bf = Axis(fig[2, 2], title="GridFittedBottom")
-ax_bd = Axis(fig[3, 2], title="difference; GridFitted - PartialCell")
-
-# ax_vp = Axis(fig[1, 3])
-# ax_vf = Axis(fig[2, 3])
-# ax_vd = Axis(fig[3, 3])
+#partial_cell_title = @sprintf("PartialCellBottom with ϵ = %.1f", minimum_fractional_Δz)
+ax_bp = Axis(fig[1, 2], title="b PartialCellBottom")
+ax_bf = Axis(fig[2, 2], title="b GridFittedBottom")
+ax_bd = Axis(fig[3, 2], title="b Difference (GridFitted - PartialCell)")
 
 color = (:black, 0.5)
 linewidth = 3
@@ -98,17 +91,27 @@ Colorbar(fig[2, 1], hmbf, label="Buoyancy", flipaxis=false)
 hmbd = heatmap!(ax_bd, Δb)
 Colorbar(fig[3, 1], hmbd, label="Buoyancy", flipaxis=false)
 
-#=
+save("resting_bumpy_ocean_b.png", fig)
+
+#display(fig)
+
+fig = Figure(size=(1200, 1800))
+
+ax_vp = Axis(fig[1, 2], title="v PartialCellBottom")
+ax_vf = Axis(fig[2, 2], title="v GridFittedBottom")
+ax_vd = Axis(fig[3, 2], title="v Difference (GridFitted - PartialCell)")
+
 hmvp = heatmap!(ax_vp, v_partial)
-#contour!(ax_vp, v_partial, levels=15)
-#Colorbar(fig[1, 1], hmvp)
+contour!(ax_vp, v_partial; levels, color, linewidth)
+Colorbar(fig[1, 1], hmvp, label="v", flipaxis=false)
 
 hmvf = heatmap!(ax_vf, v_full)
-#contour!(ax_vf, v_full, levels=15)
-#Colorbar(fig[2, 1], hmvf)
+contour!(ax_vf, v_full; levels, color, linewidth)
+Colorbar(fig[2, 1], hmvf, label="v", flipaxis=false)
 
 hmvd = heatmap!(ax_vd, Δv)
-#Colorbar(fig[3, 1], hmvd)
-=#
+Colorbar(fig[3, 1], hmvd, label="v", flipaxis=false)
 
-display(fig)
+save("resting_bumpy_ocean_v.png", fig)
+
+#display(fig)
