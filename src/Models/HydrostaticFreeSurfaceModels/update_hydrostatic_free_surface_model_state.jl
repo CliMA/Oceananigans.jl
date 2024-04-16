37e66e2e35c63c199fe6b13f82fbd333b209335e
Sid <siddhartha.bishnu@gmail.com>
2024-04-16 18:06:29 -0400

    Fix major bug

--- src/Models/HydrostaticFreeSurfaceModels/update_hydrostatic_free_surface_model_state.jl
+++ src/Models/HydrostaticFreeSurfaceModels/update_hydrostatic_free_surface_model_state.jl
@@ -34,7 +34,10 @@ function update_state!(model::HydrostaticFreeSurfaceModel, grid, callbacks; comp
     @apply_regionally update_model_field_time_series!(model, model.clock)
 
     fill_halo_regions!(prognostic_fields(model), model.clock, fields(model); async = true)
+    #=
     @apply_regionally replace_horizontal_vector_halos!(model.velocities, model.grid)
+    =#
+    grid isa ConformalCubedSphereGrid ? fill_halo_regions!((model.velocities.u, model.velocities.v)) : nothing
     @apply_regionally compute_auxiliaries!(model)
 
     fill_halo_regions!(model.diffusivity_fields; only_local_halos = true)
