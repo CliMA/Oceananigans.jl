809573389f6eeac40e71770839311bd9e6642367
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-04-17 14:29:33 +0300

    use test repo glwagner/OceananigansArtifacts.jl for regression tests

--- test/data_dependencies.jl
+++ test/data_dependencies.jl
@@ -15,7 +15,7 @@ datadep"cubed_sphere_32_grid"
 
 # Downloading the regression fields
 
-path = "https://github.com/CliMA/OceananigansArtifacts.jl/raw/main/data_for_regression_tests/"
+path = "https://github.com/glwagner/OceananigansArtifacts.jl/raw/main/data_for_regression_tests/"
 
 dh = DataDep("regression_test_data",
     "Data for Regression tests",
