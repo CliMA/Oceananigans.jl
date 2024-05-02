d67ff597b9ddcc504a56844a1bed945080320d40
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-05-02 11:41:56 -0400

    allow some scalar operations

--- src/Grids/orthogonal_spherical_shell_grid.jl
+++ src/Grids/orthogonal_spherical_shell_grid.jl
@@ -646,11 +646,11 @@ function fill_metric_halo_regions_x!(metric, ℓx, ℓy, tx::AbstractTopology, t
         for j in 1:Ny⁺
             # fill west halos
             for i in 0:-1:-Hx+1
-                metric[i, j] = metric[Nx+i, j]
+                CUDA.@allowscalar metric[i, j] = metric[Nx+i, j]
             end
             # fill east halos
             for i in Nx⁺+1:Nx⁺+Hx
-                metric[i, j] = metric[i-Nx, j]
+                CUDA.@allowscalar metric[i, j] = metric[i-Nx, j]
             end
         end
     end
@@ -673,11 +673,11 @@ function fill_metric_halo_regions_y!(metric, ℓx, ℓy, tx, ty::BoundedTopology
         for i in 1:Nx⁺
             # fill south halos
             for j in 0:-1:-Hy+1
-                metric[i, j] = metric[i, j+1]
+                CUDA.@allowscalar metric[i, j] = metric[i, j+1]
             end
             # fill north halos
             for j in Ny⁺+1:Ny⁺+Hy
-                metric[i, j] = metric[i, j-1]
+                CUDA.@allowscalar metric[i, j] = metric[i, j-1]
             end
         end
     end
@@ -694,11 +694,11 @@ function fill_metric_halo_regions_y!(metric, ℓx, ℓy, tx, ty::AbstractTopolog
         for i in 1:Nx⁺
             # fill south halos
             for j in 0:-1:-Hy+1
-                metric[i, j] = metric[i, Ny+j]
+                CUDA.@allowscalar metric[i, j] = metric[i, Ny+j]
             end
             # fill north halos
             for j in Ny⁺+1:Ny⁺+Hy
-                metric[i, j] = metric[i, j-Ny]
+                CUDA.@allowscalar metric[i, j] = metric[i, j-Ny]
             end
         end
     end
@@ -721,16 +721,16 @@ function fill_metric_halo_corner_regions!(metric, ℓx, ℓy, tx, ty, Nx, Ny, Hx
 
     @inbounds begin
         for j in 0:-1:-Hy+1, i in 0:-1:-Hx+1
-            metric[i, j] = (metric[i+1, j] + metric[i, j+1]) / 2
+            CUDA.@allowscalar metric[i, j] = (metric[i+1, j] + metric[i, j+1]) / 2
         end
         for j in Ny⁺+1:Ny⁺+Hy, i in 0:-1:-Hx+1
-            metric[i, j] = (metric[i+1, j] + metric[i, j-1]) / 2
+            CUDA.@allowscalar metric[i, j] = (metric[i+1, j] + metric[i, j-1]) / 2
         end
         for j in 0:-1:-Hy+1, i in Nx⁺+1:Nx⁺+Hx
-            metric[i, j] = (metric[i-1, j] + metric[i, j+1]) / 2
+            CUDA.@allowscalar metric[i, j] = (metric[i-1, j] + metric[i, j+1]) / 2
         end
         for j in Ny⁺+1:Ny⁺+Hy, i in Nx⁺+1:Nx⁺+Hx
-            metric[i, j] = (metric[i-1, j] + metric[i, j-1]) / 2
+            CUDA.@allowscalar metric[i, j] = (metric[i-1, j] + metric[i, j-1]) / 2
         end
     end
 
