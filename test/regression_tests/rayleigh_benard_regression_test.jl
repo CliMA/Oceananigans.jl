2328b5e22f3e8eff50477ff3c033bb608c3b8d68
Gregory Wagner <wagner.greg@gmail.com>
2024-05-05 14:50:46 -0600

    Change regression tests to use AB2

--- test/regression_tests/rayleigh_benard_regression_test.jl
+++ test/regression_tests/rayleigh_benard_regression_test.jl
@@ -47,6 +47,7 @@ function run_rayleigh_benard_regression_test(arch, grid_type)
                                    bottom = BoundaryCondition(Value, Δb))
 
     model = NonhydrostaticModel(; grid,
+                                timestepper = :QuasiAdamsBashforth2,
                                 closure = ScalarDiffusivity(ν=ν, κ=κ),
                                 tracers = (:b, :c),
                                 buoyancy = Buoyancy(model=BuoyancyTracer()),
