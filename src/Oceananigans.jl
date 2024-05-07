a5e5e19358ab2366cd026ea15ab93909325651b7
Gregory Wagner <wagner.greg@gmail.com>
2024-05-07 13:07:56 -0600

    Export cumulative integral

--- src/Oceananigans.jl
+++ src/Oceananigans.jl
@@ -41,7 +41,7 @@ export
 
     # Fields and field manipulation
     Field, CenterField, XFaceField, YFaceField, ZFaceField,
-    Average, Integral, Reduction, Accumulation, BackgroundField,
+    Average, Integral, CumulativeIntegral, Reduction, Accumulation, BackgroundField,
     interior, set!, compute!, regrid!, location,
 
     # Forcing functions
