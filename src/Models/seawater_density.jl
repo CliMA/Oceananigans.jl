3c345015a2351087f2625b741fb6b42df23aec69
Gregory Wagner <wagner.greg@gmail.com>
2024-05-08 00:15:08 -0600

    Fix doctest for seawater density

--- src/Models/seawater_density.jl
+++ src/Models/seawater_density.jl
@@ -81,7 +81,7 @@ SeawaterBuoyancy{Float64}:
 julia> model = NonhydrostaticModel(; grid, buoyancy, tracers)
 NonhydrostaticModel{CPU, RectilinearGrid}(time = 0 seconds, iteration = 0)
 ├── grid: 1×1×100 RectilinearGrid{Float64, Flat, Flat, Bounded} on CPU with 0×0×3 halo
-├── timestepper: QuasiAdamsBashforth2TimeStepper
+├── timestepper: RungeKutta3TimeStepper
 ├── advection scheme: Centered reconstruction order 2
 ├── tracers: (T, S)
 ├── closure: Nothing
