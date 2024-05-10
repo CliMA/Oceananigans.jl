bf024772d47817d639b3d37ba099ea55c808c4c7
Gregory Wagner <wagner.greg@gmail.com>
2024-05-09 20:47:13 -0600

    Fix docstring for CumulativeIntegral

--- src/AbstractOperations/metric_field_reductions.jl
+++ src/AbstractOperations/metric_field_reductions.jl
@@ -151,6 +151,13 @@ CumulativeIntegral of BinaryOperation at (Center, Center, Center) over dims 3
     └── grid: 1×1×8 RectilinearGrid{Float64, Flat, Flat, Bounded} on CPU with 0×0×3 halo
 
 julia> C = compute!(Field(C_op))
+1×1×8 Field{Center, Center, Center} on RectilinearGrid on CPU
+├── data: OffsetArrays.OffsetArray{Float64, 3, Array{Float64, 3}}, size: (1, 1, 8)
+├── grid: 1×1×8 RectilinearGrid{Float64, Flat, Flat, Bounded} on CPU with 0×0×3 halo
+├── operand: CumulativeIntegral of BinaryOperation at (Center, Center, Center) over dims 3
+├── status: time=0.0
+└── data: 1×1×14 OffsetArray(::Array{Float64, 3}, 1:1, 1:1, -2:11) with eltype Float64 with indices 1:1×1:1×-2:11
+    └── max=0.5, min=0.0078125, mean=0.199219
 
 julia> C[1, 1, 8]
 0.5
