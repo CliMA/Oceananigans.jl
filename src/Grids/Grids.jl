b46cdba984bc7041d18360a6472f343b7b0dd342
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-06-19 09:20:34 -0400

    bugfix

--- src/Grids/Grids.jl
+++ src/Grids/Grids.jl
@@ -154,6 +154,6 @@ include("grid_generation.jl")
 include("rectilinear_grid.jl")
 include("orthogonal_spherical_shell_grid.jl")
 include("latitude_longitude_grid.jl")
-include("ConformalCubedSpherePanelGrid_grid.jl")
+include("conformal_cubed_sphere_panel_grid.jl")
 
 end # module
