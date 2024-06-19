89c6c7ecd038769e11b84d94362a9bdadb667830
Sid <siddhartha.bishnu@gmail.com>
2024-06-19 18:33:00 -0400

    Refine vorticity computation for GPU compatibility

--- src/Operators/vorticity_operators.jl
+++ src/Operators/vorticity_operators.jl
@@ -17,10 +17,10 @@ end
 @inline on_corner(i, j, grid) = on_south_west_corner(i, j, grid) | on_south_east_corner(i, j, grid) | on_north_west_corner(i, j, grid) | on_north_east_corner(i, j, grid)
 
 # South-west, south-east, north-west, north-east corners
-@inline on_south_west_corner(i, j, grid) = (i == 1 && j == 1)
-@inline on_south_east_corner(i, j, grid) = (i == grid.Nx+1 && j == 1)
-@inline on_north_east_corner(i, j, grid) = (i == grid.Nx+1 && j == grid.Ny+1)
-@inline on_north_west_corner(i, j, grid) = (i == 1 && j == grid.Ny+1)
+@inline on_south_west_corner(i, j, grid) = (i == 1) & (j == 1)
+@inline on_south_east_corner(i, j, grid) = (i == grid.Nx+1) & (j == 1)
+@inline on_north_east_corner(i, j, grid) = (i == grid.Nx+1) & (j == grid.Ny+1)
+@inline on_north_west_corner(i, j, grid) = (i == 1) & (j == grid.Ny+1)
 
 #####
 ##### Vertical circulation at the corners of the cubed sphere needs to treated in a special manner.
@@ -33,16 +33,17 @@ end
 The vertical circulation associated with horizontal velocities ``u`` and ``v``.
 """
 
-function Γᶠᶠᶜ(i, j, k, grid::OrthogonalSphericalShellGrid, u, v) 
+@inline function Γᶠᶠᶜ(i, j, k, grid::OrthogonalSphericalShellGrid, u, v) 
     Hx, Hy = grid.Hx, grid.Hy
-    if i == 1 - Hx || j == 1 - Hy
-        Γ = NaN
-    elseif on_south_west_corner(i, j, grid) || on_north_west_corner(i, j, grid)
-        Γ = Δy_qᶜᶠᶜ(i, j, k, grid, v) - Δx_qᶠᶜᶜ(i, j, k, grid, u) + Δx_qᶠᶜᶜ(i, j-1, k, grid, u)
-    elseif on_south_east_corner(i, j, grid) || on_north_east_corner(i, j, grid)
-        Γ = - Δy_qᶜᶠᶜ(i-1, j, k, grid, v) + Δx_qᶠᶜᶜ(i, j-1, k, grid, u) - Δx_qᶠᶜᶜ(i, j, k, grid, u)
-    else
-        Γ = δxᶠᶠᶜ(i, j, k, grid, Δy_qᶜᶠᶜ, v) - δyᶠᶠᶜ(i, j, k, grid, Δx_qᶠᶜᶜ, u)
-    end
+    Γ = ifelse((i == 1 - Hx) | (j == 1 - Hy),
+               NaN,
+               ifelse(on_south_west_corner(i, j, grid) | on_north_west_corner(i, j, grid),
+                      Δy_qᶜᶠᶜ(i, j, k, grid, v) - Δx_qᶠᶜᶜ(i, j, k, grid, u) + Δx_qᶠᶜᶜ(i, j-1, k, grid, u),
+                      ifelse(on_south_east_corner(i, j, grid) | on_north_east_corner(i, j, grid),
+                             - Δy_qᶜᶠᶜ(i-1, j, k, grid, v) + Δx_qᶠᶜᶜ(i, j-1, k, grid, u) - Δx_qᶠᶜᶜ(i, j, k, grid, u),
+                             δxᶠᶠᶜ(i, j, k, grid, Δy_qᶜᶠᶜ, v) - δyᶠᶠᶜ(i, j, k, grid, Δx_qᶠᶜᶜ, u)
+                            )
+                     )
+              )
     return Γ
 end
