719e4c299607b6b36bb39a9281761a2eb6841ff0
Gregory Wagner <wagner.greg@gmail.com>
2024-07-01 19:43:36 -0400

    Also use total_c in the immersed flux divergence

--- src/Models/NonhydrostaticModels/nonhydrostatic_tendency_kernel_functions.jl
+++ src/Models/NonhydrostaticModels/nonhydrostatic_tendency_kernel_functions.jl
@@ -266,7 +266,7 @@ velocity components, tracer fields, and precalculated diffusivities where applic
     return ( - div_Uc(i, j, k, grid, advection, total_velocities, c)
              - div_Uc(i, j, k, grid, advection, velocities, background_fields_c)
              - ∇_dot_qᶜ(i, j, k, grid, closure, diffusivities, val_tracer_index, total_c, clock, model_fields, buoyancy)
-             - immersed_∇_dot_qᶜ(i, j, k, grid, c, c_immersed_bc, closure, diffusivities, val_tracer_index, clock, model_fields)
+             - immersed_∇_dot_qᶜ(i, j, k, grid, total_c, c_immersed_bc, closure, diffusivities, val_tracer_index, clock, model_fields)
              + biogeochemical_transition(i, j, k, grid, biogeochemistry, val_tracer_name, clock, model_fields)
              + forcing(i, j, k, grid, clock, model_fields))
 end
