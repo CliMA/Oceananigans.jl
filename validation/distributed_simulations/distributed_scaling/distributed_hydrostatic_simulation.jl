072d39bb39b68102b39d586ea9ed3414f34e893e
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-07-21 12:26:46 +1000

    formatting

--- validation/distributed_simulations/distributed_scaling/distributed_hydrostatic_simulation.jl
+++ validation/distributed_simulations/distributed_scaling/distributed_hydrostatic_simulation.jl
@@ -23,25 +23,25 @@ function double_drake_bathymetry(λ, φ)
 end
 
 function run_hydrostatic_simulation!(grid_size, ranks, FT::DataType = Float64; 
-                                     output_name = nothing, 
+                                     output_name = nothing,
                                      timestepper = :QuasiAdamsBashforth2,
                                      CFL = 0.35,
                                      barotropic_CFL = 0.75)
-        
+
     arch  = Distributed(GPU(), FT; partition = Partition(ranks...))
     grid  = LatitudeLongitudeGrid(arch; size = grid_size, longitude = (-180, 180),
-			    		          latitude = (-75, 75),
-					              z = (-5500, 0),
+                                  latitude = (-75, 75),
+                                  z = (-5500, 0),
                                   halo = (7, 7, 7))
 
     grid  = ImmersedBoundaryGrid(grid, GridFittedBottom(double_drake_bathymetry)) 
 
     momentum_advection = WENOVectorInvariant(FT)
     tracer_advection   = WENO(grid, order = 7)
-    
+
     buoyancy = SeawaterBuoyancy(FT; equation_of_state = TEOS10EquationOfState(FT))
     coriolis = HydrostaticSphericalCoriolis(FT)
-    closure  = CATKEVerticalDiffusivity(FT) 
+    closure  = CATKEVerticalDiffusivity(FT)
 
     max_Δt = 45 * 48 / grid.Δλᶠᵃᵃ
 
@@ -50,30 +50,30 @@ function run_hydrostatic_simulation!(grid_size, ranks, FT::DataType = Float64;
     model = HydrostaticFreeSurfaceModel(; grid, 
                                           momentum_advection,
                                           tracer_advection,
-                                          coriolis, 
+                                          coriolis,
                                           closure,
                                           free_surface,
                                           tracers = (:T, :S, :e),
-                                          buoyancy, 
+                                          buoyancy,
                                           timestepper)
 
     wtime = Ref(time_ns())
-    
-    function progress(sim) 
-        @info @sprintf("iteration: %d, Δt: %2e, wall time: %s (|u|, |v|, |w|): %.2e %.2e %.2e, b: %.2e \n", 
+
+    function progress(sim)
+        @info @sprintf("iteration: %d, Δt: %2e, wall time: %s (|u|, |v|, |w|): %.2e %.2e %.2e, b: %.2e \n",
               sim.model.clock.iteration, sim.Δt, prettytime((time_ns() - wtime[])*1e-9),
-              maximum(abs, sim.model.velocities.u), maximum(abs, sim.model.velocities.v), 
+              maximum(abs, sim.model.velocities.u), maximum(abs, sim.model.velocities.v),
               maximum(abs, sim.model.velocities.w), maximum(abs, sim.model.tracers.b))
        wtime[] = time_ns()
     end
 
     simulation = Simulation(model; Δt=max_Δt, stop_time = 20days, stop_iteration = 100)
-                        
+
     # Adaptive time-stepping
     wizard = TimeStepWizard(cfl=CFL; max_change=1.1, min_Δt=10, max_Δt)
     simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
     simulation.callbacks[:progress] = Callback(progress, IterationInterval(10))
-   
+
     rank = MPI.Comm_rank(MPI.COMM_WORLD)
 
     if !isnothing(output_name)
@@ -82,7 +82,7 @@ function run_hydrostatic_simulation!(grid_size, ranks, FT::DataType = Float64;
                                                             schedule = TimeInterval(1day),
                                                             overwrite_existing = true)
     end
-    
+
     run!(simulation)
 
     return nothing
