6fab8dfa46a6c462e5c0c175fcf9223c34f2a976
Jago Strong-Wright <jagoosw@protonmail.com>
2024-07-24 10:09:04 +0100

    turns out we did need this fallback

--- src/BoundaryConditions/fill_halo_regions_open.jl
+++ src/BoundaryConditions/fill_halo_regions_open.jl
@@ -92,10 +92,18 @@ end
 
 # Open boundary condition fallback for `CenterField`
 
-@inline   _fill_west_halo!(j, k, grid, c, bc::OBC, loc::Tuple{Center, Center, Center}, args...) = @inbounds c[1, j, k]           = getbc(bc, j, k, grid, args...)
-@inline   _fill_east_halo!(j, k, grid, c, bc::OBC, loc::Tuple{Center, Center, Center}, args...) = @inbounds c[grid.Nx + 1, j, k] = getbc(bc, j, k, grid, args...)
-@inline  _fill_south_halo!(i, k, grid, c, bc::OBC, loc::Tuple{Center, Center, Center}, args...) = @inbounds c[i, 1, k]           = getbc(bc, i, k, grid, args...)
-@inline  _fill_north_halo!(i, k, grid, c, bc::OBC, loc::Tuple{Center, Center, Center}, args...) = @inbounds c[i, grid.Ny + 1, k] = getbc(bc, i, k, grid, args...)
-@inline _fill_bottom_halo!(i, j, grid, c, bc::OBC, loc::Tuple{Center, Center, Center}, args...) = @inbounds c[i, j, 1]           = getbc(bc, i, j, grid, args...)
-@inline    _fill_top_halo!(i, j, grid, c, bc::OBC, loc::Tuple{Center, Center, Center}, args...) = @inbounds c[i, j, grid.Nz + 1] = getbc(bc, i, j, grid, args...)
+@inline   _fill_west_halo!(j, k, grid, c, bc::OBC, ::Tuple{Center, Center, Center}, args...) = @inbounds c[1, j, k]           = getbc(bc, j, k, grid, args...)
+@inline   _fill_east_halo!(j, k, grid, c, bc::OBC, ::Tuple{Center, Center, Center}, args...) = @inbounds c[grid.Nx + 1, j, k] = getbc(bc, j, k, grid, args...)
+@inline  _fill_south_halo!(i, k, grid, c, bc::OBC, ::Tuple{Center, Center, Center}, args...) = @inbounds c[i, 1, k]           = getbc(bc, i, k, grid, args...)
+@inline  _fill_north_halo!(i, k, grid, c, bc::OBC, ::Tuple{Center, Center, Center}, args...) = @inbounds c[i, grid.Ny + 1, k] = getbc(bc, i, k, grid, args...)
+@inline _fill_bottom_halo!(i, j, grid, c, bc::OBC, ::Tuple{Center, Center, Center}, args...) = @inbounds c[i, j, 1]           = getbc(bc, i, j, grid, args...)
+@inline    _fill_top_halo!(i, j, grid, c, bc::OBC, ::Tuple{Center, Center, Center}, args...) = @inbounds c[i, j, grid.Nz + 1] = getbc(bc, i, j, grid, args...)
 
+# Regular boundary fill for non-`CenterField` open boundaries
+
+@inline   _fill_west_halo!(j, k, grid, c, bc::OBC, loc, args...) = nothing
+@inline   _fill_east_halo!(j, k, grid, c, bc::OBC, loc, args...) = nothing
+@inline  _fill_south_halo!(i, k, grid, c, bc::OBC, loc, args...) = nothing
+@inline  _fill_north_halo!(i, k, grid, c, bc::OBC, loc, args...) = nothing
+@inline _fill_bottom_halo!(i, j, grid, c, bc::OBC, loc, args...) = nothing
+@inline    _fill_top_halo!(i, j, grid, c, bc::OBC, loc, args...) = nothing
