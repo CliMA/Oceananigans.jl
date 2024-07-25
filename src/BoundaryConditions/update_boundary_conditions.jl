d9bb081bf525d2e6b611e3f1a7e481fd3f998ea6
Jago Strong-Wright <jagoosw@protonmail.com>
2024-07-25 17:52:24 +0100

    changed argument order for `update_boundary_condition!`

--- src/BoundaryConditions/update_boundary_conditions.jl
+++ src/BoundaryConditions/update_boundary_conditions.jl
@@ -3,13 +3,13 @@ using Oceananigans: boundary_conditions
 update_boundary_condition!(bc::Union{BoundaryCondition, Nothing}, args...) = nothing
 
 function update_boundary_condition!(bcs::FieldBoundaryConditions, field, model)
-    update_boundary_condition!(bcs.west, field, model, Val(:west))
-    update_boundary_condition!(bcs.east, field, model, Val(:east))
-    update_boundary_condition!(bcs.south, field, model, Val(:south))
-    update_boundary_condition!(bcs.north, field, model, Val(:north))
-    update_boundary_condition!(bcs.bottom, field, model, Val(:bottom))
-    update_boundary_condition!(bcs.top, field, model, Val(:top))
-    update_boundary_condition!(bcs.immersed, field, model, Val(:immersed))
+    update_boundary_condition!(bcs.west, Val(:west), field, model)
+    update_boundary_condition!(bcs.east, Val(:east), field, model)
+    update_boundary_condition!(bcs.south, Val(:south), field, model)
+    update_boundary_condition!(bcs.north, Val(:north), field, model)
+    update_boundary_condition!(bcs.bottom, Val(:bottom), field, model)
+    update_boundary_condition!(bcs.top, Val(:top), field, model)
+    update_boundary_condition!(bcs.immersed, Val(:immersed), field, model)
 end
 
 update_boundary_condition!(fields::Union{NamedTuple, Tuple}, model) = 
