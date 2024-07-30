fbe7f99bf15ef5ce6d02b9a07a0494150be9a64f
Simone Silvestri <silvestri.simone0@gmail.com>
2024-07-30 19:58:06 -0400

    Update src/TimeSteppers/quasi_adams_bashforth_2.jl
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>

--- src/TimeSteppers/quasi_adams_bashforth_2.jl
+++ src/TimeSteppers/quasi_adams_bashforth_2.jl
@@ -61,19 +61,21 @@ reset!(timestepper::QuasiAdamsBashforth2TimeStepper) = nothing
 
 Step forward `model` one time step `Δt` with a 2nd-order Adams-Bashforth method and
 pressure-correction substep. Setting `euler=true` will take a forward Euler time step.
-The new tendencies are calculated in the `update_step!` at the end of the `time_step!` function.
+The tendencies are calculated by the `update_step!` at the end of the `time_step!` function.
 
-The steps of the AB2 algorithm are:
+The steps of the Quasi-Adams-Bashforth second-order (AB2) algorithm are:
 
-1. If this the first time step `(model.clock.iteration == 0)`, then `update_state!` and calculate the tendencies
-2. advance tracers in time and compute predictor velocities (including implicit vertical diffusion)
-3. solve the elliptic equation for pressure (three dimensional for the non-hydrostatic model and two-dimensional for the hydrostatic model)
-4. correct the velocities based on the results of step 3.
-5. Store the old tendencies
-6. update the model state
-7. If `compute_tendencies == true`, compute the tendencies for the next time step. NOTE: if `compute_tendencies == false` the new tendencies will not be calculated! 
-It is never suggested to set `compute_tendencies == false` except for debugging purposes.
-    
+1. If this the first time step (`model.clock.iteration == 0`), then call `update_state!` and calculate the tendencies.
+2. Advance tracers in time and compute predictor velocities (including implicit vertical diffusion).
+3. Solve the elliptic equation for pressure (three dimensional for the non-hydrostatic model, two-dimensional for the hydrostatic model).
+4. Correct the velocities based on the results of step 3.
+5. Store the old tendencies.
+6. Update the model state.
+7. If `compute_tendencies == true`, compute the tendencies for the next time step.
+
+!!! warning "`compute_tendencies` kwarg"
+    If `compute_tendencies == false` then the new tendencies at the last step are _not_ calculated!
+    Setting `compute_tendencies == false` is not recommended _except_ for debugging purposes.
 """
 function time_step!(model::AbstractModel{<:QuasiAdamsBashforth2TimeStepper}, Δt;
                     callbacks=[], euler=false, compute_tendencies=true)
