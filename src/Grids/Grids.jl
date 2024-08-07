f9a82008325bd5becb86bd3fabf1dbe981940a77
Simone Silvestri <silvestri.simone0@gmail.com>
2024-08-07 17:23:32 -0400

    bugfix

--- src/Grids/Grids.jl
+++ src/Grids/Grids.jl
@@ -157,7 +157,7 @@ const XYZFlatGrid = AbstractGrid{<:Any, Flat, Flat, Flat}
 isrectilinear(grid) = false
 
 # Fallback
-@inline  retrieve_surface_active_cells_map(::AbstractGrid, any_map_type) = nothing
+@inline  retrieve_surface_active_cells_map(::AbstractGrid) = nothing
 @inline retrieve_interior_active_cells_map(::AbstractGrid, any_map_type) = nothing
 
 include("grid_utils.jl")
