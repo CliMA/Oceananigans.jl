4934ee383f7e5cbac8bd0e9d3c4333c92982c87d
Simone Silvestri <silvestri.simone0@gmail.com>
2024-08-16 16:31:52 -0400

    comment

--- src/DistributedComputations/halo_communication.jl
+++ src/DistributedComputations/halo_communication.jl
@@ -115,7 +115,7 @@ function fill_halo_regions!(c::OffsetArray, bcs, indices, loc, grid::Distributed
     fill_corners!(c, arch.connectivity, indices, loc, arch, grid, buffers, args...; only_local_halos, kwargs...)
     
     # We increment the tag counter only if we have actually initiated the MPI communication.
-    # This is the case only if at lease one of the boundary conditions is a distributed communication 
+    # This is the case only if at least one of the boundary conditions is a distributed communication 
     # boundary condition (DCBCT) _and_ the `only_local_halos` keyword argument is false.
     increment_tag = any(isa.(bcs, DCBCT)) && !only_local_halos
     
