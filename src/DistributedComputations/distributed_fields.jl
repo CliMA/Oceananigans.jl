08d886e76d51299fdcd06e823652baca3f6e2c27
Simone Silvestri <silvestri.simone0@gmail.com>
2024-08-16 18:38:37 -0400

    add methods

--- src/DistributedComputations/distributed_fields.jl
+++ src/DistributedComputations/distributed_fields.jl
@@ -3,7 +3,15 @@ import Oceananigans.BoundaryConditions: fill_halo_regions!
 
 using CUDA: CuArray
 using Oceananigans.Grids: topology
-using Oceananigans.Fields: validate_field_data, indices, validate_boundary_conditions, validate_indices, recv_from_buffers!
+
+using Oceananigans.Fields: validate_boundary_conditions, 
+                           validate_field_data, 
+                           validate_indices,
+                           AbstractField,  
+                           indices, 
+                           recv_from_buffers!   
+
+using Oceananigans.OutputReaders: FieldTimeSeries
 
 function Field((LX, LY, LZ)::Tuple, grid::DistributedGrid, data, old_bcs, indices::Tuple, op, status)
     arch = architecture(grid)
@@ -18,6 +26,11 @@ end
 
 const DistributedField      = Field{<:Any, <:Any, <:Any, <:Any, <:DistributedGrid}
 const DistributedFieldTuple = NamedTuple{S, <:NTuple{N, DistributedField}} where {S, N}
+const DistributedFTS        = FieldTimeSeries{LX, LY, LZ, TI, K, I, D, <:DistributedGrid} where {LX, LY, LZ, TI, K, I, D}
+
+child_architecture(field::AbstractField)    = architecture(field)
+child_architecture(field::DistributedField) = child_architecture(architecture(field))
+child_architecture(field::DistributedFTS)   = child_architecture(architecture(field))
 
 function set!(u::DistributedField, f::Function)
     arch = architecture(u)
