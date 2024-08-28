2687060b864c3a29c70acc32d67a676cf219889a
Simone Silvestri <silvestri.simone0@gmail.com>
2024-08-28 10:50:12 -0400

    add more vector rotation tests

--- test/test_cubed_spheres.jl
+++ test/test_cubed_spheres.jl
@@ -7,6 +7,8 @@ using Oceananigans.CubedSpheres
 using Oceananigans.Models.HydrostaticFreeSurfaceModels
 using Oceananigans.Models.HydrostaticFreeSurfaceModels: VerticalVorticityField
 
+using OrthogonalSphericalShellGrids
+    
 # To be used in the test below as `KernelFunctionOperation`s
 @inline intrinsic_vector_x_component(i, j, k, grid, uₑ, vₑ) = 
     @inbounds intrinsic_vector(i, j, k, grid, uₑ, vₑ)[1]
@@ -19,6 +21,116 @@ using Oceananigans.Models.HydrostaticFreeSurfaceModels: VerticalVorticityField
     
 @inline extrinsic_vector_y_component(i, j, k, grid, uₑ, vₑ) =
     @inbounds intrinsic_vector(i, j, k, grid, uₑ, vₑ)[2]
+
+function kinetic_energy(u, v)
+    ke = Field(0.5 * (u * u + v * v))
+    return compute!(ke)
+end
+
+function test_vector_rotation(grid, T=Float64)
+    u = XFaceField(grid)
+    v = YFaceField(grid)
+    
+    # Purely zonal flow.
+    set!(u, 1)
+    set!(v, 0)
+
+    # Convert it to an "Instrinsic" reference frame
+    uᵢ = KernelFunctionOperation{Face, Center, Center}(intrinsic_vector_x_component, grid, u, v)
+    vᵢ = KernelFunctionOperation{Center, Face, Center}(intrinsic_vector_y_component, grid, u, v)
+    
+    uᵢ = compute!(Field(uᵢ))
+    vᵢ = compute!(Field(vᵢ))
+
+    # The extrema of u and v, as well as their mean value should
+    # be equivalent on an "intrinsic" frame
+    @test maximum(uᵢ) ≈ maximum(vᵢ)
+    @test minimum(uᵢ) ≈ minimum(vᵢ)
+    @test mean(uᵢ) ≈ mean(vᵢ)
+    @test mean(uᵢ) > 0 # The mean value should be positive
+
+    # Kinetic energy should remain the same
+    @test mean(kinetic_energy(uᵢ, vᵢ)) ≈ 0.5
+
+    # Convert it back to a purely zonal velocity (vₑ == 0)
+    uₑ = KernelFunctionOperation{Face, Center, Center}(extrinsic_vector_x_component, grid, uᵢ, vᵢ)
+    vₑ = KernelFunctionOperation{Center, Face, Center}(extrinsic_vector_y_component, grid, uᵢ, vᵢ)
+    
+    uₑ = compute!(Field(uₑ))
+    vₑ = compute!(Field(vₑ))
+
+    # Make sure that the flow was converted back to a 
+    # purely zonal flow in the extrensic frame (v ≈ 0)
+    @test all(Array(interior(vₑ)) .≈ 0)
+    @test all(Array(interior(uₑ)) .≈ 1)
+
+    # Purely meridional flow.
+    set!(u, 0)
+    set!(v, 1)
+
+    # Convert it to an "Instrinsic" reference frame
+    uᵢ = KernelFunctionOperation{Face, Center, Center}(intrinsic_vector_x_component, grid, u, v)
+    vᵢ = KernelFunctionOperation{Center, Face, Center}(intrinsic_vector_y_component, grid, u, v)
+    
+    uᵢ = compute!(Field(uᵢ))
+    vᵢ = compute!(Field(vᵢ))
+
+    # The extrema of u and v, as well as their mean value should
+    # be equivalent on an "intrinsic" frame
+    @test maximum(uᵢ) ≈ maximum(vᵢ)
+    @test minimum(uᵢ) ≈ minimum(vᵢ)
+    @test mean(uᵢ) ≈ mean(vᵢ)
+    @test mean(vᵢ) > 0 # The mean value should be positive
+
+    # Kinetic energy should remain the same
+    @test mean(kinetic_energy(uᵢ, vᵢ)) ≈ 0.5
+
+    # Convert it back to a purely zonal velocity (vₑ == 0)
+    uₑ = KernelFunctionOperation{Face, Center, Center}(extrinsic_vector_x_component, grid, uᵢ, vᵢ)
+    vₑ = KernelFunctionOperation{Center, Face, Center}(extrinsic_vector_y_component, grid, uᵢ, vᵢ)
+    
+    uₑ = compute!(Field(uₑ))
+    vₑ = compute!(Field(vₑ))
+
+    # Make sure that the flow was converted back to a 
+    # purely zonal flow in the extrensic frame (v ≈ 0)
+    @test all(Array(interior(vₑ)) .≈ 1)
+    @test all(Array(interior(uₑ)) .≈ 0)
+
+    # Mixed zonal and meridional flow.
+    set!(u, 0.5)
+    set!(v, 0.5)
+
+    # Convert it to an "Instrinsic" reference frame
+    uᵢ = KernelFunctionOperation{Face, Center, Center}(intrinsic_vector_x_component, grid, u, v)
+    vᵢ = KernelFunctionOperation{Center, Face, Center}(intrinsic_vector_y_component, grid, u, v)
+    
+    uᵢ = compute!(Field(uᵢ))
+    vᵢ = compute!(Field(vᵢ))
+
+    # The extrema of u and v, as well as their mean value should
+    # be equivalent on an "intrinsic" frame
+    @test maximum(uᵢ) ≈ maximum(vᵢ)
+    @test minimum(uᵢ) ≈ minimum(vᵢ)
+    @test mean(uᵢ) ≈ mean(vᵢ)
+    @test mean(vᵢ) > 0 # The mean value should be positive
+
+    # Kinetic energy should remain the same
+    @test mean(kinetic_energy(uᵢ, vᵢ)) ≈ 0.25
+
+    # Convert it back to a purely zonal velocity (vₑ == 0)
+    uₑ = KernelFunctionOperation{Face, Center, Center}(extrinsic_vector_x_component, grid, uᵢ, vᵢ)
+    vₑ = KernelFunctionOperation{Center, Face, Center}(extrinsic_vector_y_component, grid, uᵢ, vᵢ)
+    
+    uₑ = compute!(Field(uₑ))
+    vₑ = compute!(Field(vₑ))
+
+    # Make sure that the flow was converted back to a 
+    # purely zonal flow in the extrensic frame (v ≈ 0)
+    @test all(Array(interior(vₑ)) .≈ 1)
+    @test all(Array(interior(uₑ)) .≈ 0)
+end
+    
 
 @testset "Cubed spheres" begin
 
@@ -53,6 +165,13 @@ using Oceananigans.Models.HydrostaticFreeSurfaceModels: VerticalVorticityField
 
             @testset "Constructing a grid from file [$(typeof(arch))]" begin
                 @test grid isa ConformalCubedSphereGrid
+            end
+
+            @testset "Vector rotation [$(typeof(arch))]" begin
+                trg_grid = TripolarGrid(arch, size = (20, 20, 1), z = (0, 1))
+
+                test_vector_rotation(grid)
+                test_vector_rotation(trg_grid)
             end
 
             @testset "CubedSphereData and CubedSphereFields [$(typeof(arch))]" begin
