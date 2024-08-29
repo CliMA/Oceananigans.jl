3f70f88ecbcaad1d422c163f514f393247a45abe
Simone Silvestri <silvestri.simone0@gmail.com>
2024-08-29 10:49:49 -0400

    Update src/Models/NonhydrostaticModels/update_nonhydrostatic_model_state.jl
    Co-authored-by: Gregory L. Wagner <wagner.greg@gmail.com>

--- src/Models/NonhydrostaticModels/update_nonhydrostatic_model_state.jl
+++ src/Models/NonhydrostaticModels/update_nonhydrostatic_model_state.jl
@@ -17,7 +17,7 @@ Update peripheral aspects of the model (halo regions, diffusivities, hydrostatic
 pressure) to the current model state. If `callbacks` are provided (in an array),
 they are called in the end.
 """
-function update_state!(model::NonhydrostaticModel, callbacks=[]; compute_tendencies = false)
+function update_state!(model::NonhydrostaticModel, callbacks=[]; compute_tendencies = true)
     
     # Mask immersed tracers
     foreach(model.tracers) do tracer
