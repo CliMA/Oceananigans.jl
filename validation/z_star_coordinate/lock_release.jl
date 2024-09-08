a879db1414621e4ec4e22f0c18fa8113af7742a1
Simone Silvestri <silvestri.simone0@gmail.com>
2024-09-08 12:31:49 -0400

    better lock-release

--- validation/z_star_coordinate/lock_release.jl
+++ validation/z_star_coordinate/lock_release.jl
@@ -5,9 +5,9 @@ using Oceananigans.Advection: WENOVectorInvariant
 using Oceananigans.Models.HydrostaticFreeSurfaceModels: ZStar, ZStarSpacingGrid, Δzᶜᶜᶜ_reference
 using Printf
 
-grid = RectilinearGrid(size = (10, 10), 
-                          x = (0, 100kilometers), 
-                          z = (-10, 0), 
+grid = RectilinearGrid(size = (128, 20), 
+                          x = (0, 64kilometers), 
+                          z = (-20, 0), 
                        halo = (6, 6),
                    topology = (Bounded, Flat, Bounded))
 
@@ -17,22 +17,21 @@ model = HydrostaticFreeSurfaceModel(; grid,
                            tracer_advection = WENO(),
                                    buoyancy = BuoyancyTracer(),
                                     tracers = :b,
-                               free_surface = SplitExplicitFreeSurface(; substeps = 10))
+                               free_surface = SplitExplicitFreeSurface(; substeps = 120))
 
 g = model.free_surface.gravitational_acceleration
 
-bᵢ(x, z) = x < 50kilometers ? 1 : 0
+model.timestepper.χ = 0.0
+
+bᵢ(x, z) = x < 32kilometers ? 0.06 : 0.01
 
 set!(model, b = bᵢ)
 
-gravity_wave_speed   = sqrt(g * grid.Lz)
-barotropic_time_step = grid.Δxᶜᵃᵃ / gravity_wave_speed
-
-Δt = 0.5 * barotropic_time_step
+Δt = 1
 
 @info "the time step is $Δt"
 
-simulation = Simulation(model; Δt, stop_iteration = 100000, stop_time = 10days) 
+simulation = Simulation(model; Δt, stop_iteration = 100000, stop_time = 17hours) 
 
 field_outputs = if model.grid isa ZStarSpacingGrid
   merge(model.velocities, model.tracers, (; sⁿ = model.grid.Δzᵃᵃᶠ.sⁿ))
@@ -48,8 +47,6 @@ simulation.output_writers[:other_variables] = JLD2OutputWriter(model, field_outp
 function progress(sim)
     w  = interior(sim.model.velocities.w, :, :, sim.model.grid.Nz+1)
     u  = sim.model.velocities.u
-    v  = sim.model.velocities.v
-    η  = sim.model.free_surface.η
     b  = sim.model.tracers.b
     
     msg0 = @sprintf("Time: %s iteration %d ", prettytime(sim.model.clock.time), sim.model.clock.iteration)
@@ -67,8 +64,7 @@ function progress(sim)
     return nothing
 end
 
-simulation.callbacks[:progress] = Callback(progress, IterationInterval(1))
-simulation.callbacks[:wizard]   = Callback(TimeStepWizard(; cfl = 0.2, max_change = 1.1), IterationInterval(10))
+simulation.callbacks[:progress] = Callback(progress, IterationInterval(100))
 
 run!(simulation)
 
