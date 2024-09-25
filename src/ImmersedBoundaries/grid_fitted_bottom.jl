00ffa579bc4f4a5e979b478c64301cac3d97f05a
Gregory Wagner <wagner.greg@gmail.com>
2024-09-24 19:23:45 -0600

    Bugfix

--- src/ImmersedBoundaries/grid_fitted_bottom.jl
+++ src/ImmersedBoundaries/grid_fitted_bottom.jl
@@ -54,15 +54,15 @@ Keyword Arguments
 GridFittedBottom(bottom_height) = GridFittedBottom(bottom_height, CenterImmersedCondition())
 
 function Base.summary(ib::GridFittedBottom)
-    hmax  = maximum(ib.bottom_height)
-    hmin  = minimum(ib.bottom_height)
-    hmean = mean(ib.bottom_height)
+    zmax  = maximum(ib.bottom_height)
+    zmin  = minimum(ib.bottom_height)
+    zmean = mean(ib.bottom_height)
 
     summary1 = "GridFittedBottom("
 
-    summary2 = string("mean(z)=", prettysummary(hmean),
-                      ", min(z)=", prettysummary(hmin),
-                      ", max(z)=", prettysummary(hmax))
+    summary2 = string("mean(z)=", prettysummary(zmean),
+                      ", min(z)=", prettysummary(zmin),
+                      ", max(z)=", prettysummary(zmax))
 
     summary3 = ")"
 
@@ -87,7 +87,7 @@ Computes `ib.bottom_height` and wraps it in a Field.
 function ImmersedBoundaryGrid(grid, ib::GridFittedBottom)
     bottom_field = Field{Center, Center, Nothing}(grid)
     set!(bottom_field, ib.bottom_height)
-    launch!(architecture(grid), grid, :xy, clip_bottom_height!, ib.bottom_height, grid)
+    launch!(architecture(grid), grid, :xy, clip_bottom_height!, bottom_field, grid)
     fill_halo_regions!(bottom_field)
     new_ib = GridFittedBottom(bottom_field, ib.immersed_condition)
     TX, TY, TZ = topology(grid)
@@ -98,11 +98,11 @@ const c = Center()
 const f = Face()
 
 @kernel function clip_bottom_height!(z, grid)
-    i, j, k, = @index(Global, NTuple)
+    i, j = @index(Global, NTuple)
     Nz = size(grid, 3)
-    z_min = znode(i, j, 1,    grid, c, c, f)
-    z_max = znode(i, j, Nz+1, grid, c, c, f)
-    @inbounds z[i, j, 1] = clamp(z[i, j, 1], z_min, z_max)
+    zmin = znode(i, j, 1,    grid, c, c, f)
+    zmax = znode(i, j, Nz+1, grid, c, c, f)
+    @inbounds z[i, j, 1] = clamp(z[i, j, 1], zmin, zmax)
 end
 
 @inline function _immersed_cell(i, j, k, underlying_grid, ib::GridFittedBottom{<:Any, <:InterfaceImmersedCondition})
