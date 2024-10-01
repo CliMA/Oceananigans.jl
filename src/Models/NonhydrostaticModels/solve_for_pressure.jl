19d87a01fb063093eeadabd209ca54432d60a359
Gregory Wagner <wagner.greg@gmail.com>
2024-10-01 07:11:00 -0400

    Fix function signature plus notation update

--- src/Models/NonhydrostaticModels/solve_for_pressure.jl
+++ src/Models/NonhydrostaticModels/solve_for_pressure.jl
@@ -9,67 +9,67 @@ using Oceananigans.Solvers: solve!
 ##### Calculate the right-hand-side of the non-hydrostatic pressure Poisson equation.
 #####
 
-@kernel function _compute_source_term!(rhs, grid, Δt, U★)
+@kernel function _compute_source_term!(rhs, grid, Δt, Ũ)
     i, j, k = @index(Global, NTuple)
     active = !inactive_cell(i, j, k, grid)
-    δ = divᶜᶜᶜ(i, j, k, grid, U★.u, U★.v, U★.w)
+    δ = divᶜᶜᶜ(i, j, k, grid, Ũ.u, Ũ.v, Ũ.w)
     @inbounds rhs[i, j, k] = active * δ / Δt
 end
 
-@kernel function _fourier_tridiagonal_source_term!(rhs, ::XDirection, grid, Δt, U★)
+@kernel function _fourier_tridiagonal_source_term!(rhs, ::XDirection, grid, Δt, Ũ)
     i, j, k = @index(Global, NTuple)
     active = !inactive_cell(i, j, k, grid)
-    δ = divᶜᶜᶜ(i, j, k, grid, U★.u, U★.v, U★.w)
+    δ = divᶜᶜᶜ(i, j, k, grid, Ũ.u, Ũ.v, Ũ.w)
     @inbounds rhs[i, j, k] = active * Δxᶜᶜᶜ(i, j, k, grid) * δ / Δt
 end
 
-@kernel function _fourier_tridiagonal_source_term!(rhs, ::YDirection, grid, Δt, U★)
+@kernel function _fourier_tridiagonal_source_term!(rhs, ::YDirection, grid, Δt, Ũ)
     i, j, k = @index(Global, NTuple)
     active = !inactive_cell(i, j, k, grid)
-    δ = divᶜᶜᶜ(i, j, k, grid, U★.u, U★.v, U★.w)
+    δ = divᶜᶜᶜ(i, j, k, grid, Ũ.u, Ũ.v, Ũ.w)
     @inbounds rhs[i, j, k] = active * Δyᶜᶜᶜ(i, j, k, grid) * δ / Δt
 end
 
-@kernel function _fourier_tridiagonal_source_term!(rhs, ::ZDirection, grid, Δt, U★)
+@kernel function _fourier_tridiagonal_source_term!(rhs, ::ZDirection, grid, Δt, Ũ)
     i, j, k = @index(Global, NTuple)
     active = !inactive_cell(i, j, k, grid)
-    δ = divᶜᶜᶜ(i, j, k, grid, U★.u, U★.v, U★.w)
+    δ = divᶜᶜᶜ(i, j, k, grid, Ũ.u, Ũ.v, Ũ.w)
     @inbounds rhs[i, j, k] = active * Δzᶜᶜᶜ(i, j, k, grid) * δ / Δt
 end
 
-function compute_source_term!(pressure, solver::DistributedFFTBasedPoissonSolver, Δt, U★)
+function compute_source_term!(pressure, solver::DistributedFFTBasedPoissonSolver, Δt, Ũ)
     rhs  = solver.storage.zfield
     arch = architecture(solver)
     grid = solver.local_grid
-    launch!(arch, grid, :xyz, _compute_source_term!, rhs, grid, Δt, U★)
+    launch!(arch, grid, :xyz, _compute_source_term!, rhs, grid, Δt, Ũ)
     return nothing        
 end
 
-function compute_source_term!(pressure, solver::DistributedFourierTridiagonalPoissonSolver, Δt, U★)
+function compute_source_term!(pressure, solver::DistributedFourierTridiagonalPoissonSolver, Δt, Ũ)
     rhs = solver.storage.zfield
     arch = architecture(solver)
     grid = solver.local_grid
     tridiagonal_dir = solver.batched_tridiagonal_solver.tridiagonal_direction
     launch!(arch, grid, :xyz, _fourier_tridiagonal_source_term!,
-            rhs, grid, Δt, U★, tridiagonal_dir)
+            rhs, tridiagonal_dir, grid, Δt, Ũ)
     return nothing
 end
 
-function compute_source_term!(pressure, solver::FourierTridiagonalPoissonSolver, Δt, U★)
+function compute_source_term!(pressure, solver::FourierTridiagonalPoissonSolver, Δt, Ũ)
     rhs = solver.source_term
     arch = architecture(solver)
     grid = solver.grid
     tridiagonal_dir = solver.batched_tridiagonal_solver.tridiagonal_direction
     launch!(arch, grid, :xyz, _fourier_tridiagonal_source_term!,
-            rhs, grid, Δt, U★, tridiagonal_dir)
+            rhs, grid, Δt, Ũ, tridiagonal_dir)
     return nothing
 end
 
-function compute_source_term!(pressure, solver::FFTBasedPoissonSolver, Δt, U★)
+function compute_source_term!(pressure, solver::FFTBasedPoissonSolver, Δt, Ũ)
     rhs = solver.storage
     arch = architecture(solver)
     grid = solver.grid
-    launch!(arch, grid, :xyz, _compute_source_term!, rhs, grid, Δt, U★)
+    launch!(arch, grid, :xyz, _compute_source_term!, rhs, grid, Δt, Ũ)
     return nothing
 end
 
@@ -77,17 +77,17 @@ end
 ##### Solve for pressure
 #####
 
-function solve_for_pressure!(pressure, solver, Δt, U★)
-    compute_source_term!(pressure, solver, Δt, U★)
+function solve_for_pressure!(pressure, solver, Δt, Ũ)
+    compute_source_term!(pressure, solver, Δt, Ũ)
     solve!(pressure, solver)
     return pressure
 end
 
-function solve_for_pressure!(pressure, solver::ConjugateGradientPoissonSolver, Δt, U★)
+function solve_for_pressure!(pressure, solver::ConjugateGradientPoissonSolver, Δt, Ũ)
     rhs = solver.right_hand_side
     grid = solver.grid
     arch = architecture(grid)
-    launch!(arch, grid, :xyz, _compute_source_term!, rhs, grid, Δt, U★)
+    launch!(arch, grid, :xyz, _compute_source_term!, rhs, grid, Δt, Ũ)
     return solve!(pressure, solver.conjugate_gradient_solver, rhs)
 end
 
