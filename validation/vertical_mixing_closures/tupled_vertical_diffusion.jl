840f6026c3025872e57c66e9b134ffbeb24bdc8c
Gregory L. Wagner <wagner.greg@gmail.com>
2024-10-04 10:06:18 -0400

    Bring back support for closure tuples with CATKE (#3805)
    * Bring back support for closure tuples with CATKE
    
    * Update tupled diffusion test
    
    * Bring back CATKE tuple tests

--- validation/vertical_mixing_closures/tupled_vertical_diffusion.jl
+++ validation/vertical_mixing_closures/tupled_vertical_diffusion.jl
@@ -1,34 +1,19 @@
-using GLMakie
 using Oceananigans
 using Oceananigans.Units
+using Oceananigans.TurbulenceClosures: CATKEVerticalDiffusivity
 
-arch = CPU()
-grid = RectilinearGrid(arch, size=128, z=(-5, 5), topology=(Flat, Flat, Bounded))
+grid = RectilinearGrid(size=128, z=(-128, 0), topology=(Flat, Flat, Bounded))
 
-single_closure = VerticalScalarDiffusivity(VerticallyImplicitTimeDiscretization(), κ=1)
+closure = (VerticalScalarDiffusivity(VerticallyImplicitTimeDiscretization(), κ=1e-4), 
+           CATKEVerticalDiffusivity())
 
-tuple_closure = (VerticalScalarDiffusivity(VerticallyImplicitTimeDiscretization(), κ=1/2), 
-                 VerticalScalarDiffusivity(VerticallyImplicitTimeDiscretization(), κ=1/2))
+model = HydrostaticFreeSurfaceModel(; grid, closure,
+                                    tracers = (:b, :e),
+                                    buoyancy = BuoyancyTracer())
 
-closure = tuple_closure
-
-model = HydrostaticFreeSurfaceModel(; grid, closure, tracers=:c, buoyancy=nothing, coriolis=nothing)
-set!(model, c = (x, y, z) -> exp(-z^2))
-simulation = Simulation(model, Δt=1e-4, stop_iteration=100)
-
-c_snapshots = []
-c = model.tracers.c
-getc(sim) = push!(c_snapshots, Array(interior(c, 1, 1, :)))
-simulation.callbacks[:c] = Callback(getc, IterationInterval(10))
+bᵢ(z) = 1e-5 * z
+set!(model, b = bᵢ)
+simulation = Simulation(model, Δt=1minute, stop_iteration=10)
 
 run!(simulation)
 
-fig = Figure()
-ax = Axis(fig[1, 1])
-Nt = length(c_snapshots)
-slider = Slider(fig[2, 1], range=1:Nt, startvalue=1)
-n = slider.value
-z = znodes(c)
-c = @lift c_snapshots[$n]
-lines!(ax, c, z)
-display(fig)
