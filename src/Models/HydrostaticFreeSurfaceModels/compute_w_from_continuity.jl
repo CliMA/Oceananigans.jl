3b016356be73fe04f54cace68866f3b48587dbe8
Simone Silvestri <silvestri.simone0@gmail.com>
2024-10-07 13:03:14 +0200

    simplify a bit

--- src/Models/HydrostaticFreeSurfaceModels/compute_w_from_continuity.jl
+++ src/Models/HydrostaticFreeSurfaceModels/compute_w_from_continuity.jl
@@ -18,16 +18,7 @@ compute_w_from_continuity!(model; kwargs...) =
 compute_w_from_continuity!(velocities, arch, grid; parameters = w_kernel_parameters(grid)) = 
     launch!(arch, grid, parameters, _compute_w_from_continuity!, velocities, grid)
 
-# We use the previous vertical scaling in the continuity equation for 
-# consistency with how we have calculated the vertical integrals in the free surface step
-@inline Ax_uᶠᶜᶜ⁻(i, j, k, grid, u) = @inbounds vertical_scaling(i, j, k, grid, f, c, c) * Δrᶠᶜᶜ(i, j, k, grid) * Δyᶠᶜᶜ(i, j, k, grid) * u[i, j, k]
-@inline Ay_vᶜᶠᶜ⁻(i, j, k, grid, v) = @inbounds vertical_scaling(i, j, k, grid, c, f, c) * Δrᶜᶠᶜ(i, j, k, grid) * Δxᶜᶠᶜ(i, j, k, grid) * v[i, j, k]
-
-@inline function scaled_velocity_divergenceᶜᶜᶜ(i, j, k, grid, u, v)
-    δU = δxᶜᶜᶜ(i, j, k, grid, Ax_uᶠᶜᶜ⁻, u)
-    δV = δyᶜᶜᶜ(i, j, k, grid, Ay_vᶜᶠᶜ⁻, v)
-    return (δU + δV) / Azᶜᶜᶜ(i, j, k, grid)
-end
+@inline velocity_divergenceᶜᶜᶜ(i, j, k, grid, u, v) = flux_div_xyᶜᶜᶜ(i, j, k, grid, u, v) / Azᶜᶜᶜ(i, j, k, grid)
 
 @kernel function _compute_w_from_continuity!(U, grid)
     i, j = @index(Global, NTuple)
@@ -35,7 +26,7 @@ end
     @inbounds U.w[i, j, 1] = 0
     for k in 2:grid.Nz+1
         @inbounds U.w[i, j, k] = U.w[i, j, k-1] - 
-                                ( scaled_velocity_divergenceᶜᶜᶜ(i, j, k-1, grid, U.u, U.v) +
+                                ( velocity_divergenceᶜᶜᶜ(i, j, k-1, grid, U.u, U.v) +
                                  Δrᶜᶜᶜ(i, j, k-1, grid) *  ∂t_s_grid(i, j, k-1, grid) )
     end
 end
