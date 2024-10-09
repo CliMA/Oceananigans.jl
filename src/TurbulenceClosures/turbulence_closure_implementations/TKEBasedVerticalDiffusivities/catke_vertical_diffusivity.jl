38fbd921392aba74e2cc8ae0308c3f6c07b61bcf
Gregory Wagner <wagner.greg@gmail.com>
2024-10-09 08:45:01 -0700

    Even more specific fill halo regions for CATKE

--- src/TurbulenceClosures/turbulence_closure_implementations/TKEBasedVerticalDiffusivities/catke_vertical_diffusivity.jl
+++ src/TurbulenceClosures/turbulence_closure_implementations/TKEBasedVerticalDiffusivities/catke_vertical_diffusivity.jl
@@ -168,11 +168,10 @@ Adapt.adapt_structure(to, catke_diffusivity_fields::CATKEDiffusivityFields) =
                            adapt(to, _tupled_implicit_linear_coefficients))
 
 function fill_halo_regions!(catke_diffusivity_fields::CATKEDiffusivityFields, args...; kw...)
-    fields_with_halos_to_fill = (catke_diffusivity_fields.κu,
-                                 catke_diffusivity_fields.κc,
-                                 catke_diffusivity_fields.κe)
-
-    return fill_halo_regions!(fields_with_halos_to_fill, args...; kw...)
+    fill_halo_regions!(catke_diffusivity_fields.κu, args...; kw...)
+    fill_halo_regions!(catke_diffusivity_fields.κc, args...; kw...)
+    fill_halo_regions!(catke_diffusivity_fields.κe, args...; kw...)
+    return nothing
 end
 
 function DiffusivityFields(grid, tracer_names, bcs, closure::FlavorOfCATKE)
