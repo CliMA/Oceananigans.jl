6e3da544a7e71c52f846d0ad0f7d2b59f12da8aa
Gregory Wagner <wagner.greg@gmail.com>
2024-10-09 08:47:23 -0700

    Disambiguate distributed launching

--- src/DistributedComputations/distributed_kernel_launching.jl
+++ src/DistributedComputations/distributed_kernel_launching.jl
@@ -5,3 +5,9 @@ function launch!(arch::Distributed, args...; kwargs...)
     return launch!(child_arch, args...; kwargs...)
 end
 
+# Disambiguiate
+@inline function launch!(arch::Distributed, grid, workspec_tuple::Tuple, args...; kwargs...)
+    child_arch = child_architecture(arch)
+    return launch!(child_arch, grid, workspec_tuple, args...; kwargs...)
+end
+
