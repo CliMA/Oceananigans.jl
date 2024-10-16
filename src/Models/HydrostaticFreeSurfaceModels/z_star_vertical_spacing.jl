3151408d0b0d0dd635f0c3b81db6f9b3e6aeb115
Simone Silvestri <silvestri.simone0@gmail.com>
2024-10-16 12:04:04 +0200

    use new bottom

--- src/Models/HydrostaticFreeSurfaceModels/z_star_vertical_spacing.jl
+++ src/Models/HydrostaticFreeSurfaceModels/z_star_vertical_spacing.jl
@@ -21,10 +21,6 @@ function update_grid!(model, grid::ZStarSpacingGrid; parameters = :xy)
     η_grid = grid.zᵃᵃᶠ.∂t_s
 
     # Free surface variables
-    Hᶜᶜ = model.free_surface.auxiliary.Hᶜᶜ
-    Hᶠᶜ = model.free_surface.auxiliary.Hᶠᶜ
-    Hᶜᶠ = model.free_surface.auxiliary.Hᶜᶠ
-    Hᶠᶠ = model.free_surface.auxiliary.Hᶠᶠ
     U̅   = model.free_surface.state.U̅ 
     V̅   = model.free_surface.state.V̅ 
     η   = model.free_surface.η
@@ -32,14 +28,14 @@ function update_grid!(model, grid::ZStarSpacingGrid; parameters = :xy)
     # Update vertical spacing with available parameters 
     # No need to fill the halo as the scaling is updated _IN_ the halos
     launch!(architecture(grid), grid, parameters, _update_zstar!, 
-            sᶜᶜⁿ, sᶠᶜⁿ, sᶜᶠⁿ, sᶠᶠⁿ, sᶜᶜ⁻, sᶠᶜ⁻, sᶜᶠ⁻, η_grid, η, Hᶜᶜ, Hᶠᶜ, Hᶜᶠ, Hᶠᶠ, grid)
+            sᶜᶜⁿ, sᶠᶜⁿ, sᶜᶠⁿ, sᶠᶠⁿ, sᶜᶜ⁻, sᶠᶜ⁻, sᶜᶠ⁻, η_grid, η, grid)
 
     # Update the time derivative of the grid-scaling. Note that in this case we leverage the
     # free surface evolution equation, where the time derivative of the free surface is equal
     # to the divergence of the vertically integrated velocity field, such that
     # ∂ₜ((H + η) / H) = H⁻¹ ∂ₜη =  - H⁻¹ ∇ ⋅ ∫udz 
     launch!(architecture(grid), grid, parameters, _update_∂t_s!, 
-            ∂t_s, U̅, V̅, Hᶜᶜ, grid)
+            ∂t_s, U̅, V̅, grid)
 
     return nothing
 end
@@ -49,23 +45,28 @@ end
 @kernel function _update_∂t_s!(∂t_s, U̅, V̅, Hᶜᶜ, grid)
     i, j  = @index(Global, NTuple)
     k_top = grid.Nz + 1 
-    TX, TY, _ = topology(grid)
+    Hᶜᶜ = domain_depthᶜᶜᵃ(i, j, grid)
 
     @inbounds begin
         # ∂(η / H)/∂t = - ∇ ⋅ ∫udz / H
         ∂t_s[i, j, 1] = - 1 / Azᶜᶜᶠ(i, j, k_top-1, grid) * (δxᶜᶜᶠ(i, j, k_top-1, grid, Δy_qᶠᶜᶠ, U̅) +
-                                                            δyᶜᶜᶠ(i, j, k_top-1, grid, Δx_qᶜᶠᶠ, V̅)) / Hᶜᶜ[i, j, 1]
-    end
+                                                            δyᶜᶜᶠ(i, j, k_top-1, grid, Δx_qᶜᶠᶠ, V̅)) / Hᶜᶜ
 end
 
-@kernel function _update_zstar!(sᶜᶜⁿ, sᶠᶜⁿ, sᶜᶠⁿ, sᶠᶠⁿ, sᶜᶜ⁻, sᶠᶜ⁻, sᶜᶠ⁻, η_grid, η, Hᶜᶜ, Hᶠᶜ, Hᶜᶠ, Hᶠᶠ, grid)
+@kernel function _update_zstar!(sᶜᶜⁿ, sᶠᶜⁿ, sᶜᶠⁿ, sᶠᶠⁿ, sᶜᶜ⁻, sᶠᶜ⁻, sᶜᶠ⁻, η_grid, η, grid)
     i, j = @index(Global, NTuple)
     k_top = grid.Nz+1
+
+    Hᶜᶜ = domain_depthᶜᶜᵃ(i, j, grid)
+    Hᶠᶜ = domain_depthᶠᶜᵃ(i, j, grid)
+    Hᶜᶠ = domain_depthᶜᶠᵃ(i, j, grid)
+    Hᶠᶠ = domain_depthᶠᶠᵃ(i, j, grid)
+
     @inbounds begin
-        hᶜᶜ = (Hᶜᶜ[i, j, 1] + η[i, j, k_top]) / Hᶜᶜ[i, j, 1]
-        hᶠᶜ = (Hᶠᶜ[i, j, 1] +  ℑxᶠᵃᵃ(i, j, k_top, grid, η)) / Hᶠᶜ[i, j, 1]
-        hᶜᶠ = (Hᶜᶠ[i, j, 1] +  ℑyᵃᶠᵃ(i, j, k_top, grid, η)) / Hᶜᶠ[i, j, 1]
-        hᶠᶠ = (Hᶠᶠ[i, j, 1] + ℑxyᶠᶠᵃ(i, j, k_top, grid, η)) / Hᶠᶠ[i, j, 1]
+        hᶜᶜ = (Hᶜᶜ + η[i, j, k_top]) / Hᶜᶜ
+        hᶠᶜ = (Hᶠᶜ +  ℑxᶠᵃᵃ(i, j, k_top, grid, η)) / Hᶠᶜ
+        hᶜᶠ = (Hᶜᶠ +  ℑyᵃᶠᵃ(i, j, k_top, grid, η)) / Hᶜᶠ
+        hᶠᶠ = (Hᶠᶠ + ℑxyᶠᶠᵃ(i, j, k_top, grid, η)) / Hᶠᶠ
 
         sᶜᶜ⁻[i, j] = sᶜᶜⁿ[i, j]
         sᶠᶜ⁻[i, j] = sᶠᶜⁿ[i, j]
@@ -86,10 +87,10 @@ end
 ##### ZStar-specific implementation of the additional terms to be included in the momentum equations
 #####
 
-@inline z_minus_rᶜᶜᶜ(i, j, k, grid, η, Hᶜᶜ) = @inbounds η[i, j, grid.Nz+1] * (1 + rnode(i, j, k, grid, Center(), Center(), Center()) / Hᶜᶜ[i, j, 1])
+@inline z_minus_rᶜᶜᶜ(i, j, k, grid, η) = @inbounds η[i, j, grid.Nz+1] * (1 + rnode(i, j, k, grid, Center(), Center(), Center()) / domain_depthᶜᶜᵃ(i, j, grid))
 
-@inline ∂x_z(i, j, k, grid, free_surface) = @inbounds ∂xᶠᶜᶜ(i, j, k, grid, z_minus_rᶜᶜᶜ, free_surface.η, free_surface.auxiliary.Hᶜᶜ)
-@inline ∂y_z(i, j, k, grid, free_surface) = @inbounds ∂yᶜᶠᶜ(i, j, k, grid, z_minus_rᶜᶜᶜ, free_surface.η, free_surface.auxiliary.Hᶜᶜ)
+@inline ∂x_z(i, j, k, grid, free_surface) = @inbounds ∂xᶠᶜᶜ(i, j, k, grid, z_minus_rᶜᶜᶜ, free_surface.η)
+@inline ∂y_z(i, j, k, grid, free_surface) = @inbounds ∂yᶜᶠᶜ(i, j, k, grid, z_minus_rᶜᶜᶜ, free_surface.η)
 
 @inline grid_slope_contribution_x(i, j, k, grid::ZStarSpacingGrid, free_surface, ::Nothing, model_fields) = zero(grid)
 @inline grid_slope_contribution_y(i, j, k, grid::ZStarSpacingGrid, free_surface, ::Nothing, model_fields) = zero(grid)
