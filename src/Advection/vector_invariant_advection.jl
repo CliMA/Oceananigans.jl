9a3207c858e4a45f57044790c28faa967c592e41
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-10-25 06:31:50 +1100

    add a docstring

--- src/Advection/vector_invariant_advection.jl
+++ src/Advection/vector_invariant_advection.jl
@@ -175,10 +175,19 @@ Base.show(io::IO, a::VectorInvariant{N, FT}) where {N, FT} =
 nothing_to_default(user_value; default) = isnothing(user_value) ? default : user_value
 
 """
-    WENOVectorInvariant(; upwinding = nothing,
-                          multi_dimensional_stencil = false,
-                          weno_kw...)
+    WENOVectorInvariant(FT = Float64; 
+                        upwinding = nothing,
+                        vorticity_stencil = VelocityStencil(),
+                        order = nothing,
+                        vorticity_order = nothing,
+                        vertical_order = nothing,
+                        divergence_order = nothing,
+                        kinetic_energy_gradient_order = nothing, 
+                        multi_dimensional_stencil = false,
+                        weno_kw...)
 
+Return a vector-invariant weighted essentially non-oscillatory (WENO) scheme.
+See [`VectorInvariant`](@ref) and [`WENO`](@ref) for kwargs definitions.
 """
 function WENOVectorInvariant(FT::DataType = Float64; 
                              upwinding = nothing,
