281ce84d1019a331fb272865e50cf59ab405cfad
Simone Silvestri <silvestri.simone0@gmail.com>
2024-10-28 17:53:04 +0100

    add a test

--- validation/implicit_dissipation/implicit_dissipation.jl
+++ validation/implicit_dissipation/implicit_dissipation.jl
@@ -29,46 +29,74 @@ const α = 10
     end
 end
 
+grid = RectilinearGrid(size = 100, halo = 6, x = (-1, 1), topology = (Periodic, Flat, Flat))
 
-grid      = RectilinearGrid(size = 100, halo = 6, x = (-1, 1), topology = (Periodic, Flat, Flat))
-advection = WENO(; order = 7) 
-model     = NonhydrostaticModel(; grid, tracers = :b, timestepper = :QuasiAdamsBashforth2, advection) 
+advections = [UpwindBiased(; order = 3), 
+              WENO(; order = 5), 
+              WENO(; order = 7)]
 
-set!(model.tracers.b, bᵢ)
-set!(model.velocities.u, 1.0)
+labels  = ["upwind3",
+           "WENO5",
+           "WENO7"]
 
-b₀ = deepcopy(model.tracers.b)
-Δt = 0.1 * minimum_xspacing(grid)
+for (advection, label) in zip(advections, labels)
+    model = NonhydrostaticModel(; grid, tracers = :b, timestepper = :QuasiAdamsBashforth2, advection) 
 
-dissipation_computation = VarianceDissipationComputation(model; tracers = :b)
-simulation  = Simulation(model; Δt, stop_time = 10)
-simulation.callbacks[:compute_dissipation] = Callback(dissipation_computation, IterationInterval(1))
+    set!(model.tracers.b, bᵢ)
+    set!(model.velocities.u, 1.0)
 
-outputs = (; Px = dissipation_computation.production.b.x, b = model.tracers.b)
+    Δt = 0.1 * minimum_xspacing(grid)
 
-# Save the dissipation
-simulation.output_writers[:dissipation] = JLD2OutputWriter(model, outputs,
-                                                            filename = "dissipation.jld2", 
-                                                            schedule = IterationInterval(1),
-                                                            overwrite_existing = true)
+    dissipation_computation = VarianceDissipationComputation(model; tracers = :b)
+    simulation  = Simulation(model; Δt, stop_time = 10)
+    simulation.callbacks[:compute_dissipation] = Callback(dissipation_computation, IterationInterval(1))
 
-run!(simulation)
-b = FieldTimeSeries("dissipation.jld2", "b")
-P = FieldTimeSeries("dissipation.jld2", "Px")
+    outputs = (; Px = dissipation_computation.production.b.x, b = model.tracers.b)
+
+    filename = "dissipation_" * label * ".jld2"
+
+    # Save the dissipation
+    simulation.output_writers[:dissipation] = JLD2OutputWriter(model, outputs;
+                                                               filename,
+                                                               schedule = IterationInterval(10),
+                                                               overwrite_existing = true)
+
+    run!(simulation)
+end
+
+b_series = []
+P_series = []
 
 iter = Observable(1)
 
-bn = @lift(interior(b[$iter], :, 1, 1))
-Pn = @lift(interior(b[$iter], :, 1, 1))
+bn = []
+Pn = []
+
+for label in labels
+    filename = "dissipation_" * label * ".jld2"
+
+    push!(b_series, FieldTimeSeries(filename, "b"))
+    push!(P_series, FieldTimeSeries(filename, "Px"))
+
+    push!(bn, @lift(interior(b_series[end][$iter], :, 1, 1)))
+    push!(Pn, @lift(interior(P_series[end][$iter], :, 1, 1)))
+end
+
+x = xnodes(b_series[1][1])
 
 fig = Figure(size = (800, 400))
 ax  = Axis(fig[1, 1], xlabel = L"x", ylabel = L"tracer")
-lines!(ax, xnodes(b[1]), b0, color = :grey, linestyle = :dash, linewidth = 2)
-lines!(ax, xnodes(b[1]), bn, color = :blue)
+lines!(ax, xnodes(b[1]), interior(b[1], :, 1, 1), color = :grey, linestyle = :dash, linewidth = 2)
+for case in eachindex(labels)
+    lines!(ax, x, bn[case], label = labels[case])
+end
 
 ax  = Axis(fig[1, 1], xlabel = L"x", ylabel = L"variance dissipation")
-lines!(ax, xnodes(b[1]), Pn, color = :red)
+for case in eachindex(labels)
+    lines!(ax, x, Pn[case], label = labels[case])
+end
 
 record(fig, "implicit_dissipation.mp4", 1:length(b), framerate=8) do i
-    n[] = i
+    @info "doing iter $i"
+    iter[] = i
 end
