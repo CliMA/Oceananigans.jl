b47b39b0bc741530fdf5cb5bc1d6e9299a7df8bc
Simone Silvestri <silvestri.simone0@gmail.com>
2024-10-29 11:36:31 +0100

    not used

--- test/test_vector_rotation_operators.jl
+++ test/test_vector_rotation_operators.jl
@@ -6,8 +6,6 @@ using Oceananigans.Operators
 using Oceananigans.CubedSpheres
 using Oceananigans.Models.HydrostaticFreeSurfaceModels
 using Oceananigans.Models.HydrostaticFreeSurfaceModels: VerticalVorticityField
-
-using OrthogonalSphericalShellGrids
     
 # To be used in the test below as `KernelFunctionOperation`s
 @inline intrinsic_vector_x_component(i, j, k, grid, uₑ, vₑ) = 
@@ -16,11 +14,11 @@ using OrthogonalSphericalShellGrids
 @inline intrinsic_vector_y_component(i, j, k, grid, uₑ, vₑ) =
     @inbounds intrinsic_vector(i, j, k, grid, uₑ, vₑ)[2]
 
-@inline extrinsic_vector_x_component(i, j, k, grid, uₑ, vₑ) =
-    @inbounds intrinsic_vector(i, j, k, grid, uₑ, vₑ)[1]
+@inline extrinsic_vector_x_component(i, j, k, grid, uᵢ, vᵢ) =
+    @inbounds extrinsic_vector(i, j, k, grid, uᵢ, vᵢ)[1]
     
-@inline extrinsic_vector_y_component(i, j, k, grid, uₑ, vₑ) =
-    @inbounds intrinsic_vector(i, j, k, grid, uₑ, vₑ)[2]
+@inline extrinsic_vector_y_component(i, j, k, grid, uᵢ, vᵢ) =
+    @inbounds extrinsic_vector(i, j, k, grid, uᵢ, vᵢ)[2]
 
 function kinetic_energy(u, v)
     ke = Field(0.5 * (u * u + v * v))
@@ -134,99 +132,13 @@ function test_vector_rotation(grid)
     @test all(on_architecture(CPU(), interior(uₑ)) .≈ 0.5)
 end
     
-
-@testset "Cubed spheres" begin
-
-    @testset "Conformal cubed sphere grid" begin
-        @info "  Testing conformal cubed sphere grid..."
-
-        grid = ConformalCubedSphereGrid(panel_size=(10, 10, 1), z=(-1, 0))
-        @test try show(grid); println(); true; catch; false; end
-    end
-
+@testset "Vector rotation" begin
     for arch in archs
+        @testset "Conversion from Intrinsic to Extrinsic reference frame [$(typeof(arch))]" begin
+            @info "  Testing the conversion of a vector between the Intrinsic and Extrinsic reference frame"
 
-        @info "  Constructing a ConformalCubedSphereGrid from file [$(typeof(arch))]..."
-
-        # These tests cause an undefined `Bound Access Error` on GPU's CI with the new CUDA version.
-        # The error is not reproducible neither on Tartarus nor on Sverdrup.
-        # These are excised for the moment (PR #2253) as Cubed sphere will be reworked
-        if !(arch isa GPU)
-            # Prototype grid and model for subsequent tests
-            cs32_filepath = datadep"cubed_sphere_32_grid/cubed_sphere_32_grid.jld2"
-            grid = OldConformalCubedSphereGrid(cs32_filepath, arch, Nz=1, z=(-1, 0))
-
-            @info "  Constructing a HydrostaticFreeSurfaceModel on a ConformalCubedSphereGrid [$(typeof(arch))]..."
-
-            free_surface = ExplicitFreeSurface(gravitational_acceleration=0.1)
-            model = HydrostaticFreeSurfaceModel(; grid, free_surface,
-                                                momentum_advection = VectorInvariant(),
-                                                coriolis = nothing,
-                                                closure = nothing,
-                                                tracers = :c,
-                                                buoyancy = nothing)
-
-            @testset "Constructing a grid from file [$(typeof(arch))]" begin
-                @test grid isa ConformalCubedSphereGrid
-            end
-
-            @testset "Conversion from Intrinsic to Extrinsic reference frame [$(typeof(arch))]" begin
-                @info "  Testing the conversion of a vector between the Intrinsic and Extrinsic reference frame"
-                trg_grid = TripolarGrid(arch, size = (20, 20, 1), z = (0, 1))
-
-                test_vector_rotation(grid)
-                test_vector_rotation(trg_grid)
-            end
-
-            @testset "CubedSphereData and CubedSphereFields [$(typeof(arch))]" begin
-                @info "  Testing CubedSphereData and CubedSphereFields [$(typeof(arch))]..."
-                c = model.tracers.c
-                η = model.free_surface.η
-
-                set!(c, 0)
-                set!(η, 0)
-
-                CUDA.allowscalar(true)
-                @test all(all(face_c .== 0) for face_c in faces(c))
-                @test all(all(face_η .== 0) for face_η in faces(η))
-                CUDA.allowscalar(false)
-
-                @test maximum(abs, c) == 0
-                @test minimum(abs, c) == 0
-                @test mean(c) == 0
-
-                @test maximum(abs, η) == 0
-                @test minimum(abs, η) == 0
-                @test mean(η) == 0
-            end
-
-            @testset "Constructing a HydrostaticFreeSurfaceModel [$(typeof(arch))]" begin
-                @test model isa HydrostaticFreeSurfaceModel
-            end
-
-            @testset "Time stepping a HydrostaticFreeSurfaceModel [$(typeof(arch))]" begin
-                @info "  Time-stepping HydrostaticFreeSurfaceModel on a ConformalCubedSphereGrid [$(typeof(arch))]..."
-                time_step!(model, 1)
-                @test try time_step!(model, 1); true; catch; false; end
-            end
-
-            @testset "VerticalVorticityField on ConformalCubedSphereGrid [$(typeof(arch))]" begin
-                @info "  Testing VerticalVorticityField on a ConformalCubedSphereGrid [$(typeof(arch))]..."
-                ζ = VerticalVorticityField(model)
-
-                @test ζ isa Field
-
-                set!(model, u = (x, y, z) -> rand())
-
-                @test try
-                    compute!(ζ)
-                    true
-                catch err
-                    println(sprint(showerror, err))
-                    false
-                end
-                @test maximum(abs, ζ) > 0 # fingers crossed
-            end
+            grid = ConformalCubedSphereGrid(arch; panel_size=(10, 10, 1), z=(-1, 0))
+            test_vector_rotation(grid)
         end
     end
 end
