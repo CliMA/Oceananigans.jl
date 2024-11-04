7529ae0e7e49052d3b8bbdd8cad00376eb4f04f1
Simone Silvestri <silvestri.simone0@gmail.com>
2024-11-04 17:53:46 +0100

    add spectrum

--- check_field.jl
+++ check_field.jl
@@ -214,3 +214,59 @@ end
 # hfile = h5open(data_directory * "baroclinic_training_data.hdf5", "w")
 # hfile["timeseries"] = state
 # close(hfile)
+
+using FFTW
+using FFTW
+using Oceananigans.Grids: φnode
+using Statistics: mean
+
+struct Spectrum{S, F}
+    spec :: S
+    freq :: F
+end
+
+import Base
+
+Base.:(+)(s::Spectrum, t::Spectrum) = Spectrum(s.spec .+ t.spec, s.freq)
+Base.:(*)(s::Spectrum, t::Spectrum) = Spectrum(s.spec .* t.spec, s.freq)
+Base.:(/)(s::Spectrum, t::Int)      = Spectrum(s.spec ./ t, s.freq)
+
+Base.real(s::Spectrum) = Spectrum(real.(s.spec), s.freq)
+Base.abs(s::Spectrum)  = Spectrum( abs.(s.spec), s.freq)
+
+@inline onefunc(args...)  = 1.0
+@inline hann_window(n, N) = sin(π * n / N)^2 
+
+function power_cospectrum_1d(var1, var2, x; windowing = onefunc)
+
+    Nx = length(x)
+    Nfx = Int64(Nx)
+    
+    spectra = zeros(ComplexF64, Int(Nfx/2))
+    
+    dx = x[2] - x[1]
+
+    freqs = fftfreq(Nfx, 1.0 / dx) # 0, +ve freq,-ve freqs (lowest to highest)
+    freqs = freqs[1:Int(Nfx/2)] .* 2.0 .* π
+    
+    windowed_var1 = [var1[i] * windowing(i, Nfx) for i in 1:Nfx]
+    windowed_var2 = [var2[i] * windowing(i, Nfx) for i in 1:Nfx]
+    fourier1      = fft(windowed_var1) / Nfx
+    fourier2      = fft(windowed_var2) / Nfx
+    spectra[1]    += fourier1[1] .* conj(fourier2[1]) .+ fourier2[1] .* conj(fourier1[1])
+
+    for m in 2:Int(Nfx/2)
+        spectra[m] += fourier1[m] .* conj(fourier2[m]) .+ fourier2[m] .* conj(fourier1[m])
+    end
+
+    return Spectrum(spectra, freqs)
+end
+
+function zonal_spectrum(field, j, k)
+    x, y, z = nodes(field)
+
+    var1 = Array(interior(field, :, j, k))
+    var2 = Array(interior(field, :, j, k))
+
+    return power_cospectrum_1d(var1, var2, x)
+end
