2c6ad90b5d19953502b95c98388d4abaf8f840ff
Simone Silvestri <silvestri.simone0@gmail.com>
2024-11-07 10:37:37 +0100

    recompile everywhere

--- test/runtests.jl
+++ test/runtests.jl
@@ -179,12 +179,14 @@ CUDA.allowscalar() do
 
     if group == :distributed_solvers || group == :all
         MPI.Initialized() || MPI.Init()
+        CUDA.precompile_runtime()
         include("test_distributed_transpose.jl")
         include("test_distributed_poisson_solvers.jl")
     end
 
     if group == :distributed_hydrostatic_model || group == :all
         MPI.Initialized() || MPI.Init()
+        CUDA.precompile_runtime()
         archs = test_architectures()
         include("test_hydrostatic_regression.jl")
         include("test_distributed_hydrostatic_model.jl")
@@ -192,6 +194,7 @@ CUDA.allowscalar() do
 
     if group == :distributed_nonhydrostatic_regression || group == :all
         MPI.Initialized() || MPI.Init()
+        CUDA.precompile_runtime()
         archs = nonhydrostatic_regression_test_architectures()
         include("test_nonhydrostatic_regression.jl")
     end
