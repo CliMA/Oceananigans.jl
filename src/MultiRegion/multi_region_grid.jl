2f208f364525ae9dda6fc6c4b161c1cf2bfc024f
Gregory Wagner <wagner.greg@gmail.com>
2024-11-09 15:47:24 -0700

    Suppress doctest output for multiregion grid

--- src/MultiRegion/multi_region_grid.jl
+++ src/MultiRegion/multi_region_grid.jl
@@ -93,14 +93,7 @@ julia> grid = RectilinearGrid(size=(12, 12), extent=(1, 1), topology=(Bounded, B
 ├── Bounded  y ∈ [0.0, 1.0] regularly spaced with Δy=0.0833333
 └── Flat z
 
-julia> multi_region_grid = MultiRegionGrid(grid, partition = XPartition(4))
-┌ Warning: MultiRegion functionalities are experimental: help the development by reporting bugs or non-implemented features!
-└ @ Oceananigans.MultiRegion ~/Research/OC11.jl/src/MultiRegion/multi_region_grid.jl:108
-MultiRegionGrid{Float64, Bounded, Bounded, Flat} partitioned on CPU():
-├── grids: 3×12×1 RectilinearGrid{Float64, RightConnected, Bounded, Flat} on CPU with 3×3×0 halo
-├── partitioning: Equal partitioning in X with (4 regions)
-├── connectivity: MultiRegionObject{Tuple{@NamedTuple{west::Nothing, east::Oceananigans.MultiRegion.RegionalConnectivity{Oceananigans.MultiRegion.East, Oceananigans.MultiRegion.West}, north::Nothing, south::Nothing}, @NamedTuple{west::Oceananigans.MultiRegion.RegionalConnectivity{Oceananigans.MultiRegion.West, Oceananigans.MultiRegion.East}, east::Oceananigans.MultiRegion.RegionalConnectivity{Oceananigans.MultiRegion.East, Oceananigans.MultiRegion.West}, north::Nothing, south::Nothing}, @NamedTuple{west::Oceananigans.MultiRegion.RegionalConnectivity{Oceananigans.MultiRegion.West, Oceananigans.MultiRegion.East}, east::Oceananigans.MultiRegion.RegionalConnectivity{Oceananigans.MultiRegion.East, Oceananigans.MultiRegion.West}, north::Nothing, south::Nothing}, @NamedTuple{west::Oceananigans.MultiRegion.RegionalConnectivity{Oceananigans.MultiRegion.West, Oceananigans.MultiRegion.East}, east::Nothing, north::Nothing, south::Nothing}}, NTuple{4, CPU}}
-└── devices: (CPU(), CPU(), CPU(), CPU())
+julia> multi_region_grid = MultiRegionGrid(grid, partition = XPartition(4));
 ```
 """
 function MultiRegionGrid(global_grid; partition = XPartition(2),
