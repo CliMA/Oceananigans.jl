0937b88efd8da9c7177708124f758e4285b8a761
Simone Silvestri <silvestri.simone0@gmail.com>
2024-11-11 11:40:03 +0100

    remove the launching of the kernels

--- src/BoundaryConditions/fill_halo_regions_open.jl
+++ src/BoundaryConditions/fill_halo_regions_open.jl
@@ -14,17 +14,19 @@ function fill_open_boundary_regions!(field, boundary_conditions, indices, loc, g
 
     # gets `open_fill`, the function which fills open boundaries at `loc`, as well as `regular_fill`
     # which is the function which fills non-open boundaries at `loc` which informs `fill_halo_size` 
-    open_fill, regular_fill = get_open_halo_filling_functions(loc) 
+    fill_halo_function = get_open_halo_filling_functions(loc) 
 
-    # Computing the size of the fill halo kernel. We use the `regular_fill` function to
-    # calculate the index range. Remember: windowed fields require the halos
-    # to be computed only on a part of the boundary so we need to offset the kernel indices 
-    size   = fill_halo_size(field, regular_fill, indices, boundary_conditions, loc, grid)
-    offset = fill_halo_offset(size, regular_fill, indices)
-    params = KernelParameters(size, offset)
+    if !isnothing(fill_halo_function)
+        # Computing the parameters of the fill halo kernel. 
+        # Remember: windowed fields require the halos to be computed only on a part of the boundary 
+        # so we need to offset the kernel indices 
+        size   = fill_halo_size(field, fill_halo_function, indices, boundary_conditions, loc, grid)
+        offset = fill_halo_offset(size, fill_halo_function, indices)
+        params = KernelParameters(size, offset)
+        
+        launch!(arch, grid, params, fill_halo_function, field, left_bc, right_bc, loc, grid, args)
+    end
     
-    launch!(arch, grid, params, open_fill, field, left_bc, right_bc, loc, grid, args)
-
     return nothing
 end
 
@@ -51,15 +53,20 @@ fill_open_boundary_regions!(fields::NTuple, boundary_conditions, indices, loc, g
 @inline right_velocity_open_boundary_condition(boundary_conditions::Tuple, ::Tuple{Center, Face, Center}) = @inbounds boundary_conditions[2]
 @inline right_velocity_open_boundary_condition(boundary_conditions::Tuple, ::Tuple{Center, Center, Face}) = @inbounds boundary_conditions[2]
 
-@inline get_open_halo_filling_functions(loc) = _no_fill!, _no_fill!
-@inline get_open_halo_filling_functions(::Tuple{Face, Center, Center}) = _fill_west_and_east_open_halo!, fill_west_and_east_halo!
-@inline get_open_halo_filling_functions(::Tuple{Center, Face, Center}) = _fill_south_and_north_open_halo!, fill_south_and_north_halo!
-@inline get_open_halo_filling_functions(::Tuple{Center, Center, Face}) = _fill_bottom_and_top_open_halo!, fill_bottom_and_top_halo!
+@inline get_open_halo_filling_functions(loc) = nothing
+@inline get_open_halo_filling_functions(::Tuple{Face, Center, Center}) = _fill_west_and_east_open_halo!
+@inline get_open_halo_filling_functions(::Tuple{Center, Face, Center}) = _fill_south_and_north_open_halo!
+@inline get_open_halo_filling_functions(::Tuple{Center, Center, Face}) = _fill_bottom_and_top_open_halo!
 
-@kernel _no_fill!(args...) = nothing
+# Extending the fill_halo_size and fill_halo_offset functions for the 
+# open boundary conditions kernels. There are exactly the same as for the regular boundary conditions
+@inline fill_halo_size(field, ::typeof(_fill_west_and_east_open_halo!),   args...) = fill_halo_size(field, _fill_west_and_east_halo!, args...)
+@inline fill_halo_size(field, ::typeof(_fill_south_and_north_open_halo!), args...) = fill_halo_size(field, _fill_sourth_and_north_halo!, args...)
+@inline fill_halo_size(field, ::typeof(_fill_bottom_and_top_open_halo!),  args...) = fill_halo_size(field, _fill_bottom_and_top_halo!, args...)
 
-@inline fill_halo_size(field, ::typeof(_no_fill!), args...)    = (0, 0)
-@inline fill_halo_offset(field, ::typeof(_no_fill!), args...) = (0, 0)
+@inline fill_halo_offset(size, ::typeof(_fill_west_and_east_open_halo!),   args...) = fill_halo_offset(size, _fill_west_and_east_halo!, args...)
+@inline fill_halo_offset(size, ::typeof(_fill_south_and_north_open_halo!), args...) = fill_halo_offset(size, _fill_sourth_and_north_halo!, args...)
+@inline fill_halo_offset(size, ::typeof(_fill_bottom_and_top_open_halo!),  args...) = fill_halo_offset(size, _fill_bottom_and_top_halo!, args...)
 
 @kernel function _fill_west_and_east_open_halo!(c, west_bc, east_bc, loc, grid, args) 
     j, k = @index(Global, NTuple)
