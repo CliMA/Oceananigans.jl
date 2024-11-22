f6b9cd6ad357f597f481802965d0284e99a53066
Simone Silvestri <silvestri.simone0@gmail.com>
2024-11-21 16:08:17 -0800

    remove extra definition

--- src/TurbulenceClosures/turbulence_closure_implementations/isopycnal_skew_symmetric_diffusivity_with_triads.jl
+++ src/TurbulenceClosures/turbulence_closure_implementations/isopycnal_skew_symmetric_diffusivity_with_triads.jl
@@ -176,11 +176,6 @@ end
 @inline ϵκy⁻⁺(i, j, k, grid, loc, κ, clock, b, C) = triad_mask_y(i, j,   j, k, k+1, grid) * κᶜᶜᶜ(i, j, k, grid, loc, κ, clock) * tapering_factorᶜᶜᶜ(i, j, k, grid, b, C)
 @inline ϵκy⁻⁻(i, j, k, grid, loc, κ, clock, b, C) = triad_mask_y(i, j,   j, k, k,   grid) * κᶜᶜᶜ(i, j, k, grid, loc, κ, clock) * tapering_factorᶜᶜᶜ(i, j, k, grid, b, C)
 
-# Diffusive fluxes
-
-@inline get_tracer_κ(κ::NamedTuple, tracer_index) = @inbounds κ[tracer_index]
-@inline get_tracer_κ(κ, tracer_index) = κ
-
 # Triad diagram key
 # =================
 #
