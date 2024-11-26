4898be4c8b0854a9a5285994b8c6afdf1e827b41
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2024-11-26 13:02:02 -0500

    new NN closure including Ri

--- NNclosure_Ri_nof_BBLkappazonelast55_doublegyre_model.jl
+++ NNclosure_Ri_nof_BBLkappazonelast55_doublegyre_model.jl
@@ -21,7 +21,7 @@ using SeawaterPolynomials:TEOS10
 using ColorSchemes
 
 #%%
-filename = "doublegyre_30Cwarmflushbottom10_relaxation_8days_NN_closure_NDE_Ri_BBLkappazonelast55_temp"
+filename = "doublegyre_30Cwarmflushbottom10_relaxation_30days_NN_closure_NDE_Ri_BBLkappazonelast55_temp"
 FILE_DIR = "./Output/$(filename)"
 # FILE_DIR = "/storage6/xinkai/NN_Oceananigans/$(filename)"
 mkpath(FILE_DIR)
@@ -69,7 +69,7 @@ const S_mid = (S_north + S_south) / 2
 const τ₀ = 1e-4
 
 const μ_drag = 1/30days
-const μ_T = 1/8days
+const μ_T = 1/30days
 
 #####
 ##### Forcing and initial condition
