5a2f91b69da12097757444dc79b23f9fc617802f
Simone Silvestri <silvestri.simone0@gmail.com>
2024-11-29 10:58:20 +0100

    no need for a loop

--- src/Models/HydrostaticFreeSurfaceModels/update_hydrostatic_free_surface_model_state.jl
+++ src/Models/HydrostaticFreeSurfaceModels/update_hydrostatic_free_surface_model_state.jl
@@ -71,27 +71,25 @@ function mask_immersed_model_fields!(model, grid)
     return nothing
 end
 
-function compute_auxiliaries!(model::HydrostaticFreeSurfaceModel; w_parameters = tuple(w_kernel_parameters(model.grid)),
-                                                                  p_parameters = tuple(p_kernel_parameters(model.grid)),
-                                                                  κ_parameters = tuple(:xyz)) 
+function compute_auxiliaries!(model::HydrostaticFreeSurfaceModel; w_params = w_kernel_parameters(model.grid),
+                                                                  p_params = p_kernel_parameters(model.grid),
+                                                                  κ_params = :xyz) 
     
     grid        = model.grid
     closure     = model.closure
     tracers     = model.tracers
     diffusivity = model.diffusivity_fields
 
-    for (wpar, ppar, κpar) in zip(w_parameters, p_parameters, κ_parameters)
-        # Update the grid
-        update_grid!(model, grid; parameters = wpar)
-        unscale_tracers!(tracers, grid; parameters = wpar)
+    # Update the grid and unscale the tracers
+    update_grid!(model, grid; parameters = w_params)
+    unscale_tracers!(tracers, grid; parameters = w_params)
         
-        # Update the other auxiliary terms
-        compute_w_from_continuity!(model; parameters = wpar)
-        compute_diffusivities!(diffusivity, closure, model; parameters = κpar)
-        update_hydrostatic_pressure!(model.pressure.pHY′, architecture(grid),
-                                     grid, model.buoyancy, model.tracers; 
-                                     parameters = ppar)
-    end
+    # Update the other auxiliary terms
+    compute_w_from_continuity!(model; parameters = w_params)
+    compute_diffusivities!(diffusivity, closure, model; parameters = κ_params)
+    update_hydrostatic_pressure!(model.pressure.pHY′, architecture(grid),
+                                grid, model.buoyancy, model.tracers; 
+                                parameters = p_params)
 
     return nothing
 end
