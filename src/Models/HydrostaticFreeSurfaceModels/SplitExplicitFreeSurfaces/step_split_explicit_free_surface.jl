4373ec15ee3e39e23c53ad615583041ea69ff292
Simone Silvestri <silvestri.simone0@gmail.com>
2024-12-10 13:16:02 +0100

    remove all duplication

--- src/Models/HydrostaticFreeSurfaceModels/SplitExplicitFreeSurfaces/step_split_explicit_free_surface.jl
+++ src/Models/HydrostaticFreeSurfaceModels/SplitExplicitFreeSurfaces/step_split_explicit_free_surface.jl
@@ -89,7 +89,6 @@ function iterate_split_explicit!(free_surface, grid, GUⁿ, GVⁿ, Δτᴮ, weig
         converted_η_args = convert_args(arch, η_args)
         converted_U_args = convert_args(arch, U_args)
 
-
         @unroll for substep in 1:Nsubsteps
             Base.@_inline_meta
             averaging_weight = weights[substep]
