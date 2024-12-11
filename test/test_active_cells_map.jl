071e14a256f72bdc212706592d0ef03b66fc671f
Simone Silvestri <silvestri.simone0@gmail.com>
2024-12-11 13:56:41 +0100

    on architecture

--- test/test_active_cells_map.jl
+++ test/test_active_cells_map.jl
@@ -69,6 +69,7 @@ Nz = 10
             bottom_height[i, j] = - rand() * 5 - 5
         end
 
+        bottom_height = on_architecture(arch, bottom_height)
         immersed_grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(bottom_height))
         immersed_active_grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(bottom_height); active_cells_map = true)
 
