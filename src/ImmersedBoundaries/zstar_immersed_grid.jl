998ce3bff9da6d6fd89787d0d208034196799785
Simone Silvestri <silvestri.simone0@gmail.com>
2024-12-11 14:23:22 +0100

    removed the drift in immersed boundary grids

--- src/ImmersedBoundaries/zstar_immersed_grid.jl
+++ src/ImmersedBoundaries/zstar_immersed_grid.jl
@@ -1,5 +1,6 @@
 using Oceananigans.Grids: AbstractZStarGrid
 using Oceananigans.Operators
+using Oceananigans.Operators: ZSRG, ZSLLG, ZSOSG, superscript_location
 
 import Oceananigans.Grids: dynamic_column_depthᶜᶜᵃ, 
                            dynamic_column_depthᶜᶠᵃ,
@@ -32,3 +33,26 @@ const ZStarGridOfSomeKind = Union{ZStarImmersedGrid, AbstractZStarGrid}
 @inline σ⁻(i, j, k, ibg::IBG, ℓx, ℓy, ℓz) = σ⁻(i, j, k, ibg.underlying_grid, ℓx, ℓy, ℓz)
 
 @inline ∂t_σ(i, j, k, ibg::IBG) = ∂t_σ(i, j, k, ibg.underlying_grid)
+
+# Extend the 3D vertical spacing operators on an Immersed ZStar grid
+const IZSRG  = ImmersedBoundaryGrid{<:Any, <:Any, <:Any, <:Any, <:ZSRG}
+const IZSLLG = ImmersedBoundaryGrid{<:Any, <:Any, <:Any, <:Any, <:ZSLLG}
+const IZSOSG = ImmersedBoundaryGrid{<:Any, <:Any, <:Any, <:Any, <:ZSOSG}
+
+for LX in (:ᶠ, :ᶜ), LY in (:ᶠ, :ᶜ), LZ in (:ᶠ, :ᶜ)
+    zspacing = Symbol(:Δz, LX, LY, LZ)
+    rspacing = Symbol(:Δr, LX, LY, LZ)
+
+    ℓx = superscript_location(LX)
+    ℓy = superscript_location(LY)
+    ℓz = superscript_location(LZ)
+
+    @eval begin
+        using Oceananigans.Operators: $rspacing
+        import Oceananigans.Operators: $zspacing
+
+        @inline $zspacing(i, j, k, grid::IZSRG)  = $rspacing(i, j, k, grid) * σⁿ(i, j, k, grid, $ℓx(), $ℓy(), $ℓz())
+        @inline $zspacing(i, j, k, grid::IZSLLG) = $rspacing(i, j, k, grid) * σⁿ(i, j, k, grid, $ℓx(), $ℓy(), $ℓz())
+        @inline $zspacing(i, j, k, grid::IZSOSG) = $rspacing(i, j, k, grid) * σⁿ(i, j, k, grid, $ℓx(), $ℓy(), $ℓz())
+    end
+end
