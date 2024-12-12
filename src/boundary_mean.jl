ff0c79ea4c9034b9e6b5af59d3563c24b5d33dde
Jago Strong-Wright <jagoosw@protonmail.com>
2024-12-12 20:37:36 +0000

    renamed

--- src/boundary_mean.jl
+++ src/boundary_mean.jl
@@ -1,42 +1,54 @@
 using Adapt
+using Oceananigans: location
+using Oceananigans.Fields: Center, Face
 using Oceananigans.AbstractOperations: GridMetricOperation, Ax, Ay, Az
 using Oceananigans.BoundaryConditions: BoundaryCondition, Open, PerturbationAdvection
 
 import Adapt: adapt_structure
 import Oceananigans.BoundaryConditions: update_boundary_condition!
 
-struct BoundaryMean{V}
+struct BoundaryAdjacentMean{V}
    value :: V
 
-   BoundaryMean(; value::BV = Ref(0.0)) where BV = new{BV}(value)
+   BoundaryAdjacentMean(; value::BV = Ref(0.0)) where BV = new{BV}(value)
 end
 
-@inline (value::BoundaryMean)(args...) = value.boundary_velocity[]
+@inline (bam::BoundaryAdjacentMean)(args...) = bam.value[]
 
-Adapt.adapt_structure(to, mo::BoundaryMean) = 
-    BoundaryMean(adapt(to, mo.mean_outflow_velocity[]))
+Adapt.adapt_structure(to, mo::BoundaryAdjacentMean) = 
+    BoundaryAdjacentMean(adapt(to, mo.mean_outflow_velocity[]))
 
-const MOPABC = BoundaryCondition{<:Open{<:PerturbationAdvection}, <:BoundaryMean}
+const MOPABC = BoundaryCondition{<:Open{<:PerturbationAdvection}, <:BoundaryAdjacentMean}
 
 @inline boundary_normal_area(::Union{Val{:west}, Val{:east}}, grid)   = GridMetricOperation((Face, Center, Center), Ax, grid)
 @inline boundary_normal_area(::Union{Val{:south}, Val{:north}}, grid) = GridMetricOperation((Center, Face, Center), Ay, grid)
 @inline boundary_normal_area(::Union{Val{:bottom}, Val{:top}}, grid)  = GridMetricOperation((Center, Center, Face), Az, grid)
 
-@inline boundary_adjacent_index(::Val{:east}, grid) = (size(grid, 1), 1, 1), (2, 3)
-@inline boundary_adjacent_index(::Val{:west}, grid) = (2, 1, 1), (2, 3)
+@inline boundary_adjacent_index(::Val{:east}, grid, loc) = (size(grid, 1), 1, 1), (2, 3)
+@inline boundary_adjacent_index(::Val{:west}, grid, loc) = (first_interior_index(side, loc, grid), 1, 1), (2, 3)
 
-@inline boundary_adjacent_index(::Val{:north}, grid) = (1, size(grid, 2), 1), (1, 3)
-@inline boundary_adjacent_index(::Val{:south}, grid) = (1, 2, 1), (1, 3)
+@inline boundary_adjacent_index(::Val{:north}, grid, loc) = (1, size(grid, 2), 1), (1, 3)
+@inline boundary_adjacent_index(::Val{:south}, grid, loc) = (1, first_interior_index(side, loc, grid), 1), (1, 3)
 
-@inline boundary_adjacent_index(::Val{:top}, grid)    = (1, 1, size(grid, 3)), (2, 3)
-@inline boundary_adjacent_index(::Val{:bottom}, grid) = (1, 1, 2), (2, 3)
+@inline boundary_adjacent_index(::Val{:top}, grid, loc)    = (1, 1, size(grid, 3)), (2, 3)
+@inline boundary_adjacent_index(::Val{:bottom}, grid, loc) = (1, 1, first_interior_index(side, loc, grid)), (2, 3)
+
+@inline first_interior_index(::Union{Val{:west}, Val{:east}}, ::Tuple{<:Center, <:Any, <:Any}, grid) = 1
+@inline first_interior_index(::Union{Val{:west}, Val{:east}}, ::Tuple{Face, <:Any, <:Any}, grid) = 2
+
+@inline first_interior_index(::Union{Val{:south}, Val{:north}}, ::Tuple{<:Any, <:Center, <:Any}, grid) = 1
+@inline first_interior_index(::Union{Val{:south}, Val{:north}}, ::Tuple{<:Any, Face, <:Any}, grid) = 2
+
+@inline first_interior_index(::Union{Val{:bottom}, Val{:top}}, ::Tuple{<:Any, <:Any, <:Center}, grid) = 1
+@inline first_interior_index(::Union{Val{:bottom}, Val{:top}}, ::Tuple{<:Any, <:Any, Face}, grid) = 2
 
 function update_boundary_condition!(bc::MOPABC, val_side, u, model)
     grid = model.grid
+    loc = location(u)
 
     An = boundary_normal_area(val_side, grid)
 
-    (i, j, k), dims = boundary_adjacent_index(val_side, grid)
+    (i, j, k), dims = boundary_adjacent_index(val_side, grid, loc)
     
     total_area = sum(An; dims)[i, j, k]
 
