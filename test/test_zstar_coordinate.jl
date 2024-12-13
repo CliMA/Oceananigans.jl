c94606ae4f716150d0d72071010da52c0ec32a2e
Simone Silvestri <silvestri.simone0@gmail.com>
2024-12-13 12:32:43 +0100

    skip the correct tests

--- test/test_zstar_coordinate.jl
+++ test/test_zstar_coordinate.jl
@@ -123,13 +123,12 @@ end
                 
                 for free_surface in [split_free_surface, implicit_free_surface, explicit_free_surface]
                     
-                    # TODO: There are parameter space issues with ImmersedBoundaryGrid and a immersed LatitudeLongitudeGrid
-                    # with a stretched vertical coordinate. For the moment we are skipping these tests.
+                    # TODO: There are parameter space issues with ImplicitFreeSurface and a immersed LatitudeLongitudeGrid
+                    # For the moment we are skipping these tests.
                     if (arch == GPU) && 
                        (free_surface isa ImplicitFreeSurface) && 
                        (grid isa ImmersedBoundaryGrid) && 
-                       (grid.underlying_grid isa LatitudeLongitudeGrid) &&
-                       (grid.z.Δᵃᵃᶠ isa AbstractArray)
+                       (grid.underlying_grid isa LatitudeLongitudeGrid)
 
                         @info "  Skipping $(info_message(grid, free_surface)) because of parameter space issues"
                         continue
