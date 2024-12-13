ec9dd88b2fc49c77953a4bd84bc42114e58192a6
Jago Strong-Wright <jagoosw@protonmail.com>
2024-12-13 18:05:05 +0000

    bug

--- src/boundary_mean.jl
+++ src/boundary_mean.jl
@@ -1,5 +1,5 @@
 using Adapt
-using Oceananigans: location
+using Oceananigans: instantiated_location
 using Oceananigans.Fields: Center, Face
 using Oceananigans.AbstractOperations: GridMetricOperation, Ax, Ay, Az
 using Oceananigans.BoundaryConditions: BoundaryCondition, Open, PerturbationAdvection
@@ -25,13 +25,13 @@ const MOPABC = BoundaryCondition{<:Open{<:PerturbationAdvection}, <:BoundaryAdja
 @inline boundary_normal_area(::Union{Val{:bottom}, Val{:top}}, grid)  = GridMetricOperation((Center, Center, Face), Az, grid)
 
 @inline boundary_adjacent_index(::Val{:east}, grid, loc) = (size(grid, 1), 1, 1), (2, 3)
-@inline boundary_adjacent_index(side::Val{:west}, grid, loc) = (first_interior_index(side, loc), 1, 1), (2, 3)
+@inline boundary_adjacent_index(val_side::Val{:west}, grid, loc) = (first_interior_index(val_side, loc), 1, 1), (2, 3)
 
 @inline boundary_adjacent_index(::Val{:north}, grid, loc) = (1, size(grid, 2), 1), (1, 3)
-@inline boundary_adjacent_index(side::Val{:south}, grid, loc) = (1, first_interior_index(side, loc), 1), (1, 3)
+@inline boundary_adjacent_index(val_side::Val{:south}, grid, loc) = (1, first_interior_index(val_side, loc), 1), (1, 3)
 
 @inline boundary_adjacent_index(::Val{:top}, grid, loc) = (1, 1, size(grid, 3)), (2, 3)
-@inline boundary_adjacent_index(side::Val{:bottom}, grid, loc) = (1, 1, first_interior_index(side, loc)), (2, 3)
+@inline boundary_adjacent_index(val_side::Val{:bottom}, grid, loc) = (1, 1, first_interior_index(val_side, loc)), (2, 3)
 
 @inline first_interior_index(::Union{Val{:west}, Val{:east}}, ::Tuple{Center, <:Any, <:Any}) = 1
 @inline first_interior_index(::Union{Val{:west}, Val{:east}}, ::Tuple{Face, <:Any, <:Any}) = 2
@@ -44,7 +44,7 @@ const MOPABC = BoundaryCondition{<:Open{<:PerturbationAdvection}, <:BoundaryAdja
 
 function update_boundary_condition!(bc::MOPABC, val_side, u, model)
     grid = model.grid
-    loc = location(u)
+    loc = instantiated_location(u)
 
     An = boundary_normal_area(val_side, grid)
 
