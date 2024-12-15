d42d2999209600e341b7f68e1c3286e43f1867ff
Simone Silvestri <silvestri.simone0@gmail.com>
2024-12-15 16:36:05 +0100

    remove form here

--- src/Coriolis/hydrostatic_spherical_coriolis.jl
+++ src/Coriolis/hydrostatic_spherical_coriolis.jl
@@ -67,20 +67,6 @@ Adapt.adapt_structure(to, coriolis::HydrostaticSphericalCoriolis) =
 
 const CoriolisActiveCellEnstrophyConserving = HydrostaticSphericalCoriolis{<:ActiveCellEnstrophyConserving}
 
-@inline not_peripheral_node(args...) = !peripheral_node(args...)
-
-@inline function mask_inactive_points_ℑxyᶠᶜᵃ(i, j, k, grid, f::Function, args...) 
-    neighboring_active_nodes = ℑxyᶠᶜᵃ(i, j, k, grid, not_peripheral_node, Center(), Face(), Center())
-    return ifelse(neighboring_active_nodes == 0, zero(grid),
-                  ℑxyᶠᶜᵃ(i, j, k, grid, f, args...) / neighboring_active_nodes)
-end
-
-@inline function mask_inactive_points_ℑxyᶜᶠᵃ(i, j, k, grid, f::Function, args...) 
-    neighboring_active_nodes = @inbounds ℑxyᶜᶠᵃ(i, j, k, grid, not_peripheral_node, Face(), Center(), Center())
-    return ifelse(neighboring_active_nodes == 0, zero(grid),
-                  ℑxyᶜᶠᵃ(i, j, k, grid, f, args...) / neighboring_active_nodes)
-end
-
 @inline x_f_cross_U(i, j, k, grid, coriolis::CoriolisActiveCellEnstrophyConserving, U) =
     @inbounds - ℑyᵃᶜᵃ(i, j, k, grid, fᶠᶠᵃ, coriolis) *
                 mask_inactive_points_ℑxyᶠᶜᵃ(i, j, k, grid, Δx_qᶜᶠᶜ, U[2]) / Δxᶠᶜᶜ(i, j, k, grid)
