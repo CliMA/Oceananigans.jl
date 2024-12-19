b750fa502d2795b95f19327f7d4f6f1556d463cf
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2024-12-18 23:27:58 -0500

    latest version of NN closure

---
+++ NN_closure_global_Ri_nof_BBLRifirstzone510_train62newstrongSO_20seed.jl
@@ -0,0 +1,294 @@
+import Oceananigans.TurbulenceClosures:
+        compute_diffusivities!,
+        DiffusivityFields,
+        viscosity, 
+        diffusivity,
+        diffusive_flux_x,
+        diffusive_flux_y, 
+        diffusive_flux_z,
+        viscous_flux_ux,
+        viscous_flux_uy,
+        viscous_flux_uz,
+        viscous_flux_vx,
+        viscous_flux_vy,
+        viscous_flux_vz,
+        viscous_flux_wx,
+        viscous_flux_wy,
+        viscous_flux_wz
+
+using Oceananigans.BuoyancyModels: ∂x_b, ∂y_b, ∂z_b 
+using Oceananigans.Coriolis
+using Oceananigans.Grids: φnode
+using Oceananigans.Grids: total_size
+using Oceananigans.Utils: KernelParameters
+using Oceananigans: architecture, on_architecture
+using Lux, LuxCUDA
+using JLD2
+using ComponentArrays
+using OffsetArrays
+using SeawaterPolynomials.TEOS10
+
+using KernelAbstractions: @index, @kernel, @private
+
+import Oceananigans.TurbulenceClosures: AbstractTurbulenceClosure, ExplicitTimeDiscretization
+
+using Adapt 
+
+include("./feature_scaling.jl")
+
+@inline hack_sind(φ) = sin(φ * π / 180)
+
+@inline fᶜᶜᵃ(i, j, k, grid, coriolis::HydrostaticSphericalCoriolis) = 2 * coriolis.rotation_rate * hack_sind(φnode(i, j, k, grid, Center(), Center(), Center()))
+@inline fᶜᶜᵃ(i, j, k, grid, coriolis::FPlane)    = coriolis.f
+@inline fᶜᶜᵃ(i, j, k, grid, coriolis::BetaPlane) = coriolis.f₀ + coriolis.β * ynode(i, j, k, grid, Center(), Center(), Center())
+
+struct NN{M, P, S}
+    model   :: M
+    ps      :: P
+    st      :: S
+end
+
+@inline (neural_network::NN)(input) = first(neural_network.model(input, neural_network.ps, neural_network.st))
+@inline tosarray(x::AbstractArray) = SArray{Tuple{size(x)...}}(x)
+
+struct NNFluxClosure{A <: NN, S, G} <: AbstractTurbulenceClosure{ExplicitTimeDiscretization, 3}
+    wT               :: A
+    wS               :: A
+    scaling          :: S
+    grid_point_above :: G
+    grid_point_below :: G
+end
+
+Adapt.adapt_structure(to, nn :: NNFluxClosure) = 
+    NNFluxClosure(Adapt.adapt(to, nn.wT), 
+                  Adapt.adapt(to, nn.wS), 
+                  Adapt.adapt(to, nn.scaling),
+                  Adapt.adapt(to, nn.grid_point_above),
+                  Adapt.adapt(to, nn.grid_point_below))
+
+Adapt.adapt_structure(to, nn :: NN) = 
+    NN(Adapt.adapt(to, nn.model), 
+       Adapt.adapt(to, nn.ps), 
+       Adapt.adapt(to, nn.st))
+
+function NNFluxClosure(arch)
+    dev = ifelse(arch == GPU(), gpu_device(), cpu_device())
+    nn_path = "./NDE5_Qb_Ri_nof_BBLRifirst510_train62newstrongSO_scalingtrain62newstrongSO_validate30new_btrain56newstrongSO_3layer_128_relu_20seed_2Pr_ls10_model.jld2"
+
+    ps, sts, scaling_params, wT_model, wS_model = jldopen(nn_path, "r") do file
+        ps = file["u_validation"] |> dev |> f64
+        sts = file["sts"] |> dev |> f64
+        scaling_params = file["scaling"]
+        wT_model = file["model"].wT
+        wS_model = file["model"].wS
+        return ps, sts, scaling_params, wT_model, wS_model
+    end
+
+    scaling = construct_zeromeanunitvariance_scaling(scaling_params)
+
+    wT_NN = NN(wT_model, ps.wT, sts.wT)
+    wS_NN = NN(wS_model, ps.wS, sts.wS)
+
+    grid_point_above = 10
+    grid_point_below = 5
+
+    return NNFluxClosure(wT_NN, wS_NN, scaling, grid_point_above, grid_point_below)
+end
+
+function DiffusivityFields(grid, tracer_names, bcs, closure::NNFluxClosure)
+    arch = architecture(grid)
+    wT = ZFaceField(grid)
+    wS = ZFaceField(grid)
+    first_index = Field((Center, Center, Nothing), grid, Int32)
+    last_index = Field((Center, Center, Nothing), grid, Int32)
+
+    N_input = closure.wT.model.layers.layer_1.in_dims
+    N_levels = closure.grid_point_above + closure.grid_point_below
+
+    Nx_in, Ny_in, _ = size(wT)
+    wrk_in = zeros(N_input, Nx_in, Ny_in, N_levels)
+    wrk_in = on_architecture(arch, wrk_in)
+
+    wrk_wT = zeros(Nx_in, Ny_in, 15)
+    wrk_wS = zeros(Nx_in, Ny_in, 15)
+    wrk_wT = on_architecture(arch, wrk_wT)
+    wrk_wS = on_architecture(arch, wrk_wS)
+
+    return (; wrk_in, wrk_wT, wrk_wS, wT, wS, first_index, last_index)
+end
+
+function compute_diffusivities!(diffusivities, closure::NNFluxClosure, model; parameters = :xyz)
+    arch = model.architecture
+    grid = model.grid
+    velocities = model.velocities
+    tracers    = model.tracers
+    buoyancy   = model.buoyancy
+    coriolis   = model.coriolis    
+    clock      = model.clock
+    top_tracer_bcs = NamedTuple(c => tracers[c].boundary_conditions.top for c in propertynames(tracers))
+
+    Riᶜ = model.closure[1].Riᶜ
+    Ri = model.diffusivity_fields[1].Ri
+
+    wrk_in = diffusivities.wrk_in
+    wrk_wT = diffusivities.wrk_wT
+    wrk_wS = diffusivities.wrk_wS
+    wT = diffusivities.wT
+    wS = diffusivities.wS
+
+    first_index = diffusivities.first_index
+    last_index = diffusivities.last_index
+
+    Nx_in, Ny_in, Nz_in = total_size(wT)
+    ox_in, oy_in, oz_in = wT.data.offsets
+    kp = KernelParameters((Nx_in, Ny_in, Nz_in), (ox_in, oy_in, oz_in))
+    kp_2D = KernelParameters((Nx_in, Ny_in), (ox_in, oy_in))
+
+    N_levels = closure.grid_point_above + closure.grid_point_below
+
+    kp_wrk = KernelParameters((Nx_in, Ny_in, N_levels), (0, 0, 0))
+
+    launch!(arch, grid, kp_2D, _find_NN_active_region!, Ri, grid, Riᶜ, first_index, last_index, closure)
+
+    launch!(arch, grid, kp_wrk, 
+            _populate_input!, wrk_in, first_index, last_index, grid, closure, tracers, velocities, buoyancy, top_tracer_bcs, Ri, clock)
+
+    wrk_wT .= dropdims(closure.wT(wrk_in), dims=1)
+    wrk_wS .= dropdims(closure.wS(wrk_in), dims=1)
+
+    launch!(arch, grid, kp, _fill_adjust_nn_fluxes!, diffusivities, first_index, last_index, wrk_wT, wrk_wS, grid, closure, tracers, velocities, buoyancy, top_tracer_bcs, clock)
+    return nothing
+end
+
+@kernel function _populate_input!(input, first_index, last_index, grid, closure::NNFluxClosure, tracers, velocities, buoyancy, top_tracer_bcs, Ri, clock)
+    i, j, k = @index(Global, NTuple)
+
+    scaling = closure.scaling
+
+    ρ₀ = buoyancy.model.equation_of_state.reference_density
+    g  = buoyancy.model.gravitational_acceleration
+
+    @inbounds k_first = first_index[i, j, 1]
+    @inbounds k_last = last_index[i, j, 1]
+
+    quiescent = quiescent_condition(k_first, k_last)
+
+    @inbounds k_tracer = clamp_k_interior(k_first + k - 1, grid)
+
+    @inbounds k₋₂ = clamp_k_interior(k_tracer - 2, grid)
+    @inbounds k₋₁ = clamp_k_interior(k_tracer - 1, grid)
+    @inbounds k₀ = clamp_k_interior(k_tracer, grid)
+    @inbounds k₊₁ = clamp_k_interior(k_tracer + 1, grid)
+    @inbounds k₊₂ = clamp_k_interior(k_tracer + 2, grid)
+
+    T, S = tracers.T, tracers.S
+
+    @inbounds input[1, i, j, k] = ifelse(quiescent, 0, atan(Ri[i, j, k₋₂]))
+    @inbounds input[2, i, j, k] = ifelse(quiescent, 0, atan(Ri[i, j, k₋₁]))
+    @inbounds input[3, i, j, k] = ifelse(quiescent, 0, atan(Ri[i, j, k₀]))
+    @inbounds input[4, i, j, k] = ifelse(quiescent, 0, atan(Ri[i, j, k₊₁]))
+    @inbounds input[5, i, j, k] = ifelse(quiescent, 0, atan(Ri[i, j, k₊₂]))
+
+    @inbounds input[6, i, j, k] = scaling.∂T∂z(ifelse(quiescent, 0, ∂zᶜᶜᶠ(i, j, k₋₂, grid, T)))
+    @inbounds input[7, i, j, k] = scaling.∂T∂z(ifelse(quiescent, 0, ∂zᶜᶜᶠ(i, j, k₋₁, grid, T)))
+    @inbounds input[8, i, j, k] = scaling.∂T∂z(ifelse(quiescent, 0, ∂zᶜᶜᶠ(i, j, k₀, grid, T)))
+    @inbounds input[9, i, j, k] = scaling.∂T∂z(ifelse(quiescent, 0, ∂zᶜᶜᶠ(i, j, k₊₁, grid, T)))
+    @inbounds input[10, i, j, k] = scaling.∂T∂z(ifelse(quiescent, 0, ∂zᶜᶜᶠ(i, j, k₊₂, grid, T)))
+
+    @inbounds input[11, i, j, k] = scaling.∂S∂z(ifelse(quiescent, 0, ∂zᶜᶜᶠ(i, j, k₋₂, grid, S)))
+    @inbounds input[12, i, j, k] = scaling.∂S∂z(ifelse(quiescent, 0, ∂zᶜᶜᶠ(i, j, k₋₁, grid, S)))
+    @inbounds input[13, i, j, k] = scaling.∂S∂z(ifelse(quiescent, 0, ∂zᶜᶜᶠ(i, j, k₀, grid, S)))
+    @inbounds input[14, i, j, k] = scaling.∂S∂z(ifelse(quiescent, 0, ∂zᶜᶜᶠ(i, j, k₊₁, grid, S)))
+    @inbounds input[15, i, j, k] = scaling.∂S∂z(ifelse(quiescent, 0, ∂zᶜᶜᶠ(i, j, k₊₂, grid, S)))
+
+    @inbounds input[16, i, j, k] = scaling.∂ρ∂z(ifelse(quiescent, 0, -ρ₀ * ∂z_b(i, j, k₋₂, grid, buoyancy, tracers) / g))
+    @inbounds input[17, i, j, k] = scaling.∂ρ∂z(ifelse(quiescent, 0, -ρ₀ * ∂z_b(i, j, k₋₁, grid, buoyancy, tracers) / g))
+    @inbounds input[18, i, j, k] = scaling.∂ρ∂z(ifelse(quiescent, 0, -ρ₀ * ∂z_b(i, j, k₀, grid, buoyancy, tracers) / g))
+    @inbounds input[19, i, j, k] = scaling.∂ρ∂z(ifelse(quiescent, 0, -ρ₀ * ∂z_b(i, j, k₊₁, grid, buoyancy, tracers) / g))
+    @inbounds input[20, i, j, k] = scaling.∂ρ∂z(ifelse(quiescent, 0, -ρ₀ * ∂z_b(i, j, k₊₂, grid, buoyancy, tracers) / g))
+
+    @inbounds input[21, i, j, k] = scaling.wb(top_buoyancy_flux(i, j, grid, buoyancy, top_tracer_bcs, clock, merge(velocities, tracers)))
+
+end
+
+@kernel function _find_NN_active_region!(Ri, grid, Riᶜ, first_index, last_index, closure::NNFluxClosure)
+    i, j = @index(Global, NTuple)
+    top_index = grid.Nz + 1
+    grid_point_above_kappa = closure.grid_point_above
+    grid_point_below_kappa = closure.grid_point_below
+
+    # Find the first index of the background κᶜ
+    kloc = grid.Nz+1
+    @inbounds for k in grid.Nz:-1:2
+        kloc = ifelse(Ri[i, j, k] < Riᶜ, k, kloc)
+    end
+
+    background_κ_index = kloc - 1
+    nonbackground_κ_index = background_κ_index + 1
+
+    @inbounds last_index[i, j, 1] = ifelse(nonbackground_κ_index == top_index, top_index - 1, clamp_k_interior(background_κ_index + grid_point_above_kappa, grid))
+    @inbounds first_index[i, j, 1] = ifelse(nonbackground_κ_index == top_index, top_index, clamp_k_interior(background_κ_index - grid_point_below_kappa + 1, grid))
+end
+
+@inline function quiescent_condition(lo, hi)
+    return hi < lo
+end
+
+@inline function within_zone_condition(k, lo, hi)
+    return (k >= lo) & (k <= hi)
+end
+
+@inline function clamp_k_interior(k, grid)
+    kmax = grid.Nz
+    kmin = 2
+
+    return clamp(k, kmin, kmax)
+end
+
+@kernel function _fill_adjust_nn_fluxes!(diffusivities, first_index, last_index, wrk_wT, wrk_wS, grid, closure::NNFluxClosure, tracers, velocities, buoyancy, top_tracer_bcs, clock)
+    i, j, k = @index(Global, NTuple)
+    scaling = closure.scaling
+
+    @inbounds k_first = first_index[i, j, 1]
+    @inbounds k_last = last_index[i, j, 1]
+
+    convecting = top_buoyancy_flux(i, j, grid, buoyancy, top_tracer_bcs, clock, merge(velocities, tracers)) > 0
+    quiescent = quiescent_condition(k_first, k_last)
+    within_zone = within_zone_condition(k, k_first, k_last)
+
+    N_levels = closure.grid_point_above + closure.grid_point_below
+    @inbounds k_wrk = clamp(k - k_first + 1, 1, N_levels)
+
+    NN_active = convecting & !quiescent & within_zone
+
+    @inbounds diffusivities.wT[i, j, k] = ifelse(NN_active, scaling.wT.σ * wrk_wT[i, j, k_wrk], 0)
+    @inbounds diffusivities.wS[i, j, k] = ifelse(NN_active, scaling.wS.σ * wrk_wS[i, j, k_wrk], 0)
+end
+
+# Write here your constructor
+# NNFluxClosure() = ... insert NN here ... (make sure it is on GPU if you need it on GPU!)
+
+const NNC = NNFluxClosure
+                                                         
+#####
+##### Abstract Smagorinsky functionality
+#####
+
+# Horizontal fluxes are zero!
+@inline viscous_flux_wz( i, j, k, grid, clo::NNC, K, clk, fields, b) = zero(grid)
+@inline viscous_flux_wx( i, j, k, grid, clo::NNC, K, clk, fields, b) = zero(grid)
+@inline viscous_flux_wy( i, j, k, grid, clo::NNC, K, clk, fields, b) = zero(grid)
+@inline viscous_flux_ux( i, j, k, grid, clo::NNC, K, clk, fields, b) = zero(grid)
+@inline viscous_flux_vx( i, j, k, grid, clo::NNC, K, clk, fields, b) = zero(grid)
+@inline viscous_flux_uy( i, j, k, grid, clo::NNC, K, clk, fields, b) = zero(grid)
+@inline viscous_flux_vy( i, j, k, grid, clo::NNC, K, clk, fields, b) = zero(grid)
+@inline diffusive_flux_x(i, j, k, grid, clo::NNC, K, ::Val{tracer_index}, c, clock, fields, buoyancy) where tracer_index = zero(grid)
+@inline diffusive_flux_y(i, j, k, grid, clo::NNC, K, ::Val{tracer_index}, c, clock, fields, buoyancy) where tracer_index = zero(grid)
+
+# Viscous fluxes are zero (for now)
+@inline viscous_flux_uz(i, j, k, grid, clo::NNC, K, clk, fields, b) = zero(grid)
+@inline viscous_flux_vz(i, j, k, grid, clo::NNC, K, clk, fields, b) = zero(grid)
+
+# The only function extended by NNFluxClosure
+@inline diffusive_flux_z(i, j, k, grid, clo::NNC, K, ::Val{1}, c, clock, fields, buoyancy) = @inbounds K.wT[i, j, k]
+@inline diffusive_flux_z(i, j, k, grid, clo::NNC, K, ::Val{2}, c, clock, fields, buoyancy) = @inbounds K.wS[i, j, k]
---
+++ xin_kai_vertical_diffusivity_local_2step_train56newstrongSO.jl
@@ -0,0 +1,242 @@
+using Oceananigans
+using Oceananigans.Architectures: architecture
+using Oceananigans.BuoyancyModels: ∂z_b
+using Oceananigans.Operators
+using Oceananigans.Grids: inactive_node, total_size
+using Oceananigans.Operators: ℑzᵃᵃᶜ, ℑxyᶠᶠᵃ, ℑxyᶜᶜᵃ
+using Oceananigans.Utils: KernelParameters
+
+using Adapt
+
+using KernelAbstractions: @index, @kernel
+using KernelAbstractions.Extras.LoopInfo: @unroll
+
+using Oceananigans.TurbulenceClosures:
+        tapering_factorᶠᶜᶜ,
+        tapering_factorᶜᶠᶜ,
+        tapering_factorᶜᶜᶠ,
+        tapering_factor,
+        SmallSlopeIsopycnalTensor,
+        AbstractScalarDiffusivity,
+        ExplicitTimeDiscretization,
+        FluxTapering,
+        isopycnal_rotation_tensor_xz_ccf,
+        isopycnal_rotation_tensor_yz_ccf,
+        isopycnal_rotation_tensor_zz_ccf
+
+import Oceananigans.TurbulenceClosures:
+        compute_diffusivities!,
+        DiffusivityFields,
+        viscosity, 
+        diffusivity,
+        getclosure,
+        top_buoyancy_flux,
+        diffusive_flux_x,
+        diffusive_flux_y, 
+        diffusive_flux_z,
+        viscous_flux_ux,
+        viscous_flux_vx,
+        viscous_flux_uy,
+        viscous_flux_vy
+
+using Oceananigans.Utils: launch!
+using Oceananigans.Coriolis: fᶠᶠᵃ
+using Oceananigans.Operators
+using Oceananigans.BuoyancyModels: ∂x_b, ∂y_b, ∂z_b 
+
+using Oceananigans.TurbulenceClosures
+using Oceananigans.TurbulenceClosures: HorizontalFormulation, VerticalFormulation, AbstractScalarDiffusivity
+using Oceananigans.TurbulenceClosures: AbstractScalarBiharmonicDiffusivity
+using Oceananigans.Operators
+using Oceananigans.Operators: Δxᶜᶜᶜ, Δyᶜᶜᶜ, ℑxyᶜᶜᵃ, ζ₃ᶠᶠᶜ, div_xyᶜᶜᶜ
+using Oceananigans.Operators: Δx, Δy
+using Oceananigans.Operators: ℑxyz
+
+using Oceananigans.Operators: ℑxyzᶜᶜᶠ, ℑyzᵃᶜᶠ, ℑxzᶜᵃᶠ, Δxᶜᶜᶜ, Δyᶜᶜᶜ
+
+using Oceananigans.BoundaryConditions
+
+struct XinKaiLocalVerticalDiffusivity{TD, FT} <: AbstractScalarDiffusivity{TD, VerticalFormulation, 2}
+    ν₀  :: FT
+    νˢʰ :: FT
+    νᶜⁿ :: FT
+    Pr_convₜ :: FT
+    Pr_shearₜ :: FT
+    Riᶜ :: FT
+    δRi :: FT
+end
+
+function XinKaiLocalVerticalDiffusivity{TD}(ν₀  :: FT, 
+                                            νˢʰ :: FT,
+                                            νᶜⁿ :: FT,
+                                            Pr_convₜ :: FT,
+                                            Pr_shearₜ :: FT,
+                                            Riᶜ :: FT,
+				                            δRi :: FT) where {TD, FT}
+                                       
+    return XinKaiLocalVerticalDiffusivity{TD, FT}(ν₀, νˢʰ, νᶜⁿ, Pr_convₜ, Pr_shearₜ, Riᶜ, δRi)
+end
+
+function XinKaiLocalVerticalDiffusivity(time_discretization = VerticallyImplicitTimeDiscretization(),
+                                        FT  = Float64;
+                                        ν₀  = 1e-5, 
+                                        νˢʰ = 0.0615914063656973,
+                                        νᶜⁿ = 0.7612393837759673,
+                                        Pr_convₜ = 0.1749433627329692,
+                                        Pr_shearₜ = 1.0842017486284887,
+                                        Riᶜ = 0.4366901962987793,
+                                        δRi = 0.009695724988589002) 
+
+    TD = typeof(time_discretization)
+
+    return XinKaiLocalVerticalDiffusivity{TD}(convert(FT, ν₀),
+                                              convert(FT, νˢʰ),
+                                              convert(FT, νᶜⁿ),
+                                              convert(FT, Pr_convₜ),
+                                              convert(FT, Pr_shearₜ),
+                                              convert(FT, Riᶜ),
+                                              convert(FT, δRi))
+end
+
+XinKaiLocalVerticalDiffusivity(FT::DataType; kw...) =
+    XinKaiLocalVerticalDiffusivity(VerticallyImplicitTimeDiscretization(), FT; kw...)
+
+Adapt.adapt_structure(to, clo::XinKaiLocalVerticalDiffusivity{TD, FT}) where {TD, FT} = 
+    XinKaiLocalVerticalDiffusivity{TD, FT}(clo.ν₀, clo.νˢʰ, clo.νᶜⁿ, clo.Pr_convₜ, clo.Pr_shearₜ, clo.Riᶜ, clo.δRi)
+                                         
+#####                                    
+##### Diffusivity field utilities        
+#####                                    
+                                         
+const RBVD = XinKaiLocalVerticalDiffusivity   
+const RBVDArray = AbstractArray{<:RBVD}
+const FlavorOfXKVD = Union{RBVD, RBVDArray}
+const c = Center()
+const f = Face()
+
+@inline viscosity_location(::FlavorOfXKVD)   = (c, c, f)
+@inline diffusivity_location(::FlavorOfXKVD) = (c, c, f)
+
+@inline viscosity(::FlavorOfXKVD, diffusivities) = diffusivities.κᵘ
+@inline diffusivity(::FlavorOfXKVD, diffusivities, id) = diffusivities.κᶜ
+
+with_tracers(tracers, closure::FlavorOfXKVD) = closure
+
+# Note: computing diffusivities at cell centers for now.
+function DiffusivityFields(grid, tracer_names, bcs, closure::FlavorOfXKVD)
+    κᶜ = Field((Center, Center, Face), grid)
+    κᵘ = Field((Center, Center, Face), grid)
+    Ri = Field((Center, Center, Face), grid)
+    return (; κᶜ, κᵘ, Ri)
+end
+
+function compute_diffusivities!(diffusivities, closure::FlavorOfXKVD, model; parameters = :xyz)
+    arch = model.architecture
+    grid = model.grid
+    clock = model.clock
+    tracers = model.tracers
+    buoyancy = model.buoyancy
+    velocities = model.velocities
+    top_tracer_bcs = NamedTuple(c => tracers[c].boundary_conditions.top for c in propertynames(tracers))
+
+    Nx_in, Ny_in, Nz_in = total_size(diffusivities.κᶜ)
+    ox_in, oy_in, oz_in = diffusivities.κᶜ.data.offsets
+
+    kp = KernelParameters((Nx_in, Ny_in, Nz_in), (ox_in, oy_in, oz_in))
+
+    launch!(arch, grid, kp,
+            compute_ri_number!,
+            diffusivities,
+            grid,
+            closure,
+            velocities,
+            tracers,
+            buoyancy,
+            top_tracer_bcs,
+            clock)
+
+    # Use `only_local_halos` to ensure that no communication occurs during
+    # this call to fill_halo_regions!
+    fill_halo_regions!(diffusivities.Ri; only_local_halos=true)
+
+    launch!(arch, grid, kp,
+            compute_xinkai_diffusivities!,
+            diffusivities,
+            grid,
+            closure,
+            velocities,
+            tracers,
+            buoyancy,
+            top_tracer_bcs,
+            clock)
+
+    return nothing
+end
+
+@inline ϕ²(i, j, k, grid, ϕ, args...) = ϕ(i, j, k, grid, args...)^2
+
+@inline function shear_squaredᶜᶜᶠ(i, j, k, grid, velocities)
+    ∂z_u² = ℑxᶜᵃᵃ(i, j, k, grid, ϕ², ∂zᶠᶜᶠ, velocities.u)
+    ∂z_v² = ℑyᵃᶜᵃ(i, j, k, grid, ϕ², ∂zᶜᶠᶠ, velocities.v)
+    return ∂z_u² + ∂z_v²
+end
+
+@inline function Riᶜᶜᶠ(i, j, k, grid, velocities, buoyancy, tracers)
+    S² = shear_squaredᶜᶜᶠ(i, j, k, grid, velocities)
+    N² = ∂z_b(i, j, k, grid, buoyancy, tracers)
+    Ri = N² / S²
+
+    # Clip N² and avoid NaN
+    return ifelse(N² == 0, zero(grid), Ri)
+end
+
+const c = Center()
+const f = Face()
+
+@kernel function compute_ri_number!(diffusivities, grid, closure::FlavorOfXKVD,
+                                    velocities, tracers, buoyancy, tracer_bcs, clock)
+    i, j, k = @index(Global, NTuple)
+    @inbounds diffusivities.Ri[i, j, k] = Riᶜᶜᶠ(i, j, k, grid, velocities, buoyancy, tracers)
+end
+
+@kernel function compute_xinkai_diffusivities!(diffusivities, grid, closure::FlavorOfXKVD,
+                                                velocities, tracers, buoyancy, tracer_bcs, clock)
+    i, j, k = @index(Global, NTuple)
+    _compute_xinkai_diffusivities!(i, j, k, diffusivities, grid, closure,
+                                   velocities, tracers, buoyancy, tracer_bcs, clock)
+end
+
+@inline function _compute_xinkai_diffusivities!(i, j, k, diffusivities, grid, closure,
+                                                velocities, tracers, buoyancy, tracer_bcs, clock)
+
+    # Ensure this works with "ensembles" of closures, in addition to ordinary single closures
+    closure_ij = getclosure(i, j, closure)
+
+    ν₀  = closure_ij.ν₀  
+    νˢʰ = closure_ij.νˢʰ
+    νᶜⁿ = closure_ij.νᶜⁿ
+    Pr_convₜ = closure_ij.Pr_convₜ
+    Pr_shearₜ = closure_ij.Pr_shearₜ
+    Riᶜ = closure_ij.Riᶜ
+    δRi = closure_ij.δRi
+
+    κ₀ = ν₀ / Pr_shearₜ
+    κˢʰ = νˢʰ / Pr_shearₜ
+    κᶜⁿ = νᶜⁿ / Pr_convₜ
+
+    # (Potentially) apply a horizontal filter to the Richardson number
+    Ri = ℑxyᶜᶜᵃ(i, j, k, grid, ℑxyᶠᶠᵃ, diffusivities.Ri)
+
+    # Conditions
+    convecting = Ri < 0 # applies regardless of Qᵇ
+
+    # Convective adjustment diffusivity
+    ν_local = ifelse(convecting, (νˢʰ - νᶜⁿ) * tanh(Ri / δRi) + νˢʰ, clamp((ν₀ - νˢʰ) * Ri / Riᶜ + νˢʰ, ν₀, νˢʰ))
+    κ_local = ifelse(convecting, (κˢʰ - κᶜⁿ) * tanh(Ri / δRi) + κˢʰ, clamp((κ₀ - κˢʰ) * Ri / Riᶜ + κˢʰ, κ₀, κˢʰ))
+
+    # Update by averaging in time
+    @inbounds diffusivities.κᵘ[i, j, k] = ifelse(k <= 1 || k >= grid.Nz+1, 0, ν_local)
+    @inbounds diffusivities.κᶜ[i, j, k] = ifelse(k <= 1 || k >= grid.Nz+1, 0, κ_local)
+
+    return nothing
+end
