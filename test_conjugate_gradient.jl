a251653461c2ca6914e4e4bc4d453439384b011b
Chih-Lun (Aaron) Liu <chihlunl@gmail.com>
2025-01-02 17:29:40 -0800

    Remove test_conjugate_gradient.jl

--- test_conjugate_gradient.jl
+++
@@ -1,8 +0,0 @@
-using Oceananigans
-using Oceananigans.Solvers: ConjugateGradientPoissonSolver, fft_poisson_solver
-
-N = 2
-x = y = (0, 1)
-z = [0, 0.2, 1]
-grid = RectilinearGrid(size=(N, N, N); x, y, z, halo=(2, 2, 2), topology=(Bounded, Periodic, Bounded))
-fft_solver = fft_poisson_solver(grid)
