9599bd428c8957d7d1dd18ccefb8a6450a60354d
Simone Silvestri <silvestri.simone0@gmail.com>
2025-01-12 14:55:34 -0500

    improve mask immersed field

--- src/ImmersedBoundaries/mask_immersed_field.jl
+++ src/ImmersedBoundaries/mask_immersed_field.jl
@@ -18,8 +18,8 @@ mask_immersed_field_xy!(::ZeroField, args...; kw...) = nothing
 mask_immersed_field_xy!(::ConstantField, args...; kw...) = nothing
 
 mask_immersed_field!(field, grid, loc, value) = nothing
-mask_immersed_field!(field::Field, value=zero(eltype(field.grid))) =
-    mask_immersed_field!(field, field.grid, location(field), value)
+mask_immersed_field!(field::Field, value=zero(eltype(field.grid)); mask=peripheral_node) =
+    mask_immersed_field!(field, field.grid, location(field), value, mask)
 
 mask_immersed_field!(::Number, args...) = nothing
 
@@ -46,22 +46,23 @@ end
 
 masks `field` defined on `grid` with a value `val` at locations where `peripheral_node` evaluates to `true`
 """
-function mask_immersed_field!(field::Field, grid::ImmersedBoundaryGrid, loc, value)
+function mask_immersed_field!(field::Field, grid::ImmersedBoundaryGrid, loc, value, mask)
     arch = architecture(field)
-    loc = instantiate.(loc)
-    launch!(arch, grid, :xyz, _mask_immersed_field!, field, loc, grid, value)
+    loc  = instantiate.(loc)
+    launch!(arch, grid, :xyz, _mask_immersed_field!, field, loc, grid, value, mask)
     return nothing
 end
 
-@kernel function _mask_immersed_field!(field, loc, grid, value)
+@kernel function _mask_immersed_field!(field, (LX, LY, LZ), grid, value, mask)
     i, j, k = @index(Global, NTuple)
-    @inbounds field[i, j, k] = scalar_mask(i, j, k, grid, grid.immersed_boundary, loc..., value, field)
+    masked  = mask(i, j, k, grid, LX, LY, LZ)
+    @inbounds field[i, j, k] = ifelse(masked, value, field[i, j, k])
 end
 
 mask_immersed_field_xy!(field,     args...; kw...) = nothing
 mask_immersed_field_xy!(::Nothing, args...; kw...) = nothing
-mask_immersed_field_xy!(field, value=zero(eltype(field.grid)); k, mask = peripheral_node) =
-    mask_immersed_field_xy!(field, field.grid, location(field), value; k, mask)
+mask_immersed_field_xy!(field, value=zero(eltype(field.grid)); k, mask=peripheral_node) =
+    mask_immersed_field_xy!(field, field.grid, location(field), value, k, mask)
 
 mask_immersed_field_xy!(::Number, args...) = nothing
 
@@ -88,16 +89,16 @@ end
 
 Mask `field` on `grid` with a `value` on the slices `[:, :, k]` where `mask` is `true`.
 """
-function mask_immersed_field_xy!(field::Field, grid::ImmersedBoundaryGrid, loc, value; k, mask)
+function mask_immersed_field_xy!(field::Field, grid::ImmersedBoundaryGrid, loc, value, k, mask)
     arch = architecture(field)
     loc = instantiate.(loc)
-    return launch!(arch, grid, :xy,
-                   _mask_immersed_field_xy!, field, loc, grid, value, k, mask)
+    return launch!(arch, grid, :xy, _mask_immersed_field_xy!, field, loc, grid, value, k, mask)
 end
 
-@kernel function _mask_immersed_field_xy!(field, loc, grid, value, k, mask)
+@kernel function _mask_immersed_field_xy!(field, (LX, LY, LZ), grid, value, k, mask)
     i, j = @index(Global, NTuple)
-    @inbounds field[i, j, k] = scalar_mask(i, j, k, grid, grid.immersed_boundary, loc..., value, field, mask)
+    masked = mask(i, j, k, grid, LX, LY, LZ)
+    @inbounds field[i, j, k] = ifelse(masked, value, field[i, j, k])
 end
 
 #####
@@ -106,34 +107,33 @@ end
 
 # We mask a `ReducedField` if the entire reduced direction is immersed.
 # This requires a sweep over the reduced direction
-
-function mask_immersed_field!(field::ReducedField, grid::ImmersedBoundaryGrid, loc, value)
+function mask_immersed_field!(field::ReducedField, grid::ImmersedBoundaryGrid, loc, value, mask)
     loc  = instantiate.(loc)
     dims = reduced_dimensions(field)
-    launch!(architecture(field), grid, size(field), _mask_immersed_reduced_field!, field, dims, loc, grid, value)
+    launch!(architecture(field), grid, size(field), _mask_immersed_reduced_field!, field, dims, loc, grid, value, mask)
     return nothing
 end
 
-@kernel function _mask_immersed_reduced_field!(field, dims, loc, grid, value)
+@kernel function _mask_immersed_reduced_field!(field, dims, loc, grid, value, mask)
     i, j, k = @index(Global, NTuple)
-    mask = inactive_dimensions(i, j, k, grid, dims, loc)
-    @inbounds field[i, j, k] = ifelse(mask, value, field[i, j, k]) 
+    masked  = inactive_dimensions(i, j, k, grid, dims, loc, mask)
+    @inbounds field[i, j, k] = ifelse(masked, value, field[i, j, k]) 
 end
 
 @inline inactive_search_range(i, grid, dim, dims) = ifelse(dim ∈ dims, 1:size(grid, dim), i:i)
 
-@inline function inactive_dimensions(i₀, j₀, k₀, grid, dims, loc)
-    mask = true
+@inline function inactive_dimensions(i₀, j₀, k₀, grid, dims, loc, mask)
+    masked = true
     irange = inactive_search_range(i₀, grid, 1, dims)
     jrange = inactive_search_range(j₀, grid, 2, dims)
     krange = inactive_search_range(k₀, grid, 3, dims)
     
     # The loop activates over the whole direction only if reduced directions
     for i in irange, j in jrange, k in krange
-        mask = mask & peripheral_node(i, j, k, grid, loc...) 
+        masked = masked & mask(i, j, k, grid, loc...) 
     end
 
-    return mask
+    return masked
 end
 
 ###
@@ -146,13 +146,5 @@ const CenterOrFace = Union{Center, Face}
 const OnlyZReducedField = Field{<:CenterOrFace, <:CenterOrFace, Nothing}
 
 # Does not require a sweep
-mask_immersed_field!(field::OnlyZReducedField, grid::AGFBIBG, loc, value) = 
-    mask_immersed_field_xy!(field, grid, loc, value; k=size(grid, 3), mask=peripheral_node)
-    
-#####
-##### Masking for GridFittedBoundary
-#####
-
-@inline scalar_mask(i, j, k, grid, ::AbstractGridFittedBoundary, LX, LY, LZ, value, field, mask=peripheral_node) =
-    @inbounds ifelse(mask(i, j, k, grid, LX, LY, LZ), value, field[i, j, k])
-
+mask_immersed_field!(field::OnlyZReducedField, grid::AGFBIBG, loc, value, mask) = 
+    mask_immersed_field_xy!(field, grid, loc, value, size(grid, 3), mask)
