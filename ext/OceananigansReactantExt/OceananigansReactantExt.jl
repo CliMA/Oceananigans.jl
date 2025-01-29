25a72e271d21b729e8727b3d4434771985c0e08a
William S. Moses <gh@wsmoses.com>
2025-01-29 13:14:30 +0100

    fix

--- ext/OceananigansReactantExt/OceananigansReactantExt.jl
+++ ext/OceananigansReactantExt/OceananigansReactantExt.jl
@@ -3,17 +3,17 @@ module OceananigansReactantExt
 using Reactant
 
 include("Architectures.jl")
-include("Utils.jl")
-include("BoundaryConditions.jl")
-include("Fields.jl")
-include("MultiRegion.jl")
-include("Solvers.jl")
+# include("Utils.jl")
+# include("BoundaryConditions.jl")
+# include("Fields.jl")
+# include("MultiRegion.jl")
+# include("Solvers.jl")
 
 using .Architectures
-using .Utils
-using .BoundaryConditions
-using .Fields
-using .MultiRegion
-using .Solvers
+# using .Utils
+# using .BoundaryConditions
+# using .Fields
+# using .MultiRegion
+# using .Solvers
 
 end # module
