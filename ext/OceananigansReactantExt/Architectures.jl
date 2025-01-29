bb121c8b429b646444a68f552160f8d9db3f8033
William Moses <gh@wsmoses.com>
2025-01-30 00:08:21 +0100

    Update Architectures.jl

--- ext/OceananigansReactantExt/Architectures.jl
+++ ext/OceananigansReactantExt/Architectures.jl
@@ -11,13 +11,13 @@ architecture(::Reactant.AnyTracedRArray) = ReactantState
 array_type(::ReactantState) = ConcreteRArray
 
 on_architecture(::ReactantState, a::Array) = ConcreteRArray(a)
-on_architecture(::ReactantState, a::ConcreteRArray) = a
-on_architecture(::ReactantState, a::TracedRArray) = a
+on_architecture(::ReactantState, a::Reactant.AnyConcreteRArray) = a
+on_architecture(::ReactantState, a::Reactant.AnyTracedRArray) = a
 on_architecture(::ReactantState, a::BitArray) = ConcreteRArray(a)
 on_architecture(::ReactantState, a::SubArray{<:Any, <:Any, <:Array}) = ConcreteRArray(a)
 
 unified_array(::ReactantState, a) = a
 
-@inline device_copy_to!(dst::AnyConcreteRArray, src::AnyConcreteRArray; kw...) = Base.copyto!(dst, src)
+@inline device_copy_to!(dst::Reactant.AnyConcreteRArray, src::Reactant.AnyConcreteRArray; kw...) = Base.copyto!(dst, src)
 
 end # module
