18dcf5b84a54fdd57eb88cea6de2198a102517bd
Gregory Wagner <wagner.greg@gmail.com>
2025-02-01 08:51:49 -0700

    Use zeros(arch, FT, ...) to mirror KA form zeros(device, FT, ...)

--- src/Grids/zeros_and_ones.jl
+++ src/Grids/zeros_and_ones.jl
@@ -3,7 +3,7 @@ using Oceananigans.Architectures: device, AbstractArchitecture
 import KernelAbstractions
 import Base: zeros
 
-zeros(FT, arch::AbstractArchitecture, N...) = KernelAbstractions.zeros(device(arch), FT, N...)
+zeros(arch::AbstractArchitecture, FT, N...) = KernelAbstractions.zeros(device(arch), FT, N...)
 zeros(grid::AbstractGrid, N...) = zeros(eltype(grid), architecture(grid), N...)
 
 @inline Base.zero(grid::AbstractGrid) = zero(eltype(grid))
