050ebe56b6761f9d454b38ad87d3f7440878b562
Gregory Wagner <wagner.greg@gmail.com>
2025-02-01 09:42:42 -0700

    Fix grid-based zeros

--- src/Grids/zeros_and_ones.jl
+++ src/Grids/zeros_and_ones.jl
@@ -4,7 +4,7 @@ import KernelAbstractions
 import Base: zeros
 
 zeros(arch::AbstractArchitecture, FT, N...) = KernelAbstractions.zeros(device(arch), FT, N...)
-zeros(grid::AbstractGrid, N...) = zeros(eltype(grid), architecture(grid), N...)
+zeros(grid::AbstractGrid, N...) = zeros(architecture(grid), eltype(grid), N...)
 
 @inline Base.zero(grid::AbstractGrid) = zero(eltype(grid))
 @inline Base.one(grid::AbstractGrid)  = one(eltype(grid))
