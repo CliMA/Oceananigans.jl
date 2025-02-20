c9557ef5414d49e7a884cd868bc4edfbdfb6f3f4
Simone Silvestri <silvestri.simone0@gmail.com>
2025-02-20 21:26:00 +0100

    disambiguate

--- src/Advection/vector_invariant_advection.jl
+++ src/Advection/vector_invariant_advection.jl
@@ -301,14 +301,18 @@ for bias in (:_biased, :_symmetric)
     end
 end
 
+_advective_momentum_flux_Wu(i, j, k, grid::ImmersedBoundaryGrid, scheme::VectorInvariantVerticalEnergyConserving, W, u) = zero(grid)
+_advective_momentum_flux_Wv(i, j, k, grid::ImmersedBoundaryGrid, scheme::VectorInvariantVerticalEnergyConserving, W, v) = zero(grid)
+
 _advective_momentum_flux_Wu(i, j, k, grid, scheme::VectorInvariantVerticalEnergyConserving, W, u) = zero(grid)
 _advective_momentum_flux_Wv(i, j, k, grid, scheme::VectorInvariantVerticalEnergyConserving, W, v) = zero(grid)
 
-_advective_momentum_flux_Wu(i, j, k, grid, scheme::VectorInvariant, W, u) = 
-    _advective_momentum_flux_Wu(i, j, k, grid, scheme.vertical_scheme, W, u) 
+_advective_momentum_flux_Wu(i, j, k, grid::ImmersedBoundaryGrid, scheme::VectorInvariant, W, u) = _advective_momentum_flux_Wu(i, j, k, grid, scheme.vertical_scheme, W, u) 
+_advective_momentum_flux_Wu(i, j, k, grid::ImmersedBoundaryGrid, scheme::VectorInvariant, W, u) = _advective_momentum_flux_Wu(i, j, k, grid, scheme.vertical_scheme, W, u) 
 
-_advective_momentum_flux_Wv(i, j, k, grid, scheme::VectorInvariant, W, v) = 
-    _advective_momentum_flux_Wv(i, j, k, grid, scheme.vertical_scheme, W, v) 
+
+_advective_momentum_flux_Wu(i, j, k, grid, scheme::VectorInvariant, W, u) = _advective_momentum_flux_Wu(i, j, k, grid, scheme.vertical_scheme, W, u) 
+_advective_momentum_flux_Wv(i, j, k, grid, scheme::VectorInvariant, W, v) = _advective_momentum_flux_Wv(i, j, k, grid, scheme.vertical_scheme, W, v) 
 
 #####
 #####  Vertical advection + Kinetic Energy gradient. 3 Formulations:
