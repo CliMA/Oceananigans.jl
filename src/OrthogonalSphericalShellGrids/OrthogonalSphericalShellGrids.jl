2b99db8f17046d2c901b6552995f1209be970c48
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-02-20 22:38:28 -0700

    Change name to rotated lat-lon grid

--- src/OrthogonalSphericalShellGrids/OrthogonalSphericalShellGrids.jl
+++ src/OrthogonalSphericalShellGrids/OrthogonalSphericalShellGrids.jl
@@ -6,6 +6,6 @@ using Oceananigans.Grids: OrthogonalSphericalShellGrid, R_Earth, halo_size
 using Oceananigans.Utils: launch!
 using KernelAbstractions: @kernel, @index
 
-include("displaced_latitude_longitude_grid.jl")
+include("rotated_latitude_longitude_grid.jl")
 
 end # module
