3dfb50cf5dcc89736d3c6468d8c7e57a79c3256f
Avik Pal <avik.pal.2017@gmail.com>
2025-03-01 16:08:21 -0500

    New reactant feature: sharding + avoid direct usage of `RArray` (#4150)
    * feat: enable support for sharding
    
    * fix: update to newer Reactant releases
    
    * fix: default to nothing
    
    * chore: bump min reactant version

--- ext/OceananigansReactantExt/Architectures.jl
+++ ext/OceananigansReactantExt/Architectures.jl
@@ -11,19 +11,31 @@ const ReactantKernelAbstractionsExt = Base.get_extension(
 const ReactantBackend = ReactantKernelAbstractionsExt.ReactantBackend
 device(::ReactantState) = ReactantBackend()
 
-architecture(::Reactant.AnyConcreteRArray) = ReactantState
+architecture(::Reactant.AnyConcretePJRTArray) = ReactantState
 architecture(::Reactant.AnyTracedRArray) = ReactantState
 
-array_type(::ReactantState) = ConcreteRArray
+array_type(::ReactantState) = ConcretePJRTArray
 
-on_architecture(::ReactantState, a::Array) = ConcreteRArray(a)
-on_architecture(::ReactantState, a::Reactant.AnyConcreteRArray) = a
+to_reactant_sharding(::Nothing) = Sharding.NoSharding()
+to_reactant_sharding(s::Sharding.AbstractSharding) = s
+to_reactant_sharding(::T) where {T} = error("Unsupported sharding type $T")
+
 on_architecture(::ReactantState, a::Reactant.AnyTracedRArray) = a
-on_architecture(::ReactantState, a::BitArray) = ConcreteRArray(a)
-on_architecture(::ReactantState, a::SubArray{<:Any, <:Any, <:Array}) = ConcreteRArray(a)
+function on_architecture(r::ReactantState, a::Array)
+    return Reactant.to_rarray(a; sharding=to_reactant_sharding(r.sharding))
+end
+function on_architecture(r::ReactantState, a::Reactant.AnyConcretePJRTArray)
+    return Reactant.to_rarray(a; sharding=to_reactant_sharding(r.sharding))
+end
+function on_architecture(r::ReactantState, a::BitArray)
+    return Reactant.to_rarray(a; sharding=to_reactant_sharding(r.sharding))
+end
+function on_architecture(r::ReactantState, a::SubArray{<:Any,<:Any,<:Array})
+    return Reactant.to_rarray(a; sharding=to_reactant_sharding(r.sharding))
+end
 
 unified_array(::ReactantState, a) = a
 
-@inline device_copy_to!(dst::Reactant.AnyConcreteRArray, src::Reactant.AnyConcreteRArray; kw...) = Base.copyto!(dst, src)
+@inline device_copy_to!(dst::Reactant.AnyConcretePJRTArray, src::Reactant.AnyConcretePJRTArray; kw...) = Base.copyto!(dst, src)
 
 end # module
