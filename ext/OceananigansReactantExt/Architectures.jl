70eea0000373627be90ada2b2c1db327e664cf72
Avik Pal <avikpal@mit.edu>
2025-02-28 21:40:30 -0600

    fix: update to newer Reactant releases

--- ext/OceananigansReactantExt/Architectures.jl
+++ ext/OceananigansReactantExt/Architectures.jl
@@ -11,10 +11,10 @@ const ReactantKernelAbstractionsExt = Base.get_extension(
 const ReactantBackend = ReactantKernelAbstractionsExt.ReactantBackend
 device(::ReactantState) = ReactantBackend()
 
-architecture(::Reactant.AnyConcreteRArray) = ReactantState
+architecture(::Reactant.AnyConcretePJRTArray) = ReactantState
 architecture(::Reactant.AnyTracedRArray) = ReactantState
 
-array_type(::ReactantState) = ConcreteRArray
+array_type(::ReactantState) = ConcretePJRTArray
 
 to_reactant_sharding(::Missing) = Sharding.NoSharding()
 to_reactant_sharding(s::Sharding.AbstractSharding) = s
@@ -22,20 +22,20 @@ to_reactant_sharding(::T) where {T} = error("Unsupported sharding type $T")
 
 on_architecture(::ReactantState, a::Reactant.AnyTracedRArray) = a
 function on_architecture(r::ReactantState, a::Array)
-    return ConcreteRArray(a; sharding=to_reactant_sharding(r.sharding))
+    return Reactant.to_rarray(a; sharding=to_reactant_sharding(r.sharding))
 end
-function on_architecture(r::ReactantState, a::Reactant.AnyConcreteRArray)
-    return ConcreteRArray(a; sharding=to_reactant_sharding(r.sharding))
+function on_architecture(r::ReactantState, a::Reactant.AnyConcretePJRTArray)
+    return Reactant.to_rarray(a; sharding=to_reactant_sharding(r.sharding))
 end
 function on_architecture(r::ReactantState, a::BitArray)
-    return ConcreteRArray(a; sharding=to_reactant_sharding(r.sharding))
+    return Reactant.to_rarray(a; sharding=to_reactant_sharding(r.sharding))
 end
 function on_architecture(r::ReactantState, a::SubArray{<:Any,<:Any,<:Array})
-    return ConcreteRArray(a; sharding=to_reactant_sharding(r.sharding))
+    return Reactant.to_rarray(a; sharding=to_reactant_sharding(r.sharding))
 end
 
 unified_array(::ReactantState, a) = a
 
-@inline device_copy_to!(dst::Reactant.AnyConcreteRArray, src::Reactant.AnyConcreteRArray; kw...) = Base.copyto!(dst, src)
+@inline device_copy_to!(dst::Reactant.AnyConcretePJRTArray, src::Reactant.AnyConcretePJRTArray; kw...) = Base.copyto!(dst, src)
 
 end # module
