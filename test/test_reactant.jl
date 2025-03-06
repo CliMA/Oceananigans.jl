945cfc5b3e5f82da9ae349e7e3f02dc0a6281b3d
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-03-06 15:35:03 -0700

    Add another test with SplitExplicit

--- test/test_reactant.jl
+++ test/test_reactant.jl
@@ -94,7 +94,7 @@ function test_reactant_model_correctness(GridType, ModelType, grid_kw, model_kw;
     @test parent(v) ≈ parent(rv)
     @test parent(w) ≈ parent(rw)
 
-    return nothing
+    return r_simulation
 end
 
 function add_one!(f)
@@ -199,15 +199,18 @@ end
 end
 
 @testset "Reactant Super Simple Simulation Tests" begin
+    @info "Performing Reactanigans super simple simulation tests..."
     nonhydrostatic_model_kw = (; advection=WENO())
     hydrostatic_model_kw = (; momentum_advection=WENO())
     Nx, Ny, Nz = (10, 10, 10) # number of cells
     halo = (7, 7, 7)
     longitude = (0, 4)
+    stretched_longitude = [0, 0.1, 0.2, 0.3, 0.4, 0.6, 1.3, 2.5, 2.6, 3.5, 4.0]
     latitude = (0, 4)
     z = (-1, 0)
     lat_lon_kw = (; size=(Nx, Ny, Nz), halo, longitude, latitude, z)
     rectilinear_kw = (; size=(Nx, Ny, Nz), halo, x=(0, 1), y=(0, 1), z=(0, 1))
+    stretched_lat_lon_kw = (; size=(Nx, Ny, Nz), halo, longitude=stretched_longitude, latitude, z)
 
     # FFTs are not supported by Reactant so we don't run this test:
     # @info "Testing RectilinearGrid + NonhydrostaticModel Reactant correctness"
@@ -222,6 +225,15 @@ end
     hydrostatic_model_kw = (; momentum_advection=WENO())
     test_reactant_model_correctness(LatitudeLongitudeGrid, HydrostaticFreeSurfaceModel, lat_lon_kw, hydrostatic_model_kw)
     test_reactant_model_correctness(LatitudeLongitudeGrid, HydrostaticFreeSurfaceModel, lat_lon_kw, hydrostatic_model_kw, immersed_boundary_grid=true)
+
+    @info "Testing LatitudeLongitudeGrid + SplitExplicitFreeSurface + HydrostaticFreeSurfaceModel Reactant correctness"
+    hydrostatic_model_kw = (; momentum_advection=WENOVectorInvariant(), free_surface=SplitExplicitFreeSurface(substeps=4))
+    test_reactant_model_correctness(LatitudeLongitudeGrid, HydrostaticFreeSurfaceModel, lat_lon_kw, hydrostatic_model_kw)
+    simulation = test_reactant_model_correctness(LatitudeLongitudeGrid, HydrostaticFreeSurfaceModel, lat_lon_kw, hydrostatic_model_kw, immersed_boundary_grid=true)
+    η = simulation.model.free_surface.η
+    η_grid = η.grid
+    @test isnothing(η_grid.interior_active_cells)
+    @test isnothing(η_grid.active_z_columns)
 
     #=
     equation_of_state = TEOS10EquationOfState()
