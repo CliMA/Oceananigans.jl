90c9583ff436b1c28f243d99753ea2a83a7868fb
Simone Silvestri <silvestri.simone0@gmail.com>
2025-03-10 17:22:27 +0100

    should do the correct thing?

--- ext/OceananigansReactantExt/Simulations/run.jl
+++ ext/OceananigansReactantExt/Simulations/run.jl
@@ -1,7 +1,7 @@
 import ..TimeSteppers: first_time_step!
 
 """ Step `sim`ulation forward by one time step. """
-time_step!(sim::ReactantSimulation) = time_step!(sim.model, Δt; euler)
+time_step!(sim::ReactantSimulation) = time_step!(sim.model, Δt)
 run!(sim::ReactantSimulation) = error("run! is not supported with ReactantState architecture.")
 
 function first_time_step!(sim::ReactantSimulation)
