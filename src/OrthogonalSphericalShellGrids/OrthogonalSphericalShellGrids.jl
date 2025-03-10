068ff46a9a1197241244d1871f6860077bb1872d
Simone Silvestri <silvestri.simone0@gmail.com>
2025-03-10 15:37:46 +0100

    import launch!

--- src/OrthogonalSphericalShellGrids/OrthogonalSphericalShellGrids.jl
+++ src/OrthogonalSphericalShellGrids/OrthogonalSphericalShellGrids.jl
@@ -10,6 +10,7 @@ using Oceananigans.Grids
 using Oceananigans.Architectures: device, on_architecture, AbstractArchitecture
 using Oceananigans.BoundaryConditions
 using Oceananigans.ImmersedBoundaries
+using Oceananigans.Utils
 using Oceananigans.Fields: index_binary_search, convert_to_0_360
 using Oceananigans.Grids: RightConnected
 using Oceananigans.Grids: R_Earth, 
