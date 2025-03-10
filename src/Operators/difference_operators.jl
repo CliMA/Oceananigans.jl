de6964587f30a5f4389f5375fa7e091b6cb96f12
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-03-09 23:50:20 -0600

    Export more operators

--- src/Operators/difference_operators.jl
+++ src/Operators/difference_operators.jl
@@ -57,17 +57,55 @@ for ℓx in (:ᶜ, :ᶠ), ℓy in (:ᶜ, :ᶠ), ℓz in (:ᶜ, :ᶠ)
     δy = Symbol(:δy, ℓx, ℓy, ℓz)
     δz = Symbol(:δz, ℓx, ℓy, ℓz)
 
-    δxᵃ = Symbol(:δx, ℓx, :ᵃ, :ᵃ)
-    δyᵃ = Symbol(:δy, :ᵃ, ℓy, :ᵃ)
-    δzᵃ = Symbol(:δz, :ᵃ, :ᵃ, ℓz)
+    δxᵃᵃ = Symbol(:δx, ℓx, :ᵃ, :ᵃ)
+    δyᵃᵃ = Symbol(:δy, :ᵃ, ℓy, :ᵃ)
+    δzᵃᵃ = Symbol(:δz, :ᵃ, :ᵃ, ℓz)
+
+    δx_xy = Symbol(:δx, ℓx, ℓy, :ᵃ)
+    δx_xz = Symbol(:δx, ℓx, :ᵃ, ℓz)
+    δx_yz = Symbol(:δx, :ᵃ, ℓy, ℓz)
+
+    δy_xy = Symbol(:δy, ℓx, ℓy, :ᵃ)
+    δy_xz = Symbol(:δy, ℓx, :ᵃ, ℓz)
+    δy_yz = Symbol(:δy, :ᵃ, ℓy, ℓz)
+
+    δz_xy = Symbol(:δz, ℓx, ℓy, :ᵃ)
+    δz_xz = Symbol(:δz, ℓx, :ᵃ, ℓz)
+    δz_yz = Symbol(:δz, :ᵃ, ℓy, ℓz)
+
 
     @eval begin
-        @inline $δx(i, j, k, grid, f::Function, args...) = $δxᵃ(i, j, k, grid, f, args...)
-        @inline $δy(i, j, k, grid, f::Function, args...) = $δyᵃ(i, j, k, grid, f, args...)
-        @inline $δz(i, j, k, grid, f::Function, args...) = $δzᵃ(i, j, k, grid, f, args...)
+        @inline $δx(i, j, k, grid, f::Function, args...) = $δxᵃᵃ(i, j, k, grid, f, args...)
+        @inline $δy(i, j, k, grid, f::Function, args...) = $δyᵃᵃ(i, j, k, grid, f, args...)
+        @inline $δz(i, j, k, grid, f::Function, args...) = $δzᵃᵃ(i, j, k, grid, f, args...)
 
-        @inline $δx(i, j, k, grid, c) = $δxᵃ(i, j, k, grid, c)
-        @inline $δy(i, j, k, grid, c) = $δyᵃ(i, j, k, grid, c)
-        @inline $δz(i, j, k, grid, c) = $δzᵃ(i, j, k, grid, c)
+        @inline $δx(i, j, k, grid, c) = $δxᵃᵃ(i, j, k, grid, c)
+        @inline $δy(i, j, k, grid, c) = $δyᵃᵃ(i, j, k, grid, c)
+        @inline $δz(i, j, k, grid, c) = $δzᵃᵃ(i, j, k, grid, c)
+
+        @inline $δx_xy(i, j, k, grid, c) = $δxᵃᵃ(i, j, k, grid, c)
+        @inline $δx_xz(i, j, k, grid, c) = $δxᵃᵃ(i, j, k, grid, c)
+        @inline $δx_yz(i, j, k, grid, c) = $δxᵃᵃ(i, j, k, grid, c)
+
+        @inline $δy_xy(i, j, k, grid, c) = $δyᵃᵃ(i, j, k, grid, c)
+        @inline $δy_xz(i, j, k, grid, c) = $δyᵃᵃ(i, j, k, grid, c)
+        @inline $δy_yz(i, j, k, grid, c) = $δyᵃᵃ(i, j, k, grid, c)
+
+        @inline $δz_xy(i, j, k, grid, c) = $δzᵃᵃ(i, j, k, grid, c)
+        @inline $δz_xz(i, j, k, grid, c) = $δzᵃᵃ(i, j, k, grid, c)
+        @inline $δz_yz(i, j, k, grid, c) = $δzᵃᵃ(i, j, k, grid, c)
+
+        @inline $δx_xy(i, j, k, grid, f::Function, args...) = $δxᵃᵃ(i, j, k, grid, f, args...)
+        @inline $δx_xz(i, j, k, grid, f::Function, args...) = $δxᵃᵃ(i, j, k, grid, f, args...)
+        @inline $δx_yz(i, j, k, grid, f::Function, args...) = $δxᵃᵃ(i, j, k, grid, f, args...)
+
+        @inline $δy_xy(i, j, k, grid, f::Function, args...) = $δyᵃᵃ(i, j, k, grid, f, args...)
+        @inline $δy_xz(i, j, k, grid, f::Function, args...) = $δyᵃᵃ(i, j, k, grid, f, args...)
+        @inline $δy_yz(i, j, k, grid, f::Function, args...) = $δyᵃᵃ(i, j, k, grid, f, args...)
+
+        @inline $δz_xy(i, j, k, grid, f::Function, args...) = $δzᵃᵃ(i, j, k, grid, f, args...)
+        @inline $δz_xz(i, j, k, grid, f::Function, args...) = $δzᵃᵃ(i, j, k, grid, f, args...)
+        @inline $δz_yz(i, j, k, grid, f::Function, args...) = $δzᵃᵃ(i, j, k, grid, f, args...)
     end
 end
+
