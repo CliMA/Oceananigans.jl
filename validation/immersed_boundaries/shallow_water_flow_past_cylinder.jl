6b78a6dbadb53d88c3938077e1d01651c0aeb4b3
tomaschor <tomaschor@gmail.com>
2025-03-11 14:11:06 -0700

    more validation scripts

--- validation/immersed_boundaries/shallow_water_flow_past_cylinder.jl
+++ validation/immersed_boundaries/shallow_water_flow_past_cylinder.jl
@@ -57,7 +57,7 @@ uh, vh, h = model.solution
 outputs = merge(model.solution, (ζ=ζ,))
 
 simulation.output_writers[:fields] =
-    JLD2OutputWriter(model, outputs,
+    JLD2Writer(model, outputs,
                      schedule = TimeInterval(0.1),
                      filename = experiment_name,
                      field_slicer = nothing,
