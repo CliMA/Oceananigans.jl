2ab57f546597b56b81cf304d056529a6af7e4835
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-03-21 14:39:24 -0600

    Use VectorInvariant instead of WENOVectorInvariant

--- test/test_reactant_latitude_longitude_grid.jl
+++ test/test_reactant_latitude_longitude_grid.jl
@@ -12,7 +12,7 @@ include("reactant_test_utils.jl")
     stretched_lat_lon_kw = (; size=(Nx, Ny, Nz), halo, longitude=stretched_longitude, latitude, z)
 
     @info "Testing LatitudeLongitudeGrid + SplitExplicitFreeSurface + HydrostaticFreeSurfaceModel Reactant correctness"
-    hydrostatic_model_kw = (; momentum_advection=WENOVectorInvariant(), free_surface=SplitExplicitFreeSurface(substeps=4))
+    hydrostatic_model_kw = (; momentum_advection=VectorInvariant(), free_surface=SplitExplicitFreeSurface(substeps=4))
     test_reactant_model_correctness(LatitudeLongitudeGrid,
                                     HydrostaticFreeSurfaceModel,
                                     lat_lon_kw,
@@ -30,6 +30,18 @@ include("reactant_test_utils.jl")
     @test isnothing(η_grid.active_z_columns)
 
     #=
+    hydrostatic_model_kw = (; momentum_advection=WENOVectorInvariant(), free_surface=SplitExplicitFreeSurface(substeps=4))
+    @info "Testing LatitudeLongitudeGrid + WENO + SplitExplicitFreeSurface + HydrostaticFreeSurfaceModel Reactant correctness"
+    simulation = test_reactant_model_correctness(LatitudeLongitudeGrid,
+                                                 HydrostaticFreeSurfaceModel,
+                                                 lat_lon_kw,
+                                                 hydrostatic_model_kw,
+                                                 immersed_boundary_grid=true)
+    η = simulation.model.free_surface.η
+    η_grid = η.grid
+    @test isnothing(η_grid.interior_active_cells)
+    @test isnothing(η_grid.active_z_columns)
+
     @info "Testing LatitudeLongitudeGrid + 'complicated HydrostaticFreeSurfaceModel' Reactant correctness"
     equation_of_state = TEOS10EquationOfState()
     hydrostatic_model_kw = (momentum_advection = WENOVectorInvariant(),
