7b8ecfd4578dd07ac0028b89f27878249a05ff4b
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-03-21 14:59:33 -0600

    Add prototype code to extend maybe_extend_halos

--- ext/OceananigansReactantExt/Models.jl
+++ ext/OceananigansReactantExt/Models.jl
@@ -1,9 +1,14 @@
 module Models
 
 import Oceananigans.Models: initialization_update_state!
+import Oceananigans.Models.HydrostaticFreeSurfaceModels.SplitExplicitFreeSurfaces: maybe_extend_halos
 
 using ..TimeSteppers: ReactantModel
+using ..Grids: ReactantGrid
 
 initialization_update_state!(::ReactantModel; kw...) = nothing
+
+# We may need this but not sure:
+# maybe_extend_halos(TX, TY, ::ReactantGrid, ::FixedSubstepNumber) = grid
 
 end # module
