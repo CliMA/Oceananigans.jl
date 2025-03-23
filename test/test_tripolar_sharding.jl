f2319f63cdc505eb4b07758736c79b8b69d3f04a
Simone Silvestri <silvestri.simone0@gmail.com>
2025-03-23 19:36:37 +0100

    Update test_tripolar_sharding.jl

--- test/test_tripolar_sharding.jl
+++ test/test_tripolar_sharding.jl
@@ -20,50 +20,7 @@ ENV["XLA_FLAGS"] = "--xla_force_host_platform_device_count=8"
         @info "  Testing a tripolar grid on a $(arch.ranks) partition"
         local_grid  = TripolarGrid(arch; size = (40, 40, 1), z = (-1000, 0), halo = (2, 2, 2))
         global_grid = TripolarGrid(child_arch; size = (40, 40, 1), z = (-1000, 0), halo = (2, 2, 2))
-        
-        reconstruct_grid = reconstruct_global_grid(local_grid)
 
-        @test reconstruct_grid == global_grid
-        
-        nx, ny, _ = size(local_grid)
-        rx, ry, _ = arch.local_index .- 1
-
-        jrange = 1 + ry * ny : (ry + 1) * ny
-        irange = 1 + rx * nx : (rx + 1) * nx
-
-        for var in [:Δxᶠᶠᵃ, :Δxᶜᶜᵃ, :Δxᶠᶜᵃ, :Δxᶜᶠᵃ,
-                    :Δyᶠᶠᵃ, :Δyᶜᶜᵃ, :Δyᶠᶜᵃ, :Δyᶜᶠᵃ,
-                    :Azᶠᶠᵃ, :Azᶜᶜᵃ, :Azᶠᶜᵃ, :Azᶜᶠᵃ]
-
-            @test getproperty(local_grid, var)[1:nx, 1:ny] == getproperty(global_grid, var)[irange, jrange]
-            @test getproperty(local_grid, var)[1:nx, 1:ny] == getproperty(global_grid, var)[irange, jrange]
-            @test getproperty(local_grid, var)[1:nx, 1:ny] == getproperty(global_grid, var)[irange, jrange]
-        end
-
-        @info " Testing sharded fields on a $(arch.ranks) partition"
-
-        u = [i + 10 * j for i in 1:40, j in 1:40]
-        v = [i + 10 * j for i in 1:40, j in 1:40]
-        c = [i + 10 * j for i in 1:40, j in 1:40]
-    
-        up = XFaceField(local_grid)
-        vp = YFaceField(local_grid)
-        cp = CenterField(local_grid)
-
-        set!(up, u)
-        set!(vp, v)
-        set!(cp, c)
-        
-        us = XFaceField(global_grid)
-        vs = YFaceField(global_grid)
-        cs = CenterField(global_grid)
-
-        set!(us, u)
-        set!(vs, v)
-        set!(cs, c)
-
-        @test us == reconstruct_global_field(up)
-        @test vs == reconstruct_global_field(vp)
-        @test cs == reconstruct_global_field(cp)
+        # Actually nothing for the moment...
     end
 end
