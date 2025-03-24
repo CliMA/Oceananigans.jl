7f49b59efe3a013fdd6e93b66448337bea915dd5
Gregory L. Wagner <wagner.greg@gmail.com>
2025-03-24 07:31:09 -0600

    Generalize Simulation struct (#4261)

--- src/Simulations/simulation.jl
+++ src/Simulations/simulation.jl
@@ -8,21 +8,21 @@ import Oceananigans.TimeSteppers: reset!
 
 default_progress(simulation) = nothing
 
-mutable struct Simulation{ML, DT, ST, DI, OW, CB}
+mutable struct Simulation{ML, DT, ST, DI, OW, CB, FT, BL}
     model :: ML
     Δt :: DT
-    stop_iteration :: Float64
+    stop_iteration :: FT
     stop_time :: ST
-    wall_time_limit :: Float64
+    wall_time_limit :: FT
     diagnostics :: DI
     output_writers :: OW
     callbacks :: CB
-    run_wall_time :: Float64
-    align_time_step :: Bool
-    running :: Bool
-    initialized :: Bool
-    verbose :: Bool
-    minimum_relative_step :: Float64
+    run_wall_time :: FT
+    align_time_step :: BL
+    running :: BL
+    initialized :: BL
+    verbose :: BL
+    minimum_relative_step :: FT
 end
 
 """
