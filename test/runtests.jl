082bb1db60ac8a4775dc8316c9a67c41367566a7
Simone Silvestri <silvestri.simone0@gmail.com>
2025-03-28 13:47:57 +0100

    add sharding lat lon

--- test/runtests.jl
+++ test/runtests.jl
@@ -251,7 +251,7 @@ CUDA.allowscalar() do
     if group == :sharding || group == :all
         @testset "Sharding Reactant extension tests" begin
             # Broken for the moment (trying to fix them in https://github.com/CliMA/Oceananigans.jl/pull/4293)
-            # include("test_sharded_lat_lon.jl")
+            include("test_sharded_lat_lon.jl")
             # include("test_sharded_tripolar.jl") 
         end
     end
