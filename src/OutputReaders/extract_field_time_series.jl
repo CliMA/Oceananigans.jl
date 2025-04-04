855b0118d770902d1f603d4598398fd9affb1510
Simone Silvestri <silvestri.simone0@gmail.com>
2025-04-04 08:28:28 +0200

    try these changes

--- src/OutputReaders/extract_field_time_series.jl
+++ src/OutputReaders/extract_field_time_series.jl
@@ -1,38 +1,40 @@
 using Oceananigans.AbstractOperations: AbstractOperation
-using Oceananigans.Fields: flattened_unique_values
+using Oceananigans.Fields: flattened_unique_values, Field
+using Oceananigans.Grids: AbstractGrid
 
 #####
 ##### Utility for "extracting" FieldTimeSeries from large nested objects (eg models)
 #####
 
-extract_field_time_series(t1, tn...) = extract_field_time_series(tuple(t1, tn...))
+extract_field_time_series(args...) = ()
 
-# Utility used to extract field time series from a type through recursion
-function extract_field_time_series(t) 
-    prop = propertynames(t)
-    if isempty(prop)
-        return nothing
-    end
+# extract_field_time_series(t1, tn...) = extract_field_time_series(tuple(t1, tn...))
 
-    extracted = Tuple(extract_field_time_series(getproperty(t, p)) for p in prop)
-    flattened = flattened_unique_values(extracted)
+# # Utility used to extract field time series from a type through recursion
+# function extract_field_time_series(t) 
+#     prop = propertynames(t)
+#     if isempty(prop)
+#         return nothing
+#     end
 
-    return flattened
-end
+#     extracted = Tuple(extract_field_time_series(getproperty(t, p)) for p in prop)
+#     flattened = flattened_unique_values(extracted)
 
-# Termination (move all here when we switch the code up)
-extract_field_time_series(f::FieldTimeSeries) = f
+#     return flattened
+# end
 
-# For types that do not contain `FieldTimeSeries`, halt the recursion
-CannotPossiblyContainFTS = (:Number, :AbstractArray)
+# # Termination (move all here when we switch the code up)
+# extract_field_time_series(f::FieldTimeSeries) = f
 
-for T in CannotPossiblyContainFTS
-    @eval extract_field_time_series(::$T) = nothing
-end
+# # For types that do not contain `FieldTimeSeries`, halt the recursion
+# CannotPossiblyContainFTS = (:Number, :AbstractArray, :AbstractGrid, :Field)
 
-# Special recursion rules for `Tuple` and `Field` types
-extract_field_time_series(t::AbstractField)     = Tuple(extract_field_time_series(getproperty(t, p)) for p in propertynames(t))
-extract_field_time_series(t::AbstractOperation) = Tuple(extract_field_time_series(getproperty(t, p)) for p in propertynames(t))
+# for T in CannotPossiblyContainFTS
+#     @eval extract_field_time_series(::$T) = nothing
+# end
 
-extract_field_time_series(t::Union{Tuple, NamedTuple}) = map(extract_field_time_series, t)
+# # Special recursion rules for `Tuple` and `Field` types
+# extract_field_time_series(t::AbstractField)     = Tuple(extract_field_time_series(getproperty(t, p)) for p in propertynames(t))
+# extract_field_time_series(t::AbstractOperation) = Tuple(extract_field_time_series(getproperty(t, p)) for p in propertynames(t))
+# extract_field_time_series(t::Union{Tuple, NamedTuple}) = map(extract_field_time_series, t)
 
