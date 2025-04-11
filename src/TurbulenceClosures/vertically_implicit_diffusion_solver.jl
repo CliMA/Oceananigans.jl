c5ed0357a86a8c0599708e1448cecb570d321579
Simone Silvestri <silvestri.simone0@gmail.com>
2025-04-11 11:48:45 +0200

    back

--- src/TurbulenceClosures/vertically_implicit_diffusion_solver.jl
+++ src/TurbulenceClosures/vertically_implicit_diffusion_solver.jl
@@ -204,12 +204,9 @@ function implicit_step!(field::Field,
 
     LX, LY, LZ = location(field)
     # Nullify tracer_index if `field` is not a tracer   
-    if (LX, LY, LZ) == (Center, Center, Center) 
-        return nothing
-    else
-        tracer_index = nothing
-        return solve!(field, implicit_solver, field,
-                      # ivd_*_diagonal gets called with these args after (i, j, k, grid):
-                      vi_closure, vi_diffusivity_fields, tracer_index, LX(), LY(), LZ(), Δt, clock; kwargs...)
-    end
+    (LX, LY, LZ) == (Center, Center, Center) || tracer_index = nothing
+        
+    return solve!(field, implicit_solver, field,
+                  # ivd_*_diagonal gets called with these args after (i, j, k, grid):
+                  vi_closure, vi_diffusivity_fields, tracer_index, LX(), LY(), LZ(), Δt, clock; kwargs...)
 end
