5afc77732389e2496ff424b38090b910b25b5c01
Simone Silvestri <silvestri.simone0@gmail.com>
2025-04-15 10:47:24 +0200

    remove constructors

--- validation/implicit_free_surface/geostrophic_adjustment_test.jl
+++ validation/implicit_free_surface/geostrophic_adjustment_test.jl
@@ -6,7 +6,6 @@ using Oceananigans.MultiRegion
 using Statistics
 using Printf
 using LinearAlgebra, SparseArrays
-using Oceananigans.Solvers: constructors, unpack_constructors
 using Oceananigans.Grids
 using GLMakie
 using JLD2
