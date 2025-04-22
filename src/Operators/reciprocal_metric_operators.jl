f1556083e8547d689dfe9f8ec8466a206bc7fd0f
Simone Silvestri <silvestri.simone0@gmail.com>
2025-04-22 21:36:18 +0200

    this is better

--- src/Operators/reciprocal_metric_operators.jl
+++ src/Operators/reciprocal_metric_operators.jl
@@ -1,9 +1,9 @@
 # Reciprocal metrics operators, at the moment equal to the inverse of the metric operators
 for L1 in (:ᶜ, :ᶠ), L2 in (:ᶜ, :ᶠ), L3 in (:ᶜ, :ᶠ)
-    for metric in (:Δ, :A)
+    for func in (:Δ, :A)
         for dir in (:x, :y, :z)
-            rcp_metric = Symbol(metric, dir, L1, L2, L3, :⁻¹)
-            metric = Symbol(metric, dir, L1, L2, L3)
+            rcp_metric = Symbol(func, dir, L1, L2, L3, :⁻¹)
+            metric = Symbol(func, dir, L1, L2, L3)
             @eval @inline $rcp_metric(i, j, k, grid) = 1 / $metric(i, j, k, grid)
         end
     end
