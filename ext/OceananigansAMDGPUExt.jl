12b8cc2363de2569beeb9ec680b841ecd915b909
Simone Silvestri <silvestri.simone0@gmail.com>
2025-04-26 17:00:19 +0200

    bugfix

--- ext/OceananigansAMDGPUExt.jl
+++ ext/OceananigansAMDGPUExt.jl
@@ -30,9 +30,9 @@ on_architecture(::ROCGPU, a::StepRangeLen) = a
 @inline convert_to_device(::ROCGPU, args) = AMDGPU.rocconvert(args)
 @inline convert_to_device(::ROCGPU, args::Tuple) = map(AMDGPU.rocconvert, args)
 
-AMDGPU.Devices.@device_override @inline function __validindex(ctx::MappedCompilerMetadata)
+AMDGPU.Device.@device_override @inline function __validindex(ctx::MappedCompilerMetadata)
     if __dynamic_checkbounds(ctx)
-        I = @inbounds linear_expand(__iterspace(ctx), MDGPU.Device.blockIdx().x, AMDGPU.Device.threadIdx().x)
+        I = @inbounds linear_expand(__iterspace(ctx), AMDGPU.Device.blockIdx().x, AMDGPU.Device.threadIdx().x)
         return I in __linear_ndrange(ctx)
     else
         return true
