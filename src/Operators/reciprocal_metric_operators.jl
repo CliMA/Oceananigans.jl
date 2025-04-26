6c3301524d837223d42995c635c07e9a0b6f1ee3
Simone Silvestri <silvestri.simone0@gmail.com>
2025-04-26 16:36:41 +0200

    corrections

--- src/Operators/reciprocal_metric_operators.jl
+++ src/Operators/reciprocal_metric_operators.jl
@@ -3,18 +3,18 @@ for L1 in (:ᶜ, :ᶠ, :ᵃ), L2 in (:ᶜ, :ᶠ, :ᵃ), L3 in (:ᶜ, :ᶠ, :ᵃ)
     for dir in (:x, :y)
         rcp_spacing = Symbol(:Δ, dir, :⁻¹, L1, L2, L3)
         spacing = Symbol(:Δ, dir, L1, L2, L3)
-        @eval @inline $rcp_metric(i, j, k, grid) = 1 / $metric(i, j, k, grid)
+        @eval @inline $rcp_spacing(i, j, k, grid) = 1 / $spacing(i, j, k, grid)
     end
     
     for dir in (:x, :y, :z)
         rcp_area = Symbol(:A, dir, :⁻¹, L1, L2, L3)
         area = Symbol(:A, dir, L1, L2, L3)
-        @eval @inline $rcp_metric(i, j, k, grid) = 1 / $metric(i, j, k, grid)
+        @eval @inline $rcp_area(i, j, k, grid) = 1 / $area(i, j, k, grid)
     end
 
-    rcp_volume = Symbol(:V⁻¹, L1, L2, L3)
-    volume = Symbol(:V, L1, L2, L3)
-    @eval @inline $rcp_volume(i, j, k, grid) = 1 / $volume(i, j, k, grid)
+    rcp_vol = Symbol(:V⁻¹, L1, L2, L3)
+    vol = Symbol(:V, L1, L2, L3)
+    @eval @inline $rcp_vol(i, j, k, grid) = 1 / $vol(i, j, k, grid)
 end
 
 @inline Δr⁻¹ᵃᵃᶜ(i, j, k, grid) = getspacing(k, grid.z.Δ⁻¹ᵃᵃᶜ)
@@ -24,23 +24,23 @@ end
 @inline Δz⁻¹ᵃᵃᶠ(i, j, k, grid) = getspacing(k, grid.z.Δ⁻¹ᵃᵃᶠ)
 
 for L1 in (:ᶜ, :ᶠ), L2 in (:ᶜ, :ᶠ)
-    Δzᵃᵃˡ = Symbol(:Δz, :ᵃ, :ᵃ, L1)
-    Δrᵃᵃˡ = Symbol(:Δr, :ᵃ, :ᵃ, L1)
-    Δzˡᵃˡ = Symbol(:Δz, L2, :ᵃ, L1)
-    Δrˡᵃˡ = Symbol(:Δr, L2, :ᵃ, L1)
-    Δzᵃˡˡ = Symbol(:Δz, :ᵃ, L2, L1)
-    Δrᵃˡˡ = Symbol(:Δr, :ᵃ, L2, L1)
+    Δz⁻¹ᵃᵃˡ = Symbol(:Δz⁻¹, :ᵃ, :ᵃ, L1)
+    Δr⁻¹ᵃᵃˡ = Symbol(:Δr⁻¹, :ᵃ, :ᵃ, L1)
+    Δz⁻¹ˡᵃˡ = Symbol(:Δz⁻¹, L2, :ᵃ, L1)
+    Δr⁻¹ˡᵃˡ = Symbol(:Δr⁻¹, L2, :ᵃ, L1)
+    Δz⁻¹ᵃˡˡ = Symbol(:Δz⁻¹, :ᵃ, L2, L1)
+    Δr⁻¹ᵃˡˡ = Symbol(:Δr⁻¹, :ᵃ, L2, L1)
 
-    @eval @inline $Δzˡᵃˡ(i, j, k, grid) = $Δzᵃᵃˡ(i, j, k, grid)
-    @eval @inline $Δzᵃˡˡ(i, j, k, grid) = $Δzᵃᵃˡ(i, j, k, grid)
-    @eval @inline $Δrˡᵃˡ(i, j, k, grid) = $Δrᵃᵃˡ(i, j, k, grid)
-    @eval @inline $Δrᵃˡˡ(i, j, k, grid) = $Δrᵃᵃˡ(i, j, k, grid)
+    @eval @inline $Δr⁻¹ᵃᵃˡ(i, j, k, grid) = $Δz⁻¹ᵃᵃˡ(i, j, k, grid)
+    @eval @inline $Δz⁻¹ˡᵃˡ(i, j, k, grid) = $Δz⁻¹ᵃᵃˡ(i, j, k, grid)
+    @eval @inline $Δr⁻¹ˡᵃˡ(i, j, k, grid) = $Δr⁻¹ᵃᵃˡ(i, j, k, grid)
+    @eval @inline $Δr⁻¹ᵃˡˡ(i, j, k, grid) = $Δr⁻¹ᵃᵃˡ(i, j, k, grid)
 
     for L3 in (:ᶜ, :ᶠ)
-        Δzˡˡˡ = Symbol(:Δz, L2, L3, L1)
-        Δrˡˡˡ = Symbol(:Δr, L2, L3, L1)
+        Δz⁻¹ˡˡˡ = Symbol(:Δz⁻¹, L2, L3, L1)
+        Δr⁻¹ˡˡˡ = Symbol(:Δr⁻¹, L2, L3, L1)
 
-        @eval @inline $Δzˡˡˡ(i, j, k, grid) = $Δzˡᵃˡ(i, j, k, grid)
-        @eval @inline $Δrˡˡˡ(i, j, k, grid) = $Δrˡᵃˡ(i, j, k, grid)
+        @eval @inline $Δz⁻¹ˡˡˡ(i, j, k, grid) = $Δz⁻¹ˡᵃˡ(i, j, k, grid)
+        @eval @inline $Δr⁻¹ˡˡˡ(i, j, k, grid) = $Δr⁻¹ˡᵃˡ(i, j, k, grid)
     end
 end
