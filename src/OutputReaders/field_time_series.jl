a2fb18e252387be276a46f54a82617ef2ebb2b6b
Lois Baker <lbaker@ed.ac.uk>
2025-05-13 15:29:05 +0100

    Add a space

--- src/OutputReaders/field_time_series.jl
+++ src/OutputReaders/field_time_series.jl
@@ -249,7 +249,7 @@ mutable struct FieldTimeSeries{LX, LY, LZ, TI, K, I, D, G, ET, B, χ, P, N, KW} 
         if times isa AbstractArray
             # Try to convert to a range
             time_range = range(first(times), last(times), length=length(times))
-            if isapprox(time_range,times) 
+            if isapprox(time_range, times) 
                 times = time_range
             end
 
