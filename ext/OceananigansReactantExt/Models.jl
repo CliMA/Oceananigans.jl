60ec39499311230153c1a85e29aa0f8428719029
Gregory L. Wagner <wagner.greg@gmail.com>
2025-05-14 16:09:22 -0600

    Update Models.jl

--- ext/OceananigansReactantExt/Models.jl
+++ ext/OceananigansReactantExt/Models.jl
@@ -18,7 +18,6 @@ const ReactantHFSM{TS, E} = Union{
     HydrostaticFreeSurfaceModel{TS, E, <:Distributed{<:ReactantState}},
 }
 
-# initialization_update_state!(model::ReactantModel; kw...) = @jit update_state(model; kw...)
 initialize_immersed_boundary_grid!(grid) = nothing
 
 using Oceananigans.ImmersedBoundaries:
