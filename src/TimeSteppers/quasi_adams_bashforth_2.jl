ca070b559e2ff9331d151e8425a314ebf11a6e35
Simone Silvestri <silvestri.simone0@gmail.com>
2025-05-15 15:07:40 +0200

    fix it

--- src/TimeSteppers/quasi_adams_bashforth_2.jl
+++ src/TimeSteppers/quasi_adams_bashforth_2.jl
@@ -103,7 +103,7 @@ function time_step!(model::AbstractModel{<:QuasiAdamsBashforth2TimeStepper}, Δt
     model.clock.last_stage_Δt = Δt # just one stage
 
     calculate_pressure_correction!(model, Δt)
-    @apply_regionally correct_velocities_and_store_tendencies!(model, Δt)
+    @apply_regionally correct_velocities_and_cache_previous_tendencies!(model, Δt)
     correct_advection!(model, Δt)
 
     update_state!(model, callbacks; compute_tendencies=true)
