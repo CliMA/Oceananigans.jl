837e3fd76ab4df2cad1c12e4f1bec78cd08ef1eb
Simone Silvestri <silvestri.simone0@gmail.com>
2025-05-19 15:32:02 +0200

    explicit time discretization

--- src/Simulations/VarianceDissipationComputations/diffusive_dissipation.jl
+++ src/Simulations/VarianceDissipationComputations/diffusive_dissipation.jl
@@ -1,3 +1,5 @@
+using Oceananigans.TurbulenceClosures: ExplicitTimeDiscretization
+
 @kernel function _assemble_diffusive_dissipation!(K, grid, χ, Vⁿ, Vⁿ⁻¹, cⁿ⁺¹, cⁿ)
     i, j, k = @index(Global, NTuple)
     compute_diffusive_dissipation!(K, i, j, k, grid, Vⁿ, Vⁿ⁻¹, χ, cⁿ⁺¹, cⁿ)
@@ -48,11 +50,16 @@ end
     end
 end
 
+# For a nothing closure, do nothing...
+compute_diffusive_fluxes!(Vⁿ, i, j, k, grid, ::Nothing, K, args...) = nothing
+    
+const etd = ExplicitTimeDiscretization()
+
 @inline function compute_diffusive_fluxes!(Vⁿ, i, j, k, grid, clo, K, b, c, c_id, clk, fields)
     @inbounds begin
-        Vⁿ.x[i, j, k] += _diffusive_flux_x(i, j, k, grid, clo, K, c_id, c, clk, fields, b) * Axᶠᶜᶜ(i, j, k, grid) * σⁿ(i, j, k, grid, f, c, c)
-        Vⁿ.y[i, j, k] += _diffusive_flux_y(i, j, k, grid, clo, K, c_id, c, clk, fields, b) * Ayᶜᶠᶜ(i, j, k, grid) * σⁿ(i, j, k, grid, c, f, c)
-        Vⁿ.z[i, j, k] += _diffusive_flux_z(i, j, k, grid, clo, K, c_id, c, clk, fields, b) * Azᶜᶜᶠ(i, j, k, grid) * σⁿ(i, j, k, grid, c, c, f)
+        Vⁿ.x[i, j, k] += _diffusive_flux_x(i, j, k, grid, etd, clo, K, c_id, c, clk, fields, b) * Axᶠᶜᶜ(i, j, k, grid) * σⁿ(i, j, k, grid, f, c, c)
+        Vⁿ.y[i, j, k] += _diffusive_flux_y(i, j, k, grid, etd, clo, K, c_id, c, clk, fields, b) * Ayᶜᶠᶜ(i, j, k, grid) * σⁿ(i, j, k, grid, c, f, c)
+        Vⁿ.z[i, j, k] += _diffusive_flux_z(i, j, k, grid, etd, clo, K, c_id, c, clk, fields, b) * Azᶜᶜᶠ(i, j, k, grid) * σⁿ(i, j, k, grid, c, c, f)
     end
     return nothing
 end
