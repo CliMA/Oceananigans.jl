dd693d8bd2d18ae4b81b4d715e45a34c45c639e4
Simone Silvestri <silvestri.simone0@gmail.com>
2025-05-19 15:26:59 +0200

    be fair with the timestep size

--- validation/implicit_dissipation/variance_dissipation.jl
+++ validation/implicit_dissipation/variance_dissipation.jl
@@ -32,8 +32,6 @@ a = 0.5
     end
 end
 
-Δt_max = 0.2 * minimum_xspacing(grid)
-
 # Change to test pure advection schemes
 tracer_advection = WENO(order=9)
 closure = ScalarDiffusivity(κ=5e-5)
@@ -74,7 +72,13 @@ for (ts, timestepper) in zip((:AB2, :RK3), (:QuasiAdamsBashforth2, :SplitRungeKu
     set!(model, c=c₀)
     set!(model.auxiliary_fields.c⁻, c₀)
 
-    sim = Simulation(model, Δt=Δt_max, stop_time=10)
+    Δt = if ts == :AB2
+        0.2 * minimum_xspacing(grid)
+    else
+        0.6 * minimum_xspacing(grid)
+    end
+
+    sim = Simulation(model; Δt, stop_time=10)
 
     ϵ = VarianceDissipation(:c, grid)
     f = Oceananigans.Simulations.VarianceDissipationComputations.flatten_dissipation_fields(ϵ)
@@ -102,30 +106,32 @@ r_Δtc² = FieldTimeSeries("one_d_simulation_RK3.jld2", "Δtc²")
 r_Acx  = FieldTimeSeries("one_d_simulation_RK3.jld2", "Acx")
 r_Dcx  = FieldTimeSeries("one_d_simulation_RK3.jld2", "Dcx")
 
-Nt = length(a_c.times)
+Nta = length(a_c.times)
+Ntr = length(r_c.times)
 
-a_∫closs = - [sum(interior(a_Δtc²[i], :, 1, 1) .* grid.Δxᶜᵃᵃ)  for i in 2:Nt-1]
-a_∫A     = - [sum(interior(a_Acx[i] , :, 1, 1))                for i in 2:Nt-1]
-a_∫D     = - [sum(interior(a_Dcx[i] , :, 1, 1))                for i in 2:Nt-1]
+a_∫closs = - [sum(interior(a_Δtc²[i], :, 1, 1) .* grid.Δxᶜᵃᵃ) for i in 2:Nta-1]
+a_∫A     = - [sum(interior(a_Acx[i] , :, 1, 1))               for i in 2:Nta-1]
+a_∫D     = - [sum(interior(a_Dcx[i] , :, 1, 1))               for i in 2:Nta-1]
 a_∫T     = a_∫D .+ a_∫A
 
-r_∫closs = - [sum(interior(r_Δtc²[i], :, 1, 1) .* grid.Δxᶜᵃᵃ)  for i in 2:Nt-1]
-r_∫A     = - [sum(interior(r_Acx[i] , :, 1, 1))                for i in 2:Nt-1]
-r_∫D     = - [sum(interior(r_Dcx[i] , :, 1, 1))                for i in 2:Nt-1]
+r_∫closs = - [sum(interior(r_Δtc²[i], :, 1, 1) .* grid.Δxᶜᵃᵃ) for i in 2:Ntr-1]
+r_∫A     = - [sum(interior(r_Acx[i] , :, 1, 1))               for i in 2:Ntr-1]
+r_∫D     = - [sum(interior(r_Dcx[i] , :, 1, 1))               for i in 2:Ntr-1]
 r_∫T     = r_∫D .+ r_∫A
 
-times = a_c.times[2:end-1]
+atimes = a_c.times[2:end-1]
+rtimes = r_c.times[2:end-1]
 
 fig = Figure()
 ax  = Axis(fig[1, 1], title="Dissipation", xlabel="Time (s)", ylabel="Dissipation", yscale=log10)
 
-scatter!(ax, times, a_∫closs, label="AB2 total variance loss", color=:blue)
-lines!(ax, times, a_∫A, label="AB2 advection dissipation", color=:red)
-lines!(ax, times, a_∫D, label="AB2 diffusive dissipation", color=:green)
-lines!(ax, times, a_∫T, label="AB2 total dissipation", color=:purple)
+scatter!(ax, atimes, a_∫closs, label="AB2 total variance loss", color=:blue)
+lines!(ax, atimes, a_∫A, label="AB2 advection dissipation", color=:red)
+lines!(ax, atimes, a_∫D, label="AB2 diffusive dissipation", color=:green)
+lines!(ax, atimes, a_∫T, label="AB2 total dissipation", color=:purple)
 
-scatter!(ax, times, r_∫closs, label="RK3 total variance loss", color=:blue, marker=:diamond)
-lines!(ax, times, r_∫A, label="RK3 advection dissipation", color=:red, linestyle=:dash)
-lines!(ax, times, r_∫D, label="RK3 diffusive dissipation", color=:green, linestyle=:dash)
-lines!(ax, times, r_∫T, label="RK3 total dissipation", color=:purple, linestyle=:dash)
+scatter!(ax, rtimes, r_∫closs, label="RK3 total variance loss", color=:blue, marker=:diamond)
+lines!(ax, rtimes, r_∫A, label="RK3 advection dissipation", color=:red, linestyle=:dash)
+lines!(ax, rtimes, r_∫D, label="RK3 diffusive dissipation", color=:green, linestyle=:dash)
+lines!(ax, rtimes, r_∫T, label="RK3 total dissipation", color=:purple, linestyle=:dash)
 axislegend(ax, position=:lb)
