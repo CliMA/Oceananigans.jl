886629fe89c2816bfa0eec84d8ad79a31c1fde2f
tomchor <tomaschor@gmail.com>
2025-05-20 12:52:45 -0600

    mwe still working on casoer

--- mwe_obc.jl
+++ mwe_obc.jl
@@ -9,22 +9,22 @@ grid_base = RectilinearGrid(topology = (Bounded, Flat, Bounded), size = (Nx, Nz)
 flat_bottom(x) = 1
 grid = ImmersedBoundaryGrid(grid_base, PartialCellBottom(flat_bottom))
 U = 1
-inflow_timescale = 0.0
+inflow_timescale = 1e-4
 outflow_timescale = Inf
 u_boundaries = FieldBoundaryConditions(
     west   = PerturbationAdvectionOpenBoundaryCondition(U; inflow_timescale, outflow_timescale),
     east   = PerturbationAdvectionOpenBoundaryCondition(U; inflow_timescale, outflow_timescale),)
 
 model = NonhydrostaticModel(; grid,
-                              #boundary_conditions = (; u = u_boundaries),
-                              #pressure_solver = ConjugateGradientPoissonSolver(grid, maxiter=5),
-                              #advection = WENO(; grid, order=5)
+                              boundary_conditions = (; u = u_boundaries),
+                              pressure_solver = ConjugateGradientPoissonSolver(grid, maxiter=5),
+                              advection = WENO(; grid, order=5)
                              )
-#set!(model, u=U)
+set!(model, u=U)
 
 Δt = 0.5 * minimum_zspacing(grid) / abs(U)
 simulation = Simulation(model; Δt = Δt, stop_time=10)
 
-#conjure_time_step_wizard!(simulation, IterationInterval(1), cfl = 0.1)
+conjure_time_step_wizard!(simulation, IterationInterval(1), cfl = 0.1)
 
 run!(simulation)
