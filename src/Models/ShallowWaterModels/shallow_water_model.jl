94f691b5d096bfa007c5ece049205485e350c4de
Simone Silvestri <silvestri.simone0@gmail.com>
2025-05-20 15:38:22 +0200

    more cleanup

--- src/Models/ShallowWaterModels/shallow_water_model.jl
+++ src/Models/ShallowWaterModels/shallow_water_model.jl
@@ -9,7 +9,7 @@ using Oceananigans.Fields: Field, tracernames, TracerFields, XFaceField, YFaceFi
 using Oceananigans.Forcings: model_forcing
 using Oceananigans.Grids: topology, Flat, architecture, RectilinearGrid, Face, Center
 using Oceananigans.ImmersedBoundaries: ImmersedBoundaryGrid
-using Oceananigans.Models: validate_model_halo, NaNChecker, validate_tracer_advection
+using Oceananigans.Models: validate_model_halo, validate_tracer_advection
 using Oceananigans.TimeSteppers: Clock, TimeStepper, update_state!
 using Oceananigans.TurbulenceClosures: with_tracers, build_diffusivity_fields
 using Oceananigans.Utils: tupleit
