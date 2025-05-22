a4ebe04b5bb6586115f0944f53702d2e7d1b3a4c
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-05-22 07:54:09 +0300

    Apply suggestions from code review

--- src/OutputReaders/set_field_time_series.jl
+++ src/OutputReaders/set_field_time_series.jl
@@ -9,6 +9,7 @@ iterations_from_file(file) = parse.(Int, keys(file["timeseries/t"]))
 
 function find_time_index(time::Number, file_times, Δt)
     # Accommodate round-off discrepancies between the FTS times and file times
+    # (see https://github.com/CliMA/Oceananigans.jl/pull/4505)
     ϵ = 100 * eps(eltype(file_times))
     return findfirst(t -> isapprox(t, time, atol=ϵ*Δt), file_times)
 end
