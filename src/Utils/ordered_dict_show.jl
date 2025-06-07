7490936cd2b91cf4adf9f5630615790ec0fdc8d7
Thimoté Dupuch <50801309+thimotedupuch@users.noreply.github.com>
2025-06-07 20:52:16 +0200

    Update ordered_dict_show.jl
    Changed ordered_dict_show function without changing the original logic of the function

--- src/Utils/ordered_dict_show.jl
+++ src/Utils/ordered_dict_show.jl
@@ -4,15 +4,27 @@ function ordered_dict_show(dict::OrderedDict, padchar)
     name = "OrderedDict"
     N = length(dict)
 
-    if N === 0
+    if N == 0
         return "$name with no entries"
-    elseif N == 1
-        return string("$name with 1 entry:", "\n",
-                      padchar, "   └── ", dict.keys[1], " => ", summary(dict.vals[1]))
-    else
-        return string(name, " with $N entries:\n",
-                      Tuple(string(padchar, "   ├── $name => ", summary(dict[name]), "\n") for name in dict.keys[1:end-1])...,
-                            string(padchar, "   └── ", dict.keys[end], " => ", summary(dict.vals[end])))
+    else if N == 1
+        k = dict |> keys |> first
+        sum_v = dict |> values |> first |> summary
+        return "$name with 1 entry:\n$padchar   └── $k => $sum_v"
     end
+
+    keys_list = dict |> keys |> collect
+    summary_vals_list = dict |> values |> collect |> v -> summary(v)
+    lines = ["$name with $N entries:"]
+
+    for i in 1:N-1
+        k, sum_v  = keys_list[i], summary_vals_list[i]
+        push!(lines, "$padchar   ├── $k => $sum_v")
+    end
+
+    last_k, last_sum_v = last(keys_list), last(summary_vals_list)
+    push!(lines, "$padchar   └── $last_k => $last_sum_v")
+
+    return join(lines, "\n")
 end
+
 
