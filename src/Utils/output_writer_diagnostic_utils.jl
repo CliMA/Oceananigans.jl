f0d995558cfb482b53974ddab264ca7822e55fff
Thimoté Dupuch <50801309+thimotedupuch@users.noreply.github.com>
2025-06-07 20:57:18 +0200

    Update output_writer_diagnostic_utils.jl
    Improved formatting and changed push! functions as one liners

--- src/Utils/output_writer_diagnostic_utils.jl
+++ src/Utils/output_writer_diagnostic_utils.jl
@@ -8,22 +8,16 @@ using Oceananigans: AbstractOutputWriter, AbstractDiagnostic
 #####
 
 defaultname(::AbstractDiagnostic, nelems) = Symbol(:diag, nelems+1)
+
 defaultname(::AbstractOutputWriter, nelems) = Symbol(:writer, nelems+1)
 
 const DiagOrWriterDict = OrderedDict{S, <:Union{AbstractDiagnostic, AbstractOutputWriter}} where S
 
-function push!(container::DiagOrWriterDict, elem)
-    name = defaultname(elem, length(container))
-    container[name] = elem
-    return nothing
-end
+push!(container::DiagOrWriterDict, elem) = (container[defaultname(elem, length(container))] = elem; nothing)
 
 getindex(container::DiagOrWriterDict, inds::Integer...) = getindex(container.vals, inds...)
+
 setindex!(container::DiagOrWriterDict, newvals, inds::Integer...) = setindex!(container.vals, newvals, inds...)
 
-function push!(container::DiagOrWriterDict, elems...)
-    for elem in elems
-        push!(container, elem)
-    end
-    return nothing
-end
+push!(container::DiagOrWriterDict, elems...) = (foreach(e -> push!(container, e), elems); nothing)
+
