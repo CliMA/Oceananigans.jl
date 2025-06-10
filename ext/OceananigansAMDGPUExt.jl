009bff7cf7d033595043a816b4cd396a0721b53e
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-06-10 16:51:35 +1000

    extend plan transforms for Flat

--- ext/OceananigansAMDGPUExt.jl
+++ ext/OceananigansAMDGPUExt.jl
@@ -4,12 +4,16 @@ using AMDGPU
 using Oceananigans
 using Oceananigans.Utils: linear_expand, __linear_ndrange, MappedCompilerMetadata
 using KernelAbstractions: __dynamic_checkbounds, __iterspace
+
 import KernelAbstractions: __validindex
 
 import Oceananigans.Architectures:
     architecture,
     convert_to_device,
     on_architecture
+import Oceananigans.Solvers:
+    plan_backward_transform,
+    plan_forward_transform
 
 const ROCGPU = GPU{<:AMDGPU.ROCBackend}
 ROCGPU() = GPU(AMDGPU.ROCBackend())
@@ -29,6 +33,9 @@ on_architecture(::ROCGPU, a::StepRangeLen) = a
 
 @inline convert_to_device(::ROCGPU, args) = AMDGPU.rocconvert(args)
 @inline convert_to_device(::ROCGPU, args::Tuple) = map(AMDGPU.rocconvert, args)
+
+plan_backward_transform(A::ROCArray, ::Flat, args...) = nothing
+plan_forward_transform(A::ROCArray, ::Flat, args...) = nothing
 
 AMDGPU.Device.@device_override @inline function __validindex(ctx::MappedCompilerMetadata)
     if __dynamic_checkbounds(ctx)
