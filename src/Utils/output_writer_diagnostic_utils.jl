90761cab494e25b044653118f3ce60f9300ca9c6
Gregory L. Wagner <gregory.leclaire.wagner@gmail.com>
2025-06-10 10:28:10 -0600

    Update src/Utils/output_writer_diagnostic_utils.jl

--- src/Utils/output_writer_diagnostic_utils.jl
+++ src/Utils/output_writer_diagnostic_utils.jl
@@ -13,7 +13,11 @@ defaultname(::AbstractOutputWriter, nelems) = Symbol(:writer, nelems+1)
 
 const DiagOrWriterDict = OrderedDict{S, <:Union{AbstractDiagnostic, AbstractOutputWriter}} where S
 
-push!(container::DiagOrWriterDict, elem) = (container[defaultname(elem, length(container))] = elem; nothing)
+function push!(container::DiagOrWriterDict, elem)
+    name = defaultname(elem, length(container))
+    container[name] = elem
+    return nothing
+end
 
 getindex(container::DiagOrWriterDict, inds::Integer...) = getindex(container.vals, inds...)
 
