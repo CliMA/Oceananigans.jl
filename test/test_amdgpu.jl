495be4819b91d21881ace64f485f786b7cf218f1
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-06-10 21:44:53 +0800

    test few grids

--- test/test_amdgpu.jl
+++ test/test_amdgpu.jl
@@ -2,16 +2,18 @@ include("dependencies_for_runtests.jl")
 
 using AMDGPU
 
-function timestep_simulation(model)
+function build_and_timestep_simulation(model)
+    FT = eltype(model)
+
     for field in merge(model.velocities, model.tracers)
         @test parent(field) isa ROCArray
     end
 
-    simulation = Simulation(model, Δt=1minute, stop_iteration=3)
+    simulation = Simulation(model, Δt=1minute, stop_iteration=3, verbose=false)
     run!(simulation)
 
     @test iteration(simulation) == 3
-    @test time(simulation) == 3minutes
+    @test time(simulation) ≈ FT(3minutes)
 
     return nothing
 end
@@ -21,41 +23,57 @@ end
     arch = GPU(roc)
 
     for FT in float_types
-        @info "    Testing HydrostaticFreeSurfaceModel on $arch with $FT"
+        @info "Testing grids on $arch with $FT..."
 
-        grid = RectilinearGrid(arch, FT, size=(4, 8, 16), x=[0, 1, 2, 3, 4], y=(0, 1), z=(0, 16))
+        regular_grid = RectilinearGrid(arch, FT, size=(4, 8, 16), x=(0, 4), y=(0, 1), z=(0, 16))
+        horizontally_stretched_grid = RectilinearGrid(arch, FT, size=(4, 8, 16), x=[0, 1, 2, 3, 4], y=(0, 1), z=(0, 16))
+        vertically_stretched_grid = RectilinearGrid(arch, FT, size=(16, 8, 4), x=(0, 16), y=(0, 1), z=[0, 1, 2, 3, 4])
 
-        @test parent(grid.xᶠᵃᵃ) isa ROCArray
-        @test parent(grid.xᶜᵃᵃ) isa ROCArray
-        @test eltype(grid) == FT
-        @test architecture(grid) isa GPU
+        @test parent(horizontally_stretched_grid.xᶠᵃᵃ) isa ROCArray
+        @test parent(horizontally_stretched_grid.xᶜᵃᵃ) isa ROCArray
+
+        @test parent(vertically_stretched_grid.z.cᵃᵃᶠ) isa ROCArray
+        @test parent(vertically_stretched_grid.z.cᵃᵃᶜ) isa ROCArray
+        @test parent(vertically_stretched_grid.z.Δᵃᵃᶠ) isa ROCArray
+        @test parent(vertically_stretched_grid.z.Δᵃᵃᶜ) isa ROCArray
+
+        for grid in (regular_grid, horizontally_stretched_grid, vertically_stretched_grid)
+            @test eltype(grid) == FT
+            @test architecture(grid) isa GPU
+        end
+
+        @info "Testing HydrostaticFreeSurfaceModel on $arch with $FT..."
 
         coriolis = FPlane(latitude=45)
         buoyancy = BuoyancyTracer()
         tracers = :b
         advection = WENO(order=5)
-        momentum_advection = tracer_advection = advection
 
-        pressure_solvers = (Oceananigans.Solvers.ConjugateGradientPoissonSolver(grid, maxiter=10; reltol=1e-7, abstol=1e-7, preconditioner=nothing),
-                            Oceananigans.Solvers.FFTBasedPoissonSolver(grid))
+        for grid in (regular_grid, horizontally_stretched_grid, vertically_stretched_grid)
+            momentum_advection = tracer_advection = advection
 
-        free_surface = SplitExplicitFreeSurface(grid; substeps=60)
+            free_surface = SplitExplicitFreeSurface(grid; substeps=60)
 
-        model = HydrostaticFreeSurfaceModel(; grid, free_surface,
-                                            coriolis, buoyancy, tracers,
-                                            momentum_advection, tracer_advection)
+            model = HydrostaticFreeSurfaceModel(; grid, free_surface,
+                                                coriolis, buoyancy, tracers,
+                                                momentum_advection, tracer_advection)
 
-        timestep_simulation(model)
+            build_and_timestep_simulation(model)
+        end
 
-        @info "    Testing NonhydrostaticModel on $arch with $FT"
+        @info "Testing NonhydrostaticModel on $arch with $FT..."
 
+        for grid in (regular_grid, vertically_stretched_grid)
+            pressure_solvers = (Oceananigans.Solvers.ConjugateGradientPoissonSolver(grid, maxiter=10; reltol=1e-7, abstol=1e-7, preconditioner=nothing),
+                                Oceananigans.Solvers.FFTBasedPoissonSolver(grid))
 
-        for pressure_solver in pressure_solvers
-            model = NonhydrostaticModel(; grid, pressure_solver,
-                                        coriolis, buoyancy,
-                                        tracers, advection)
+            for pressure_solver in pressure_solvers
+                model = NonhydrostaticModel(; grid, pressure_solver,
+                                            coriolis, buoyancy,
+                                            tracers, advection)
 
-            timestep_simulation(model)
+                build_and_timestep_simulation(model)
+            end
         end
     end
 end
