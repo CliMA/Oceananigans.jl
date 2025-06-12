066879356aa59c30a678773885f46c8a75752218
Michel Schanen <mschanen@anl.gov>
2025-06-12 09:42:01 -0500

    Populate AMDGPU extension

--- ext/OceananigansCUDAExt.jl
+++ ext/OceananigansCUDAExt.jl
@@ -3,6 +3,8 @@ module OceananigansCUDAExt
 using Oceananigans
 using InteractiveUtils
 using CUDA, CUDA.CUSPARSE, CUDA.CUFFT
+using Oceananigans.Utils: linear_expand, __linear_ndrange, MappedCompilerMetadata
+using KernelAbstractions: __dynamic_checkbounds, __iterspace
 using KernelAbstractions
 import Oceananigans.Architectures as AC
 import Oceananigans.BoundaryConditions as BC
@@ -19,7 +21,7 @@ import Oceananigans.DistributedComputations: Distributed
 const GPUVar = Union{CuArray, CuContext, CuPtr, Ptr}
 
 function __init__()
-    if CUDA.has_cuda()
+    if CUDA.functional()
         @debug "CUDA-enabled GPU(s) detected:"
         for (gpu, dev) in enumerate(CUDA.devices())
             @debug "$dev: $(CUDA.name(dev))"
@@ -56,8 +58,8 @@ AC.architecture(::CuArray) = CUDAGPU()
 AC.architecture(::CuSparseMatrixCSC) = CUDAGPU()
 AC.array_type(::AC.GPU{CUDABackend}) = CuArray
 
+AC.on_architecture(::CUDAGPU, a::Number) = a
 AC.on_architecture(::AC.CPU, a::CuArray) = Array(a)
-
 AC.on_architecture(::CUDAGPU, a::Array) = CuArray(a)
 AC.on_architecture(::CUDAGPU, a::CuArray) = a
 AC.on_architecture(::CUDAGPU, a::BitArray) = CuArray(a)
@@ -65,6 +67,8 @@ AC.on_architecture(::CUDAGPU, a::SubArray{<:Any, <:Any, <:CuArray}) = a
 AC.on_architecture(::CUDAGPU, a::SubArray{<:Any, <:Any, <:Array}) = CuArray(a)
 AC.on_architecture(::AC.CPU, a::SubArray{<:Any, <:Any, <:CuArray}) = Array(a)
 AC.on_architecture(::CUDAGPU, a::StepRangeLen) = a
+AC.on_architecture(arch::Distributed, a::CuArray) = AC.on_architecture(AC.child_architecture(arch), a)
+AC.on_architecture(arch::Distributed, a::SubArray{<:Any, <:Any, <:CuArray}) = AC.on_architecture(child_architecture(arch), a)
 
 # cu alters the type of `a`, so we convert it back to the correct type
 unified_array(::CUDAGPU, a::AbstractArray) = map(eltype(a), cu(a; unified = true))
@@ -86,13 +90,9 @@ end
 @inline AC.constructors(::AC.CPU, A::CuSparseMatrixCSC) = (A.dims[1], A.dims[2], Int64.(Array(A.colPtr)), Int64.(Array(A.rowVal)), Array(A.nzVal))
 @inline AC.constructors(::AC.GPU{CUDABackend}, A::CuSparseMatrixCSC) = (A.colPtr, A.rowVal, A.nzVal,  A.dims)
 
-@inline AC.unpack_constructors(::AC.CPU, constr::Tuple) = (constr[3], constr[4], constr[5])
-@inline AC.copy_unpack_constructors(::AC.CPU, constr::Tuple) = deepcopy((constr[3], constr[4], constr[5]))
-
 @inline AC.arch_sparse_matrix(::AC.GPU{CUDABackend}, constr::Tuple) = CuSparseMatrixCSC(constr...)
 @inline AC.arch_sparse_matrix(::AC.CPU, A::CuSparseMatrixCSC)   = SparseMatrixCSC(AC.constructors(AC.CPU(), A)...)
 @inline AC.arch_sparse_matrix(::AC.GPU{CUDABackend}, A::SparseMatrixCSC)     = CuSparseMatrixCSC(AC.constructors(AC.GPU(), A)...)
-
 @inline AC.arch_sparse_matrix(::AC.GPU{CUDABackend}, A::CuSparseMatrixCSC) = A
 
 @inline AC.convert_to_device(::CUDAGPU, args) = CUDA.cudaconvert(args)
@@ -119,10 +119,10 @@ end
 
 # CUDA version, the indices are passed implicitly
 # You must not use KA here as this code is executed in another scope
-CUDA.@device_override @inline function KernelAbstractions.__validindex(ctx::UT.MappedCompilerMetadata)
+CUDA.@device_override @inline function __validindex(ctx::MappedCompilerMetadata)
     if __dynamic_checkbounds(ctx)
-        index = @inbounds UT.linear_expand(__iterspace(ctx), blockIdx().x, threadIdx().x)
-        return index ≤ UT.__linear_ndrange(ctx)
+        index = @inbounds linear_expand(__iterspace(ctx), CUDA.blockIdx().x, CUDA.threadIdx().x)
+        return index ≤ __linear_ndrange(ctx)
     else
         return true
     end
@@ -134,7 +134,5 @@ end
 @inline UT.switch_device!(dev::CuDevice)            = device!(dev)
 @inline UT.sync_device!(::CUDAGPU)      = CUDA.synchronize()
 @inline UT.sync_device!(::CUDABackend)      = CUDA.synchronize()
-AC.on_architecture(arch::Distributed, a::CuArray) = AC.on_architecture(AC.child_architecture(arch), a)
-AC.on_architecture(arch::Distributed, a::SubArray{<:Any, <:Any, <:CuArray}) = AC.on_architecture(child_architecture(arch), a)
 
 end # module OceananigansCUDAExt
