0e0f32f3178ea4d62135aa4bc822103959e95f14
Sid <siddhartha.bishnu@gmail.com>
2025-06-13 16:11:54 +0100

    Resolve isbits type handling issue

--- src/Grids/orthogonal_spherical_shell_grid.jl
+++ src/Grids/orthogonal_spherical_shell_grid.jl
@@ -100,8 +100,10 @@ const OSSG = OrthogonalSphericalShellGrid
 const ZRegOSSG = OrthogonalSphericalShellGrid{<:Any, <:Any, <:Any, <:Any, <:RegularVerticalCoordinate}
 const ZRegOrthogonalSphericalShellGrid = ZRegOSSG
 
-@inline cpu_face_constructor_ξ(grid::OrthogonalSphericalShellGrid) = Array(grid.conformal_mapping.ξᶠᵃᵃ[1:size(grid, 1)+1])
-@inline cpu_face_constructor_η(grid::OrthogonalSphericalShellGrid) = Array(grid.conformal_mapping.ηᵃᶠᵃ[1:size(grid, 2)+1])
+@inline cpu_face_constructor_ξ(grid::OrthogonalSphericalShellGrid) = (grid.conformal_mapping.ξᶠᵃᵃ[1],
+                                                                      grid.conformal_mapping.ξᶠᵃᵃ[size(grid, 1)+1])
+@inline cpu_face_constructor_η(grid::OrthogonalSphericalShellGrid) = (grid.conformal_mapping.ηᵃᶠᵃ[1],
+                                                                      grid.conformal_mapping.ηᵃᶠᵃ[size(grid, 2)+1])
 
 # convenience constructor for OSSG without any conformal_mapping properties
 OrthogonalSphericalShellGrid(architecture, Nx, Ny, Nz, Hx, Hy, Hz, Lz,
