1e9a9db038ba1f05adfe19d4ac496546187a87a7
tomaschor <tomaschor@gmail.com>
2025-06-16 10:25:50 -0700

    reorganize test_poisson_solvers_immersed_boundaries

--- test/test_poisson_solvers_immersed_boundaries.jl
+++ test/test_poisson_solvers_immersed_boundaries.jl
@@ -10,12 +10,11 @@ using Oceananigans.Solvers: ConjugateGradientPoissonSolver
 topos = [(Periodic, Bounded,  Bounded),
          (Bounded,  Periodic, Bounded),
          (Bounded,  Bounded,  Bounded),
-         (Periodic, Periodic, Bounded)]
-
-two_dimensional_topologies = [(Flat,     Bounded,  Bounded),
-                              (Bounded,  Flat,     Bounded),
-                              (Flat,     Periodic, Bounded),
-                              (Periodic, Flat,     Bounded)]
+         (Periodic, Periodic, Bounded),
+         (Flat,     Bounded,  Bounded),
+         (Bounded,  Flat,     Bounded),
+         (Flat,     Periodic, Bounded),
+         (Periodic, Flat,     Bounded)]
 
 function make_random_immersed_grid(grid)
     Lz = grid.Lz
@@ -47,31 +46,30 @@ end
 default_Ns() = [7, 16]
 default_rect_Ns() = [11, 16]
 
-function test_divergence_free_solution(arch, topos, two_dimensional_topologies)
+function size_and_extent_from_topo(N, topo)
+    contains_flat = any(t -> t == Flat, topo)
+    # Assumes we'll have at most one instance of Flat (i.e., 2D or 3D grids only)
+    if contains_flat
+        return (; size=(N, N), extent=(1, 1))
+    else
+        return (; size=(N, N, N), extent=(1, 1, 1))
+    end
+end
+
+function test_divergence_free_solution(arch, float_type, topos)
     for topo in topos
+        @info "    Testing $topo topology on square grids [$(typeof(arch)), $float_type]..."
         for N in default_Ns()
-            grids_3d = [make_random_immersed_grid(RectilinearGrid(arch, topology=topo, size=(N, N, N), extent=(1, 1, 1))),
-                        make_random_immersed_grid(RectilinearGrid(arch, topology=topo, size=(1, N, N), extent=(1, 1, 1))),
-                        make_random_immersed_grid(RectilinearGrid(arch, topology=topo, size=(N, 1, N), extent=(1, 1, 1))),
-                        make_random_immersed_grid(RectilinearGrid(arch, topology=topo, size=(N, N, 1), extent=(1, 1, 1)))]
+            grid = make_random_immersed_grid(RectilinearGrid(arch, float_type, topology=topo; size_and_extent_from_topo(N, topo)...))
 
-            grids_2d = [make_random_immersed_grid(RectilinearGrid(arch, size=(N, N), extent=(1, 1), topology=topo))
-                        for topo in two_dimensional_topologies]
-
-            grids = []
-            push!(grids, grids_3d..., grids_2d...)
-
-            for grid in grids
-                N == 7 && @info "    Testing $(topology(grid)) topology on square grids [$(typeof(arch))]..."
-                ϕ, ∇²ϕ, R = compute_pressure_solution(grid)
-                @test CUDA.@allowscalar interior(∇²ϕ) ≈ interior(R)
-                @test isapprox(mean(ϕ), 0, atol=eps(eltype(grid)))
-            end
+            ϕ, ∇²ϕ, R = compute_pressure_solution(grid)
+            @test CUDA.@allowscalar interior(∇²ϕ) ≈ interior(R)
+            @test isapprox(mean(ϕ), 0, atol=eps(eltype(grid)))
         end
     end
 end
 
-function test_rectangular_grids(arch, topos)
+function test_divergence_free_solutions_on_rectangular_grids(arch, topos)
     Ns = default_rect_Ns()
     for topo in topos
         @info "    Testing $topo topology on rectangular grids with even and prime sizes [$(typeof(arch))]..."
@@ -84,24 +82,13 @@ function test_rectangular_grids(arch, topos)
     end
 end
 
-function test_float32_grids(arch)
-    Float32_grids = [make_random_immersed_grid(RectilinearGrid(arch, Float32, topology=(Periodic, Bounded, Bounded), size=(16, 16, 16), extent=(1, 1, 1))),
-                     make_random_immersed_grid(RectilinearGrid(arch, Float32, topology=(Bounded, Bounded, Bounded), size=(7, 11, 13), extent=(1, 1, 1)))]
-    for grid in Float32_grids
-        ϕ, ∇²ϕ, R = compute_pressure_solution(grid)
-        @test CUDA.@allowscalar interior(∇²ϕ) ≈ interior(R)
-        @test isapprox(mean(ϕ), 0, atol=eps(eltype(grid)))
-    end
-end
-
 @testset "Poisson solvers immersed" begin
     @info "Testing immersed Poisson solvers..."
-    for arch in archs
-        @testset "Divergence-free solution [$(typeof(arch))]" begin
-            @info "  Testing divergence-free solution [$(typeof(arch))]..."
-            test_divergence_free_solution(arch, topos, two_dimensional_topologies)
-            test_rectangular_grids(arch, topos)
-            test_float32_grids(arch)
+    for arch in archs, float_type in float_types
+        @testset "Divergence-free solution [$(typeof(arch)), $float_type]" begin
+            @info "  Testing divergence-free solution [$(typeof(arch)), $float_type]..."
+            test_divergence_free_solution(arch, float_type, topos)
+            test_divergence_free_solutions_on_rectangular_grids(arch, topos)
         end
     end
 end
