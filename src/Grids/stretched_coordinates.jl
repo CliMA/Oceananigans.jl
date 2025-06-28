57255f171b522bbd1c02b85981fb117ed7ffc446
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-06-28 12:57:06 +1000

    add stretched_coordinates

---
+++ src/Grids/stretched_coordinates.jl
@@ -0,0 +1,108 @@
+@inline rightbiased_exponential_mapping(z, l, r, h) = @. r - (r - l) * expm1((r - z) / h) / expm1((r - l) / h)
+@inline leftbiased_exponential_mapping(z, l, r, h)  = @. l + (r - l) * expm1((z - l) / h) / expm1((r - l) / h)
+
+struct ExponentialCoordinate <: Function
+    size :: Int
+    left :: Float64
+    right :: Float64
+    scale :: Float64
+    bias :: Symbol
+end
+
+"""
+    ExponentialCoordinate(size::Int, left, right=0; scale=(right-left)/5, bias=:right)
+
+Return a type that describes a one-dimensional coordinate with `size` cells that
+are exponentially spaced (or, equivalently, with spacings that grow linearly).
+
+The coordinate spans the range [`left`, `right`]. The exponential e-folding is controlled by `scale`.
+The coordinate interfaces are stacked on the `bias`-side of the domain.
+
+Arguments
+=========
+- `size`: The number of cells in the coordinate.
+- `left`: The left-most interface of the coordinate.
+- `right`: The right-most interface of the coordinate.
+
+Keyword Arguments
+=================
+- `scale`: The length scale of the exponential e-folding. Default: `(right - left) / 5`
+- `bias :: Symbol`: Determine whether left or right biased. Default: `:right`.
+
+Examples
+========
+
+```jldoctest ExponentialCoordinate
+using ClimaOcean
+
+Nz = 10
+left = -1000
+right = 100
+
+z = ExponentialCoordinate(Nz, left, right)
+
+[z(k) for k in 1:Nz+1]
+
+# output
+
+11-element Vector{Float64}:
+ -1000.0
+  -564.247649441104
+  -299.95048878528615
+  -139.64615757253702
+   -42.41666580727582
+    16.55600197663209
+    52.324733072619736
+    74.0195651413529
+    87.17814594835643
+    95.15922864611028
+   100.0
+```
+
+Above, the default `bias` is `:right`. We can get a left-biased grid via:
+
+```jldoctest ExponentialCoordinate
+z = ExponentialCoordinate(Nz, left, right, bias=:left)
+
+[z(k) for k in 1:Nz+1]
+
+# output
+
+11-element Vector{Float64}:
+ -1000.0
+  -995.1592286461103
+  -987.1781459483565
+  -974.0195651413529
+  -952.3247330726198
+  -916.556001976632
+  -857.5833341927241
+  -760.353842427463
+  -600.0495112147139
+  -335.75235055889596
+   100.0
+```
+"""
+ExponentialCoordinate(size::Int, left, right; scale=(right-left)/5, bias=:right) =
+    ExponentialCoordinate(size, left, right, scale, bias)
+
+function (g::ExponentialCoordinate)(k)
+    Nz, left, right, scale = g.size, g.left, g.right, g.scale
+
+    # uniform coordinate
+    ξₖ = left + (k-1) * (right - left) / Nz
+
+    # mapped coordinate
+    if g.bias === :right
+       zₖ = rightbiased_exponential_mapping(ξₖ, left, right, scale)
+    elseif g.bias === :left
+       zₖ = leftbiased_exponential_mapping(ξₖ, left, right, scale)
+    end
+
+    if abs(zₖ - left) < 10eps(Float32)
+        zₖ = left
+    elseif abs(zₖ - right) < 10eps(Float32)
+        zₖ = right
+    end
+
+    return zₖ
+end
