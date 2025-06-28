cda750f02711303ab1508dcdb4157b3868d1e500
Gregory L. Wagner <wagner.greg@gmail.com>
2025-06-28 12:57:49 -0600

    Example with `TEOS10EquationOfState` + "single wrapped" `*nodes` (#4615)
    * Update ocean_wind_mixing_and_convection.jl
    
    * needed @allowscalar
    
    could it be avoided?
    
    * linear EOS -> TEOS10
    
    * Update ocean_wind_mixing_and_convection.jl
    
    * Update examples/ocean_wind_mixing_and_convection.jl
    
    * Update ocean_wind_mixing_and_convection.jl
    
    * fixes
    
    * Update ext/OceananigansMakieExt.jl
    
    * Dont compute! twice
    
    * Fix *nodes
    
    * Move alias def to top level
    
    * Fix tests and docstrings
    
    * simplify visualization
    
    * fix Makie's Observable link
    
    * Pin Reactant
    
    * fix docstests
    
    * Update test_conjugate_gradient_poisson_solver.jl
    
    * set_theme!
    
    ---------
    
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>

--- src/Grids/vertical_discretization.jl
+++ src/Grids/vertical_discretization.jl
@@ -198,8 +198,8 @@ end
 @inline znode(k, grid, ℓz) = rnode(k, grid, ℓz)
 @inline znode(i, j, k, grid, ℓx, ℓy, ℓz) = rnode(i, j, k, grid, ℓx, ℓy, ℓz)
 
-@inline rnodes(grid::AUG, ℓz::Face;   with_halos=false) = _property(grid.z.cᵃᵃᶠ, ℓz, topology(grid, 3), size(grid, 3), with_halos)
-@inline rnodes(grid::AUG, ℓz::Center; with_halos=false) = _property(grid.z.cᵃᵃᶜ, ℓz, topology(grid, 3), size(grid, 3), with_halos)
+@inline rnodes(grid::AUG, ℓz::F; with_halos=false) = _property(grid.z.cᵃᵃᶠ, ℓz, topology(grid, 3), grid.Nz, grid.Hz, with_halos)
+@inline rnodes(grid::AUG, ℓz::C; with_halos=false) = _property(grid.z.cᵃᵃᶜ, ℓz, topology(grid, 3), grid.Nz, grid.Hz, with_halos)
 @inline rnodes(grid::AUG, ℓx, ℓy, ℓz; with_halos=false) = rnodes(grid, ℓz; with_halos)
 
 rnodes(grid::AUG, ::Nothing; kwargs...) = 1:1
