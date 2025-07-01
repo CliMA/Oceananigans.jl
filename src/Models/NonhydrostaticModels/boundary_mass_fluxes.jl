2aeb88ca052b43c56de459253b3f335cd50d3e0b
tomaschor <tomaschor@gmail.com>
2025-07-01 11:48:11 -0700

    move functions to boundary_mass_fluxes.jl

--- src/Models/NonhydrostaticModels/boundary_mass_fluxes.jl
+++ src/Models/NonhydrostaticModels/boundary_mass_fluxes.jl
@@ -2,7 +2,10 @@ using Oceananigans.BoundaryConditions: BoundaryCondition, Open, PerturbationAdve
 using Oceananigans.AbstractOperations: Average
 using Oceananigans.Fields: Field
 
-const OBC  = BoundaryCondition{<:Open} # OpenBoundaryCondition (with no matching scheme)
+const OBC  = BoundaryCondition{<:Open} # OpenBoundaryCondition
+const MatchingScheme = Union{FlatExtrapolation, PerturbationAdvection}
+const ROBC = BoundaryCondition{<:Open{<:MatchingScheme}} # Radiation OpenBoundaryCondition
+const FOBC = BoundaryCondition{<:Open{<:Nothing}} # "Fixed-velocity" OpenBoundaryCondition (with no matching scheme)
 
 # Left boundary averages for normal velocity components
 west_average(u)   = Field(Average(view(u, 1, :, :), dims=(2, 3)))[]
@@ -65,4 +68,94 @@ function initialize_boundary_mass_fluxes(velocities::NamedTuple)
     end
 
     return boundary_fluxes
+end
+
+function gather_boundary_fluxes(model)
+
+    velocities = model.velocities
+    grid = model.grid
+
+    # Get the boundary conditions for the velocities
+    u_bcs = velocities.u.boundary_conditions
+    v_bcs = velocities.v.boundary_conditions
+    w_bcs = velocities.w.boundary_conditions
+
+    # Collect left and right ROBC boundary conditions into separate lists
+    left_ROBCs = Symbol[]
+    right_ROBCs = Symbol[]
+
+    # Initialize fluxes to zero
+    left_flux = zero(grid)
+    right_flux = zero(grid)
+
+    # Calculate flux through left boundaries
+    if u_bcs.west isa OBC
+        left_flux += west_average(velocities.u)
+        u_bcs.west isa ROBC && push!(left_ROBCs, :west)
+    end
+    if v_bcs.south isa OBC
+        left_flux += south_average(velocities.v)
+        v_bcs.south isa ROBC && push!(left_ROBCs, :south)
+    end
+    if w_bcs.bottom isa OBC
+        left_flux += bottom_average(velocities.w)
+        w_bcs.bottom isa ROBC && push!(left_ROBCs, :bottom)
+    end
+
+    # Calculate flux through right boundaries
+    if u_bcs.east isa OBC
+        right_flux += east_average(velocities.u)
+        u_bcs.east isa ROBC && push!(right_ROBCs, :east)
+    end
+    if v_bcs.north isa OBC
+        right_flux += north_average(velocities.v)
+        v_bcs.north isa ROBC && push!(right_ROBCs, :north)
+    end
+    if w_bcs.top isa OBC
+        right_flux += top_average(velocities.w)
+        w_bcs.top isa ROBC && push!(right_ROBCs, :top)
+    end
+
+    # Calculate total flux (positive means net inflow)
+    total_flux = left_flux - right_flux
+
+    return total_flux, left_ROBCs, right_ROBCs
+end
+
+"""
+correct_boundary_mass_flux!(model::NonhydrostaticModel)
+
+Correct boundary mass fluxes for perturbation advection boundary conditions to ensure
+zero net mass flux through each boundary.
+"""
+function correct_boundary_mass_flux!(model)
+    velocities = model.velocities
+    grid = model.grid
+
+    total_flux, left_ROBCs, right_ROBCs = gather_boundary_fluxes(model)
+
+    # Calculate flux correction per boundary
+    extra_flux_per_boundary = total_flux / (length(left_ROBCs) + length(right_ROBCs))
+
+    # Subtract extra flux from left boundaries to reduce inflow
+    for bc in left_ROBCs
+        if bc == :west
+            velocities.u[1, :, :] = velocities.u[1, :, :] .- extra_flux_per_boundary
+        elseif bc == :south
+            velocities.v[:, 1, :] = velocities.v[:, 1, :] .- extra_flux_per_boundary
+        elseif bc == :bottom
+            velocities.w[:, :, 1] = velocities.w[:, :, 1] .- extra_flux_per_boundary
+        end
+    end
+
+    # Add extra flux to right boundaries to increase outflow
+    for bc in right_ROBCs
+        if bc == :east
+            velocities.u[grid.Nx + 1, :, :] = velocities.u[grid.Nx + 1, :, :] .+ extra_flux_per_boundary
+        elseif bc == :north
+            velocities.v[:, grid.Ny + 1, :] = velocities.v[:, grid.Ny + 1, :] .+ extra_flux_per_boundary
+        elseif bc == :top
+            velocities.w[:, :, grid.Nz + 1] = velocities.w[:, :, grid.Nz + 1] .+ extra_flux_per_boundary
+        end
+    end
 end
