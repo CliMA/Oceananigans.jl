f9a16a36b9eb6fbc8582c9c1d6a8fa8ca49d3c2b
tomaschor <tomaschor@gmail.com>
2025-07-17 17:08:55 +0000

    rename validation script

--- validation/open_boundaries/2d_flow_over_bathymetry.jl
+++ validation/open_boundaries/2d_flow_over_bathymetry.jl
@@ -3,15 +3,10 @@ using Oceananigans.Units
 using Oceananigans.BoundaryConditions: PerturbationAdvectionOpenBoundaryCondition, OpenBoundaryCondition
 using Oceananigans.Diagnostics: AdvectiveCFL
 using Oceananigans.Solvers: ConjugateGradientPoissonSolver, FFTBasedPoissonSolver, DiagonallyDominantPreconditioner, fft_poisson_solver
-using Oceananigans.AbstractOperations: ∂x, ∂z, Average
 
 # using GLMakie
-using GLMakie: Figure, heatmap!, Colorbar, Axis, recordframe!, VideoStream
+using GLMakie: Figure, heatmap!, Colorbar, Axis, recordframe!, VideoStream, save
 using Printf
-using Test
-using BenchmarkTools
-using Random: seed!
-seed!(156)
 
 function create_mass_conservation_simulation(; 
     use_open_boundary_condition = true,
@@ -29,13 +24,12 @@ function create_mass_conservation_simulation(;
     timestepper = :QuasiAdamsBashforth2,
     animation = false,
     animation_framerate = 12)
-    # Create underlying grid
+
+    #+++ Choose grid and pressure solver
     underlying_grid = RectilinearGrid(arch, topology = (Bounded, Flat, Bounded),
                                       size = (N, N),
                                       extent = (Lx, Lz),
                                       halo = (4, 4))
-
-    # Choose grid type based on immersed_bottom parameter
     grid = immersed_bottom isa Nothing ? underlying_grid : ImmersedBoundaryGrid(underlying_grid, immersed_bottom)
 
     if poisson_solver isa Nothing
@@ -53,18 +47,21 @@ function create_mass_conservation_simulation(;
     else
         error("Unknown poisson_solver option: $poisson_solver")
     end
+    #---
 
-    # Set boundary conditions based on boolean flag
+    #+++ Set boundary conditions
     if use_open_boundary_condition
         u_boundary_conditions = FieldBoundaryConditions(
-            west = OpenBoundaryCondition(U₀),
+            west = PerturbationAdvectionOpenBoundaryCondition(U₀; inflow_timescale, outflow_timescale),
             east = PerturbationAdvectionOpenBoundaryCondition(U₀; inflow_timescale, outflow_timescale)
         )
         boundary_conditions = (; u = u_boundary_conditions)
     else
         boundary_conditions = NamedTuple()
     end
+    #---
 
+    #+++ Create model and simulation
     model = NonhydrostaticModel(; grid, boundary_conditions, pressure_solver, timestepper, advection = WENO(order=5))
     uᵢ(x, z) = U₀ + U₀ * 1e-2 * rand()
     set!(model, u=uᵢ)
@@ -73,6 +70,7 @@ function create_mass_conservation_simulation(;
     Δt = 0.1 * minimum_zspacing(grid) / abs(maximum(model.velocities.u))
     simulation = Simulation(model; Δt, stop_time, verbose=false)
     conjure_time_step_wizard!(simulation, IterationInterval(1), cfl=0.1)
+    #---
 
     #+++ Progress Messenger
     if add_progress_messenger
@@ -102,8 +100,7 @@ function create_mass_conservation_simulation(;
         # Create figure and axes
         global fig = Figure(size = (1500, 400))
         global io = VideoStream(fig; framerate = animation_framerate)
-        time_str = @sprintf("t = %s", prettytime(time(simulation)))
-        #fig[0, 1:3] = Label(fig, time_str, fontsize=18, tellwidth=false)
+
         ax1 = Axis(fig[1, 1], title = "u-velocity", xlabel = "x", ylabel = "z")
         ax2 = Axis(fig[1, 2], title = "Divergence", xlabel = "x", ylabel = "z")
         ax3 = Axis(fig[1, 3], title = "y-Vorticity", xlabel = "x", ylabel = "z")
@@ -120,8 +117,8 @@ function create_mass_conservation_simulation(;
             compute!(ωy)
 
             hm1 = heatmap!(ax1, u, colormap = :balance, colorrange = (-5U₀, 5U₀))
-            hm2 = heatmap!(ax2, ∇u, colormap = :balance, colorrange = (-1e-10, 1e-10))
-            hm3 = heatmap!(ax3, ωy, colormap = :balance, colorrange = (-U₀, U₀))
+            hm2 = heatmap!(ax2, ∇u, colormap = :balance, colorrange = (-1e-9, 1e-9))
+            hm3 = heatmap!(ax3, ωy, colormap = :balance, colorrange = (-U₀/10, U₀/10))
             if time(sim) == 0
                 Colorbar(fig[2, 1], hm1, vertical=false)
                 Colorbar(fig[2, 2], hm2, vertical=false)
@@ -137,19 +134,28 @@ function create_mass_conservation_simulation(;
     return simulation
 end
 
+# using BenchmarkTools
+# simulation = create_mass_conservation_simulation(; use_open_boundary_condition = true, common_kwargs...);
+# b1 = @benchmark time_step!(simulation)
+
+# simulation = create_mass_conservation_simulation(; use_open_boundary_condition = false, common_kwargs...);
+# b2 = @benchmark time_step!(simulation)
+
 Lx = 700meters
 Lz = 600meters
-bottom(x) = -500meters + (Lz/4) * exp(-(x-Lx/2)^2 / (2 * (Lx/20))^2)
-common_kwargs = (; arch = CPU(),
+bottom(x) = -(3Lz/4) + (Lz/4) * sin(2π * x / (Lx/3))
+common_kwargs = (; arch = GPU(),
                    immersed_bottom = GridFittedBottom(bottom),
                    Lx,
                    Lz,
                    stop_time = 0.5day,
                    U₀ = 0.1,
+                   inflow_timescale = 1minute,
+                   outflow_timescale = 10minutes,
                    poisson_solver = :conjugate_gradient_with_fft_preconditioner ,
                    add_progress_messenger = true,
                    timestepper = :RungeKutta3)
 
 simulation = create_mass_conservation_simulation(; use_open_boundary_condition = true, animation = true, common_kwargs...);
 run!(simulation)
-save("mass_conservation.mp4", io)
+save("2d_flow_over_bathymetry.mp4", io)
