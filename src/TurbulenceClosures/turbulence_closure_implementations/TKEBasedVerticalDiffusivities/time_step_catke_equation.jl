a061b7f426c977a0b5ab895700c5f8a2bab408e1
Simone Silvestri <silvestri.simone0@gmail.com>
2025-08-08 15:44:37 +0200

    go for it

--- src/TurbulenceClosures/turbulence_closure_implementations/TKEBasedVerticalDiffusivities/time_step_catke_equation.jl
+++ src/TurbulenceClosures/turbulence_closure_implementations/TKEBasedVerticalDiffusivities/time_step_catke_equation.jl
@@ -146,10 +146,103 @@ const c = Center()
     @inbounds κe[i, j, k] = κe★
 end
 
-@kernel function substep_turbulent_kinetic_energy!(Le, grid, closure,
-                                                   next_velocities, previous_velocities,
-                                                   tracers, buoyancy, diffusivities,
-                                                   Δt, γⁿ, ζⁿ, slow_Gⁿ, e⁻)
+@kernel function _ab2_substep_turbulent_kinetic_energy!(Le, grid, closure,
+                                                        next_velocities, previous_velocities,
+                                                        tracers, buoyancy, diffusivities,
+                                                        Δτ, χ, slow_Gⁿe, G⁻e)
+
+    i, j, k = @index(Global, NTuple)
+
+    e = tracers.e
+    closure_ij = getclosure(i, j, closure)
+
+    # Compute additional diagonal component of the linear TKE operator
+    wb = explicit_buoyancy_flux(i, j, k, grid, closure_ij, next_velocities, tracers, buoyancy, diffusivities)
+    wb⁻ = min(zero(grid), wb)
+    wb⁺ = max(zero(grid), wb)
+
+    eⁱʲᵏ = @inbounds e[i, j, k]
+    eᵐⁱⁿ = closure_ij.minimum_tke
+    wb⁻_e = wb⁻ / eⁱʲᵏ * (eⁱʲᵏ > eᵐⁱⁿ)
+
+    # Treat the divergence of TKE flux at solid bottoms implicitly.
+    # This will damp TKE near boundaries. The bottom-localized TKE flux may be written
+    #
+    #       ∂t e = - δ(z + h) ∇ ⋅ Jᵉ + ⋯
+    #       ∂t e = + δ(z + h) Jᵉ / Δz + ⋯
+    #
+    # where δ(z + h) is a δ-function that is 0 everywhere except adjacent to the bottom boundary
+    # at $z = -h$ and Δz is the grid spacing at the bottom
+    #
+    # Thus if
+    #
+    #       Jᵉ ≡ - Cᵂϵ * √e³
+    #          = - (Cᵂϵ * √e) e
+    #
+    # Then the contribution of Jᵉ to the implicit flux is
+    #
+    #       Lᵂ = - Cᵂϵ * √e / Δz.
+
+    on_bottom = bottommost_active_node(i, j, k, grid, c, c, c)
+    active = !inactive_cell(i, j, k, grid)
+    Δz = Δzᶜᶜᶜ(i, j, k, grid)
+    Cᵂϵ = closure_ij.turbulent_kinetic_energy_equation.Cᵂϵ
+    e⁺ = clip(eⁱʲᵏ)
+    w★ = sqrt(e⁺)
+    div_Jᵉ_e = - on_bottom * Cᵂϵ * w★ / Δz
+
+    # Implicit TKE dissipation
+    ω = dissipation_rate(i, j, k, grid, closure_ij, next_velocities, tracers, buoyancy, diffusivities)
+
+    # The interior contributions to the linear implicit term `L` are defined via
+    #
+    #       ∂t e = Lⁱ e + ⋯,
+    #
+    # So
+    #
+    #       Lⁱ e = wb - ϵ
+    #            = (wb / e - ω) e,
+    #               ↖--------↗
+    #                  = Lⁱ
+    #
+    # where ω = ϵ / e ∼ √e / ℓ.
+
+    @inbounds Le[i, j, k] = (wb⁻_e - ω + div_Jᵉ_e) * active
+
+    # Compute fast TKE RHS
+    u⁺ = next_velocities.u
+    v⁺ = next_velocities.v
+    uⁿ = previous_velocities.u
+    vⁿ = previous_velocities.v
+    κu = diffusivities.κu
+
+    # TODO: correctly handle closure / diffusivity tuples
+    # TODO: the shear_production is actually a slow term so we _could_ precompute.
+    P = shear_production(i, j, k, grid, κu, uⁿ, u⁺, vⁿ, v⁺)
+    ϵ = dissipation(i, j, k, grid, closure_ij, next_velocities, tracers, buoyancy, diffusivities)
+    fast_Gⁿe = P + wb⁺ - ϵ
+
+    # Advance TKE and store tendency
+    FT = eltype(χ)
+    Δτ = convert(FT, Δτ)
+
+    # See below.    
+    α = convert(FT, 1.5) + χ
+    β = convert(FT, 0.5) + χ
+    σᶜᶜⁿ = σⁿ(i, j, k, grid, Center(), Center(), Center())
+    σᶜᶜ⁻ = σ⁻(i, j, k, grid, Center(), Center(), Center())
+
+    @inbounds begin
+        total_Gⁿe = slow_Gⁿe[i, j, k] + fast_Gⁿe * σᶜᶜⁿ
+        e[i, j, k] += Δτ * (α * total_Gⁿe - β * G⁻e[i, j, k]) * active / σᶜᶜⁿ
+        G⁻e[i, j, k] = total_Gⁿe * active
+    end
+end
+
+@kernel function _rk3_substep_turbulent_kinetic_energy!(Le, grid, closure,
+                                                        next_velocities, previous_velocities,
+                                                        tracers, buoyancy, diffusivities,
+                                                        Δt, γⁿ, ζⁿ, slow_Gⁿ, e⁻)
 
     i, j, k = @index(Global, NTuple)
 
