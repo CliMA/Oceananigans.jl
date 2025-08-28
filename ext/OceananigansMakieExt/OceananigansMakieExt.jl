50028f6c9a60ae1f951bcbf57a67cfd8d2083a97
Sid <siddhartha.bishnu@gmail.com>
2025-08-28 16:56:23 +0100

    Add slightly tweaked Imaginocean functions

--- ext/OceananigansMakieExt/OceananigansMakieExt.jl
+++ ext/OceananigansMakieExt/OceananigansMakieExt.jl
@@ -3,9 +3,11 @@ module OceananigansMakieExt
 include("DimensionalityUtils.jl")
 include("MakieConversions.jl")
 include("PlotExtensions.jl")
+include("Imaginocean.jl")
 
 using .DimensionalityUtils
 using .MakieConversions
 using .PlotExtensions
+using .Imaginocean
 
 end # module OceananigansMakieExt
