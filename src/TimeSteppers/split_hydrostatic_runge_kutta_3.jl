bc8fab40e2324112f2069731eb1e5811e3f691e4
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-08-30 07:18:25 +1000

    update citations for SRK4

--- src/TimeSteppers/split_hydrostatic_runge_kutta_3.jl
+++ src/TimeSteppers/split_hydrostatic_runge_kutta_3.jl
@@ -2,7 +2,7 @@ using Oceananigans.Architectures: architecture
 using Oceananigans: fields
 
 """
-    SplitRungeKutta3TimeStepper{FT, TG, TE, PF, TI} <: AbstractTimeStepper
+    SplitRungeKutta3TimeStepper{FT, TG, PF, TI} <: AbstractTimeStepper
 
 Hold parameters and tendency fields for a low storage, third-order Runge-Kutta-Wray
 time-stepping scheme described by [Lan et al. (2022)](@cite Lan2022).
@@ -19,14 +19,14 @@ end
     SplitRungeKutta3TimeStepper(grid, prognostic_fields, args...;
                                 implicit_solver::TI = nothing,
                                 Gⁿ::TG = map(similar, prognostic_fields),
-                                Ψ⁻::PF = map(similar, prognostic_fields)
-                                G⁻::TE = nothing) where {TI, TG, PF, TE}
+                                Ψ⁻::PF = map(similar, prognostic_fields),
+                                kwargs...) where {TI, TG, PF}
 
 Return a 3rd-order `SplitRungeKutta3TimeStepper` on `grid` and with `tracers`.
-The tendency fields `Gⁿ` and `G⁻`, and the previous state ` Ψ⁻` can be modified
+The tendency fields `Gⁿ` and `G⁻`, and the previous state `Ψ⁻` can be modified
 via optional `kwargs`.
 
-The scheme is described by [Lan et al. (2022)](@cite Lan2022). In a nutshell,
+The scheme is described by [Knoth and Wensch (2014)](@cite Knoth-Wensch-2014). In a nutshell,
 the 3rd-order Runge-Kutta timestepper steps forward the state `Uⁿ` by `Δt` via 3 substeps.
 A barotropic velocity correction step is applied after at each substep.
 
@@ -46,8 +46,8 @@ The state at the first substep is taken to be the one that corresponds to the ``
 References
 ==========
 
-Knoth, O., and Wensch, J. (2014). Generalized Split-Explicit Runge-Kutta Methods for the Compressible Euler Equations. 
-    Monthly Weather Review, 142, 2067-2081, 
+Knoth, O., and Wensch, J. (2014). Generalized Split-Explicit Runge-Kutta Methods for the
+    compressible Euler equations. Monthly Weather Review, 142, 2067-2081,
     https://doi.org/10.1175/MWR-D-13-00068.1.
 """
 function SplitRungeKutta3TimeStepper(grid, prognostic_fields, args...;
@@ -62,7 +62,7 @@ function SplitRungeKutta3TimeStepper(grid, prognostic_fields, args...;
     FT = eltype(grid)
     β¹ = 3
     β² = 2
-    
+
     return SplitRungeKutta3TimeStepper{FT, TG, PF, TI}(β¹, β², Gⁿ, Ψ⁻, implicit_solver)
 end
 
