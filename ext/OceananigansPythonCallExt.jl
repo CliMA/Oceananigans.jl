f4358ec96b27f112311d4c24e7cfd254ab37d36c
Gregory Wagner <wagner.greg@gmail.com>
2025-09-12 02:09:27 -0700

    switch to non-xarray input

--- ext/OceananigansPythonCallExt.jl
+++ ext/OceananigansPythonCallExt.jl
@@ -33,62 +33,101 @@ function add_import_pkg(name, channel="conda-forge")
     return pkg
 end
 
-function regridding_weights(dst_field, src_field; method="conservative")
-    xesmf = add_import_pkg("xesmf")
-    numpy = add_import_pkg("numpy")
+flip(::Center) = Face()
+flip(::Face) = Center()
+flip(::Nothing) = nothing
+
+function coordinate_data_arrays(λ, φ)
     xarray = add_import_pkg("xarray")
 
+    λ_da = xarray.DataArray(
+        λ',
+        dims=["y", "x"],
+        coords= Dict(
+            "lat" => (["y", "x"], φ'),
+            "lon" => (["y", "x"], λ')
+        ),
+        name="longitude"
+    )
+
+    φ_da = xarray.DataArray(
+        φ',
+        dims=["y", "x"],
+        coords= Dict(
+            "lat" => (["y", "x"], φ'),
+            "lon" => (["y", "x"], λ')
+        ),
+        name="latitude"
+    )
+
+    return λ_da, φ_da
+end
+
+function regridding_weights(dst_field, src_field; method="conservative")
+
     # Extract center coordinates from both fields
-    λd = λnodes(dst_field)
-    φd = φnodes(dst_field)
-    λs = λnodes(src_field)
-    φs = φnodes(src_field)
+    λᵈ = λnodes(dst_field)
+    φᵈ = φnodes(dst_field)
+    λˢ = λnodes(src_field)
+    φˢ = φnodes(src_field)
 
     # Extract boundary coordinates
-    
+    dst_loc = Oceananigans.Fields.instantiated_location(dst_field)
+    src_loc = Oceananigans.Fields.instantiated_location(src_field)
+    flipped_dst_loc = (flip(dst_loc[1]), flip(dst_loc[2]), dst_loc[3])
+    flipped_src_loc = (flip(src_loc[1]), flip(src_loc[2]), src_loc[3])
 
-    λd_b = λnodes(dst_field.grid, Face(), Face(), Center())
-    φd_b = φnodes(dst_field.grid, Face(), Face(), Center())
-    λs_b = λnodes(src_field.grid, Face(), Face(), Center())
-    φs_b = φnodes(src_field.grid, Face(), Face(), Center())
+    λᵈᵇ = λnodes(dst_field.grid, flipped_dst_loc...)
+    φᵈᵇ = φnodes(dst_field.grid, flipped_dst_loc...)
+    λˢᵇ = λnodes(src_field.grid, flipped_src_loc...)
+    φˢᵇ = φnodes(src_field.grid, flipped_src_loc...)
 
     # Ensure coordinates are on CPU
-    λd = on_architecture(CPU(), λd)
-    φd = on_architecture(CPU(), φd)
-    λs = on_architecture(CPU(), λs)
-    φs = on_architecture(CPU(), φs)
-    λd_b = on_architecture(CPU(), λd_b)
-    φd_b = on_architecture(CPU(), φd_b)
-    λs_b = on_architecture(CPU(), λs_b)
-    φs_b = on_architecture(CPU(), φs_b)
+    λᵈ = on_architecture(CPU(), λᵈ)
+    φᵈ = on_architecture(CPU(), φᵈ)
+    λˢ = on_architecture(CPU(), λˢ)
+    φˢ = on_architecture(CPU(), φˢ)
 
+    λᵈᵇ = on_architecture(CPU(), λᵈᵇ)
+    φᵈᵇ = on_architecture(CPU(), φᵈᵇ)
+    λˢᵇ = on_architecture(CPU(), λˢᵇ)
+    φˢᵇ = on_architecture(CPU(), φˢᵇ)
+
+    #=
     # Convert 1D coordinates to 2D if needed
-    if ndims(λd) == 1
-        λd = repeat(λd', size(φd, 1))
-        φd = repeat(φd, 1, size(λd, 2))
+    if ndims(λᵈ) == 1
+        λᵈ = repeat(λᵈ, 1, size(φᵈ, 1))
+        φᵈ = repeat(φᵈ, size(λᵈ, 1), 1)
+        λᵈᵇ = repeat(λᵈᵇ, 1, size(φᵈᵇ, 1))
+        φᵈᵇ = repeat(φᵈᵇ, size(λᵈᵇ, 1), 1)
     end
 
-    if ndims(λs) == 1
-        λs = repeat(λs', size(φs, 1))
-        φs = repeat(φs, 1, size(λs, 2))
+    if ndims(λˢ) == 1
+        λˢ = repeat(λˢ, 1, size(φˢ, 1))
+        φˢ = repeat(φˢ, size(λˢ, 1), 1)
+        λˢᵇ = repeat(λˢᵇ, 1, size(φˢᵇ, 1))
+        φˢᵇ = repeat(φˢᵇ, size(λˢᵇ, 1), 1)
     end
+    =#
 
-    if ndims(λd_b) == 1
-        λd_b = repeat(λd_b', size(φd_b, 1))
-        φd_b = repeat(φd_b, 1, size(λd_b, 2))
-    end
+    # λᵈx,  φᵈx  = coordinate_data_arrays(λᵈ, φᵈ)
+    # λˢx,  φˢx  = coordinate_data_arrays(λˢ, φˢ)
+    # λᵈᵇx, φᵈᵇx = coordinate_data_arrays(λᵈᵇ, φᵈᵇ)
+    # λˢᵇx, φˢᵇx = coordinate_data_arrays(λˢᵇ, φˢᵇ)
 
-    if ndims(λs_b) == 1
-        λs_b = repeat(λs_b', size(φs_b, 1))
-        φs_b = repeat(φs_b, 1, size(λs_b, 2))
-    end
+    dst_coordinates = Dict("lat"   => λᵈ, 
+                           "lon"   => φᵈ,
+                           "lat_b" => λᵈᵇ,
+                           "lon_b" => φᵈᵇ)
+        
 
-    # Create coordinate datasets
-    src_ds = structured_coordinate_dataset(φs, λs, φs_b, λs_b, numpy, xarray)
-    dst_ds = structured_coordinate_dataset(φd, λd, φd_b, λd_b, numpy, xarray)
-
-    # Create regridder and extract weights
-    regridder = xesmf.Regridder(src_ds, dst_ds, method, periodic=pytrue)
+    src_coordinates = Dict("lat"   => λˢ, 
+                           "lon"   => φˢ,
+                           "lat_b" => λˢᵇ,
+                           "lon_b" => φˢᵇ)
+        
+    xesmf = add_import_pkg("xesmf")
+    regridder = xesmf.Regridder(src_coordinates, dst_coordinates, method)
     
     return nothing
 end
