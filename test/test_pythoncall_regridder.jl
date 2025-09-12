a730a8cd8196a524a9553d9842cbf2a8fef00093
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-09-13 05:38:18 +1000

    add mwe in test

--- test/test_pythoncall_regridder.jl
+++ test/test_pythoncall_regridder.jl
@@ -4,6 +4,12 @@ using PythonCall
 using CondaPkg
 
 @testset "PythonCall extension" begin
-    xesmf = add_import_pkg("xesmf")
-    @test 1 == 1
+    tg = TripolarGrid(size=(360, 170, 1), z=(0, 1))
+    ll = LatitudeLongitudeGrid(size=(360, 180, 1), longitude=(0, 360), latitude=(-90, 90), z=(0, 1))
+
+    ctg = CenterField(tg)
+    cll = CenterField(ll)
+
+    W = Oceananigans.Fields.regridding_weights(cll, ctg)
+    @test W isa SparseMatrixCSC
 end
