e2e5b3b0cc1748ad00ab65743c9c4d02a4543825
Gregory Wagner <wagner.greg@gmail.com>
2025-09-13 10:44:04 -0400

    dont try to use Array for multiregion

--- test/test_multi_region_cubed_sphere.jl
+++ test/test_multi_region_cubed_sphere.jl
@@ -784,10 +784,18 @@ end
 
                 run!(simulation)
 
-                u2 = deepcopy(model.velocities.u)
-                v2 = deepcopy(model.velocities.v)
-                w2 = deepcopy(model.velocities.w)
-                b2 = deepcopy(model.tracers.b)
+                if arch isa GPU
+                    u2 = on_architecture(CPU(), model.velocities.u)
+                    v2 = on_architecture(CPU(), model.velocities.v)
+                    w2 = on_architecture(CPU(), model.velocities.w)
+                    b2 = on_architecture(CPU(), model.tracers.b)
+                else
+                    u2 = deepcopy(model.velocities.u)
+                    v2 = deepcopy(model.velocities.v)
+                    w2 = deepcopy(model.velocities.w)
+                    b2 = deepcopy(model.tracers.b)
+                end
+
                 simulation.stop_iteration = 10
 
                 @test iteration(simulation) == 10
@@ -797,10 +805,10 @@ end
                 vt = FieldTimeSeries(filename_output_writer * ".jld2", "v")
                 wt = FieldTimeSeries(filename_output_writer * ".jld2", "w")
                 bt = FieldTimeSeries(filename_output_writer * ".jld2", "b")
-                @test parent(ut[2]) == Array(parent(u2))
-                @test parent(vt[2]) == Array(parent(v2))
-                @test parent(wt[2]) == Array(parent(w2))
-                @test parent(bt[2]) == Array(parent(b2))
+                @test ut[2] == u2
+                @test vt[2] == v2
+                @test wt[2] == w2
+                @test bt[2] == b2
 
                 if grid == underlying_grid
                     @info "  Restarting simulation from pickup file on conformal cubed sphere grid [$FT, $(typeof(arch))]..."
@@ -830,10 +838,23 @@ end
                 vt = FieldTimeSeries(filename_output_writer * ".jld2", "v")
                 wt = FieldTimeSeries(filename_output_writer * ".jld2", "w")
                 bt = FieldTimeSeries(filename_output_writer * ".jld2", "b")
-                @test parent(ut[end]) == Array(parent(model.velocities.u))
-                @test parent(vt[end]) == Array(parent(model.velocities.v))
-                @test parent(wt[end]) == Array(parent(model.velocities.w))
-                @test parent(bt[end]) == Array(parent(model.tracers.b))
+
+                if arch isa GPU
+                    uN = on_architecture(CPU(), model.velocities.u)
+                    vN = on_architecture(CPU(), model.velocities.v)
+                    wN = on_architecture(CPU(), model.velocities.w)
+                    bN = on_architecture(CPU(), model.tracers.b)
+                else
+                    uN = model.velocities.u
+                    vN = model.velocities.v
+                    wN = model.velocities.w
+                    bN = model.tracers.b
+                end
+
+                @test ut[end] == uN
+                @test vt[end] == vN
+                @test wt[end] == wN
+                @test bt[end] == bN
             end
         end
     end
