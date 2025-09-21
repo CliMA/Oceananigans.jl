c9dc6c341b7e4cc40b6daf767a9c309972484aa1
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-09-21 16:34:42 +1000

    regrid uniform field and check integral

--- test/test_xesmf.jl
+++ test/test_xesmf.jl
@@ -1,14 +1,38 @@
 include("dependencies_for_runtests.jl")
 
 using XESMF
+using LinearAlgebra
 
 @testset "XESMF extension" begin
-    tg = TripolarGrid(size=(360, 170, 1), z=(0, 1))
-    ll = LatitudeLongitudeGrid(size=(360, 180, 1), longitude=(0, 360), latitude=(-90, 90), z=(0, 1))
 
-    ctg = CenterField(tg)
+    Nz = 2
+    z = (-1, 0)
+    southernmost_latitude = -30
+    radius = 3.4
+
+    ll = LatitudeLongitudeGrid(; size=(360, 180, Nz),
+                               longitude=(0, 360),
+                               latitude=(southernmost_latitude, 90),
+                               z, radius)
+
+    tg = TripolarGrid(; size=(360, 170, Nz), z, southernmost_latitude, radius)
+
     cll = CenterField(ll)
+    ctg = CenterField(tg)
 
-    W = Oceananigans.Fields.regridding_weights(cll, ctg)
+    set!(cll, 1)
+
+    # ∫ cll dA = 3πR²
+    @test Field(Integral(cll, dims=(1, 2)))[1, 1, Nz] ≈ 3π * radius^2
+
+    W = Oceananigans.Fields.regridding_weights(ctg, cll)
+
     @test W isa SparseMatrixCSC
+
+    for k in 1:Nz
+        LinearAlgebra.mul!(vec(interior(ctg, :, :, k)), W, vec(interior(cll, :, :, k)))
+    end
+
+    # ∫ ctg dA = ∫ cll dA
+    @test Field(Integral(ctg, dims=(1, 2)))[1, 1, Nz] ≈ 3π * radius^2
 end
