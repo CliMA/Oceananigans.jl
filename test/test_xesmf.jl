33a0d4796ae4ef6c3ec509071d45eceaa164e1a0
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-09-22 17:02:35 +1000

    integrals match with rtol=1e-4
    is this good enough? bad?

--- test/test_xesmf.jl
+++ test/test_xesmf.jl
@@ -15,10 +15,10 @@ function regrid_conservatively!(dst_field, weights, src_field)
 end
 
 z = (-1, 0)
-southernmost_latitude = -30
-radius = 1
+southernmost_latitude = -80
+radius = Oceananigans.Grids.R_Earth
 
-llg_coarse = LatitudeLongitudeGrid(; size=(24, 24, Nz),
+llg_coarse = LatitudeLongitudeGrid(; size=(176, 88, 1),
                                    longitude=(0, 360),
                                    latitude=(southernmost_latitude, 90),
                                    z, radius)
@@ -34,17 +34,16 @@ tg = TripolarGrid(; size=(360, 170, 1), z, southernmost_latitude, radius)
 
     for (src_grid, dst_grid) in ((llg_coarse, llg_fine),
                                  (llg_fine, llg_coarse),
-                                 (llg_coarse, tg))
+                                 (tg, llg_fine))
 
         @info "  Regridding from $(nameof(typeof(src_grid))) to $(nameof(typeof(dst_grid)))"
 
         src_field = CenterField(src_grid)
         dst_field = CenterField(dst_grid)
 
-        set!(src_field, 1)
-
-        # ∫ src_field dA = 3πR²
-        @test Field(Integral(src_field, dims=(1, 2)))[1, 1, 1] ≈ 3π * radius^2
+        λ₀, φ₀ = 150, 30.  # degrees
+        width = 12         # degrees
+        set!(src_field, (λ, φ, z) -> exp(-((λ - λ₀)^2 + (φ - φ₀)^2) / 2width^2))
 
         W = Oceananigans.Fields.regridding_weights(dst_field, src_field)
         @test W isa SparseMatrixCSC
@@ -52,6 +51,7 @@ tg = TripolarGrid(; size=(360, 170, 1), z, southernmost_latitude, radius)
         regrid_conservatively!(dst_field, W, src_field)
 
         # ∫ dst_field dA = ∫ src_field dA
-        @test first(Field(Integral(dst_field, dims=(1, 2)))) ≈ first(Field(Integral(src_field, dims=(1, 2))))
+        @test isapprox(first(Field(Integral(dst_field, dims=(1, 2)))),
+                       first(Field(Integral(src_field, dims=(1, 2)))), rtol=1e-4)
     end
 end
