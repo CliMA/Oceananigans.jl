e81dc180eecac8c44a3578c6e5426837dc0e70e3
Simone Silvestri <silvestri.simone0@gmail.com>
2025-10-02 13:35:39 +0200

    fix tests

--- src/TurbulenceClosures/abstract_scalar_biharmonic_diffusivity_closure.jl
+++ src/TurbulenceClosures/abstract_scalar_biharmonic_diffusivity_closure.jl
@@ -16,10 +16,18 @@ const ASBD = AbstractScalarBiharmonicDiffusivity
 #####
 
 const ccc = (Center(), Center(), Center())
+
+# Methods without explicit passing of `id`
 @inline νᶜᶜᶜ(i, j, k, grid, closure::ASBD, K, clock, fields) = νᶜᶜᶜ(i, j, k, grid, ccc, viscosity(closure, K), clock, fields)
 @inline νᶠᶠᶜ(i, j, k, grid, closure::ASBD, K, clock, fields) = νᶠᶠᶜ(i, j, k, grid, ccc, viscosity(closure, K), clock, fields)
 @inline νᶠᶜᶠ(i, j, k, grid, closure::ASBD, K, clock, fields) = νᶠᶜᶠ(i, j, k, grid, ccc, viscosity(closure, K), clock, fields)
 @inline νᶜᶠᶠ(i, j, k, grid, closure::ASBD, K, clock, fields) = νᶜᶠᶠ(i, j, k, grid, ccc, viscosity(closure, K), clock, fields)
+
+# Methods with explicit `id` passing (0 in case of velocities)
+@inline νᶜᶜᶜ(i, j, k, grid, closure::ASBD, K, id, clock, fields) = νᶜᶜᶜ(i, j, k, grid, ccc, viscosity(closure, K), clock, fields)
+@inline νᶠᶠᶜ(i, j, k, grid, closure::ASBD, K, id, clock, fields) = νᶠᶠᶜ(i, j, k, grid, ccc, viscosity(closure, K), clock, fields)
+@inline νᶠᶜᶠ(i, j, k, grid, closure::ASBD, K, id, clock, fields) = νᶠᶜᶠ(i, j, k, grid, ccc, viscosity(closure, K), clock, fields)
+@inline νᶜᶠᶠ(i, j, k, grid, closure::ASBD, K, id, clock, fields) = νᶜᶠᶠ(i, j, k, grid, ccc, viscosity(closure, K), clock, fields)
 
 @inline κᶠᶜᶜ(i, j, k, grid, closure::ASBD, K, id, clock, fields) = κᶠᶜᶜ(i, j, k, grid, ccc, diffusivity(closure, K, id), clock, fields)
 @inline κᶜᶠᶜ(i, j, k, grid, closure::ASBD, K, id, clock, fields) = κᶜᶠᶜ(i, j, k, grid, ccc, diffusivity(closure, K, id), clock, fields)
