23c48d35914c65adf805447af6a5adb2bd4dc728
Gregory L. Wagner <wagner.greg@gmail.com>
2025-10-04 20:47:08 -0400

    Update src/OutputReaders/field_time_series_indexing.jl

--- src/OutputReaders/field_time_series_indexing.jl
+++ src/OutputReaders/field_time_series_indexing.jl
@@ -104,8 +104,8 @@ end
 
     Δtₜ₁ = time_gap_seconds(t, t₁)
     Δt₁₂ = time_gap_seconds(t₂, t₁)
-    ñ = iszero(Δt₁₂) ? 0.0 : Δtₜ₁ / Δt₁₂
-    ñ = ifelse(n₂ == n₁, 0.0, ñ)
+    ñ = Δtₜ₁ / Δt₁₂
+    ñ = ifelse(n₂ == n₁, zero(ñ), ñ)
 
     return ñ, n₁, n₂
 end
