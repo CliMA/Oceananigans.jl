8a1bdbf4ebf47156be5091697974229af8db22dc
Gregory Wagner <wagner.greg@gmail.com>
2025-10-05 09:28:19 -0400

    clean up validation case

--- validation/dateclocks/datetime_clock_timestepping.jl
+++ validation/dateclocks/datetime_clock_timestepping.jl
@@ -1,49 +1,34 @@
 using Oceananigans
 using Dates
-using Oceananigans.Units: hour, Time
-using Oceananigans.TimeSteppers: Clock
-using Oceananigans.Architectures: CPU
 
-function run_datetime_clock_validation(; arch = CPU())
-    grid = RectilinearGrid(arch, size=(1, 1, 1), extent=(1, 1, 1))
-    start_time = DateTime(2020, 1, 1)
-    times = start_time:Hour(1):start_time + Hour(3)
+arch = CPU()
+grid = RectilinearGrid(arch, size=(1, 1, 1), extent=(1, 1, 1))
 
-    u_forcing = FieldTimeSeries{Face, Center, Center}(grid, times)
+start_time = DateTime(2020, 1, 1)
+stop_time = start_time + Hour(6)
+times = start_time:Hour(1):stop_time
+c_forcing = FieldTimeSeries{Center, Center, Center}(grid, times)
+set!(c_forcing[3], 1)
+set!(c_forcing[4], -1)
 
-    for (n, _) in enumerate(times)
-        set!(u_forcing[n], (x, y, z) -> Float64(n - 1))
-    end
+clock = Clock(time=start_time)
+model = HydrostaticFreeSurfaceModel(; grid, clock, tracers=:c, forcing=(; c=c_forcing))
+simulation = Simulation(model, Δt=60, stop_time=stop_time)
 
-    clock = Clock(time=start_time)
-    model = HydrostaticFreeSurfaceModel(; grid, clock, forcing=(; u=u_forcing))
+ow = JLD2Writer(model, model.tracers,
+                filename = "datetime_clock_timestepping.jld2",
+                overwrite_existing = true,
+                schedule = IterationInterval(1))
 
-    forcing_history = Float64[]
-    time_history = DateTime[]
+simulation.output_writers[:jld2] = ow
 
-    push!(forcing_history, u_forcing[1, 1, 1, Time(model.clock.time)])
-    push!(time_history, model.clock.time)
+run!(simulation)
 
-    time_step!(model, hour; euler=true)
-    push!(forcing_history, u_forcing[1, 1, 1, Time(model.clock.time)])
-    push!(time_history, model.clock.time)
+ct = FieldTimeSeries("datetime_clock_timestepping.jld2", "c")
+t = ct.times
 
-    for _ in 1:2
-        time_step!(model, hour)
-        push!(forcing_history, u_forcing[1, 1, 1, Time(model.clock.time)])
-        push!(time_history, model.clock.time)
-    end
-
-    expected_times = [start_time + Hour(n) for n in 0:3]
-    expected_forcing = Float64.(0:3)
-
-    @assert time_history == expected_times "Clock time history does not match expected hourly dates."
-    @assert forcing_history == expected_forcing "FieldTimeSeries forcing did not align with DateTime clock."
-
-    return (; time_history, forcing_history)
-end
-
-if abspath(PROGRAM_FILE) == @__FILE__
-    results = run_datetime_clock_validation()
-    @info "DateTime clock validation succeeded" results...
-end
+using GLMakie
+fig = Figure()
+ax = Axis(fig[1, 1], xlabel="Time", ylabel="c")
+lines!(ax, t, interior(ct, 1, 1, 1, :))
+display(fig)
