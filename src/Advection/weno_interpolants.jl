e49ba2baff49007bf6a892862dd4ef6125fdca78
Simone Silvestri <silvestri.simone0@gmail.com>
2025-10-06 13:26:34 +0200

    added the different schemes

--- src/Advection/weno_interpolants.jl
+++ src/Advection/weno_interpolants.jl
@@ -101,11 +101,11 @@ for FT in fully_supported_float_types
         @inline C★(::WENO{5, $FT}, red_order, ::Val{4}) = ifelse(red_order <5, $(FT(0)), $(FT(1//126)))
 
         # WENO 11th order
-        @inline C★(::WENO{6, $FT}, red_order, ::Val{0}) = ifelse(red_order==1, $(FT(1)), ifelse(red_order==2, $(FT(2//3)),  ifelse(red_order==3, $(FT(3//10)),  ifelse(red_order==4, $(FT(4//35)),  ifelse(red_order==5, $(FT(5//126), $(FT(1//77))))))))
-        @inline C★(::WENO{6, $FT}, red_order, ::Val{1}) = ifelse(red_order==1, $(FT(0)), ifelse(red_order==2, $(FT(1//3)),  ifelse(red_order==3, $(FT(3//5)),   ifelse(red_order==4, $(FT(18//35)), ifelse(red_order==5, $(FT(20//63), $(FT(25//154))))))))
-        @inline C★(::WENO{6, $FT}, red_order, ::Val{2}) = ifelse(red_order <3, $(FT(0)), ifelse(red_order==3, $(FT(1//10)), ifelse(red_order==4, $(FT(12//35)), ifelse(red_order==5, $(FT(100//231), $(FT(100//231)))))))
-        @inline C★(::WENO{6, $FT}, red_order, ::Val{3}) = ifelse(red_order <4, $(FT(0)), ifelse(red_order==4, $(FT(1//35)), ifelse(red_order==5, $(FT(10//63), $(FT(25//77))))))
-        @inline C★(::WENO{6, $FT}, red_order, ::Val{4}) = ifelse(red_order <5, $(FT(0)), ifelse(red_order==5, $(FT(1//126), $(FT(5//77)))))
+        @inline C★(::WENO{6, $FT}, red_order, ::Val{0}) = ifelse(red_order==1, $(FT(1)), ifelse(red_order==2, $(FT(2//3)),  ifelse(red_order==3, $(FT(3//10)),  ifelse(red_order==4, $(FT(4//35)),  ifelse(red_order==5, $(FT(5//126)), $(FT(1//77)))))))
+        @inline C★(::WENO{6, $FT}, red_order, ::Val{1}) = ifelse(red_order==1, $(FT(0)), ifelse(red_order==2, $(FT(1//3)),  ifelse(red_order==3, $(FT(3//5)),   ifelse(red_order==4, $(FT(18//35)), ifelse(red_order==5, $(FT(20//63)), $(FT(25//154)))))))
+        @inline C★(::WENO{6, $FT}, red_order, ::Val{2}) = ifelse(red_order <3, $(FT(0)), ifelse(red_order==3, $(FT(1//10)), ifelse(red_order==4, $(FT(12//35)), ifelse(red_order==5, $(FT(100//231)), $(FT(100//231))))))
+        @inline C★(::WENO{6, $FT}, red_order, ::Val{3}) = ifelse(red_order <4, $(FT(0)), ifelse(red_order==4, $(FT(1//35)), ifelse(red_order==5, $(FT(10//63)), $(FT(25//77)))))
+        @inline C★(::WENO{6, $FT}, red_order, ::Val{4}) = ifelse(red_order <5, $(FT(0)), ifelse(red_order==5, $(FT(1//126)), $(FT(5//77))))
         @inline C★(::WENO{6, $FT}, red_order, ::Val{5}) = ifelse(red_order <6, $(FT(0)), $(FT(1//462)))
     end
 end
