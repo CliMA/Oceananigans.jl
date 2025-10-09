22c762b9ac269f4bb5af7b5858c84cef784ff5f4
Simone Silvestri <silvestri.simone0@gmail.com>
2025-10-09 11:48:39 +0200

    fix mt problem

--- src/Utils/prettysummary.jl
+++ src/Utils/prettysummary.jl
@@ -5,8 +5,12 @@ prettysummary(x, args...) = summary(x)
 
 function prettysummary(f::Function, showmethods=true)
     ft = typeof(f)
-    mt = ft.name.mt
-    name = mt.name
+    if if VERSION >= v"1.12.0"
+        name = ft.name.name
+    else
+        mt = ft.name.mt
+        name = mt.name
+    end
     n = length(methods(f))
     m = n==1 ? "method" : "methods"
     sname = string(name)
