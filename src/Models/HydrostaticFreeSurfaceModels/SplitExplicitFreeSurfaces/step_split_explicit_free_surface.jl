4b2f590f1ecbccbbc6757a47ec2288b9150815d3
Simone Silvestri <silvestri.simone0@gmail.com>
2025-10-13 23:42:27 +0200

    bugfix

--- src/Models/HydrostaticFreeSurfaceModels/SplitExplicitFreeSurfaces/step_split_explicit_free_surface.jl
+++ src/Models/HydrostaticFreeSurfaceModels/SplitExplicitFreeSurfaces/step_split_explicit_free_surface.jl
@@ -95,10 +95,10 @@ function iterate_split_explicit!(free_surface, grid, GUⁿ, GVⁿ, Δτᴮ, F, c
         # To alleviate this penalty we convert first and then we substep!
         converted_η_args = convert_to_device(arch, η_args)
         converted_U_args = convert_to_device(arch, U_args)
-        
+
         converted_U_fill_halo_args = convert_to_device(arch, U_fill_halo_args)
         converted_V_fill_halo_args = convert_to_device(arch, V_fill_halo_args)
-        converted_η_fill_halo_args = convert_to_device(arch, η)
+        converted_η_fill_halo_args = convert_to_device(arch, η_fill_halo_args)
 
         @unroll for substep in 1:Nsubsteps
             Base.@_inline_meta
