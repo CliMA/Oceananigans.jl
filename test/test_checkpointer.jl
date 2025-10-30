ee79883ba0e3c84bd56127f484dd7f7032baae6a
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-10-30 13:02:42 -0600

    Fix `archs` for checkpointer tests

--- test/test_checkpointer.jl
+++ test/test_checkpointer.jl
@@ -173,7 +173,7 @@ function test_thermal_bubble_checkpointing(arch, timestepper)
     return nothing
 end
 
-for arch in [CPU(), GPU()]
+for arch in archs
     for pickup_method in (:boolean, :iteration, :filepath)
         @testset "Minimal restore [$(typeof(arch)), $(pickup_method)]" begin
             @info "  Testing minimal restore [$(typeof(arch)), $(pickup_method)]..."
