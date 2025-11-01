455c2606127120abfc43b33af047d0395fba3817
Simone Silvestri <silvestri.simone0@gmail.com>
2025-11-01 06:56:17 +0100

    bugfixes

--- src/BuoyancyFormulations/buoyancy_force.jl
+++ src/BuoyancyFormulations/buoyancy_force.jl
@@ -64,7 +64,7 @@ end
 
 # Fallback for when no grid is available, we overwrite `materialize_gradients` to false
 BuoyancyForce(formulation::AbstractBuoyancyFormulation; materialize_gradients=false, kwargs...) = 
-    BuoyancyForce(nothing; formulation, materialize_gradients=false)
+    BuoyancyForce(nothing; formulation, materialize_gradients=false, kwargs...)
 
 Adapt.adapt_structure(to, bf::BuoyancyForce) = 
     BuoyancyForce(Adapt.adapt(to, bf.formulation), 
