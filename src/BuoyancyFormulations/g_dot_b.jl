4c6ad47154a5437915677076dd19c63deae42e26
Simone Silvestri <silvestri.simone0@gmail.com>
2025-11-03 14:39:06 +0100

    typo

--- src/BuoyancyFormulations/g_dot_b.jl
+++ src/BuoyancyFormulations/g_dot_b.jl
@@ -7,5 +7,5 @@ const NZBF = BuoyancyForce{<:Any, NegativeZDirection}
 
 @inline x_dot_g_bᶠᶜᶜ(i, j, k, grid, ::NZBF) = 0
 @inline y_dot_g_bᶜᶠᶜ(i, j, k, grid, ::NZBF) = 0
-@inline z_dot_g_bᶜᶜᶠ(i, j, k, grid, bf::NZBF) = zᵃᵃᶠ(i, j, k, grid, buoyancy_perturbationᶜᶜᶜ, bf.formulation, C)
+@inline z_dot_g_bᶜᶜᶠ(i, j, k, grid, bf::NZBF) = ℑzᵃᵃᶠ(i, j, k, grid, buoyancy_perturbationᶜᶜᶜ, bf.formulation, C)
 
