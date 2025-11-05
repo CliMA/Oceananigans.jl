a1ee7cafd409f20d98dd0db972211841874a94d6
Simone Silvestri <silvestri.simone0@gmail.com>
2025-11-05 06:52:30 +0100

    fix grid metric operation

--- src/AbstractOperations/grid_metrics.jl
+++ src/AbstractOperations/grid_metrics.jl
@@ -79,10 +79,11 @@ julia> c_dz[1, 1, 1]
 3.0
 ```
 """
-grid_metric_operation(loc::Tuple, metric, grid) = grid_metric_operation(instantiate(loc), metric, grid)
-
 grid_metric_operation(loc::Tuple{LX, LY, LZ}, metric, grid) where {LX<:Location, LY<:Location, LZ<:Location} =
     KernelFunctionOperation{LX, LY, LZ}(metric_function(loc, metric), grid)
+
+# Instantiated location if location types are passed as values
+grid_metric_operation(Loc::Tuple, metric, grid) = grid_metric_operation((Loc[1](), Loc[2](), Loc[3]()), metric, grid)
 
 const NodeMetric = Union{XNode, YNode, ZNode, ΛNode, ΦNode, RNode}
 
