c554c7baab9bac77bf76ac0a697e5504d4264419
Gregory L. Wagner <wagner.greg@gmail.com>
2025-11-10 21:55:15 -0700

    Export public Smagorinsky constructors (#4910)
    * Export public Smagorinsky constructors
    
    This PR changes exports for Smagorinsky utilities. Previously we exported the "Coefficient" objects, but the public interface is supposed to be SmagorinskyLilly and DynamicSmagorinsky.
    
    cc @tomchor @giordano
    
    * Bump version from 0.100.7 to 0.101.0
    
    * Add DynamicSmagorinsky and LagrangianAveraging
    
    * Update test_time_stepping.jl
    
    * add default averaging for DynamicSmagorinsky
    
    * add docstring for DynamicSmagorinsky
    
    * a lil more
    
    * bibtex citations
    
    * alignment
    
    * line breaks
    
    * Update dynamic_coefficient.jl
    
    * fix doctests
    
    * fix doctests
    
    ---------
    
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>

--- src/TurbulenceClosures/turbulence_closure_implementations/Smagorinskys/dynamic_coefficient.jl
+++ src/TurbulenceClosures/turbulence_closure_implementations/Smagorinskys/dynamic_coefficient.jl
@@ -17,11 +17,90 @@ const DynamicSmagorinsky = Union{
     Smagorinsky{<:Any, <:DeviceDynamicCoefficient},
 }
 
-function DynamicSmagorinsky(time_discretization=ExplicitTimeDiscretization(), FT=Oceananigans.defaults.FloatType; averaging,
-                            Pr=1.0, schedule=IterationInterval(1), minimum_numerator=1e-32)
+"""
+    DynamicSmagorinsky([time_discretization=ExplicitTimeDiscretization(), FT=Float64;]
+                       averaging = LagrangianAveraging(),
+                       Pr = 1,
+                       schedule = IterationInterval(1),
+                       minimum_numerator = 1e-32)
+
+Returns a `Smagorinsky`-type closure with dynamic computation of the Smagorinsky coefficient
+according to the scale invariant procedure described by [Bou-Zeid et al. (2005)](@cite BouZeid05).
+
+`DynamicSmagorinsky` requires an `averaging` procedure.
+The default is `LagrangianAveraging`, which averages flow characteristics along Lagrangian trajectories).
+Another option is a tuple of integers indicating a directional averaging procedure along chosen dimensions
+For example, `averaging = (1, 2)` invokes averaging in the ``x, y`` directions.
+
+The coefficient is updated according to `schedule`. Less frequent updates than `IterationInterval(1)`
+may be used as a performance optimization for cases where dynamic coefficient computation is relatively expensive.
+`minimum_numerator` defines the minimum value that is acceptable in the denominator of the final calculation.
+
+Examples
+========
+
+```jldoctest smag
+julia> using Oceananigans
+
+julia> closure = DynamicSmagorinsky()
+DynamicSmagorinsky{Float64}:
+├── averaging = Oceananigans.TurbulenceClosures.Smagorinskys.LagrangianAveraging()
+├── schedule = IterationInterval(1, 0)
+├── Pr = 1.0
+└── minimum_numerator = 1.0e-32
+```
+
+To compute the dynamic coefficient every 5 time steps, for example, we can use
+
+```jldoctest smag
+julia> closure = DynamicSmagorinsky(schedule = IterationInterval(5))
+DynamicSmagorinsky{Float64}:
+├── averaging = Oceananigans.TurbulenceClosures.Smagorinskys.LagrangianAveraging()
+├── schedule = IterationInterval(5, 0)
+├── Pr = 1.0
+└── minimum_numerator = 1.0e-32
+```
+
+For situations that are homogeneous in the ``x``-direction, averaging in ``x`` might provide
+cost savings and better averaging properties than a Lagrangian average:
+
+```jldoctest smag
+julia> closure = DynamicSmagorinsky(averaging=1)
+DynamicSmagorinsky{Float64}:
+├── averaging = (1,)
+├── schedule = IterationInterval(1, 0)
+├── Pr = 1.0
+└── minimum_numerator = 1.0e-32
+```
+
+or to average in the ``x`` and ``y`` directions:
+
+```jldoctest smag
+julia> closure = DynamicSmagorinsky(averaging=(1, 2))
+DynamicSmagorinsky{Float64}:
+├── averaging = (1, 2)
+├── schedule = IterationInterval(1, 0)
+├── Pr = 1.0
+└── minimum_numerator = 1.0e-32
+```
+
+References
+==========
+
+Bou-Zeid, Elie, Meneveau, Charles, and Parlange, Marc. (2005) A scale-dependent Lagrangian dynamic model for
+large eddy simulation of complex turbulent flows, Physics of Fluids, **17**, 025105.
+"""
+function DynamicSmagorinsky(time_discretization = ExplicitTimeDiscretization(),
+                            FT = Oceananigans.defaults.FloatType;
+                            averaging = LagrangianAveraging(),
+                            Pr = 1,
+                            schedule = IterationInterval(1),
+                            minimum_numerator = 1e-32)
+
     coefficient = DynamicCoefficient(FT; averaging, schedule, minimum_numerator)
     TD = typeof(time_discretization)
     Pr = convert_diffusivity(FT, Pr; discrete_form=false)
+
     return Smagorinsky{TD}(coefficient, Pr)
 end
 
@@ -35,6 +114,11 @@ struct LagrangianAveraging end
 const LagrangianAveragedCoefficient = DynamicCoefficient{<:LagrangianAveraging}
 const LagrangianAveragedDynamicSmagorinsky = Smagorinsky{<:Any, <:LagrangianAveragedCoefficient}
 
+tupleit(::LagrangianAveraging) = LagrangianAveraging()
+tupleit(::Colon) = Colon()
+tupleit(a::Number) = tuple(a)
+tupleit(a::Tuple) = a
+
 """
     DynamicCoefficient([FT=Float64;] averaging, schedule=IterationInterval(1), minimum_numerator=1e-32)
 
@@ -43,7 +127,7 @@ according to the scale invariant procedure described by [Bou-Zeid et al. (2005)]
 
 `DynamicCoefficient` requires an `averaging` procedure, which can be a `LagrangianAveraging` (which
 averages fluid parcels along their Lagrangian trajectory) or a tuple of integers indicating
-a directional averaging procedure along chosen dimensions (e.g. `averaging=(1,2)` uses averages
+a directional averaging procedure along chosen dimensions (e.g. `averaging=(1, 2)` uses averages
 in the `x` and `y` directions).
 
 `DynamicCoefficient` is updated according to `schedule`, and `minimum_numerator` defines the minimum
@@ -53,7 +137,7 @@ Examples
 ========
 
 ```jldoctest
-julia> using Oceananigans
+julia> using Oceananigans.TurbulenceClosures
 
 julia> dynamic_coeff = DynamicCoefficient(averaging=(1, 2))
 DynamicCoefficient with
@@ -61,26 +145,22 @@ DynamicCoefficient with
 ├── schedule = IterationInterval(1, 0)
 └── minimum_numerator = 1.0e-32
 
+julia> using Oceananigans.TurbulenceClosures.Smagorinskys: Smagorinsky
+
 julia> dynamic_smagorinsky = Smagorinsky(coefficient=dynamic_coeff)
-Smagorinsky closure with
-├── coefficient = DynamicCoefficient(averaging = (1, 2), schedule = IterationInterval(1, 0))
-└── Pr = 1.0
+DynamicSmagorinsky{Float64}:
+├── averaging = (1, 2)
+├── schedule = IterationInterval(1, 0)
+├── Pr = 1.0
+└── minimum_numerator = 1.0e-32
 ```
 
-The dynamic Smagorinsky above has its coefficient recalculated at every time step, which will almost
-certainly be very slow. To alleviate the high computational cost of the `DynamicCoefficient`
-calculation, users may introduce an approximation wherein the dynamic coefficient is recomputed only
-every so often. This is standard practice in the literature and, while in principle any frequency
-choice is possible (as long as the coefficient changes relatively slowly compared to a single
-time-step), all published studies seem to recalculate it every 5 steps (e.g., Bou-Zeid et al. 2005;
-Chen et al. 2016; Salesky et al. 2017; Chor et al 2021). This choice seems to stem from the
-results by Bou-Zeid et al. (2005) who found that considerably speed up simulations while still
-producing very similar results to an update frequency of every time step. Users can change the
-update frequency using the `schedule` keyword argument. For example, a `DynamicCoefficient` that
-gets updated every 4 timesteps is obtained via:
+To alleviate the computational cost of the `DynamicCoefficient` calculation,
+users may introduce an approximation wherein the dynamic coefficient is recomputed only
+every so often. For example,
 
 ```jldoctest
-julia> using Oceananigans
+julia> using Oceananigans, Oceananigans.TurbulenceClosures
 
 julia> dynamic_coeff = DynamicCoefficient(averaging=(1, 2), schedule=IterationInterval(4))
 DynamicCoefficient with
@@ -88,24 +168,34 @@ DynamicCoefficient with
 ├── schedule = IterationInterval(4, 0)
 └── minimum_numerator = 1.0e-32
 
+julia> using Oceananigans.TurbulenceClosures.Smagorinskys: Smagorinsky
+
 julia> dynamic_smagorinsky = Smagorinsky(coefficient=dynamic_coeff)
-Smagorinsky closure with
-├── coefficient = DynamicCoefficient(averaging = (1, 2), schedule = IterationInterval(4, 0))
-└── Pr = 1.0
+DynamicSmagorinsky{Float64}:
+├── averaging = (1, 2)
+├── schedule = IterationInterval(4, 0)
+├── Pr = 1.0
+└── minimum_numerator = 1.0e-32
 ```
+
+`schedule`s other than `IterationInterval(1)` are valid if the coefficient at any particular location
+changes slowly relative to the frequency of recalculation.
+Some published studies compute the dynamic coefficient every 5 steps
+(e.g., [Bou-Zeid et al. (2005)](@cite BouZeid05); [Chen et al. 2016](@cite Chen2016);
+[Salesky et al. (2017)](@cite Salesky2017); [Chor et al. 2021](@cite Chor2021))
+to balance fidelity with computational cost.
+
+See also [`DynamicSmagorinsky`](@ref).
 
 References
 ==========
 
-Bou-Zeid, Elie, Meneveau, Charles, and Parlange, Marc. (2005) A scale-dependent Lagrangian dynamic model for
-large eddy simulation of complex turbulent flows, Physics of Fluids, **17**, 025105.
-
-Salesky, Scott T., Chamecki, Marcelo, and Bou-Zeid Elie. (2017) On the nature of the transition between
-roll and cellular organization in the convective boundary layer, Boundary-layer meteorology 163, 41-68.
-
 Chen, Bicheng, Yang, Di, Meneveau, Charles and Chamecki, Marcelo. (2016) Effects of swell on
 transport and dispersion of oil plumes within the ocean mixed layer, Journal of Geophysical
 Research: Oceans, 121(5), pp.3564-3578.
+
+Salesky, Scott T., Chamecki, Marcelo, and Bou-Zeid Elie. (2017) On the nature of the transition between
+roll and cellular organization in the convective boundary layer, Boundary-layer meteorology 163, 41-68.
 
 Chor, Tomas, McWilliams, James C., Chamecki, Marcelo. (2021) Modifications to the K-Profile
 Parameterization with nondiffusive fluxes for Langmuir turbulence, Journal of Physical Oceanography,
@@ -113,14 +203,29 @@ Parameterization with nondiffusive fluxes for Langmuir turbulence, Journal of Ph
 """
 function DynamicCoefficient(FT=Oceananigans.defaults.FloatType; averaging, schedule=IterationInterval(1), minimum_numerator=1e-32)
     minimum_numerator = convert(FT, minimum_numerator)
+    averaging = tupleit(averaging)
     return DynamicCoefficient(averaging, minimum_numerator, schedule)
 end
 
 Base.summary(dc::DynamicCoefficient) = string("DynamicCoefficient(averaging = $(dc.averaging), schedule = $(dc.schedule))")
+
 Base.show(io::IO, dc::DynamicCoefficient) = print(io, "DynamicCoefficient with\n",
                                                       "├── averaging = ", dc.averaging, "\n",
                                                       "├── schedule = ", dc.schedule, "\n",
                                                       "└── minimum_numerator = ", dc.minimum_numerator)
+
+function Base.show(io::IO, c::DynamicSmagorinsky)
+
+    FT = eltype(c.coefficient.minimum_numerator)
+
+    print(io, "DynamicSmagorinsky{$FT}:", '\n',
+              "├── averaging = ", c.coefficient.averaging, '\n',
+              "├── schedule = ", c.coefficient.schedule, '\n',
+              "├── Pr = ", c.Pr, '\n',
+              "└── minimum_numerator = ", c.coefficient.minimum_numerator)
+end
+
+
 
 #####
 ##### Some common utilities independent of averaging
