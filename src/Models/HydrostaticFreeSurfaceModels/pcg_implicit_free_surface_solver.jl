eccea1fc570715996a3260b368e8069fda61983f
Simone Silvestri <silvestri.simone0@gmail.com>
2025-11-14 14:57:03 +0100

    Refactor implicit_free_surface_right_hand_side! function

--- src/Models/HydrostaticFreeSurfaceModels/pcg_implicit_free_surface_solver.jl
+++ src/Models/HydrostaticFreeSurfaceModels/pcg_implicit_free_surface_solver.jl
@@ -108,12 +108,12 @@ compute_regional_rhs!(rhs, arch, grid, g, Δt, ∫ᶻQ, η) =
             implicit_free_surface_right_hand_side!,
             rhs, grid, g, Δt, ∫ᶻQ, η)
 
-@kernel function implicit_free_surface_right_hand_side!(rhs, grid, g, Δt, ∫ᶻQ, η)
+@kernel function implicit_free_surface_right_hand_side!(rhs, grid, g, Δt, U, η)
     i, j = @index(Global, NTuple)
     k_top = grid.Nz + 1
     Az   = Azᶜᶜᶠ(i, j, k_top, grid)
-    δx_U = δxᶜᶜᶜ(i, j, kᴺ, grid, Δy_qᶠᶜᶜ, barotropic_U, U, u)
-    δy_V = δyᶜᶜᶜ(i, j, kᴺ, grid, Δx_qᶜᶠᶜ, barotropic_V, V, v)
+    δx_U = δxᶜᶜᶜ(i, j, kᴺ, grid, Δy_qᶠᶜᶜ, barotropic_U, nothing, U.u)
+    δy_V = δyᶜᶜᶜ(i, j, kᴺ, grid, Δx_qᶜᶠᶜ, barotropic_V, nothing, U.v)
     @inbounds rhs[i, j, k_top] = (δx_U + δy_V - Az * η[i, j, k_top] / Δt) / (g * Δt)
 end
 
