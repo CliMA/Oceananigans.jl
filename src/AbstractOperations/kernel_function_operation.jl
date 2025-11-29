5ec87d1ed18c3af02f6ed145be5df8c4e6223c44
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-11-29 10:00:07 +0000

    Use explicit imports in more modules (#4997)
    * Explicit imports in `OrthogonalSphericalShellGrids`
    
    * Explicit imports in `Diagnostics`
    
    * Explicit imports in `AbstractOperations`
    
    * Explicit imports in `Models.HydrostaticFreeSurfaceModels`
    
    * Eval in `Operators` module directly
    
    Instead of importing the whole module and evaluate in `Main`.

--- src/AbstractOperations/kernel_function_operation.jl
+++ src/AbstractOperations/kernel_function_operation.jl
@@ -1,5 +1,5 @@
 using Oceananigans.Grids: prettysummary
-using Oceananigans.Utils: shortsummary
+using Oceananigans.Utils: shortsummary, construct_regionally
 
 struct KernelFunctionOperation{LX, LY, LZ, G, T, K, D} <: AbstractOperation{LX, LY, LZ, G, T}
     kernel_function :: K
@@ -76,7 +76,7 @@ Adapt.adapt_structure(to, κ::KernelFunctionOperation{LX, LY, LZ}) where {LX, LY
                                         Adapt.adapt(to, κ.grid),
                                         Tuple(Adapt.adapt(to, a) for a in κ.arguments)...)
 
-on_architecture(to, κ::KernelFunctionOperation{LX, LY, LZ}) where {LX, LY, LZ} =
+Architectures.on_architecture(to, κ::KernelFunctionOperation{LX, LY, LZ}) where {LX, LY, LZ} =
     KernelFunctionOperation{LX, LY, LZ}(on_architecture(to, κ.kernel_function),
                                         on_architecture(to, κ.grid),
                                         Tuple(on_architecture(to, a) for a in κ.arguments)...)
