65e3caff77ef9ce704ee319f21533e7d672e9881
Francis J. Poulin <fpoulin@uwaterloo.ca>
2025-12-03 13:22:02 -0500

    Update src/Coriolis/Coriolis.jl
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- src/Coriolis/Coriolis.jl
+++ src/Coriolis/Coriolis.jl
@@ -3,7 +3,6 @@ module Coriolis
 export
     FPlane, ConstantCartesianCoriolis, BetaPlane, NonTraditionalBetaPlane,
     SphericalCoriolis, HydrostaticSphericalCoriolis,
-    ActiveCellEnstrophyConserving,
     x_f_cross_U, y_f_cross_U, z_f_cross_U
 
 using Printf
