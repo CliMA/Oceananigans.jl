b341e6595cebcbcbd76c68e2d926e2ffc1f09f7b
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2025-12-05 17:06:54 -0500

    Add test for AveragedSpecifiedTimes functionality

--- test/runtests.jl
+++ test/runtests.jl
@@ -96,6 +96,7 @@ CUDA.allowscalar() do
             include("test_implicit_diffusion_diagnostic.jl")
             include("test_output_writers.jl")
             include("test_output_readers.jl")
+            include("test_averaged_specified_times.jl")
             include("test_set_field_time_series.jl")
         end
     end
