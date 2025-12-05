13187b6696e51caee5fe775ee052e9154d8fc268
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-12-05 07:56:53 -0700

    update benchmark

--- benchmark/benchmark_hi_res_hydrostatic.jl
+++ benchmark/benchmark_hi_res_hydrostatic.jl
@@ -27,31 +27,59 @@ function hi_res_hydrostatic_model(grid;
                                         buoyancy, closure, free_surface, tracers,
                                         vertical_coordinate, timestepper)
 
-    # Sensible initial condition
-    Ξ(x, y, z) = rand()
-    dTdz = 1 / grid.Lz
-    Tᵢ(x, y, z) = 20 + dTdz * z + 1e-4 * Ξ(x, y, z)
-    Sᵢ(x, y, z) = 35 + 1e-4 * Ξ(x, y, z)
-    uᵢ(x, y, z) = 1e-3 * Ξ(x, y, z)
+    # Initial condition that excites a baroclinic wave
+    Tᵢ(λ, φ, z) = 30 * (1 - tanh((abs(φ) - 45) / 8)) / 2 + rand()
+    Sᵢ(λ, φ, z) = 28 - 5e-3 * z + rand()
+    set!(model, T=Tᵢ, S=Sᵢ)
     
-    set!(model, T=Tᵢ, S=Sᵢ, u=uᵢ, v=uᵢ)
-
     return model
+end
+
+function latitude_longitude_grid(arch, Nx, Ny, Nz; immersed=true, halo=(7, 7, 7), kw...)
+    grid = LatitudeLongitudeGrid(arch; topology=(Periodic, Bounded, Bounded), halo, kw...)
+
+    if immersed
+        dλ = 20 # ridge width in degrees
+        ridge(λ, φ) = -1000 + 800 * exp(-(λ - 30)^2 / 2dλ^2)
+        grid = ImmersedBoundaryGrid(grid, PartialCellBottom(ridge))
+    end
+
+    return gride
+end
+
+"""
+    tripolar_grid(arch, Nx, Ny, Nz; halo=(7, 7, 7), kw...)
+
+Return a tripolar grid with Gaussian islands over the two north poles.
+"""
+function tripolar_grid(arch, Nx, Ny, Nz; halo=(7, 7, 7), kw...)
+    dφ, dλ = 4, 8
+    λ₀, φ₀ = 70, 55
+    h = 100
+    
+    isle(λ, φ) = exp(-(λ - λ₀)^2 / 2dλ^2 - (φ - φ₀)^2 / 2dφ^2)
+    gaussian_isles(λ, φ) = - H + (H + h) * (isle(λ, φ) + isle(λ - 180, φ))
+    
+    underlying_grid = TripolarGrid(arch; size=(Nx, Ny, Nz), halo, z)
+    grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(gaussian_isles))
+
+    return grid
 end
 
 # Configurations
 # group = get(ENV, "BENCHMARK_GROUP", "all") |> Symbol
 
-config = :channel
+config = :tripolar
+immersed = true # note :tripolar is always immersed
 Nx = 512
 Ny = 256
 Nz = 128
 arch = GPU()
-immersed = false
 Oceananigans.defaults.FloatType = Float64
 Nt = 100
-
-lat_lon_kw = (longitude=(0, 360), latitude=(-80, 80), z=(-1000, 0), size=(Nx, Ny, Nz), halo=(7, 7, 7))
+z = (-3000, 0)
+lat_lon_kw = (; longitude=(0, 360), latitude=(-80, 80), z)
+tripolar_kw = (; z)
 
 model_kw = (;
     # momentum_advection = nothing,
@@ -60,26 +88,16 @@ model_kw = (;
     tracer_advection = WENO(order=7),
 )
 
-dλ = 20 # ridge width in degrees
-ridge(λ, φ) = -1000 + 800 * exp(-(λ - 30)^2 / 2dλ^2)
-
-if config == :channel
-    grid = LatitudeLongitudeGrid(arch; topology=(Periodic, Bounded, Bounded), lat_lon_kw...)
-
-    if immersed
-        grid = ImmersedBoundaryGrid(grid, GridFittedBottom(ridge))
-    end
-
+if config == :tripolar
+    grid = tripolar_grid(arch, Nx, Ny, Nz; tripolar_kw...)
     model = hi_res_hydrostatic_model(grid; model_kw...)
+
+elseif config == :channel
+    grid = latitude_longitude_grid(arch, Nx, Ny, Nz; immersed, topology=(Periodic, Bounded, Bounded), lat_lon_kw...)
+    model = hi_res_hydrostatic_model(grid; model_kw...)
+
 elseif config == :box
-
-    Oceananigans.defaults.FloatType = FT
-    grid = LatitudeLongitudeGrid(arch; topology=(Bounded, Bounded, Bounded), lat_lon_kw...)
-
-    if immersed
-        grid = ImmersedBoundaryGrid(grid, GridFittedBottom(ridge))
-    end
-
+    grid = latitude_longitude_grid(arch, Nx, Ny, Nz; immersed, topology=(Bounded, Bounded, Bounded), lat_lon_kw...)
     model = hi_res_hydrostatic_model(grid; model_kw...)
 end
 
