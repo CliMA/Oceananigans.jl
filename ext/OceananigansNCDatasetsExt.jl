17c81ecd715b509cff2c856bab0795f7b5714dd1
tomaschor <tomaschor@gmail.com>
2025-12-11 18:17:27 -0300

    generalize set! for FieldTimeSeries

--- ext/OceananigansNCDatasetsExt.jl
+++ ext/OceananigansNCDatasetsExt.jl
@@ -38,6 +38,7 @@ using Oceananigans.OutputWriters:
     convert_output,
     fetch_and_convert_output,
     show_array_type
+using Oceananigans.OutputReaders: InMemoryFTS
 using NCDatasets: AbstractDataset
 
 import NCDatasets: defVar
@@ -1635,17 +1636,14 @@ function FieldTimeSeries_from_netcdf(path::String, name::String;
 
     isnothing(grid) && (grid = reconstruct_grid(file))
 
-    # if boundary_conditions isa UnspecifiedBoundaryConditions
-    #     boundary_conditions = file[name].attrib["boundary_conditions"] |> materialize_from_netcdf
-    #     boundary_conditions = on_architecture(architecture, boundary_conditions)
-    # end
+    @warn "Reading boundary conditions from NetCDF files is not supported for FieldTimeSeries. Defaulting to UnspecifiedBoundaryConditions."
+    boundary_conditions = UnspecifiedBoundaryConditions()
 
     isnothing(location) && (location = file[name].attrib["location"] |> materialize_from_netcdf)
     LX, LY, LZ = location
     loc = (LX(), LY(), LZ())
 
     isnothing(times) && (times = file["time"] |> collect)
-    close(file)
 
     Nt = time_indices_length(backend, times)
     data = new_data(eltype(grid), grid, loc, indices, Nt)
@@ -1654,8 +1652,57 @@ function FieldTimeSeries_from_netcdf(path::String, name::String;
                                               times, path, name, time_indexing, reader_kw)
 
     set!(time_series, path, name)
+    close(file)
 
     return time_series
+end
+
+iterations_from_file(file::NCDataset) = parse.(Int, keys(file["time"]))
+
+function set_from_netcdf!(fts::InMemoryFTS, file::NCDataset, name::String=fts.name; warn_missing_data=true)
+    file_iterations = iterations_from_file(file)
+    Main.@infiltrate
+    file_times = [file["timeseries/t/$i"] for i in file_iterations]
+    close(file)
+
+    # Compute a timescale for comparisons
+    Δt = mean(diff(file_times))
+
+    arch = architecture(fts)
+
+    # TODO: a potential optimization here might be to load
+    # all of the data into a single array, and then transfer that
+    # to parent(fts).
+
+    # Index times on the CPU
+    cpu_times = on_architecture(CPU(), fts.times)
+
+    for n in time_indices(fts)
+        t = cpu_times[n]
+        file_index = find_time_index(t, file_times, Δt)
+
+        if isnothing(file_index) # the time does not exist in the file
+            if warn_missing_data
+                msg = @sprintf("No data found for time %.1e and time index %d\n", t, n)
+                msg *= @sprintf("for field %s at path %s", file.path, name)
+                @warn msg
+            end
+        else
+            file_iter = file_iterations[file_index]
+
+            # Note: use the CPU for this step
+            field_n = Field(instantiated_location(fts), file.path, name, file_iter,
+                            grid = on_architecture(CPU(), fts.grid),
+                            architecture = cpu_architecture(arch),
+                            indices = fts.indices,
+                            boundary_conditions = fts.boundary_conditions)
+
+            # Potentially transfer from CPU to GPU
+            set!(fts[n], field_n)
+        end
+    end
+
+    return nothing
 end
 
 end # module
