7a7dc73f721c64c30247a50b55fba104b81a4f09
Simone Silvestri <silvestri.simone0@gmail.com>
2025-12-28 16:57:44 +0100

    continue fixing

--- src/Models/HydrostaticFreeSurfaceModels/compute_hydrostatic_free_surface_buffers.jl
+++ src/Models/HydrostaticFreeSurfaceModels/compute_hydrostatic_free_surface_buffers.jl
@@ -1,7 +1,7 @@
 using Oceananigans.Grids: halo_size, XFlatGrid, YFlatGrid, ZFlatGrid, get_active_cells_map
 using Oceananigans.DistributedComputations: Distributed, DistributedGrid, AsynchronousDistributed, synchronize_communication!
 using Oceananigans.ImmersedBoundaries: CellMaps
-using Oceananigans.Models.NonhydrostaticModels: buffer_tendency_kernel_parameters, buffer_parameters
+using Oceananigans.Models.NonhydrostaticModels: buffer_tendency_kernel_parameters, buffer_κ_kernel_parameters, buffer_parameters
 
 const DistributedActiveInteriorIBG = ImmersedBoundaryGrid{FT, TX, TY, TZ,
                                                           <:DistributedGrid, I, <:CellMaps, S,
