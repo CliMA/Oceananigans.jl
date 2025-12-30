34e62a068955942a244328a323652d8510fab2ab
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-12-30 12:13:27 -0700

    clean up

--- src/OutputReaders/field_time_series_reductions.jl
+++ src/OutputReaders/field_time_series_reductions.jl
@@ -20,12 +20,16 @@ import Oceananigans.Fields: conditional_length
 #####
 
 # Element-wise accumulation operations for time dimension reduction
-@inline accumulate_time!(::typeof(sum!), r, a) = r .+= a
-@inline accumulate_time!(::typeof(prod!), r, a) = r .*= a
-@inline accumulate_time!(::typeof(maximum!), r, a) = r .= max.(r, a)
-@inline accumulate_time!(::typeof(minimum!), r, a) = r .= min.(r, a)
-@inline accumulate_time!(::typeof(all!), r, a) = r .&= a
-@inline accumulate_time!(::typeof(any!), r, a) = r .|= a
+@inline broadcasted_accumulate!(::typeof(sum!), r, a) = r .+= a
+@inline broadcasted_accumulate!(::typeof(prod!), r, a) = r .*= a
+@inline broadcasted_accumulate!(::typeof(maximum!), r, a) = r .= max.(r, a)
+@inline broadcasted_accumulate!(::typeof(minimum!), r, a) = r .= min.(r, a)
+@inline broadcasted_accumulate!(::typeof(all!), r, a) = r .&= a
+@inline broadcasted_accumulate!(::typeof(any!), r, a) = r .|= a
+
+tupleit(a) = tuple(a)
+tupleit(a::Tuple) = a
+tupleit(a::Colon) = Colon()
 
 for reduction in (:sum, :maximum, :minimum, :all, :any, :prod)
     reduction! = Symbol(reduction, '!')
@@ -34,35 +38,29 @@ for reduction in (:sum, :maximum, :minimum, :all, :any, :prod)
 
         # Allocating
         function Base.$(reduction)(f::Function, fts::FTS; dims=:, kw...)
+            dims = tupleit(dims)
+
             if dims isa Colon
                 return Base.$(reduction)($(reduction)(f, fts[n]; kw...) for n in 1:length(fts.times))
+            elseif 4 ∈ dims
+                # Reduce over time dimension
+                spatial_dims = filter(d -> d != 4, dims)
+                loc = isempty(spatial_dims) ? instantiated_location(fts) : reduced_location(instantiated_location(fts); dims=spatial_dims)
+                new_times = [mean(fts.times)]
+                rts = FieldTimeSeries(loc, fts.grid, new_times; indices=fts.indices, kw...)
+                return Base.$(reduction!)(f, rts, fts; dims, kw...)
             else
-                if !(dims isa Tuple)
-                    dims = Tuple(dims)
-                end
-
-                if 4 ∈ dims
-                    # Reduce over time dimension
-                    spatial_dims = filter(d -> d != 4, dims)
-                    loc = isempty(spatial_dims) ? instantiated_location(fts) : reduced_location(instantiated_location(fts); dims=spatial_dims)
-                    new_times = [mean(fts.times)]
-                    rts = FieldTimeSeries(loc, fts.grid, new_times; indices=fts.indices, kw...)
-                    return Base.$(reduction!)(f, rts, fts; dims, kw...)
-                else
-                    loc = reduced_location(instantiated_location(fts); dims)
-                    times = fts.times
-                    rts = FieldTimeSeries(loc, fts.grid, times; indices=fts.indices, kw...)
-                    return Base.$(reduction!)(f, rts, fts; dims, kw...)
-                end
+                loc = reduced_location(instantiated_location(fts); dims)
+                times = fts.times
+                rts = FieldTimeSeries(loc, fts.grid, times; indices=fts.indices, kw...)
+                return Base.$(reduction!)(f, rts, fts; dims, kw...)
             end
         end
 
         Base.$(reduction)(fts::FTS; kw...) = Base.$(reduction)(identity, fts; kw...)
 
         function Base.$(reduction!)(f::Function, rts::FTS, fts::FTS; dims=:, kw...)
-            if !(dims isa Tuple)
-                dims = Tuple(dims)
-            end
+            dims = tupleit(dims)
 
             if 4 ∈ dims
                 # Reduce over time dimension
@@ -70,18 +68,15 @@ for reduction in (:sum, :maximum, :minimum, :all, :any, :prod)
                 # Initialize with the first time step
                 if isempty(spatial_dims)
                     set!(rts[1], fts[1])
+                    for n = 2:length(fts)
+                        broadcasted_accumulate!(Base.$(reduction!), interior(rts[1]), interior(fts[n]))
+                    end
                 else
                     # Use the allocating reduction for the first step
                     temp = Base.$(reduction)(f, fts[1]; dims=spatial_dims, kw...)
                     set!(rts[1], temp)
-                end
-                # Accumulate over remaining time steps using element-wise operations
-                for n = 2:length(fts)
-                    if isempty(spatial_dims)
-                        accumulate_time!(Base.$(reduction!), interior(rts[1]), interior(fts[n]))
-                    else
-                        temp = Base.$(reduction)(f, fts[n]; dims=spatial_dims, kw...)
-                        accumulate_time!(Base.$(reduction!), interior(rts[1]), interior(temp))
+                    for n = 2:length(fts)
+                        broadcasted_accumulate!(Base.$(reduction!), interior(rts[1]), interior(fts[n]))
                     end
                 end
             else
@@ -98,14 +93,14 @@ for reduction in (:sum, :maximum, :minimum, :all, :any, :prod)
 end
 
 function Statistics._mean(f, c::FTS, ::Colon; condition=nothing)
-    condition == nothing || error("condition_operant for FieldTimeSeries not implemented")
+    condition == nothing || error("condition_operand for FieldTimeSeries not implemented")
     # TODO: implement condition_operand for FieldTimeSeries?
     # operator = condition_operand(f, c, condition, mask)
     return sum(c) / conditional_length(c)
 end
 
 function Statistics._mean(f, c::FTS, dims; condition=nothing)
-    condition == nothing || error("condition_operant for FieldTimeSeries not implemented")
+    condition == nothing || error("condition_operand for FieldTimeSeries not implemented")
     # TODO: implement condition_operand for FieldTimeSeries?
     # operand = condition_operand(f, c, condition, mask)
     r = sum(c; dims)
