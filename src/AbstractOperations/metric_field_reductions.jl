a19b22234c7bdca7e4b1f29582e42a5ce9fe32bc
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-12-30 19:58:38 -0700

    fix doctest for CumulativeIntegral

--- src/AbstractOperations/metric_field_reductions.jl
+++ src/AbstractOperations/metric_field_reductions.jl
@@ -169,36 +169,24 @@ Example
 Compute the cumulative integral of ``f(z) = z`` over z ∈ [0, 1].
 
 ```jldoctest
-julia> using Oceananigans
+using Oceananigans
 
-julia> grid = RectilinearGrid(size=8, z=(0, 1), topology=(Flat, Flat, Bounded));
+grid = RectilinearGrid(size=8, z=(0, 1), topology=(Flat, Flat, Bounded))
 
-julia> c = CenterField(grid);
+c = CenterField(grid)
+set!(c, z -> z)
 
-julia> set!(c, z -> z)
-1×1×8 Field{Center, Center, Center} on RectilinearGrid on CPU
-├── grid: 1×1×8 RectilinearGrid{Float64, Flat, Flat, Bounded} on CPU with 0×0×3 halo
-├── boundary conditions: FieldBoundaryConditions
-│   └── west: Nothing, east: Nothing, south: Nothing, north: Nothing, bottom: ZeroFlux, top: ZeroFlux, immersed: Nothing
-└── data: 1×1×14 OffsetArray(::Array{Float64, 3}, 1:1, 1:1, -2:11) with eltype Float64 with indices 1:1×1:1×-2:11
-    └── max=0.9375, min=0.0625, mean=0.5
+∫ᶻc_ci = CumulativeIntegral(c, dims=3)
+∫ᶻc = Field(∫ᶻc_ci)
 
-julia> C_op = CumulativeIntegral(c, dims=3)
-CumulativeIntegral of BinaryOperation at (Center, Center, Center) over dims 3
-└── operand: BinaryOperation at (Center, Center, Center)
-    └── grid: 1×1×8 RectilinearGrid{Float64, Flat, Flat, Bounded} on CPU with 0×0×3 halo
-
-julia> C = compute!(Field(C_op))
-1×1×8 Field{Center, Center, Center} on RectilinearGrid on CPU
-├── data: OffsetArrays.OffsetArray{Float64, 3, Array{Float64, 3}}, size: (1, 1, 8)
+# output
+1×1×9 Field{Center, Center, Face} on RectilinearGrid on CPU
+├── data: OffsetArrays.OffsetArray{Float64, 3, Array{Float64, 3}}, size: (1, 1, 9)
 ├── grid: 1×1×8 RectilinearGrid{Float64, Flat, Flat, Bounded} on CPU with 0×0×3 halo
 ├── operand: CumulativeIntegral of BinaryOperation at (Center, Center, Center) over dims 3
 ├── status: time=0.0
-└── data: 1×1×14 OffsetArray(::Array{Float64, 3}, 1:1, 1:1, -2:11) with eltype Float64 with indices 1:1×1:1×-2:11
-    └── max=0.5, min=0.0078125, mean=0.199219
-
-julia> C[1, 1, 8]
-0.5
+└── data: 1×1×15 OffsetArray(::Array{Float64, 3}, 1:1, 1:1, -2:12) with eltype Float64 with indices 1:1×1:1×-2:12
+    └── max=0.5, min=0.0, mean=0.177083
 ```
 """
 function CumulativeIntegral(field::AbstractField; dims, reverse=false, condition=nothing, mask=0)
