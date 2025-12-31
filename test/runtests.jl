b402543654f37ad4de045cf61a361610a5a76993
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-12-31 10:37:53 -0700

    comment out other tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -28,6 +28,7 @@ CUDA.allowscalar() do
         end
     end
 
+    #=
     # Initialization steps
     if group == :init || group == :all
         include("test_init.jl")
@@ -266,6 +267,7 @@ CUDA.allowscalar() do
             include("test_reactant_latitude_longitude_grid.jl")
         end
     end
+    =#
 
     # Tests for Reactant correctness (comparing vanilla vs ReactantState)
     if group == :reactant_correctness || group == :all
@@ -274,6 +276,7 @@ CUDA.allowscalar() do
         end
     end
 
+    #=
     # Tests for XESMF extension
     if group == :xesmf || group == :all
         @testset "XESMF extension tests" begin
@@ -327,6 +330,7 @@ CUDA.allowscalar() do
     if group == :convergence
         include("test_convergence.jl")
     end
+    =#
 end
 
 end #CUDA.allowscalar()
