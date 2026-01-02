c66344c8ac75125eb0d67f7862d99fa15e11a7df
Navid C. Constantinou <navidcy@users.noreply.github.com>
2026-01-02 23:04:12 +1100

    code alignment

--- src/OutputWriters/windowed_time_average.jl
+++ src/OutputWriters/windowed_time_average.jl
@@ -143,7 +143,7 @@ to time-average its outputs before writing them to disk:
 using Oceananigans
 using Oceananigans.Units
 
-grid=RectilinearGrid(size=(1, 1, 1), extent=(1, 1, 1))
+grid = RectilinearGrid(size=(1, 1, 1), extent=(1, 1, 1))
 model = NonhydrostaticModel(grid)
 
 simulation = Simulation(model, Δt=10minutes, stop_time=30days)
