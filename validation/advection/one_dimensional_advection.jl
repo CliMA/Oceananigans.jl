01f230f8792a2b8b2cd23bebc8c990720e804a58
Navid C. Constantinou <navidcy@users.noreply.github.com>
2026-01-03 19:48:39 +1100

    cleaner/code alignment

--- validation/advection/one_dimensional_advection.jl
+++ validation/advection/one_dimensional_advection.jl
@@ -36,18 +36,15 @@ set!(c_real, c₀)
 # Change to test pure advection schemes
 advection = WENO(order=7)
 
-model1 = NonhydrostaticModel(grid;
-                             timestepper = :RungeKutta3,
-                             advection,
-                             tracers = :c)
+model1 = NonhydrostaticModel(grid; timestepper = :RungeKutta3, advection, tracers = :c)
 set!(model1, c=c₀, u=1)
 sim1 = Simulation(model1, Δt=Δt_max, stop_time=10)
 
 model2 = HydrostaticFreeSurfaceModel(grid;
-                                    velocities = PrescribedVelocityFields(u=1),
-                                    timestepper = :SplitRungeKutta3,
-                                    tracer_advection = advection,
-                                    tracers = :c)
+                                     velocities = PrescribedVelocityFields(u=1),
+                                     timestepper = :SplitRungeKutta3,
+                                     tracer_advection = advection,
+                                     tracers = :c)
 set!(model2, c=c₀)
 sim2 = Simulation(model2, Δt=Δt_max, stop_time=10)
 
