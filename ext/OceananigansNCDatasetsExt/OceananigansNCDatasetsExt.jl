da5a2e4935577a7d29bd5f46828dd1529800f350
tomaschor <tomaschor@gmail.com>
2026-01-05 11:44:51 -0300

    add whitespaces to file

--- ext/OceananigansNCDatasetsExt/OceananigansNCDatasetsExt.jl
+++ ext/OceananigansNCDatasetsExt/OceananigansNCDatasetsExt.jl
@@ -2,6 +2,8 @@ module OceananigansNCDatasetsExt
 
 using NCDatasets
 
+using Oceananigans.Fields
+
 using Dates: AbstractTime, UTC, now, DateTime
 using Printf: @sprintf
 using OrderedCollections: OrderedDict
@@ -12,18 +14,23 @@ using Oceananigans: initialize!, prettytime, pretty_filesize, AbstractModel
 using Oceananigans.Architectures: CPU, GPU, on_architecture
 using Oceananigans.AbstractOperations: KernelFunctionOperation, AbstractOperation
 using Oceananigans.BuoyancyFormulations: BuoyancyForce, BuoyancyTracer, SeawaterBuoyancy, LinearEquationOfState
-using Oceananigans.Fields
 using Oceananigans.Fields: Reduction, reduced_dimensions, reduced_location, location, indices
-using Oceananigans.Grids: Center, Face, Flat, Periodic, Bounded,
-                          AbstractGrid, RectilinearGrid, LatitudeLongitudeGrid, StaticVerticalDiscretization,
-                          topology, halo_size, xspacings, yspacings, zspacings, λspacings, φspacings,
-                          parent_index_range, nodes, ξnodes, ηnodes, rnodes, validate_index, peripheral_node,
-                          constructor_arguments, architecture
-using Oceananigans.ImmersedBoundaries: ImmersedBoundaryGrid, GridFittedBottom, GFBIBG, GridFittedBoundary, PartialCellBottom, PCBIBG,
-                                       CenterImmersedCondition, InterfaceImmersedCondition
+
+using Oceananigans.Grids:
+    Center, Face, Flat, Periodic, Bounded,
+    AbstractGrid, RectilinearGrid, LatitudeLongitudeGrid, StaticVerticalDiscretization,
+    topology, halo_size, xspacings, yspacings, zspacings, λspacings, φspacings,
+    parent_index_range, nodes, ξnodes, ηnodes, rnodes, validate_index, peripheral_node,
+    constructor_arguments, architecture
+
+using Oceananigans.ImmersedBoundaries:
+    ImmersedBoundaryGrid, GridFittedBottom, GFBIBG, GridFittedBoundary, PartialCellBottom, PCBIBG,
+    CenterImmersedCondition, InterfaceImmersedCondition
+
 using Oceananigans.Models: ShallowWaterModel, LagrangianParticles
-using Oceananigans.Utils: TimeInterval, IterationInterval, WallTimeInterval, materialize_schedule,
-                          versioninfo_with_gpu, oceananigans_versioninfo, prettykeys
+using Oceananigans.Utils:
+    TimeInterval, IterationInterval, WallTimeInterval, materialize_schedule,
+    versioninfo_with_gpu, oceananigans_versioninfo, prettykeys, add_time_interval
 using Oceananigans.OutputWriters:
     auto_extension,
     output_averaging_schedule,
@@ -39,6 +46,7 @@ using Oceananigans.OutputWriters:
     convert_output,
     fetch_and_convert_output,
     show_array_type
+
 using Oceananigans.OutputReaders: InMemoryFTS, time_indices
 using NCDatasets: AbstractDataset
 
