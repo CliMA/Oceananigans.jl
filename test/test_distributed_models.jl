ebe5c8f7796390e4eda26f937bab9d100cd2c95a
Simone Silvestri <silvestri.simone0@gmail.com>
2026-01-06 09:44:53 -0100

    Update test/test_distributed_models.jl
    Co-authored-by: Mikolaj-A-Kowalski <32641577+Mikolaj-A-Kowalski@users.noreply.github.com>

--- test/test_distributed_models.jl
+++ test/test_distributed_models.jl
@@ -289,7 +289,7 @@ function test_complex_boundary_conditions(Rx, Ry, child_arch)
     # A grid with unity spacings in all directions
     grid  = RectilinearGrid(arch, topology=(Bounded, Bounded, Bounded), size=(2*Rx, 2*Ry, 1), extent=(2*Rx,2*Ry, 1))
     u_bc  = FluxBoundaryCondition(clock_field_dependent_boudary_condition, discrete_form=true)
-    u_bcs = FieldBoundaryCondition(top=u_bcs)
+    u_bcs = FieldBoundaryConditions(top=u_bc)
     
     # A model with an Euler step
     model = HydrostaticFreeSurfaceModel(; grid, boundary_conditions=(; u=u_bcs), timestepper=:QuasiAdamsBashforth2)
