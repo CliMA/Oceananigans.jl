a603740902725f008c5b72b83f94eef262becb03
Mosè Giordano <mose@gnu.org>
2026-01-06 19:36:03 +0100

    Use explicit imports in `BuoyancyFormulations`

--- src/BuoyancyFormulations/BuoyancyFormulations.jl
+++ src/BuoyancyFormulations/BuoyancyFormulations.jl
@@ -5,12 +5,11 @@ export
     ∂x_b, ∂y_b, ∂z_b, buoyancy_perturbationᶜᶜᶜ, x_dot_g_bᶠᶜᶜ, y_dot_g_bᶜᶠᶜ, z_dot_g_bᶜᶜᶠ,
     top_buoyancy_flux
 
-using Oceananigans
-using Oceananigans.Grids
-using Oceananigans.Operators
+using Oceananigans: Oceananigans
+using Oceananigans.Operators: ℑxᶠᵃᵃ, ℑyᵃᶠᵃ, ℑzᵃᵃᶠ, ∂xᶠᶜᶜ, ∂yᶜᶠᶜ, ∂zᶜᶜᶠ
 using Oceananigans.BoundaryConditions: getbc
 
-import SeawaterPolynomials: ρ′, thermal_expansion, haline_contraction, with_float_type
+using SeawaterPolynomials: SeawaterPolynomials, ρ′, thermal_expansion, haline_contraction, with_float_type
 
 """
     AbstractBuoyancyFormulation{EOS}
