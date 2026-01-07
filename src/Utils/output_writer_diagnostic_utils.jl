ca95154f441ddd50c446f888ca623080804b6328
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2026-01-07 16:15:32 -0700

    Remove ambiguous `push!` method

--- src/Utils/output_writer_diagnostic_utils.jl
+++ src/Utils/output_writer_diagnostic_utils.jl
@@ -7,18 +7,6 @@ using Oceananigans: AbstractOutputWriter, AbstractDiagnostic
 ##### Utilities shared between diagnostics and output writers
 #####
 
-defaultname(::AbstractDiagnostic, nelems) = Symbol(:diag, nelems+1)
-
-defaultname(::AbstractOutputWriter, nelems) = Symbol(:writer, nelems+1)
-
-const DiagOrWriterDict = OrderedDict{S, <:Union{AbstractDiagnostic, AbstractOutputWriter}} where S
-
-function push!(container::DiagOrWriterDict, elem)
-    name = defaultname(elem, length(container))
-    container[name] = elem
-    return nothing
-end
-
 getindex(container::DiagOrWriterDict, inds::Integer...) = getindex(container.vals, inds...)
 
 setindex!(container::DiagOrWriterDict, newvals, inds::Integer...) = setindex!(container.vals, newvals, inds...)
