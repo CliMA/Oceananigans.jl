d3c7ee65d238bbcfa5ee540dd30247ed479b253e
Simone Silvestri <silvestri.simone0@gmail.com>
2026-01-12 18:25:29 -0500

    Update src/Models/HydrostaticFreeSurfaceModels/z_star_vertical_spacing.jl
    Co-authored-by: Ali Ramadhan <ali.hh.ramadhan@gmail.com>

--- src/Models/HydrostaticFreeSurfaceModels/z_star_vertical_spacing.jl
+++ src/Models/HydrostaticFreeSurfaceModels/z_star_vertical_spacing.jl
@@ -103,7 +103,7 @@ function update_grid_vertical_velocity!(velocities, model, grid::MutableGridOfSo
 
     # the barotropic velocities are retrieved from the free surface model for a
     # SplitExplicitFreeSurface and are calculated for other free surface models
-    # Here we distringuish between the model (prognostic) velocities and the transport velocities
+    # Here we distinguish between the model (prognostic) velocities and the transport velocities
     # used to advect tracers...
     if velocities === model.velocities
         U, V = barotropic_velocities(model.free_surface)
