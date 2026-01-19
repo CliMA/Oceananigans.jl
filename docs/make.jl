79a26d90ac635ad927a64450f0e329009c8b2e52
Lazaro Alonso <lazarus.alon@gmail.com>
2026-01-19 19:58:33 +0100

    DV deploy

--- docs/make.jl
+++ docs/make.jl
@@ -271,8 +271,8 @@ makedocs(; sitename = "Oceananigans.jl",
 #     end
 # end
 
-deploydocs(repo = "github.com/CliMA/OceananigansDocumentation.git",
-           versions = ["stable" => "v^", "dev" => "dev", "v#.#.#"],
+DocumenterVitepress.deploydocs(repo = "github.com/CliMA/OceananigansDocumentation.git",
+        #    versions = ["stable" => "v^", "dev" => "dev", "v#.#.#"],
            forcepush = true,
            push_preview = true,
            devbranch = "main")
