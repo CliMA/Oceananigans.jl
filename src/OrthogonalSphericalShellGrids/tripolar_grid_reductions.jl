d02193965c4db88b28bfdb34c7f838c98c635027
Simone Silvestri <silvestri.simone0@gmail.com>
2026-01-19 07:52:48 -0500

    remove code duplication

--- src/OrthogonalSphericalShellGrids/tripolar_grid_reductions.jl
+++ src/OrthogonalSphericalShellGrids/tripolar_grid_reductions.jl
@@ -60,10 +60,7 @@ const TF = Union{<:AbstractField{<:Any, <:Any, <:Any, <:TripolarGridOfSomeKind},
 @inline conditional_length(c::TF, dims::NTuple{1}) = conditional_length(c, dims[1])
 @inline conditional_length(c::TF, dims::NTuple{2}) = conditional_length(condition_operand(identity, c, PrognosticTripolarCells(), 0), dims)
 
-condition_operand(::typeof(identity), op::TF, ::Nothing, mask) =
-    condition_operand(nothing, op, nothing, mask)
-
-@inline function condition_operand(::Nothing, op::TF, ::Nothing, mask)
+@inline function tripolar_condition_operand(func, op, condition, mask)
     arch = architecture(op)
 
     if !(arch isa Distributed) || (arch.ranks[2] == arch.local_index[2]) # The last core
@@ -72,32 +69,17 @@ condition_operand(::typeof(identity), op::TF, ::Nothing, mask) =
         tripolar_condition = nothing
     end
 
-    return ConditionalOperation(op; func=nothing, condition=tripolar_condition, mask)
-end
-
-@inline function condition_operand(func, op::TF, condition, mask)
-    arch = architecture(op)
-
-    if !(arch isa Distributed) || (arch.ranks[2] == arch.local_index[2]) # The last core
-        tripolar_condition = PrognosticTripolarCells(condition)
-    else # intermediate cores
-        tripolar_condition = condition
-    end
-
     return ConditionalOperation(op; func, condition=tripolar_condition, mask)
 end
 
+@inline condition_operand(::typeof(identity), op::TF, ::Nothing, mask) = tripolar_condition_operand(nothing, op, nothing, mask)
+@inline condition_operand(::Nothing, op::TF, ::Nothing, mask) = tripolar_condition_operand(nothing, op, nothing, mask)
+@inline condition_operand(func, op::TF, condition, mask) = tripolar_condition_operand(func, op, condition, mask)
+
 @inline function condition_operand(func, op::TF, condition::AbstractArray, mask)
     arch = architecture(op)
-    condition = on_architecture(architecture(operand.grid), condition)
-
-    if !(arch isa Distributed) || (arch.ranks[2] == arch.local_index[2]) # The last core
-        tripolar_condition = PrognosticTripolarCells(condition)
-    else # intermediate cores
-        tripolar_condition = condition
-    end
-
-    return ConditionalOperation(operand; func, condition=tripolar_condition, mask)
+    condition = on_architecture(arch, condition)
+    return tripolar_condition_operand(func, op, condition, mask)
 end
 
 # Disambiguation for Immersed Tripolar Fields (ITF) and Immersed Tripolar Reduced Fields (ITRF)
@@ -124,23 +106,7 @@ const ITRF = Union{XITRF, YITRF, ZITRF, YZITRF, XZITRF, XYITRF, XYZITRF}
 # Disambiguation: ITRF matches both IRF and TF, so we need explicit methods
 # For ITRF, we combine both tripolar (PrognosticTripolarCells) and immersed (NotImmersedColumn) conditions
 
-condition_operand(::typeof(identity), op::ITRF, ::Nothing, mask) =
-    condition_operand(nothing, op, nothing, mask)
-
-@inline function condition_operand(::Nothing, op::ITRF, ::Nothing, mask)
-    arch = architecture(op)
-    immersed_cond = NotImmersedColumn(immersed_column(op), nothing)
-
-    if !(arch isa Distributed) || (arch.ranks[2] == arch.local_index[2]) # The last core
-        tripolar_condition = PrognosticTripolarCells(immersed_cond)
-    else # intermediate cores
-        tripolar_condition = immersed_cond
-    end
-
-    return ConditionalOperation(op; func=nothing, condition=tripolar_condition, mask)
-end
-
-@inline function condition_operand(func, op::ITRF, condition, mask)
+function immersed_reduced_tripolar_condition_operand(func, op, condition, mask)
     arch = architecture(op)
     immersed_cond = NotImmersedColumn(immersed_column(op), condition)
 
@@ -152,17 +118,13 @@ end
 
     return ConditionalOperation(op; func, condition=tripolar_condition, mask)
 end
+
+@inline condition_operand(::typeof(identity), op::ITRF, ::Nothing, mask) = immersed_reduced_tripolar_condition_operand(nothing, op, nothing, mask)
+@inline condition_operand(::Nothing, op::ITRF, ::Nothing, mask) = immersed_reduced_tripolar_condition_operand(nothing, op, nothing, mask)
+@inline condition_operand(func, op::ITRF, condition, mask) = immersed_reduced_tripolar_condition_operand(func, op, condition, mask)
 
 @inline function condition_operand(func, op::ITRF, condition::AbstractArray, mask)
     arch = architecture(op)
     condition = on_architecture(architecture(op.grid), condition)
-    immersed_cond = NotImmersedColumn(immersed_column(op), condition)
-
-    if !(arch isa Distributed) || (arch.ranks[2] == arch.local_index[2]) # The last core
-        tripolar_condition = PrognosticTripolarCells(immersed_cond)
-    else # intermediate cores
-        tripolar_condition = immersed_cond
-    end
-
-    return ConditionalOperation(op; func, condition=tripolar_condition, mask)
+    return immersed_reduced_tripolar_condition_operand(func, op, condition, mask)
 end
