e32bd63a6763a06e9118f001c4dc1fa727d4661e
Simone Silvestri <silvestri.simone0@gmail.com>
2026-01-21 11:29:35 +0100

    add some regression tests

---
+++ test/regression_tests/hydrostatic_rotation_regression_test.jl
@@ -0,0 +1,105 @@
+using Oceananigans.DistributedComputations: reconstruct_global_field, all_reduce
+using Oceananigans.Grids
+
+using JLD2
+
+function run_hydrostatic_rotation_regression_test(grid, closure, timestepper; regenerate_data = false)
+
+    g = Oceananigans.defaults.gravitational_acceleration
+    R = grid.radius
+    Ω = Oceananigans.defaults.planet_rotation_rate
+
+    # Add some shear on the velocity field
+    uᵢ(λ, φ, z) = 0.1 * cosd(φ) * sind(λ) + 0.05 * z
+    ηᵢ(λ, φ, z) = (R * Ω * 0.1 + 0.1^2 / 2) * sind(φ)^2 / g * sind(λ)
+    cᵢ(λ, φ, z) = max(Gaussian(λ, φ - 5, 10), 0.1)
+    vᵢ(λ, φ, z) = 0.1
+
+    coriolis = HydrostaticSphericalCoriolis(rotation_rate = Ω)
+    free_surface = SplitExplicitFreeSurface(grid; substeps = 20, gravitational_acceleration = g)
+
+    model = HydrostaticFreeSurfaceModel(grid;
+                                        momentum_advection = WENOVectorInvariant(order=5),
+                                        free_surface,
+                                        coriolis,
+                                        closure,
+                                        tracers = (:c, :b),
+                                        timestepper,
+                                        tracer_advection = WENO(),
+                                        buoyancy = BuoyancyTracer())
+
+    set!(model, u=uᵢ, c=cᵢ, η=ηᵢ)
+
+    # CFL capped Δt
+    Δt_local = 0.1 * Δ_min(grid) / sqrt(g * grid.Lz)
+    Δt = all_reduce(min, Δt_local, architecture(grid))
+
+    stop_iteration = 20
+    simulation = Simulation(model; Δt, stop_iteration)
+
+    coord_str = grid.z isa MutableVerticalDiscretization ? "Mutable" : "Static"
+    closure_str = isnothing(closure) ? "Nothing" : "CATKE"
+    timestepper_str = timestepper == :QuasiAdamsBashforth2 ? "AB2" : "RK3"
+
+    output_filename = "hydrostatic_rotation_regression_$(coord_str)_$(closure_str)_$(timestepper_str).jld2"
+
+    u, v, w = model.velocities
+    c, b = model.tracers
+    η = model.free_surface.displacement
+
+    if regenerate_data && !(grid isa DistributedGrid) # never regenerate on Distributed
+        @warn "Generating new data for the Hydrostatic regression test."
+
+        directory =  joinpath(dirname(@__FILE__), "data")
+        outputs   = (; u, v, w, c, b, η)
+        simulation.output_writers[:fields] = JLD2Writer(model, outputs,
+                                                        dir = directory,
+                                                        schedule = IterationInterval(stop_iteration),
+                                                        filename = output_filename,
+                                                        with_halos = false,
+                                                        overwrite_existing = true)
+    end
+
+    run!(simulation)
+
+    # Test results
+    test_fields = (
+        u = Array(interior(reconstruct_global_field(u))),
+        v = Array(interior(reconstruct_global_field(v))),
+        w = Array(interior(reconstruct_global_field(w))),
+        c = Array(interior(reconstruct_global_field(c))),
+        b = Array(interior(reconstruct_global_field(b))),
+        η = Array(interior(reconstruct_global_field(η)))
+    )
+
+    if !regenerate_data
+        datadep_path = "regression_truth_data/" * output_filename
+        regression_data_path = @datadep_str datadep_path
+        file = jldopen(regression_data_path)
+
+        cpu_arch = cpu_architecture(architecture(grid))
+
+        # Data was saved with 2 halos per direction (see issue #3260)
+        truth_fields = (
+            u = file["timeseries/u/$stop_iteration"],
+            v = file["timeseries/v/$stop_iteration"],
+            w = file["timeseries/w/$stop_iteration"],
+            η = file["timeseries/η/$stop_iteration"]
+        )
+
+        close(file)
+
+        summarize_regression_test(test_fields, truth_fields)
+    else
+        truth_fields = test_fields
+    end
+
+    @test all(test_fields.u .≈ truth_fields.u)
+    @test all(test_fields.v .≈ truth_fields.v)
+    @test all(test_fields.w .≈ truth_fields.w)
+    @test all(test_fields.η .≈ truth_fields.η)
+    @test all(test_fields.c .≈ truth_fields.c)
+    @test all(test_fields.b .≈ truth_fields.b)
+
+    return nothing
+end
--- test/test_hydrostatic_regression.jl
+++ test/test_hydrostatic_regression.jl
@@ -57,41 +57,67 @@ function get_fields_from_checkpoint(filename)
 end
 
 include("regression_tests/hydrostatic_free_turbulence_regression_test.jl")
+include("regression_tests/hydrostatic_rotation_regression_test.jl")
 
 @testset "Hydrostatic Regression" begin
     @info "Running hydrostatic regression tests..."
 
     for arch in archs
-        longitudes = [(-180, 180), (-160, 160)]
-        latitudes  = [(-60, 60)]
-        zs         = [(-90, 0)]
+        @testset "Free turbulence tests" begin
+            longitudes = [(-180, 180), (-160, 160)]
+            latitudes  = [(-60, 60)]
+            zs         = [(-90, 0)]
 
-        explicit_free_surface = ExplicitFreeSurface(gravitational_acceleration = 1.0)
-        implicit_free_surface = ImplicitFreeSurface(gravitational_acceleration = 1.0,
-                                                    solver_method = :PreconditionedConjugateGradient,
-                                                    reltol = 0, abstol = 1e-15)
+            explicit_free_surface = ExplicitFreeSurface(gravitational_acceleration = 1.0)
+            implicit_free_surface = ImplicitFreeSurface(gravitational_acceleration = 1.0,
+                                                        solver_method = :PreconditionedConjugateGradient,
+                                                        reltol = 0, abstol = 1e-15)
 
-        for longitude in longitudes, latitude in latitudes, z in zs, precompute_metrics in (true, false)
-            longitude[1] == -180 ? size = (180, 60, 3) : size = (160, 60, 3)
-            grid  = LatitudeLongitudeGrid(arch; size, longitude, latitude, z, precompute_metrics, halo=(2, 2, 2))
+            for longitude in longitudes, latitude in latitudes, z in zs, precompute_metrics in (true, false)
+                longitude[1] == -180 ? size = (180, 60, 3) : size = (160, 60, 3)
+                grid  = LatitudeLongitudeGrid(arch; size, longitude, latitude, z, precompute_metrics, halo=(2, 2, 2))
 
-            split_explicit_free_surface = SplitExplicitFreeSurface(grid,
-                                                                   gravitational_acceleration = 1.0,
-                                                                   substeps = 5)
+                split_explicit_free_surface = SplitExplicitFreeSurface(grid,
+                                                                    gravitational_acceleration = 1.0,
+                                                                    substeps = 5)
 
-            for free_surface in [explicit_free_surface, implicit_free_surface, split_explicit_free_surface]
+                for free_surface in [explicit_free_surface, implicit_free_surface, split_explicit_free_surface]
 
-                # GPU + ImplicitFreeSurface + precompute metrics cannot be tested on sverdrup at the moment
-                # because "uses too much parameter space (maximum 0x1100 bytes)" error
-                if !(precompute_metrics && free_surface isa ImplicitFreeSurface && arch isa GPU) &&
-                   !(free_surface isa ImplicitFreeSurface && arch isa Distributed) # Also no implicit free surface on distributed
+                    # GPU + ImplicitFreeSurface + precompute metrics cannot be tested on sverdrup at the moment
+                    # because "uses too much parameter space (maximum 0x1100 bytes)" error
+                    if !(precompute_metrics && free_surface isa ImplicitFreeSurface && arch isa GPU) &&
+                    !(free_surface isa ImplicitFreeSurface && arch isa Distributed) # Also no implicit free surface on distributed
 
-                    testset_str, info_str = show_hydrostatic_test(grid, free_surface, precompute_metrics)
+                        testset_str, info_str = show_hydrostatic_test(grid, free_surface, precompute_metrics)
 
-                    @testset "$testset_str" begin
-                        @info "$info_str"
-                        run_hydrostatic_free_turbulence_regression_test(grid, free_surface)
+                        @testset "$testset_str" begin
+                            @info "$info_str"
+                            run_hydrostatic_free_turbulence_regression_test(grid, free_surface)
+                        end
                     end
+                end
+            end
+        end
+
+        @testset "Rotation with shear tests" begin
+            z_static  = ExponentialDiscretization(10, -500, 0)
+            z_mutable = ExponentialDiscretization(10, -500, 0; mutable=true)
+
+            closures = (nothing, CATKEVerticalDiffusivity())
+            timesteppers = (:QuasiAdamsBashforth2, :SplitRungeKutta3)
+
+            for z in (z_static, z_mutable), closure in closures, timestepper in timesteppers
+                coord_str = z isa MutableVerticalDiscretization ? "Mutable" : "Static"
+                closure_str = isnothing(closure) ? "Nothing" : "CATKE"
+                timestepper_str = timestepper == :QuasiAdamsBashforth2 ? "AB2" : "RK3"
+
+                testset_str = "Hydrostatic rotation regression [$(arch), $(coord_str) z, $(closure_str) closure, $(timestepper_str) timestepper]"
+                info_str    =  "  Testing $testset_str"
+
+                grid = LatitudeLongitudeGrid(arch; size=(150, 150, 10), latitude=(-80, 80), longitude=(0, 360), z)
+
+                @testset "$testset_str" begin
+                    run_hydrostatic_rotation_regression_test(grid, closure, timestepper; regenerate_data = true)
                 end
             end
         end
