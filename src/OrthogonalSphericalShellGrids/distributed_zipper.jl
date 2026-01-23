43dc8ee737b632a9dc19a77509f08094f4c3351b
Simone Silvestri <silvestri.simone0@gmail.com>
2026-01-23 19:27:50 +0100

    instantiate location instead of location

--- src/OrthogonalSphericalShellGrids/distributed_zipper.jl
+++ src/OrthogonalSphericalShellGrids/distributed_zipper.jl
@@ -1,7 +1,7 @@
 using Oceananigans.BoundaryConditions: get_boundary_kernels, DistributedCommunication
 using Oceananigans.DistributedComputations: cooperative_waitall!, recv_from_buffers!, distributed_fill_halo_event!
 using Oceananigans.DistributedComputations: CommunicationBuffers, fill_corners!, loc_id
-using Oceananigans.Fields: location
+using Oceananigans.Fields: instantiated_location
 
 import Oceananigans.BoundaryConditions: fill_halo_regions!
 import Oceananigans.DistributedComputations: synchronize_communication!
