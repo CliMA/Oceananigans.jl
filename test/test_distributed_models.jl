04b3ecfa332230000e270f13412dc6a95b6abac5
Sid <siddhartha.bishnu@gmail.com>
2026-01-26 18:46:50 +0000

    Increase both resolution and barotropic substeps

--- test/test_distributed_models.jl
+++ test/test_distributed_models.jl
@@ -287,12 +287,12 @@ function test_complex_boundary_conditions(Rx, Ry, child_arch)
     arch  = Distributed(child_arch, partition=Partition(Rx, Ry))
 
     # A grid with unity spacings in all directions
-    grid  = RectilinearGrid(arch, topology=(Bounded, Bounded, Bounded), size=(2*Rx, 2*Ry, 1), extent=(2*Rx,2*Ry, 1))
+    grid  = RectilinearGrid(arch, topology=(Bounded, Bounded, Bounded), size=(8Rx, 8Ry, 1), extent=(8Rx, 8Ry, 1))
     u_bc  = FluxBoundaryCondition(clock_field_dependent_boudary_condition, discrete_form=true)
     u_bcs = FieldBoundaryConditions(top=u_bc)
 
     # A model with an Euler step
-    model = HydrostaticFreeSurfaceModel(grid; boundary_conditions=(; u=u_bcs), timestepper=:QuasiAdamsBashforth2, free_surface=SplitExplicitFreeSurface(grid; substeps=5))
+    model = HydrostaticFreeSurfaceModel(grid; boundary_conditions=(; u=u_bcs), timestepper=:QuasiAdamsBashforth2, free_surface=SplitExplicitFreeSurface(grid; substeps=20))
     model.timestepper.χ = -0.5
     @test model.velocities.u.boundary_conditions.top isa typeof(u_bc)
 
