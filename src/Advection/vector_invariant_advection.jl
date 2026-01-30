b16bdaa0569de51dbb156459654b22e6168e4c6d
M.A. Kowalski <mak60@cam.ac.uk>
2026-01-30 18:19:59 -0500

    test: update doctests

--- src/Advection/vector_invariant_advection.jl
+++ src/Advection/vector_invariant_advection.jl
@@ -194,11 +194,11 @@ julia> using Oceananigans
 
 julia> WENOVectorInvariant()
 WENOVectorInvariant{5, Float64, Float32}(vorticity_order=9, vertical_order=5)
-├── vorticity_scheme: WENO{5, Float64, Float32}(order=9)
+├── vorticity_scheme: WENO{5, Float64, Float32, Oceananigans.Utils.NewtonDivWithConversion{Float32}}(order=9)
 ├── vorticity_stencil: Oceananigans.Advection.VelocityStencil
-├── vertical_advection_scheme: WENO{3, Float64, Float32}(order=5)
-├── kinetic_energy_gradient_scheme: WENO{3, Float64, Float32}(order=5)
-├── divergence_scheme: WENO{3, Float64, Float32}(order=5)
+├── vertical_advection_scheme: WENO{3, Float64, Float32, Oceananigans.Utils.NewtonDivWithConversion{Float32}}(order=5)
+├── kinetic_energy_gradient_scheme: WENO{3, Float64, Float32, Oceananigans.Utils.NewtonDivWithConversion{Float32}}(order=5)
+├── divergence_scheme: WENO{3, Float64, Float32, Oceananigans.Utils.NewtonDivWithConversion{Float32}}(order=5)
 └── upwinding: OnlySelfUpwinding
 ```
 """
