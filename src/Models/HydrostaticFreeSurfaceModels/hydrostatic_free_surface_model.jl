fb401a25accbb55dfe64c1ceb81ee3b49f5c3a91
Simone Silvestri <silvestri.simone0@gmail.com>
2026-02-03 14:00:52 +0100

    remove one space

--- src/Models/HydrostaticFreeSurfaceModels/hydrostatic_free_surface_model.jl
+++ src/Models/HydrostaticFreeSurfaceModels/hydrostatic_free_surface_model.jl
@@ -300,7 +300,6 @@ transport_velocity_fields(velocities, free_surface) =
      v = YFaceField(velocities.v.grid; boundary_conditions=velocities.v.boundary_conditions),
      w = ZFaceField(velocities.w.grid; boundary_conditions=velocities.w.boundary_conditions))
 
-
 validate_velocity_boundary_conditions(grid, velocities) = validate_vertical_velocity_boundary_conditions(velocities.w)
 
 function validate_vertical_velocity_boundary_conditions(w)
