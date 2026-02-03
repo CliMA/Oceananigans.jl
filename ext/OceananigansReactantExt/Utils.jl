aa01bf6999f8ab31a1f2d82b72bf2abc9bce0494
dkytezab <danielkytezable@gmail.com>
2026-02-02 17:32:33 -0800

    Remove commented Reactant workgroup heuristics
    Clean up ext/OceananigansReactantExt/Utils.jl by removing a large block of commented-out Reactant-specific workgroup heuristics (MAX_REACTANT_WORKGROUP and several heuristic_workgroup variants for 1D/2D/3D/4D). This is a non-functional cleanup to reduce clutter and improve readability.

--- ext/OceananigansReactantExt/Utils.jl
+++ ext/OceananigansReactantExt/Utils.jl
@@ -20,34 +20,4 @@ function prettysummary(concrete_number::ConcreteIFRTNumber)
     return string("ConcreteIFRTNumber(", prettysummary(number), ")")
 end
 
-#####
-##### Reactant-specific workgroup heuristics
-#####
-##### On Reactant, we use larger workgroups to avoid nested affine.parallel + affine.if
-##### structures that fail with raise=true. Using workgroup size ≥ worksize ensures
-##### a single workgroup, avoiding the nested structure entirely.
-#####
-
-# const MAX_REACTANT_WORKGROUP = 256
-
-# # 1D case
-# heuristic_workgroup(::Oceananigans.ReactantState, Wx) = min(Wx, MAX_REACTANT_WORKGROUP)
-
-# # 2D/3D/4D case - use workgroup that encompasses all work (up to limit)
-# function heuristic_workgroup(::Oceananigans.ReactantState, Wx::Int, Wy::Int, Wz=nothing, Wt=nothing)
-#     if Wx == 1 && Wy == 1
-#         return (1, 1)
-#     elseif Wx == 1
-#         return (1, min(MAX_REACTANT_WORKGROUP, Wy))
-#     elseif Wy == 1
-#         return (min(MAX_REACTANT_WORKGROUP, Wx), 1)
-#     else
-#         # For Reactant: use workgroup size that encompasses the work to avoid
-#         # nested affine.parallel loops that fail with raise=true
-#         wg_x = min(Wx, MAX_REACTANT_WORKGROUP)
-#         wg_y = min(Wy, MAX_REACTANT_WORKGROUP)
-#         return (wg_x, wg_y)
-#     end
-# end
-
 end # module
