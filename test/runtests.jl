d80b8d3b1e8835193ef5fc6d00198f9940197c2c
Gregory L. Wagner <wagner.greg@gmail.com>
2026-02-26 11:10:48 +0000

    Apply suggestion from @glwagner

--- test/runtests.jl
+++ test/runtests.jl
@@ -272,15 +272,13 @@ CUDA.allowscalar() do
             include("test_reactant.jl")
             include("test_reactant_fft_models.jl")
             include("test_reactant_hydrostatic_free_surface_models.jl")
-        end
-    end
-
-    if group == :reactant_2 || group == :all
-        @testset "Reactant extension tests 2" begin
-            include("test_reactant_latitude_longitude_grid.jl")
-        end
-    end
-    =#
+   # Initialization steps
+   if group == :init || group == :all
+       include("test_init.jl")
+@@ -279,6 +280,14 @@ CUDA.allowscalar() do
+           include("test_reactant_latitude_longitude_grid.jl")
+       end
+   end
 
     # Tests for Reactant correctness (comparing vanilla vs ReactantState)
     if group == :reactant_correctness || group == :all
