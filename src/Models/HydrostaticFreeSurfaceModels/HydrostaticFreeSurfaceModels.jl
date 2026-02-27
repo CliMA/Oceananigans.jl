d5f1b0b2801a6cf8b09397b9d77e045e43fcb27f
jackdfranklin <jackfranklin314@gmail.com>
2026-02-27 17:34:18 +0000

    Cleanup imports to non-existent function

--- src/Models/HydrostaticFreeSurfaceModels/HydrostaticFreeSurfaceModels.jl
+++ src/Models/HydrostaticFreeSurfaceModels/HydrostaticFreeSurfaceModels.jl
@@ -9,7 +9,7 @@ using KernelAbstractions: @index, @kernel
 using Adapt: Adapt
 
 using Oceananigans.Architectures: architecture
-using Oceananigans.Utils: launch!, launch_conditioned!,@apply_regionally
+using Oceananigans.Utils: launch!, apply_regionally
 using Oceananigans.Grids: AbstractGrid, OrthogonalSphericalShellGrid, Periodic, RectilinearGrid
 using Oceananigans.Fields: ZFaceField
 using Oceananigans.Grids: AbstractGrid, StaticVerticalDiscretization, OrthogonalSphericalShellGrid, Periodic, RectilinearGrid
