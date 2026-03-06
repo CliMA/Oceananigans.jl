97c4a937b05c3b1c6c37b959d0566dd8ea02be57
Gregory L. Wagner <wagner.greg@gmail.com>
2026-03-06 21:13:46 +0000

    Apply suggestion from @glwagner

--- ext/OceananigansReactantExt/Architectures.jl
+++ ext/OceananigansReactantExt/Architectures.jl
@@ -30,7 +30,7 @@ on_architecture(::CPU, a::AnyConcreteReactantArray) = Array(a)
 on_architecture(::CPU, a::SubArray{<:Any,<:Any,<:AnyConcreteReactantArray}) = Array(a)
 
 using OffsetArrays: OffsetArray
-on_architecture(::CPU, a::OffsetArray{<:Any,<:Any,<:AnyConcreteReactantArray}) =
+on_architecture(::CPU, a::OffsetArray{<:Any, <:Any, <:AnyConcreteReactantArray}) =
     OffsetArray(Array(parent(a)), a.offsets)
 
 const ArraysToRArray = Union{Array,
