b62c63ce1ac9e521d59eb5a7390846c1b390f3b2
Simone Silvestri <silvestri.simone0@gmail.com>
2026-03-12 16:41:26 +0100

    Refactor ζ₃ᶠᶠᶜ to handle immersed nodes
    Refactor ζ₃ᶠᶠᶜ function to include peripheral node check

--- src/Operators/vorticity_operators.jl
+++ src/Operators/vorticity_operators.jl
@@ -1,3 +1,5 @@
+using Oceananigans.Grids: peripheral_node, Face, Center
+
 """ Vertical circulation associated with horizontal velocities u, v. """
 @inline Γᶠᶠᶜ(i, j, k, grid, u, v) = δxᶠᶠᶜ(i, j, k, grid, Δy_qᶜᶠᶜ, v) - δyᶠᶠᶜ(i, j, k, grid, Δx_qᶠᶜᶜ, u)
 
@@ -6,4 +8,8 @@
 
 The vertical vorticity associated with horizontal velocities ``u`` and ``v``.
 """
-@inline ζ₃ᶠᶠᶜ(i, j, k, grid, u, v) = Γᶠᶠᶜ(i, j, k, grid, u, v) * Az⁻¹ᶠᶠᶜ(i, j, k, grid)
+@inline function ζ₃ᶠᶠᶜ(i, j, k, grid, u, v) 
+    ζᶠᶠᶜ = Γᶠᶠᶜ(i, j, k, grid, u, v) * Az⁻¹ᶠᶠᶜ(i, j, k, grid)
+    immersed = peripheral_node(i, j, k, grid, Face(), Face(), Center())
+    return ifelse(immersed, zero(grid), ζᶠᶠᶜ)
+end
