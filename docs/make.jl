85a27b8c184c48105b971cc915755d326e3942ae
Benoît Pasquier <4486578+briochemc@users.noreply.github.com>
2026-03-20 15:15:33 +1100

    Temporarily drop Benchmark section from Docs + delete `legacy_benchmarks` (#5412)

--- docs/make.jl
+++ docs/make.jl
@@ -166,7 +166,7 @@ appendix_pages = [
     "Staggered grid" => "appendix/staggered_grid.md",
     "Fractional step method" => "appendix/fractional_step.md",
     "Convergence tests" => "appendix/convergence_tests.md",
-    "Performance benchmarks" => "appendix/benchmarks.md",
+    # "Performance benchmarks" => "appendix/benchmarks.md", # commented out after https://github.com/CliMA/Oceananigans.jl/pull/5317, to be reinstated with the new benchmarks
     "Library" => "appendix/library.md",
     "Function index" => "appendix/function_index.md"
 ]
