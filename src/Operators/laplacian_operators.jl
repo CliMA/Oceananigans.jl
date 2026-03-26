d41403735c98db0b6aa9b3461b6697b1b61cee2a
Simone Silvestri <silvestri.simone0@gmail.com>
2026-03-26 13:40:10 +0100

    typo

--- src/Operators/laplacian_operators.jl
+++ src/Operators/laplacian_operators.jl
@@ -36,23 +36,23 @@ which ends up at the location `ccc`.
 @inline function ∇²ᶜᶜᶜ(i, j, k, grid, c)
     return V⁻¹ᶜᶜᶜ(i, j, k, grid) * (δxᶜᵃᵃ(i, j, k, grid, Ax_qᶠᶜᶜ, ∂xᵣᶠᶜᶜ, c) +
                                     δyᵃᶜᵃ(i, j, k, grid, Ay_qᶜᶠᶜ, ∂yᵣᶜᶠᶜ, c) +
-                                    δzᵃᵃᶜ(i, j, k, grid, Az_qᶜᶜᶠ, ∂zᵣᶜᶜᶠ, c))
+                                    δzᵃᵃᶜ(i, j, k, grid, Az_qᶜᶜᶠ,  ∂zᶜᶜᶠ, c))
 end
 
 @inline function ∇²ᶠᶜᶜ(i, j, k, grid, u)
     return V⁻¹ᶠᶜᶜ(i, j, k, grid) * (δxᶠᵃᵃ(i, j, k, grid, Ax_qᶜᶜᶜ, ∂xᵣᶜᶜᶜ, u) +
                                     δyᵃᶜᵃ(i, j, k, grid, Ay_qᶠᶠᶜ, ∂yᵣᶠᶠᶜ, u) +
-                                    δzᵃᵃᶜ(i, j, k, grid, Az_qᶠᶜᶠ, ∂zᵣᶠᶜᶠ, u))
+                                    δzᵃᵃᶜ(i, j, k, grid, Az_qᶠᶜᶠ,  ∂zᶠᶜᶠ, u))
 end
 
 @inline function ∇²ᶜᶠᶜ(i, j, k, grid, v)
     return V⁻¹ᶜᶠᶜ(i, j, k, grid) * (δxᶜᵃᵃ(i, j, k, grid, Ax_qᶠᶠᶜ, ∂xᵣᶠᶠᶜ, v) +
                                     δyᵃᶠᵃ(i, j, k, grid, Ay_qᶜᶜᶜ, ∂yᵣᶜᶜᶜ, v) +
-                                    δzᵃᵃᶜ(i, j, k, grid, Az_qᶜᶠᶠ, ∂zᵣᶜᶠᶠ, v))
+                                    δzᵃᵃᶜ(i, j, k, grid, Az_qᶜᶠᶠ,  ∂zᶜᶠᶠ, v))
 end
 
 @inline function ∇²ᶜᶜᶠ(i, j, k, grid, w)
     return V⁻¹ᶜᶜᶠ(i, j, k, grid) * (δxᶜᵃᵃ(i, j, k, grid, Ax_qᶠᶜᶠ, ∂xᵣᶠᶜᶠ, w) +
                                     δyᵃᶜᵃ(i, j, k, grid, Ay_qᶜᶠᶠ, ∂yᵣᶜᶠᶠ, w) +
-                                    δzᵃᵃᶠ(i, j, k, grid, Az_qᶜᶜᶜ, ∂zᵣᶜᶜᶜ, w))
+                                    δzᵃᵃᶠ(i, j, k, grid, Az_qᶜᶜᶜ,  ∂zᶜᶜᶜ, w))
 end
