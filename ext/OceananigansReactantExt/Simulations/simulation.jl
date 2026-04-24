6a55a7a12db5c49539057ee03aacd60287d3ace3
Julia Sloan <jsloan@caltech.edu>
2026-04-24 14:34:59 -0700

    (0.107.4) unify Simulation interface in ReactantExt

--- ext/OceananigansReactantExt/Simulations/simulation.jl
+++ ext/OceananigansReactantExt/Simulations/simulation.jl
@@ -2,7 +2,13 @@ const ReactantSimulation = Simulation{<:ReactantModel}
 
 function Simulation(model::ReactantModel; Δt,
                     verbose = true,
-                    stop_iteration = Inf)
+                    stop_iteration = Inf,
+                    stop_time = nothing,
+                    wall_time_limit = Inf,
+                    align_time_step = false,
+                    minimum_relative_step = 0)
+
+   @assert isnothing(stop_time) "`stop_time` is not supported for ReactantModel"
 
    diagnostics = nothing
    output_writers = nothing
@@ -13,17 +19,17 @@ function Simulation(model::ReactantModel; Δt,
    return Simulation(model,
                      Δt,
                      Float64(stop_iteration),
-                     nothing, # disallow stop_time
-                     Inf,
+                     stop_time,
+                     Float64(wall_time_limit),
                      diagnostics,
                      output_writers,
                      callbacks,
                      0.0,
-                     false,
+                     align_time_step,
                      false,
                      false,
                      verbose,
-                     0.0)
+                     Float64(minimum_relative_step))
 end
 
 iteration(sim::ReactantSimulation) = Reactant.to_number(iteration(sim.model))
