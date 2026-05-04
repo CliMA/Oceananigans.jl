6bc28111b8edc7041dbaaa26098e588ba3cbd4c4
Mosè Giordano <mose@gnu.org>
2026-05-04 09:36:05 +0100

    Revert "Include MPI to prevent precompilation output capture"
    This reverts commit a84e8e652b20d284316098dddbfac413ba7d0e42.

--- docs/make.jl
+++ docs/make.jl
@@ -9,7 +9,6 @@ Distributed.addprocs(2)
     using Printf
 
     using CUDA
-    using MPI # to avoid capturing precompilation output
     using CairoMakie # to avoid capturing precompilation output by Literate
     set_theme!(Theme(fontsize=20))
     CairoMakie.activate!(type = "png")
