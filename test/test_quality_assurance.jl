686cd7171514563827f2edd75804f226bf4b388f
Mosè Giordano <765740+giordano@users.noreply.github.com>
2026-05-18 14:23:09 +0200

    Update number of ambiguities in tests (#5603)

--- test/test_quality_assurance.jl
+++ test/test_quality_assurance.jl
@@ -34,7 +34,7 @@ end
     # Until we resolve all ambiguities, we make sure we don't increase them.
     # Do not increase this number. If ambiguities increase, resolve them before merging.
     number_of_ambiguities = length(detect_ambiguities(Oceananigans; recursive=true))
-    @test number_of_ambiguities <= 326
+    @test number_of_ambiguities <= 323
     @info "Number of ambiguities: $number_of_ambiguities"
 
     modules = (
