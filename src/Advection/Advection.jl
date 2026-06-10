fa94960e9ccbcfda7f3eaf54e8caf81030e14670
Tomás Chor <tomaschor@gmail.com>
2026-06-09 20:07:10 -0700

    Update src/Advection/Advection.jl
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>

--- src/Advection/Advection.jl
+++ src/Advection/Advection.jl
@@ -21,9 +21,9 @@ export
     EnstrophyConserving
 
 using Adapt: Adapt
-using OffsetArrays: OffsetArray
-using MuladdMacro: @muladd
 using DocStringExtensions: SIGNATURES
+using MuladdMacro: @muladd
+using OffsetArrays: OffsetArray
 
 using Oceananigans: Oceananigans, fully_supported_float_types
 using Oceananigans.Architectures: Architectures, architecture, on_architecture, CPU
