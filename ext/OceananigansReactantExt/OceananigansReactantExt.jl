8c523fbfaeb2ee3b2808cdb98d2a694e7be6c39c
jlk9 <josek97@utexas.edu>
2026-06-12 14:04:23 -0400

    Adding promotion for vertical coordinates

--- ext/OceananigansReactantExt/OceananigansReactantExt.jl
+++ ext/OceananigansReactantExt/OceananigansReactantExt.jl
@@ -220,7 +220,7 @@ Base.@nospecializeinfer function Reactant.traced_type_inner(
 
     FT2 = Reactant.traced_type_inner(FT, seen, mode, track_numbers, sharding, runtime)
 
-    for NF in (XF2, XC2, YF2, YC2, DXCC2, DXFC2, DYCF2, DYCF2, DXFF2)
+    for NF in (XF2, XC2, YF2, YC2, DXCC2, DXFC2, DXCF2, DYFC2, DYCF2, DXFF2)
         if NF === Nothing
            continue
         end
@@ -229,6 +229,15 @@ Base.@nospecializeinfer function Reactant.traced_type_inner(
 
     res = Oceananigans.Grids.LatitudeLongitudeGrid{FT2, TX2, TY2, TZ2, Z2, DXF2, DXC2, XF2, XC2, DYF2, DYC2, YF2, YC2,
                                                    DXCC2, DXFC2, DXCF2, DXFF2, DYFC2, DYCF2, Arch, I2}
+    
+    #=
+    @show " "
+    @show "We hit traced type inner!"
+    @show OA
+    @show res
+    @show " "
+    =#
+
     return res
 end
 
@@ -241,35 +250,46 @@ Base.@nospecializeinfer function Reactant.traced_type_inner(
         @nospecialize(runtime)
     ) where {FT, TX, TY, TZ, CZ, FX, FY, VX, VY, Arch}
 
-        @show (FT, TX, TY, TZ, CZ, FX, FY, VX, VY)
+    #@show (FT, TX, TY, TZ, CZ, FX, FY, VX, VY)
 
-        TX2 = Reactant.traced_type_inner(TX, seen, mode, track_numbers, sharding, runtime)
-        TY2 = Reactant.traced_type_inner(TY, seen, mode, track_numbers, sharding, runtime)
-        TZ2 = Reactant.traced_type_inner(TZ, seen, mode, track_numbers, sharding, runtime)
-        CZ2 = Reactant.traced_type_inner(CZ, seen, mode, track_numbers, sharding, runtime)
-        FX2 = Reactant.traced_type_inner(FX, seen, mode, track_numbers, sharding, runtime)
-        FY2 = Reactant.traced_type_inner(FY, seen, mode, track_numbers, sharding, runtime)
-        VX2 = Reactant.traced_type_inner(VX, seen, mode, track_numbers, sharding, runtime)
-        VY2 = Reactant.traced_type_inner(VY, seen, mode, track_numbers, sharding, runtime)
-        FT2 = Reactant.traced_type_inner(FT, seen, mode, track_numbers, sharding, runtime)
+    TX2 = Reactant.traced_type_inner(TX, seen, mode, track_numbers, sharding, runtime)
+    TY2 = Reactant.traced_type_inner(TY, seen, mode, track_numbers, sharding, runtime)
+    TZ2 = Reactant.traced_type_inner(TZ, seen, mode, track_numbers, sharding, runtime)
+    CZ2 = Reactant.traced_type_inner(CZ, seen, mode, track_numbers, sharding, runtime)
+    FX2 = Reactant.traced_type_inner(FX, seen, mode, track_numbers, sharding, runtime)
+    FY2 = Reactant.traced_type_inner(FY, seen, mode, track_numbers, sharding, runtime)
+    VX2 = Reactant.traced_type_inner(VX, seen, mode, track_numbers, sharding, runtime)
+    VY2 = Reactant.traced_type_inner(VY, seen, mode, track_numbers, sharding, runtime)
+    FT2 = Reactant.traced_type_inner(FT, seen, mode, track_numbers, sharding, runtime)
 
-        for NF in (FX2, FY2, VX2, VY2)
-            if NF === Nothing
-               continue
-            end
-            FT2 = Reactant.promote_traced_type(FT2, eltype(NF))
+    for NF in (FX2, FY2, VX2, VY2)
+        if NF === Nothing
+            continue
         end
-
-        @show (FT2, TX2, TY2, TZ2, CZ2, FX2, FY2, VX2, VY2)
-
-        res = Oceananigans.Grids.RectilinearGrid{FT2, TX2, TY2, TZ2, CZ2, FX2, FY2, VX2, VY2, Arch}
-
-        @show "We hit traced type inner!"
-        @show OA
-        @show res
-
-        return res
+        FT2 = Reactant.promote_traced_type(FT2, eltype(NF))
     end
+
+    # Also promote over the vertical discretization's data:
+    if CZ2 !== Nothing
+        for P in Base.fieldtypes(CZ2)
+            P === Nothing && continue
+            FT2 = Reactant.promote_traced_type(FT2, eltype(P))
+        end
+    end
+
+    #@show (FT2, TX2, TY2, TZ2, CZ2, FX2, FY2, VX2, VY2)
+
+    res = Oceananigans.Grids.RectilinearGrid{FT2, TX2, TY2, TZ2, CZ2, FX2, FY2, VX2, VY2, Arch}
+    #=
+    @show " "
+    @show "We hit traced type inner!"
+    @show OA
+    @show res
+    @show " "
+    =#
+
+    return res
+end
 
 @inline function Reactant.make_tracer(
         seen,
@@ -280,9 +300,9 @@ Base.@nospecializeinfer function Reactant.traced_type_inner(
 
         res = Reactant.make_tracer_via_immutable_constructor(seen, prev, args...; kwargs...)
 
-        @show "Hit make tracer!"
-        @show Core.Typeof(prev)
-        @show Core.Typeof(res)
+        #@show "Hit make tracer!"
+        #@show Core.Typeof(prev)
+        #@show Core.Typeof(res)
 
         return res
 end
