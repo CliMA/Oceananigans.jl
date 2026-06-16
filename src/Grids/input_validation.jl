14231503aa2eccd23d710de5dbc7fa973a224fc1
Emma Boland <emmomp@bas.ac.uk>
2026-06-16 14:12:52 +0100

    Update input_validation.jl
    Added missing )

--- src/Grids/input_validation.jl
+++ src/Grids/input_validation.jl
@@ -156,7 +156,7 @@ end
 
 function validate_dimension_specification(T, ξ::ExponentialDiscretization, dir, N, FT)
     if !(length(ξ.faces)) == N+1
-        throw(ArgumentError("Coordinate $(summary(ξ) has length $(length(ξ.faces)) while direction $(dir) requires a coordinate of length $(N+1)"))
+        throw(ArgumentError("Coordinate $(summary(ξ)) has length $(length(ξ.faces)) while direction $(dir) requires a coordinate of length $(N+1)"))
     end
     return ξ
 end
