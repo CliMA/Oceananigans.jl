a4efd9abb6ab6847b2d4ec99cc58775d9172de7b
tomaschor <tomaschor@gmail.com>
2026-07-08 12:58:58 -0500

    remove extra methods that created ambiguities

--- src/DistributedComputations/halo_communication.jl
+++ src/DistributedComputations/halo_communication.jl
@@ -183,10 +183,6 @@ cooperative_waitall!(req::Array{MPI.Request}) = MPI.Waitall(req)
 # argument used only for distributed halo-filling boundary conditions
 distributed_fill_halo_event!(c, kernel!, bcs, loc, arch, grid, buffers, args...; kwargs...) = fill_halo_event!(c, kernel!, bcs, loc, grid, args...; kwargs...)
 
-# fill_halo_event! does nothing for DistributedFillHalo (only `distributed_fill_halo_event!` handle DistributedFillHalo)
-@inline fill_halo_event!(c, kernel!::DistributedFillHalo, bcs::Tuple{Any},      loc, grid, args...; kwargs...) = nothing
-@inline fill_halo_event!(c, kernel!::DistributedFillHalo, bcs::Tuple{Any, Any}, loc, grid, args...; kwargs...) = nothing
-
 # There are two additional keyword arguments (with respect to serial `fill_halo_event!`s) that take an effect on `DistributedGrids`:
 # - only_local_halos: if true, only the local halos are filled, i.e. corresponding to non-communicating boundary conditions
 # - async: if true, ansynchronous MPI communication is enabled
