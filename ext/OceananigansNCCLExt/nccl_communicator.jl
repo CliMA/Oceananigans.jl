5e7a90cb4dde930b6edbecc7c720af941ac858c6
Mosè Giordano <mose@gnu.org>
2026-07-16 23:01:44 +0100

    Revert "DROP ME: try Int8 again"
    This reverts commit f0003a2d91ac9895dc5cad95a9974065beec415a.

--- ext/OceananigansNCCLExt/nccl_communicator.jl
+++ ext/OceananigansNCCLExt/nccl_communicator.jl
@@ -52,7 +52,7 @@ function create_nccl_comm_from_mpi(mpi_subcomm)
         MPI.Bcast!(id_bytes, mpi_subcomm; root=0)
 
         # Reconstruct UniqueID on all ranks.
-        nccl_internal = ntuple(i -> reinterpret(Int8, id_bytes[i]), Val(128))
+        nccl_internal = ntuple(i -> reinterpret(Cchar, id_bytes[i]), Val(128))
         nccl_id_all = NCCL.UniqueID(nccl_internal)
 
         nccl_comm = NCCL.Communicator(nranks, my_rank; unique_id=nccl_id_all)
