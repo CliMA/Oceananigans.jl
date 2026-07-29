a6a4305bc3506a638a5cac8cc20b708ddafbbe56
Simone Silvestri <silvestri.simone0@gmail.com>
2026-07-29 08:24:27 +0200

    Fix ConservativeRegridding extension (#5827)
    * fix stuff
    
    * add the test
    
    ---------
    
    Co-authored-by: Taimoor Sohail <taimoor.sohail@unimelb.edu.au>

--- ext/OceananigansConservativeRegriddingExt.jl
+++ ext/OceananigansConservativeRegriddingExt.jl
@@ -1,12 +1,12 @@
 module OceananigansConservativeRegriddingExt
 
 using ConservativeRegridding: Regridder, regrid!
-using Oceananigans.AbstractOperations: RegriddedOperation
+using Oceananigans.AbstractOperations: AbstractOperations, RegriddedOperation
 using Oceananigans.Architectures: Architectures, architecture, CPU
 using Oceananigans.Fields: Fields, AbstractField, Field
 using Oceananigans.ImmersedBoundaries: underlying_grid
 
-function RegriddedOperation(source::AbstractField{LX, LY, LZ}, destination_grid) where {LX, LY, LZ}
+function AbstractOperations.RegriddedOperation(source::AbstractField{LX, LY, LZ}, destination_grid) where {LX, LY, LZ}
     source_architecture = architecture(source)
     destination_grid = Architectures.on_architecture(source_architecture, destination_grid)
     destination = Field{LX, LY, LZ}(destination_grid)
