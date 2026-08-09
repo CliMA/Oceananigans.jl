c2b1e539a1ac1558b72371b4cfcb3914589caffe
Navid C. Constantinou <navidcy@users.noreply.github.com>
2026-08-10 08:45:53 +1000

    import interpolation operators

--- src/StokesDrifts.jl
+++ src/StokesDrifts.jl
@@ -13,6 +13,7 @@ export
 using Adapt: Adapt, adapt
 using Oceananigans.Fields: Field
 using Oceananigans.Grids: AbstractGrid, node, Face, Center
+using Oceananigans.Operators: ℑxzᶠᵃᶜ, ℑyzᵃᶠᶜ, ℑxzᶜᵃᶠ, ℑyzᵃᶜᶠ, ℑzᵃᵃᶜ, ℑzᵃᵃᶠ
 using Oceananigans.Utils: prettysummary
 
 #####
