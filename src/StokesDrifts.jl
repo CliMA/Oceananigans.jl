f3aa63a88483decf2bf7015d2345bf67a5fc8e7e
Navid C. Constantinou <navidcy@users.noreply.github.com>
2026-08-10 11:17:24 +1000

    add missing import

--- src/StokesDrifts.jl
+++ src/StokesDrifts.jl
@@ -13,7 +13,7 @@ export
 using Adapt: Adapt, adapt
 using Oceananigans.Fields: Field
 using Oceananigans.Grids: AbstractGrid, node, znode, Face, Center
-using Oceananigans.Operators: ℑxzᶠᵃᶜ, ℑyzᵃᶠᶜ, ℑxzᶜᵃᶠ, ℑyzᵃᶜᶠ, ℑzᵃᵃᶜ
+using Oceananigans.Operators: ℑxzᶠᵃᶜ, ℑxyᶠᶜᵃ, ℑyzᵃᶠᶜ, ℑxzᶜᵃᶠ, ℑyzᵃᶜᶠ, ℑzᵃᵃᶜ
 using Oceananigans.Utils: prettysummary
 
 #####
