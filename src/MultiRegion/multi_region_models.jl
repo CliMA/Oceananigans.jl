06651bdb05d955545d7fcf62a33fba8afebbb90d
Simone Silvestri <silvestri.simone0@gmail.com>
2026-08-26 17:27:42 +0200

    fix the multi region runner

--- src/MultiRegion/multi_region_models.jl
+++ src/MultiRegion/multi_region_models.jl
@@ -170,7 +170,7 @@ function SplitExplicitFreeSurfaces.iterate_split_explicit!(free_surface::FillHal
     @apply_regionally velocity_kernel!, _     = configure_kernel(arch, grid, parameters, _split_explicit_barotropic_velocity!)
     @apply_regionally free_surface_kernel!, _ = configure_kernel(arch, grid, parameters, _split_explicit_free_surface!)
 
-    U_args = (grid, Val(true), Δτᴮ, η, U, V, GUⁿ, GVⁿ, g, Ũ, Ṽ, timestepper, cᵁ, cⱽ)
+    U_args = (grid, Val(true), Δτᴮ, η, U, V, GUⁿ, GVⁿ, g, Ũ, Ṽ, timestepper)
     η_args = (grid, Val(true), Δτᴮ, η, U, V, F, clock, η̅, U̅, V̅, timestepper)
 
     @unroll for substep in 1:Nsubsteps
