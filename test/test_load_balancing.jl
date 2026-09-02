af4b4f93c2942190269eddaa200bc33dbd35bcf7
jackdfranklin <jackfranklin314@gmail.com>
2026-09-02 15:48:32 +0100

    Fix and add extra load balancing tests

--- test/test_load_balancing.jl
+++ test/test_load_balancing.jl
@@ -39,9 +39,19 @@ rectilinear_constructors = [
     Iterators.product(sizes, halos, xs, ys, zs, topologies)
 ]
 
+ib_constructors = [
+  bottom_height -> GridFittedBottom(bottom_height),
+  bottom_height -> PartialCellBottom(bottom_height)
+]
+
+strategies = [nothing, SimplifiedGeneralisedBlockDistribution(), GeneralisedBlockDistribution()]
+
+partitions = [Partition(x, y) for (x,y) in Iterators.product([1,2,4],[1,2,4])]
+
 grid_constructors = Iterators.flatten([latlong_constructors, rectilinear_constructors])
 
-@testset "Total active cells consistent" for (arch, grid_constructor) in Iterators.product(archs, grid_constructors)
+@testset "Total active cells consistent" for (arch, grid_constructor, ib_constructor) in
+    Iterators.product(archs, grid_constructors, ib_constructors)
 
   underlying_grid = grid_constructor(arch)
 
@@ -49,7 +59,7 @@ grid_constructors = Iterators.flatten([latlong_constructors, rectilinear_constru
 
   bottom_height = -30.0 .* rand(Float64, (Nx, Ny)) .+ 15.0
 
-  ib = GridFittedBottom(bottom_height)
+  ib = ib_constructor(bottom_height)
   immersed_grid = ImmersedBoundaryGrid(underlying_grid, ib; active_cells_map = true)
 
   active_cells_map = immersed_grid.interior_active_cells
@@ -59,12 +69,35 @@ grid_constructors = Iterators.flatten([latlong_constructors, rectilinear_constru
 
 end
 
-@testset "Partitioning consistent" for (len, ranks) in product((10, 100, 1000), (2,4,8))
-  weights = rand(len)
+@testset "Partitioning consistent" begin
 
-  partitions = partition_1d(weights, ranks)
+  @testset "1d partitioning consistent" for (len, ranks) in Iterators.product((10, 100, 1000), (2,4,8))
+    weights = rand(len)
 
-  sizes = ends_to_sizes(partitions)
+    partitions = partition_1d(weights, ranks)
 
-  @test sum(sizes) == l
+    sizes = ends_to_sizes(partitions)
+
+    @test sum(sizes) == len
+  end
+
+  @testset "Grid partitioning consistent" for (arch, gridc, ibc, strategy, partition) in
+      Iterators.product(archs, grid_constructors, ib_constructors, strategies, partitions)
+
+    grid = gridc(arch)
+
+    Nx, Ny, Nz = size(underlying_grid)
+
+    bottom_height = -30.0 .* rand(Float64, (Nx, Ny)) .+ 15.0
+    ib = ibc(bottom_height)
+    weight_map = create_weight_map(grid, ib)
+    balanced_partition = create_balanced_partition(strategy, partition, weight_map)
+
+    @test sum(balanced_partition.x) == Nx
+    @test sum(balanced_partition.y) == Ny
+
+    @test length(balanced_partition.x) == partition.x
+    @test length(balanced_partition.y) == partition.y
+
+  end
 end
