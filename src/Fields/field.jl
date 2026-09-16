4b6e82243dfe0f3be28047d5294e8ca1c0768a5f
jackdfranklin <jackfranklin314@gmail.com>
2026-09-16 16:26:00 +0100

    Remove extra type from Field constructor

--- src/Fields/field.jl
+++ src/Fields/field.jl
@@ -31,7 +31,7 @@ struct Field{LX, LY, LZ, O, G, I, D, T, B, S, F} <: AbstractField{LX, LY, LZ, G,
     Base.@constprop :aggressive function Field{LX, LY, LZ}(grid::G, data::D, bcs::B, indices::I, op::O, status::S, buffers::F) where {LX, LY, LZ, G, D, B, O, S, I, F}
         T = eltype(data)
         @apply_regionally local_bcs = construct_boundary_conditions_kernels(bcs, data, grid, (LX(), LY(), LZ()), indices) # Adding the kernels to the bcs
-        return new{LX, LY, LZ, O, G, I, D, T, typeof(local_bcs), S, F, Nothing}(grid, data, local_bcs, indices, op, status, buffers, nothing)
+        return new{LX, LY, LZ, O, G, I, D, T, typeof(local_bcs), S, F}(grid, data, local_bcs, indices, op, status, buffers, nothing)
     end
 end
 
