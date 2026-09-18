f259ee66bdc59cb422ce82e9f413c4f4d6452cc5
jackdfranklin <jackfranklin314@gmail.com>
2026-09-18 15:54:45 +0100

    Return to using a while loop to sync CuEvents Trying to avoid stream synchronizations

--- ext/OceananigansCUDAExt.jl
+++ ext/OceananigansCUDAExt.jl
@@ -152,7 +152,13 @@ end
   return event
 end
 
-DC.sync_event(event::CUDA.CuEvent) = CUDA.synchronize(event)
+function DC.sync_event(event::CUDA.CuEvent)
+  event_complete = CUDA.isdone(event)
+  while !event_complete
+    event_complete = CUDA.isdone(event)
+  end
+  return nothing
+end
 
 # Use faster versions of `newton_div` on Nvidia GPUs
 CUDA.@device_override UT.newton_div(::Type{UT.BackendOptimizedDivision}, a, b) = a * fast_inv_cuda(b)
