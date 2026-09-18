5832bb134f71714f17db1a6303cccf26d9eac0e5
jackdfranklin <jackfranklin314@gmail.com>
2026-09-18 09:59:20 +0100

    Remove unnecessary `synchronize_communication`s

--- src/DistributedComputations/distributed_fields.jl
+++ src/DistributedComputations/distributed_fields.jl
@@ -47,9 +47,6 @@ function _set!(u::DistributedField, V::VT) where {VT}
     NV = size(V)
     Nu = global_size(u)
 
-    synchronize_communication!(u)
-    synchronize_communication!(V)
-
     # Suppress singleton indices
     NV′ = filter(n -> n > 1, NV)
     Nu′ = filter(n -> n > 1, Nu)
@@ -64,7 +61,6 @@ function _set!(u::DistributedField, V::VT) where {VT}
 end
 
 function set!(u::DistributedField, V::Field)
-    synchronize_communication!(u)
     if size(V) == global_size(u)
         v = partition(V, u)
         return set_to_array!(u, v)
@@ -115,7 +111,6 @@ $(TYPEDSIGNATURES)
 Reconstruct a global field from a local field by combining the data from all processes.
 """
 function reconstruct_global_field(field::DistributedField)
-    synchronize_communication!(field)
     arch = architecture(field)
     field_indices = field.indices
 
@@ -156,8 +151,6 @@ partition_dimensions(f::DistributedField) = partition_dimensions(architecture(f)
 function maybe_all_reduce!(op, f::ReducedAbstractField)
     reduced_dims   = reduced_dimensions(f)
     partition_dims = partition_dimensions(f)
-
-    synchronize_communication!(f)
 
     if any([dim ∈ partition_dims for dim in reduced_dims])
         all_reduce!(op, parent(f), architecture(f))
